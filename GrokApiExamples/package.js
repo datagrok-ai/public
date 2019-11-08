8aecc37121c397c25cdd054aede4ec43c5fd3fa6
Alex <alex.aprm@Gmail.com>
2019-11-08 16:59:15 +0300

    Grok API Examples naming

--- GrokApiExamples/package.js
+++ GrokApiExamples/package.js
@@ -1,5 +1,11 @@
-class ExamplesPackage extends GrokPackage {
+class GrokApiExamplesPackage extends GrokPackage {
 
     // Guaranteed to get called exactly once before the execution of any function below
     init() { console.log('Examples package initialized.'); }
+    
+    //description: test
+    void test() {
+       alert('Examples package test');
+    }
+    	
 }
