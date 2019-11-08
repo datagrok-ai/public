1f3c91b318e9f15095bd0ba93f3cf448cd7431f8
Vasiliy Nerozin <vnerozin@gmail.com>
2019-11-08 12:05:45 +0300

    GROK-5821: Pedometer JS demo

--- Pedometer/package.js
+++ Pedometer/package.js
@@ -13,9 +13,18 @@ class PedometerPackage extends GrokPackage {
     //input: column y {semType: Accelerometer-Y} [Y axis]
     //input: column z {semType: Accelerometer-Z} [Z axis]
     pedometer(accel, x, y, z) {
-        let sampleRate = 32
-        // Sample rate input
-        // Execute StepsDetector
-        // Line Chart with columns
+        let view = gr.getTableView(accel.name);
+
+        let viewer = view.markup({content: ''});
+        let sampleRate = ui.intInput('Sample rate, Hz', 32);
+        viewer.root.appendChild(ui.inputs([sampleRate]));
+
+        gr.callFunc('Demo:PythonScripts:DetectSteps', {"accel": accel, "x": x, "y": y, "z": z, "sample_rate": sampleRate.value})
+            .then(result => {
+                view.lineChart({
+                    xColumnName: 'time',
+                    yColumnNames: [x.name, y.name, z.name, 'steps']
+                })
+            });
     }
 }
