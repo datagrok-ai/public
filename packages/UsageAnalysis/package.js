b4f764375a3e3fe3029d3ef74133647b53b832d6
Vasiliy Nerozin <vnerozin@gmail.com>
2019-12-02 18:17:31 +0300

    GROK-106: UI usage analysis system

--- packages/UsageAnalysis/package.js
+++ packages/UsageAnalysis/package.js
@@ -4,8 +4,10 @@ class UsageAnalysisPackage extends GrokPackage {
 
     //tags: app
     startApp() {
+        let acc = null;
         let view = gr.newView('Usage');
         let results = ui.div();
+        let panesExpanded = {};
 
         let date = ui.stringInput('Date', 'today');
         date.root.classList.add('usage-analysis-date', 'pure-form');
@@ -16,7 +18,7 @@ class UsageAnalysisPackage extends GrokPackage {
         users.doDrop = (user) => users.addTag(user.login);
 
         let addUser = ui.div([ui.iconFA('plus', () => {
-            gr.dapi.users.list().then((allUsers) => {
+            gr.dapi.users.order('login').list().then((allUsers) => {
                 Menu.popup()
                     .items(allUsers.map(u => u.login), (item) => users.addTag(item))
                     .show();
@@ -24,24 +26,36 @@ class UsageAnalysisPackage extends GrokPackage {
         })], 'usage-analysis-users-plus');
 
         function showUsage() {
-            let acc = ui.accordion();
+            if (acc !== null)
+                for (let p of acc.panes)
+                    panesExpanded[p.name] = p.expanded;
 
-            function addPane(paneName, queryName, f) {
+            acc = ui.accordion();
+
+            function addPane(paneName, queryName, f, supportUsers = true) {
+                if (!(paneName in panesExpanded))
+                    panesExpanded[paneName] = false;
                 acc.addPane(paneName, () => {
                     let host = ui.div([], 'usage-analysis-card');
                     host.appendChild(ui.loader());
-                    gr.query('UsageAnalysis:' + queryName, {'date': date.value}).then((t) => {
+                    let params = {'date': date.value};
+                    let selectedUsers = users.tags;
+                    if (supportUsers && selectedUsers.length !== 0) {
+                        queryName += 'AndUsers';
+                        params['users'] = selectedUsers;
+                    }
+                    gr.query('UsageAnalysis:' + queryName, params).then((t) => {
                         host.removeChild(host.firstChild);
                         host.appendChild(f(t));
                     });
                     return host;
-                });
+                }, panesExpanded[paneName]);
             }
 
             while (results.firstChild)
                 results.removeChild(results.firstChild);
 
-            acc.addPane('Users', () => {
+            acc.addPane('Unique users', () => {
                 let host = ui.div();
                 host.appendChild(ui.loader());
                 gr.query('UsageAnalysis:UniqueUsersByDate', {'date': date.value})
@@ -59,12 +73,13 @@ class UsageAnalysisPackage extends GrokPackage {
             addPane('Errors', 'ErrorsOnDate', (t) => Viewer.grid(t).root);
             addPane('Event Types', 'EventsSummaryOnDate', (t) => Viewer.barChart(t).root);
             addPane('Error Types', 'ErrorsSummaryOnDate', (t) => Viewer.barChart(t).root);
-            addPane('Test Tracking', 'ManualActivityByDate', (t) => Viewer.grid(t).root);
+            addPane('Test Tracking', 'ManualActivityByDate', (t) => Viewer.grid(t).root, false);
 
             results.appendChild(acc.root);
         }
 
         date.onChanged(this.debounce(showUsage, 750));
+        users.onChanged(showUsage);
 
         showUsage();
 
--- packages/UsageAnalysis/queries/queries.sql
+++ packages/UsageAnalysis/queries/queries.sql
@@ -12,7 +12,7 @@ group by u.name, u.id
 --name: events on @date
 --input: string date { pattern: datetime }
 --connection: datagrok
-select u.email as user, u.id as user_id, t.name, t.source, e.event_time, e.description from events e
+select u.login as user, u.id as user_id, t.name, t.source, e.event_time, e.description from events e
 inner join event_types t on e.event_type_id = t.id
 inner join users_sessions s on e.session_id = s.id
 inner join users u on u.id = s.user_id
@@ -21,11 +21,35 @@ order by e.event_time desc
 --end
 
 
+--name: events on @date and @users
+--input: string date { pattern: datetime }
+--input: list users
+--connection: datagrok
+select u.login as user, u.id as user_id, t.name, t.source, e.event_time, e.description from events e
+inner join event_types t on e.event_type_id = t.id
+inner join users_sessions s on e.session_id = s.id
+inner join users u on u.id = s.user_id
+where @date(e.event_time) and u.login = any(@users)
+order by e.event_time desc
+--end
+
+
 --name: errors on @date
 --input: string date { pattern: datetime }
 --connection: datagrok
-select * from events e
+select name, friendly_name, source, event_time, run_number as count, error_message, error_stack_trace from events e
 where @date(e.event_time) and e.error_stack_trace is not null
+--end
+
+
+--name: errors on @date and @users
+--input: string date { pattern: datetime }
+--input: list users
+--connection: datagrok
+select e.name, e.friendly_name, e.source, e.event_time, e.run_number as count, e.error_message, e.error_stack_trace from events e
+inner join users_sessions s on e.session_id = s.id
+inner join users u on u.id = s.user_id
+where @date(e.event_time) and e.error_stack_trace is not null and u.login = any(@users)
 --end
 
 
@@ -40,12 +64,40 @@ order by count(et.friendly_name) desc
 --end
 
 
+--name: events summary on @date and @users
+--input: string date { pattern: datetime }
+--input: list users
+--connection: datagrok
+select et.friendly_name, count(et.friendly_name) from events e
+inner join event_types et on e.event_type_id = et.id
+inner join users_sessions s on e.session_id = s.id
+inner join users u on u.id = s.user_id
+where @date(e.event_time) and u.login = any(@users)
+group by et.friendly_name
+order by count(et.friendly_name) desc
+--end
+
+
 --name: errors summary on @date
 --input: string date { pattern: datetime }
 --connection: datagrok
 select concat(e.friendly_name, ': ', e.error_message), count(et.friendly_name) from events e
 join event_types et on e.event_type_id = et.id
 where @date(e.event_time) and e.error_stack_trace is not null
+group by concat(e.friendly_name, ': ', e.error_message)
+order by count(et.friendly_name) desc
+--end
+
+
+--name: errors summary on @date and @users
+--input: string date { pattern: datetime }
+--input: list users
+--connection: datagrok
+select concat(e.friendly_name, ': ', e.error_message), count(et.friendly_name) from events e
+join event_types et on e.event_type_id = et.id
+inner join users_sessions s on e.session_id = s.id
+inner join users u on u.id = s.user_id
+where @date(e.event_time) and e.error_stack_trace is not null and u.login = any(@users)
 group by concat(e.friendly_name, ': ', e.error_message)
 order by count(et.friendly_name) desc
 --end
