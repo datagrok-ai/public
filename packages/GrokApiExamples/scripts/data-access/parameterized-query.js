df3c278dd6a13137416450d451167892da445d4b
Vasiliy Nerozin <vnerozin@gmail.com>
2019-12-19 12:23:48 +0300

    GROK-6019: JS API: data-access/parameterized-query does not work

--- packages/GrokApiExamples/scripts/data-access/parameterized-query.js
+++ packages/GrokApiExamples/scripts/data-access/parameterized-query.js
@@ -1,4 +1,4 @@
 // An example of using parametrized query
 
-gr.query('StoresInState', {'state': 'NY'})
+gr.query('Demo:CoffeeCompany:StoresInState', {'state': 'NY'})
     .then(t => gr.balloon.info('Stores: ' + t.rowCount));
