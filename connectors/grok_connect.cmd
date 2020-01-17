f23dda2b3ae9909eaa58d403d679f11ea4823adc
Vasiliy Nerozin <vnerozin@gmail.com>
2020-01-17 19:17:29 +0300

    GROK-6114: GrokConnect: Test program

--- connectors/grok_connect.cmd
+++ connectors/grok_connect.cmd
@@ -1,25 +1,74 @@
-:: Base project directory and executable
-SET GROK_CONNECT_DIR=grok_connect
-SET GROK_CONNECT=grok_connect-1.0.3.jar
+:: Builds "GrokConnect" server or runs as shell application
+::
+:: Requirements:
+::   * jdk 8 (https://www.oracle.com/technetwork/java/javase/downloads/jdk8-downloads-2133151.html)
+::   * maven 3+ (https://maven.apache.org/install.html)
+::
+:: Parameters:
+::   * mode: test/run/shell
+::
+::
+:: "shell" mode:
+::
+::    Template:
+::        grok_connect_shell.cmd <mode> <args>
+::
+::    where:
+::      * mode - run/build
+::      * args:
+::          -q, --query  <arg>   Query JSON file path
+::          -o, --output <arg>   Output CSV file path (optional, if is not specified CSV will be printed into console)
+::
+::    see query json example in "query_example.json"
+::
+::    Examples:
+::        grok_connect.cmd shell build -q examples\query.json -o output.csv
+::        grok_connect.cmd shell build -q examples\query.json
+::        grok_connect.cmd shell -q examples\query.json
+::
+@echo off
+set GROK_CONNECT_DIR=grok_connect
+set GROK_CONNECT=grok_connect-1.0.3.jar
+set TARGET_DIR=%GROK_CONNECT_DIR%\target
 
-:: Remove target
-del /s /q %GROK_CONNECT_DIR%\target
+if "%1" == "shell" (
+    set REST_ARGS=%*
+    call set REST_ARGS=%%REST_ARGS:*%1=%%
 
-:: Get dart test dependencies
-cd ..\ddt
-call pub get
+    if "%2" == "build" (
+        del /s /q %TARGET_DIR%
+        mvn -Dmaven.test.skip=true package
+        call set REST_ARGS=%%REST_ARGS:*%2=%%
+    )
 
-cd ..\grok_shared
-call pub get
+    java -Xmx4g -classpath %GROK_CONNECT_DIR%\lib\*;%TARGET_DIR%\%GROK_CONNECT% grok_connect.GrokConnectShell %REST_ARGS%
+) else (
+    :: Remove target
+    del /s /q %TARGET_DIR%
 
-cd ..\ddt\bin\serialization_test
-call pub get
+    if "%1" == "test" (
+        :: Get dart test dependencies
+        cd ..\ddt
+        call pub get
 
-cd ..\..\..\public\connectors
+        cd ..\grok_shared
+        call pub get
 
-:: Build project
-call mvn package
+        cd ..\ddt\bin\serialization_test
+        call pub get
 
-:: Run connector server with shared libraries
-call java -Xmx4g -classpath %GROK_CONNECT_DIR%\lib\*;%GROK_CONNECT_DIR%\target\%GROK_CONNECT% grok_connect.GrokConnect
-pause
+        cd ..\..\..\public\connectors
+
+        :: Build project
+        call mvn package
+    ) else (
+        :: Build project
+        mvn -Dmaven.test.skip=true package
+    )
+
+    :: Run connector server with shared libraries
+    if "%1" == "run" (
+        call java -Xmx4g -classpath %GROK_CONNECT_DIR%\lib\*;%TARGET_DIR%\%GROK_CONNECT% grok_connect.GrokConnect
+        pause
+    )
+)
