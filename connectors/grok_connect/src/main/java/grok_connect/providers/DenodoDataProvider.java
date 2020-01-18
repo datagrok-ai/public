a6b770124b385560611214fe87026a20a3161a54
skalkin <skalkin@gmail.com>
2020-01-18 18:35:23 -0500

    GROK-6169: GrokConnect: Denodo integration

--- connectors/grok_connect/src/main/java/grok_connect/providers/DenodoDataProvider.java
+++ connectors/grok_connect/src/main/java/grok_connect/providers/DenodoDataProvider.java
@@ -4,7 +4,6 @@ import grok_connect.connectors_info.DataConnection;
 import grok_connect.connectors_info.DataSource;
 import grok_connect.connectors_info.DbCredentials;
 import grok_connect.utils.Property;
-import jdk.nashorn.internal.objects.Global;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -12,34 +11,26 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 
 
-public class ImpalaDataProvider extends JdbcDataProvider {
-    public ImpalaDataProvider() {
+public class DenodoDataProvider extends JdbcDataProvider {
+    public DenodoDataProvider() {
         descriptor = new DataSource();
-        descriptor.type = "Impala";
+        descriptor.type = "Denodo";
         descriptor.description = "Query Impala database";
 
-        descriptor.connectionTemplate = new ArrayList<Property>() {{
-            add(new Property(Property.STRING_TYPE, DbCredentials.SERVER));
-            add(new Property(Property.STRING_TYPE, DbCredentials.SCHEMA));
-            add(new Property(Property.INT_TYPE, DbCredentials.PORT));
-            add(new Property(Property.STRING_TYPE, DbCredentials.DB, DbCredentials.DB_DESCRIPTION));
-        }};
-
+        descriptor.connectionTemplate = DbCredentials.dbConnectionTemplate;
         descriptor.credentialsTemplate = DbCredentials.dbCredentialsTemplate;
     }
 
+
     public Connection getConnection(DataConnection conn) throws ClassNotFoundException, SQLException {
-        Class.forName("com.cloudera.impala.jdbc41.Driver");
+        //Class.forName("com.denodo.vdp.jdbc.Driver");
+        Class.forName("com.denodo.vdb.jdbcdriver.VDBJDBCDriver");
         String connStr = getConnectionString(conn);
         return DriverManager.getConnection(connStr, conn.credentials.getLogin(), conn.credentials.getPassword());
     }
 
     public String getConnectionString(DataConnection conn) {
         String port = (conn.getPort() == null) ? "" : ":" + conn.getPort();
-
-        String schema = (String)conn.parameters.get(DbCredentials.SCHEMA);
-        schema = schema == null ? "/default" : "/" + schema;
-
-        return "jdbc:impala://" + conn.getServer() + port + schema + ";AuthMech=3;SSL=1;AllowSelfSignedCerts=1";
+        return "jdbc:vdb://" + conn.getServer() + port +  "/" + conn.getDb();
     }
 }
