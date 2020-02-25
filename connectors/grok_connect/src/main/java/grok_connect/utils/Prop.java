41bdc4e270838a08dffbb9dc1dc96715bea6849a
Alex Paramonov <alex.aprm@gmail.com>
2020-02-25 17:50:28 +0300

    GROK-6326 Cache DataQueries results

--- connectors/grok_connect/src/main/java/grok_connect/utils/Prop.java
+++ connectors/grok_connect/src/main/java/grok_connect/utils/Prop.java
@@ -5,11 +5,17 @@ import com.google.gson.annotations.*;
 
 public class Prop {
     public String editor;
+    public bool ignore;
 
     public Prop() {
     }
 
     public Prop(String editor) {
         this.editor = editor;
+    }
+
+    public Prop(String editor, bool ignore) {
+        this.editor = editor;
+        this.ignore = ignore;
     }
 }
