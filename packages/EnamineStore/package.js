d2e4a598755f8e9b2016f733a51ce435a137acd6
Vasiliy Nerozin <vnerozin@gmail.com>
2020-03-09 17:49:09 +0300

    GROK-6421: EnamineStore integration demo

--- packages/EnamineStore/package.js
+++ packages/EnamineStore/package.js
@@ -7,23 +7,57 @@ class EnamineStorePackage extends GrokPackage {
     //output: widget result
     //condition: true
     enamineStore(smiles) {
-        // sub, sim, exact
-        let host = ui.div([ui.loader()]);
-        this.storeSearch(smiles,  'sim', t => {
-            let grid = Grid.create(t);
-            grid.root.style.width = '350px';
-            grid.root.style.height = '400px';
-            while (host.firstChild)
-                host.removeChild(host.firstChild);
-            host.appendChild(grid.root);
-        })
+        let host = ui.div([
+            ui.h2('Exact'),
+            this.storeSearch(smiles, 'exact'),
+            ui.h2('Similar'),
+            this.storeSearch(smiles, 'sim'),
+            ui.h2('Substructure'),
+            this.storeSearch(smiles, 'sub')
+        ]);
+        host.firstChild.style.marginTop = '6px';
         return new Widget(host);
     }
 
-    storeSearch(smiles, mode, handler) {
-        grok.query('EnamineStore:API', {
+    storeSearch(smiles, mode) {
+        const currency = 'USD';
+        let host = ui.divH([ui.loader()]);
+        grok.query('EnamineStore:Search', {
             'code': `search_${smiles}_${mode}`,
-            'currency': 'USD'
-        }, true, 100).then(handler);
+            'currency': currency
+        }, true, 100).then(t => {
+            host.removeChild(host.firstChild);
+            if (t.rowCount === 0)
+                host.appendChild(ui.divText('No matches'));
+            for (let n = 0; n < t.rowCount; n++) {
+                let smiles = t.get('data/smile', n);
+                let mol = ui.svgMol(smiles, 150, 75);
+                let id = t.get('data/Id', n);
+                let tooltip = ui.tableFromMap({
+                    'ID': id,
+                    'Name': t.get('data/name', n),
+                    'Formula': t.get('data/formula', n),
+                    'MW': t.get('data/mw', n),
+                    'Availability': t.get('data/availability', n),
+                    'Delivery': t.get('data/deliveryDays', n),
+                });
+                // let packsHost = ui.divV([ui.h3('Packs'), ui.loader()]);
+                // grok.query('EnamineStore:Packs', {'code': id, 'currency': currency}, true, 100).then(t => {
+                //     packsHost.removeChild(packsHost.lastChild);
+                //     let packs = {};
+                //     for (let n = 0; n < t.rowCount; n++)
+                //         packs[`${t.get('amount', n)} ${t.get('measure', n)}`] = `${t.get('price', n)} ${currency}`;
+                //     packsHost.appendChild(ui.tableFromMap(packs));
+                // });
+                // tooltip.appendChild(packsHost);
+                ui.tooltip(mol, tooltip);
+                mol.addEventListener('click', function() {
+                    window.open(t.get('data/productUrl', n), '_blank');
+                });
+                host.appendChild(mol);
+            }
+            host.style.overflowY = 'auto';
+        });
+        return host;
     }
 }
