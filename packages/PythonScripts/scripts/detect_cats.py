2f78f6ad3fd5006bc1b892f3a7791263b3ffaec9
Vasiliy Nerozin <vnerozin@gmail.com>
2020-03-24 19:02:59 +0300

    GROK-6449: "Open file" file scripting feature

--- packages/PythonScripts/scripts/detect_cats.py
+++ packages/PythonScripts/scripts/detect_cats.py
@@ -1,14 +1,14 @@
 #name: Detect Cats
 #description: Detects cats on image
 #language: python
-#input: file filePath
+#input: file file
 #output: bool hasCats
-#tag: panel, widget
-#condition: x.size < 1e6 && x.ext == "jpg"
+#tags: demo, files, panel, ml, opencv
+#condition: file.size < 1e6 && (file.name.endsWith("jpg") || file.name.endsWith("jpeg"))
 
 import cv2
 
-image = cv2.imread(filePath)
+image = cv2.imread(file)
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 detector = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalcatface.xml")
 hasCats = len(detector.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=3, minSize=(75, 75))) != 0
