73db1abfc749b4d9dad8459311625eedd4f92bec
osakhniuk <osakhniuk@datagrok.ai>
2020-04-06 15:59:32 +0300

    package.js to FileSchemasDemo

---
+++ packages/FileSchemasDemo/package.js
@@ -0,0 +1,5 @@
+class SchemasDemoPackage extends GrokPackage {
+
+    // Guaranteed to get called exactly once before the execution of any function below
+    init() { console.log('SchemasDemo package initialized.'); }
+}
