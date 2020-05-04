e38542dcccc573ce9afcc3d6017f1a93e6ac2637
Andrew Skalkin <skalkin@gmail.com>
2020-05-04 00:13:00 -0400

    Leaflet viewer - WIP

---
+++ packages/Leaflet/package.js
@@ -0,0 +1,10 @@
+class LeafletPackage extends GrokPackage {
+
+    //name: Leaflet
+    //description: Leaflet map
+    //tags: viewer
+    //output: viewer result
+    leafletViewer() {
+        return new LeafletViewer();
+    }
+}
