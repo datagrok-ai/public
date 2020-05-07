fbeb062deecb235514b12d30970bb17fbb5ef1c1
osakhniuk <osakhniuk@datagrok.ai>
2020-05-07 22:48:55 +0300

    delete demo tags in Covid package

--- packages/Covid/scripts/time_series_covid19_recovered_global.js
+++ packages/Covid/scripts/time_series_covid19_recovered_global.js
@@ -1,5 +1,5 @@
 //name: time_series_covid19_recovered_global
-//tags: demo, covid-19
+//tags: covid-19
 //language: javascript
 grok.loadDataFrame('https://data.humdata.org/hxlproxy/api/data-preview.csv?url=https%3A%2F%2Fraw.githubusercontent.com%2FCSSEGISandData%2FCOVID-19%2Fmaster%2Fcsse_covid_19_data%2Fcsse_covid_19_time_series%2Ftime_series_covid19_recovered_global.csv&filename=time_series_covid19_recovered_global.csv')
     .then(t => grok.addTableView(t));
