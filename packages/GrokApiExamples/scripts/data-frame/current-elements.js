7ff2766126a8b8eaa26ab30b74f3b7148115230a
osakhniuk <osakhniuk@datagrok.ai>
2020-05-12 15:12:02 +0300

    Update current-elements.js

--- packages/GrokApiExamples/scripts/data-frame/current-elements.js
+++ packages/GrokApiExamples/scripts/data-frame/current-elements.js
@@ -7,6 +7,7 @@ t.currentRow = 4;
 t.currentCol = t.col('age');
 t.currentCell = t.cell(5, 'sex');
 
-t.onCurrentCellChanged.subscribe((_) => grok.balloon.info(`Row: ${t.currentRow.idx}`));
+t.onCurrentCellChanged.subscribe((_) => grok.balloon.info(`Cell: ${t.currentCell.rowIndex}, ${t.currentCell.column.name}`));
 t.onCurrentColChanged.subscribe((_) => grok.balloon.info(`Column: ${t.currentCol.name}`));
-t.onCurrentRowChanged.subscribe((_) => grok.balloon.info(`Cell: ${t.currentCell.rowIndex}, ${t.currentCell.column.name}`));
+t.onCurrentRowChanged.subscribe((_) => grok.balloon.info(`Row: ${t.currentRow.idx}`));
+
