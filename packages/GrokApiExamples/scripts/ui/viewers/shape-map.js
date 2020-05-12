87ea25b0f4048e69215058fdac3d02b5bc40c843
osakhniuk <osakhniuk@datagrok.ai>
2020-05-12 15:28:48 +0300

    fix links to .csv files in JS Examles

--- packages/GrokApiExamples/scripts/ui/viewers/shape-map.js
+++ packages/GrokApiExamples/scripts/ui/viewers/shape-map.js
@@ -1,6 +1,6 @@
 // https://datagrok.ai/help/viewers/shape-map
 
-grok.loadDataFrame('https://public.datagrok.ai/demo//earnings-by-state.csv").then((t) => {
+grok.loadDataFrame('https://public.datagrok.ai/demo//earnings-by-state.csv').then((t) => {
     let view = grok.addTableView(t);
     view.shapeMap();
 })
