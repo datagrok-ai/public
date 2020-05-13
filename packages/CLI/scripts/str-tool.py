2fa92eef0efa53be74d9f6594d2efc46a922cb98
Vasiliy Nerozin <vnerozin@gmail.com>
2020-05-13 19:58:36 +0300

    GROK-6808: Command-line utilities demo

--- packages/CLI/scripts/str-tool.py
+++ packages/CLI/scripts/str-tool.py
@@ -2,25 +2,22 @@
 #description: Calculates string weight
 #language: python
 #tags: demo, cli
-#require: str-tool-macos, str-tool-linux
+#require: str-tool-linux
 #input: dataframe data [Input data table]
 #input: column input {type:categorical} [Impute data table columns]
 #input: bool do_log = false [Use LOG weights]
-#output: dataframe output {action:replace(data)} [Output data]
+#output: dataframe output {action:join(data)} [Output data]
 #output: string stdout [STDOUT content]
 #output: string stderr [STDERR content]
 
-import os
 import tempfile
-import pandas as pd
 from subprocess import Popen, PIPE
-
 
 working_dir = tempfile.mkdtemp()
 
 input_path = os.path.join(working_dir, 'input.csv')
 output_path = os.path.join(working_dir, 'output.csv')
-input.to_csv(input_path)
+data.to_csv(input_path, index=False)
 
 params = [os.path.join(utils_dir, 'str-tool-linux'), '-i', input_path, '-o', output_path]
 if do_log:
