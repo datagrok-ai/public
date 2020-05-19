80bb98bae674e8d1537ebec655dfbdd0d7aef4a8
Vasiliy Nerozin <vnerozin@gmail.com>
2020-05-19 09:32:05 +0300

    GROK-6864: Scripting: File metadata extractors

--- packages/FileEditors/package.js
+++ packages/FileEditors/package.js
@@ -11,33 +11,18 @@ class FileEditorsPackage extends GrokPackage {
         view.append(canvas);
 
         pdfjsLib.getDocument(url)
-            .then(function(pdf) {
-                return pdf.getPage(1);
-            })
-            .then(function(page) {
-                // Set scale (zoom) level
-                var scale = 1.5;
-
-                // Get viewport (dimensions)
-                var viewport = page.getViewport(scale);
-
-                // Fetch canvas' 2d context
-                var context = canvas.getContext('2d');
-
-                // Set dimensions to Canvas
+            .then((pdf) => pdf.getPage(1))
+            .then((page) => {
+                let scale = 1.5;
+                let viewport = page.getViewport(scale);
+                let context = canvas.getContext('2d');
                 canvas.height = viewport.height;
                 canvas.width = viewport.width;
-
-                // Prepare object needed by render method
-                var renderContext = {
+                page.render({
                     canvasContext: context,
                     viewport: viewport
-                };
-
-                // Render PDF page
-                page.render(renderContext);
+                });
             });
-
 
         return view;
     }
