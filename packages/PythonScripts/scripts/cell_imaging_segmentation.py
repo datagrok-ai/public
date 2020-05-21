2b3607cbcb5a1d9590f8397356e1d2fe2d1e5b07
Vasiliy Nerozin <vnerozin@gmail.com>
2020-05-21 17:47:32 +0300

    GROK-6886: Demo: Cell imaging segmentation

--- packages/PythonScripts/scripts/cell_imaging_segmentation.py
+++ packages/PythonScripts/scripts/cell_imaging_segmentation.py
@@ -15,6 +15,7 @@ import matplotlib.pyplot as plt
 # Gray color image
 img = cv.imread(file)
 img = cv.cvtColor(img, cv.COLOR_RGB2BGR)
+orig_img = img.copy()
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 _, thresh = cv.threshold(gray, 0, 255, cv.THRESH_BINARY_INV + cv.THRESH_OTSU)
 
@@ -36,17 +37,7 @@ _, markers = cv.connectedComponents(sure_fg)
 markers = markers + 1
 markers[unknown == 255] = 0
 markers = cv.watershed(img, markers)
-markers[0, :] = 0; markers[:, 0] = 0; markers[-1, :] = 0; markers[:, -1] = 0
 img[markers == -1] = [255, 0, 255]
-_, m2 = cv.threshold(markers.astype(np.uint8), 0, 255, cv.THRESH_BINARY | cv.THRESH_OTSU)
-contours, hierarchy = cv.findContours(m2, cv.RETR_LIST, cv.CHAIN_APPROX_NONE)
-for c in contours:
-    cv.drawContours(img, c, -1, (255, 0, 255), 3)
-
-segmented = plt.imshow(img)
-plt.subplots_adjust(left=0, right=1, top=1, bottom=0)
-plt.axis('off')
-plt.show()
 
 # Count cells segments
 gray = cv.cvtColor(gray, cv.COLOR_GRAY2BGR)
@@ -57,3 +48,15 @@ _, _, stats, _ = cv.connectedComponentsWithStats(th, connectivity=4)
 areas = stats[:, cv.CC_STAT_AREA]
 th = np.median(areas) * 1.6
 cells = np.sum(areas < th)
+
+# Plot beautified image
+markers[0, :] = 0; markers[:, 0] = 0; markers[-1, :] = 0; markers[:, -1] = 0
+orig_img[markers == -1] = [255, 0, 255]
+_, m2 = cv.threshold(markers.astype(np.uint8), 0, 255, cv.THRESH_BINARY | cv.THRESH_OTSU)
+contours, hierarchy = cv.findContours(m2, cv.RETR_LIST, cv.CHAIN_APPROX_NONE)
+for c in contours:
+    cv.drawContours(orig_img, c, -1, (255, 0, 255), 3)
+segmented = plt.imshow(orig_img)
+plt.subplots_adjust(left=0, right=1, top=1, bottom=0)
+plt.axis('off')
+plt.show()
