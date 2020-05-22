345c9f7322733cbbde99e4ffa53f7a85b4d24947
Andrew Skalkin <skalkin@gmail.com>
2020-05-22 19:22:24 -0400

    GROK-6896: Property panel: ability to inspect JS objects

---
+++ packages/GrokApiExamples/scripts/shell/current-object-js.js
@@ -0,0 +1,9 @@
+// You can visualize arbitrary JS objects for debug purposes
+
+grok.shell.o = {
+    x: 5,
+    y: {
+        a: 'foo',
+        c: 'bar'
+    }
+};
---
+++ packages/GrokApiExamples/scripts/shell/current-object.js
@@ -0,0 +1,3 @@
+// Current object gets shown in the property panel on the right.
+
+grok.shell.o = grok.shell.user;
