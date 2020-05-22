6aaabf750e4ba65c620e1f992fc3f74ad7277ed1
Alex Paramonov <alex.aprm@gmail.com>
2020-05-22 14:52:56 +0300

    GROK-6518 Plugin development environment

--- packages/__window.js
+++ packages/__window.js
@@ -1,7 +1,11 @@
 import * as _grok from '../grok-api/grok';
 import * as _dg from '../grok-api/dg';
 import * as _ui from '../grok-api/ui';
+import * as _rxjs from "rxjs";
+import * as _rxjsOperators from "rxjs/operators";
 
 window.grok = _grok;
 window.DG = _dg;
 window.ui = _ui;
+window.rxjs = _rxjs;
+window.rxjsOperators = _rxjsOperators;
