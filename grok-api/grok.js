8a4e87047e7bedd3954b86c7ccfffb3fe2f5c44f
Andrew Skalkin <skalkin@gmail.com>
2020-05-22 19:22:24 -0400

    GROK-6896: Property panel: ability to inspect JS objects

--- grok-api/grok.js
+++ grok-api/grok.js
@@ -9,47 +9,62 @@ import {Functions} from "./src/functions";
 import {Events} from "./src/events";
 import {DockManager} from "./src/docking";
 import {_toDart, _wrap} from "./src/wrappers";
-import {Balloon, Menu, TabControl} from "./src/ui_classes";
+import {Menu, TabControl} from "./src/ui_classes";
 
 
 /** Grok entry point, use it to get access to top-level views, tables, methods, etc. */
-
 class Shell {
 
+    /** Current table, or null.
+     *  @returns {DataFrame} */
     get t() { return new DataFrame(grok_CurrentTable()); }
 
-    /** Current view */
-    set v(view) { grok_Set_CurrentView(view.d); }
+    /** Current view
+     *  @returns {View} */
     get v() { return View.fromDart(grok_Get_CurrentView()); }
+    set v(view) { grok_Set_CurrentView(view.d); }
 
-    /** Current project */
+    /** Current project
+     *  @returns {Project} */
     get project() { return new Project(grok_Project()); }
 
-    /** List of table names that are currently open */
+    /** List of table names that are currently open
+     *  @returns {string[]} */
     get tableNames() { return grok_TableNames(); }
 
-    /** List of currently open tables table */
+    /** List of currently open tables table
+     *  @returns {DataFrame[]}*/
     get tables() { return this.tableNames.map(this.tableByName); }
 
-    /** Current user */
+    /** Current user
+     *  @returns {User} */
     get user() { return new User(grok_User()); }
 
-    /** Current object */
+    /** Current object (rendered in the property panel) */
     get o() { return _wrap(grok_Get_CurrentObject(), false); }
     set o(x) { grok_Set_CurrentObject(_toDart(x)); }
 
+    /** Sidebar.
+     *  @returns {TabControl} */
     get sidebar() { return new TabControl(grok_Get_Sidebar()); }
 
+    /** Top menu (not visible by default)
+     *  @returns {Menu} */
     get topMenu() { return new Menu(grok_Get_TopMenu()); }
 
-    /** Shows information message (green background) */
+    /** Shows information message (green background)
+     * @param {string} s - message */
     info(s) { grok_Balloon(s, 'info'); }
 
-    /** Shows information message (red background) */
+    /** Shows information message (red background)
+     * @param {string} s - message */
     error(s) { grok_Balloon(s, 'error'); }
 
     dockElement(e, title = null, dockStyle = 'fill', ratio = 0.5) { grok_DockElement(e, title, dockStyle, ratio); }
 
+    /** Opens the view that handles the specified url.
+     *  @param {string} url
+     *  @returns {View} */
     route(url) { return View.fromDart(grok_Route(url)); }
 
     get settings() { return new Settings(); }
@@ -97,6 +112,7 @@ class Data {
      *
      * @returns {DataFrame} */
     testData(dataset, rows = 10000, columns = 3) { return new DataFrame(grok_TestData(dataset, rows, columns)); }
+
     getDemoTable(path) {
         return new Promise((resolve, reject) => grok_GetDemoTable(path, (t) => resolve(_wrap(t))));
     }
@@ -109,30 +125,38 @@ class Data {
     }
 
     linkTables(t1, t2, keyColumns1, keyColumns2, linkTypes) { grok_LinkTables(t1.d, t2.d, keyColumns1, keyColumns2, linkTypes); };
+
     compareTables(t1, t2, keyColumns1, keyColumns2, valueColumns1, valueColumns2) { grok_CompareTables(t1.d, t2.d, keyColumns1, keyColumns2, valueColumns1, valueColumns2); };
+
     joinTables(t1, t2, keyColumns1, keyColumns2, valueColumns1, valueColumns2, joinType, inPlace) {
         return new DataFrame(grok_JoinTables(t1.d, t2.d, keyColumns1, keyColumns2, valueColumns1, valueColumns2, joinType, inPlace));
     }
 
     openTable(id) { return new Promise((resolve, reject) => grok_OpenTable(id, (t) => resolve(new DataFrame(t)))); }
+
     query(queryName, queryParameters = null, adHoc = false, pollingInterval = 1000) {
         return new Promise((resolve, reject) => grok_Query(queryName, queryParameters, adHoc, pollingInterval, (t) => resolve(new DataFrame(t))));
     }
+
     callQuery(queryName, queryParameters = null, adHoc = false, pollingInterval = 1000) {
         return new Promise((resolve, reject) => grok_CallQuery(queryName, queryParameters, adHoc, pollingInterval, (c) => resolve(new FuncCall(c))));
     }
+
     detectSemanticTypes(t) { return new Promise((resolve, reject) => grok_DetectSematicTypes(t.d, (_) => resolve())); }
 
 }
+
 
 class Settings {
     /** Hide dock tabs in presentaion mode **/
     get hideTabsInPresentationMode() { return grok_Get_HideTabsInPresentationMode(); }
     set hideTabsInPresentationMode(x) { return grok_Set_HideTabsInPresentationMode(x); }
+
     /** Presentaion mode **/
     get presentationMode() { return grok_Get_PresentationMode(); }
     set presentationMode(x) { return grok_Set_PresentationMode(x); }
 }
+
 
 export class utils {
     static *range(length) {
