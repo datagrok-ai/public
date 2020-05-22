2fadc60cf0faf908160a7ebabbe0791c4e2f1dd9
Andrew Skalkin <skalkin@gmail.com>
2020-05-22 15:58:22 -0400

    GROK-6894: JS API: docking - WIP

--- grok-api/src/docking.js
+++ grok-api/src/docking.js
@@ -19,13 +19,24 @@ export class DockNode {
     get container() { return new DockContainer(grok_DockNode_Get_Container(this.d)); }
 
     detachFromParent() { return grok_DockNode_DetachFromParent(this.d); }
+    removeChild(childNode) { return grok_DockNode_RemoveChild(this.d, childNode); }
 };
 
 
 export class DockContainer {
     constructor(d) { this.d = d; }
 
-    get element() { return grok_DockContainer_Get_Element(this.d); }
+    get containerElement() { return grok_DockContainer_Get_ContainerElement(this.d); }
+
+    destroy() { grok_DockContainer_Destroy(this.d); }
+
+    /** Undocks a panel and converts it into a floating dialog window
+     *  It is assumed that only leaf nodes (panels) can be undocked */
+    undock() { grok_DockContainer_Undock(this.d); }
+
+    /** Removes a dock container from the dock layout hierarcy
+     *  @returns {DockNode} - the node that was removed from the dock tree */
+    //remove() { return new DockNode(grok_DockContainer_Remove(this.d)); }
 }
 
 
@@ -35,12 +46,14 @@ export class DockManager {
 
     get element() { return grok_DockManager_Get_Element(this.d); }
 
+    get rootNode() { return new DockNode(grok_DockManager_Get_RootNode(this.d)); }
+
     /**
      * The document view is then central area of the dock layout hierarchy.
      * This is where more important panels are placed (e.g. the text editor in an IDE,
-     * 3D view in a modelling package etc
+     * 3D view in a modeling package etc
      */
-    get documentContainer() { return new DockContainer(grok_DockManaget_Get_DocumentView(this.d)); }
+    get documentContainer() { return new DockContainer(grok_DockManaget_Get_DocumentContainer(this.d)); }
 
     /**
      * Docks the element relative to the reference node.
