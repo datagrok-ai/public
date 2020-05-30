3e2c24484b0e7b81de5fab6f76e9bf6a7b65c9cd
Andrew Skalkin <skalkin@gmail.com>
2020-05-30 10:28:42 -0400

    API documentation - WIP

--- grok-api/grok.js
+++ grok-api/grok.js
@@ -65,15 +65,31 @@ class Shell {
      * @param {string} s - message */
     error(s) { grok_Balloon(s, 'error'); }
 
-    dockElement(e, title = null, dockStyle = 'fill', ratio = 0.5) { grok_DockElement(e, title, dockStyle, ratio); }
+    dockElement(e, title = null, dockStyle = 'fill', ratio = 0.5) {
+        grok_DockElement(e, title, dockStyle, ratio);
+    }
 
     /** Opens the view that handles the specified url.
      *  @param {string} url
      *  @returns {View} */
     route(url) { return View.fromDart(grok_Route(url)); }
 
-    addView(v) { grok_AddView(v.d); }
+    /**
+     * Adds a view.
+     * @param {View} v
+     * @returns {View}
+     */
+    addView(v) {
+        grok_AddView(v.d);
+        return v;
+    }
 
+    /**
+     * Adds a new view with the specified name.
+     * @param {string } name - view name
+     * @param {object[]} children - content to be added by calling {@link ui.appendAll}
+     * @returns {View}
+     */
     newView(name = 'view', children = []) {
         let view = View.create();
         view.name = name;
@@ -114,27 +130,72 @@ class Data {
      * "random walk": random walk data for the specified number of dimensions
      *
      * @returns {DataFrame} */
-    testData(dataset, rows = 10000, columns = 3) { return new DataFrame(grok_TestData(dataset, rows, columns)); }
+    testData(dataset, rows = 10000, columns = 3) {
+        return new DataFrame(grok_TestData(dataset, rows, columns));
+    }
 
     getDemoTable(path) {
         return new Promise((resolve, reject) => grok_GetDemoTable(path, (t) => resolve(toJs(t))));
     }
 
-    /** @returns {DataFrame} */
+    /**
+     * Parses the CSV string.
+     * @returns {DataFrame}
+     * */
     parseCsv(csv) { return new DataFrame(grok_ParseCsv(csv)); }
 
+    /**
+     * Loads table from the specified URL.
+     * @param {string} csvUrl
+     * @returns {Promise<DataFrame>}
+     * */
     loadDataFrame(csvUrl) {
         return new Promise((resolve, reject) => grok_LoadDataFrame(csvUrl, (t) => resolve(toJs(t, false))));
     }
 
+    /**
+     * @param {DataFrame} t1
+     * @param {DataFrame} t2
+     * @param {string[]} keyColumns1
+     * @param {string[]} keyColumns2
+     * @param {SyncType[]} linkTypes
+     * @returns {Promise<DataFrame>}
+     * */
     linkTables(t1, t2, keyColumns1, keyColumns2, linkTypes) { grok_LinkTables(t1.d, t2.d, keyColumns1, keyColumns2, linkTypes); };
 
-    compareTables(t1, t2, keyColumns1, keyColumns2, valueColumns1, valueColumns2) { grok_CompareTables(t1.d, t2.d, keyColumns1, keyColumns2, valueColumns1, valueColumns2); };
+    /**
+     * Opens the visual table comparison tool.
+     * @param {DataFrame} t1
+     * @param {DataFrame} t2
+     * @param {string[]} keyColumns1
+     * @param {string[]} keyColumns2
+     * @param {string[]} valueColumns1
+     * @param {string[]} valueColumns2
+     * */
+    compareTables(t1, t2, keyColumns1, keyColumns2, valueColumns1, valueColumns2) {
+        grok_CompareTables(t1.d, t2.d, keyColumns1, keyColumns2, valueColumns1, valueColumns2);
+    };
 
+    /**
+     * Merges two tables by the specified key columns.
+     * @param {DataFrame} t1
+     * @param {DataFrame} t2
+     * @param {string[]} keyColumns1
+     * @param {string[]} keyColumns2
+     * @param {string[]} valueColumns1
+     * @param {string[]} valueColumns2
+     * @param {JoinType} joinType
+     * @param {boolean} inPlace - merges content in-place into the source table
+     * */
     joinTables(t1, t2, keyColumns1, keyColumns2, valueColumns1, valueColumns2, joinType, inPlace) {
         return new DataFrame(grok_JoinTables(t1.d, t2.d, keyColumns1, keyColumns2, valueColumns1, valueColumns2, joinType, inPlace));
     }
 
+    /**
+     * Merges two tables by the specified key columns.
+     * @param {string} id - table GUID
+     * @returns {Promise<DataFrame>}
+     */
     openTable(id) { return new Promise((resolve, reject) => grok_OpenTable(id, (t) => resolve(new DataFrame(t)))); }
 
     query(queryName, queryParameters = null, adHoc = false, pollingInterval = 1000) {
@@ -161,6 +222,7 @@ class Settings {
 }
 
 
+/** Controls tool windows */
 class Windows {
     get showSidebar() { return grok_Windows_Get_ShowSidebar(); }
     set showSidebar(x) { return grok_Windows_Set_ShowSidebar(x); }
