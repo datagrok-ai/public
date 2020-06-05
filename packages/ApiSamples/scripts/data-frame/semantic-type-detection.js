c3b6e5c1269a22615268614dd6739f95a0c8b513
Vasiliy Nerozin <vnerozin@gmail.com>
2020-06-05 15:29:44 +0300

    GROK-6956: JS API Examples: data-frame/semantic-type-detection.js does not add semtype to the and balloon is not shown

--- packages/ApiSamples/scripts/data-frame/semantic-type-detection.js
+++ packages/ApiSamples/scripts/data-frame/semantic-type-detection.js
@@ -6,9 +6,8 @@ let demog = grok.data.parseCsv('smiles\nFc1cc(Cl)ccc1Br');
 // Normally, the smiles column will be detected as 'Molecule' when we create a table view.
 // However, this time we will override it
 
-demog.onSemanticTypeDetecting.subscribe((args) => {
+demog.onSemanticTypeDetecting.subscribe((_) => {
     demog.col('smiles').semType = 'bananas';
-    args.preventDefault();
 });
 demog.onSemanticTypeDetected.subscribe((_) => { grok.shell.info(demog.col('smiles').semType); });
 
