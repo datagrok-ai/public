611724bf98f3975beaabb28e85b682d7eb3dfa60
osakhniuk <osakhniuk@datagrok.ai>
2020-06-05 13:46:22 +0300

    fix all-chembl-structures.js (add namespace)

--- packages/ApiSamples/scripts/performance/all-chembl-structures.js
+++ packages/ApiSamples/scripts/performance/all-chembl-structures.js
@@ -1,3 +1,3 @@
 // Retrieve all molecules from chembl (~2 millions), and visualize in the grid
 
-grok.data.query('AllChemblStructures', {}).then(t => grok.shell.addTableView(t));
+grok.data.query('Chembl:AllChemblStructures', {}).then(t => grok.shell.addTableView(t));
