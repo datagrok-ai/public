a20645679d91d3e80bb50472a2378b9ac9714d2a
Alex Paramonov <alex.aprm@gmail.com>
2020-06-05 15:01:13 +0300

    GROK-6958 JS API Examples: performance/1m-aggregation.js does not work

--- packages/ApiSamples/scripts/performance/1m-aggregation.js
+++ packages/ApiSamples/scripts/performance/1m-aggregation.js
@@ -1,6 +1,14 @@
 // Aggregate a table with 1 million rows
 //
 // See also: https://public.datagrok.ai/help/concepts/performance
+let time = function(s, f) {
+    let start = new Date();
+    let result = f();
+    let stop = new Date();
+    console.log(`${s}: ${stop - start}ms`);
+    grok.shell.info(`${s}: ${stop - start}ms`);
+    return result;
+};
 
 let wells = time('create', () => grok.data.testData('wells', 1000000));
 
