716ec013a1712878d6bfd43dea0ff8e70c37ee69
Alex Paramonov <aparamonov@datagrok.ai>
2020-06-26 22:51:42 +0300

    New JS example

--- packages/ApiSamples/scripts/functions/progress.js
+++ packages/ApiSamples/scripts/functions/progress.js
@@ -1,3 +1,6 @@
+// See ProgressIndicator:countToTen function:
+// https://github.com/datagrok-ai/public/blob/a53c5606186eb88eb9085559bb2568aabd79c729/packages/ProgressIndicator/src/package.js#L12
+
 let p = DG.ProgressIndicator.create();
 p.onProgressUpdated.subscribe((p) => grok.shell.info(p.percent));
 
