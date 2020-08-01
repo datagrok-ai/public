6aa5915259a035c4ff325d030f8b39cbbd2f4b32
Andrew Skalkin <skalkin@gmail.com>
2020-08-01 16:35:55 -0400

    GROK-7156: Support for qualified numbers - WIP

--- packages/ApiSamples/scripts/data-frame/qualified-numbers.js
+++ packages/ApiSamples/scripts/data-frame/qualified-numbers.js
@@ -1,5 +1,13 @@
-let qCol = DG.Column.qnum(DG.COLUMN_TYPE.QNUM, 3);
-let t = DG.DataFrame.fromColumns([ qCol ]);
-qCol.set(0, DG.Qnum.greater(5));
+let manual = DG.Column.qnum('manual', 3);
+manual.set(0, DG.Qnum.greater(5));
+manual.set(1, DG.Qnum.exact(5));
+manual.set(1, DG.Qnum.less(5));
 
+// qualifier is stripped
+let fromExact = DG.Column.qnum('exact', 3, [1.123, 2.2345, 3.4567], true);
+
+// the platform expects qualified numbers
+let fromQnum = DG.Column.qnum('qnum', 3, [1.123, 2.2345, 3.4567], false);
+
+let t = DG.DataFrame.fromColumns([ manual, fromExact, fromQnum ]);
 grok.shell.addTableView(t);
