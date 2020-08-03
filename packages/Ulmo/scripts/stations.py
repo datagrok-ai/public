6f358dc446205751086ce7c9d87209caf49aeac7
Andrew Skalkin <skalkin@gmail.com>
2020-08-03 18:07:43 -0400

    GROK-4259: Integration with ULMO - WIP

---
+++ packages/Ulmo/scripts/stations.py
@@ -0,0 +1,10 @@
+#name: GetStations
+#language: python
+#environment: ulmo
+#input: string country = GM
+#output: dataframe stations
+
+import ulmo
+import pandas
+
+stations = ulmo.ncdc.ghcn_daily.get_stations(country=country, as_dataframe=True)
