cb24d34ca873ce99e8e317dfd64acd12a76f49e7
Andrew Skalkin <skalkin@gmail.com>
2020-08-05 22:33:57 -0400

    Row header-specific coloring

--- packages/ApiSamples/scripts/grid/custom-cell-rendering.js
+++ packages/ApiSamples/scripts/grid/custom-cell-rendering.js
@@ -6,7 +6,7 @@ view.grid.onCellRender.subscribe(function (args) {
     args.g.beginPath();
     args.g.arc(args.bounds.x + args.bounds.width / 2, args.bounds.y + args.bounds.height / 2, 10, 0, Math.PI * 2, true);
     args.g.closePath();
-    args.g.fillStyle = 'blue';
+    args.g.fillStyle = (args.cell.isColHeader ? 'red' : 'blue');
     args.g.fill();
     args.preventDefault();
 });
