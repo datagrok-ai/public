ae91cfb8c62af02ac0b97a037d76e3eb14614136
onuf <onuf.diana@gmail.com>
2020-08-25 00:35:23 +0300

    GROK-7244: add language detection script

---
+++ packages/NLP/scripts/language_detection.py
@@ -0,0 +1,23 @@
+#name: Language Detection
+#description: Detect the language in which a text is written
+#language: python
+#input: string text [A text to analyze]
+#output: string language [Detected language]
+#tags: nlp, panel
+
+import cld3
+import pycountry
+
+
+language = "Undetermined"
+
+# Detect language
+detected = cld3.get_language(text)
+
+# Look up its name
+if detected:
+    langcode = detected.language
+    if len(langcode) == 2:
+        language = pycountry.languages.get(alpha_2=langcode).name
+    elif len(langcode) == 3:
+        language = pycountry.languages.get(alpha_3=langcode).name
