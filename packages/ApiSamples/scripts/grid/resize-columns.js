32d8326a1c142490f059fca0004c9590bb9e0f79
Andrew Skalkin <skalkin@gmail.com>
2020-08-25 16:45:43 -0400

    GROK-7268: JS API: Grid.columns: rowHeader, byIndex

--- packages/ApiSamples/scripts/grid/resize-columns.js
+++ packages/ApiSamples/scripts/grid/resize-columns.js
@@ -1,4 +1,7 @@
 // Resize a column.
 
 let view = grok.shell.addTableView(grok.data.demo.demog());
+
 view.grid.columns.byName('age').width = 200;
+view.grid.columns.byIndex(4).width = 300;
+view.grid.columns.rowHeader.width = 100;
