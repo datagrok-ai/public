0768e6d7e670d26518801f0d4823ac3559b53ec2
onuf <onuf.diana@gmail.com>
2020-08-28 22:28:52 +0300

    Update NLP package

--- packages/NLP/scripts/sentiment-analysis.py
+++ packages/NLP/scripts/sentiment-analysis.py
@@ -5,20 +5,16 @@
 #output: string sentiment [Detected sentiment]
 #output: double polarity [Polarity of expression]
 #tags: nlp, panel
-#environment: flair_sentiment
 
-from flair.data import Sentence
-from flair.models import TextClassifier
+from transformers import pipeline
 
 
 # Download a model for sentiment analysis
-classifier = TextClassifier.load('en-sentiment')
+classifier = pipeline("sentiment-analysis")
 
 # Predict a label for the given text
-sentence = Sentence(text)
-classifier.predict(sentence)
-label = sentence.labels[0]
+label = classifier(text)[0]
 
 # Interpret the results
-sentiment = label.value
-polarity = label.score
+sentiment = label["label"]
+polarity = label["score"]
