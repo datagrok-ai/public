c12dc0c69ce01d0ef2716e800365fe8a274c2264
Alex Paramonov <aparamonov@datagrok.ai>
2020-09-01 22:40:09 +0300

    Added API examples

---
+++ packages/ApiSamples/scripts/dapi/groups.js
@@ -0,0 +1,24 @@
+async function layoutsTest() {
+//get list of groups named "demo"
+    let demoGroup = await grok.dapi.groups.filter('demo').first();
+
+//create a new Demo group, if doesn't exist and save
+    if (demoGroup == null)
+        demoGroup = await grok.dapi.groups.createNew("Demo");
+
+//create a subgroup
+    let demoSubGroup = DG.Group.create(`Demo ${Math.floor(Math.random() * 100)}`);
+
+//add to "Demo" group
+    demoSubGroup.includeTo(demoGroup);
+
+//find "admin" user
+    let adminUser = await grok.dapi.users.filter('login = "admin"').first();
+
+//add to subgroup and save
+    demoSubGroup.addAdminMember(adminUser.group);
+    await grok.dapi.groups.saveRelations(demoSubGroup);
+
+}
+
+groupsTest();
