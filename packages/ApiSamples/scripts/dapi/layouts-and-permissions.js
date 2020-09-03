f87d0d2468fee0f5d0959df5249f6e369223e639
Alex Paramonov <aparamonov@datagrok.ai>
2020-09-03 14:21:28 +0300

    GROK-7259 Expose groups and permissions to JS WIP

--- packages/ApiSamples/scripts/dapi/layouts-and-permissions.js
+++ packages/ApiSamples/scripts/dapi/layouts-and-permissions.js
@@ -7,13 +7,13 @@ async function layoutsTest() {
         {'entityId': allLayouts[1].id, 'property': 'prop', 'value': 'mark2'},
     ]);
 
-    allLayouts[1].id = 'cddcabeb-70ad-42da-a526-dfb187784ffd';
-
     await grok.dapi.layouts.save(allLayouts[1]);
 
     let markedLayouts = await grok.dapi.layouts.filter('prop = "mark2"').list();
     grok.shell.info(`marked layouts: ${markedLayouts.length}`);
 
+    let adminUser = await grok.dapi.users.filter('login = "admin"').first();
+    await grok.dapi.permissions.grant(allLayouts[1], adminUser.group, true);
+    console.log(await grok.dapi.permissions.get(allLayouts[1]));
 }
-
 layoutsTest();
