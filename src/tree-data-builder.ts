5b08e755a63ca48827688dd3fd4102aa97327071
Nikolay Seliverstov <nikolay.seliverstov@arcadia.spb.ru>
2020-09-03 19:02:40 +0300

    #9 aggregation settings

--- src/tree-data-builder.ts
+++ src/tree-data-builder.ts
@@ -3,57 +3,79 @@ import {HierarchyNode} from 'd3';
 import * as DG from 'datagrok-api/dg';
 
 export type TreeData = HierarchyNode<Branch>
+export type AggregationType = 'count' | 'sum' | 'avg';
 
-export type ColumnValue = any;
+export type ColumnCategory = any;
+export type ColumnValue = number;
 export type RowId = number;
 export class Branch {
-    readonly value: ColumnValue;
-    readonly children: Map<ColumnValue, Branch>;
+    readonly category: ColumnCategory;
+    value: ColumnValue = 0;
+    readonly children: Map<ColumnCategory, Branch>;
     readonly leafIds: RowId[] = [];
-    
-    constructor(value: ColumnValue) {
-        this.value = value;
+
+    constructor(category: ColumnCategory) {
+        this.category = category;
         this.children = new Map();
     }
 }
 
+type Aggregator = (x: Branch) => number;
+
 export class TreeDataBuilder {
     buildTreeData(
         categoryColumns: DG.Column[],
-        valueColumn: "" | DG.Column,
-        selectedRows: Int32Array
+        valueColumn: DG.Column | undefined,
+        selectedRows: Int32Array,
+        aggregationType: AggregationType
     ) {
         const root = new Branch("root");
-        
-        const lastColumn = categoryColumns[categoryColumns.length - 1]; 
-        
+
+        const lastCategoryColumn = categoryColumns[categoryColumns.length - 1];
+
         for(const rowId of selectedRows) {
             let currentBranch = root;
-            for (const column of categoryColumns) {
-                const value = column.get(rowId);
-                if (value === null || value === undefined) {
-                    currentBranch.leafIds.push(rowId);
+            for (const categoryColumn of categoryColumns) {
+                const category = categoryColumn.get(rowId);
+                if (category === null || category === undefined) {
                     break;
                 }
 
-                const existingBranch = currentBranch.children.get(value);
+                const existingBranch = currentBranch.children.get(category);
                 if (existingBranch) {
                     currentBranch = existingBranch;
                 }
                 else {
                     const parent = currentBranch;
-                    currentBranch = new Branch(value);
-                    parent.children.set(value, currentBranch);
+                    currentBranch = new Branch(category);
+                    parent.children.set(category, currentBranch);
                 }
-                
-                if (lastColumn === column) {
-                    currentBranch.leafIds.push(rowId);
+
+                if (lastCategoryColumn === categoryColumn) {
                 }
+            }
+            currentBranch.leafIds.push(rowId);
+            if (valueColumn) {
+                currentBranch.value = valueColumn.get(rowId);
             }
         }
 
         return d3
             .hierarchy(root, x => Array.from(x.children.values()))
-            .sum(x => x.leafIds.length);
+            .sum(this.getAggregator(aggregationType));
+    }
+
+    private getAggregator(aggregationType: AggregationType): Aggregator {
+        switch (aggregationType) {
+            case 'sum':
+                return x => x.value;
+            case 'avg':
+                return x => x.leafIds.length
+                    ? (x.value / x.leafIds.length)
+                    : 0;
+            case 'count':
+            default:
+        }
+        return x => x.leafIds.length;
     }
 }
