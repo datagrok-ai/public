e0ee10ce3ea6cd9d45d3531fcea292b2eb271fe1
onuf <onuf.diana@gmail.com>
2020-09-09 15:07:54 +0300

    GROK-7333: grok create

--- tools/bin/commands/create.js
+++ tools/bin/commands/create.js
@@ -8,7 +8,7 @@ module.exports = {
 };
 
 const curDir = process.cwd();
-const curFolder = path.basename(process.cwd());
+const curFolder = path.basename(curDir);
 const grokDir = path.join(os.homedir(), '.grok');
 const confPath = path.join(grokDir, 'config.yaml');
 const config = yaml.safeLoad(fs.readFileSync(confPath));
@@ -43,18 +43,33 @@ function createDirectoryContents(name, configPath, templateDir, packageDir) {
     })
 }
 
+function isEmpty(dir) {
+    return fs.readdirSync(dir).length === 0;
+}
+
 function create(args) {
     const nOptions = Object.keys(args).length - 1;
-    if (args['_'].length === 2 && nOptions < 1) {
-        const name = args['_'][1];
+    const nArgs = args['_'].length;
+    if (nArgs <= 2 && nOptions < 1) {
+        let name = curFolder;
+        if (nArgs === 2) {
+            name = args['_'][1];
+        }
         const validName = /^([A-Za-z\-_\d])+$/.test(name);
         if (validName) {
             let packageDir = curDir;
-            if (curFolder !== name && !fs.existsSync(path.join(packageDir, name))) {
+            if (curFolder !== name) {
                 packageDir = path.join(packageDir, name);
-                fs.mkdirSync(packageDir);
+                if (!fs.existsSync(packageDir)) {
+                    fs.mkdirSync(packageDir);
+                }
             }
-            const templateDir = path.join(path.dirname(__dirname), '/../', 'package-template');
+            if (!isEmpty(packageDir)) {
+                console.log();
+                console.log('The package directory should be empty');
+                return false;
+            }
+            const templateDir = path.join(path.dirname(path.dirname(__dirname)), 'package-template');
             createDirectoryContents(name, confPath, templateDir, packageDir);
         } else {
             console.log('Package name may only include letters, numbers, underscores, or hyphens');
