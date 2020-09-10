59246a1f1b0c51573b66a14166f37b85ec8ce3a4
ZipZaap <48830393+ZipZaap@users.noreply.github.com>
2020-09-10 18:02:36 +0300

    GROK-7234 : package now available through viewer menu

--- packages/ChaRPy/src/package.js
+++ packages/ChaRPy/src/package.js
@@ -6,133 +6,79 @@ import map from "./maps.json";
 
 export let _package = new DG.Package();
 
-//name: toScript
-//top-menu: convert | toScript
-export async function toScript() {
+grok.events.onContextMenu.subscribe((args) => {
+    if (args.args.context instanceof DG.Viewer) {
+        // grok.shell.info(args.args.context.table.name);
+        args.args.menu.item('to R script', async () => {
 
-    // recursive multi-level object merging
-    function assignOnlyIntersection(target, source) {
-        Object.keys(target).forEach(key => {
-            (target[key] == null) && delete target[key]
-            if (target[key] in source) {
-                target[key] = source[target[key]];
-            } else if (typeof target[key] === 'object') {
-                assignOnlyIntersection(target[key], source)
+            function assignOnlyIntersection(target, source) {
+                Object.keys(target).forEach(key => {
+                    (target[key] == null) && delete target[key]
+                    if (target[key] in source) {
+                        target[key] = source[target[key]];
+                    } else if (typeof target[key] === 'object') {
+                        assignOnlyIntersection(target[key], source)
+                    }
+                })
             }
-        })
-    }
-
-    // dynamic default string trimming
-    function dynamicReplace(stRing, optionsObj, map) {
-        Object.keys(optionsObj).forEach(key => {
-            (optionsObj[key] == null) && delete optionsObj[key]
-            if (typeof optionsObj[key] === 'object' &&
-                Object.keys(optionsObj[key]).every(elem => elem != '0')) {
-                stRing = dynamicReplace(stRing, optionsObj[key], map);
-            } else {
-                stRing = stRing.replace("!(" + key + ")",map[key]);
-                stRing = stRing.split("!(" + key + ")").join(optionsObj[key]);
+            function dynamicReplace(stRing, optionsObj, map) {
+                Object.keys(optionsObj).forEach(key => {
+                    (optionsObj[key] == null) && delete optionsObj[key]
+                    if (typeof optionsObj[key] === 'object' &&
+                        Object.keys(optionsObj[key]).every(elem => elem != '0')) {
+                        stRing = dynamicReplace(stRing, optionsObj[key], map);
+                    } else {
+                        stRing = stRing.replace("!(" + key + ")",map[key]);
+                        stRing = stRing.split("!(" + key + ")").join(optionsObj[key]);
+                    }
+                })
+                return stRing;
             }
-        })
-        return stRing;
+            async function strReplace(optionsObj) {
+
+                // extract default string and viewer type
+                let stRing;
+                let paramsMap;
+                if (optionsObj.type === 'Trellis plot') {
+                    stRing = map.plotScripts[optionsObj.look.innerViewerLook.type] +
+                        " + facet_grid(!(xColumnNames)!(yColumnNames))"; // need to append 'Look'
+                    paramsMap = map.additionalOps[optionsObj.look.innerViewerLook.type];
+                } else {
+                    stRing = map.plotScripts[optionsObj.type];
+                    paramsMap = map.additionalOps[optionsObj.type];
+                }
+
+                // Replace misc grok codes with R analogues
+                assignOnlyIntersection(optionsObj, map.miscCodes);
+
+                // trim the default code string
+                stRing = dynamicReplace(stRing, optionsObj, paramsMap);
+                stRing = stRing.replace(/!\([^)]*\) */g, "");
+
+                // add a print statement
+                stRing = stRing + "\nprint(plt)"
+                return stRing;
+            }
+
+            let options = JSON.parse(args.args.context.getOptions());
+            let rCode = await strReplace(options);
+
+            let block =
+                $(ui.splitV([
+                    ui.textArea(rCode),
+                    ui.splitH([
+                        args.args.context,
+                        DG.Viewer.fromType('Scripting Viewer',args.args.context.table,{script: map.header + rCode})])
+                ])).css('flex-grow', '1');
+
+
+
+            ui.dialog('OUTPUT SCRIPT').add(block[0]).showModal(true);
+        });
     }
+});
 
-    // main slicing function
-    async function strReplace(optionsObj) {
+//name: exportFunc
+//tags: autostart
+export function export_123() {}
 
-        // extract default string and viewer type
-        let stRing;
-        let paramsMap;
-        if (optionsObj.type === 'Trellis plot') {
-            stRing = map.plotScripts[optionsObj.look.innerViewerLook.type] +
-                " + facet_grid(!(xColumnNames)!(yColumnNames))"; // need to append 'Look'
-            paramsMap = map.additionalOps[optionsObj.look.innerViewerLook.type];
-        } else {
-            stRing = map.plotScripts[optionsObj.type];
-            paramsMap = map.additionalOps[optionsObj.type];
-        }
-
-        // Replace misc grok codes with R analogues
-        assignOnlyIntersection(optionsObj, map.miscCodes);
-
-        // trim the default code string
-        stRing = dynamicReplace(stRing, optionsObj, paramsMap);
-        stRing = stRing.replace(/!\([^)]*\) */g, "");
-
-        // add a print statement
-        stRing = stRing + "\nprint(plt)"
-        return stRing;
-    }
-
-    // test viewer + options
-    let view = grok.shell.addTableView(grok.data.demo.demog());
-
-    // Scatter plot
-    // let plot = view.scatterPlot({
-    //     x: 'height',
-    //     y: 'weight',
-    //     color: 'race'
-    // });
-    // plot.setOptions({
-    //     showRegressionLine: true,
-    //     markerType: 'square'
-    // });
-
-    // // Histogram
-    // let plot = view.histogram({
-    //     value: 'weight'
-    // });
-    // plot.setOptions({
-    //     bins : 40
-    // });
-
-    // // Bar chart
-    // let plot = view.barChart({
-    //     split: 'study',
-    //     value: 'age'
-    // });
-    // plot.setOptions({
-    //     valueAggrType : "skew"
-    // });
-
-    // // Box plot
-    // let plot = view.boxPlot({
-    //     value : 'height',
-    //     category: 'site',
-    //     markerColorColumnName: 'sex'
-    //     // binColorColumnName: 'height'
-    // })
-
-    // Correlation plot
-    let plot = view.corrPlot({
-        xs: ['age', 'weight', 'height'],
-        ys: ['age', 'weight', 'height'],
-    });
-
-    // let plot = view.lineChart();
-
-
-    // collect viewer properties
-    let options = JSON.parse(plot.getOptions());
-
-    //choose and slice the string
-    let rCode = await strReplace(options);
-
-    // output code in a dialogue window
-    // let input = document.createElement('TEXTAREA');
-    // input.value = rCode
-    // ui.dialog('OUTPUT SCRIPT')
-    //     .add(input)
-    //     .onOK(() => { grok.shell.info('OK!'); })
-    //     .showModal(true);
-
-    ui.dialog('OUTPUT SCRIPT')
-        .add(view.root)
-        .onOK(() => { grok.shell.info('OK!'); })
-        .showModal(true);
-
-    // run the generated script in R
-    view.addViewer('Scripting Viewer', {
-        script: map.header + rCode
-    });
-}
