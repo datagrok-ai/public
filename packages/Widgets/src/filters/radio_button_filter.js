fe4d3ea0eeb668c18354d21779da3afa0d2abafd
Andrew Skalkin <skalkin@gmail.com>
2020-09-24 00:54:48 -0400

    GROK-7319: RadioButtonFilter - WIP

--- packages/Widgets/src/filters/radio_button_filter.js
+++ packages/Widgets/src/filters/radio_button_filter.js
@@ -2,6 +2,7 @@
  * Single-option categorical filter
  * */
 class RadioButtonFilter extends DG.Filter {
+
     constructor() {
         super();
         this.root = ui.divV(null, 'd4-radio-button-filter');
@@ -10,10 +11,22 @@ class RadioButtonFilter extends DG.Filter {
     attach(dFrame) {
         this.dataFrame = DG.toJs(dFrame);
         this.column = this.dataFrame.columns.categorical[0];
+        this.dataFrame.onRowsFiltering.subscribe((_) => applyFilter());
         this.render();
     }
 
-    onTableAttached() {
+    applyFilter() {
+        let indexes = this.column.getRawData();
+        const checks = this.root.querySelectorAll('input');
+        for (let checkIdx = 0; checkIdx < checks.length; checkIdx++) {
+            if (!checks[checkIdx].checked) {
+                for (let i = 0; i < this.dataFrame.rowCount; i++)
+                    if (indexes[i] === checkIdx)
+                        this.dataFrame.filter.set(i, false, false);
+            }
+        }
+
+        this.dataFrame.filter.fireChanged();
     }
 
     render() {
@@ -22,9 +35,13 @@ class RadioButtonFilter extends DG.Filter {
 
         for (let category of this.column.categories)
             $(`<input>${category}</input>`)
+                .on('change', () => this.applyFilter())
                 .prop({type: 'radio' })
+                .prop({name: category})
+                .prop({value: 'yes'})
                 .appendTo(this.root);
     }
 
+    onTableAttached() {}
     onPropertyChanged(prop) { }
 }
