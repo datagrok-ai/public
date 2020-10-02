40dde72d15c1fabc34951853affd5b7d5e822ef6
Andrew Skalkin <skalkin@gmail.com>
2020-10-02 16:54:45 -0400

    GROK-7421: Canvas viewers: a way to schedule recalculations before the next redraw

---
+++ packages/ApiSamples/scripts/grid/performance/10k-columns-updates.js
@@ -0,0 +1,6 @@
+let table = grok.data.testData('random walk', 100, 10000);
+let grid = grok.shell.addTableView(table).grid;
+
+for (let i = 0; i < table.columns.length; i++)
+    grid.columns.byIndex(i).width = 10;
+
