7de87a7b2a6f8ee81fd63fee434c22b7eac3b64e
onuf <onuf.diana@gmail.com>
2020-10-02 15:04:22 +0300

    Fix app annotation

--- tools/entity-template/app.js
+++ tools/entity-template/app.js
@@ -1,7 +1,6 @@
 
 //name: #{NAME}
 //tags: app
-//input: string name
-export function #{NAME}(name) {
-    grok.shell.info('Hello, ' + name);
+export function #{NAME}() {
+    grok.shell.info('Hello!');
 }
