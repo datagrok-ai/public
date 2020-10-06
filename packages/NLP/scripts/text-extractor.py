193f7e73bd831f6cce1c00f4bde081811728f5ad
onuf <onuf.diana@gmail.com>
2020-10-06 07:24:23 +0300

    GROK-7242: support for multiple file formats (text extraction)

---
+++ packages/NLP/scripts/text-extractor.py
@@ -0,0 +1,45 @@
+#name: Text Extractor
+#description: Extracts text from a file
+#language: python
+#tags: nlp, panel, extractor
+#input: file file [A file that contains text]
+#output: string text {semType: text} [Extracted text]
+#condition: file.isFile && 0 < file.size && file.size < 1e6 && supportedExt(file.name)
+
+import cld3
+import pycountry
+import textract
+
+params = {'filename': file}
+
+if file.endswith(('gif', 'jpg', 'jpeg', 'png', 'tiff', 'tif')):
+    text = textract.process(**params).decode().strip()
+    # The default setting for OCR
+    code = "eng"
+
+    # Detect up to 3 most frequent languages in the text
+    for lang in cld3.get_frequent_languages(text, 3):
+        # Look up the name
+        langcode = lang.language
+        if len(langcode) == 2:
+            language = pycountry.languages.get(alpha_2=langcode).name
+        elif len(langcode) == 3:
+            language = pycountry.languages.get(alpha_3=langcode).name
+        else:
+            # No language detected
+            break
+
+        # Update code for multiple languages scenario, e.g., "eng+fra".
+        # Tesseract might subsequently use only some of the given codes
+        # instead of the full list, which helps to extract non-English texts,
+        # i.e., the default "eng" will be ignored for texts written in French.
+        code += "+" + pycountry.languages.get(name=language).alpha_3
+
+    params.update({'method': 'tesseract', 'language': code})
+
+# Extract text
+text = textract.process(**params).decode().strip()
+
+text = "\n".join(line for line in text.splitlines() if line and not line.isspace())
+if not text or text.isspace():
+    text = "—"
