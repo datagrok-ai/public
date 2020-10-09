bdaf418f9daf193679add076ead8d92a480b1f97
Oleksii Martynchuk <48830393+ZipZaap@users.noreply.github.com>
2020-10-09 05:47:21 -0900

    New Biosensors package, first implementation of Pyphysio pipeline

---
+++ packages/Biosensors/scripts/import.py
@@ -0,0 +1,30 @@
+#name: importPyphysio
+#language: python
+#input: dataframe ecg_data
+#input: int fsamp = 2048
+#input: string signalType = "ecg" {choices : ["ecg"]}
+#output: graphics plt
+
+# import packages
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# import the Signal classes
+import pyphysio as ph
+import pandas as pd
+
+# convert to numpy
+ecg_data = np.array(ecg_data.ecg_data)
+
+# create label
+label = np.zeros(1200)
+label[300:600] = 1
+label[900:1200] = 2
+label = ph.EvenlySignal(label, sampling_freq = 10, signal_type = 'label')
+
+# convert to signal class
+ecg = ph.EvenlySignal(values = ecg_data, sampling_freq = fsamp, signal_type = signalType)
+
+# render
+plt = ecg.plot()
