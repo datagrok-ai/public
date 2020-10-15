7d8a9900ebb64c2d6bc04c54d805b5e8c50e88f4
Andrew Skalkin <skalkin@gmail.com>
2020-10-15 01:52:40 -0400

    GROK-7473: Chem: ability to align structures to a scaffold - WIP

---
+++ packages/Chem/src/chem_column_property_panel.js
@@ -0,0 +1,3 @@
+function getMolColumnPropertyPanel(column) {
+    return ui.div([ui.h1('Mol Column Settings')]);
+}
--- packages/Chem/src/rdkit_cell_renderer.js
+++ packages/Chem/src/rdkit_cell_renderer.js
@@ -11,8 +11,30 @@ class RDKitCellRenderer extends DG.GridCellRenderer {
         let value = gridCell.cell.value;
         if (value == null || value === '')
             return;
+
         let mol = Module.get_mol(value);
-        mol.draw_to_canvas_with_offset(g.canvas, x, -y, w, h);
+        let scaffold = gridCell.tableColumn.dataFrame.columns.byName('smiles').tags['chem-scaffold'];
+
+        if (scaffold === null)
+            // regular drawing
+            mol.draw_to_canvas_with_offset(g.canvas, x, -y, w, h);
+        else {
+            // align to scaffold
+            let qmol = Module.get_mol(scaffold);
+            if (mol.is_valid() && qmol.is_valid()) {
+                const mdetails = mol.get_substruct_match(qmol);
+                const match = JSON.parse(mdetails);
+                const useCoordgen = true;
+
+                if (match.atoms && match.atoms.length)
+                    mol.generate_aligned_coords(qmol, useCoordgen);
+                // if (match.atoms && match.atoms.length)
+                //     draw_with_highlights(mol, match);
+                // else
+                    mol.draw_to_canvas_with_offset(g.canvas, x, -y, w, h);
+            }
+        }
+
         mol.delete();
     }
 }
