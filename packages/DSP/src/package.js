a1fcd1de7ab9bdddee8a6126923e8e4e871e631a
Ivan Stankov <ivanstankov@Ivans-MacBook-Pro.local>
2020-10-22 18:29:17 +0300

    Better Wasm load

--- packages/DSP/src/package.js
+++ packages/DSP/src/package.js
@@ -11,21 +11,20 @@ export let _package = new DG.Package();
 //input: column column_to_filter [Signal to Filter]
 //input: int window_size [SMA Window Size]
 export async function SMA_filter(data, column_to_filter, window_size) {
-    await initWasm();
-    let js_wrapped_sma = Module.cwrap("sma", "null", ["number", "number", "number"]);
+    await initDSPModule();
+    let js_wrapped_sma = DSPModule.cwrap("sma", "null", ["number", "number", "number"]);
     let column_name = column_to_filter.name + ' SMA Filtered';
     let filter_array = column_to_filter.getRawData();
     let nDataBytes = filter_array.length * filter_array.BYTES_PER_ELEMENT;
-    let dataPtr = Module._malloc(nDataBytes);
-    let dataHeap = new Uint8Array(Module.HEAPU8.buffer, dataPtr, nDataBytes);
+    let dataPtr = DSPModule._malloc(nDataBytes);
+    let dataHeap = new Uint8Array(DSPModule.HEAPU8.buffer, dataPtr, nDataBytes);
     dataHeap.set(new Uint8Array(filter_array.buffer));
     js_wrapped_sma(dataPtr, filter_array.length, window_size);
     let result = new Float32Array(dataHeap.buffer, dataHeap.byteOffset, filter_array.length);
     let column = new Float32Array(filter_array.length);
-    let i = 0;
-    for (i = 0; i < filter_array.length; i++) { column[i] = result[i]; }
+    for (let i = 0; i < filter_array.length; i++) { column[i] = result[i]; }
     data.columns.add(DG.Column.fromFloat32Array(column_name, column));
-    Module._free(dataHeap.byteOffset);
+    DSPModule._free(dataHeap.byteOffset);
 }
 
 //top-menu: DSP | Filters | Exponential Filter
@@ -34,22 +33,21 @@ export async function SMA_filter(data, column_to_filter, window_size) {
 //input: column column_to_filter [Signal to Filter]
 //input: double filter_ratio [Exponential Filter Parameter]
 export async function Exp_filter(data, column_to_filter, filter_ratio) {
-    await initWasm();
-    let js_wrapped_exp = Module.cwrap("exps", "null", ["number", "number", "number"]);
+    await initDSPModule();
+    let js_wrapped_exp = DSPModule.cwrap("exps", "null", ["number", "number", "number"]);
     let column_name = column_to_filter.name + ' Exponentially Filtered';
     let filter_array = column_to_filter.getRawData();
     let nDataBytes = filter_array.length * filter_array.BYTES_PER_ELEMENT;
-    let dataPtr = Module._malloc(nDataBytes);
+    let dataPtr = DSPModule._malloc(nDataBytes);
     console.log(dataPtr);
-    let dataHeap = new Uint8Array(Module.HEAPU8.buffer, dataPtr, nDataBytes);
+    let dataHeap = new Uint8Array(DSPModule.HEAPU8.buffer, dataPtr, nDataBytes);
     dataHeap.set(new Uint8Array(filter_array.buffer));
     js_wrapped_exp(dataPtr, filter_array.length, filter_ratio);
     let result = new Float32Array(dataHeap.buffer, dataHeap.byteOffset, filter_array.length);
     let column = new Float32Array(filter_array.length);
-    let i = 0;
-    for (i = 0; i < filter_array.length; i++) { column[i] = result[i]; }
+    for (let i = 0; i < filter_array.length; i++) { column[i] = result[i]; }
     data.columns.add(DG.Column.fromFloat32Array(column_name, column));
-    Module._free(dataHeap.byteOffset);
+    DSPModule._free(dataHeap.byteOffset);
 }
 
 //top-menu: DSP | Filters | Kalmam Filter
@@ -60,21 +58,20 @@ export async function Exp_filter(data, column_to_filter, filter_ratio) {
 //input: double R [Covariance of the observation noise]
 //input: double P [a posteriori estimate covariance]
 export async function Kalman_filter(data, column_to_filter, Q, R, P) {
-    await initWasm();
-    let js_wrapped_kalm = Module.cwrap("kalman", "null", ["number", "number", "number", "number", "number"]);
+    await initDSPModule();
+    let js_wrapped_kalm = DSPModule.cwrap("kalman", "null", ["number", "number", "number", "number", "number"]);
     let column_name = column_to_filter.name + ' Kalman Filtered';
     let filter_array = column_to_filter.getRawData();
     let nDataBytes = filter_array.length * filter_array.BYTES_PER_ELEMENT;
-    let dataPtr = Module._malloc(nDataBytes);
-    let dataHeap = new Uint8Array(Module.HEAPU8.buffer, dataPtr, nDataBytes);
+    let dataPtr = DSPModule._malloc(nDataBytes);
+    let dataHeap = new Uint8Array(DSPModule.HEAPU8.buffer, dataPtr, nDataBytes);
     dataHeap.set(new Uint8Array(filter_array.buffer));
     js_wrapped_kalm(dataPtr, filter_array.length, Q, R, P)
     let result = new Float32Array(dataHeap.buffer, dataHeap.byteOffset, filter_array.length);
     let column = new Float32Array(filter_array.length);
-    let i = 0;
-    for (i = 0; i < filter_array.length; i++) { column[i] = result[i]; }
+    for (let i = 0; i < filter_array.length; i++) { column[i] = result[i]; }
     data.columns.add(DG.Column.fromFloat32Array(column_name, column));
-    Module._free(dataHeap.byteOffset);
+    DSPModule._free(dataHeap.byteOffset);
 }
 
 //top-menu: DSP | Preprocess | Min-Max
@@ -82,21 +79,20 @@ export async function Kalman_filter(data, column_to_filter, Q, R, P) {
 //input: dataframe data [Input data table]
 //input: column column_to_filter [Signal to Filter]
 export async function MinMax_transform(data, column_to_filter) {
-    await initWasm();
-    let js_wrapped_sma = Module.cwrap("minmax", "null", ["number", "number"]);
+    await initDSPModule();
+    let js_wrapped_sma = DSPModule.cwrap("minmax", "null", ["number", "number"]);
     let column_name = column_to_filter.name + ' Min Max Normalized';
     let filter_array = column_to_filter.getRawData();
     let nDataBytes = filter_array.length * filter_array.BYTES_PER_ELEMENT;
-    let dataPtr = Module._malloc(nDataBytes);
-    let dataHeap = new Uint8Array(Module.HEAPU8.buffer, dataPtr, nDataBytes);
+    let dataPtr = DSPModule._malloc(nDataBytes);
+    let dataHeap = new Uint8Array(DSPModule.HEAPU8.buffer, dataPtr, nDataBytes);
     dataHeap.set(new Uint8Array(filter_array.buffer));
     js_wrapped_sma(dataPtr, filter_array.length);
     let result = new Float32Array(dataHeap.buffer, dataHeap.byteOffset, filter_array.length);
     let column = new Float32Array(filter_array.length);
-    let i = 0;
-    for (i = 0; i < filter_array.length; i++) { column[i] = result[i]; }
+    for (let i = 0; i < filter_array.length; i++) { column[i] = result[i]; }
     data.columns.add(DG.Column.fromFloat32Array(column_name, column));
-    Module._free(dataHeap.byteOffset);
+    DSPModule._free(dataHeap.byteOffset);
 }
 
 //top-menu: DSP | Preprocess | Z-score
@@ -104,21 +100,20 @@ export async function MinMax_transform(data, column_to_filter) {
 //input: dataframe data [Input data table]
 //input: column column_to_filter [Signal to Filter]
 export async function Zscore_transform(data, column_to_filter) {
-    await initWasm();
-    let js_wrapped_sma = Module.cwrap("zscore", "null", ["number", "number"]);
+    await initDSPModule();
+    let js_wrapped_sma = DSPModule.cwrap("zscore", "null", ["number", "number"]);
     let column_name = column_to_filter.name + ' Z-score Normalized';
     let filter_array = column_to_filter.getRawData();
     let nDataBytes = filter_array.length * filter_array.BYTES_PER_ELEMENT;
-    let dataPtr = Module._malloc(nDataBytes);
-    let dataHeap = new Uint8Array(Module.HEAPU8.buffer, dataPtr, nDataBytes);
+    let dataPtr = DSPModule._malloc(nDataBytes);
+    let dataHeap = new Uint8Array(DSPModule.HEAPU8.buffer, dataPtr, nDataBytes);
     dataHeap.set(new Uint8Array(filter_array.buffer));
     js_wrapped_sma(dataPtr, filter_array.length);
     let result = new Float32Array(dataHeap.buffer, dataHeap.byteOffset, filter_array.length);
     let column = new Float32Array(filter_array.length);
-    let i = 0;
-    for (i = 0; i < filter_array.length; i++) { column[i] = result[i]; }
+    for (let i = 0; i < filter_array.length; i++) { column[i] = result[i]; }
     data.columns.add(DG.Column.fromFloat32Array(column_name, column));
-    Module._free(dataHeap.byteOffset);
+    DSPModule._free(dataHeap.byteOffset);
 }
 
 //top-menu: DSP | Preprocess | Box Cox
@@ -128,21 +123,20 @@ export async function Zscore_transform(data, column_to_filter) {
 //input: double lambda
 //input: double ofset
 export async function box_cox_transform(data, column_to_filter, lambda, ofset) {
-    await initWasm();
-    let js_wrapped_sma = Module.cwrap("boxcox", "null", ["number", "number", "number", "number"]);
+    await initDSPModule();
+    let js_wrapped_sma = DSPModule.cwrap("boxcox", "null", ["number", "number", "number", "number"]);
     let column_name = column_to_filter.name + ' Box Cox Transformed';
     let filter_array = column_to_filter.getRawData();
     let nDataBytes = filter_array.length * filter_array.BYTES_PER_ELEMENT;
-    let dataPtr = Module._malloc(nDataBytes);
-    let dataHeap = new Uint8Array(Module.HEAPU8.buffer, dataPtr, nDataBytes);
+    let dataPtr = DSPModule._malloc(nDataBytes);
+    let dataHeap = new Uint8Array(DSPModule.HEAPU8.buffer, dataPtr, nDataBytes);
     dataHeap.set(new Uint8Array(filter_array.buffer));
     js_wrapped_sma(dataPtr, filter_array.length, lambda, ofset);
     let result = new Float32Array(dataHeap.buffer, dataHeap.byteOffset, filter_array.length);
     let column = new Float32Array(filter_array.length);
-    let i = 0;
-    for (i = 0; i < filter_array.length; i++) { column[i] = result[i]; }
+    for (let i = 0; i < filter_array.length; i++) { column[i] = result[i]; }
     data.columns.add(DG.Column.fromFloat32Array(column_name, column));
-    Module._free(dataHeap.byteOffset);
+    DSPModule._free(dataHeap.byteOffset);
 }
 
 //top-menu: DSP | Filters | Fourier Filter
@@ -151,22 +145,24 @@ export async function box_cox_transform(data, column_to_filter, lambda, ofset) {
 //input: column column_to_filter [Signal to Filter]
 //input: double lowcut
 //input: double hicut
-export async function fourier_filter(data, column_to_filter, lowcut, hicut) {
-    await initWasm();
-    let js_wrapped_ff = Module.cwrap("ffilter", "null", ["number", "number", "number", "number"]);
+//input: double observationTime[Time taken to record observed signal]
+export async function fourier_filter(data, column_to_filter, lowcut, hicut, observationTime) {
+    await initDSPModule();
+    let lowcuthz = lowcut * observationTime - 1;
+    let hicuthz = hicut * observationTime - 1;
+    let js_wrapped_ff = DSPModule.cwrap("ffilter", "null", ["number", "number", "number", "number"]);
     let column_name = column_to_filter.name + ' Fourier Filtered (L: ' + lowcut + '; H: ' + hicut + ')';
     let filter_array = column_to_filter.getRawData();
     let nDataBytes = filter_array.length * filter_array.BYTES_PER_ELEMENT;
-    let dataPtr = Module._malloc(nDataBytes);
-    let dataHeap = new Uint8Array(Module.HEAPU8.buffer, dataPtr, nDataBytes);
+    let dataPtr = DSPModule._malloc(nDataBytes);
+    let dataHeap = new Uint8Array(DSPModule.HEAPU8.buffer, dataPtr, nDataBytes);
     dataHeap.set(new Uint8Array(filter_array.buffer));
-    js_wrapped_ff(dataPtr, filter_array.length, lowcut, hicut);
+    js_wrapped_ff(dataPtr, filter_array.length, lowcuthz, hicuthz);
     let result = new Float32Array(dataHeap.buffer, dataHeap.byteOffset, filter_array.length);
     let column = new Float32Array(filter_array.length);
-    let i = 0;
-    for (i = 0; i < filter_array.length; i++) { column[i] = result[i]; }
+    for (let i = 0; i < filter_array.length; i++) { column[i] = result[i]; }
     data.columns.add(DG.Column.fromFloat32Array(column_name, column));
-    Module._free(dataHeap.byteOffset);
+    DSPModule._free(dataHeap.byteOffset);
 }
 
 
@@ -176,20 +172,18 @@ export async function fourier_filter(data, column_to_filter, lowcut, hicut) {
 //input: column Column [Column]
 //input: double observationTime[Time taken to record observed signal]
 export async function spectral_density(data, Column, observationTime) {
-    await initWasm();
-    let js_wrapped_sd = Module.cwrap("sdensity", "null", ["number", "number"]);
+    await initDSPModule();
+    let js_wrapped_sd = DSPModule.cwrap("sdensity", "null", ["number", "number"]);
     let column_name = Column.name + ' Density';
     let filter_array = Column.getRawData();
     let nDataBytes = filter_array.length * filter_array.BYTES_PER_ELEMENT;
-    let dataPtr = Module._malloc(nDataBytes);
-    let dataHeap = new Uint8Array(Module.HEAPU8.buffer, dataPtr, nDataBytes);
+    let dataPtr = DSPModule._malloc(nDataBytes);
+    let dataHeap = new Uint8Array(DSPModule.HEAPU8.buffer, dataPtr, nDataBytes);
     dataHeap.set(new Uint8Array(filter_array.buffer));
     js_wrapped_sd(dataPtr, filter_array.length);
     let result = new Float32Array(dataHeap.buffer, dataHeap.byteOffset, filter_array.length);
-    let column = new Float32Array(filter_array.length);
-    let i = 0;
-    for (i = 0; i < filter_array.length; i++) { column[i] = result[i]; }
-    //
+    let column = new Float32Array(Math.floor(filter_array.length / 2));
+    for (let i = 0; i < column.length; i++) { column[i] = result[i]; }
     let newName = data.name + " Densities";
     let tableNames = grok.shell.tableNames;
     let doesExist = tableNames.includes(newName);
@@ -199,7 +193,7 @@ export async function spectral_density(data, Column, observationTime) {
     }
     else {
         let col1 = [];
-        for (i = 0; i < column.length; i++) {
+        for (let i = 0; i < column.length; i++) {
             col1[i] = (i + 1) / observationTime;
         }
         let t = DG.DataFrame.create(column.length);
@@ -208,7 +202,46 @@ export async function spectral_density(data, Column, observationTime) {
         t.name = newName;
         grok.shell.addTableView(t);
     }
-    Module._free(dataHeap.byteOffset);
+    DSPModule._free(dataHeap.byteOffset);
+}
+
+//top-menu: DSP | Preprocess | Spectral Amplitude
+//name: Spectral Amplitude
+//input: dataframe data [Input Dataframe]
+//input: column Column [Column]
+//input: double observationTime[Time taken to record observed signal]
+export async function spectral_amplitude(data, Column, observationTime) {
+    await initDSPModule();
+    let js_wrapped_sd = DSPModule.cwrap("samplitude", "null", ["number", "number"]);
+    let column_name = Column.name + ' Amplitude';
+    let filter_array = Column.getRawData();
+    let nDataBytes = filter_array.length * filter_array.BYTES_PER_ELEMENT;
+    let dataPtr = DSPModule._malloc(nDataBytes);
+    let dataHeap = new Uint8Array(DSPModule.HEAPU8.buffer, dataPtr, nDataBytes);
+    dataHeap.set(new Uint8Array(filter_array.buffer));
+    js_wrapped_sd(dataPtr, filter_array.length);
+    let result = new Float32Array(dataHeap.buffer, dataHeap.byteOffset, filter_array.length);
+    let column = new Float32Array(Math.floor(filter_array.length / 2));
+    for (let i = 0; i < column.length; i++) { column[i] = result[i]; }
+    let newName = data.name + " Amplitudes";
+    let tableNames = grok.shell.tableNames;
+    let doesExist = tableNames.includes(newName);
+    if (doesExist) {
+        let t = grok.shell.tableByName(newName);
+        t.columns.add(DG.Column.fromFloat32Array(column_name, column));
+    }
+    else {
+        let col1 = [];
+        for (let i = 0; i < column.length; i++) {
+            col1[i] = (i + 1) / observationTime;
+        }
+        let t = DG.DataFrame.create(column.length);
+        t.columns.add(DG.Column.fromList('double', 'Frequency', col1));
+        t.columns.add(DG.Column.fromFloat32Array(column_name, column));
+        t.name = newName;
+        grok.shell.addTableView(t);
+    }
+    DSPModule._free(dataHeap.byteOffset);
 }
 
 //top-menu: DSP | Preprocess | Get Trend
@@ -216,21 +249,20 @@ export async function spectral_density(data, Column, observationTime) {
 //input: dataframe data [Input Dataframe]
 //input: column Column [Column]
 export async function get_trend(data, Column) {
-    await initWasm();
-    let js_wrapped_gt = Module.cwrap("gettrend", "null", ["number", "number"]);
+    await initDSPModule();
+    let js_wrapped_gt = DSPModule.cwrap("gettrend", "null", ["number", "number"]);
     let column_name = Column.name + ' Trend';
     let filter_array = Column.getRawData();
     let nDataBytes = filter_array.length * filter_array.BYTES_PER_ELEMENT;
-    let dataPtr = Module._malloc(nDataBytes);
-    let dataHeap = new Uint8Array(Module.HEAPU8.buffer, dataPtr, nDataBytes);
+    let dataPtr = DSPModule._malloc(nDataBytes);
+    let dataHeap = new Uint8Array(DSPModule.HEAPU8.buffer, dataPtr, nDataBytes);
     dataHeap.set(new Uint8Array(filter_array.buffer));
     js_wrapped_gt(dataPtr, filter_array.length);
     let result = new Float32Array(dataHeap.buffer, dataHeap.byteOffset, filter_array.length);
     let column = new Float32Array(filter_array.length);
-    let i = 0;
-    for (i = 0; i < filter_array.length; i++) { column[i] = result[i]; }
+    for (let i = 0; i < filter_array.length; i++) { column[i] = result[i]; }
     data.columns.add(DG.Column.fromFloat32Array(column_name, column));
-    Module._free(dataHeap.byteOffset);
+    DSPModule._free(dataHeap.byteOffset);
 }
 
 //top-menu: DSP | Preprocess | Detrend
@@ -238,19 +270,18 @@ export async function get_trend(data, Column) {
 //input: dataframe data [Input Dataframe]
 //input: column Column [Column]
 export async function remove_trend(data, Column) {
-    await initWasm();
-    let js_wrapped_rt = Module.cwrap("removetrend", "null", ["number", "number"]);
+    await initDSPModule();
+    let js_wrapped_rt = DSPModule.cwrap("removetrend", "null", ["number", "number"]);
     let column_name = Column.name + ' Detrended';
     let filter_array = Column.getRawData();
     let nDataBytes = filter_array.length * filter_array.BYTES_PER_ELEMENT;
-    let dataPtr = Module._malloc(nDataBytes);
-    let dataHeap = new Uint8Array(Module.HEAPU8.buffer, dataPtr, nDataBytes);
+    let dataPtr = DSPModule._malloc(nDataBytes);
+    let dataHeap = new Uint8Array(DSPModule.HEAPU8.buffer, dataPtr, nDataBytes);
     dataHeap.set(new Uint8Array(filter_array.buffer));
     js_wrapped_rt(dataPtr, filter_array.length);
     let result = new Float32Array(dataHeap.buffer, dataHeap.byteOffset, filter_array.length);
     let column = new Float32Array(filter_array.length);
-    let i = 0;
-    for (i = 0; i < filter_array.length; i++) { column[i] = result[i]; }
+    for (let i = 0; i < filter_array.length; i++) { column[i] = result[i]; }
     data.columns.add(DG.Column.fromFloat32Array(column_name, column));
-    Module._free(dataHeap.byteOffset);
+    DSPModule._free(dataHeap.byteOffset);
 }
