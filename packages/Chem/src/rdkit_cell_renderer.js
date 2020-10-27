d554eda3449c032bcfc11f9ec2db23b350bb2382
dskatov <dskatov@datagrok.ai>
2020-10-27 17:13:18 +0300

    GROK-7473: Chem: ability to align structures to a scaffold - WIP; column-based scaffolds

--- packages/Chem/src/rdkit_cell_renderer.js
+++ packages/Chem/src/rdkit_cell_renderer.js
@@ -8,34 +8,68 @@ class RDKitCellRenderer extends DG.GridCellRenderer {
     get cellType() { return 'RDMolecule'; }
 
     render(g, x, y, w, h, gridCell, cellStyle) {
+        
         let value = gridCell.cell.value;
         if (value == null || value === '')
             return;
 
         let mol = Module.get_mol(value);
-        let scaffold = gridCell.tableColumn.dataFrame.columns.byName('smiles').tags['chem-scaffold'];
-
-        if (scaffold === null) {
-            // regular drawing
-            let qmol = Module.get_mol("");
-            mol.generate_aligned_coords(qmol, true);
-            mol.draw_to_canvas_with_offset(g.canvas, x, -y, w, h);
-            qmol.delete();
-        } else {
-            // align to scaffold
-            let qmol = Module.get_mol(scaffold);
-            if (mol.is_valid() && qmol.is_valid()) {
-                //const mdetails = mol.get_substruct_match(qmol);
-                // const match = JSON.parse(mdetails);
-                // const useCoordgen = true;
-                // if (match.atoms && match.atoms.length)
-                //    draw_with_highlights(mol, match);
-                mol.generate_aligned_coords(qmol, true);
-                mol.draw_to_canvas_with_offset(g.canvas, x, -y, w, h);
+        // let scaffold = gridCell.tableColumn.dataFrame.columns.byName('smiles').tags['chem-scaffold'];
+        let df = gridCell.tableColumn.dataFrame;
+        let rowScaffoldCol = null;
+        for (let j = 0; j < df.columns.length; ++j) {
+            let col = df.columns.byIndex(j);
+            let tags = col.tags;
+            if (tags && tags['row-scaffold']) {
+                rowScaffoldCol = col;
+                break;
             }
-            qmol.delete();
         }
-
+        let smilesToMolBlock = function (smiles) {
+            let qmol = Module.get_mol(smiles);
+            let scaffoldInMolBlock = qmol.get_v3Kmolblock();
+            qmol.delete();
+            return scaffoldInMolBlock;
+        }
+        let drawAtomic = function (mol) {
+            mol.draw_to_canvas_with_offset(g.canvas, x, -y, w, h);
+        }
+        let drawSimple = function (mol) {
+            if (gridCell.tableColumn.name === 'scaffold') {
+                // specially draw with MolBlock orientation
+                let smol = Module.get_mol(smilesToMolBlock(value));
+                mol.generate_aligned_coords(smol, true);
+                drawAtomic(mol);
+                smol.delete();
+            } else {
+                drawAtomic(mol);
+            }
+        }
+        if (rowScaffoldCol == null) {
+            // regular drawing
+            drawSimple(mol);
+        } else {
+            let idx = gridCell.tableRowIndex;
+            let scaffold = df.get(rowScaffoldCol.name, idx);
+            // align to scaffold
+            let smol = Module.get_mol(smilesToMolBlock(scaffold));
+            try {
+                if (mol.is_valid() && smol.is_valid()) {
+                    const mdetails = mol.get_substruct_match(smol);
+                    const match = JSON.parse(mdetails);
+                    // draw_with_highlights(mol, match);
+                    if (match.atoms && match.atoms.length) {
+                        mol.generate_aligned_coords(smol, true);
+                        drawAtomic(mol);
+                    } else {
+                        drawSimple(mol);
+                    }
+                }
+            } catch {
+                drawSimple(mol);
+            }
+            smol.delete();
+        }
         mol.delete();
     }
 }
