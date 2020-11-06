4d3da42f2c6cb7927909823269f214996bf962b7
onuf <onuf.diana@gmail.com>
2020-11-06 07:32:57 +0200

    NLP package updates

--- packages/NLP/detectors.js
+++ packages/NLP/detectors.js
@@ -5,16 +5,20 @@ class NlpPackageDetectors extends DG.Package {
     //input: file file
     //output: bool result
     detectTextFile(file) {
-        let extensions = ["csv", "doc", "docx", "eml", "epub", "gif", "htm", "html",
-                        "jpeg", "jpg", "json", "log", "mp3", "msg", "odt", "ogg",
-                        "pdf", "png", "pptx", "ps", "psv", "rtf", "tff", "tif",
-                        "tiff", "tsv", "txt", "wav", "xls", "xlsx"];
+        let extensions = ["doc", "docx", "odt", "pdf", "rtf", "txt"];
         return extensions.includes(file.extension);
     }
 
     //input: string filename
     //output: bool result
     supportedExt(filename) {
+        let extensions = ["doc", "docx", "odt", "pdf", "rtf", "txt"];
+        return extensions.some(ext => filename.endsWith(ext));
+    }
+
+    //input: string filename
+    //output: bool result
+    supportedExtExtract(filename) {
         let extensions = ["csv", "doc", "docx", "eml", "epub", "gif", "htm", "html",
                         "jpeg", "jpg", "json", "log", "mp3", "msg", "odt", "ogg",
                         "pdf", "png", "pptx", "ps", "psv", "rtf", "tff", "tif",
