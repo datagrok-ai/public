065ad6fdbc4edd2e593ad7c5b67684c3426bc1ff
Oleksii Martynchuk <48830393+ZipZaap@users.noreply.github.com>
2020-11-09 12:47:28 -0800

    GROK-7190: ui overhaul and major changes to internal pipelines

--- packages/Impute/scripts/cleandata.R
+++ packages/Impute/scripts/cleandata.R
@@ -3,10 +3,6 @@
 #language: r
 #tags: template, demo
 #input: dataframe data [Input data table]
-#input: column_list columns [list of all columns of interest]
-#input: double varRemovalThreshold = 0.5 [% missingness per column]
-#input: double indRemovalThreshold = 0.5 [% missingness per row]
-#input: double naCoding [values to be manually converted to NA]
 #input: bool meta = FALSE [whether to collect metadata]
 #output: dataframe cleanDf [processed dataframe]
 #output: graphics naPlot
@@ -19,57 +15,6 @@ require(stats)
 require(ltm)
 require(ggplot2)
 require(ggdendro)
-
-# CLEAN function
-clean <- function(X, var_remove = NULL, var_removal_threshold = 0.5, ind_removal_threshold = 1,
-                  missingness_coding = NA) {
-
-  # remove undesired variables
-  if (!is.null(var_remove)) {X[var_remove] <- NULL }
-
-  # give warning when strings are present variables
-  strings_present <- sum(sapply(X, is.character)) > 0
-
-  if (strings_present == TRUE) {
-    stop("Warning! Your data contains string variables. Please inspect your data and either remove these variables using the
-          var_remove argument or convert them into type factor/numeric where applicable.") }
-
-  # convert all variables to numeric
-  vars_non_num <- names(X)[!sapply(X, is.numeric)]
-
-  if (length(vars_non_num) != 0) {
-    X <- as.data.frame(sapply(X, as.numeric)) }
-  if (length(vars_non_num) != 0) {
-    message(paste("Variable(s) ", (paste(vars_non_num, collapse = ", ")), " converted to numeric.",
-                  sep = "")) }
-
-  # convert to NA
-  X <- as.data.frame(lapply(X, function(x) replace(x, x %in% missingness_coding,
-                                                   NA)))
-
-  # remove variables above missingness threshold
-  missfrac_per_var <- colMeans(is.na(X))
-  vars_above_thres <- colnames(X)[missfrac_per_var >= var_removal_threshold]
-  if (length(vars_above_thres) != 0)
-    new_df <- X[, -which(missfrac_per_var >= var_removal_threshold)] else new_df <- X
-
-  if (length(vars_above_thres) != 0) {
-    message(paste("Variable(s) ", (paste(vars_above_thres, collapse = ", ")), " removed due to exceeding the pre-defined removal threshold (>",
-                  var_removal_threshold * 100, "%) for missingness.", sep = "")) }
-
-  # remove individuals above missingness threshold
-  missfrac_per_ind <- rowMeans(is.na(new_df))
-  inds_above_thres <- rownames(X)[missfrac_per_ind >= ind_removal_threshold]
-  if (length(inds_above_thres) != 0) {
-    clean_df <- new_df[-which(missfrac_per_ind >= ind_removal_threshold), ] } else { clean_df <- new_df }
-
-  if (length(inds_above_thres) != 0) {
-    message(paste(length(inds_above_thres), " individual(s) removed due to exceeding the pre-defined removal threshold (>",
-                  ind_removal_threshold * 100, "%) for missingness.", sep = "")) }
-
-  return(clean_df)
-
-}
 
 # METADATA function
 get_data <- function(X) {
@@ -154,11 +99,10 @@ get_data <- function(X) {
 }
 
 
-X <- data[,columns]
-X <- clean(X,
-           var_removal_threshold = varRemovalThreshold,
-           ind_removal_threshold = indRemovalThreshold,
-           missingness_coding = naCoding)
+# convert all variables to numeric
+vars_non_num <- names(data)[!sapply(data, is.numeric)]
+if (length(vars_non_num) != 0) {
+  X <- as.data.frame(sapply(data, as.numeric)) }
 
 if (meta == TRUE) {
   metadata <- get_data(X)
--- packages/Impute/scripts/mice.R
+++ packages/Impute/scripts/mice.R
@@ -3,11 +3,21 @@
 #language: r
 #tags: template, demo
 #input: dataframe data [Input data table]
+#input: column_list columns
 #input: int maxIter = 20 [ scalar giving the number of impute iterations]
 #output: dataframe imputedDF [imputed dataset]
 
 require(mice)
+require(gdata)
+
+# convert all variables to numeric
+vars_non_num <- names(data)[!sapply(data, is.numeric)]
+bigMap <- mapLevels(data[,c(vars_non_num)])
+if (length(vars_non_num) != 0) {
+  data <- as.data.frame(sapply(data, as.integer)) }
 
 imputedData <- mice::mice(data, m = 1,maxit = maxIter)
 imputedDF <- mice::complete(imputedData,1)
 
+mapLevels(imputedDF[,c(vars_non_num)]) <- bigMap
+imputedDF <- imputedDF[,columns]
