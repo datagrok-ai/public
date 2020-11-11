55fbab32ad2d969999b771b1883c483d40b967a9
Kostya <doncovkonstantin@gmail.com>
2020-11-12 00:37:02 +0200

    GROK-106: UI usage analysis system: events filter, context menus for LogEvent and User, added three filters(date, user, event) in queries

--- packages/UsageAnalysis/queries/queries.sql
+++ packages/UsageAnalysis/queries/queries.sql
@@ -43,23 +43,21 @@ select * from month_errors, week_errors, day_errors;
 --end
 
 
---name: unique users per day last month
+--name: unique users per day on @date and @users and @events
+--input: string date { pattern: datetime }
+--input: list users
+--input: list events
 --connection: datagrok
-select d.date::date, count(t.id) as user_count from (
-	(
-		select date_trunc('day', (current_date - offs)) as date
-    	from generate_series(0, 30, 1) as offs
-    ) d
-    left outer join
-    (
-    	select distinct on (date(e.event_time), u.id) u.id, e.event_time as date
-    	from events e
-		inner join users_sessions s on e.session_id = s.id
-		inner join users u on u.id = s.user_id
-	) t
-	on d.date = date_trunc('day', t.date)
-)
-group by d.date;
+select t.date::date, count(t.id) as user_count from (
+	select distinct on (date(e.event_time), u.id) u.id, e.event_time as date
+	from events e
+	inner join users_sessions s on e.session_id = s.id
+	inner join users u on u.id = s.user_id
+	where @date(e.event_time)
+	and (u.login = any(@users) or @users = ARRAY['all'])
+	and (e.friendly_name = any(@events) or @events = ARRAY['all'])
+) t
+group by t.date::date;
 --end
 
 
@@ -74,47 +72,54 @@ group by u.name, u.id
 --end
 
 
---name: events on @date
+----name: events on @date
+----input: string date { pattern: datetime }
+----connection: datagrok
+--select u.login as user, u.id as user_id, t.name, t.source, e.id as event_id, e.event_time, e.description from events e
+--inner join event_types t on e.event_type_id = t.id
+--inner join users_sessions s on e.session_id = s.id
+--inner join users u on u.id = s.user_id
+--where @date(e.event_time)
+--order by e.event_time desc
+----end
+
+
+--name: events on @date and @users and @events
 --input: string date { pattern: datetime }
+--input: list users
+--input: list events
 --connection: datagrok
 select u.login as user, u.id as user_id, t.name, t.source, e.id as event_id, e.event_time, e.description from events e
 inner join event_types t on e.event_type_id = t.id
 inner join users_sessions s on e.session_id = s.id
 inner join users u on u.id = s.user_id
 where @date(e.event_time)
+and (u.login = any(@users) or @users = ARRAY['all'])
+and (e.friendly_name = any(@events) or @events = ARRAY['all'])
 order by e.event_time desc
 --end
 
 
---name: events on @date and @users
+----name: errors on @date
+----input: string date { pattern: datetime }
+----connection: datagrok
+--select friendly_name as name, event_time, error_message, error_stack_trace, source, run_number as count, e.id as event_id from events e
+--where @date(e.event_time) and e.error_stack_trace is not null
+----end
+
+
+--name: errors on @date and @users and @events
 --input: string date { pattern: datetime }
 --input: list users
---connection: datagrok
-select u.login as user, u.id as user_id, t.name, t.source, e.id as event_id, e.event_time, e.description from events e
-inner join event_types t on e.event_type_id = t.id
-inner join users_sessions s on e.session_id = s.id
-inner join users u on u.id = s.user_id
-where @date(e.event_time) and u.login = any(@users)
-order by e.event_time desc
---end
-
-
---name: errors on @date
---input: string date { pattern: datetime }
---connection: datagrok
-select friendly_name as name, event_time, error_message, error_stack_trace, source, run_number as count, e.id as event_id from events e
-where @date(e.event_time) and e.error_stack_trace is not null
---end
-
-
---name: errors on @date and @users
---input: string date { pattern: datetime }
---input: list users
+--input: list events
 --connection: datagrok
 select e.friendly_name as name, e.event_time, e.error_message, e.error_stack_trace, e.source, e.run_number as count, e.id as event_id from events e
 inner join users_sessions s on e.session_id = s.id
 inner join users u on u.id = s.user_id
-where @date(e.event_time) and e.error_stack_trace is not null and u.login = any(@users)
+where e.error_stack_trace is not null
+and @date(e.event_time)
+and (u.login = any(@users) or @users = ARRAY['all'])
+and (e.friendly_name = any(@events) or @events = ARRAY['all'])
 --end
 
 
