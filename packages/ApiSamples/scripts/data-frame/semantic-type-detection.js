424d5b46a07d9c5b3a1434f3af1ae5c58581dd44
onuf <onuf.diana@gmail.com>
2020-11-12 07:36:47 +0200

    Changed the table name

--- packages/ApiSamples/scripts/data-frame/semantic-type-detection.js
+++ packages/ApiSamples/scripts/data-frame/semantic-type-detection.js
@@ -1,14 +1,14 @@
 // Custom handling of the semantic type detection
 // See https://datagrok.ai/help/discover/semantic-types
 
-let demog = grok.data.parseCsv('smiles\nFc1cc(Cl)ccc1Br');
+let table = grok.data.parseCsv('smiles\nFc1cc(Cl)ccc1Br');
 
 // Normally, the smiles column will be detected as 'Molecule' when we create a table view.
 // However, this time we will override it
 
-demog.onSemanticTypeDetecting.subscribe((_) => {
-    demog.col('smiles').semType = 'bananas';
+table.onSemanticTypeDetecting.subscribe((_) => {
+    table.col('smiles').semType = 'bananas';
 });
-demog.onSemanticTypeDetected.subscribe((_) => { grok.shell.info(demog.col('smiles').semType); });
+table.onSemanticTypeDetected.subscribe((_) => { grok.shell.info(table.col('smiles').semType); });
 
-grok.shell.addTableView(demog);
+grok.shell.addTableView(table);
