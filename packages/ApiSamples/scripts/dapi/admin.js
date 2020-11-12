2b119cf379f0f0482bd33beb17de8ce4feedacaf
Kostya <doncovkonstantin@gmail.com>
2020-11-12 15:02:46 +0200

    Fixed ui.table()

--- packages/ApiSamples/scripts/dapi/admin.js
+++ packages/ApiSamples/scripts/dapi/admin.js
@@ -2,5 +2,5 @@ grok.dapi.admin.getServiceInfos().then(result => {
     let v = grok.shell.newView('list');
 
     v.root.appendChild(ui.table(result, (item, idx) =>
-        [`${item.key}:`, item.status]).root);
+        [`${item.key}:`, item.status]));
 });
