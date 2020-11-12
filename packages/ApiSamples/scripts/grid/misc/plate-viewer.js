1d8cb9f3537be3821272c2dcc2872768ba716705
Andrew Skalkin <skalkin@gmail.com>
2020-11-12 14:09:48 -0500

    Plate viewer prototype - WIP

---
+++ packages/ApiSamples/scripts/grid/misc/plate-viewer.js
@@ -0,0 +1,12 @@
+// Use grid as a plate viewer
+
+let wells = grok.data.demo.wells(8 * 12);
+let plate = wells
+    .groupBy(['row'])
+    .pivot('col')
+    .min('volume')
+    .aggregate();
+
+let grid = DG.Viewer.fromType(DG.VIEWER.HEAT_MAP, plate);
+let view = grok.shell.newView();
+view.append(grid);
