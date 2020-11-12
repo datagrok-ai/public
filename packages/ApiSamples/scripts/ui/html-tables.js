967c4f77485a79b4cf95fefc5159d4ae54c9e626
Kostya <doncovkonstantin@gmail.com>
2020-11-13 00:06:59 +0200

    GROK-7600: API: add HtmlTable with remove method

--- packages/ApiSamples/scripts/ui/html-tables.js
+++ packages/ApiSamples/scripts/ui/html-tables.js
@@ -1,10 +1,24 @@
 // Creating HTML-driven tables
 // For high-performance grid, check out samples under /grid
 
-grok.shell.newView('tables', [
+grok.shell.newView('table from map', [
     ui.tableFromMap({
         user: grok.shell.user.toMarkup(),
         project: grok.shell.project.toMarkup(),
         time: new Date(),
     })
 ]);
+
+let view = grok.shell.newView('table with renderer');
+var myList = [
+  {key: 'first object', 'value': 5},
+  {'key': 'second object', value: false},
+  {key: 'third object', value: false},
+];
+
+let table = DG.HtmlTable.create(myList, (item, idx) => [item.key, item.value]);
+
+//and let's remove the third object from the table
+table.remove(myList[2]);
+
+view.root.appendChild(table.root);
