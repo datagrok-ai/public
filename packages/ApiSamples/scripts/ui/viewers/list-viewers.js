3b1eee962416355be71206a1c5e7a645d4f1bb57
Kostya <doncovkonstantin@gmail.com>
2020-11-17 20:34:38 +0200

    Added list-viewers snippet

---
+++ packages/ApiSamples/scripts/ui/viewers/list-viewers.js
@@ -0,0 +1,10 @@
+// You can iterate over the added to the TableView viewers
+
+let parentView = grok.shell.addTableView(grok.data.demo.demog());
+
+parentView.scatterPlot({x: 'height', y: 'weight'});
+parentView.histogram({value: 'age'});
+parentView.lineChart();
+
+for (let view of parentView.viewers)
+  console.log(view);
