d5cb5db048c8db87c04286121483d1ec284eea35
onuf <onuf.diana@gmail.com>
2020-11-19 18:08:53 +0200

    GROK-7617: Chord Diagram (WIP)
    code restructuring

--- packages/Chord/src/chord-viewer.js
+++ packages/Chord/src/chord-viewer.js
@@ -9,14 +9,37 @@ export class ChordViewer extends DG.JsViewer {
     constructor() {
         super();
         this.initialized = false;
+        this.numColumns = [];
+        this.strColumns = [];
+        this.inpCol;
+        this.outCol;
+        this.data = [];
+        this.conf = {};
+        this.chords = [];
+        this.chordConf = {};
     }
 
     init() {
         this.initialized = true;
     }
 
+    testColumns() {
+        return (this.strColumns.length >= 2 && this.numColumns.length >= 1);
+    }
+
     onTableAttached() {
         this.init();
+
+        let columns = this.dataFrame.columns.toList();
+        this.strColumns = columns.filter(col => col.type === 'string');
+        this.numColumns = columns.filter(col => ['double', 'int'].includes(col.type));
+
+        if (this.testColumns()) {
+            // TODO: Choose the most relevant columns
+            this.inpCol = this.strColumns[0];
+            this.outCol = this.strColumns[1];
+            this.generateData();
+        }
 
         this.subs.push(DG.debounce(this.dataFrame.selection.onChanged, 50).subscribe((_) => this.render()));
         this.subs.push(DG.debounce(this.dataFrame.filter.onChanged, 50).subscribe((_) => this.render()));
@@ -26,48 +49,33 @@ export class ChordViewer extends DG.JsViewer {
     }
 
     onPropertyChanged(property) {
-        if (this.initialized)
+        super.onPropertyChanged(property);
+        if (this.initialized && this.testColumns()) {
             this.render();
+        }
     }
 
     detach() {
         this.subs.forEach((sub) => sub.unsubscribe());
     }
 
-    render() {
+    generateData() {
 
-        // Make a new method #1 [Q: do column methods like isNumeric/isAlpha exist?]
-        let columns = this.dataFrame.columns.toList();
-        let strColumns = columns.filter(col => col.type === 'string');
-        let numColumns = columns.filter(col => ['double', 'int'].includes(col.type));
-        if (strColumns.length < 2 || numColumns.length < 1) {
-            // TODO: Disable the viewer's icon in the menu
-            this.root.innerText = "Not enough data to produce the result."
-            return;
-        }
-
-        // Make a new method #2
-        // TODO: Choose the appropriate columns
-        let inpCol = strColumns[0];
-        let outCol = strColumns[1];
-        let numCol = numColumns[0];
-
-        let data = inpCol.categories.map(cat => { return {
-                id: cat.toLowerCase(),
-                label: cat,
-                len: 0,
-                color: "#80b1d3"
+        this.data = this.inpCol.categories.map(cat => { return {
+            id: cat.toLowerCase(),
+            label: cat,
+            len: 0,
+            color: "#80b1d3"
         }});
-        let chords = [];
 
-        for (let i = 0; i < inpCol.length; i++) {
-            let inpId = inpCol.get(i).toLowerCase();
-            let outId = outCol.get(i).toLowerCase();
+        for (let i = 0; i < this.inpCol.length; i++) {
 
-            data.find(obj => obj.id === inpId).len += 1;
+            let inpId = this.inpCol.get(i).toLowerCase();
+            let outId = this.outCol.get(i).toLowerCase();
+            this.data.find(obj => obj.id === inpId).len += 100;
 
-            // TODO: Calculate `start` and `end` based on numColumn(s)
-            chords.push({
+            // TODO: Calculate `start` and `end` based on numColumns
+            this.chords.push({
                 source: {
                     id: inpId,
                     start: 1,
@@ -80,30 +88,37 @@ export class ChordViewer extends DG.JsViewer {
                 }
             });
         }
-        console.log(data);
-        console.log(chords);
+        console.log(this.data);
+        console.log(this.chords);
+    }
 
+    render() {
 
-        this.root.style += "width: 100%; height: 100%;";
+        if (!this.testColumns()) {
+            this.root.innerText = "Not enough data to produce the result.";
+            return;
+        }
+
+        this.root.classList.add('viewer-window');
         let size = Math.min(this.root.clientWidth, this.root.clientHeight);
         let svg = d3.select(this.root)
                     .append("div")
                         .attr('id', 'chart')
                         .attr('class', 'chord-diagram-container')
                         .attr('width', size)
-                        .attr('height', size)
+                        .attr('height', size);
 
         let circos = Circos({
             container: '#chart',
             width: size,
             height: size
-        })
+        });
 
-        circos.layout(data);
-        circos.chords('beta-track', chords);
+        circos.layout(this.data);
+        circos.chords('beta-track', this.chords);
         circos.render();
         document.getElementById('chart')
                 .children[0].children[0]
-                .setAttribute('viewBox', `${-size/2} ${-size/2} ${size*2} ${size*2}`)
+                .setAttribute('viewBox', `${-size/2} ${-size/2} ${size*2} ${size*2}`);
     }
 }
