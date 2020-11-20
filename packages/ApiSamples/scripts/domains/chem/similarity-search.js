d952077136249e2e00bd394374870eb8a92fee18
dskatov <dskatov@datagrok.ai>
2020-11-20 12:03:17 +0300

    GROK-7438 RDKit-based searches. Moved samples GROK-6504 Use RDKit for structure rendering. Scaffold column rendering

--- packages/ApiSamples/scripts/domains/chem/similarity-search.js
+++
@@ -1,8 +0,0 @@
-// An example of using similarity search.
-//
-// https://datagrok.ai/help/domains/chem/similarity-search
-
-grok.data.loadTable('https://public.datagrok.ai/demo/sar_small.csv')
-    .then(molecules => grok.chem.similaritySearch(molecules.col('smiles'), 'O=C1CN=C(C2CCCCC2)C2:C:C:C:C:C:2N1')
-        .then(similar => grok.shell.addTableView(similar)));
-
