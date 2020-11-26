90c910f01706d5649cefcc8c3211885c6f4d95a5
Alex Paramonov <aparamonov@datagrok.ai>
2020-11-26 12:00:27 +0300

    JS Docking example

--- packages/ApiSamples/scripts/ui/docking/docking-table-view.js
+++ packages/ApiSamples/scripts/ui/docking/docking-table-view.js
@@ -1,2 +1,15 @@
-let view = grok.shell.addTableView(grok.data.demo.demog());
-view.dockManager.dock(ui.divText('Element'));
+let table = grok.data.demo.demog();
+let view = grok.shell.addTableView(table);
+
+let div = ui.divText('');
+view.dockManager.dock(div);
+view.dockManager.dock(div, 'left'); //dock to another side at any time
+
+let viewer = DG.Viewer.fromType('Scatter Plot', table);
+view.addViewer(viewer);
+
+view.dockManager.dock(viewer, 'right');
+
+div.append(ui.bigButton('close', (_) => {
+    view.dockManager.close(div);
+}));
