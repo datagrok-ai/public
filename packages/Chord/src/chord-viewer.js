6f6d2d3687df46f7935203de7d8a70d0df96821c
onuf <onuf.diana@gmail.com>
2020-11-30 12:40:06 +0200

    GROK-7617: Chord Diagram (WIP)
    Improvements: configuration for chords, columns can be selected from the viewer's properties, segment length depends on value frequency, aggregation result gets displayed on mouseover

--- packages/Chord/src/chord-viewer.js
+++ packages/Chord/src/chord-viewer.js
@@ -2,7 +2,7 @@ import * as ui from 'datagrok-api/ui';
 import * as DG from 'datagrok-api/dg';
 import * as Circos from 'circos';
 import * as d3 from 'd3';
-import { layoutConf } from './configuration.js';
+import { layoutConf, chordConf } from './configuration.js';
 
 
 export class ChordViewer extends DG.JsViewer {
@@ -22,7 +22,7 @@ export class ChordViewer extends DG.JsViewer {
         this.data = [];
         this.conf = layoutConf;
         this.chords = [];
-        this.chordConf = {};
+        this.chordConf = chordConf;
     }
 
     init() {
@@ -40,12 +40,8 @@ export class ChordViewer extends DG.JsViewer {
         this.strColumns = columns.filter(col => col.type === 'string');
         this.numColumns = columns.filter(col => ['double', 'int'].includes(col.type));
 
-        if (this.testColumns()) {
-            // TODO: Choose the most relevant columns
-            this.fromColumnName = this.strColumns[0].name;
-            this.toColumnName = this.strColumns[1].name;
-            this.generateData();
-        }
+        // TODO: Choose the most relevant columns
+        if (this.testColumns()) this.generateData(this.strColumns[0].name, this.strColumns[1].name);
 
         this.subs.push(DG.debounce(this.dataFrame.selection.onChanged, 50).subscribe((_) => this.render()));
         this.subs.push(DG.debounce(this.dataFrame.filter.onChanged, 50).subscribe((_) => this.render()));
@@ -57,6 +53,12 @@ export class ChordViewer extends DG.JsViewer {
     onPropertyChanged(property) {
         super.onPropertyChanged(property);
         if (this.initialized && this.testColumns()) {
+            if (property.name === 'fromColumnName') {
+                this.generateData(property.get(), this.toColumnName);
+            }
+            if (property.name === 'toColumnName') {
+                this.generateData(this.fromColumnName, property.get());
+            }
             this.render();
         }
     }
@@ -65,7 +67,10 @@ export class ChordViewer extends DG.JsViewer {
         this.subs.forEach((sub) => sub.unsubscribe());
     }
 
-    generateData() {
+    generateData(fromColumnName, toColumnName) {
+        
+        this.fromColumnName = fromColumnName;
+        this.toColumnName = toColumnName;
 
         this.aggregatedTable = this.dataFrame
             .groupBy([this.fromColumnName, this.toColumnName])
@@ -74,15 +79,18 @@ export class ChordViewer extends DG.JsViewer {
 
         this.fromCol = this.aggregatedTable.getCol(this.fromColumnName);
         this.toCol = this.aggregatedTable.getCol(this.toColumnName);
+        this.freqMap = {};
 
-        function toCircos(s) { return {
-            id: s,
-            label: s,
-            len: 30,
-            color: "#80b1d3"
-        }}
-
-        this.data = Array.from(new Set(this.fromCol.categories.concat(this.toCol.categories))).map(toCircos);
+        this.fromCol.toList().forEach(k => this.freqMap[k] = (this.freqMap[k] || 0) + 1);
+        this.toCol.toList().forEach(k => this.freqMap[k] = (this.freqMap[k] || 0) + 1);
+        this.data = Array.from(new Set(this.fromCol.categories.concat(this.toCol.categories)))
+                         .sort((a, b) => this.freqMap[b] - this.freqMap[a])
+                         .map(s => { return {
+                             id: s,
+                             label: s,
+                             len: this.freqMap[s],
+                             color: "#80b1d3"
+                         }});
     }
 
     computeChords() {
@@ -90,6 +98,7 @@ export class ChordViewer extends DG.JsViewer {
         for (let i = 0; i < this.aggregatedTable.rowCount; i++) {
             let sourceId = this.fromCol.get(i);
             let targetId = this.toCol.get(i);
+            let count = this.aggregatedTable.getCol('count').get(i);
             let sourceBlock = this.data.find(obj => obj.id === sourceId);
             let targetBlock = this.data.find(obj => obj.id === targetId);
             let sourceCenter = (sourceBlock.end - sourceBlock.start) / 2;
@@ -106,7 +115,8 @@ export class ChordViewer extends DG.JsViewer {
                     id: targetId,
                     start: targetCenter - (targetCenter / 2),
                     end: targetCenter + (targetCenter / 2)
-                }
+                },
+                value: count
             });
         }
 
@@ -121,7 +131,9 @@ export class ChordViewer extends DG.JsViewer {
 
         this.root.classList.add('viewer-window');
         $(this.root).empty();
-        let size = Math.min(this.root.clientWidth, this.root.clientHeight);
+        let width = this.root.clientWidth;
+        let height = this.root.clientHeight;
+        let size = Math.min(width, height);
         let svg = d3.select(this.root)
                     .append("div")
                         .attr('id', 'chart')
@@ -137,10 +149,10 @@ export class ChordViewer extends DG.JsViewer {
 
         circos.layout(this.data, this.conf);
         this.computeChords();
-        circos.chords('beta-track', this.chords);
+        circos.chords('beta-track', this.chords, this.chordConf);
         circos.render();
         document.getElementById('chart')
                 .children[0].children[0]
-                .setAttribute('viewBox', `${-size/2} ${-size/2} ${size*2} ${size*2}`);
+                .setAttribute('viewBox', `${-width/2} ${-height/2} ${size*2} ${size*2}`);
     }
 }
