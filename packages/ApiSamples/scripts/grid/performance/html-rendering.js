9ba5987f73d24860be04c7a4e8d9b6f7feec737e
Andrew Skalkin <skalkin@gmail.com>
2020-12-02 15:39:04 -0500

    Re-enabled OCL rendering

---
+++ packages/ApiSamples/scripts/grid/performance/html-rendering.js
@@ -0,0 +1,12 @@
+// Dynamically generated HTML-based cells
+
+let t = grok.data.demo.demog();
+let view = grok.shell.addTableView(t);
+for (let c of t.columns.toList())
+    view.grid.columns.byName(c.name).cellType = 'html';
+
+view.grid.onCellPrepare(function (gc) {
+    if (gc.isTableCell) {
+        gc.style.element = ui.divText(gc.cell.value.toString());
+    }
+});
