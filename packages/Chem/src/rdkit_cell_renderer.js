7dab670b102a458591e58ebf6c3301e19dbdab92
dskatov <dskatov@datagrok.ai>
2020-12-02 15:36:09 +0300

    GROK-6504 Use RDKit for structure rendering. No Coordgen

--- packages/Chem/src/rdkit_cell_renderer.js
+++ packages/Chem/src/rdkit_cell_renderer.js
@@ -23,14 +23,8 @@ class RDKitCellRenderer extends DG.GridCellRenderer {
         let value = gridCell.cell.value;
         if (value == null || value === '')
             return;
-        
-        let mol = this.molCache.getOrCreate(value, (s) => {
-            let mol = Module.get_mol(s);
-            //let smol = Module.get_mol("");
-            //mol.generate_aligned_coords(smol, true);
-            //smol.delete();
-            return mol;
-        });
+
+        let mol = this.molCache.getOrCreate(value, (s) => Module.get_mol(s));
 
         if (!mol.is_valid())
             return;
