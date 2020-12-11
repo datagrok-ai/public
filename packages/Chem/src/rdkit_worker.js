1cc845af680abccec5d69e18075025f8ec148924
dskatov <dskatov@datagrok.ai>
2020-12-11 18:26:19 +0300

    GROK-7438: RDKit-based searches. Continue indexing even if get_mol crashes while indexing

--- packages/Chem/src/rdkit_worker.js
+++ packages/Chem/src/rdkit_worker.js
@@ -5,25 +5,25 @@ let _rdKitModule = null;
 let _substructLibrary = null;
 
 onmessage = async function (e) {
-  
-  const {op, args} = e.data;
-  let port = e.ports[0];
-  
-  if (op === 'module::init') {
-    _rdKitModule = await initRDKitModule();
-    console.log("RDKit (worker) initialized");
-    _substructLibrary = new RdKitSubstructLibrary(_rdKitModule);
-    port.postMessage({ op: op });
-  } else if (op === 'substructLibrary::init') {
-    _substructLibrary.init(args[0]);
-    port.postMessage({ op: op });
-  } else if (op === 'substructLibrary::search') {
-    const result = _substructLibrary.search(args[0]);
-    port.postMessage({ op: op, retval: result });
-  } else if (op === 'substructLibrary::deinit') {
-    _substructLibrary.deinit();
-    _substructLibrary = null;
-    port.postMessage({ op: op });
-  }
-  
+
+    const {op, args} = e.data;
+    let port = e.ports[0];
+
+    if (op === 'module::init') {
+        _rdKitModule = await initRDKitModule();
+        console.log("RDKit (worker) initialized");
+        _substructLibrary = new RdKitSubstructLibrary(_rdKitModule);
+        port.postMessage({op: op});
+    } else if (op === 'substructLibrary::init') {
+        _substructLibrary.init(args[0]);
+        port.postMessage({op: op});
+    } else if (op === 'substructLibrary::search') {
+        const result = _substructLibrary.search(args[0]);
+        port.postMessage({op: op, retval: result});
+    } else if (op === 'substructLibrary::deinit') {
+        _substructLibrary.deinit();
+        _substructLibrary = null;
+        port.postMessage({op: op});
+    }
+
 }
