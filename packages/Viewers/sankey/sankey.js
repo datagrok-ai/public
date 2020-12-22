a15011249278e5a5692f10fe52405f46a885059f
onuf <onuf.diana@gmail.com>
2020-12-22 16:00:54 +0200

    GROK-4697: Viewers: Sankey Diagram
    Repaired Sankey

--- packages/Viewers/sankey/sankey.js
+++ packages/Viewers/sankey/sankey.js
@@ -1,461 +1,124 @@
 import * as d3 from 'd3';
+import {sankey, sankeyLinkHorizontal} from 'd3-sankey';
 
 export class SankeyViewer extends DG.JsViewer {
+  constructor() {
+    super();
+
+    // Properties
+    this.sourceColumnName = this.string('sourceColumnName');
+    this.targetColumnName = this.string('targetColumnName');
+    this.valueColumnName = this.float('valueColumnName');
+  }
+
   init() {
-    this.root.className = 'sankey';
+    // Data
+    this.graph = {};
+    // Chart Settings
+    this.margin = {top: 10, right: 10, bottom: 10, left: 10};
+    this.color = d3.scaleOrdinal(d3.schemeCategory10); // TODO: use DG.Color.categoricalPalette
+    this.initialized = true;
+  }
 
-    var units = "Widgets";
+  onTableAttached() {
+    this.init();
 
-    // set the dimensions and margins of the graph
-    var margin = {top: 10, right: 10, bottom: 10, left: 10},
-      width = 700 - margin.left - margin.right,
-      height = 300 - margin.top - margin.bottom;
+    let columns = this.dataFrame.columns.toList();
+    this.strColumns = columns.filter(col => col.type === 'string');
+    this.numColumns = columns.filter(col => ['double', 'int'].includes(col.type));
 
-    // format variables
-    var formatNumber = d3.format(",.0f"),    // zero decimal places
-      format = function (d) {
-        return formatNumber(d) + " " + units;
-      },
-      color = d3.scaleOrdinal(d3.schemeCategory10);
+    this.sourceColumnName = this.strColumns[0].name;
+    this.targetColumnName = this.strColumns[1].name;
+    this.valueColumnName = this.numColumns[0].name;
+    this.prepareData();
 
-    // append the svg object to the body of the page
-    var svg = d3.select(this.root).append("svg")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
-      .append("g")
-      .attr("transform",
-        "translate(" + margin.left + "," + margin.top + ")");
+    this.subs.push(DG.debounce(this.dataFrame.selection.onChanged, 50).subscribe((_) => this.render()));
+    this.subs.push(DG.debounce(this.dataFrame.filter.onChanged, 50).subscribe((_) => this.render()));
+    this.subs.push(DG.debounce(this.onSizeChanged, 50).subscribe((_) => this.render()));
 
-    // Set the sankey diagram properties
-    var sankey = d3_sankey()
-      .nodeWidth(36)
-      .nodePadding(40)
-      .size([width, height]);
+    this.render();
+  }
 
-    var path = sankey.link();
+  prepareData() {
 
+    let sourceCol = this.dataFrame.getCol(this.sourceColumnName);
+    let targetCol = this.dataFrame.getCol(this.targetColumnName);
+    let valueCol = this.dataFrame.getCol(this.valueColumnName);
+    let sourceCats = sourceCol.categories;
+    let targetCats = targetCol.categories;
+    let nodes = Array.from(new Set(sourceCats.concat(targetCats)))
+      .map((node, index) => ({node: index, name: node}));
 
-    // transform data into the sankey format
-    var sourceCol = this.dataFrame.getCol(this.options.source);
-    var targetCol = this.dataFrame.getCol(this.options.target);
-    var valueCol = this.dataFrame.getCol(this.options.value);
-    let nodes = [...new Set(sourceCol.categories.concat(targetCol.categories))]
-      .map((x, i) => ({node: i, name: x}));
-
-    var links = [];
-    for (let i = 0; i < this.dataFrame.rowCount; i++)
+    let links = [];
+    let rowCount = this.dataFrame.rowCount;
+    let source = sourceCol.getRawData();
+    let target = targetCol.getRawData();
+    let value = valueCol.getRawData();
+    for (let i = 0; i < rowCount; i++) {
       links.push({
-        source: nodes.findIndex(node => node.name === sourceCol.get(i)),
-        target: nodes.findIndex(node => node.name === targetCol.get(i)),
-        value: valueCol.get(i)
+        source: nodes.findIndex(node => node.name === sourceCats[source[i]]),
+        target: nodes.findIndex(node => node.name === targetCats[target[i]]),
+        value: value[i]
       });
+    }
 
-    var graph = {
+    this.graph = {
       nodes: nodes,
       links: links
     };
 
-    sankey
-      .nodes(graph.nodes)
-      .links(graph.links)
-      .layout(32);
-
-    // add in the links
-    var link = svg.append("g").selectAll(".link")
-      .data(graph.links)
-      .enter().append("path")
-      .attr("class", "link")
-      .attr("d", path)
-      .style("stroke-width", function (d) {
-        return Math.max(1, d.dy);
-      })
-      .sort(function (a, b) {
-        return b.dy - a.dy;
-      });
-
-    // add the link titles
-    link.append("title")
-      .text(function (d) {
-        return d.source.name + " → " +
-          d.target.name + "\n" + format(d.value);
-      });
-
-    // add in the nodes
-    var node = svg.append("g").selectAll(".node")
-      .data(graph.nodes)
-      .enter().append("g")
-      .attr("class", "node")
-      .attr("transform", function (d) {
-        return "translate(" + d.x + "," + d.y + ")";
-      })
-      .call(d3.drag()
-        .subject(function (d) {
-          return d;
-        })
-        .on("start", function () {
-          this.parentNode.appendChild(this);
-        })
-        .on("drag", dragmove));
-
-    // add the rectangles for the nodes
-    node.append("rect")
-      .attr("height", function (d) {
-        return d.dy;
-      })
-      .attr("width", sankey.nodeWidth())
-      .style("fill", function (d) {
-        return d.color = color(d.name.replace(/ .*/, ""));
-      })
-      .style("stroke", function (d) {
-        return d3.rgb(d.color).darker(2);
-      })
-      .append("title")
-      .text(function (d) {
-        return d.name + "\n" + format(d.value);
-      });
-
-    // add in the title for the nodes
-    node.append("text")
-      .attr("x", -6)
-      .attr("y", function (d) {
-        return d.dy / 2;
-      })
-      .attr("dy", ".35em")
-      .attr("text-anchor", "end")
-      .attr("transform", null)
-      .text(function (d) {
-        return d.name;
-      })
-      .filter(function (d) {
-        return d.x < width / 2;
-      })
-      .attr("x", 6 + sankey.nodeWidth())
-      .attr("text-anchor", "start");
-
-    // the function for moving the nodes
-    function dragmove(d) {
-      d3.select(this)
-        .attr("transform",
-          "translate("
-          + d.x + ","
-          + (d.y = Math.max(
-            0, Math.min(height - d.dy, d3.event.y))
-          ) + ")");
-      sankey.relayout();
-      link.attr("d", path);
-    }
   }
 
-  onTableAttached() {
-    this.options = {source: 'source', target: 'target', value: 'value'};
-    this.init();
-
-    this.subs.push(this.dataFrame.selection.onChanged.subscribe((_) => this.render()));
-    this.subs.push(this.dataFrame.filter.onChanged.subscribe((_) => this.render()));
-    this.render();
+  detach() {
+    this.subs.forEach((sub) => sub.unsubscribe());
   }
 
   render() {
+    $(this.root).empty();
+    let width = this.root.parentElement.clientWidth - this.margin.left - this.margin.right;
+    let height = this.root.parentElement.clientHeight - this.margin.top - this.margin.bottom;
+
+    let {nodes, links} = sankey().extent([[0, 0], [width, height]])(this.graph);
+
+    let svg = d3.select(this.root).append("svg")
+        .attr("width", width + this.margin.left + this.margin.right)
+        .attr("height", height + this.margin.top + this.margin.bottom)
+      .append("g")
+        .attr("transform", `translate(${this.margin.left}, ${this.margin.top})`);
+
+    svg.append('g').attr("stroke", "#000")
+      .selectAll("rect")
+      .data(nodes)
+      .join("rect")
+        .attr("x", d => d.x0)
+        .attr("y", d => d.y0)
+        .attr("height", d => d.y1 - d.y0)
+        .attr("width", d => d.x1 - d.x0)
+        .attr("fill", d => this.color(d.name))
+      .on('mouseover', d => ui.tooltip.showRowGroup(this.dataFrame, i => true, d.x, d.y))
+      .on('mouseout', () => ui.tooltip.hide())
+      .on('mousedown', d => this.dataFrame.selection.handleClick(i => true, d));
+
+    svg.append("g")
+        .attr("fill", "none")
+        .attr("stroke", "#000")
+        .attr("stroke-opacity", 0.2)
+      .selectAll("path")
+      .data(links)
+      .join("path")
+        .attr("d", sankeyLinkHorizontal())
+        .attr("stroke-width", d => Math.max(1, d.width));
+
+    svg.append("g")
+        .attr("font-family", "sans-serif")
+        .attr("font-size", 10)
+      .selectAll("text")
+      .data(nodes)
+      .join("text")
+        .attr("x", d => d.x0 < width / 2 ? d.x1 + 6 : d.x0 - 6)
+        .attr("y", d => (d.y1 + d.y0) / 2)
+        .attr("dy", "0.35em")
+        .attr("text-anchor", d => d.x0 < width / 2 ? "start" : "end")
+        .text(d => d.name);
   }
 }
-
-const d3_sankey = function () {
-  var sankey = {},
-    nodeWidth = 24,
-    nodePadding = 8,
-    size = [1, 1],
-    nodes = [],
-    links = [];
-
-  sankey.nodeWidth = function (_) {
-    if (!arguments.length) return nodeWidth;
-    nodeWidth = +_;
-    return sankey;
-  };
-
-  sankey.nodePadding = function (_) {
-    if (!arguments.length) return nodePadding;
-    nodePadding = +_;
-    return sankey;
-  };
-
-  sankey.nodes = function (_) {
-    if (!arguments.length) return nodes;
-    nodes = _;
-    return sankey;
-  };
-
-  sankey.links = function (_) {
-    if (!arguments.length) return links;
-    links = _;
-    return sankey;
-  };
-
-  sankey.size = function (_) {
-    if (!arguments.length) return size;
-    size = _;
-    return sankey;
-  };
-
-  sankey.layout = function (iterations) {
-    computeNodeLinks();
-    computeNodeValues();
-    computeNodeBreadths();
-    computeNodeDepths(iterations);
-    computeLinkDepths();
-    return sankey;
-  };
-
-  sankey.relayout = function () {
-    computeLinkDepths();
-    return sankey;
-  };
-
-  sankey.link = function () {
-    var curvature = .5;
-
-    function link(d) {
-      var x0 = d.source.x + d.source.dx,
-        x1 = d.target.x,
-        xi = d3.interpolateNumber(x0, x1),
-        x2 = xi(curvature),
-        x3 = xi(1 - curvature),
-        y0 = d.source.y + d.sy + d.dy / 2,
-        y1 = d.target.y + d.ty + d.dy / 2;
-      return "M" + x0 + "," + y0
-        + "C" + x2 + "," + y0
-        + " " + x3 + "," + y1
-        + " " + x1 + "," + y1;
-    }
-
-    link.curvature = function (_) {
-      if (!arguments.length) return curvature;
-      curvature = +_;
-      return link;
-    };
-
-    return link;
-  };
-
-  // Populate the sourceLinks and targetLinks for each node.
-  // Also, if the source and target are not objects, assume they are indices.
-  function computeNodeLinks() {
-    nodes.forEach(function (node) {
-      node.sourceLinks = [];
-      node.targetLinks = [];
-    });
-    links.forEach(function (link) {
-      var source = link.source,
-        target = link.target;
-      if (typeof source === "number") source = link.source = nodes[link.source];
-      if (typeof target === "number") target = link.target = nodes[link.target];
-      source.sourceLinks.push(link);
-      target.targetLinks.push(link);
-    });
-  }
-
-  // Compute the value (size) of each node by summing the associated links.
-  function computeNodeValues() {
-    nodes.forEach(function (node) {
-      node.value = Math.max(
-        d3.sum(node.sourceLinks, value),
-        d3.sum(node.targetLinks, value)
-      );
-    });
-  }
-
-  // Iteratively assign the breadth (x-position) for each node.
-  // Nodes are assigned the maximum breadth of incoming neighbors plus one;
-  // nodes with no incoming links are assigned breadth zero, while
-  // nodes with no outgoing links are assigned the maximum breadth.
-  function computeNodeBreadths() {
-    var remainingNodes = nodes,
-      nextNodes,
-      x = 0;
-
-    while (remainingNodes.length) {
-      nextNodes = [];
-      remainingNodes.forEach(function (node) {
-        node.x = x;
-        node.dx = nodeWidth;
-        node.sourceLinks.forEach(function (link) {
-          if (nextNodes.indexOf(link.target) < 0) {
-            nextNodes.push(link.target);
-          }
-        });
-      });
-      remainingNodes = nextNodes;
-      ++x;
-    }
-
-    //
-    moveSinksRight(x);
-    scaleNodeBreadths((size[0] - nodeWidth) / (x - 1));
-  }
-
-  function moveSourcesRight() {
-    nodes.forEach(function (node) {
-      if (!node.targetLinks.length) {
-        node.x = d3.min(node.sourceLinks, function (d) {
-          return d.target.x;
-        }) - 1;
-      }
-    });
-  }
-
-  function moveSinksRight(x) {
-    nodes.forEach(function (node) {
-      if (!node.sourceLinks.length) {
-        node.x = x - 1;
-      }
-    });
-  }
-
-  function scaleNodeBreadths(kx) {
-    nodes.forEach(function (node) {
-      node.x *= kx;
-    });
-  }
-
-  function computeNodeDepths(iterations) {
-    var nodesByBreadth = d3.nest()
-      .key(function (d) {
-        return d.x;
-      })
-      .sortKeys(d3.ascending)
-      .entries(nodes)
-      .map(function (d) {
-        return d.values;
-      });
-
-    //
-    initializeNodeDepth();
-    resolveCollisions();
-    for (var alpha = 1; iterations > 0; --iterations) {
-      relaxRightToLeft(alpha *= .99);
-      resolveCollisions();
-      relaxLeftToRight(alpha);
-      resolveCollisions();
-    }
-
-    function initializeNodeDepth() {
-      var ky = d3.min(nodesByBreadth, function (nodes) {
-        return (size[1] - (nodes.length - 1) * nodePadding) / d3.sum(nodes, value);
-      });
-
-      nodesByBreadth.forEach(function (nodes) {
-        nodes.forEach(function (node, i) {
-          node.y = i;
-          node.dy = node.value * ky;
-        });
-      });
-
-      links.forEach(function (link) {
-        link.dy = link.value * ky;
-      });
-    }
-
-    function relaxLeftToRight(alpha) {
-      nodesByBreadth.forEach(function (nodes, breadth) {
-        nodes.forEach(function (node) {
-          if (node.targetLinks.length) {
-            var y = d3.sum(node.targetLinks, weightedSource) / d3.sum(node.targetLinks, value);
-            node.y += (y - center(node)) * alpha;
-          }
-        });
-      });
-
-      function weightedSource(link) {
-        return center(link.source) * link.value;
-      }
-    }
-
-    function relaxRightToLeft(alpha) {
-      nodesByBreadth.slice().reverse().forEach(function (nodes) {
-        nodes.forEach(function (node) {
-          if (node.sourceLinks.length) {
-            var y = d3.sum(node.sourceLinks, weightedTarget) / d3.sum(node.sourceLinks, value);
-            node.y += (y - center(node)) * alpha;
-          }
-        });
-      });
-
-      function weightedTarget(link) {
-        return center(link.target) * link.value;
-      }
-    }
-
-    function resolveCollisions() {
-      nodesByBreadth.forEach(function (nodes) {
-        var node,
-          dy,
-          y0 = 0,
-          n = nodes.length,
-          i;
-
-        // Push any overlapping nodes down.
-        nodes.sort(ascendingDepth);
-        for (i = 0; i < n; ++i) {
-          node = nodes[i];
-          dy = y0 - node.y;
-          if (dy > 0) node.y += dy;
-          y0 = node.y + node.dy + nodePadding;
-        }
-
-        // If the bottommost node goes outside the bounds, push it back up.
-        dy = y0 - nodePadding - size[1];
-        if (dy > 0) {
-          y0 = node.y -= dy;
-
-          // Push any overlapping nodes back up.
-          for (i = n - 2; i >= 0; --i) {
-            node = nodes[i];
-            dy = node.y + node.dy + nodePadding - y0;
-            if (dy > 0) node.y -= dy;
-            y0 = node.y;
-          }
-        }
-      });
-    }
-
-    function ascendingDepth(a, b) {
-      return a.y - b.y;
-    }
-  }
-
-  function computeLinkDepths() {
-    nodes.forEach(function (node) {
-      node.sourceLinks.sort(ascendingTargetDepth);
-      node.targetLinks.sort(ascendingSourceDepth);
-    });
-    nodes.forEach(function (node) {
-      var sy = 0, ty = 0;
-      node.sourceLinks.forEach(function (link) {
-        link.sy = sy;
-        sy += link.dy;
-      });
-      node.targetLinks.forEach(function (link) {
-        link.ty = ty;
-        ty += link.dy;
-      });
-    });
-
-    function ascendingSourceDepth(a, b) {
-      return a.source.y - b.source.y;
-    }
-
-    function ascendingTargetDepth(a, b) {
-      return a.target.y - b.target.y;
-    }
-  }
-
-  function center(node) {
-    return node.y + node.dy / 2;
-  }
-
-  function value(link) {
-    return link.value;
-  }
-
-  return sankey;
-};
