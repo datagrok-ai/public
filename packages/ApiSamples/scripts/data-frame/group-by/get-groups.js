ef956cfdd0ae4e523128ce88655bdfaae002b60b
Kostya <doncovkonstantin@gmail.com>
2020-12-23 14:17:51 +0200

    Added GroupByBuilder.getGroups() snippet

---
+++ packages/ApiSamples/scripts/data-frame/group-by/get-groups.js
@@ -0,0 +1,9 @@
+// Shows groups of DataFrames by race and sex
+
+let demog = grok.data.demo.demog();
+
+let agesByRaceGroups = demog
+    .groupBy(['race', 'sex'])
+    .getGroups();
+
+Object.keys(agesByRaceGroups).forEach((key, ind) => grok.shell.addTableView(agesByRaceGroups[key]));
