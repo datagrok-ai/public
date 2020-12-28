4616fae8704b91c0455614f47980c155f314282c
Oleksii Martynchuk <48830393+ZipZaap@users.noreply.github.com>
2020-12-28 14:42:36 +0200

    GROK-7832: basic methods for sample analysis

--- packages/VDJtools/scripts/vdj-tools.py
+++ packages/VDJtools/scripts/vdj-tools.py
@@ -2,16 +2,35 @@
 #description: vdjtools-1.2.1.jar wrapper
 #language: python
 #tags: demo, files, panel, cli, metadata-extractor, vdjtools
-#require: vdjtools-1.2.1.jar
-#input: file file
+#require: vdjtools.jar
+#input: dataframe clonotypeDf
+#input: string shcall
+#input: string resultsfile
 #output: dataframe metadata
 
+from subprocess import Popen, PIPE
 import pandas as pd
 import tempfile
-from subprocess import Popen, PIPE
+import shutil
+import os
 
-working_dir = tempfile.mkdtemp()
-params = ['java', '-Xmx20G', '-jar', os.path.join(utils_dir,'vdjtools-1.2.1.jar'),'CalcSpectratype',file, 'out/0']
-process = Popen(params, cwd=working_dir, stdout=PIPE, stderr=PIPE)
+# create temporary folders to store files
+mainFolder = tempfile.mkdtemp()
+samplesFolder = tempfile.mkdtemp(suffix='_samples',dir=mainFolder)
+
+# deposit file into the temporary directory
+saveTo = os.path.join(samplesFolder, 'clonotypes.txt')
+clonotypeDf.to_csv(saveTo, index=None, sep='\t', mode='a')
+
+# create and execute sh call
+shcall = shcall.replace('pathToJar', os.path.join(utils_dir, 'vdjtools.jar'))
+shcall = shcall.replace('samplePath', saveTo)
+shcall = shcall.split()
+process = Popen(shcall, cwd=mainFolder, stdout=PIPE, stderr=PIPE)
 process.communicate()
-metadata = pd.read_csv(os.path.join(working_dir, 'out/0.spectratype.nt.wt'),sep = '\t')
+
+# retrive results from temporary storage
+metadata = pd.read_table(os.path.join(mainFolder, resultsfile))
+
+# delete temporary folder and all subfolders
+shutil.rmtree(mainFolder, ignore_errors=True)
--- packages/VDJtools/src/package.js
+++ packages/VDJtools/src/package.js
@@ -5,8 +5,107 @@ import * as DG from "datagrok-api/dg";
 
 export let _package = new DG.Package();
 
-//name: test
-//input: string s
-export function test(s) {
-    grok.shell.info(_package.webRoot);
+function paramSelector(method) {
+    let methodparams;
+    if (method === 'CalcBasicStats') {
+        let p1 = ui.choiceInput('Weighting method', '',['by clonotype frequency','unweighted']);
+        methodparams = [p1];
+    }
+
+    if (method === 'CalcSegmentUsage') {
+        let p1 = ui.choiceInput('Weighting method', '',['by clonotype frequency','unweighted']);
+        methodparams = [p1];
+    }
+
+    if (method === 'CalcSpectratype') {
+        let p1 = ui.choiceInput('Weighting method', '',['by clonotype frequency','unweighted']);
+        let p2 = ui.choiceInput('CDR3 sequence', '',['amino acid', 'nucleotide']);
+        methodparams = [p1, p2];
+
+    }
+    return methodparams;
+}
+
+function shCallCompose(method, methodparams) {
+    let defaultCall = 'java -Xmx10G -jar pathToJar routine';
+    defaultCall = defaultCall.replace('routine', method);
+    let resultsFile = 'out/0.'
+
+    if (method === 'CalcBasicStats') {
+        resultsFile = resultsFile + 'basicstats.txt'
+        if (methodparams[0].value === 'unweighted') {
+            defaultCall = defaultCall + ' -u';
+        }
+    }
+
+    if (method === 'CalcSegmentUsage') {
+        resultsFile = resultsFile + 'segments.wt.V.txt'
+        if (methodparams[0].value === 'unweighted') {
+            defaultCall = defaultCall + ' -u';
+            resultsFile = resultsFile.replace('wt','unwt')
+        }
+    }
+
+    if (method === 'CalcSpectratype') {
+        resultsFile = resultsFile + 'spectratype.nt.wt.txt'
+        if (methodparams[0].value === 'unweighted') {
+            defaultCall = defaultCall + ' -u';
+            resultsFile = resultsFile.replace('wt','unwt')
+        }
+        if (methodparams[1].value === 'amino acid') {
+            defaultCall = defaultCall + ' -a';
+            resultsFile = resultsFile.replace('nt','aa')
+        }
+    }
+    defaultCall = defaultCall + ' samplePath out/0'
+    return [defaultCall, resultsFile];
+}
+
+async function analyze(data, shCall) {
+    let metadata = await grok.functions.call('VDJtools:vdjwrapper',
+        {
+            'clonotypeDf': data,
+            'shcall':shCall[0],
+            'resultsfile':shCall[1]
+        })
+    return metadata;
+}
+
+//name: vdjtools
+export async function vdjtools() {
+
+    //Create dialogue window
+    let v = ui.dialog('VDJ Tools');
+
+    let tableName = ui.choiceInput('Table', null, grok.shell.tableNames);
+    let dataTable = grok.shell.tableByName(tableName.value);
+    let analysisRoutine = ui.choiceInput('Routine','',['CalcBasicStats', 'CalcSegmentUsage', 'CalcSpectratype']);
+    let methodparams;
+
+    let container0 = ui.div();
+    let inputs0 = ui.inputs([tableName, analysisRoutine]);
+    container0.appendChild(inputs0);
+
+    let container_acc = ui.div();
+    let acc = ui.accordion();
+
+    analysisRoutine.onChanged(function () {
+        $(container_acc).empty()
+        methodparams = paramSelector(analysisRoutine.value);
+        container_acc.appendChild(ui.inputs(methodparams));
+    });
+    acc.addPane('parameters',() => container_acc);
+
+
+    v.add(container0);
+    v.add(acc).onOK(async ()=>{
+
+        let shCall = shCallCompose(analysisRoutine.value, methodparams)
+        grok.shell.info('call '+ shCall[0]);
+        grok.shell.info('output ' + shCall[1]);
+        let metadata = await analyze(dataTable, shCall);
+        grok.shell.addTableView(metadata);
+
+    }).show();
+
 }
