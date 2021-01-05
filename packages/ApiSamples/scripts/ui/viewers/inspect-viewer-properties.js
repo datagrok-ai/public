aa30260c5a010d3674da497278eba83c1cda63fb
Andrew Skalkin <skalkin@gmail.com>
2021-01-04 20:31:55 -0500

    Improved a sample code snippet.

--- packages/ApiSamples/scripts/ui/viewers/inspect-viewer-properties.js
+++ packages/ApiSamples/scripts/ui/viewers/inspect-viewer-properties.js
@@ -1,8 +1,12 @@
 let view = grok.shell.addTableView(grok.data.demo.demog());
 let sp = view.scatterPlot();
 
-var properties = sp.props.getProperties()
+// Use viewer.props.<property> to get or set a property
+sp.props.xColumnName = 'race';
+grok.shell.info(sp.props.xColumnName);
+
+var descriptions = sp.props.getProperties()
   .map((p) => p.propertyType + ' ' + p.name + ': ' + p.description + ' ' + p.columnFilter)
   .join('<br>')
 
-grok.shell.info(properties);
+grok.shell.info(descriptions);
