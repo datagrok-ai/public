c88be59da4ae8b10b79b218c6a5a4813729fefb4
Andrew Skalkin <skalkin@gmail.com>
2021-01-08 00:14:08 -0500

    GROK-7858: Charts: Tree Viewer

---
+++ packages/ApiSamples/scripts/data-frame/advanced/to-hierarchy.js
@@ -0,0 +1,27 @@
+function toHierarchy(dataFrame, splitByColumnNames) {
+  let data = {
+    name: 'All',
+    children: []
+  };
+
+  let aggregated = dataFrame.groupBy(splitByColumnNames).aggregate();
+  let columns = aggregated.columns.byNames(splitByColumnNames);
+  let parentNodes = columns.map(c => null);
+
+  for (let i = 0; i < aggregated.rowCount; i++) {
+    let idx = i === 0 ? 0 : columns.findIndex((col) => col.get(i) !== col.get(j));
+
+    for (let colIdx = idx; colIdx < columns.length; colIdx++) {
+      let parentNode = colIdx === 0 ? data : parentNodes[colIdx - 1];
+      let node = { name: columns[colIdx].toStr(i) };
+      parentNodes[colIdx] = node;
+      if (!parentNode.children)
+        parentNode.children = [];
+      parentNode.children.push(node);
+    }
+  }
+
+  return data;
+};
+
+grok.shell.info(toHierarchy(grok.data.demo.demog(), ['sex', 'race']));
