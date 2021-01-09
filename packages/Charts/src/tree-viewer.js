5f59cd338d3ac898c1d9a980a84a82301a0d5f85
Andrew Skalkin <skalkin@gmail.com>
2021-01-09 11:59:30 -0500

    GROK-7865: Charts: Tree Map

--- packages/Charts/src/tree-viewer.js
+++ packages/Charts/src/tree-viewer.js
@@ -7,14 +7,14 @@ export class TreeViewer extends EChartViewer {
   constructor() {
     super();
     
+    this.initCommonProperties();
     this.layout = this.string('layout', 'orthogonal', { choices: ['orthogonal', 'radial'] });
     this.orient = this.string('orient', 'LR', { choices: ['LR', 'RL', 'TB', 'BT'] });
     this.expandAndCollapse = this.bool('expandAndCollapse', true);
     this.animationDuration = this.int('animationDuration', 750);
     this.edgeShape = this.string('curve', '', { choices: ['curve', 'polyline'] });
     this.symbol = this.string('symbol', 'emptyCircle', { choices: ['circle', 'rect', 'roundRect', 'triangle', 'diamond', 'pin', 'arrow', 'none'] });
-
-    this.top = this.string('top', '10%');
+    this.symbolSize = this.int('symbolSize', 7);
 
     this.option = {
       tooltip: {
@@ -24,13 +24,6 @@ export class TreeViewer extends EChartViewer {
       series: [
         {
           type: 'tree',
-
-          top: '1%',
-          left: '7%',
-          bottom: '1%',
-          right: '20%',
-
-          symbolSize: 7,
 
           label: {
             position: 'left',
@@ -46,32 +39,14 @@ export class TreeViewer extends EChartViewer {
               align: 'left'
             }
           },
-
-          expandAndCollapse: true,
-          animationDuration: 550,
-          animationDurationUpdate: 750
         }
       ]
     };
+
+    this.onPropertyChanged(null);
   }
 
-  onTableAttached() {
-    this.subs.push(this.dataFrame.selection.onChanged.subscribe((_) => this.render()));
-    this.subs.push(this.dataFrame.filter.onChanged.subscribe((_) => this.render()));
-
-    this.render();
-  }
-
-  onPropertyChanged(p) {
-    if (p !== null) {
-      this.option.series[0][p.name] = p.get(this);
-      this.myChart.setOption(this.option);
-    }
-  }
-
-  render() {
-    let data = Utils.toHierarchy(this.dataFrame, ['sex', 'race', 'dis_pop'], this.dataFrame.filter);
-    this.option.series[0].data = [ data ];
-    this.myChart.setOption(this.option);
+  getSeriesData() {
+    return [ Utils.toTree(this.dataFrame, ['sex', 'race', 'dis_pop'], this.dataFrame.filter) ];
   }
 }
