d1511b289a2d699496fca189839d5008e69eb7c2
atuzhykov <atuzhykov@datagrok.ai>
2021-02-01 16:22:11 +0200

    Added new package ChemblBrowser

---
+++ packages/ChemblBrowser/src/package.js
@@ -0,0 +1,23 @@
+/* Do not change these import lines. Datagrok will import API library in exactly the same manner */
+import * as grok from 'datagrok-api/grok';
+import * as ui from 'datagrok-api/ui';
+import * as DG from 'datagrok-api/dg';
+
+export let _package = new DG.Package();
+let packageName = 'Chembl'
+
+//name: test
+//input: string s
+export function test(s) {
+  grok.shell.info(_package.webRoot);
+}
+
+
+
+//name: getAllChemblStructures
+//output: dataframe df
+export async function getAllChemblStructures() {
+  let queryName = 'allChemblStructures'
+  return await grok.data.query(`${packageName}:${queryName}`);
+}
+
