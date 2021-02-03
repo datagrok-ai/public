0fa38ef0b1afa3465ee79075dec8a26eaf552770
Andrew Skalkin <skalkin@gmail.com>
2021-02-03 14:01:38 -0500

    GROK-7964: JS API: Column.init(value | indexToValueFunction)

--- js-api/src/dataframe.js
+++ js-api/src/dataframe.js
@@ -624,6 +624,20 @@ export class Column {
     grok_Column_Set_Name(this.d, s);
   }
 
+  /**
+   * Initializes all values in the column to [columnInitializer].
+   * @param {string | number | Function} valueInitializer
+   * @returns {Column}
+   * */
+  init(valueInitializer) {
+    let type = typeof valueInitializer;
+    if (type === 'function')
+      grok_Column_Init(this.d, valueInitializer);
+    else if (type === 'number' || type === 'string')
+      grok_Column_SetAllValues(this.d, valueInitializer);
+    return this;
+  }
+
   /** Returns the raw buffer containing data. Return type depends on the column type:
    * {Int32Array} for ints, {@link INT_NULL} represents null.
    * {Float32Array} for floats, {@link FLOAT_NULL} represents null.
@@ -892,6 +906,36 @@ export class ColumnList {
   addNew(name, type) {
     return toJs(grok_ColumnList_AddNew(this.d, name, type));
   }
+
+  /** Adds a string column
+   * @param {string} name
+   * @returns {Column} */
+  addNewString(name) { return this.addNew(name, TYPE.STRING); }
+
+  /** Adds a new integer column
+   * @param {string} name
+   * @returns {Column} */
+  addNewInt(name) { return this.addNew(name, TYPE.INT); }
+
+  /** Adds a new float column
+   * @param {string} name
+   * @returns {Column} */
+  addNewFloat(name) { return this.addNew(name, TYPE.FLOAT); }
+
+  /** Adds a new qualified number column
+   * @param {string} name
+   * @returns {Column} */
+  addNewQnum(name) { return this.addNew(name, TYPE.QNUM); }
+
+  /** Adds a new datetime column
+   * @param {string} name
+   * @returns {Column} */
+  addNewDateTime(name) { return this.addNew(name, TYPE.DATE_TIME); }
+
+  /** Adds a new boolean column
+   * @param {string} name
+   * @returns {Column} */
+  addNewBool(name) { return this.addNew(name, TYPE.BOOL); }
 
   /** Adds a virtual column.
    * @param {string} name
---
+++ packages/ApiSamples/scripts/data-frame/modification/init-column.js
@@ -0,0 +1,15 @@
+// Different ways to initialize the content of the column
+
+let t = DG.DataFrame.fromCsv(
+  `make, model,    cylinders, volume, price
+Honda, Civic,    4,         1.4,    15000
+Honda, Accord,   6,         1.8,    20000
+Tesla, Roadster, ,          1.6,    100000
+Tesla, Model S,  ,          1.6,    120000`);
+
+// initialize to scalar
+t.col('make').init('VAZ');
+t.col('cylinders').init(4);
+
+// index-based initializer
+t.columns.addNewInt('index').init((i) => i * 2);
