ef7c781bb7a6936f4fe23b398339b16eae1887eb
onuf <onuf.diana@gmail.com>
2021-02-04 18:52:55 +0200

    GROK-7855: Charts Package (Timelines Viewer)
    Axis zoom

--- packages/Charts/src/timelinesviewer.js
+++ packages/Charts/src/timelinesviewer.js
@@ -9,7 +9,7 @@ export class TimelinesViewer extends EChartViewer {
     this.subjectColumnName = this.string('subjectColumnName', 'USUBJID');
     this.startColumnName = this.string('startColumnName', 'AESTDY');
     this.endColumnName = this.string('endColumnName', 'AEENDY');
-    this.colorByColumnName = this.string('colorByColumnName', 'SEX');
+    this.colorByColumnName = this.string('colorByColumnName', 'EVENT');
 
     this.data = [];
 
@@ -21,26 +21,32 @@ export class TimelinesViewer extends EChartViewer {
       grid: {
         left: '3%',
         right: '4%',
+        top: '4%',
         bottom: '3%',
         containLabel: true
       },
       xAxis: {
-        type: 'time',
-        boundaryGap: ['5%', '5%'],
-        axisLabel: { formatter: '{yyyy}-{MM}-{dd}' }
+        type: 'value',
+        min: value => value.min - 1,
+        max: value => value.max + 1
       },
       yAxis: {
         type: 'category',
         axisTick: { show: false },
         axisLine: { show: false },
       },
+      dataZoom: [
+        { type: 'inside', xAxisIndex: [1, 2]},
+        { type: 'inside', yAxisIndex: 0}
+      ],
       series: [
         {
           type: 'custom',
+          progressive: 0,   // Disable progressive rendering
           encode: {
             x: [1, 2], 
             y: 0,
-            tooltip: [3, 4]
+            tooltip: 3
           }
         }
       ]
@@ -50,7 +56,12 @@ export class TimelinesViewer extends EChartViewer {
   }
 
   onTableAttached() {
-    this.colorMap = this.dataFrame.getCol(this.colorByColumnName).categories.reduce((colorMap, c, i) => {
+    this.columns = this.dataFrame.columns.byNames([
+      this.subjectColumnName, this.startColumnName,
+      this.endColumnName, this.colorByColumnName
+    ]).map((col, ind) => (col === null) ? this.dataFrame.columns.byIndex(ind) : col);
+
+    this.colorMap = this.columns[3].categories.reduce((colorMap, c, i) => {
       colorMap[c] = DG.Color.toRgb(DG.Color.getCategoricalColor(i));
       return colorMap;
     }, {});
@@ -86,25 +97,26 @@ export class TimelinesViewer extends EChartViewer {
 
   getSeriesData() {
     this.data.length = 0;
-
-    let columns = this.dataFrame.columns.byNames([
-      this.subjectColumnName, this.startColumnName,
-      this.endColumnName, this.colorByColumnName
-    ]);
+    let max = this.columns[2].max;
+    let min = this.columns[1].min;
 
     for (let i = 0; i < this.dataFrame.rowCount; i++) {
       let row = [];
-      for (let j = 0; j < columns.length; j++) {
-        row.push((columns[j].type === 'datetime') ?
-          new Date(`${columns[j].get(i)}`) : columns[j].get(i));
+      for (let j = 0; j < this.columns.length; j++) {
+        row.push((this.columns[j].type === 'datetime') ?
+          new Date(`${this.columns[j].get(i)}`) : (this.columns[j].isNone(i)) ?
+          (j === 1) ? min : max : this.columns[j].get(i));
       }
       this.data.push(row);
     }
+
+    this.chart.on('click', params => this.dataFrame.selection.handleClick(
+      i => this.columns[0].get(i) === params.data[0], params.event));
     return this.data;
   }
   
   render() {  
-    this.option.series[0].data = this.getSeriesData(); 
+    this.option.series[0].data = this.getSeriesData();
     this.chart.setOption(this.option);
   }
 }
