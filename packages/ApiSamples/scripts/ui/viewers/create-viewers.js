a8d1c968c07295a3b37d4aa82466543ec03607a5
Andrew Skalkin <skalkin@gmail.com>
2021-02-11 08:10:24 -0500

    Code snippet for using viewers outside of the TableView

---
+++ packages/ApiSamples/scripts/ui/viewers/create-viewers.js
@@ -0,0 +1,9 @@
+// Create viewers and use them anywhere via static methods of the DG.Viewer class
+// See also: adding viewers to the TableView
+
+let t = grok.data.demo.demog();
+grok.shell.newView('foo').append(ui.divH([
+  DG.Viewer.scatterPlot(t, { x: 'height', y: 'weight'}),
+  DG.Viewer.histogram(t, { value: 'height' }),
+  DG.Viewer.barChart(t),
+]));
