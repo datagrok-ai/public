ac8a710bc85a6233a973371e77299f3ea0a24531
Andrii Tuzhykov <andrewtuzhykov@gmail.com>
2021-02-15 22:38:56 +0200

    GROK-7836 JS API: final samples version
    Signed-off-by: Andrii Tuzhykov <andrewtuzhykov@gmail.com>

--- packages/ApiSamples/scripts/dapi/files.js
+++ packages/ApiSamples/scripts/dapi/files.js
@@ -4,20 +4,20 @@
 
 // write files
 grok.dapi.files.write('Demo:TestJobs:Files:DemoFiles/testFile.dat', [0,1,2]);
-grok.dapi.files.writeAsText('Demo:TestJobs:Files:DemoFiles/testFile.dat/testFile.txt', 'testString');
+grok.dapi.files.writeAsText('Demo:TestJobs:Files:DemoFiles/testFile.txt', 'testString');
+
 
 //read files
-grok.dapi.files.readAsBytes('Demo:TestJobs:Files:DemoFiles/testFile.dat');
-grok.dapi.files.readAsText('Demo:TestJobs:Files:DemoFiles/testFile.txt');
+grok.dapi.files.readAsBytes('Demo:TestJobs:Files:DemoFiles/testFile.dat').then(res => console.log(res));
+grok.dapi.files.readAsText('Demo:TestJobs:Files:DemoFiles/testFile.txt').then(res => console.log(res));
 
-grok.dapi.files.exists('Demo:TestJobs:Files:DemoFiles/testFile.dat')
-    .then(isFile => console.log(isFile));
+grok.dapi.files.exists('Demo:TestJobs:Files:DemoFiles/testFile.dat').then(isFile => console.log(isFile));
 
 let recursive = true;
 let searchPattern = "";
-grok.dapi.files.list('Demo:TestJobs:Files:DemoFiles/geo', recursive, searchPattern);
+grok.dapi.files.list('Demo:TestJobs:Files:DemoFiles/geo', recursive, searchPattern).then(res => console.log(res));;
+
 grok.dapi.files.move(['Demo:TestJobs:Files:DemoFiles/testFile.txt'],'/geo');
 
-grok.dapi.files.delete('Demo:TestJobs:Files:DemoFiles/testFile.dat/testFile.txt');
-
-
+grok.dapi.files.delete('Demo:TestJobs:Files:DemoFiles/geo/testFile.txt');
+grok.dapi.files.delete('Demo:TestJobs:Files:DemoFiles/testFile.dat');
