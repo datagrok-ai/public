f026252f893dfb752a2ff56be73ba940385ccd32
Andrii Tuzhykov <andrewtuzhykov@gmail.com>
2021-02-18 16:44:58 +0200

    GROK-7949 Packages: ChemblBrowser App queries and UI realized
    Signed-off-by: Andrii Tuzhykov <andrewtuzhykov@gmail.com>

--- packages/ChemblBrowser/queries/queries.sql
+++ packages/ChemblBrowser/queries/queries.sql
@@ -4,12 +4,12 @@ select
   canonical_smiles, molregno
 from
   compound_structures
-limit 40
+limit 1000
 --end
 
 --name: proteinClassification
 --connection: chembl:chembl
-select protein_class_id, parent_id, pref_name, definition, class_level from protein_classification
+select pref_name, definition, class_level from protein_classification
 --end
 
 
@@ -21,7 +21,66 @@ select
 from
   molecule_dictionary
   left join compound_structures on molecule_dictionary.molregno = compound_structures.molregno
+limit 1000
 --end
 
 
 
+--name: compoundsSelectiveToOneTargetOverSecond
+--connection: chembl:chembl
+--input: string selectiveFor = CHEMBL301
+--input: string over = CHEMBL4036
+SELECT md.chembl_id,
+cs.canonical_smiles
+FROM target_dictionary td
+  JOIN assays a ON td.tid = a.tid
+  JOIN activities act ON a.assay_id = act.assay_id
+  JOIN molecule_dictionary md ON md.molregno = act.molregno
+  JOIN compound_structures cs ON md.molregno = cs.molregno
+    AND act.standard_relation = '='
+    AND act.standard_type     IN ('IC50')
+    AND act.standard_units    = 'nM'
+    AND act.standard_value    < 50
+    AND td.chembl_id          = @selectiveFor
+INTERSECT
+SELECT md.chembl_id,
+cs.canonical_smiles
+FROM target_dictionary td
+  JOIN assays a ON td.tid = a.tid
+  JOIN activities act ON a.assay_id = act.assay_id
+  JOIN molecule_dictionary md ON md.molregno = act.molregno
+  JOIN compound_structures cs ON md.molregno = cs.molregno
+AND act.standard_relation     = '='
+AND act.standard_type         IN ('IC50')
+AND act.standard_units        = 'nM'
+AND act.standard_value        > 200
+AND td.chembl_id              = @over;
+--end
+
+--name: compoundActivityDetailsForTarget
+--connection: chembl:chembl
+--input: string target = CHEMBL1827
+SELECT m.chembl_id AS compound_chembl_id,
+s.canonical_smiles,
+r.compound_key,
+coalesce(d.pubmed_id::text, d.doi) AS pubmed_id_or_doi,
+a.description                   AS assay_description,   act.standard_type,
+act.standard_relation,
+act.standard_value,
+act.standard_units,
+act.activity_comment
+FROM compound_structures s,
+molecule_dictionary m,
+compound_records r,
+docs d,
+activities act,
+assays a,
+target_dictionary t
+WHERE s.molregno     = m.molregno
+AND m.molregno       = r.molregno
+AND r.record_id      = act.record_id
+AND r.doc_id         = d.doc_id
+AND act.assay_id     = a.assay_id
+AND a.tid            = t.tid
+AND t.chembl_id      = @target;
+--end
--- packages/ChemblBrowser/src/package.js
+++ packages/ChemblBrowser/src/package.js
@@ -16,16 +16,74 @@ export function test(s) {
 export async function Browser() {
 
   let v = grok.shell.newView('demo: ChemblBrowser');
-  let query = ui.choiceInput('query', 'allChemblStructures', ['allChemblStructures', 'allChemblIdsWithInchiKeys','proteinClassification']);
-  let container = ui.div();
-  v.append(container);
+    let proteinClassification = ui.divV([
+        ui.divH([
+            ui.divText('Protein Classification'),
 
-  let inputs = [query]
-  container.appendChild(ui.inputs(inputs));
+            ui.bigButton('RUN', () => {
+                grok.data.query(`${packageName}:proteinClassification`, {}).then(t => grok.shell.addTableView(t));
+            }),
 
-  container.appendChild(ui.bigButton('RUN', () => {
-    grok.data.query(`${packageName}:${query.caption}`, {}).then(t => grok.shell.addTableView(t));
-  }));
+            ui.bigButton('RUN in cards', async () => {
+             let data = await grok.data.query(`${packageName}:proteinClassification`, {});
+                grok.shell.newView( "Protein Classification", [ ui.virtualView(data.rowCount, (i) => ui.card(ui.tableFromMap({
+                pref_name: data.row(i).pref_name,
+                definition: data.row(i).definition,
+                class_level: data.row(i).class_level,
+            }))).root]
+                )})])]);
+
+
+
+    let allChemblStructures = ui.divV([
+        ui.divH([
+            ui.divText('Chembl Structures'),
+            ui.bigButton('RUN', () => {
+                grok.data.query(`${packageName}:allChemblStructures`, {}).then(t => grok.shell.addTableView(t));
+            }),
+
+            ui.bigButton('RUN in cards', async () => {
+                let data = await grok.data.query(`${packageName}:allChemblStructures`, {});
+                grok.shell.newView( "Chembl Structures", [ ui.virtualView(data.rowCount, (i) => ui.card(ui.tableFromMap({
+                        canonical_smiles: data.row(i).canonical_smiles,
+                        molregno: data.row(i).molregno,
+                    }))).root]
+                )}),
+        ])]);
+
+    let target = ui.stringInput('Target', 'CHEMBL1827');
+
+    let compoundActivityDetailsForTarget = ui.divV([
+     ui.divH([
+         ui.divText('Compound activity details for @target'),
+         target,
+         ui.bigButton('RUN', () => {
+             grok.data.query(`${packageName}:compoundActivityDetailsForTarget`, {'target':target.stringValue}).then(t => grok.shell.addTableView(t));
+         })
+     ])]);
+
+
+    let selectiveForTarget = ui.stringInput(' Selective For Target', 'CHEMBL301');
+    let OverTarget = ui.stringInput('Over Target', 'CHEMBL4036');
+
+    let compoundsSelectiveToOneTargetOverSecond = ui.divV([
+        ui.divH([
+            ui.divText('Compounds which are selective to one target over a second target'),
+            selectiveForTarget, OverTarget,
+            ui.bigButton('RUN', () => {
+                grok.data.query(`${packageName}:compoundsSelectiveToOneTargetOverSecond`, {'selectiveFor':selectiveForTarget.stringValue, 'over': OverTarget.stringValue}).then(t => grok.shell.addTableView(t));
+            })
+        ])]);
+
+
+
+
+
+    v.append(ui.divV([
+        proteinClassification,  allChemblStructures, compoundActivityDetailsForTarget, compoundsSelectiveToOneTargetOverSecond]));
 
 
 }
+
+
+
