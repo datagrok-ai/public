a9e1a2da52f25b0c2fde6799e8ca35135db7adf2
dskatov <dskatov@datagrok.ai>
2021-02-22 12:31:06 +0300

    GROK-8090 Chem: Column features, WIP

--- packages/Chem/src/chem_column_property_panel.js
+++ packages/Chem/src/chem_column_property_panel.js
@@ -1,3 +1,15 @@
-function getMolColumnPropertyPanel(column) {
-  return ui.div([ui.h1('Mol Column Settings')]);
+function getMolColumnPropertyPanel(col) {
+
+  const names = col.dataFrame.columns.names();
+  return ui.div([
+    ui.inputs([
+      ui.choiceInput('Scaffold column', 'None', ['None'].concat(names),
+        v => {}),
+      ui.boolInput('Highlight scaffolds', col?.tags && col.tags['highlight-scaffold'] === 'true',
+        v => {}),
+      ui.boolInput('Regenerate coords', col?.tags && col.tags['regenerate-coords'] === 'true',
+        v => {})
+    ])
+  ]);
+
 }
