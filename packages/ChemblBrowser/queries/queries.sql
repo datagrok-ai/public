a9144cb0ae6d6967410dacc5bb0c045b1ab81e68
Andrii Tuzhykov <andrewtuzhykov@gmail.com>
2021-03-03 17:54:45 +0200

    GROK-7949 Improved ChemblBrowser with TileViewer
    Signed-off-by: Andrii Tuzhykov <andrewtuzhykov@gmail.com>

--- packages/ChemblBrowser/queries/queries.sql
+++ packages/ChemblBrowser/queries/queries.sql
@@ -1,94 +1,43 @@
 --name: allChemblStructures
 --connection: chembl:chembl
 select
-  canonical_smiles, molregno, standard_inchi
+  canonical_smiles, molregno
 from
   compound_structures
 limit 100
 --end
 
---name: proteinClassification
---connection: chembl:chembl
-select pref_name, definition, class_level from protein_classification
---end
 
-
---name: allChemblIdsWithInchiKeys
+--name: FindBySubstructure
 --connection: chembl:chembl
+--input: string sub
 select
-  molecule_dictionary.chembl_id,
-  compound_structures.standard_inchi_key
+  canonical_smiles, molregno
 from
-  molecule_dictionary
-  left join compound_structures on molecule_dictionary.molregno = compound_structures.molregno
-limit 1000
+  compound_structures
+where
+  canonical_smiles ilike '%' || @sub || '%'
 --end
 
---name: CompoundProperties
+--name: FindByName
+--connection: chembl:chembl
+--input: string sub
+SELECT *
+FROM compound_records
+WHERE compound_name ilike '%' || @sub || '%'
+LEFT JOIN compound_structures
+ON compound_records.molregno = compound_structures.molregno
+--end
+
+
+--name: FindByMolregno
 --connection: chembl:chembl
 --input: int molregno
-select * from
-  compound_properties
+select
+  canonical_smiles, molregno
+from
+  compound_structures
 where molregno = @molregno
 --end
 
 
-
---name: compoundsSelectiveToOneTargetOverSecond
---connection: chembl:chembl
---input: string selectiveFor = CHEMBL301
---input: string over = CHEMBL4036
-SELECT md.chembl_id,
-cs.canonical_smiles
-FROM target_dictionary td
-  JOIN assays a ON td.tid = a.tid
-  JOIN activities act ON a.assay_id = act.assay_id
-  JOIN molecule_dictionary md ON md.molregno = act.molregno
-  JOIN compound_structures cs ON md.molregno = cs.molregno
-    AND act.standard_relation = '='
-    AND act.standard_type     IN ('IC50')
-    AND act.standard_units    = 'nM'
-    AND act.standard_value    < 50
-    AND td.chembl_id          = @selectiveFor
-INTERSECT
-SELECT md.chembl_id,
-cs.canonical_smiles
-FROM target_dictionary td
-  JOIN assays a ON td.tid = a.tid
-  JOIN activities act ON a.assay_id = act.assay_id
-  JOIN molecule_dictionary md ON md.molregno = act.molregno
-  JOIN compound_structures cs ON md.molregno = cs.molregno
-AND act.standard_relation     = '='
-AND act.standard_type         IN ('IC50')
-AND act.standard_units        = 'nM'
-AND act.standard_value        > 200
-AND td.chembl_id              = @over;
---end
-
---name: compoundActivityDetailsForTarget
---connection: chembl:chembl
---input: string target = CHEMBL1827
-SELECT m.chembl_id AS compound_chembl_id,
-s.canonical_smiles,
-r.compound_key,
-coalesce(d.pubmed_id::text, d.doi) AS pubmed_id_or_doi,
-a.description                   AS assay_description,   act.standard_type,
-act.standard_relation,
-act.standard_value,
-act.standard_units,
-act.activity_comment
-FROM compound_structures s,
-molecule_dictionary m,
-compound_records r,
-docs d,
-activities act,
-assays a,
-target_dictionary t
-WHERE s.molregno     = m.molregno
-AND m.molregno       = r.molregno
-AND r.record_id      = act.record_id
-AND r.doc_id         = d.doc_id
-AND act.assay_id     = a.assay_id
-AND a.tid            = t.tid
-AND t.chembl_id      = @target;
---end
