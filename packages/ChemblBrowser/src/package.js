a270f3ed9608c4300d3cdbccc71800c268635089
Andrii Tuzhykov <andrewtuzhykov@gmail.com>
2021-03-03 17:54:45 +0200

    GROK-7949 Improved ChemblBrowser with TileViewer
    Signed-off-by: Andrii Tuzhykov <andrewtuzhykov@gmail.com>

--- packages/ChemblBrowser/src/package.js
+++ packages/ChemblBrowser/src/package.js
@@ -15,30 +15,53 @@ export function test(s) {
 //tags: app
 export async function Browser() {
 
+    async function FindBySubstructure() {
+        let sub = molecule.stringValue;
+        let query = await grok.data.query(`${packageName}:FindBySubstructure`, {'sub': sub});
+        grok.shell.addTableView(query);
 
-        async function molregnoView(molregno) {
-            let data = grok.data.query(`${packageName}:CompoundProperties`, {'molregno': parseInt(molregno)})
-            let view = grok.shell.addTableView(data);
-        }
-
-        async function initView()  {
-
-        let data = await grok.data.query(`${packageName}:allChemblStructures`, {});
-        let container = ui.divH([],'card-container');
-        for (let i=0; i<data.rowCount; i++){
-            let h2 = ui.h2(`molregno: ${data.col('molregno').get(i)}`);
-            let h3 = ui.h3(`standard_inchi: ${data.col('standard_inchi').get(i)}`);
-            h2.addEventListener('click', e => molregnoView(h2.innerText))
-            h2.addEventListener('dblclick', e => grok.shell.info(h3.innerText))
-            container.append(ui.card(ui.div([
-                h2, grok.chem.svgMol(data.col('canonical_smiles').get(i),150, 100)]
-            )))
-        }
-        grok.shell.newView( "demo: ChemblBrowser", [container])
-        $('.card-container').css('flex-wrap','wrap');
     }
 
-    initView();
+    async function FindByName() {
+        let query = await grok.data.query(`${packageName}:FindByName`, {'sub': subName.stringValue});
+        grok.shell.addTableView(query);
+
+    }
+
+
+    async function FindByMolregno() {
+        let query = await grok.data.query(`${packageName}:FindByMolregno`, {'sub': molregno.value});
+        grok.shell.addTableView(query);
+    }
+
+
+
+    let molecule = ui.moleculeInput('', '');
+    let subName = ui.stringInput('Find by name', '');
+    let molregno = ui.intInput('Find by molregno');
+
+    molecule.onChanged(() => FindBySubstructure());
+    subName.onChanged(() => FindByName());
+    molregno.onChanged(() => FindByMolregno());
+
+
+
+    let inputs = [molecule, subName,molregno];
+    let controlPanel = ui.divV([ui.h2("Find By Substructure"), ui.inputs(inputs)]);
+
+    let data = await grok.data.query(`${packageName}:allChemblStructures`, {});
+    let v = grok.shell.newView('Chembl Browser');
+    let r = DG.Viewer.fromType(DG.VIEWER.TILE_VIEWER, data);
+
+    v.append(ui.divV([controlPanel, r]));
+
+
+    v.box = true;
+
+
+
+
+
 }
 
 
