77eba6f1303fc99061d4075857b84e27016da158
Alex Paramonov <aparamonov@datagrok.ai>
2021-03-05 17:28:57 +0300

    New example code

--- packages/ApiSamples/scripts/ui/tab-control.js
+++ packages/ApiSamples/scripts/ui/tab-control.js
@@ -5,15 +5,15 @@ let v = grok.shell.newView('Tabs');
 // Simple API
 v.append(ui.h1('Horizontal'));
 v.append(
-  ui.tabControl({
-    'FIRST': ui.divText('First panel'),           // an element
-    'SECOND': () => ui.divText('Second panel')    // a function that returns an element
-  })
+    ui.tabControl({
+        'FIRST': ui.panel('First panel', {style :{backgroundColor: '#EFE'}}),           // an element
+        'SECOND': () => ui.panel('Second panel', {style :{backgroundColor: '#EEF'}})    // a function that returns an element
+    })
 );
 
 // Detailed API
 v.append(ui.h1('Vertical'));
 let tabsV = ui.tabControl(null, true);
-tabsV.addPane('FIRST', () => ui.divText('First panel'));
-tabsV.addPane('SECOND', () => ui.divText('Second panel'));
+tabsV.addPane('FIRST', () => ui.panel('First panel', {style :{backgroundColor: '#EFE'}}));
+tabsV.addPane('SECOND', () => ui.panel('Second panel', {style :{backgroundColor: '#EEF'}}));
 v.append(tabsV);
