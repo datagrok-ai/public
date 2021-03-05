98f3c2c6996d648e6edd47b78d3aff34009a4f90
Vadym Kovadlo <vadym.kovadlo@gmail.com>
2021-03-05 20:52:41 +0200

    GROK-7626 BioSignals: separate module getMethodsAccordingToSignalType

--- packages/BioSignals/src/package.js
+++ packages/BioSignals/src/package.js
@@ -3,6 +3,8 @@ import * as grok from 'datagrok-api/grok';
 import * as ui from 'datagrok-api/ui';
 import * as DG from "datagrok-api/dg";
 
+import {getMethodsAccordingTo} from "./getMethodsAccordingToSignalType.js";
+
 export let _package = new DG.Package();
 
 async function asample(data, col, windowSize, offset) {
@@ -162,63 +164,6 @@ function paramsToTable(filtersLST, allParams) {
     })
   }
   return paramsT;
-}
-
-function getMethodsAccordingTo(signalType) {
-  const dspMethods = ['Moving Average Filter', 'Exponential Filter', 'Min Max Normalization', 'Z-score Normalization', 'Box Cox Transform', 'Get Trend', 'Detrend', 'Fourier Filter', 'Spectral Density', 'Subsample', 'Averaging Downsampling'];
-  let commonFilters = dspMethods.concat(['IIR', 'FIR', 'normalize', 'resample', 'KalmanFilter', 'ImputeNAN', 'RemoveSpikes', 'ConvolutionalFilter']);
-  let commonEstimators = ['Local energy'];
-  let commonIndicators = [];
-  switch (signalType) {
-    case 'ECG':
-      return {
-        filters: commonFilters,
-        estimators: commonEstimators.concat(['Beat from ECG']),
-        indicators: commonIndicators.concat(['HRV time domain', 'HRV frequency domain', 'HRV nonlinear domain'])
-      };
-    case 'EDA':
-      return {
-        filters: commonFilters.concat(['DenoiseEDA']),
-        estimators: commonEstimators.concat(['Phasic estimation']),
-        indicators: commonIndicators
-      };
-    case 'Accelerometer':
-      return {
-        filters: commonFilters,
-        estimators: commonEstimators,
-        indicators: commonIndicators
-      };
-    case 'EMG':
-      return {
-        filters: commonFilters,
-        estimators: commonEstimators,
-        indicators: commonIndicators
-      };
-    case 'EEG':
-      return {
-        filters: commonFilters,
-        estimators: commonEstimators,
-        indicators: commonIndicators
-      };
-    case 'ABP':
-      return {
-        filters: commonFilters,
-        estimators: commonEstimators.concat(['BeatFromBP']),
-        indicators: commonIndicators
-      };
-    case 'BVP(PPG)':
-      return {
-        filters: commonFilters,
-        estimators: commonEstimators.concat(['BeatFromBP']),
-        indicators: commonIndicators
-      };
-    case 'Respiration':
-      return {
-        filters: commonFilters,
-        estimators: commonEstimators,
-        indicators: commonIndicators
-      };
-  }
 }
 
 function showTheRestOfLayout(formView) {
