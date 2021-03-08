e4bf1876518313907688fdf13693879b3960f33e
Andrii Tuzhykov <andrewtuzhykov@gmail.com>
2021-03-08 18:27:25 +0200

    GROK-7949 general query and routing [wip]
    Signed-off-by: Andrii Tuzhykov <andrewtuzhykov@gmail.com>

--- packages/ChemblBrowser/queries/queries.sql
+++ packages/ChemblBrowser/queries/queries.sql
@@ -8,100 +8,36 @@ limit 1000
 --end
 
 
---name: FindBySubstructure
---connection: chembl:chembl
---input: string sub
-select
-  canonical_smiles, molregno
-from
-  compound_structures
-where
-  canonical_smiles ilike '%' || @sub || '%'
-limit 1000
---end
-
---name: FindByName
---connection: chembl:chembl
---input: string sub
-SELECT compound_records.molregno,  compound_structures.canonical_smiles, compound_records.compound_name
-FROM compound_records
-INNER JOIN compound_structures
-ON compound_records.molregno = compound_structures.molregno
-WHERE compound_name ilike '%' || @sub || '%'
-limit 1000
---end
-
-
 --name: FindByMolregno
 --connection: chembl:chembl
 --input: int molregno
 select
-  canonical_smiles, molregno
+  compound_structures.canonical_smiles,
+  compound_structures.molregno,
+  compound_records.compound_name,
+  compound_properties.num_ro5_violations,
+  molecule_synonyms.synonyms,
+  molecule_dictionary.max_phase,
+  molecule_dictionary.chembl_id
 from
   compound_structures
-where molregno = @molregno
-limit 1000
+  left join compound_records on compound_records.molregno = compound_structures.molregno
+  left join molecule_dictionary on molecule_dictionary.molregno = compound_structures.molregno
+  left join molecule_synonyms on molecule_synonyms.molregno = compound_structures.molregno
+  left join compound_properties on compound_properties.molregno = compound_structures.molregno
+where
+ compound_records.molregno = @molregno
 --end
 
---name: FindByRO5
---connection: chembl:chembl
---input: int num_ro5_violations
-SELECT compound_structures.molregno, compound_structures.canonical_smiles, compound_properties.num_ro5_violations
-FROM compound_structures
-INNER JOIN compound_properties
-ON compound_structures.molregno = compound_properties.molregno
-WHERE num_ro5_violations = @num_ro5_violations
-limit 1000
---end
-
---name: ShowSynonyms
---connection: chembl:chembl
-SELECT compound_structures.molregno, compound_structures.canonical_smiles, molecule_synonyms.synonyms
-FROM compound_structures
-INNER JOIN molecule_synonyms
-ON compound_structures.molregno = molecule_synonyms.molregno
-limit 1000
---end
-
---name: ShowChemblID
---connection: chembl:chembl
-SELECT compound_structures.molregno, compound_structures.canonical_smiles, molecule_dictionary.chembl_id
-FROM compound_structures
-INNER JOIN molecule_dictionary
-ON compound_structures.molregno = molecule_dictionary.molregno
-limit 1000
---end
-
---name: FilterByMoleculeType
---connection: chembl:chembl
---input: string molecule_type
-SELECT compound_structures.molregno, compound_structures.canonical_smiles, molecule_dictionary.molecule_type
-FROM compound_structures
-INNER JOIN molecule_dictionary
-ON compound_structures.molregno = molecule_dictionary.molregno
-WHERE molecule_type = @molecule_type
-limit 1000
---end
-
---name: FilterByMaxPhase
---connection: chembl:chembl
---input: int max_phase
-SELECT compound_structures.molregno, compound_structures.canonical_smiles, molecule_dictionary.max_phase
-FROM compound_structures
-INNER JOIN molecule_dictionary
-ON compound_structures.molregno = molecule_dictionary.molregno
-WHERE max_phase = @max_phase
-limit 1000
---end
 
 
 --name: ChemblBrowserQuery
 --connection: chembl:chembl
---input: string substructure = c1
---input: string subname = benzil
---input: int max_phase = 0
---input: string molecule_type = Protein { choices: Query("SELECT DISTINCT molecule_type FROM molecule_dictionary") }
---input: int num_ro5_violations = 0
+--input: string substructure
+--input: string molecule_type
+--input: string subname
+--input: int max_phase
+--input: int num_ro5_violations
 select
   compound_structures.canonical_smiles,
   compound_structures.molregno,
@@ -120,9 +56,10 @@ from
 where
  (compound_structures.canonical_smiles ilike '%' || @substructure || '%' or @substructure is null)
  and (compound_records.compound_name ilike '%' || @subname || '%' or @subname is null)
- and (molecule_dictionary.max_phase = @max_phase or @max_phase is null)
- and (compound_properties.num_ro5_violations = @num_ro5_violations or @num_ro5_violations is null)
-limit 1000
+ and (molecule_dictionary.max_phase = @max_phase or @max_phase = -1)
+ and (compound_properties.num_ro5_violations = @num_ro5_violations or @num_ro5_violations = -1)
+ and (molecule_dictionary.molecule_type = @molecule_type or @molecule_type = 'All')
+ limit 10000
 --end
 
 
--- packages/ChemblBrowser/src/package.js
+++ packages/ChemblBrowser/src/package.js
@@ -22,12 +22,11 @@ export async function Browser() {
 
     // Filter inputs
     let molecule = ui.moleculeInput('Substructure', 'C1CCCCC1');
-    let subName = ui.stringInput('Subname', 'aspirin');
-    let molregno = ui.intInput('Molregno', 42);
-    let ro5Violation = ui.choiceInput('RO5 Violations', '0', ['0','1','2','3','4'] );
-    let maxPhase = ui.choiceInput('Max Phase', '0', ['0','1','2','3','4'] );
-    let molecule_types = ['Protein','Oligonucleotide','Unknown','Antibody','Oligosaccharide','Unclassified','Enzyme','Cell'];
-    let molecule_type = ui.choiceInput('Molecule type', molecule_types[0], molecule_types );
+    let subName = ui.stringInput('Subname', '');
+    let molregno = ui.stringInput('Molregno', '');
+    let ro5Violation = ui.choiceInput('RO5 Violations', 'All', ['All','0','1','2','3','4'] );
+    let maxPhase = ui.choiceInput('Max Phase', 'All', ['All', '0','1','2','3','4'] );
+    let molecule_type = ui.choiceInput('Molecule type', 'All', ['Protein','Oligonucleotide','Unknown','Antibody','Oligosaccharide','Unclassified','Enzyme','Cell', 'All']);
     let clear = ui.button([ui.iconFA('trash-alt'), 'Clear filters'], () => clearFilters());
 
     let controlPanel = ui.form([
@@ -44,15 +43,20 @@ export async function Browser() {
     // Filter handlers
     molecule.onChanged(() => update());
     subName.onChanged(() => update());
-    molregno.onChanged(() => update());
+    molregno.onChanged(() => findByMolregno());
     ro5Violation.onChanged(() => update());
     maxPhase.onChanged(() => update());
     molecule_type.onChanged(() => update());
 
 
     async function initView() {
-        let queryParameters = {'substructure': molecule.value, 'subname': subName.value,'molregno': molregno.value,'num_ro5_violations': parseInt(ro5Violation.value),'max_phase': parseInt(maxPhase.value),'molecule_type': molecule_type.value};
-        let data = await grok.data.query(`${packageName}:ChemblBrowserQuery`, queryParameters);
+
+        let parser = document.createElement('a');
+        parser.href = window.location;
+        let pathSegments = parser.pathname.split('?');
+        if (pathSegments.length>2) {console.log(pathSegments)};
+
+        let data = await grok.data.query(`${packageName}:allChemblStructures`, {});
         data.col('canonical_smiles').semType = DG.SEMTYPE.MOLECULE;
         v = grok.shell.newView('Chembl Browser');
         r = DG.Viewer.fromType(DG.VIEWER.TILE_VIEWER, data);
@@ -62,23 +66,48 @@ export async function Browser() {
     }
 
     async function update() {
-        let queryParameters = {'substructure': molecule.value, 'subname': subName.value,'molregno': molregno.value,'num_ro5_violations': parseInt(ro5Violation.value),'max_phase': parseInt(maxPhase.value),'molecule_type': molecule_type.value};
+        let ro5 = ro5Violation.value == 'All' ? -1 : parseInt(ro5Violation.value);
+        let max_phase = ro5Violation.value == 'All' ? -1 : parseInt(maxPhase.value);
+        let queryParameters = {'substructure': molecule.value, 'subname': subName.value,'num_ro5_violations': ro5,'max_phase': max_phase, 'molecule_type': molecule_type.value};
         let query = await grok.data.query(`${packageName}:ChemblBrowserQuery`, queryParameters);
-        query.col('canonical_smiles').semType = DG.SEMTYPE.MOLECULE;
-        v.root.children[0].remove();
-        r = DG.Viewer.fromType(DG.VIEWER.TILE_VIEWER, query);
-        v.toolbox = ui.div(controlPanel);
-        v.append(ui.divV([r]));
-        v.box = true;
+        if (query.rowCount == 0) {
+            grok.shell.info("No results for this filter were found")
+        } else {
+            query.col('canonical_smiles').semType = DG.SEMTYPE.MOLECULE;
+            v.root.children[0].remove();
+            r = DG.Viewer.fromType(DG.VIEWER.TILE_VIEWER, query);
+            v.toolbox = ui.div(controlPanel);
+            v.append(ui.divV([r]));
+            v.box = true;
+
+            v.path = '';
+            v.path = `/apps/Chemblbrowser/substructure=${molecule.value}?subname=${subName.value}?num_ro5_violations=${ro5}?max_phase=${max_phase}?molecule_type=${molecule_type.value}`;
+        }
+    }
+
+    async function findByMolregno() {
+        let query = await grok.data.query(`${packageName}:FindByMolregno`, {'molregno': parseInt(molregno.value)});
+        if (query.rowCount == 0) {
+            grok.shell.info("No results for this filter were found")
+        } else {
+            query.col('canonical_smiles').semType = DG.SEMTYPE.MOLECULE;
+            v.root.children[0].remove();
+            r = DG.Viewer.fromType(DG.VIEWER.TILE_VIEWER, query);
+            v.toolbox = ui.div(controlPanel);
+            v.append(ui.divV([r]));
+            v.box = true;
+            v.path = '/apps/Chemblbrowser/?molregno=' + molregno.value;
+        }
     }
 
     async function clearFilters() {
         molecule.value = '';
         subName.value = '';
-        ro5Violation.value = '';
-        maxPhase.value = '';
-        molecule_type.value = '';
+        ro5Violation.value = 'All';
+        maxPhase.value = 'All';
+        molecule_type.value = 'All';
         molregno.value = null;
+        v.path = '';
     }
 
     await initView();
