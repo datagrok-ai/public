3fff443bdfdc2838307c14e6b19eeb4add6d71c7
Andrii Tuzhykov <andrewtuzhykov@gmail.com>
2021-03-08 15:17:15 +0200

    GROK-7949 general query [wip]
    Signed-off-by: Andrii Tuzhykov <andrewtuzhykov@gmail.com>

--- packages/ChemblBrowser/queries/queries.sql
+++ packages/ChemblBrowser/queries/queries.sql
@@ -95,4 +95,34 @@ limit 1000
 --end
 
 
+--name: ChemblBrowserQuery
+--connection: chembl:chembl
+--input: string substructure = c1
+--input: string subname = benzil
+--input: int max_phase = 0
+--input: string molecule_type = Protein { choices: Query("SELECT DISTINCT molecule_type FROM molecule_dictionary") }
+--input: int num_ro5_violations = 0
+select
+  compound_structures.canonical_smiles,
+  compound_structures.molregno,
+  compound_records.compound_name,
+  compound_properties.num_ro5_violations,
+  molecule_synonyms.synonyms,
+  molecule_dictionary.molecule_type,
+  molecule_dictionary.max_phase,
+  molecule_dictionary.chembl_id
+from
+  compound_structures
+  left join compound_records on compound_records.molregno = compound_structures.molregno
+  left join molecule_dictionary on molecule_dictionary.molregno = compound_structures.molregno
+  left join molecule_synonyms on molecule_synonyms.molregno = compound_structures.molregno
+  left join compound_properties on compound_properties.molregno = compound_structures.molregno
+where
+ (compound_structures.canonical_smiles ilike '%' || @substructure || '%' or @substructure is null)
+ and (compound_records.compound_name ilike '%' || @subname || '%' or @subname is null)
+ and (molecule_dictionary.max_phase = @max_phase or @max_phase is null)
+ and (compound_properties.num_ro5_violations = @num_ro5_violations or @num_ro5_violations is null)
+limit 1000
+--end
+
 
