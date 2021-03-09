0a21468dcccfbcecd55ae72a80268aab740e7261
Andrii Tuzhykov <andrewtuzhykov@gmail.com>
2021-03-09 14:51:22 +0200

    GROK-7949 Routing added
    Signed-off-by: Andrii Tuzhykov <andrewtuzhykov@gmail.com>

--- packages/ChemblBrowser/src/package.js
+++ packages/ChemblBrowser/src/package.js
@@ -24,9 +24,9 @@ export async function Browser() {
     let molecule = ui.moleculeInput('Substructure', 'C1CCCCC1');
     let subName = ui.stringInput('Subname', '');
     let molregno = ui.stringInput('Molregno', '');
-    let ro5Violation = ui.choiceInput('RO5 Violations', 'All', ['All','0','1','2','3','4'] );
-    let maxPhase = ui.choiceInput('Max Phase', 'All', ['All', '0','1','2','3','4'] );
-    let molecule_type = ui.choiceInput('Molecule type', 'All', ['Protein','Oligonucleotide','Unknown','Antibody','Oligosaccharide','Unclassified','Enzyme','Cell', 'All']);
+    let ro5Violation = ui.choiceInput('RO5 Violations', 'All', ['All', '0', '1', '2', '3', '4']);
+    let maxPhase = ui.choiceInput('Max Phase', 'All', ['All', '0', '1', '2', '3', '4']);
+    let molecule_type = ui.choiceInput('Molecule type', 'All', ['Protein', 'Oligonucleotide', 'Unknown', 'Antibody', 'Oligosaccharide', 'Unclassified', 'Enzyme', 'Cell', 'All']);
     let clear = ui.button([ui.iconFA('trash-alt'), 'Clear filters'], () => clearFilters());
 
     let controlPanel = ui.form([
@@ -53,22 +53,75 @@ export async function Browser() {
 
         let parser = document.createElement('a');
         parser.href = window.location;
-        let pathSegments = parser.pathname.split('?');
-        if (pathSegments.length>2) {console.log(pathSegments)};
+        let pathSegments = parser.href.split("?");
 
-        let data = await grok.data.query(`${packageName}:allChemblStructures`, {});
-        data.col('canonical_smiles').semType = DG.SEMTYPE.MOLECULE;
-        v = grok.shell.newView('Chembl Browser');
-        r = DG.Viewer.fromType(DG.VIEWER.TILE_VIEWER, data);
-        v.append(ui.divV([r]));
-        v.toolbox = ui.div(controlPanel);
-        v.box = true;
+
+        // if we came to app by link with molregno in URL
+        if (pathSegments.length == 2 && pathSegments[1].includes("molregno")) {
+            let parsedMolregno = parseInt(pathSegments[1].split("=")[1]);
+            molregno.value = parsedMolregno;
+            let data = await grok.data.query(`${packageName}:FindByMolregno`, {'molregno': parsedMolregno});
+            data.col('canonical_smiles').semType = DG.SEMTYPE.MOLECULE;
+            v = grok.shell.newView('Chembl Browser');
+            r = DG.Viewer.fromType(DG.VIEWER.TILE_VIEWER, data);
+            v.append(ui.divV([r]));
+            v.toolbox = ui.div(controlPanel);
+            v.box = true;
+
+        }
+        // if we came to app by link with set of parameters in URL
+        else if (pathSegments.length > 2 && pathSegments[1].includes("substructure")) {
+
+            let parsedSubstructure = pathSegments[1].split("=")[1];
+            let parsedSubname = pathSegments[2].split("=")[1];
+            let parsedRo5 = parseInt(pathSegments[3].split("=")[1]);
+            let parsedMaxPhase = parseInt(pathSegments[4].split("=")[1]);
+            let parsedMoleculeType = pathSegments[5].split("=")[1];
+
+            let queryParameters = {
+                'substructure': parsedSubstructure,
+                'subname': parsedSubname,
+                'num_ro5_violations': parsedRo5,
+                'max_phase': parsedMaxPhase,
+                'molecule_type': parsedMoleculeType
+            };
+
+            console.log(queryParameters);
+
+            molecule.value = parsedSubstructure;
+            subName.value = parsedSubname;
+            ro5Violation.value = parsedRo5;
+            maxPhase = parsedMaxPhase;
+            molecule_type.value = parsedMoleculeType;
+
+            let data = await grok.data.query(`${packageName}:ChemblBrowserQuery`, queryParameters);
+            data.col('canonical_smiles').semType = DG.SEMTYPE.MOLECULE;
+            v = grok.shell.newView('Chembl Browser');
+            r = DG.Viewer.fromType(DG.VIEWER.TILE_VIEWER, data);
+            v.append(ui.divV([r]));
+            v.toolbox = ui.div(controlPanel);
+            v.box = true;
+        } else {
+            let data = await grok.data.query(`${packageName}:allChemblStructures`, {});
+            data.col('canonical_smiles').semType = DG.SEMTYPE.MOLECULE;
+            v = grok.shell.newView('Chembl Browser');
+            r = DG.Viewer.fromType(DG.VIEWER.TILE_VIEWER, data);
+            v.append(ui.divV([r]));
+            v.toolbox = ui.div(controlPanel);
+            v.box = true;
+        }
     }
 
     async function update() {
         let ro5 = ro5Violation.value == 'All' ? -1 : parseInt(ro5Violation.value);
         let max_phase = ro5Violation.value == 'All' ? -1 : parseInt(maxPhase.value);
-        let queryParameters = {'substructure': molecule.value, 'subname': subName.value,'num_ro5_violations': ro5,'max_phase': max_phase, 'molecule_type': molecule_type.value};
+        let queryParameters = {
+            'substructure': molecule.value,
+            'subname': subName.value,
+            'num_ro5_violations': ro5,
+            'max_phase': max_phase,
+            'molecule_type': molecule_type.value
+        };
         let query = await grok.data.query(`${packageName}:ChemblBrowserQuery`, queryParameters);
         if (query.rowCount == 0) {
             grok.shell.info("No results for this filter were found")
@@ -79,9 +132,8 @@ export async function Browser() {
             v.toolbox = ui.div(controlPanel);
             v.append(ui.divV([r]));
             v.box = true;
-
             v.path = '';
-            v.path = `/apps/Chemblbrowser/substructure=${molecule.value}?subname=${subName.value}?num_ro5_violations=${ro5}?max_phase=${max_phase}?molecule_type=${molecule_type.value}`;
+            v.path = `/apps/Chemblbrowser?substructure=${molecule.value}?subname=${subName.value}?num_ro5_violations=${ro5}?max_phase=${max_phase}?molecule_type=${molecule_type.value}`;
         }
     }
 
@@ -96,7 +148,7 @@ export async function Browser() {
             v.toolbox = ui.div(controlPanel);
             v.append(ui.divV([r]));
             v.box = true;
-            v.path = '/apps/Chemblbrowser/?molregno=' + molregno.value;
+            v.path = '/apps/Chemblbrowser?molregno=' + molregno.value;
         }
     }
 
