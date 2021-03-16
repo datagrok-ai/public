243c0bf9cc77637d636c4e7d92c501acd825349d
onuf <onuf.diana@gmail.com>
2021-03-16 22:12:45 +0200

    GROK-8241 Timelines: Add `Reset View` item to context menu
    Every viewer instance has an independent context menu option.

--- packages/Charts/src/package.js
+++ packages/Charts/src/package.js
@@ -16,7 +16,7 @@ export let _package = new DG.Package();
 //name: test
 //input: string s
 export function test(s) {
-    grok.shell.info(_package.webRoot);
+  grok.shell.info(_package.webRoot);
 }
 
 //name: TimelinesViewer
@@ -24,68 +24,78 @@ export function test(s) {
 //tags: viewer
 //output: viewer result
 export function _TimelinesViewer() {
-    return new TimelinesViewer();
+  return new TimelinesViewer();
 }
 
 //name: timelinesViewerDemo
 export function timelinesViewerDemo() {
-    let adverseEvents = DG.DataFrame.fromCsv(
-        `USUBJID, AESTDY, AEENDY, SEX, AGE
-        s1, 10/02/2018, 10/09/2018, F, 48
-        s2, 10/04/2018, 10/07/2018, M, 51
-        s3, 10/02/2018, 10/05/2018, F, 39
-        s4, 10/07/2018, 10/08/2018, M, 43`);
+  let adverseEvents = DG.DataFrame.fromCsv(
+    `USUBJID, AESTDY, AEENDY, SEX, AGE
+    s1, 10/02/2018, 10/09/2018, F, 48
+    s2, 10/04/2018, 10/07/2018, M, 51
+    s3, 10/02/2018, 10/05/2018, F, 39
+    s4, 10/07/2018, 10/08/2018, M, 43`);
 
-    let view = grok.shell.addTableView(adverseEvents);
-    view.addViewer('TimelinesViewer');
+  let view = grok.shell.addTableView(adverseEvents);
+  view.addViewer('TimelinesViewer');
 }
 
 //name: RadarViewer
 //tags: viewer
 //output: viewer result
 export function _RadarViewer() {
-    return new RadarViewer();
+  return new RadarViewer();
 }
-
-
 
 //name: radarViewerDemo
 export function radarViewerDemo() {
-    let view = grok.shell.addTableView(grok.data.demo.demog(100));
-    view.addViewer('RadarViewer');
+  let view = grok.shell.addTableView(grok.data.demo.demog(100));
+  view.addViewer('RadarViewer');
 }
 
 //name: TreeViewer
 //tags: viewer
 //output: viewer result
 export function _TreeViewer() {
-    return new TreeViewer();
+  return new TreeViewer();
 }
 
 //name: TreeMapViewer
 //tags: viewer
 //output: viewer result
 export function _TreeMapViewer() {
-    return new TreeMapViewer();
+  return new TreeMapViewer();
 }
 
 //name: SunburstViewer
 //tags: viewer
 //output: viewer result
 export function _SunburstViewer() {
-    return new SunburstViewer();
+  return new SunburstViewer();
 }
 
 //name: SankeyViewer
 //tags: viewer
 //output: viewer result
 export function _SankeyViewer() {
-    return new SankeyViewer();
+  return new SankeyViewer();
 }
 
 //name: ChordViewer
 //tags: viewer
 //output: viewer result
 export function _ChordViewer() {
-    return new ChordViewer();
+  return new ChordViewer();
+}
+
+//tags: init
+export function init() {
+  grok.events.onContextMenu.subscribe(args => {
+    if (args.args.context instanceof TimelinesViewer) {
+      args.args.menu.item('Reset View', () => {
+        args.args.context.zoomState = [[0, 100], [0, 100], [0, 100], [0, 100]];
+        args.args.context.render();
+      });
+    }
+  });
 }
