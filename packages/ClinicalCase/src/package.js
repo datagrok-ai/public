5edaaa43e646764870d3bf8cd973e2baa7f21779
onuf <onuf.diana@gmail.com>
2021-03-16 09:15:20 +0200

    GROK-8227 Clinical Case: validate values on column level
    Type validation for dataframe- and column-based info panels

--- packages/ClinicalCase/src/package.js
+++ packages/ClinicalCase/src/package.js
@@ -13,6 +13,66 @@ let links = {
   lb: { key: 'USUBJID', start: 'LBDY', event: 'LBTEST'}
 };
 
+let typeMap = { 'Char': 'string', 'Num': 'int' };
+
+let terminology, submissionValueCol;
+let submissionValues = [];
+
+//name: SDTM Summary
+//tags: panel, widgets
+//input: dataframe df
+//output: widget result
+//condition: df.tags.get("sdtm")
+export function sdtmSummaryPanel(df) {
+  let domain = df.getTag('sdtm-domain');
+  let text = `SDTM domain: ${domain.toUpperCase()}\n`;
+  for (let column of df.columns) {
+    let name = column.name;
+    let variable = meta.domains[domain][name];
+    text += `${name} ${variable ?
+      typeMap[variable.type] === column.type ?
+      'valid' : 'invalid' : 'unknown variable'}\n`;
+  }
+  return new DG.Widget(ui.divText(text));
+}
+
+//name: SDTM Variable
+//tags: panel, widgets
+//input: column varCol
+//output: widget result
+//condition: t.tags.get("sdtm")
+export function sdtmVariablePanel(varCol) {
+  let domain = meta.domains[varCol.dataFrame.getTag('sdtm-domain')];
+  let variable = domain[varCol.name];
+  let text = `${varCol.name}\n${variable ?
+    variable.label + '\nType: ' + (typeMap[variable.type] === varCol.type ?
+    'valid' : 'invalid') : 'Unknown variable'}\n`;
+
+  let isTerm = submissionValues.includes(varCol.name);
+  text += `CDISC Submission Value: ${isTerm}\n`;
+
+  if (isTerm) {
+    let match = terminology.rows.match({ 'CDISC Submission Value': varCol.name }).toDataFrame();
+    text += `CDISC Synonym(s): ${match.get('CDISC Synonym(s)', 0)}\n`;
+    text += `CDISC Definition: ${match.get('CDISC Definition', 0)}\n`;
+    text += `NCI Preferred Term: ${match.get('NCI Preferred Term', 0)}\n`;
+
+    let relatedRecords = terminology.rows.match({ 'Codelist Code': match.get('Code', 0) }).toDataFrame();
+    let rowCount = relatedRecords.rowCount;
+    if (rowCount) {
+      text += `CDISC Value | CDISC Synonym | NCI Preferred Term\n`;
+      let valueCol = relatedRecords.getCol('CDISC Submission Value');
+      let synCol = relatedRecords.getCol('CDISC Synonym(s)');
+      let nciTermCol = relatedRecords.getCol('NCI Preferred Term');
+      for (let i = 0; i < rowCount; i++) {
+        text += `${valueCol.get(i)} | ${synCol.get(i)} | ${nciTermCol.get(i)}\n`;
+      }
+    }
+  }
+  return new DG.Widget(ui.divText(text));
+}
+
+
 //name: Clinical Case
 //tags: app
 export function clinicalCaseApp() {
@@ -20,19 +80,25 @@ export function clinicalCaseApp() {
 }
 
 //tags: autostart
-export function clinicalCaseInit() {
-  grok.events.onTableAdded.subscribe((args) => {
+export async function clinicalCaseInit() {
+  terminology = await grok.data.loadTable(`${_package.webRoot}tables/sdtm-terminology.csv`);
+  submissionValueCol = terminology.getCol('CDISC Submission Value');
+  submissionValues = submissionValueCol.categories;
+
+  grok.events.onTableAdded.subscribe(args => {
     let t = args.args.dataFrame;
     let domain = meta.domains[t.name.toLowerCase()];
     if (domain) {
+      t.setTag('sdtm', true);
+      t.setTag('sdtm-domain', t.name.toLowerCase());
       for (let variableName in domain)
         if (t.columns.contains(variableName)) {
-          t.col(variableName).semType = 'sdtm-' + t.name.toLowerCase() + '-' + variableName;
-          t.col(variableName).setTag(DG.TAGS.DESCRIPTION, domain[variableName]);
+          //t.col(variableName).semType = 'sdtm-' + t.name.toLowerCase() + '-' + variableName;
+          t.col(variableName).setTag(DG.TAGS.DESCRIPTION, domain[variableName]['label']);
         }
     }
 
-    if (Object.keys(links).every((key) => grok.shell.tableByName(key))) {
+    if (Object.keys(links).every(key => grok.shell.tableByName(key))) {
       grok.shell.topMenu.group('Clin').item('Timelines', () => clinicalCaseTimelines());
     }
   });
