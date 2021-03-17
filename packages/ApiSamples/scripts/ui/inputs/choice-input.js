11d817af36e00edfc5ab8ebe9a70055d60dc724c
dskatov <dskatov@datagrok.ai>
2021-03-17 13:49:46 +0300

    Improving the choice-input.js example.

--- packages/ApiSamples/scripts/ui/inputs/choice-input.js
+++ packages/ApiSamples/scripts/ui/inputs/choice-input.js
@@ -11,15 +11,21 @@ let choices_new = ui.choiceInput('Value', 'B', ['B', 'C', 'D']);
 inputs.replaceChild(choices_new.root, choices.root);
 choices = choices_new;
 choices.onChanged((v) => {
-  ui.dialog().show();
+  grok.shell.info('The selected value is changed');
 });
+choices.value = 'C';
 
 // Method #2. Using raw DOM manipulation
 let items = ['C', 'D', 'E'];
 let choicesDOM = choices.input;
 for (index = 0; index < items.length; ++index) {
-  choicesDOM.options[index] = new Option(items[index], index);
+  choicesDOM.options[index] = new Option(
+    items[index],
+    items[index]); // this is a name you select by
 }
+// Selecting an item with a name 'D'
+// The event choices.onChanged WON'T be triggered
+choicesDOM.value = 'D';
 
 // Method #3. Using jQuery syntax
 $(choicesDOM).append(new Option('F', 'F'));
