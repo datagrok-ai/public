d6b0ff5a2ac8e53f69b220f76b5d7b7d3ed760ba
dskatov <dskatov@datagrok.ai>
2021-03-24 20:43:21 +0300

    Adding utils library.

---
+++ libraries/utils/webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  devtool: 'inline-source-map',
+
+  entry: {
+    package: './src/package.ts'
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".jsx", ".css"]
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.ts(x?)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "ts-loader"
+          }
+        ]
+      },
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        loader: "source-map-loader"
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ]
+  },
+
+  externals: {
+    'datagrok-api/dg': 'DG',
+    'datagrok-api/grok': 'grok',
+    'datagrok-api/ui': 'ui',
+    "rxjs": "rxjs",
+    "rxjs/operators": "rxjs.operators"
+  },
+
+  output: {
+    filename: '[name].js',
+    library: 'datagrok_utils',
+    path: path.resolve(__dirname, 'dist')
+  }
+};
