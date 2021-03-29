d3593acf120f9e5946dcd11d18b995369fc8df73
Alex Paramonov <aparamonov@datagrok.ai>
2021-03-29 22:25:05 +0300

    GROK-8338 JS API: Migrate to Typescript

---
+++ libraries/utils/src/foo.ts
@@ -0,0 +1,6 @@
+declare var grok: any;
+
+export function bar(a: string) : string {
+    grok.aaa();
+    return `${a} aa`;
+}
---
+++ libraries/utils/src/package.js
@@ -0,0 +1,4 @@
+/* Do not change these import lines. Datagrok will import API library in exactly the same manner */
+export function utilsFoo() {
+    return 42;
+}
