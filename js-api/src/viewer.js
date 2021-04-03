98c9533592fd6e3e00c929a755d42f9b865ad47e
onuf <56073962+onuf@users.noreply.github.com>
2021-04-03 16:51:49 +0300

    GROK-8338 JS API: Migrate to Typescript

--- js-api/src/viewer.js
+++ js-api/src/viewer.js
@@ -55,8 +55,8 @@ export class Viewer extends Widget {
     return api.grok_Viewer_GetViewerTypes();
   }
   /**
-     * Sets viewer options. See also {@link getOptions}
-     *  Sample: https://public.datagrok.ai/js/samples/ui/viewers/types/scatter-plot
+     *  Sets viewer options. See also {@link getOptions}
+     *  Sample: {@link https://public.datagrok.ai/js/samples/ui/viewers/types/scatter-plot}
      *  @param {object} map */
   setOptions(map) {
     api.grok_Viewer_Options(this.d, JSON.stringify(map));
