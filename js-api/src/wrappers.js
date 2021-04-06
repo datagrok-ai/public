739bf22d408e002c873ca94c32b39c8c06ed93a1
Alex Paramonov <aparamonov@datagrok.ai>
2021-04-06 13:30:16 +0300

    GROK-8338 JS API: Migrate to Typescript

--- js-api/src/wrappers.js
+++
@@ -1,9 +0,0 @@
-export function paramsToJs(params) {
-  return DG.paramsToJs(params);
-}
-export function toJs(d, check = false) {
-  return DG.toJs(d);
-}
-export function toDart(x) {
-  return DG.toDart(x);
-}
