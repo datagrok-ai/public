5d630c38917e1c0001729be5c8612c05916a755e
dskatov <dskatov@datagrok.ai>
2021-04-12 13:23:21 +0300

    GROK-7432 Chem: tweaked detectors to match on the endsWith keyword

--- packages/Chem/detectors.js
+++ packages/Chem/detectors.js
@@ -5,11 +5,10 @@ class ChemPackageDetectors extends DG.Package {
   //output: string semType
   detectRDSmiles(col) {
     if ((
-      col.name === 'smiles' ||
-      col.name === 'rdkit' ||
-      col.name === 'molecule' ||
-      col.name === 'scaffold' ||
-      col.name === 'Scaffold'
+      col.name.toLowerCase().endsWith('smiles') ||
+      col.name.toLowerCase().endsWith('rdkit') ||
+      col.name.toLowerCase().endsWith('molecule') ||
+      col.name.toLowerCase().endsWith('scaffold')
     ) && col.type === DG.TYPE.STRING) {
       col.semType = DG.SEMTYPE.MOLECULE;
       return col.semType;
