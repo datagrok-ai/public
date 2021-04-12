c7a466247ec2725d275cbdde4b00caa1e8ff3757
Vadym Kovadlo <vadym.kovadlo@gmail.com>
2021-04-12 21:49:57 +0300

    GROK-8413 sequenceTranslator: usage of sampleCategories in detector(WIP)
    - use 'replace' instead of 'split().map().join()' in package.ts

--- packages/SequenceTranslator/detectors.js
+++ packages/SequenceTranslator/detectors.js
@@ -1,45 +1,16 @@
 class SequencetranslatorPackageDetectors extends DG.Package {
-  // https://stackoverflow.com/questions/19269545/how-to-get-a-number-of-random-elements-from-an-array
-  getRandom(arr, n) {
-    let result = new Array(n),
-      len = arr.length,
-      taken = new Array(len);
-    while (n--) {
-      let x = Math.floor(Math.random() * len);
-      result[n] = arr[x in taken ? taken[x] : x];
-      taken[x] = --len in taken ? taken[len] : len;
-    }
-    return result;
-  }
-
   //tags: semTypeDetector
   //input: column col
   //output: string semType
   detectNucleotides(col) {
     if (col.type === DG.TYPE.STRING) {
-      const numberOfRandomValues = Math.max(1, Math.round(0.1 * col.categories.length));
-      const decimatedColumn = this.getRandom(col.categories.slice(0, col.categories.length - 1), numberOfRandomValues);
-      if (this.isClassicFormat(decimatedColumn)) return 'dna_sequence/classic code';
-      if (this.isBioSpringCodeForASOGapmers(decimatedColumn)) return 'BioSpring Code For ASO Gapmers';
-      if (this.isGCRSCodeForASOGampers(decimatedColumn)) return 'Janssen GCRS code For ASO Gapmers';
+      const checksNum = Math.max(1, Math.round(0.1 * col.length));
+      if (DG.Detector.sampleCategories(col, (s) => /^[ATGC]*$/.test(s), 1, checksNum))
+        return 'dna_sequence/classic code';
+      if (DG.Detector.sampleCategories(col, (s) => /^[*56789ATGC]*$/.test(s), 1, checksNum))
+        return 'BioSpring Code For ASO Gapmers';
+      if (DG.Detector.sampleCategories(col, (s) => /^(moe|5mC|n|ps|A|C|G|T|U)*$/.test(s), 1, checksNum))
+        return 'Janssen GCRS code For ASO Gapmers';
     }
-  }
-
-  isClassicFormat(decimatedColumn) {
-    for (let valueInRow of decimatedColumn)
-      if (!/^[ATGC]+$/.test(valueInRow)) return false;
-    return true;
-  }
-
-  isBioSpringCodeForASOGapmers(decimatedColumn) {
-    for (let valueInRow of decimatedColumn)
-      if (!/^[*56789ACTG]+$/.test(valueInRow)) return false;
-    return true;
-  }
-
-  isGCRSCodeForASOGampers(decimatedColumn) {
-    for (let valueInRow of decimatedColumn)
-      if (!(valueInRow.slice(0, 3) === 'moe')) return false;
-    return true;
   }
 }
