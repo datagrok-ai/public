639ed35f03fa459737ec27371365697d0d300453
tlk00 <anatoliy.kuznetsov@gmail.com>
2021-04-13 17:11:00 -0400

    GROK-8397: WASM: initial skeleton for GrokCache WASM package build and test scripts

---
+++ packages/GrokCache/src/cpp-src/build_webasm_debug.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#
+# Utility script to rebuild WASM module
+#
+emcc -std=c++11 -O0 --bind -s "MODULARIZE=1" -s "EXPORT_NAME=createGrokCache" -s EXPORTED_RUNTIME_METHODS='["ccall","cwrap"]' -s WASM=1 -s ALLOW_MEMORY_GROWTH=1 -s DISABLE_EXCEPTION_CATCHING=0 -s NO_EXIT_RUNTIME=1 grokcache.cpp -o grokcache.js
