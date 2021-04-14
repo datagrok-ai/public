531160b82a2a9a5e427eedec2cdf5113488e0073
Vadym Kovadlo <vadym.kovadlo@gmail.com>
2021-04-14 22:56:57 +0300

    GROK-8470 sequenceTranslator: added Axolabs converters (WIP)

--- packages/SequenceTranslator/src/package.ts
+++ packages/SequenceTranslator/src/package.ts
@@ -72,6 +72,46 @@ export function GCRSToClassic(nucleotides: string) {
   return nucleotides.replace(/(moe|5m|n|ps|U)/g, function (x: string) {return obj[x];});
 }
 
+//name: GCRSToAxolabs
+//input: string nucleotides {semType: GCRS / siRNA}
+//output: string result {semType: Axolabs / siRNA}
+export function GCRSToAxolabs(nucleotides: string) {
+  const obj: {[index: string]: string} = {
+    "fU": "Uf", "fA": "Af", "fC": "Cf", "fG": "Gf", "mU": "u", "mA": "a", "mC": "c", "mG": "g", "ps": "s"
+  };
+  return nucleotides.replace(/(fU|fA|fC|fG|mU|mA|mC|mG|ps)/g, function (x: string) {return obj[x];});
+}
+
+//name: axolabsToGCRS
+//input: string nucleotides {semType: Axolabs / siRNA}
+//output: string result {semType: GCRS / siRNA}
+export function axolabsToGCRS(nucleotides: string) {
+  const obj: {[index: string]: string} = {
+    "Uf": "fU", "Af": "fA", "Cf": "fC", "Gf": "fG", "u": "mU", "a": "mA", "c": "mC", "g": "mG", "s": "ps"
+  };
+  return nucleotides.replace(/(Uf|Af|Cf|Gf|u|a|c|g|s)/g, function (x: string) {return obj[x];});
+}
+
+//name: axolabsToBioSpring
+//input: string nucleotides {semType: Axolabs / siRNA}
+//output: string result {semType: BioSpring / siRNA}
+export function axolabsToBioSpring(nucleotides: string) {
+  const obj: {[index: string]: string} = {
+    "Uf": "1", "Af": "2", "Cf": "3", "Gf": "4", "u": "5", "a": "6", "c": "7", "g": "8", "s": "*"
+  };
+  return nucleotides.replace(/(Uf|Af|Cf|Gf|u|a|c|g|s)/g, function (x: string) {return obj[x];});
+}
+
+//name: axolabsToClassic
+//input: string nucleotides {semType: Axolabs / siRNA}
+//output: string result {semType: nucleotides}
+export function axolabsToClassic(nucleotides: string) {
+  const obj: {[index: string]: string} = {
+    "Uf": "U", "Af": "A", "Cf": "C", "Gf": "G", "u": "U", "a": "A", "c": "C", "g": "G", "s": ""
+  };
+  return nucleotides.replace(/(Uf|Af|Cf|Gf|u|a|c|g|s)/g, function (x: string) {return obj[x];});
+}
+
 //name: classicComplement
 //input: string nucleotides {semType: nucleotides}
 //output: string result {semType: nucleotides}
