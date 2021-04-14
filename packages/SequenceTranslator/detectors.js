7d28a4b318083b2b5183d60af5be4c2320ba56db
Vadym Kovadlo <vadym.kovadlo@gmail.com>
2021-04-14 10:57:27 +0300

    GROK-8470 sequenceTranslator: added semType detectors of all codes

--- packages/SequenceTranslator/detectors.js
+++ packages/SequenceTranslator/detectors.js
@@ -4,12 +4,18 @@ class SequencetranslatorPackageDetectors extends DG.Package {
   //output: string semType
   detectNucleotides(col) {
     if (col.type === DG.TYPE.STRING) {
-      if (DG.Detector.sampleCategories(col, (s) => /^[ATGC]*$/.test(s)))
+      if (DG.Detector.sampleCategories(col, (s) => /^[ATGCU]{10,}$/.test(s)))
         return 'nucleotides';
-      if (DG.Detector.sampleCategories(col, (s) => /^[*56789ATGC]*$/.test(s)))
+      if (DG.Detector.sampleCategories(col, (s) => /^[*56789ATGC]{30,}$/.test(s)))
         return 'BioSpring / Gapmers';
-      if (DG.Detector.sampleCategories(col, (s) => /^(moe|5mC|n|ps|A|C|G|T|U)*$/.test(s)))
+      if (DG.Detector.sampleCategories(col, (s) => /^(?=.*moe)(?=.*5mC)(?=.*ps){30,}/.test(s)))
         return 'GCRS / Gapmers';
+      if (DG.Detector.sampleCategories(col, (s) => /^[*1-8]{30,}$/.test(s)))
+        return 'BioSpring / siRNA';
+      if (DG.Detector.sampleCategories(col, (s) => /^[fsACGUacgu]{30,}$/.test(s)))
+        return 'Axolabs / siRNA';
+      if (DG.Detector.sampleCategories(col, (s) => /^[fmpsACGU]{30,}$/.test(s)))
+        return 'GCRS / siRNA';
     }
   }
 }
