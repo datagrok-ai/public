eafb3513593932fde1e3e0f592d00493658290d6
Vadym Kovadlo <vadym.kovadlo@gmail.com>
2021-04-16 00:44:13 +0300

    GROK-8470 sequenceTranslator: new app design
    - deleted complement functions

--- packages/SequenceTranslator/detectors.js
+++ packages/SequenceTranslator/detectors.js
@@ -1,17 +1,21 @@
-import {isClassicCode, isAsoGapmerBioSpringCode, isSiRnaAxolabsCode, isAsoGapmerGCRSCode, isSiRnaBioSpringCode, isSiRnaGCRSCode} from "./src/package.ts";
-
 class SequencetranslatorPackageDetectors extends DG.Package {
   //tags: semTypeDetector
   //input: column col
   //output: string semType
   detectNucleotides(col) {
     if (col.type === DG.TYPE.STRING) {
-      if (DG.Detector.sampleCategories(col, (s) => isClassicCode(s))) return 'nucleotides';
-      if (DG.Detector.sampleCategories(col, (s) => isAsoGapmerBioSpringCode(s))) return 'BioSpring / Gapmers';
-      if (DG.Detector.sampleCategories(col, (s) => isAsoGapmerGCRSCode(s))) return 'GCRS / Gapmers';
-      if (DG.Detector.sampleCategories(col, (s) => isSiRnaBioSpringCode(s))) return 'BioSpring / siRNA';
-      if (DG.Detector.sampleCategories(col, (s) => isSiRnaAxolabsCode(s))) return 'Axolabs / siRNA';
-      if (DG.Detector.sampleCategories(col, (s) => isSiRnaGCRSCode(s))) return 'GCRS / siRNA';
+      if (DG.Detector.sampleCategories(col, (s) => /^[ATGCU]{10,}$/.test(s)))
+        return 'nucleotides';
+      if (DG.Detector.sampleCategories(col, (s) => /^[*56789ATGC]{30,}$/.test(s)))
+        return 'BioSpring / Gapmers';
+      if (DG.Detector.sampleCategories(col, (s) => /^(?=.*moe)(?=.*5mC)(?=.*ps){30,}/.test(s)))
+        return 'GCRS / Gapmers';
+      if (DG.Detector.sampleCategories(col, (s) => /^[*1-8]{30,}$/.test(s)))
+        return 'BioSpring / siRNA';
+      if (DG.Detector.sampleCategories(col, (s) => /^[fsACGUacgu]{30,}$/.test(s)))
+        return 'Axolabs / siRNA';
+      if (DG.Detector.sampleCategories(col, (s) => /^[fmpsACGU]{30,}$/.test(s)))
+        return 'GCRS / siRNA';
     }
   }
 }
