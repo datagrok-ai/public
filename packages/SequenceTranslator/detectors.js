0f0c13589e6b66818b3104c0f2e3231e51b41244
Vadym Kovadlo <vadym.kovadlo@gmail.com>
2021-04-19 16:16:31 +0300

    GROK-8497 sequenceTranslator: show molecules of input sequence(WIP)
    -split semType 'nucleotides' into two 'RNA nucleotides' and 'DNA nucleotides'
    -updated icon of App

--- packages/SequenceTranslator/detectors.js
+++ packages/SequenceTranslator/detectors.js
@@ -4,8 +4,10 @@ class SequencetranslatorPackageDetectors extends DG.Package {
   //output: string semType
   detectNucleotides(col) {
     if (col.type === DG.TYPE.STRING) {
-      if (DG.Detector.sampleCategories(col, (s) => /^[ATGCU]{10,}$/.test(s)))
-        return 'nucleotides';
+      if (DG.Detector.sampleCategories(col, (s) => /^[ATGC]{10,}$/.test(s)))
+        return 'DNA nucleotides';
+      if (DG.Detector.sampleCategories(col, (s) => /^[AUGC]{10,}$/.test(s)))
+        return 'RNA nucleotides';
       if (DG.Detector.sampleCategories(col, (s) => /^[*56789ATGC]{30,}$/.test(s)))
         return 'BioSpring / Gapmers';
       if (DG.Detector.sampleCategories(col, (s) => /^(?=.*moe)(?=.*5mC)(?=.*ps){30,}/.test(s)))
