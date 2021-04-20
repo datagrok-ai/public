a8fecf6ed920cd996cdb34c541fd11bd4e53fcee
Andrew Skalkin <skalkin@gmail.com>
2021-04-20 08:27:48 -0400

    GROK-8503: JS API: Grid: expose "invalidate" method

---
+++ packages/ApiSamples/scripts/grid/advanced/paint-counts.js
@@ -0,0 +1,7 @@
+let counts = {};
+let grid = grok.shell.addTableView(grok.data.demo.demog()).grid;
+grid.onCellPrepare(function (gc) {
+  let xy = `${gc.gridColumn.name}_${gc.gridRow}`;
+  counts[xy] = counts[xy] == null ? 1 : counts[xy] + 1;
+  gc.customText = `${counts[xy]}`;
+});
