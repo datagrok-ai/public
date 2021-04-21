5956a09ab4a3163563748d020908b6376ed36f4a
onuf <56073962+onuf@users.noreply.github.com>
2021-04-21 14:25:43 +0300

    GROK-8384 PhyloTreeViewer: selection of descendants, more efficient rendering on current row change

--- packages/PhyloTreeViewer/src/tree-viewer.js
+++ packages/PhyloTreeViewer/src/tree-viewer.js
@@ -3,52 +3,57 @@ export class PhyloTreeViewer extends DG.JsViewer {
     super();
     this.radialLayout = this.bool('radialLayout', false);
     this.fontSize = this.string('fontSize', '6px');
+    this.nodeIdColumnName = this.string('nodeIdColumnName', 'node');
     this.defaultSize = 400;
     this.root.style = 'position: absolute; left: 0; right: 0; top: 0; bottom: 0;';
-    this.nodeSelectionSourceColumnName = this.string('nodeSelectionSourceColumnName', 'node');
+    this.tree = d3.layout.phylotree();
   }
 
   onTableAttached() {
     this.newick = this.dataFrame.getTag('.newick');
     this.parsedNewick = JSON.parse(this.dataFrame.getTag('.newickJson'));
-    this.nodeSourceColumn = this.dataFrame.col(this.nodeSelectionSourceColumnName);
-    this.subs.push(this.dataFrame.onCurrentRowChanged.subscribe(() => this.render()));
+    this.nodeSourceColumn = this.dataFrame.col(this.nodeIdColumnName);
+    this.subs.push(this.dataFrame.onCurrentRowChanged.subscribe(() => this.render(false)));
     this.render();
   }
 
   onPropertyChanged() { this.render(); }
 
-  render() {
-    $(this.root).empty();
+  render(redraw = true) {
+    if (redraw) {
+      $(this.root).empty();
 
-    if (this.newick == null) {
-      this.root.appendChild(ui.divText('Newick tag not found.', 'd4-viewer-error'));
-      return;
+      if (this.newick == null) {
+        this.root.appendChild(ui.divText('Newick tag not found.', 'd4-viewer-error'));
+        return;
+      }
+
+      const svg = d3.select(this.root).append("svg");
+
+      this.tree
+        .svg(svg)
+        .options({
+          'left-right-spacing': 'fit-to-size',
+          'top-bottom-spacing': 'fit-to-size',
+          zoom: true,
+        })
+        .size([
+          this.root.parentElement.clientHeight || this.defaultSize,
+          this.root.parentElement.clientWidth || this.defaultSize
+        ])
+        .font_size(parseInt(this.fontSize))
+        .radial(this.radialLayout);
+
+      this.tree(this.parsedNewick).layout();
     }
 
-    const svg = d3.select(this.root).append("svg");
-
-    const tree = d3.layout.phylotree()
-      .svg(svg)
-      .options({
-        'left-right-spacing': 'fit-to-size',
-        'top-bottom-spacing': 'fit-to-size',
-        zoom: true,
-      })
-      .size([
-        this.root.parentElement.clientHeight || this.defaultSize,
-        this.root.parentElement.clientWidth || this.defaultSize
-      ])
-      .font_size(parseInt(this.fontSize))
-      .radial(this.radialLayout);
-
-    tree(this.parsedNewick).layout();
-
     if (this.nodeSourceColumn) {
-      const node = this.nodeSourceColumn.get(this.dataFrame.currentRow.idx);
-      if (node) {
-        tree.modify_selection(node => false);
-        tree.modify_selection(tree.path_to_root(tree.get_node_by_name(node)));
+      const nodeName = this.nodeSourceColumn.get(this.dataFrame.currentRow.idx);
+      if (nodeName) {
+        const node = this.tree.get_node_by_name(nodeName);
+        this.tree.modify_selection(() => false);
+        this.tree.modify_selection(this.tree.path_to_root(node)
+          .concat(this.tree.select_all_descendants(node, true, true)));
       }
     }
   }
