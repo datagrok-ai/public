e1d1dea65c8a5a763a3d83a8a5bce6d7c572a460
onuf <56073962+onuf@users.noreply.github.com>
2021-04-22 13:50:03 +0300

    GROK-8384 PhyloTreeViewer

--- packages/PhyloTreeViewer/src/tree-viewer.js
+++ packages/PhyloTreeViewer/src/tree-viewer.js
@@ -2,8 +2,12 @@ export class PhyloTreeViewer extends DG.JsViewer {
   constructor() {
     super();
     this.radialLayout = this.bool('radialLayout', false);
-    this.fontSize = this.string('fontSize', '6px');
+    this.fontSize = this.string('fontSize', '9px');
     this.nodeIdColumnName = this.string('nodeIdColumnName', 'node');
+    this.selection = this.string('selection', 'descendants', { choices: [
+      'none', 'path to root', 'descendants', 'path to root & descendants'
+    ]});
+    this.tooltipOffset = 10;
     this.defaultSize = 400;
     this.root.style = 'position: absolute; left: 0; right: 0; top: 0; bottom: 0;';
     this.tree = d3.layout.phylotree();
@@ -15,6 +19,7 @@ export class PhyloTreeViewer extends DG.JsViewer {
     this.nodeSourceColumn = this.dataFrame.col(this.nodeIdColumnName);
 
     this.subs.push(this.dataFrame.onCurrentRowChanged.subscribe(() => this.render(false)));
+    this.subs.push(DG.debounce(ui.onSizeChanged(this.root), 50).subscribe((_) => this.render()));
     this.render();
 
     d3.select(this.root).selectAll('.node > text')
@@ -71,12 +76,17 @@ export class PhyloTreeViewer extends DG.JsViewer {
     const nodeName = this.nodeSourceColumn.get(this.dataFrame.currentRow.idx);
     if (nodeName) {
       this.tree.modify_selection(() => false);
+      if (this.selection === 'none') return;
 
       const node = this.tree.get_node_by_name(nodeName);
       if (nodeName === 'root' || node.depth === 0) return;
 
-      this.tree.modify_selection(this.tree.path_to_root(node)
-        .concat(this.tree.select_all_descendants(node, true, true)));
+      const selection = (this.selection === 'path to root') ?
+      this.tree.path_to_root(node) : (this.selection === 'descendants') ?
+      this.tree.select_all_descendants(node, true, true) : (this.selection === 'path to root & descendants') ?
+      this.tree.path_to_root(node).concat(this.tree.select_all_descendants(node, true, true)) : [];
+
+      this.tree.modify_selection(selection);
     }
   }
 }
