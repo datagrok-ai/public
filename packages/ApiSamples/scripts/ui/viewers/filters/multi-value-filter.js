90608ad580147af8c9d6d5a2c8446dceb4beb036
Andrew Skalkin <skalkin@gmail.com>
2021-04-28 12:12:19 -0400

    GROK-8565: Multi-value filters

---
+++ packages/ApiSamples/scripts/ui/viewers/filters/multi-value-filter.js
@@ -0,0 +1,30 @@
+// Filters automatically switch to the multi-value mode
+// when the DG.TAGS.MULTI_VALUE_SEPARATOR tag is set on a column
+
+let languages = `Country,Languages
+Belgium,"Dutch
+French
+German"
+Burundi,"French
+Kirundi
+English"
+Cameroon, "English
+French"
+Canada, "English
+French"
+Italy, "Italian
+French
+German
+Slovene
+Corsican"
+Rwanda, "English
+French"
+Netherlands, Dutch
+Switzerland, "French
+German
+Italian"`;
+
+let t = DG.DataFrame.fromCsv(languages);
+t.col('languages').setTag(DG.TAGS.MULTI_VALUE_SEPARATOR, '\n');
+
+grok.shell.addTableView(t).addViewer(DG.VIEWER.FILTERS);
