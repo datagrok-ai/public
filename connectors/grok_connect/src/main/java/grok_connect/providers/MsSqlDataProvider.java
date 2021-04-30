b7ff907c6b5d3163c73373d4a012aaa6cca7220c
kdoncov <kdoncov@datagrok.ai>
2021-04-30 20:01:09 +0300

    GROK-8593: Grok connect: can't connect to MsSQL using TLS10

--- connectors/grok_connect/src/main/java/grok_connect/providers/MsSqlDataProvider.java
+++ connectors/grok_connect/src/main/java/grok_connect/providers/MsSqlDataProvider.java
@@ -62,7 +62,7 @@ public class MsSqlDataProvider extends JdbcDataProvider {
         String schema = conn.get("schema");
         db = (db == null || db.length() == 0) && schema != null ? schema : db;
         return "jdbc:sqlserver://" + conn.getServer() + port + ";databaseName=" + db +
-                (conn.ssl() ? "integratedSecurity=true;encrypt=true;trustServerCertificate=true;" : "");
+                (conn.ssl() ? "integratedSecurity=true;encrypt=true;trustServerCertificate=true;sslProtocol=TLS;" : "");
     }
 
     public DataFrame getSchema(DataConnection connection, String schema, String table)
