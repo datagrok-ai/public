411042fb176d0703fde6cca768800cc4a3800d5a
dskatov <dskatov@datagrok.ai>
2021-05-06 19:30:00 +0300

    GROK-8623 Dataframe: Detect column max significant digits in CSV loading. WIP Adding a sample

---
+++ packages/ApiSamples/scripts/data-frame/construction/create-from-csv-format.js
@@ -0,0 +1,19 @@
+// Creating a DataFrame from a CSV string and using the numeric format from it in the grid
+
+let table = DG.DataFrame.fromCsv(
+`country, continent, year,  lifeExp,  pop,       gdpPercap
+Angola,   Africa,    1992,  40.647,   8735988,   2627.8457031
+Austria,  Europe,    1992,  76.04,    7914969,   27042.0195313
+Austria,  Europe,    1992,  76.04,    7914969,   27042.0195313
+Bolivia,  Americas,  1992,  59.957,   6893451,   2961.699707
+Bulgaria, Europe,    1992,  71.19,    8658506,   6302.6235352
+Poland,   Europe,    1992,  70.99,    38370697,  7738.8813477
+Zambia,   Africa,    1992,  46.1,     8381163,   1210.8846436
+`);
+
+let view = grok.shell.addTableView(table);
+
+// By default, the `gdpPercap` column format will be set in the grid to 2 significant digits.
+// It's possible to override the grid's column formatting to the one coming from the CSV data:
+
+view.grid.col('gdpPercap').format = table.col('gdpPercap').tags[DG.TAGS.SOURCE_FORMAT];
