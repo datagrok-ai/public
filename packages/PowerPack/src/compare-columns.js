ca92c7687f7d1db5ecd506c282c3c3c2672c3fbe
Vadym Kovadlo <vadym.kovadlo@gmail.com>
2021-05-12 21:44:33 +0300

    GROK-8571 PowerPack: Compare Columns(WIP)

--- packages/PowerPack/src/compare-columns.js
+++ packages/PowerPack/src/compare-columns.js
@@ -1,6 +1,37 @@
 import * as grok from 'datagrok-api/grok';
-//name: test
+import * as ui from 'datagrok-api/ui';
+import * as DG from 'datagrok-api/dg';
+
+//name: Compare Columns
 //tags: autostart
 export function compareColumns() {
-    grok.shell.info('Comparing columns');
+  let tables = grok.shell.tables;
+  let tablesNames = tables.map((t) => t.name);
+
+  let t1 = ui.choiceInput('', '', tablesNames, () => {});
+  let t2 = ui.choiceInput('', '', tablesNames, () => {});
+  let c1 = ui.choiceInput('', '', tables[0].columns.names(), () => {});
+  let c2 = ui.choiceInput('', '', tables[0].columns.names(), () => {});
+
+  t1.input.style.width = '150px';
+  t2.input.style.width = '150px';
+  c1.input.style.width = '150px';
+  c2.input.style.width = '150px';
+
+  ui.dialog('Compare Columns')
+    .add(
+      ui.divV([
+        ui.label('Tables'),
+        ui.divH([t1, t2], {style: {marginBottom: '10px'}}),
+        ui.label('Columns'),
+        ui.divH([c1, c2])
+      ])
+    )
+    .add(ui.button('Compare', () => {
+      let tbls = grok.shell.tables[0];
+      let a = tbls.columns.byName(c1.value);
+      let b = tbls.columns.byName(c2.value);
+      tbls.columns.addNewBool('Result').init((i) => (a.get(i) === b.get(i)));
+    }))
+    .show();
 }
