db5b9bdc2f72c470666103e41c1c668c05814f29
Vadym Kovadlo <vadym.kovadlo@gmail.com>
2021-05-13 16:04:00 +0300

    GROK-8571 PowerPack: Compare Columns(WIP)

--- packages/PowerPack/src/compare-columns.js
+++ packages/PowerPack/src/compare-columns.js
@@ -2,36 +2,83 @@ import * as grok from 'datagrok-api/grok';
 import * as ui from 'datagrok-api/ui';
 import * as DG from 'datagrok-api/dg';
 
-//name: Compare Columns
-//tags: autostart
-export function compareColumns() {
-  let tables = grok.shell.tables;
-  let tablesNames = tables.map((t) => t.name);
+function compare(c1, c2) {
+  if (c1.name === c2.name) {c2.name = c1.stringValue + ' (1)'};
+  let df = DG.DataFrame.fromColumns([c1, c2]);
+  df.columns.addNewCalculated(
+    c1.name + '==' + c2.name,
+    '${' + c1.name + '}==${' + c2.name + '}'
+  ).then((_) => {
+    grok.shell.addTableView(df);
+  });
+}
 
-  let t1 = ui.choiceInput('', '', tablesNames, () => {});
-  let t2 = ui.choiceInput('', '', tablesNames, () => {});
-  let c1 = ui.choiceInput('', '', tables[0].columns.names(), () => {});
-  let c2 = ui.choiceInput('', '', tables[0].columns.names(), () => {});
+function addCols(t1, t2, container) {
 
-  t1.input.style.width = '150px';
-  t2.input.style.width = '150px';
+  let firstColumnAdded = false,
+    secondColumnAdded = false;
+
+  let c1 = ui.choiceInput('', '', grok.shell.tables.find(({name}) => name === t1.value).columns.names(), (chosenColumnName) => {
+    firstColumnAdded = true;
+    if (secondColumnAdded)
+      compare(
+        grok.shell.tables.find(({name}) => name === t1.value).columns.byName(chosenColumnName),
+        grok.shell.tables.find(({name}) => name === t2.value).columns.byName(c2.value)
+      );
+  });
+
+  let c2 = ui.choiceInput('', '', grok.shell.tables.find(({name}) => name === t2.value).columns.names(), (chosenColumnName) => {
+    secondColumnAdded = true;
+    if (firstColumnAdded)
+      compare(
+        grok.shell.tables.find(({name}) => name === t1.value).columns.byName(c1.value),
+        grok.shell.tables.find(({name}) => name === t2.value).columns.byName(chosenColumnName)
+      );
+  });
+
   c1.input.style.width = '150px';
   c2.input.style.width = '150px';
 
+  return ui.div([ui.label('Columns'), ui.divH([c1, c2])]);
+}
+
+//name: Compare Columns
+//tags: autostart
+export function compareColumns() {
+
+  let tables = grok.shell.tables;
+  let tablesNames = tables.map((t) => t.name);
+
+  let firstTableAdded = false,
+    secondTableAdded = false;
+
+  let t1 = ui.choiceInput('', '', tablesNames, () => {
+    firstTableAdded = true;
+    if (secondTableAdded) {
+      container.innerHTML = '';
+      container.append(addCols(t1, t2, container));
+    }
+  });
+
+  let t2 = ui.choiceInput('', '', tablesNames, () => {
+    secondTableAdded = true;
+    if (firstTableAdded) {
+      container.innerHTML = '';
+      container.append(addCols(t1, t2, container));
+    }
+  });
+
+  t1.input.style.width = '150px';
+  t2.input.style.width = '150px';
+
+  let container = ui.div();
   ui.dialog('Compare Columns')
     .add(
       ui.divV([
         ui.label('Tables'),
         ui.divH([t1, t2], {style: {marginBottom: '10px'}}),
-        ui.label('Columns'),
-        ui.divH([c1, c2])
+        container
       ])
     )
-    .add(ui.button('Compare', () => {
-      let tbls = grok.shell.tables[0];
-      let a = tbls.columns.byName(c1.value);
-      let b = tbls.columns.byName(c2.value);
-      tbls.columns.addNewBool('Result').init((i) => (a.get(i) === b.get(i)));
-    }))
     .show();
 }
