8c35bd241233d0f8f80659ff21372a0bc7a46e6d
Vadym Kovadlo <vadym.kovadlo@gmail.com>
2021-05-21 20:09:21 +0300

    PowerPack: rewrite Compare Columns to TypeScript

--- packages/PowerPack/src/compare-columns.ts
+++ packages/PowerPack/src/compare-columns.ts
@@ -5,44 +5,44 @@ import * as DG from 'datagrok-api/dg';
 
 function showOutputs(t1: { value: string; }, t2: { value: string; }, c1: DG.InputBase, c2: DG.InputBase, outputs: { innerHTML: string; append: (arg0: HTMLDivElement) => void; }) {
 
-  // @ts-ignore
-  let selection = grok.shell.tables.find(({name}) => name === t1.value).selection;
-  // @ts-ignore
-  let col1 = grok.shell.tables.find(({name}) => name === t1.value).columns.byName(c1.value);
-  // @ts-ignore
-  let col2 = grok.shell.tables.find(({name}) => name === t2.value).columns.byName(c2.value);
+  let selection = grok.shell.tables.find(({name}) => name === t1.value)?.selection;
+  let col1 = grok.shell.tables.find(({name}) => name === t1.value)?.columns.byName(c1.value);
+  let col2 = grok.shell.tables.find(({name}) => name === t2.value)?.columns.byName(c2.value);
 
   outputs.innerHTML = '';
 
-  if (col1.length === col2.length) {
+  if (col1?.length === col2?.length) {
 
-    let counter = 0;
+    let counter = 0; // @ts-ignore
     for (let i = 0; i < col1.length; i++)
-      if (col1.getString(i) == col2.getString(i))
-        counter++;
+      { // @ts-ignore
+        if (col1.getString(i) == col2.getString(i))
+          counter++;
+      }
 
     let link = ui.element('a');
-    link.textContent = ' Select';
-    link.onclick = (ev) => {selection.init((i) => col1.get(i) != col2.get(i));};
+    link.textContent = '             Select';
+    link.onclick = (ev) => {// @ts-ignore
+      selection?.init((i) => col1.get(i) != col2.get(i));
+    };
 
-    let str1: string = 'Mismatched: ' + (selection.length - counter).toString();
     // @ts-ignore
+    let str1: string = 'Mismatched: ' + (selection.length - counter).toString();
     outputs.append(
       ui.div([
         ui.divText('Matched: ' + counter.toString(), {}),
-        ui.divText(str1 + link)
-        //ui.buttonsInput([ui.span([{[str1]: link}])])
+        ui.divH([ui.divText(str1), link])
       ])
     );
 
   } else {
 
     outputs.append(
-        ui.divV([
-          ui.divText('Length mismatch: '),
-          ui.divText(t1.value + ': ' + col1.length + ' rows, '),
-          ui.divText(t2.value + ': ' + col2.length + ' rows')
-        ])
+      ui.divV([
+        ui.divText('Length mismatch: '),
+        ui.divText(t1.value + ': ' + col1?.length + ' rows, '),
+        ui.divText(t2.value + ': ' + col2?.length + ' rows')
+      ])
     );
   }
 }
@@ -50,7 +50,7 @@ function showOutputs(t1: { value: string; }, t2: { value: string; }, c1: DG.Inpu
 function addCols(t1: DG.InputBase, t2: DG.InputBase, outputs: HTMLDivElement) {
 
   let firstColumnAdded = false,
-      secondColumnAdded = false;
+    secondColumnAdded = false;
 
   // @ts-ignore
   let c1 = ui.choiceInput('Columns', '', grok.shell.tables.find(({name}) => name === t1.value).columns.names(), () => {
@@ -60,10 +60,10 @@ function addCols(t1: DG.InputBase, t2: DG.InputBase, outputs: HTMLDivElement) {
       showOutputs(t1, t2, c1, c2, outputs);
     } else { // @ts-ignore
       if (t1.value === t2.value && df.columns.length === 2) {
-            // @ts-ignore
-            let columnsNames = df.columns.names();
-            c2.value = (columnsNames.indexOf(c1.value) === 0) ? columnsNames[1] : columnsNames[0];
-          }
+        // @ts-ignore
+        let columnsNames = df.columns.names();
+        c2.value = (columnsNames.indexOf(c1.value) === 0) ? columnsNames[1] : columnsNames[0];
+      }
     }
   });
 
@@ -75,27 +75,24 @@ function addCols(t1: DG.InputBase, t2: DG.InputBase, outputs: HTMLDivElement) {
       showOutputs(t1, t2, c1, c2, outputs);
     } else { // @ts-ignore
       if (t1.value === t2.value && df.columns.length === 2) {
-            // @ts-ignore
-            let columnsNames = df.columns.names();
-            c1.value = (columnsNames.indexOf(c2.value) === 0) ? columnsNames[1] : columnsNames[0];
-          }
+        // @ts-ignore
+        let columnsNames = df.columns.names();
+        c1.value = (columnsNames.indexOf(c2.value) === 0) ? columnsNames[1] : columnsNames[0];
+      }
     }
   });
 
-  // @ts-ignore
-  return ui.divH([c1, c2]);
+  return ui.divH([c1.root, c2.root]);
 }
 
 //name: Compare Columns
 //tags: autostart
 export function compareColumns() {
 
-  grok.shell.info('hello, world! true');
-
   let tablesNames = grok.shell.tables.map((t) => t.name);
 
   let firstTableAdded = false,
-      secondTableAdded = false;
+    secondTableAdded = false;
 
   let t1 = ui.choiceInput('Tables', '', tablesNames, () => {
     firstTableAdded = true;
@@ -126,9 +123,7 @@ export function compareColumns() {
     outputs
   ], 'ui-form-aligned');
 
-  //inputSection.css('ui-form-aligned');
-
   ui.dialog('Compare Columns')
-      .add(inputSection)
-      .show();
+    .add(inputSection)
+    .show();
 }
