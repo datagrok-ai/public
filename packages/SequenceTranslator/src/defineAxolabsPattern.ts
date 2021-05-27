b5e58e178a906b4e22c2d8e4ba4145a58934ed79
Vadym Kovadlo <vadym.kovadlo@gmail.com>
2021-05-27 19:05:53 +0300

    GROK-8743 SequenceTranslator: update PTO and basis (WIP)

--- packages/SequenceTranslator/src/defineAxolabsPattern.ts
+++ packages/SequenceTranslator/src/defineAxolabsPattern.ts
@@ -15,6 +15,7 @@ SS/AS Modification - sections of dialog for changing base and PTO statuses of on
 const baseChoices: string[] = ["2'OMe RNA (mX)", "2'Fluoro RNA (fX)", "2'OMe U (mU)", "UNA", "GNA", "InvAb", "dX"];
 const defaultBase: string = "2'OMe RNA (mX)";
 const defaultPto: boolean = false;
+const defaultAvailability: boolean = true;
 const defaultSequenceLength: number = 23;
 
 //name: defineAxolabsPattern
@@ -22,16 +23,17 @@ const defaultSequenceLength: number = 23;
 //output: string result {semType: Axolabs}
 export function defineAxolabsPattern(nucleotides: string) {
 
-  function updateSsPattern(sequenceLength: number) {
+  function updateSsPattern() {
     ssPattern.innerHTML = '';
     const ss5 = ui.divText("SS-5'");
     ss5.style.marginTop = '26px';
     ssPattern.append(ss5);
-    for (let i = 0; i < sequenceLength; i++) {
+    for (let i = 0; i < sequenceLength.value; i++) {
+      ssAvailabilityStatuses[i] = ui.boolInput('', ssAvailabilityStatuses[i].value, (v: boolean) => grok.shell.info(v));
       ssPattern.append(
         ui.divV([
           ui.divText((i + 1).toString()),
-          ui.boolInput('', true, (v: boolean) => grok.shell.info(v)).root
+          ssAvailabilityStatuses[i]
         ])
       );
     }
@@ -40,15 +42,16 @@ export function defineAxolabsPattern(nucleotides: string) {
     ssPattern.append(three);
   }
 
-  function updateAsPattern(sequenceLength: number) {
+  function updateAsPattern() {
     asPattern.innerHTML = '';
     const as3 = ui.divText("AS-3'");
     as3.style.marginTop = '10px';
     asPattern.append(as3);
-    for (let i = 0; i < sequenceLength; i++) {
+    for (let i = 0; i < sequenceLength.value; i++) {
+      asAvailabilityStatuses[i] = ui.boolInput('', asAvailabilityStatuses[i].value, (v: boolean) => grok.shell.info(v));
       asPattern.append(
         ui.divV([
-          ui.boolInput('', true, (v: boolean) => grok.shell.info(v)).root,
+          asAvailabilityStatuses[i],
           ui.divText((i + 1).toString())
         ])
       );
@@ -58,56 +61,74 @@ export function defineAxolabsPattern(nucleotides: string) {
     asPattern.append(five);
   }
 
-  function updateModification(section: HTMLElement, sequenceLength: number, baseStatuses: string[], ptoStatuses: boolean[]) {
-    section.innerHTML = '';
-
-    for (let i = 0; i < sequenceLength; i++) {
-      section.append(
+  function updateAsModification() {
+    asModificationItems.innerHTML = '';
+    for (let i = 0; i < sequenceLength.value; i++) {
+      asPtoStatuses[i] = ui.boolInput('', asPtoStatuses[i].value);
+      asBaseStatuses[i] = ui.choiceInput('', asBaseStatuses[i].value, baseChoices);
+      asModificationItems.append(
         ui.divH([
           ui.block25([ui.label((i + 1).toString())])!,
-          ui.block50([ui.choiceInput('', baseStatuses[i], baseChoices, (v: string) => {
-            baseStatuses[i] = v
-          }).root])!,
-          ui.block25([
-            ui.div([
-              ui.boolInput('', ptoStatuses[i], (v: boolean) => {
-                $('.modif .ui-input-bool .ui-input-editor').on('click', function() {
-                  let ind = parseInt($(this).parent().parent().attr('class')!.slice(7));
-                  ptoStatuses[ind] = v
-                });
-              }).root
-            ], 'myinfo-' + (i + 1).toString())
-          ])!
+          ui.block50([asBaseStatuses[i]])!,
+          ui.block25([asPtoStatuses[i]])!
         ], {style: {alignItems: "center"}})
-      )
+      );
     }
   }
 
-  function updateUiForNewSequenceLength(sequenceLength: number) {
-    updateModification(asModification, sequenceLength, asBaseStatuses, asPtoStatuses);
-    updateModification(ssModification, sequenceLength, ssBaseStatuses, ssPtoStatuses);
-    updateSsPattern(sequenceLength);
-    updateAsPattern(sequenceLength);
+  function updateSsModification() {
+    ssModificationItems.innerHTML = '';
+    for (let i = 0; i < sequenceLength.value; i++) {
+      ssPtoStatuses[i] = ui.boolInput('', ssPtoStatuses[i].value);
+      ssBaseStatuses[i] = ui.choiceInput('', ssBaseStatuses[i].value, baseChoices);
+      ssModificationItems.append(
+        ui.divH([
+          ui.block25([ui.label((i + 1).toString())])!,
+          ui.block50([ssBaseStatuses[i]])!,
+          ui.block25([ssPtoStatuses[i]])!
+        ], {style: {alignItems: "center"}})
+      );
+    }
   }
 
-  let ssModification = ui.div([]),
-    asModification = ui.div(),
+  function updateUiForNewSequenceLength() {
+    updateSsModification();
+    updateAsModification();
+    updateSsPattern();
+    updateAsPattern();
+  }
+
+  function updatePtoAllAtOnce(newPtoValue: boolean) {
+    for (let i = 0; i < sequenceLength.value; i++) {
+      ssPtoStatuses[i].value = newPtoValue;
+      asPtoStatuses[i].value = newPtoValue;
+    }
+  }
+
+  function updateBasisAllAtOnce(newBasisValue: string) {
+    for (let i = 0; i < sequenceLength.value; i++) {
+      ssBaseStatuses[i].value = newBasisValue;
+      asBaseStatuses[i].value = newBasisValue;
+    }
+  }
+
+  let ssModificationItems = ui.div([]),
+    asModificationItems = ui.div([]),
     ssPattern = ui.divH([]),
     asPattern = ui.divH([]);
 
-  let ssBaseStatuses: string[] = Array(defaultSequenceLength).fill(defaultBase),
-    asBaseStatuses: string[] = Array(defaultSequenceLength).fill(defaultBase),
-    ssPtoStatuses: boolean[] = Array(defaultSequenceLength).fill(defaultPto),
-    asPtoStatuses: boolean[] = Array(defaultSequenceLength).fill(defaultPto);
+  let ssAvailabilityStatuses = Array(defaultSequenceLength).fill(ui.boolInput('', defaultAvailability)),
+    asAvailabilityStatuses = Array(defaultSequenceLength).fill(ui.boolInput('', defaultAvailability)),
+    ssBaseStatuses = Array(defaultSequenceLength).fill(ui.choiceInput('', defaultBase, baseChoices)),
+    asBaseStatuses = Array(defaultSequenceLength).fill(ui.choiceInput('', defaultBase, baseChoices)),
+    ssPtoStatuses = Array(defaultSequenceLength).fill(ui.boolInput('', defaultPto)),
+    asPtoStatuses = Array(defaultSequenceLength).fill(ui.boolInput('', defaultPto));
 
-  let sequenceLength = ui.intInput('Enter sequence length', defaultSequenceLength, (v: number) => updateUiForNewSequenceLength(v));
+  let sequenceLength = ui.intInput('Enter sequence length', defaultSequenceLength, () => updateUiForNewSequenceLength());
 
-  let sequenceBase = ui.choiceInput('Sequence basis', defaultBase, baseChoices, (v: string) => {
-    updateModification(asPattern, sequenceLength.value, asBaseStatuses, asPtoStatuses);
-    updateModification(ssPattern, sequenceLength.value, ssBaseStatuses, ssPtoStatuses);
-  });
+  let sequenceBase = ui.choiceInput('Sequence basis', defaultBase, baseChoices, (v: string) => {updateBasisAllAtOnce(v)});
 
-  let fullyPto = ui.boolInput('Fully PTO', defaultPto, (v: boolean) => {grok.shell.info('Coming soon')});
+  let fullyPto = ui.boolInput('Fully PTO', defaultPto, (v: boolean) => updatePtoAllAtOnce(v));
 
   let createAsStrand = ui.boolInput('Create AS strand', true, (v: boolean) => {
     asModificationSection.hidden = (!v);
@@ -117,7 +138,7 @@ export function defineAxolabsPattern(nucleotides: string) {
   let threeModification = ui.stringInput("Addidional 3' modification", "", (v: string) => grok.shell.info('Coming soon'));
   let fiveModification = ui.stringInput("Addidional 5' modification", "", (v: string) => grok.shell.info('Coming soon'));
 
-  updateUiForNewSequenceLength(defaultSequenceLength);
+  updateUiForNewSequenceLength();
 
   let patternDesignSection = ui.divV([
     ui.h1('Pattern Design'),
@@ -144,7 +165,7 @@ export function defineAxolabsPattern(nucleotides: string) {
       ui.block50([ui.divText('Modification')])!,
       ui.block25([ui.divText('PTO')])!
     ]),
-    ssModification,
+    ssModificationItems,
     threeModification.root,
     fiveModification.root
   ], {style: {marginLeft: "30px", width: "250px"}});
@@ -156,7 +177,7 @@ export function defineAxolabsPattern(nucleotides: string) {
       ui.block50([ui.divText('Modification')])!,
       ui.block25([ui.divText('PTO')])!
     ]),
-    asModification
+    asModificationItems
   ], {style: {marginLeft: "30px", width: "250px"}});
 
   ui.dialog('Axolabs')
@@ -169,4 +190,3 @@ export function defineAxolabsPattern(nucleotides: string) {
     )
     .showModal(true);
 }
-
