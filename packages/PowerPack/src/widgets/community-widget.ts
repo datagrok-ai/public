992bee1671b8714f1df255193c1255aa017b6fba
Andrew Skalkin <skalkin@gmail.com>
2021-05-28 17:09:40 -0400

    GROK-8757: Widget-based Welcome page - WIP

---
+++ packages/PowerPack/src/widgets/community-widget.ts
@@ -0,0 +1,21 @@
+/* Do not change these import lines. Datagrok will import API library in exactly the same manner */
+import * as grok from 'datagrok-api/grok';
+import * as ui from 'datagrok-api/ui';
+import * as DG from 'datagrok-api/dg';
+
+export class CommunityWidget extends DG.Widget {
+  caption: string = 'Community';
+
+  constructor() {
+    super(ui.div());
+
+    this.root.appendChild(ui.divText('Community Topics'));
+
+    grok.dapi.fetchProxy('https://community.datagrok.ai/latest.json')
+      .then((r) => r.json())
+      .then((json) => {
+        this.root.appendChild(ui.list(json.topic_list.topics.map((t: any) => t.title)));
+      });
+  }
+}
+
