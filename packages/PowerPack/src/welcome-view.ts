c3d0547c48ccaade05e76bf7e4b8823de77f7cd9
Andrew Skalkin <skalkin@gmail.com>
2021-05-28 20:33:54 -0400

    GROK-8757: Widget-based Welcome page - WIP

--- packages/PowerPack/src/welcome-view.ts
+++ packages/PowerPack/src/welcome-view.ts
@@ -2,11 +2,27 @@ import * as grok from 'datagrok-api/grok';
 import * as ui from 'datagrok-api/ui';
 import * as DG from 'datagrok-api/dg';
 
+function card(w: DG.Widget): HTMLElement {
+  let host = ui.divV([], 'power-pack-widget-host');
+
+  let header = ui.div([
+    ui.divText(w.props.caption ?? '', 'd4-dialog-title'),
+    ui.icons.settings(() => { grok.shell.o = w}, 'Edit settings'),
+    ui.icons.close(() => host.remove(), 'Remove'),
+  ], 'd4-dialog-header');
+
+  ui.tools.setHoverVisibility(host, Array.from(host.querySelectorAll('i')));
+
+  host.appendChild(header);
+  host.appendChild(ui.div([w.root], 'power-pack-widget-content'));
+  return host;
+}
+
 export function welcomeView() {
   let view = grok.shell.newView('Welcome');
 
   let widgetFunctions = DG.Func.find({returnType: 'widget'});
 
   for (let f of widgetFunctions)
-    f.apply().then((w: DG.Widget) => view.root.appendChild(w.root));
+    f.apply().then((w: DG.Widget) => view.root.appendChild(card(w)));
 }
--- packages/PowerPack/src/widgets/recent-projects-widget.ts
+++ packages/PowerPack/src/widgets/recent-projects-widget.ts
@@ -2,10 +2,13 @@ import * as DG from "datagrok-api/dg";
 import * as ui from "datagrok-api/ui";
 
 export class RecentProjectsWidget extends DG.Widget {
-  caption: string = 'Recent projects';
+  caption: string;
 
   constructor() {
     super(ui.div());
     this.root.appendChild(ui.divText('Recent projects'));
+
+    // properties
+    this.caption = super.addProperty('caption', DG.TYPE.STRING, 'Recent projects');
   }
 }
