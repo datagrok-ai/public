c6bd05de91a9d0e694ccb2301954160033531c58
dskatov <dskatov@datagrok.ai>
2021-05-31 19:32:00 +0300

    GROK-8788 Packages: Curation, Beta

--- packages/UsageAnalysis/queries/info-panels.sql
+++
@@ -1,9 +0,0 @@
---name: userById
---tags: panel
---render: RowToTable
---input: string id { semType: user_id }
---connection: datagrok
-select u.first_name, u.last_name from users u
-where id = @id
---end
-
