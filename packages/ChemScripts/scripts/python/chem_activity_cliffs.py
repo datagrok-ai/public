fa21a4df61c4ac40841e0bc1b02f96562a5dc214
StLeonid <lstolbov@datagrok.ai>
2021-06-07 17:37:57 +0300

    JS wrap for activity cliffs script

--- packages/ChemScripts/scripts/python/chem_activity_cliffs.py
+++ packages/ChemScripts/scripts/python/chem_activity_cliffs.py
@@ -9,11 +9,12 @@
 import numpy as np
 import math
 from rdkit import Chem
+import random
 from rdkit.Chem import AllChem
 from rdkit import DataStructs
 from sklearn.manifold import TSNE
 
-similarityValue = 50
+similarityValue = 97
 automaticSimilarityLimit = False
 components = 3
 
@@ -33,7 +34,6 @@ length = len(smiles)
 mols = np.full(length, None, dtype=object)
 accepted = []
 fingerprints = []
-npfingerprints = []
 
 for i in range(0, length):
     mol = Chem.MolFromSmiles(smiles[i], sanitize=True)
@@ -44,15 +44,11 @@ for i in range(0, length):
 
 for i in accepted:
     fingerprints.append(Chem.RDKFingerprint(mols[i]))
-    arr = np.zeros((0,))
-    fp = AllChem.GetMorganFingerprintAsBitVect(mols[i], 2)
-    DataStructs.ConvertToNumpyArray(fp, arr)
-    npfingerprints.append(arr)
 
-output_pairs = pd.DataFrame(columns=['n1', 'n2', "sim", "difference", "sali"])
+pairs = []
 maxSALI = 0
 realLength = len(accepted)
-
+pairs_count = 0
 for i in range(0, realLength - 1):
     for j in range(0, realLength - i - 1):
         sim = DataStructs.TanimotoSimilarity(fingerprints[accepted[i]], fingerprints[accepted[j + i + 1]])
@@ -64,11 +60,14 @@ for i in range(0, realLength - 1):
         else:
             saliVal = math.inf
             activityDif = 0
-        output_pairs.loc[i] = [np.int16(i), np.int16(j + i + 1), sim, activityDif, saliVal]
+        pair = (i, j + i + 1, sim, activityDif, saliVal)
+        pairs.append(pair)
 
 optSimilarityLimit = initialSimilarityLimit
 
-output_pairs.loc[output_pairs['sim'] > optSimilarityLimit]
+output_pairs = pd.DataFrame(pairs, columns=['n1', 'n2', "sim", "difference", "sali"])
+output_pairs = output_pairs.loc[output_pairs['sim'] > optSimilarityLimit]
+output_pairs = output_pairs.reset_index(drop=True)
 
 neighboursCount = [0] * length
 similarityCount = [0] * length
@@ -81,55 +80,11 @@ for i in range(0, len(output_pairs.index)):
     similarityCount[output_pairs.loc[i, "n1"].astype(int)] += output_pairs.loc[i, "sim"]
     similarityCount[output_pairs.loc[i, "n2"].astype(int)] += output_pairs.loc[i, "sim"]
 
-notAssigned = list(range(0, length))
+mx = []
+my = []
 
+for i in range(0, length):
+   mx.append(random.random())
+   my.append(random.random())
 
-def extract_sub_graph(node, used, pairs):
-    if node in used:
-        return list()
-
-    neighbours = []
-
-    for i in range(0, len(pairs.index)):
-        if node == pairs.loc[i, "n1"].astype(int):
-            neighbours.append(pairs.loc[i, "n2"].astype(int))
-        elif node == pairs.loc[i, "n2"].astype(int):
-            neighbours.append(pairs.loc[i, "n1"].astype(int))
-
-    graph_nodes = [node]
-    used.append(node)
-
-    for i in range(0, len(neighbours)):
-        new_nodes = extract_sub_graph(neighbours[i], used, pairs)
-        graph_nodes += new_nodes
-        used += new_nodes
-
-    return graph_nodes
-
-
-while len(notAssigned) > 0:
-    graph = extract_sub_graph(notAssigned[0], list(), output_pairs)
-    if len(graph) == 1:
-        separatedGraphs[0] += graph
-    else:
-        separatedGraphs.append(graph)
-
-    notAssigned = list(set(notAssigned) - set(graph))
-
-for i in range(0, len(separatedGraphs)):
-    for j in separatedGraphs[i]:
-        groupNumbers[j] = i
-
-
-def tanimoto_dist(a, b):
-    dotprod = np.dot(a, b)
-    tc = dotprod / (np.sum(a) + np.sum(b) - dotprod)
-    return 1.0 - tc
-
-
-tsne = TSNE(n_components=components, metric=tanimoto_dist)
-tsne_X = tsne.fit_transform(npfingerprints)
-x = tsne_X.T[0]
-y = tsne_X.T[1]
-
-output_coords = pd.DataFrame({'graph': groupNumbers, "x_coord": x, "y_coord": y})
+output_coords = pd.DataFrame({"smiles": smiles, "activities":activities, "x_coord": mx, "y_coord": my})
