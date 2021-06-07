f35825fa738d6470a55a991be68ff85fd3091ab4
kdoncov <kdoncov@datagrok.ai>
2021-06-07 16:41:20 +0300

    GROK-8702: UsageAnalysis harmonization

--- packages/UsageAnalysisUi/queries/info-panels.sql
+++ packages/UsageAnalysisUi/queries/info-panels.sql
@@ -2,7 +2,7 @@
 --tags: panel
 --render: RowToTable
 --input: string id { semType: user_id }
---connection: datagrok
+--connection: System:Datagrok
 select u.first_name, u.last_name from users u
 where id = @id
 --end
