f57f63316978faecfa52f8f3eefed9256201b6e3
Dmitry <illarionow@yahoo.com>
2021-06-11 11:47:57 +0200

    Create markdown.js
    Markdown JS API Example

---
+++ packages/ApiSamples/scripts/ui/components/markdown.js
@@ -0,0 +1,15 @@
+// Render markdown 
+
+let document = `# Intro
+Datagrok unlocks the value of the complex data by empowering non-technical users to: \n 
+  * [Discover](/help/discover/fair). 
+  * [Cleanse](/help/transform/data-wrangling). 
+  * [Visualize](/help/visualize/viewers).
+  * [Explore data](/help/#explore).
+  * [Build and deploy predictive models](/help/learn/predictive-modeling).
+  *	[Collaborate with others](/help/collaborate/sharing).
+`;
+
+grok.shell.newView('Markdown example',[
+  ui.markdown(document)
+]);
