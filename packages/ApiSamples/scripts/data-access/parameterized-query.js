36a43a3331d8feb15f105c4945510c594eb369af
Andrew Skalkin <skalkin@gmail.com>
2021-06-17 17:58:56 -0400

    Adjusted demo query name to 'System:CoffeeCompany:StoresInState'

--- packages/ApiSamples/scripts/data-access/parameterized-query.js
+++ packages/ApiSamples/scripts/data-access/parameterized-query.js
@@ -2,5 +2,5 @@
 //help-url: https://datagrok.ai/help/access/parameterized-queries
 // An example of using parameterized query
 
-grok.data.query('Demo:CoffeeCompany:StoresInState', {'state': 'NY'})
+grok.data.query('System:CoffeeCompany:StoresInState', {'state': 'NY'})
   .then(t => grok.shell.info('Stores: ' + t.rowCount));
