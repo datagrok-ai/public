4556e80829d4409c2b9255dbb430f3cc6e17ff54
Dmitry <illarionow@yahoo.com>
2021-06-22 14:20:42 +0200

    Layout templates for JS API Examples

---
+++ packages/ApiSamples/scripts/ui/layouts/templates/Gallery grid.js
@@ -0,0 +1,18 @@
+//Layout: Gallery grid
+
+let windows = grok.shell.windows;
+windows.showToolbox = false; //*hide tool box
+windows.showProperties = false; //*hide property panel
+
+let gallery = ui.divH([],'grok-gallery-grid');
+for (let i =0; i<40; i++){
+  gallery.append(ui.card(ui.p('item-'+i.toString())))
+}
+
+let view = grok.shell.newView('Gallery grid', [
+  gallery
+],'layout');
+
+view.box = true;
+
+$('.layout div').css('border','1px dashed var(--grey-2)');
---
+++ packages/ApiSamples/scripts/ui/layouts/templates/Splitter (1 Column 2 Rows).js
@@ -0,0 +1,19 @@
+//Layout: Splitter 1 Column 2 Row
+
+let windows = grok.shell.windows;
+windows.showToolbox = false; //*hide tool box
+windows.showProperties = false; //*hide property panel
+
+let view = grok.shell.newView('1 Column - 2 Rows Splitter', [
+    ui.splitH([
+      ui.panel('Column 2'),
+      ui.splitV([
+        ui.panel('Row 1'),
+        ui.panel('Row 2')
+      ])
+    ])
+  ],'layout');
+  
+  view.box = true;
+  
+$('.layout .ui-div').css('border','1px dashed var(--grey-2)');
