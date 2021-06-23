3c0845316f0f4cc19b09b3dec46a84f69f1a1ebc
Vadym Kovadlo <vadym.kovadlo@gmail.com>
2021-06-23 21:39:23 +0300

    GROK-8941 OligoBatchCalculator: calculate optical density(OD), nmole and mass from Yield Amount & Units
    - take raw sequences as input

--- packages/OligoBatchCalculator/src/package.ts
+++ packages/OligoBatchCalculator/src/package.ts
@@ -6,29 +6,66 @@ import * as DG from 'datagrok-api/dg';
 export let _package = new DG.Package();
 
 function calculateMolecularWeight(sequence: string): number {
+  const step = (/^[AUGC]/g.test(sequence)) ? 1 : 2;
   let molecularWeight = 0;
-  for (let i = 0; i < sequence.length; i += 2)
-    molecularWeight += weights[sequence.slice(i, i + 2)];
+  for (let i = 0; i < sequence.length; i += step)
+    molecularWeight += weights[sequence.slice(i, i + step)];
   return (sequence.length > 0) ? molecularWeight - 61.97 : 0;
 }
 
-function calculateNMole(molecularWeights: number[], extinctionCoefficients: number[], amount: number, units: string) {
+function calculateNMole(molecularWeights: number[], extinctionCoefficients: number[], amount: number, units: string): number[] {
   let nmoles = Array(molecularWeights.length);
-  const coefficient = (units == 'mg') ? 1 : 1000;
+  if (units == 'nmole' || units == 'µmole') return nmoles.fill(amount);
+  if (units == 'OD') {
+    for (let i = 0; i < molecularWeights.length; i++)
+      nmoles[i] = amount * 1000000 / extinctionCoefficients[i];
+    return (molecularWeights[0] > 0) ? nmoles : Array(molecularWeights.length).fill(0);
+  }
+  const coefficient = units == 'mg' ? 1000000 : 1000000000;
   for (let i = 0; i < molecularWeights.length; i++)
-    nmoles[i] = coefficient / molecularWeights[i];
+    nmoles[i] = amount * coefficient / molecularWeights[i]
   return (molecularWeights[0] > 0) ? nmoles : Array(molecularWeights.length).fill(0);
 }
 
+function calculateMass(extinctionCoefficients: number[], molecularWeights: number[], nmoles: number[], amount: number, units: string) {
+  let mass = Array(molecularWeights.length);
+  if (units == 'mg' || units == 'µg') return mass.fill(amount);
+  if (units == 'OD') {
+    for (let i = 0; i < molecularWeights.length; i++)
+      mass[i] = amount / extinctionCoefficients[i] * molecularWeights[i];
+    return (molecularWeights[0] > 0) ? mass : Array(molecularWeights.length).fill(0);
+  }
+  const coefficient = units == 'mg' ? 1 : 1000;
+  for (let i = 0; i < extinctionCoefficients.length; i++)
+    mass[i] = amount / extinctionCoefficients[i] * molecularWeights[i] * coefficient;
+  return mass;
+}
+
+function calculateOd(extinctionCoefficients: number[], molecularWeights: number[], nmoles: number[], amount: number, units: string) {
+  let od = Array(molecularWeights.length);
+  if (units == 'OD') return od.fill(amount);
+  if (units == 'mg' || units == 'µg') {
+    for (let i = 0; i < extinctionCoefficients.length; i++)
+      od[i] = amount * extinctionCoefficients[i] / molecularWeights[i];
+    return od;
+  }
+  const coefficient = units == 'mg' ? 1 : 1000;
+  for (let i = 0; i < extinctionCoefficients.length; i++)
+    od[i] = amount * extinctionCoefficients[i] / coefficient;
+  return od;
+}
+
 function normalizeSequence(sequence: string): string {
-  const obj: {[index: string]: string} = {
-    "fU": "rU", "fA": "rA", "fC": "rC", "fG": "rG", "mU": "rU", "mA": "rA", "mC": "rC", "mG": "rG", "ps": ""
-  };
-  return sequence.replace(/(fU|fA|fC|fG|mU|mA|mC|mG|ps)/g, function (x: string) {return obj[x];});
+  const isRna = (/^[AUGC]/g.test(sequence));
+  const obj: {[index: string]: string} = isRna ?
+    {"A": "rA", "U": "rU", "G": "rG", "C": "rC"} :
+    {"fU": "rU", "fA": "rA", "fC": "rC", "fG": "rG", "mU": "rU", "mA": "rA", "mC": "rC", "mG": "rG", "ps": ""};
+  if (isRna) return sequence.replace(/[AUGC]/g, function (x) {return obj[x]});
+  return sequence.replace(/(fU|fA|fC|fG|mU|mA|mC|mG|ps)/g, function (x) {return obj[x]});
 }
 
 function prepareInputTextField(text: string) {
-  return text.replace(/\s/g, '').split('\n');
+  return text.split('\n');
 }
 
 const weights: {[index: string]: number} = {
@@ -40,7 +77,11 @@ const weights: {[index: string]: number} = {
   "mA":	343.24,
   "mC":	319.21,
   "mG":	359.24,
-  "ps":	16.07
+  "ps":	16.07,
+  "A": 313.21,
+  "U": 306.17,
+  "G": 329.21,
+  "C": 289.18
 };
 
 //name: OligoBatchCalculator
@@ -71,7 +112,7 @@ export function OligoBatchCalculator() {
     'rG': {'rA': 25200, 'rC': 17400, 'rG': 21600, 'rU': 21200},
     'rU': {'rA': 24600, 'rC': 17200, 'rG': 20000, 'rU': 19600}
   },
-  defaultInputSequence = 'fAmCmGmAmCpsmU';
+  defaultInput = 'fAmCmGmAmCpsmU\nmApsmApsfGmAmUmCfGfAfC\nmAmUfGmGmUmCmAfAmGmA';
 
   function getExtinctionCoefficientUsingNearestNeighborMethod(sequence: string) {
     sequence = normalizeSequence(sequence);
@@ -83,22 +124,27 @@ export function OligoBatchCalculator() {
     return ec1 - ec2;
   }
 
-  function updateTable(sequences: string[]) {
+  function updateTable(text: string) {
+    let sequences = prepareInputTextField(text);
     tableDiv.innerHTML = '';
-    let od260 = Array(sequences.length).fill(yieldAmount.value);
-    let mass = Array(sequences.length).fill(30);
     let molecularWeights = sequences.map((s) => calculateMolecularWeight(s));
     let extinctionCoefficients = sequences.map((s) => getExtinctionCoefficientUsingNearestNeighborMethod(s));
     let nMole = calculateNMole(molecularWeights, extinctionCoefficients, yieldAmount.value, units.value);
+    let mass = calculateMass(extinctionCoefficients, molecularWeights, nMole, yieldAmount.value, units.value);
+    let od260 = calculateOd(extinctionCoefficients, molecularWeights, nMole, yieldAmount.value, units.value);
+
+    let moleName = (units.value == 'µmole') ? 'µmole' : 'nmole';
+    let massName = (units.value == 'µg') ? units.value : 'mg';
+
     table = DG.DataFrame.fromColumns([
       DG.Column.fromList('int', 'Item#', [...new Int16Array(sequences.length + 1).keys()].slice(1)),
       DG.Column.fromList('string', 'Sequence', sequences),
       DG.Column.fromList('int', 'Length', sequences.map((s) => s.length)),
       DG.Column.fromList('int', 'OD-260', od260),
-      DG.Column.fromList('double', 'nmole', nMole),
-      DG.Column.fromList('double', 'Mass', mass),
-      DG.Column.fromList('double', 'nmole/OD', nMole.map(function(n, i) {return n / od260[i];})),
-      DG.Column.fromList('double', 'μg/OD', mass.map(function(n, i) {return n / od260[i];})),
+      DG.Column.fromList('double', moleName, nMole),
+      DG.Column.fromList('double', 'Mass (' + massName + ')', mass),
+      DG.Column.fromList('double', moleName + '/OD', nMole.map(function(n, i) {return n / od260[i]})),
+      DG.Column.fromList('double', massName + '/OD', mass.map(function(n, i) {return n / od260[i]})),
       DG.Column.fromList('double', 'MW', molecularWeights),
       DG.Column.fromList('int', 'Ext. Coefficient', extinctionCoefficients),
     ]);
@@ -110,7 +156,7 @@ export function OligoBatchCalculator() {
   windows.showToolbox = false;
   windows.showHelp = false;
 
-  let text1 = ui.divText('Enter Yield Amount & Units');
+  let text1 = ui.h1('Yield Amount & Units');
   let text2 = ui.divText('Search Modifications');
 
   let yieldAmount = ui.floatInput('', 1, () => updateTable(inputSequenceField.value));
@@ -120,20 +166,20 @@ export function OligoBatchCalculator() {
   });
   let analyzeAsSingleStrand = ui.button('Analyze As Single Strand', () => grok.shell.info('Coming soon'));
   let analyzeAsDuplexStrand = ui.button('Analyze As Duplex Strand', () => grok.shell.info('Coming soon'));
-  let clearSequences = ui.button('Clear Sequences', () => inputSequenceField.value = '');
+  let clearSequences = ui.button('CLEAR', () => inputSequenceField.value = '');
   let addModifications = ui.button('Add Modifications', () => grok.shell.info('Coming soon'));
   let threeMod = ui.boolInput("3' MOD", false);
   let internal = ui.boolInput('INTERNAL', false);
   let fiveMod = ui.boolInput("5' MOD", false);
   let table = DG.DataFrame.create();
-  let inputSequenceField = ui.textInput("", defaultInputSequence, async (txt: string) => {
-    updateTable(prepareInputTextField(txt));
-  });
+  let inputSequenceField = ui.textInput("", defaultInput, async (txt: string) => updateTable(txt));
 
   let tableDiv = ui.block([]);
-  updateTable([defaultInputSequence]);
+  updateTable(defaultInput);
 
-  grok.shell.newView('Sequence Translator', [
+  let loadFileButton = ui.button('LOAD AS CSV', () => {grok.shell.info('Coming soon')});
+
+  grok.shell.newView('Oligo Batch Calculator', [
     ui.divH([
       ui.divV([
         text1,
@@ -161,9 +207,8 @@ export function OligoBatchCalculator() {
           inputSequenceField.root
         ],'input-base')
       ], 'sequenceInput'),
-
     ]),
-    ui.button('Load Data To Excel', () => {}),
+    loadFileButton,
     tableDiv
   ]);
 
