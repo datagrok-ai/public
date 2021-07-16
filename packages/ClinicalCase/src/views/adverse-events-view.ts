16a1f73f3f289a337e3dfee172215dc229954416
skalkin <skalkin@gmail.com>
2021-07-16 00:36:37 -0400

    GROK-9199: Clinical Case: Adverse Events: show events preceding the adverse event

--- packages/ClinicalCase/src/views/adverse-events-view.ts
+++ packages/ClinicalCase/src/views/adverse-events-view.ts
@@ -1,6 +1,7 @@
+import * as grok from 'datagrok-api/grok';
 import * as DG from "datagrok-api/dg";
 import * as ui from "datagrok-api/ui";
-import { study } from "../clinical-study";
+import { study, ClinRow } from "../clinical-study";
 
 export class AdverseEventsView extends DG.ViewBase {
 
@@ -36,6 +37,9 @@ export class AdverseEventsView extends DG.ViewBase {
     }).root;
 
     let grid = study.domains.ae.plot.grid();
+    study.domains.ae.onCurrentRowChanged.subscribe((_) => {
+      grok.shell.o = new ClinRow(study.domains.ae.currentRow);
+    });
 
     this.root.appendChild(ui.div([
       ui.divH([
