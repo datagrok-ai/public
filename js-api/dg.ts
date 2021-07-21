3f8051da5c471780483704ef9daefc1a22d8bb77
Alex Paramonov <aparamonov@datagrok.ai>
2021-07-21 11:44:10 +0300

    GROK-9246 TS: Add DayJs

--- js-api/dg.ts
+++ js-api/dg.ts
@@ -2,6 +2,7 @@ import * as _chem from './src/chem';
 import * as _ml from './src/ml';
 import * as _utils from './src/utils';
 import $ from 'cash-dom';
+import dayjs from 'dayjs';
 
 export * from './src/const';
 export * from './src/events';
@@ -26,4 +27,5 @@ export let utils = _utils;
 
 $(function () {
   (<any>window).$ = $;
+  (<any>window).dayjs = dayjs;
 });
