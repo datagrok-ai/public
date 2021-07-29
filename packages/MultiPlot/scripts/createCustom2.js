60e20ebc228b4687c0f12686e5159ac3cec48747
rudimvik <rudimvik@gmail.com>
2021-07-30 02:10:01 +0300

    GROK-9337: MultiPlot: multiple tables

--- packages/MultiPlot/scripts/createCustom2.js
+++ packages/MultiPlot/scripts/createCustom2.js
@@ -3,9 +3,10 @@
 //language: javascript
 
 let options = {
+    id: '01-701-1015',
     series: [
         {
-            tableName: 'ae__2__lb__2_',
+            tableName: 'lb2',
             title: 'outside title 0',
             type: 'scatter',
             x: 'LBDY',
@@ -16,19 +17,20 @@ let options = {
             show: 1,
             visualMap: {
                 type: 'piecewise',
+                column: 'LBSEQ',
                 pieces: [
                     { min: 20, max: 250, color: ['red'] },
                 ],
                 dimension: 2,
             },
         },
-      
+
         // timeLines
         {
             tableName: 'ae__2__lb__2_',
             title: 'outside title 1',
             type: 'timeLine',
-            y: 'LBTEST',
+            y: 'AETERM',
             x: ['AESTDY', 'AEENDY'],
             yType: 'category',
             color: 'red',
@@ -36,38 +38,80 @@ let options = {
             height: '2flex',
             show: 1,
         },
+
         // linechart with filter 
         {
-          tableName: 'ae__2__lb__2_',
-          title: 'outside title 0',
-          type: 'scatter',
-          x: 'AESTDY',
-          y: 'LBTEST',
-          condition: {
-            field: 'USUBJID',
-            value: '01-701-1023'
-          },
-          yType: 'category',
-          markerShape: 'square',
-          height: '1flex',
-          show: 1,
+            tableName: 'lb2',
+            title: 'outside title 0',
+            type: 'line',
+            x: 'LBDY',
+            y: 'LBSTRESN',
+            condition2: {
+                field: 'USUBJID',
+                value: '01-701-1023'
+            },
+            yType: 'value',
+            markerShape: 'square',
+            height: '1flex',
+            show: 1,
+        },
+        {
+            tableName: 'lb2',
+            //title: 'outside title 0',
+            type: 'line',
+            x: 'LBDY',
+            y: 'LBSEQ',
+            condition2: {
+                field: 'USUBJID',
+                value: '01-701-1023'
+            },
+            yType: 'value',
+            markerShape: 'square',
+            height: '1flex',
+            show: 1,
         }
     ]
 }
 
+let myId = '01-701-1146'
+myId = '01-701-1015'
 async function func1() {
-    let tables = grok.shell.tables;
-    for (table of tables) {
-        if (table.name == 'ae__2__lb__2_') {
-            let view = grok.shell.addTableView(table);
-            let viewer = await table.plot.fromType('MultiPlot', {
-                paramA: "string outside",
-                paramOptions: JSON.stringify(options),
-                "Param A": "string outside2"
-            })
-            view.addViewer(viewer);
-        }
+    let tablesIter = grok.shell.tables;
+    let tables = {}
+    for (table of tablesIter) {
+        tables[table.name] = table;
     }
+    for (table of tablesIter) {
+        console.log('Table found: ', table.name);
+        if (table.name == 'ae__2__lb__2_') {
+            table.filter.init(e => {
+                let row = table.row(e);
+                return row["USUBJID"] === myId;
+            })
+        }
+        if (table.name == 'ae2') {
+            table.filter.init(e => {
+                let row = table.row(e);
+                return row['USUBJID'] === myId;
+            })
+        } // ae2
+        if (table.name == 'lb2') {
+            table.filter.init(e => {
+                let row = table.row(e);
+                return row['USUBJID'] === myId;
+            })
+        } // lb2
+    } // tables
+
+    let startTable = tables['lb2']
+    console.log(startTable)
+    let view = grok.shell.addTableView(startTable);
+    let viewer = await startTable.plot.fromType('MultiPlot', {
+        paramA: "string outside",
+        paramOptions: JSON.stringify(options),
+        "Param A": "string outside2"
+    })
+    view.addViewer(viewer);
 }
 
 func1();
