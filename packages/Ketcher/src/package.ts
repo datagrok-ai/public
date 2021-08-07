ff3320ff24fda9cc121362e0a31b46a8911f3cf6
skalkin <skalkin@gmail.com>
2021-08-07 15:34:13 -0400

    #49: Ketcher - work in progress

--- packages/Ketcher/src/package.ts
+++ packages/Ketcher/src/package.ts
@@ -6,6 +6,7 @@ import { Editor } from "ketcher-react";
 import { RemoteStructServiceProvider } from 'ketcher-core'
 import React from 'react';
 import ReactDOM from 'react-dom';
+import buildKetcherAsync from "ketcher-react/dist/script";
 
 export let _package = new DG.Package();
 
@@ -15,27 +16,28 @@ const structServiceProvider = new RemoteStructServiceProvider(
 )
 
 //name: ketch
-export function ketch() {
-
+export async function ketch() {
 
   let props = {
     //staticResourcesUrl: process.env.PUBLIC_URL,
-    staticResourcesUrl: _package.webRoot,
+    staticResourcesUrl: _package.webRoot!,
     structServiceProvider: structServiceProvider
   };
 
+  // Andrew: this works but I don't know how to set smiles, or receive events
   let host = ui.div([], { style: {height: '500px', width: '500px'}});
-  // @ts-ignore
   let component = React.createElement(Editor, props, null);
   ReactDOM.render(component, host);
+
+  // Andrew: this seems to be what we need but I can't get it to build
+  // let ketcher = await buildKetcherAsync({
+  //   element: host,
+  //   structServiceProvider: structServiceProvider,
+  //   staticResourcesUrl: _package.webRoot!
+  // });
+  //ketcher.setMolecule('Fc1cc(Cl)ccc1Br');
 
   ui.dialog()
     .add(host)
     .show();
 }
-
-// function createSketcher() {
-//   return <Editor
-//     staticResourcesUrl={_package.webRoot!}
-//     structServiceProvider={structServiceProvider}/>;
-// }
