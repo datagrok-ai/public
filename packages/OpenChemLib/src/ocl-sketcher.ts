a619f5677ab55b07203b449acef3b107ac4ffb52
skalkin <skalkin@gmail.com>
2021-08-10 19:38:30 -0400

    #49, #51: Ketcher and OpenChemLib sketchers: first working version

--- packages/OpenChemLib/src/ocl-sketcher.ts
+++ packages/OpenChemLib/src/ocl-sketcher.ts
@@ -6,24 +6,26 @@ import * as OCL from 'openchemlib/full.js';
 let sketcherId = 0;
 
 export class OpenChemLibSketcher extends DG.chem.SketcherBase {
-  _sketcher: any;
+  // @ts-ignore
+  _sketcher: OCL.StructureEditor;
 
   constructor() {
     super();
-
-    let id = `ocl-sketcher-${sketcherId++}`;
-    let host = ui.div([], {
-      id: id,
-      style: {width: '600px', height: '600px'}
-    });
-    this.root.appendChild(host);
-
-    let that = this;
-    setTimeout(function() {
-      that._sketcher = OCL.StructureEditor.createSVGEditor(id, 1);
-      that._sketcher.setChangeListenerCallback(function() {
-        that.onChanged.next(null);
-      });
-    }, 100);
   }
+
+  async init() {
+    let id = `ocl-sketcher-${sketcherId++}`;
+    this.root.id = id;
+
+    this._sketcher = OCL.StructureEditor.createSVGEditor(id, 1);
+    this._sketcher.setChangeListenerCallback((id, molecule) => {
+      this.onChanged.next(null);
+    });
+  }
+
+  get smiles() { return this._sketcher.getSmiles(); }
+  set smiles(s) { this._sketcher.setSmiles(s); }
+
+  get molFile() { return this._sketcher.getMolFile(); }
+  set molFile(s) { this._sketcher.setMolFile(s); }
 }
