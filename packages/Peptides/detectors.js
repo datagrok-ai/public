ea0b9c13b8aea2783d52e9a080e487c87e9b488a
dskatov <dskatov@datagrok.ai>
2021-08-11 14:16:57 +0300

    Packages: JavaScript formatting

--- packages/Peptides/detectors.js
+++ packages/Peptides/detectors.js
@@ -1,20 +1,23 @@
 class PeptidesPackageDetectors extends DG.Package {
 
-    //tags: semTypeDetector
-    //input: column col
-    //output: string semType
-    detectNucleotides(col) {
-        if(col.type === DG.TYPE.STRING){
-        for (let i = 0; i != col.categories.length; i++){
-            var pepRgx = new RegExp("(G|L|Y|S|E|Q|D|N|F|A|K|R|H|C|V|P|W|I|M|T|(\[(G|L|Y|S|E|Q|D|N|F|A|K|R|H|C|V|P|W|I|M|T).+\])){3,30}").test(col.categories[i]);
-            
-            if (!pepRgx)
-                return null;
-        }
+  //tags: semTypeDetector
+  //input: column col
+  //output: string semType
+  detectNucleotides(col) {
 
-        col.semType = 'peptide';
-        return(col.semType);
-        } 
-        return null;   
+    if (col.type === DG.TYPE.STRING) {
+      for (let i = 0; i != col.categories.length; i++) {
+        var pepRgx = new RegExp(
+          "(G|L|Y|S|E|Q|D|N|F|A|K|R|H|C|V|P|W|I|M|T|(\[(G|L|Y|S|E|Q|D|N|F|A|K|R|H|C|V|P|W|I|M|T).+\])){3,30}")
+          .test(col.categories[i]);
+        if (!pepRgx)
+          return null;
+      }
+      col.semType = 'peptide';
+      return (col.semType);
     }
+
+    return null;
+
+  }
 }
