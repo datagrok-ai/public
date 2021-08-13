b15a0206e814594f9f8cbc8e585d8d3eb8786ad8
onuf <56073962+onuf@users.noreply.github.com>
2021-08-13 18:22:58 +0300

    #53 Tutorials package
    initial commit

---
+++ packages/Tutorials/src/package.ts
@@ -0,0 +1,24 @@
+import * as grok from 'datagrok-api/grok';
+import * as ui from 'datagrok-api/ui';
+import * as DG from 'datagrok-api/dg';
+import { Track } from './tutorial';
+
+export const _package = new DG.Package();
+
+//name: Tutorials
+//tags: app
+export async function trackOverview() {
+  const chem = new Track('Cheminformatics');
+  const eda = new Track('Exploratory Data Analysis');
+  const ml = new Track('Machine Learning');
+  const tracks = DG.DataFrame.fromColumns([
+    DG.Column.fromList('string', 'track', [chem, eda, ml].map((t) => t.name)),
+    DG.Column.fromList('int', 'start', [10, 1, 15]),
+    DG.Column.fromList('int', 'end', [29, 29, 29]),
+  ]);
+  const timelines = await tracks.plot.fromType('TimelinesViewer', {
+    showZoomSliders: false,
+    lineWidth: 20,
+  });
+  grok.shell.newView('Tracks', [timelines.root]);
+}
