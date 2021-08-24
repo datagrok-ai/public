e5bc6bfecd7039d765f4209c4b77258659b565d3
Maria Dolotova <mdoloto1@its.jnj.com>
2021-08-24 09:43:05 +0300

    added function for creation of AE risk assessment dataframe(relative risk, risk difference, odds ratio)

--- packages/ClinicalCase/src/data-preparation/data-preparation.ts
+++ packages/ClinicalCase/src/data-preparation/data-preparation.ts
@@ -80,3 +80,86 @@ export function createKaplanMeierDataframe(){
 }
 
 
+export function createUniqueCountDataframe(df: DG.DataFrame, groupCol: string[], uniqueCountCol: string, renameCol: string){
+  let grouped = df.groupBy(groupCol)
+    .uniqueCount(uniqueCountCol)
+    .aggregate();
+    grouped.getCol(`unique(${uniqueCountCol})`).name = renameCol;
+    return grouped;
+}
+
+
+export function createAERiskAssessmentDataframe(dm: DG.DataFrame, ae: DG.DataFrame, ex: DG.DataFrame) {
+
+  let subjArm = createUniqueCountDataframe(ex, [ 'EXTRT' ], 'USUBJID', 'TOTALSUBJ');
+  let joinedAeEX = grok.data.joinTables(ae, ex, [ 'USUBJID' ], [ 'USUBJID' ], [ 'USUBJID', 'AETERM' ], [ 'EXTRT' ], DG.JOIN_TYPE.LEFT, false);
+  let subjAE = createUniqueCountDataframe(joinedAeEX, [ 'AETERM', 'EXTRT' ], 'USUBJID', 'AECOUNT');
+
+  let tj = grok.data.joinTables(subjAE, subjArm, [ 'EXTRT' ], [ 'EXTRT' ], [ 'AETERM', 'EXTRT', 'AECOUNT' ], [ 'TOTALSUBJ' ], DG.JOIN_TYPE.LEFT, false);
+
+  tj.columns.addNewFloat('PERCENT')
+    .init((i) => parseFloat(tj.get('AECOUNT', i)) / parseFloat(tj.get('TOTALSUBJ', i)));
+
+  let aeRiskRatioPlacebo = tj.groupBy([ 'AETERM', 'EXTRT', 'AECOUNT', 'TOTALSUBJ', 'PERCENT' ])
+    .where('EXTRT = PLACEBO')
+    .aggregate();
+
+  let aeRiskRatioActive = tj.groupBy([ 'AETERM', 'EXTRT', 'AECOUNT', 'TOTALSUBJ', 'PERCENT' ])
+    .where('EXTRT = XANOMELINE')
+    .aggregate();
+
+
+  let tj2 = grok.data.joinTables(aeRiskRatioActive, aeRiskRatioPlacebo, [ 'AETERM' ], [ 'AETERM' ],
+    [ 'AETERM', 'EXTRT', 'AECOUNT', 'TOTALSUBJ', 'PERCENT' ],
+    [ 'AETERM', 'EXTRT', 'AECOUNT', 'TOTALSUBJ', 'PERCENT' ], DG.JOIN_TYPE.OUTER, false);
+
+
+
+  let column1 = tj2.columns.byName('null.AETERM');
+  let percent1 = tj2.columns.byName('null.PERCENT');
+  let exposed1 = tj2.columns.byName('null.AECOUNT');
+  let column2 = tj2.columns.byName('null.AETERM (2)');
+  let percent2 = tj2.columns.byName('null.PERCENT (2)');
+  let exposed2 = tj2.columns.byName('null.AECOUNT (2)');
+  let totalExposed1;
+  let totalExposed2;
+
+  let rowCount = tj2.rowCount;
+
+  for (let i = 0; i < rowCount; i++) {
+    if (column1.isNone(i)) {
+      const value = column2.get(i);
+      column1.set(i, value)
+      percent1.set(i, 0);
+      exposed1.set(i, 0);
+    } else {
+      totalExposed1 = parseFloat(tj2.get('null.TOTALSUBJ', i))
+    }
+  }
+
+
+  for (let i = 0; i < rowCount; i++) {
+    if (column2.isNone(i)) {
+      const value = column1.get(i);
+      column2.set(i, value)
+      percent2.set(i, 0);
+      exposed2.set(i, 0);
+    } else {
+      totalExposed2 = parseFloat(tj2.get('null.TOTALSUBJ (2)', i))
+    }
+  }
+
+
+  tj2.columns.addNewFloat('RELATIVE RISK')
+    .init((i) => parseFloat(tj2.get('null.PERCENT', i)) / parseFloat(tj2.get('null.PERCENT (2)', i)));
+
+  tj2.columns.addNewFloat('RISK DIFF')
+    .init((i) => parseFloat(tj2.get('null.PERCENT', i)) - parseFloat(tj2.get('null.PERCENT (2)', i)));
+
+
+  tj2.columns.addNewFloat('ODDS RATIO').init((i) => (parseFloat(tj2.get('null.AECOUNT', i)) * (totalExposed2 - parseFloat(tj2.get('null.AECOUNT (2)', i)))) /
+    (parseFloat(tj2.get('null.AECOUNT (2)', i)) * (totalExposed1 - parseFloat(tj2.get('null.AECOUNT', i)))));
+
+}
+
+
