86b543a3583c942d5ba6704f92d66e3408fe03e1
Maria Dolotova <mdoloto1@its.jnj.com>
2021-09-02 16:45:29 +0300

    updated survival analysis view and r scripts

--- packages/ClinicalCase/scripts/survivalPlot.r
+++ packages/ClinicalCase/scripts/survivalPlot.r
@@ -2,19 +2,18 @@
 #language: r
 #input: string inputStrata = '1'
 #input: string confInt = 0.95
-#output: graphics
+#input: dataframe survivalDf
+#output: graphics plot
+#output: dataframe diagnostics
 
 renv::init()
 renv::install("ggfortify")
 
 require("ggfortify")
-require("tidyverse")
 require("survival")
 require("ggplot2")
-require("dplyr")
-require("ggplot")
 
-data(veteran)
+diagnostics = as.data.frame(installed.packages()[,c(1,3:4)])
 
 if (inputStrata == '') {
   inputStrata = '1'
@@ -22,7 +21,8 @@ if (inputStrata == '') {
 
 modform <- as.formula(paste("Surv(time, status)", inputStrata, sep = " ~ "))
 
-km_trt_fit <- survfit(modform, data=veteran, conf.int = as.numeric(confInt))
-autoplot(km_trt_fit)  + 
+km_trt_fit <- survfit(modform, data=survivalDf, conf.int = as.numeric(confInt))
+
+autoplot(km_trt_fit) +
   theme_bw()
 
--- packages/ClinicalCase/src/views/survival-analysis-view.ts
+++ packages/ClinicalCase/src/views/survival-analysis-view.ts
@@ -1,99 +1,126 @@
 import * as DG from "datagrok-api/dg";
 import * as grok from 'datagrok-api/grok';
 import * as ui from "datagrok-api/ui";
-import { getColNames } from "../data-preparation/utils";
+import { dataframeContentToRow } from "../data-preparation/utils";
 
 export class SurvivalAnalysisView extends DG.ViewBase {
 
-survivalPlotDiv = ui.div();
-covariatesPlotDiv = ui.div();
-survivalColumns: string[];
-confIntervals = [0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 0.99];
-confInterval = 0.7;
-strata = '';
-
-
+  survivalPlotDiv = ui.div();
+  covariatesPlotDiv = ui.div();
+  survivalColumns: string[];
+  confIntervals = [ 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 0.99 ];
+  confInterval = 0.7;
+  strata = '';
+  survivalDataframe: DG.DataFrame;
+  selectedCovariates: string[];
 
   constructor() {
     super();
 
-    let survivalDataframe = grok.shell.table('survival');
-    this.survivalColumns = getColNames(survivalDataframe);
-    let survivalChoices = [''].concat(this.survivalColumns);
+    this.survivalDataframe = grok.shell.table('survival');
+    this.survivalColumns = this.survivalDataframe.columns.names();
+    let survivalOpions = [ '' ].concat(this.survivalColumns);
+    let covariatesOptions = this.survivalColumns.filter(it => it !== 'time' && it !== 'status');
 
 
-    let confIntChoices = ui.choiceInput('Confidence Intreval', this.confIntervals[0], this.confIntervals);
+    let confIntChoices = ui.choiceInput('Confidence Intreval', this.confIntervals[ 0 ], this.confIntervals);
     confIntChoices.onChanged((v) => {
       this.confInterval = confIntChoices.value;
-      grok.functions.call(
-        "Clinicalcase:survivalPlot", {
-          "inputStrata": this.strata,
-          "confInt": this.confInterval.toString()
-        }).then((result) => {
-          this.updatePlotDiv(result, this.survivalPlotDiv);
-      });     
+      this.updateSurvivalPlot();
     });
 
-    let strataChoices = ui.choiceInput('Strata', survivalChoices[0], survivalChoices);
+    let strataChoices = ui.choiceInput('Strata', survivalOpions[ 0 ], survivalOpions);
     strataChoices.onChanged((v) => {
       this.strata = strataChoices.value;
-      grok.functions.call(
-        "Clinicalcase:survivalPlot", {
-          "inputStrata": this.strata,
-          "confInt": this.confInterval.toString()
-        }).then((result) => {
-          this.updatePlotDiv(result, this.survivalPlotDiv);
-      });     
+      this.updateSurvivalPlot();
     });
 
-/*     let covariatesChoices = ui.multiChoiceInput('Value', ['A'], ['A', 'B', 'C']);
+    let covariatesChoices = ui.multiChoiceInput('Covariates', null, covariatesOptions);
     covariatesChoices.onChanged((v) => {
-      grok.functions.call(
-        "Clinicalcase:covariates", {
-          "coVariates": "trt"
-        }).then((result) => {
-          this.updatePlotDiv(result, this.covariatesPlotDiv);
-      });     
-    }); */
+      this.selectedCovariates = covariatesChoices.value;
+      this.updateCovariatesPlot();
+    });
 
+    let filters = DG.Viewer.fromType('Filters', this.survivalDataframe, {
+      'columnNames': this.survivalColumns,
+      'showContextMenu':false,
+    });
+
+    let applyFilters = ui.button('Apply to curves', () => { });
+    applyFilters.addEventListener('click', (event) => {
+      let test = this.survivalDataframe;
+      this.updateSurvivalPlot();
+      if (this.selectedCovariates) {
+        this.updateCovariatesPlot();
+      }
+    });
+    
     grok.functions.call(
-        "Clinicalcase:survivalPlot", {
-          "inputStrata": this.strata,
-          "confInt": this.confInterval.toString()
+      "Clinicalcase:survivalPlot", {
+      "survivalDf": this.survivalDataframe,
+      "inputStrata": this.strata,
+      "confInt": this.confInterval.toString()
 
-        }).then((survivalResult) => {
+    }).then((survivalResult) => {
 
-        grok.functions.call(
-          "Clinicalcase:covariates", {
-            "coVariates": "trt"
-        }).then((covariatesResult) => {
-
-          this.root.appendChild(
-            ui.splitV([ 
-              ui.tabControl({'Survival chart': 
+      this.root.appendChild(
+        ui.splitV([
+          ui.tabControl({
+            'Survival data':
+              ui.splitV([
                 ui.divH([
-                  ui.divV([
-                    confIntChoices.root,
-                    strataChoices.root
-                  ]), 
-                  this.survivalPlotDiv]), 
-              'Co-variates': 
-              this.covariatesPlotDiv}).root
-            ], { style: { width: '100%', height: '100%' } })
-          );
-          this.updatePlotDiv(survivalResult, this.survivalPlotDiv);
-          this.updatePlotDiv(covariatesResult, this.covariatesPlotDiv);
+                  this.survivalDataframe.plot.grid().root,
+                  applyFilters
+                ]),
+                filters.root
+              ], { style: { width: '100%' } }),
+            'Survival chart':
+              ui.divH([
+                ui.divV([
+                  confIntChoices.root,
+                  strataChoices.root,
+                ]),
+                this.survivalPlotDiv ]),
+            'Co-variates':
+              ui.divH([
+                ui.divV([
+                  covariatesChoices.root
+                ]),
+                this.covariatesPlotDiv ])
+          }).root
+        ], { style: { width: '100%', height: '100%' } })
+      );
+      this.updatePlotDiv(survivalResult['plot'], this.survivalPlotDiv);
+      console.warn(dataframeContentToRow(survivalResult['diagnostics']));
+    });
 
-        });
-
-      });
-
-     
-
-  } 
+  }
 
   private updatePlotDiv(img: string, div: HTMLDivElement) {
     div.innerHTML = '';
     div.append(ui.image(`data:image/png;base64,${img}`, 700, 700));
+  }
+
+  private updateSurvivalPlot() {
+    grok.functions.call(
+      "Clinicalcase:survivalPlot", {
+      "survivalDf": this.survivalDataframe,
+      "inputStrata": this.strata,
+      "confInt": this.confInterval.toString()
+    }).then((result) => {
+      this.updatePlotDiv(result['plot'], this.survivalPlotDiv);
+      console.warn(dataframeContentToRow(result['diagnostics']));
+    });
+  }
+
+  private updateCovariatesPlot() {
+    grok.functions.call(
+      "Clinicalcase:covariates", {
+      "covariatesDf": this.survivalDataframe,
+      "coVariates": this.selectedCovariates.join(' + ')
+    }).then((result) => {
+      this.updatePlotDiv(result['plot'], this.covariatesPlotDiv);
+      console.warn(dataframeContentToRow(result['diagnostics']));
+    });
   }
 }
