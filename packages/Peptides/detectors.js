3fc1d0ae64a1f3fefdb107cb604ac5b1549169a4
Leonid <stolbovla@yandex.ru>
2021-09-13 22:07:39 +0300

    GROK 9602
    Peptides -initial dataset for aligned sequences, detector for aligned sequences

--- packages/Peptides/detectors.js
+++ packages/Peptides/detectors.js
@@ -1,23 +1,21 @@
 class PeptidesPackageDetectors extends DG.Package {
-
+    
   //tags: semTypeDetector
   //input: column col
   //output: string semType
-  // detectNucleotides(col) {
+  detectAligned(col) {
 
-  //   if (col.type === DG.TYPE.STRING) {
-  //     for (let i = 0; i != col.categories.length; i++) {
-  //       var pepRgx = new RegExp(
-  //         "(G|L|Y|S|E|Q|D|N|F|A|K|R|H|C|V|P|W|I|M|T|(\[(G|L|Y|S|E|Q|D|N|F|A|K|R|H|C|V|P|W|I|M|T).+\])){3,30}")
-  //         .test(col.categories[i]);
-  //       if (!pepRgx)
-  //         return null;
-  //     }
-  //     col.semType = 'peptide';
-  //     return (col.semType);
-  //   }
+    if (col.type === DG.TYPE.STRING) {
+      for (let i = 0; i != col.categories.length; i++) {
+        //var papRgx = new RegExp("^((\w+)?-){5,49}\w+$");
 
-  //   return null;
+        var papRgx = new RegExp("NH2").test(col.categories[i]);
 
-  // }
+        if (!papRgx)
+          return null;
+      }
+      col.semType = 'alignedSequence';
+      return (col.semType);
+    }
+  }
 }
--- packages/Peptides/src/package.js
+++ packages/Peptides/src/package.js
@@ -5,6 +5,7 @@ import * as DG from 'datagrok-api/dg';
 
 export let _package = new DG.Package();
 
+
 // function main(tableName, localTables) {
 //   grok.shell.info(tableName);
 //   let peptides = localTables.find(({name}) => name === tableName);
@@ -15,124 +16,90 @@ export let _package = new DG.Package();
 async function main() {
   let pi = DG.TaskBarProgressIndicator.create('Loading Peptides');
   //let peptides = await grok.data.loadTable('https://datagrok.jnj.com/p/ejaeger.il23peptideidp5562/il-23_peptide_idp-5562')
-  let path = _package.webRoot + 'files/' + 'data.csv';
+  let path = _package.webRoot + 'files/' + 'aligned.csv';
+  let peptides = (await grok.data.loadTable(path));
 
-  let peptidesRaw = (await grok.data.loadTable(path))
-  
-  let names = [
-    "N", "a3", "a4", "a5", "a6",
-    "a7", "a8", "a9", "a10",
-    "a11", "a12", "a13", "a14",
-    "a15", "a16", "a17", "C", "Activity"
-  ];
-  let htmlNames = [
-    "a3", "a4", "a5", "a6",
-    "a7", "a8", "a9", "a10",
-    "a11", "a12", "a13", "a14",
-    "a15", "a16", "a17"
-  ];
 
-  let peptides = DG.DataFrame.fromColumns(peptidesRaw.columns.byNames(names));
+  // let names = [
+  //   "N", "a3", "a4", "a5", "a6",
+  //   "a7", "a8", "a9", "a10",
+  //   "a11", "a12", "a13", "a14",
+  //   "a15", "a16", "a17", "C", "Activity"
+  // ];
+  // let htmlNames = [
+  //   "a3", "a4", "a5", "a6",
+  //   "a7", "a8", "a9", "a10",
+  //   "a11", "a12", "a13", "a14",
+  //   "a15", "a16", "a17"
+  // ];
   
   let view = grok.shell.addTableView(peptides);
 
-  for(let i = 0; i < names.length; i++){
-    if(htmlNames.includes(names[i])){
-      view.grid.columns.byName(names[i]).width = 50;
-      view.grid.columns.byName(names[i]).cellType = 'html';
-    }
-  }
-  // view.grid.columns.byName('N').width = 50;
-  // view.grid.columns.byName('a3').width = 50;
-  // view.grid.columns.byName('a3').cellType = 'html';
-  // view.grid.columns.byName('a4').width = 50;
-  // view.grid.columns.byName('a4').cellType = 'html';
-  // view.grid.columns.byName('a5').width = 50;
-  // view.grid.columns.byName('a5').cellType = 'html';
-  // view.grid.columns.byName('a6').width = 50;
-  // view.grid.columns.byName('a6').cellType = 'html';
-  // view.grid.columns.byName('a7').width = 50;
-  // view.grid.columns.byName('a7').cellType = 'html';
-  // view.grid.columns.byName('a8').width = 50;
-  // view.grid.columns.byName('a8').cellType = 'html';
-  // view.grid.columns.byName('a9').width = 50;
-  // view.grid.columns.byName('a9').cellType = 'html';
-  // view.grid.columns.byName('a10').width = 50;
-  // view.grid.columns.byName('a10').cellType = 'html';
-  // view.grid.columns.byName('a11').width = 50;
-  // view.grid.columns.byName('a11').cellType = 'html';
-  // view.grid.columns.byName('a12').width = 50;
-  // view.grid.columns.byName('a12').cellType = 'html';
-  // view.grid.columns.byName('a13').width = 50;
-  // view.grid.columns.byName('a13').cellType = 'html';
-  // view.grid.columns.byName('a14').width = 50;
-  // view.grid.columns.byName('a14').cellType = 'html';
-  // view.grid.columns.byName('a15').width = 50;
-  // view.grid.columns.byName('a15').cellType = 'html';
-  // view.grid.columns.byName('a16').width = 50;
-  // view.grid.columns.byName('a16').cellType = 'html';
-  // view.grid.columns.byName('a17').width = 50;
-  // view.grid.columns.byName('a17').cellType = 'html';
-  // view.grid.columns.byName('C').width = 50;
+  // for(let i = 0; i < names.length; i++){
+  //   if(htmlNames.includes(names[i])){
+  //     view.grid.columns.byName(names[i]).width = 50;
+  //     view.grid.columns.byName(names[i]).cellType = 'html';
+  //   }
+  // }
 
-  let css = {
-    style:{
-    textAlign:'center',
-    fontWeight:'bold',
-    marginTop:'5px'
-    }
-  };
+  // let css = {
+  //   style:{
+  //   textAlign:'center',
+  //   fontWeight:'bold',
+  //   marginTop:'5px'
+  //   }
+  // };
 
-  let css2 = {
-    style:{
-    textAlign:'center',
-    marginTop:'5px',
-    color: "red"
-    }
-  };
+  // let css2 = {
+  //   style:{
+  //   textAlign:'center',
+  //   marginTop:'5px',
+  //   color: "red"
+  //   }
+  // };
 
-  let css3 = {
-    style:{
-    textAlign:'center',
-    marginTop:'5px',
-    color: "green"
-    }
-  };
+  // let css3 = {
+  //   style:{
+  //   textAlign:'center',
+  //   marginTop:'5px',
+  //   color: "green"
+  //   }
+  // };
 
-  let aaRgx = new RegExp("^[G|L|Y|S|E|Q|D|N|F|A|K|R|H|C|V|P|W|I|M|T|]{1}$");
-  let adRgx = new RegExp("^d[G|L|Y|S|E|Q|D|N|F|A|K|R|H|C|V|P|W|I|M|T|]{1}$");
-  let amRgx = new RegExp("^[G|L|Y|S|E|Q|D|N|F|A|K|R|H|C|V|P|W|I|M|T|]{1}\\(.+\\)$");
-  let admRgx = new RegExp("^d[G|L|Y|S|E|Q|D|N|F|A|K|R|H|C|V|P|W|I|M|T|]{1}\\(.+\\)$$");
-  let aNameRgx = new RegExp("^a\\d.*$");
+  // let aaRgx = new RegExp("^[G|L|Y|S|E|Q|D|N|F|A|K|R|H|C|V|P|W|I|M|T|]{1}$");
+  // let adRgx = new RegExp("^d[G|L|Y|S|E|Q|D|N|F|A|K|R|H|C|V|P|W|I|M|T|]{1}$");
+  // let amRgx = new RegExp("^[G|L|Y|S|E|Q|D|N|F|A|K|R|H|C|V|P|W|I|M|T|]{1}\\(.+\\)$");
+  // let admRgx = new RegExp("^d[G|L|Y|S|E|Q|D|N|F|A|K|R|H|C|V|P|W|I|M|T|]{1}\\(.+\\)$$");
+  // let aNameRgx = new RegExp("^a\\d.*$");
   
-  view.grid.onCellPrepare(function (gc) {
-    if (gc.isTableCell && gc.gridColumn.name === 'N'){
-      gc.style.backColor = 0xff1f77b4;
-    }
-    if (gc.isTableCell && gc.gridColumn.name === 'C'){
-      gc.style.backColor = 0xffff7f00;
-    }
-    if(gc.isTableCell && !aaRgx.test(gc.cell.value) && !amRgx.test(gc.cell.value) && aNameRgx.test(gc.gridColumn.name)){
-      gc.style.element = ui.divText(gc.cell.value, css2);
-    }
-    if(gc.isTableCell && aaRgx.test(gc.cell.value)){
-      console.log(gc)
-      gc.style.element = ui.divText(gc.cell.value, css);
-    }
-    if(gc.isTableCell && amRgx.test(gc.cell.value)){
-      console.log(gc)
-      gc.style.element = ui.divText(gc.cell.value, css);
-    }
-    if(gc.isTableCell && adRgx.test(gc.cell.value)){
-      console.log(gc)
-      gc.style.element = ui.divText(gc.cell.value, css3);
-    }
-    if(gc.isTableCell && admRgx.test(gc.cell.value)){
-      console.log(gc)
-      gc.style.element = ui.divText(gc.cell.value, css3);
-    }
+  // view.grid.onCellPrepare(function (gc) {
+  //   if (gc.isTableCell && gc.gridColumn.name === 'N'){
+  //     gc.style.backColor = 0xff1f77b4;
+  //   }
+  //   if (gc.isTableCell && gc.gridColumn.name === 'C'){
+  //     gc.style.backColor = 0xffff7f00;
+  //   }
+  //   if(gc.isTableCell && !aaRgx.test(gc.cell.value) && !amRgx.test(gc.cell.value) && aNameRgx.test(gc.gridColumn.name)){
+  //     gc.style.element = ui.divText(gc.cell.value, css2);
+  //   }
+  //   if(gc.isTableCell && aaRgx.test(gc.cell.value)){
+  //     console.log(gc)
+  //     gc.style.element = ui.divText(gc.cell.value, css);
+  //   }
+  //   if(gc.isTableCell && amRgx.test(gc.cell.value)){
+  //     console.log(gc)
+  //     gc.style.element = ui.divText(gc.cell.value, css);
+  //   }
+  //   if(gc.isTableCell && adRgx.test(gc.cell.value)){
+  //     console.log(gc)
+  //     gc.style.element = ui.divText(gc.cell.value, css3);
+  //   }
+  //   if(gc.isTableCell && admRgx.test(gc.cell.value)){
+  //     console.log(gc)
+  //     gc.style.element = ui.divText(gc.cell.value, css3);
+  //   }
 
-  });
+  // });
 
   pi.close();
 }
@@ -184,3 +151,41 @@ export function Peptides() {
   //let view = grok.shell.addTableView(table);
 
 }
+
+
+  //name: peptidesSAR
+  //tags: demo, peptides 
+  //description: 
+  //top-menu: Chem | peptides
+  //input: dataframe df {semType: alignedSequence}
+  export function peptidesSAR(df) {
+    
+    grok.shell.warning(df.getCol('Activity').toList().length);
+
+    //let colSmiles = DG.Column.fromList("string", "smiles", smiles.toList());
+    //let dfSmiles = DG.DataFrame.fromColumns([colSmiles]);
+
+  
+    //df.columns.insert(x_coord);
+    // df.columns.insert(y_coord);
+    // df.columns.insert(sali);
+    
+    // df.columns.byName('x_coord').name = '~x_coord';
+    // df.columns.byName('y_coord').name = '~y_coord';
+
+    // let view = grok.shell.getTableView(df.name);
+    // let sp = view.addViewer(DG.Viewer.scatterPlot(df, {
+    //   xColumnName: '~x_coord',
+    //   yColumnName: '~y_coord',
+    //   size: 'sali'
+    // }));
+
+    // sp.props.showXSelector = false;
+    // sp.props.showYSelector = false;
+    // sp.props.showSizeSelector = false;
+    // sp.props.showColorSelector = false;
+    // sp.props.markerMinSize = 5;
+    // sp.props.markerMaxSize = 25;
+    // sp.props.colorColumnName = 'activity';
+
+  }  
