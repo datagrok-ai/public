89690f24e5d834235ecb7b7c4235f2165724476b
skalkin <skalkin@gmail.com>
2021-09-14 22:39:38 +0300

    Peptides: simplified the aligned sequence detector

--- packages/Peptides/detectors.js
+++ packages/Peptides/detectors.js
@@ -5,18 +5,8 @@ class PeptidesPackageDetectors extends DG.Package {
   //tags: semTypeDetector
   //input: column col
   //output: string semType
-  detectAligned(col: DG.Column) {
-
-    if (col.type === DG.TYPE.STRING) {
-      for (let i = 0; i != col.categories.length; i++) {
-        let papRgx = new RegExp("^((\\w+)?-){5,49}\\w+$").test(col.categories[i]);
-        if (!papRgx)
-          return null;
-      }
-      col.semType = 'alignedSequence';
-      return (col.semType);
-    }
-
-    return null;
+  detectAligned(col) {
+    let regexp = new RegExp("^((\\w+)?-){5,49}\\w+$");
+    return Detector.sampleCategories(col, (s) => regexp.test(s)) ? 'alignedSequence' : null;
   }
 }
