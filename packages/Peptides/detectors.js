0a92d391e34b47aa2b98dc60835afa306e6441a0
Leonid <stolbovla@yandex.ru>
2021-09-14 15:03:18 +0300

    GROK-9602: Peptides - detector fixed

--- packages/Peptides/detectors.js
+++ packages/Peptides/detectors.js
@@ -1,5 +1,5 @@
 class PeptidesPackageDetectors extends DG.Package {
-    
+
   //tags: semTypeDetector
   //input: column col
   //output: string semType
@@ -7,15 +7,14 @@ class PeptidesPackageDetectors extends DG.Package {
 
     if (col.type === DG.TYPE.STRING) {
       for (let i = 0; i != col.categories.length; i++) {
-        //var papRgx = new RegExp("^((\w+)?-){5,49}\w+$");
-
-        var papRgx = new RegExp("NH2").test(col.categories[i]);
-
+        let papRgx = new RegExp("^((\\w+)?-){5,49}\\w+$").test(col.categories[i]);
         if (!papRgx)
           return null;
       }
       col.semType = 'alignedSequence';
       return (col.semType);
     }
+
+    return null;
   }
 }
