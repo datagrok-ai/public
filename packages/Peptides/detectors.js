63e7b1aca21ecc92a0b678a4e50c010ff09e5dc9
skalkin <skalkin@gmail.com>
2021-09-14 23:23:15 +0300

    Peptides: simplified the aligned sequence detector

--- packages/Peptides/detectors.js
+++ packages/Peptides/detectors.js
@@ -1,5 +1,3 @@
-import * as DG from 'datagrok-api/dg';
-
 class PeptidesPackageDetectors extends DG.Package {
 
   //tags: semTypeDetector
@@ -7,6 +5,6 @@ class PeptidesPackageDetectors extends DG.Package {
   //output: string semType
   detectAligned(col) {
     let regexp = new RegExp("^((\\w+)?-){5,49}\\w+$");
-    return Detector.sampleCategories(col, (s) => regexp.test(s)) ? 'alignedSequence' : null;
+    return DG.Detector.sampleCategories(col, (s) => regexp.test(s)) ? 'alignedSequence' : null;
   }
 }
