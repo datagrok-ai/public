d2ab613748bdf1923a310bc8acf227b6ea9836d2
Volodymyr Dyma <emissarys@protonmail.com>
2021-09-15 22:55:54 +0300

    Peptides: added activity scaling options

--- packages/Peptides/src/SARViewer/sar-viewer.ts
+++ packages/Peptides/src/SARViewer/sar-viewer.ts
@@ -7,29 +7,30 @@ export class SARViewer extends DG.JsViewer {
   initialized: boolean;
   activityColumnName: string;
   activityScalingMethod: string;
-  duplicatesHandingMethod: string;
+  // duplicatesHandingMethod: string;
   constructor() {
     super();
 
     this.activityColumnName = this.string('activityColumnColumnName', 'Activity');
     this.activityScalingMethod = this.string('activityScalingMethod', 'none', {choices: ['none', 'ln', '-ln']});
-    this.duplicatesHandingMethod = this.string('duplicatesHandlingMethod', 'median', {choices: ['median']});
+    // this.duplicatesHandingMethod = this.string('duplicatesHandlingMethod', 'median', {choices: ['median']});
 
     this.initialized = false;
   }
 
-  init() {
-    this.initialized = true;
-  }
 
   onTableAttached() {
-    this.init();
-    
     if (typeof this.dataFrame !== 'undefined') {
-      this.subs.push(DG.debounce(this.dataFrame.selection.onChanged, 50).subscribe((_) => this.render()));
-      this.subs.push(DG.debounce(this.dataFrame.filter.onChanged, 50).subscribe((_) => this.render()));
-      // this.subs.push(DG.debounce(ui.onSizeChanged(this.root), 50).subscribe((_) => this.render()));
+      // this.subs.push(DG.debounce(this.dataFrame.selection.onChanged, 50).subscribe((_: any) => this.render()));
+      this.subs.push(DG.debounce(this.dataFrame.filter.onChanged, 50).subscribe((_: any) => this.render()));
+      // this.subs.push(DG.debounce(ui.onSizeChanged(this.root), 50).subscribe((_: any) => this.render()));
     }
+
+    this.render();
+  }
+
+  onPropertyChanged(property: DG.Property) {
+    super.onPropertyChanged(property);
 
     this.render();
   }
@@ -37,7 +38,7 @@ export class SARViewer extends DG.JsViewer {
   async render() {
     $(this.root).empty();
     if (typeof this.dataFrame !== 'undefined') {
-      const grid = await describe(this.dataFrame, this.activityColumnName);
+      const grid = await describe(this.dataFrame, this.activityColumnName, this.activityScalingMethod);
       if (grid !== null) {
         this.root.appendChild(grid.root);
       }
