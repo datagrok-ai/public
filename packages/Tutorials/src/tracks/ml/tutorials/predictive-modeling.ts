e16bd7df38c7bb7c94658a086abb623e2730a119
onuf <56073962+onuf@users.noreply.github.com>
2021-09-19 17:36:27 +0300

    #53 Tutorials package: ML (predictive modeling tutorial): actions for the model training view

--- packages/Tutorials/src/tracks/ml/tutorials/predictive-modeling.ts
+++ packages/Tutorials/src/tracks/ml/tutorials/predictive-modeling.ts
@@ -1,7 +1,7 @@
 import * as grok from 'datagrok-api/grok';
 import * as DG from 'datagrok-api/dg';
 import $ from 'cash-dom';
-import { fromEvent } from 'rxjs';
+import { fromEvent, interval } from 'rxjs';
 import { filter, map } from 'rxjs/operators';
 import { Tutorial } from "../../../tutorial";
 
@@ -11,6 +11,35 @@ export class PredictiveModelingTutorial extends Tutorial {
   get description() {
     return 'Predictive modeling uses statistics to predict outcomes';
   }
+
+  private async buttonClickAction(root: HTMLElement, instructions: string, caption: string) {
+    const btn = $(root).find('button.ui-btn').filter((idx, btn) => btn.textContent === caption)[0];
+    if (btn == null) return;
+    const source = fromEvent(btn, 'click');
+    await this.action(instructions, source, btn);
+  };
+
+  private async columnInpAction(root: HTMLElement, instructions: string, caption: string, value: string) {
+    const columnInput = $(root)
+      .find('div.ui-input-root.ui-input-column')
+      .filter((idx, inp) => $(inp).find('label.ui-label.ui-input-label')[0]?.textContent === caption)[0];
+    if (columnInput == null) return;
+    const source = interval(1000).pipe(
+      map((_) => $(columnInput).find('div.d4-column-selector-column')[0]?.textContent),
+      filter((val) => val === value));
+    await this.action(instructions, source, columnInput);
+  };
+
+  private async columnsInpAction(root: HTMLElement, instructions: string, caption: string, value: string) {
+    const columnsInput = $(root)
+      .find('div.ui-input-root.ui-input-columns')
+      .filter((idx, inp) => $(inp).find('label.ui-label.ui-input-label')[0]?.textContent === caption)[0];
+    if (columnsInput == null) return;
+    const source = interval(1000).pipe(
+      map((_) => $(columnsInput).find('div.ui-input-editor > div.ui-input-column-names')[0]?.textContent),
+      filter((val) => val === value));
+    await this.action(instructions, source, columnsInput);
+  };
 
   protected async _run() {
 
@@ -49,10 +78,10 @@ export class PredictiveModelingTutorial extends Tutorial {
           inputRoot!);
       }
 
-      // TODO: transform to actions
       //await viewInputAction('Set "Table" to "demog"', 'Table', 'demog');
-      this.describe('Set "Predict" to "SEX"');
-      this.describe('Set "Features" to ["AGE", "WEIGHT", "HEIGHT"]');
+      await this.columnInpAction(pmv!.root, 'Set "Predict" to "SEX"', 'Predict', 'SEX');
+      await this.columnsInpAction(pmv!.root, 'Set "Features" to ["AGE", "HEIGHT", "WEIGHT"]',
+        'Features', '(3) AGE, HEIGHT, WEIGHT');
 
       this.describe('You will now see that the column names are highlighted in red. ' +
         'Click on the question mark to the right of the input to see a warning message. ' +
@@ -69,7 +98,7 @@ export class PredictiveModelingTutorial extends Tutorial {
       })));
 
       await this.dlgInputAction(dlg!, 'Set "Table" to "demog"', 'Table', 'demog');
-      await this.dlgInputAction(dlg!, 'Choose [AGE, WEIGHT, HEIGHT] for "Impute"', 'Impute', 'AGE,HEIGHT,WEIGHT');
+      await this.dlgInputAction(dlg!, 'Choose [AGE, HEIGHT, WEIGHT] for "Impute"', 'Impute', 'AGE,HEIGHT,WEIGHT');
       await this.dlgInputAction(dlg!, 'Set "Data" to DIS_POP', 'Data', 'DIS_POP');
       await this.dlgInputAction(dlg!, 'Set the number of nearest neighbors to "5"', 'Nearest Neighbours', '5');
 
@@ -79,13 +108,14 @@ export class PredictiveModelingTutorial extends Tutorial {
         ));
 
       await viewInputAction(`Set "Method" to "${method}"`, 'Method', method);
+      await this.buttonClickAction(pmv!.root, 'Click the "Train" button', 'TRAIN');
     };
 
     this.title('Train a model');
     await trainModel('Distributed Random Forest');
 
     const browseModels = async () => {
-      await this.action('Click on "ML | Browse Models" to open the Models Browser',
+      await this.action('Click on "Functions | Models" to open the Models Browser',
         grok.events.onViewAdded.pipe(filter((v: DG.View) => v.type === DG.View.MODELS)));
     };
 
