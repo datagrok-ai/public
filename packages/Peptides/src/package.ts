f7e89c8a6fafd65986ffaa3404a957b55dd21a96
Sasha Tsepilova <oleksandra.tsepilova@ucu.edu.ua>
2021-09-21 16:15:12 +0300

    logo Viewer for Peptides

--- packages/Peptides/src/package.ts
+++ packages/Peptides/src/package.ts
@@ -2,8 +2,13 @@
 import * as grok from 'datagrok-api/grok';
 import * as ui from 'datagrok-api/ui';
 import * as DG from 'datagrok-api/dg';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { ProteinLogo } from 'logojs-react';
 import {SARViewer} from './peptide-sar-viewer/sar-viewer';
-import {AlignedSequenceCellRenderer} from './utils/cell-renderer'
+import {AlignedSequenceCellRenderer} from './utils/cell-renderer';
+import { DataFrame } from 'datagrok-api/dg';
+import { splitAlignedPeptides } from './split-aligned';
 
 export const _package = new DG.Package();
 
@@ -114,4 +119,150 @@ export function sar(): SARViewer {
 //output: grid_cell_renderer result
 export function alignedSequenceCellRenderer() {
   return new AlignedSequenceCellRenderer();
+}
+
+class Logo extends DG.JsViewer {
+  initialized: boolean;
+  option: any;
+  colSemType: string;
+  splitted: DataFrame | null;
+  ppm: Array<Array<number>>;
+  reactHost: HTMLDivElement | null;
+  PROT_NUMS: { [id: string]: number };
+  LET_COLORS: Array<any>;
+  target: DataFrame | undefined | null;
+
+  constructor() {
+    super();
+    this.initialized = false;
+    this.colSemType = this.string('colSemType', 'alignedSequence');
+
+    this.splitted = null;
+    this.ppm = [];
+    this.reactHost = null;
+    this.target = null;
+    this.PROT_NUMS = { 'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7, 'I': 8, 'K': 9, 'L': 10, 'M': 11, 'N': 12, 'P': 13, 'Q': 14, 'R': 15, 'S': 16, 'T': 17, 'U': 18, 'V': 19, 'W': 20, 'Y': 21, 'Z': 22 };
+    this.LET_COLORS = [
+      { color: "rgb(44,160,44)", regex: "A" },
+      { color: "rgb(44,160,44)", regex: "B" },
+      { color: "rgb(188,189,34)", regex: "C" },
+      { color: "rgb(31,119,180)", regex: "D" },
+      { color: "rgb(30,110,96)", regex: "E" },
+      { color: "rgb(24,110,79)", regex: "F" },
+      { color: "rgb(214,39,40)", regex: "G" },
+      { color: "rgb(158,218,229)", regex: "H" },
+      { color: "rgb(122, 102, 189)", regex: "I" },
+      { color: "rgb(108, 218, 229)", regex: "K" },
+      { color: "rgb(158,218,229)", regex: "L" },
+      { color: "rgb(141, 124, 217)", regex: "M" },
+      { color: "rgb(235,137,70)", regex: "N" },
+      { color: "rgb(255,152,150)", regex: "P" },
+      { color: "rgb(205, 111, 71)", regex: "Q" },
+      { color: "rgb(23,190,207)", regex: "R" },
+      { color: "rgb(255,187,120)", regex: "S" },
+      { color: "rgb(245,167,100)", regex: "T" },
+      { color: "rgb(188,189,34)", regex: "U" },
+      { color: "rgb(23,190,207", regex: "V" },
+      { color: "rgb(182, 223, 138)", regex: "W" },
+      { color: "rgb(152,223,138)", regex: "Y" },
+      { color: "rgb(205, 111, 71)", regex: "Z" }
+    ];
+  }
+
+  init() {
+    this.initialized = true;
+    this.reactHost = ui.div([], { style: { height: '500px', width: '500px' } })
+    console.log("INIT")
+    this.target = this.dataFrame;
+    this.splitted = splitAlignedPeptides(this.dataFrame!.columns.bySemType(this.colSemType));
+  }
+
+  onTableAttached() {
+    if (typeof this.dataFrame !== 'undefined') {
+      if (!this.initialized) {
+        this.init();
+      }
+
+      this.subs.push(DG.debounce(this.dataFrame.selection.onChanged, 50).subscribe((_: any) => this.render()));
+      this.subs.push(DG.debounce(this.dataFrame.filter.onChanged, 50).subscribe((_: any) => this.render()));
+      this.subs.push(DG.debounce(ui.onSizeChanged(this.root), 50).subscribe((_: any) => this.render()));
+    }
+
+    this.render();
+  }
+
+  detach() {
+    this.subs.forEach(sub => sub.unsubscribe());
+  }
+
+  onPropertyChanged(property: DG.Property) {
+    super.onPropertyChanged(property);
+
+    this.render();
+  }
+
+  async render() {
+    let bits = this.dataFrame!.selection;
+    let selected = false;
+    if (bits.trueCount > 0) {
+      selected = true;
+      this.target = this.dataFrame!
+        .groupBy([this.dataFrame!.columns.bySemType(this.colSemType).name])
+        .whereRowMask(this.dataFrame!.selection)
+        .aggregate();
+    }
+    if (selected)
+      this.splitted = splitAlignedPeptides(this.target!.columns.bySemType(this.colSemType));
+    else this.splitted = splitAlignedPeptides(this.dataFrame!.columns.bySemType(this.colSemType));
+    $(this.root).empty();
+
+    if (typeof this.dataFrame !== 'undefined') {
+      this.findLogo();
+
+      if (this.reactHost !== null) {
+        this.root.appendChild(this.reactHost);
+      }
+    }
+  }
+
+  async findLogo() {
+    this.getInfoFromDf();
+    ReactDOM.render(React.createElement(ProteinLogo, { alphabet: this.LET_COLORS, ppm: this.ppm }, null), this.reactHost);
+  }
+
+  getInfoFromDf() {
+    let index: number = 0;
+    this.ppm = []
+
+    for (let col of this.splitted!.columns) {
+      let size = col.length;
+      this.ppm.push(new Array(22).fill(0))
+      for (let i = 0; i < col.length; i++) {
+        if (col.get(i) != '-') {
+          this.ppm[index][this.PROT_NUMS[col.get(i)]] += 1 / size;
+        }
+      }
+      index++;
+    }
+  }
+}
+
+//name: Show logo
+//tags: panel, widget
+//input: column col {semType: alignedSequence}
+//output: widget result
+export function showl(col: DG.Column): DG.Widget {
+
+  const startBtn = ui.button('Start', async () => {
+    const peptidesView = grok.shell.v;
+    (<DG.TableView>peptidesView).addViewer('peptide-logo-viewer');
+  });
+  return new DG.Widget(ui.div(startBtn));
+}
+
+//name: peptide-logo-viewer
+//tags: viewer, panel
+//output: viewer result
+export function logov() {
+  return new Logo();
 }
