5f3472e4061aac9d2484eb2c5414680a09604e2f
onuf <56073962+onuf@users.noreply.github.com>
2021-09-21 12:10:28 +0300

    JS API: add examples with the linear color-coding

--- packages/ApiSamples/scripts/grid/color-coding-conditional.js
+++ packages/ApiSamples/scripts/grid/color-coding-conditional.js
@@ -1,4 +1,9 @@
 let t = grok.data.demo.demog();
-t.col('height').tags['.color-coding-type'] = 'Conditional';
-t.col('height').tags['.color-coding-conditional'] = `{"20-170":"#00FF00","170-190":"#220505"}`;
+
+t.col('height').tags[DG.TAGS.COLOR_CODING_TYPE] = 'Conditional';
+t.col('height').tags[DG.TAGS.COLOR_CODING_CONDITIONAL] = `{"20-170":"#00FF00","170-190":"#220505"}`;
+
+t.col('age').tags[DG.TAGS.COLOR_CODING_TYPE] = 'Linear';
+t.col('age').tags[DG.TAGS.COLOR_CODING_LINEAR] = `[${DG.Color.orange}, ${DG.Color.green}]`;
+
 grok.shell.addTableView(t);
