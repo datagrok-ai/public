d296dd59b4ada61e7facd0924de1b09036cca8ec
onuf <56073962+onuf@users.noreply.github.com>
2021-10-04 10:01:19 +0300

    GROK-8976 Form: onFormCreating event for adjusting the default column selection

---
+++ packages/ApiSamples/scripts/events/form-creation.js
@@ -0,0 +1,8 @@
+// The default Form viewer shows 20 columns ranked by relevance.
+// The list of columns can be modified to match other heuristics.
+const df = await grok.data.getDemoTable('chem/smiles.csv');
+const view = grok.shell.addTableView(df);
+
+grok.events.onFormCreating.subscribe((data) => {
+  data.columns = data.columns.filter((col) => col.semType === DG.SEMTYPE.MOLECULE);
+});
