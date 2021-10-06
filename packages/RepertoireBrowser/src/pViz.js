6613826fd375ea1db5468b6c86735621bf620431
Leonid <stolbovla@yandex.ru>
2021-10-06 11:16:57 +0300

    JBIO: removing test files

--- packages/RepertoireBrowser/src/pViz.js
+++ packages/RepertoireBrowser/src/pViz.js
@@ -2,7 +2,7 @@ import * as grok from 'datagrok-api/grok';
 import * as ui from 'datagrok-api/ui';
 import * as DG from "datagrok-api/dg";
 
-import json from "./VR000030945.json";
+import json from "./TPP000153303.json";
 import mutcodes from "./mutcodes.json";
 import {MiscMethods} from "./misc.js"
 
