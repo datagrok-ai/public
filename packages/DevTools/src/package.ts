717f28798cf5aaf7c09a5a2a257515bb6f70e4d8
skalkin <skalkin@gmail.com>
2021-10-09 19:06:30 +0300

    #135: DevTools: a lightweight package unit-testing framework

--- packages/DevTools/src/package.ts
+++ packages/DevTools/src/package.ts
@@ -14,7 +14,7 @@ import {
   EntityType,
 } from './constants';
 import './styles.css';
-import * as tests from "../../Chem/src/tests/chem-tests";
+import * as tests from "./tests/test-examples";
 import {testPackages} from "./package-testing";
 
 export const _package = new DG.Package();
