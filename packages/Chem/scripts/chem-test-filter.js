8146b6964d196b6af4d938da1aaf75ff3d620c62
dskatov <dskatov@datagrok.ai>
2021-10-13 14:57:08 +0300

    #132 Chem: Move Substructure Filter panel to the package

---
+++ packages/Chem/scripts/chem-test-filter.js
@@ -0,0 +1,20 @@
+//name: chemTestFilter
+//description: Test a packaged molecular filter on a selected dataframe
+//language: javascript
+
+grok.data.loadTable('https://public.datagrok.ai/demo/sar_small.csv').then(
+  async (df) => {
+    let filter = await grok.functions.call("Chem:substructureFilter");
+    filter.attach(df);
+    grok.shell.addTableView(df);
+    let colChoice = ui.columnInput('Column', filter.dataFrame, filter.column, (col) => {
+      filter.column = col;
+      filter.dataFrame.filter.setAll(true, false);
+      filter.dataFrame.rows.requestFilter();
+    });
+    ui.dialog({title: 'Chem Filter'})
+      .add(colChoice)
+      .add(filter.root)
+      .show();
+  }
+)
