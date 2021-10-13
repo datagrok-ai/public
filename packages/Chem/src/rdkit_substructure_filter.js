c45ca0c9c25047baea736ce39d82d5db716e952c
dskatov <dskatov@datagrok.ai>
2021-10-13 14:57:08 +0300

    #132 Chem: Move Substructure Filter panel to the package

--- packages/Chem/src/rdkit_substructure_filter.js
+++ packages/Chem/src/rdkit_substructure_filter.js
@@ -4,68 +4,63 @@
  * 1. On onRowsFiltering event, only FILTER OUT rows that do not satisfy this filter's criteria
  * 2. Call dataFrame.rows.requestFilter when filtering criteria changes.
  * */
+
 class SubstructureFilter extends DG.Filter {
 
-
-    constructor() {
-        super();
-        this.WHITE_MOL = `
+  constructor() {
+    
+    super();
+    this.WHITE_MOL = `
   0  0  0  0  0  0  0  0  0  0999 V2000
 M  END
 `;
-        this.molfile = '';
-        this.root = ui.divV([]);
-        this.root.appendChild(grok.chem.sketcher((_, molfile) => {
-            this.molfile = molfile;
-            this.dataFrame.rows.requestFilter();
-        }))
+    this.molfile = '';
+    this.root = ui.divV([]);
+    this.root.appendChild(grok.chem.sketcher((_, molfile) => {
+        this.molfile = molfile;
+        this.dataFrame.rows.requestFilter();
+      }));
+      
+  }
+
+  attach(dFrame) {
+    
+    this.dataFrame = DG.toJs(dFrame);
+    this.column = this.dataFrame.columns.bySemType(DG.SEMTYPE.MOLECULE);
+    this.dataFrame.onRowsFiltering.subscribe((_) => this.applyFilter());
+  
+  }
+
+  reset() {
+    
+    this.dataFrame.filter.setAll(true, false);
+    if (this.column?.tags['chem-scaffold-filter']) {
+      delete this.column.tags['chem-scaffold-filter'];
     }
+    this.dataFrame.filter.fireChanged();
+  
+  }
 
-    attach(dFrame) {
-        this.dataFrame = DG.toJs(dFrame);
-        this.column = this.dataFrame.columns.bySemType(DG.SEMTYPE.MOLECULE);
-        let colChoice = ui.columnInput('Column', this.dataFrame, this.column, (col) => {
-            this.column = col;
-            this.dataFrame.filter.setAll(true, false);
-            this.dataFrame.rows.requestFilter();
-
-        })
-
-        this.root.appendChild(colChoice.root);
-        this.dataFrame.onRowsFiltering.subscribe((_) => this.applyFilter());
-
+  applyFilter() {
+    
+    if (!this.molfile || this.molfile.endsWith(this.WHITE_MOL)) {
+      this.reset();
+      return;
     }
-
-    reset() {
-        this.dataFrame.filter.setAll(true, false);
-        if (this.column?.tags['chem-scaffold-filter']) {
-            delete this.column.tags['chem-scaffold-filter'];
-        }
-        this.dataFrame.filter.fireChanged();
-    }
-
-    applyFilter() {
-        if (!this.molfile || this.molfile.endsWith(this.WHITE_MOL)) {
-            this.reset();
-            return;
-        }
-        grok.functions.call('Chem:searchSubstructure',
-            {
-                'molStringsColumn': this.column,
-                'molString': this.molfile,
-                'substructLibrary': false,
-                'molStringSmarts': ''
-            })
-            .then((bitset_col) => {
-                this.dataFrame.filter.copyFrom(bitset_col.get(0));
-                this.column.setTag('chem-scaffold-filter', this.molfile);
-                this.dataFrame.filter.fireChanged();
-
-            }).catch((e) => {
-            console.warn(e);
-            this.reset();
-        })
-
-
-    }
+    grok.functions.call('Chem:searchSubstructure', {
+      'molStringsColumn' : this.column,
+      'molString': this.molfile,
+      'substructLibrary': false,
+      'molStringSmarts': ''
+    })
+    .then((bitset_col) => {
+      this.dataFrame.filter.copyFrom(bitset_col.get(0));
+      this.column.setTag('chem-scaffold-filter', this.molfile);
+      this.dataFrame.filter.fireChanged();
+    }).catch((e) => {
+      console.warning(e);
+      this.reset();
+    })
+    
+  }
 }
