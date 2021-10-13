3a65892638b3b9a086c76a41f889e2e2cf404716
Alex Paramonov <aparamonov@datagrok.ai>
2021-10-13 19:51:44 +0300

    GROK-9804 Expose DataSourceCardView to TS-API

---
+++ js-api/src/views/card_view.ts
@@ -0,0 +1,79 @@
+import {View} from "./view";
+import {ObjectHandler} from "../../ui";
+let api = <any>window;
+
+/** Base view for working with a collection of objects that reside on the server.
+ *  Typically, results are filtered by applying AND operation between two
+ *  filters: {@link permanentFilter} (which is set programmatically and is not visible)
+ *  and {@link searchValue} entered by the user.
+ *
+ *  More details on the smart search syntax: {@link https://datagrok.ai/help/overview/smart-search}
+ *
+ */
+export class CardView extends View {
+  d: any;
+
+  constructor(d: any) {
+    super(d);
+  }
+
+  static create(options?: any): CardView {
+    return new CardView(api.grok_CardView_Create(options));
+  }
+
+  /**
+   * User-specified {@link https://datagrok.ai/help/overview/smart-search | filter expression}.
+   * @type {string} */
+  get searchValue(): string {
+    return api.grok_CardView_Get_SearchValue(this.d);
+  }
+
+  set searchValue(s: string) {
+    api.grok_CardView_Set_SearchValue(this.d, s);
+  }
+
+  get meta(): ObjectHandler {
+    return api.grok_CardView_Get_Meta(this.d);
+  }
+
+  set meta(s: ObjectHandler) {
+    api.grok_CardView_Set_Meta(this.d, s);
+  }
+
+  /** Programmatically defined invisible
+   * {@link https://datagrok.ai/help/overview/smart-search | filter expression}.
+   *  @type {string} */
+  get permanentFilter(): string {
+    return api.grok_CardView_Get_PermanentFilter(this.d);
+  }
+
+  set permanentFilter(s: string) {
+    api.grok_CardView_Set_PermanentFilter(this.d, s);
+  }
+}
+
+
+/** Projects view */
+export class ProjectsView extends CardView {
+  /** @constructs ProjectsView */
+  constructor(d: any) {
+    super(d);
+  }
+
+  static create(params: object): ProjectsView {
+    return new ProjectsView(api.grok_ProjectsView(params));
+  }
+}
+
+
+/** Scripts view */
+export class ScriptsView extends CardView {
+  /** @constructs ProjectsView */
+  constructor(d: any) {
+    super(d);
+  }
+
+  static create(params: object): ScriptsView {
+    return new ScriptsView(api.grok_ScriptsView(params));
+  }
+}
