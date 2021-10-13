8caa1cfbf0ffe82cdd2e9713e50ea642ad162c2c
dskatov <dskatov@datagrok.ai>
2021-10-13 17:35:16 +0300

    Update CONTRIB.md, grooming

--- packages/Chem/src/chem_substructure_filter.js
+++ packages/Chem/src/chem_substructure_filter.js
@@ -8,7 +8,6 @@
 class SubstructureFilter extends DG.Filter {
 
   constructor() {
-    
     super();
     this.WHITE_MOL = `
   0  0  0  0  0  0  0  0  0  0999 V2000
@@ -20,29 +19,23 @@ M  END
         this.molfile = molfile;
         this.dataFrame.rows.requestFilter();
       }));
-      
   }
 
   attach(dFrame) {
-    
     this.dataFrame = dFrame;
     this.column = this.dataFrame.columns.bySemType(DG.SEMTYPE.MOLECULE);
     this.dataFrame.onRowsFiltering.subscribe((_) => this.applyFilter());
-  
   }
 
   reset() {
-    
     this.dataFrame.filter.setAll(true, false);
     if (this.column?.tags['chem-scaffold-filter']) {
       delete this.column.tags['chem-scaffold-filter'];
     }
     this.dataFrame.filter.fireChanged();
-  
   }
 
   applyFilter() {
-    
     if (!this.molfile || this.molfile.endsWith(this.WHITE_MOL)) {
       this.reset();
       return;
@@ -61,6 +54,5 @@ M  END
       console.warn(e);
       this.reset();
     })
-    
   }
 }
