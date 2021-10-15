1323fe2ccecc4dee8767575f68acdafeda23ce85
Ivan Feofilaktov <van199928@mail.ru>
2021-10-15 14:01:49 +0200

    Peptides: Sar-list update

--- packages/Peptides/src/package.ts
+++ packages/Peptides/src/package.ts
@@ -52,7 +52,7 @@ async function main(chosenFile: string) {
             });
             maxWidth = maxWidth < textSize ? textSize: maxWidth;
           }
-          tableGrid.col(col.name)!.width = Math.min(maxWidth * 10, 1000);
+          tableGrid.col(col.name)!.width = Math.min(maxWidth * 10, 650);
         }, 500);
       }
     }
