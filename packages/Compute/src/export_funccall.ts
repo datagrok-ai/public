fb227a87417ea7f1c7257ea5b4f78bc90861e017
Aufar Zakiev <zaufar@gmail.com>
2021-10-28 12:18:20 +0300

    Refactored exportFuncCall

--- packages/Compute/src/export_funccall.ts
+++ packages/Compute/src/export_funccall.ts
@@ -7,7 +7,7 @@ import {saveAs} from 'file-saver';
 
 export function exportFuncCall(funcName: string) {
   const BLOB_TYPE = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
-  const workbook = new ExcelJS.Workbook();
+  const exportWorkbook = new ExcelJS.Workbook();
 
   const isScalarType = (type: string) => {
     const SCALAR_TYPES = ['int', 'double', 'bool', 'string', 'datetime'];
@@ -20,20 +20,23 @@ export function exportFuncCall(funcName: string) {
     const targetFuncScript = res.prepare({a: 3, b: 3});
     const dfOutputs = res.outputs.filter((output: Property) => isDataframe(output.propertyType)) as Property[];
     const scalarOutputs = res.outputs.filter((output: Property) => isScalarType(output.propertyType)) as Property[];
+
     targetFuncScript.call().then((data: FuncCall) => {
       dfOutputs.forEach((dfOutput) => {
-        const currentDfSheet = workbook.addWorksheet(dfOutput.name);
+        const currentDfSheet = exportWorkbook.addWorksheet(dfOutput.name);
         const currentDf = (data.outputs[dfOutput.name] as DataFrame);
         currentDfSheet.addRow((currentDf.columns as ColumnList).names());
         for (let i =0; i< currentDf.rowCount; i++) {
           currentDfSheet.addRow([...currentDf.row(i).cells].map((cell: Cell) => cell.value));
         }
       });
-      const scalarsSheet = workbook.addWorksheet('Scalars');
+
+      const scalarsSheet = exportWorkbook.addWorksheet('Scalars');
       scalarOutputs.forEach((scalarOutput) => {
         scalarsSheet.addRow([scalarOutput.name, data.outputs[scalarOutput.name]]);
       });
-      workbook.xlsx.writeBuffer().then((data) => {
+
+      exportWorkbook.xlsx.writeBuffer().then((data) => {
         const blob = new Blob([data], {type: BLOB_TYPE});
         saveAs(blob, funcName);
       });
