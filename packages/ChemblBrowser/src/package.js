347a8e66ead7a15c5a80ac4e7221a8b6ffb17899
Ivan Feofilaktov <van199928@mail.ru>
2021-10-29 14:06:01 +0200

    ChemblBrowser: fix

--- packages/ChemblBrowser/src/package.js
+++ packages/ChemblBrowser/src/package.js
@@ -4,133 +4,323 @@ import * as ui from 'datagrok-api/ui';
 import * as DG from 'datagrok-api/dg';
 
 export let _package = new DG.Package();
+let packageName = 'Chemblbrowser'
 
-//name: ChemblSubstructureSearch
-//input: string mol {semType: Molecule}
-//output: dataframe dbResult
-export async function
-chemblSubstructureSearch(mol) {
-    try {
-        let df = await grok.data.query('Chemblapi:SubstructureSmile', {'smile': mol})
-        df = df.clone(null, ['canonical_smiles', 'molecule_chembl_id']);
-        if (df == null) {
-            return DG.DataFrame.create();
+//name: test
+//input: string s
+export function test(s) {
+    grok.shell.info(_package.webRoot);
+}
+
+let r = null;
+let v = null;
+
+
+//name: Browser
+//tags: app
+export async function Browser() {
+
+
+    // Filter inputs
+    let molecule = ui.moleculeInput('Substructure', 'C1CCCCC1');
+    let subName = ui.stringInput('Subname', '');
+    let molregno = ui.stringInput('Molregno', '');
+    let ro5Violation = ui.choiceInput('RO5 Violations', 'All', ['All', '0', '1', '2', '3', '4']);
+    let maxPhase = ui.choiceInput('Max Phase', 'All', ['All', '0', '1', '2', '3', '4']);
+    let molecule_type = ui.choiceInput('Molecule type', 'All', ['Protein', 'Oligonucleotide', 'Unknown', 'Antibody', 'Oligosaccharide', 'Unclassified', 'Enzyme', 'Cell', 'All']);
+    let clear = ui.button([ui.iconFA('trash-alt'), 'Clear filters'], () => clearFilters());
+
+    let controlPanel = ui.form([
+        molecule,
+        subName,
+        molregno,
+        ro5Violation,
+        maxPhase,
+        molecule_type,
+        clear
+    ]);
+    $(controlPanel).addClass('ui-form-condensed'); //TODO: use ui.smallForm instead
+
+    // Filter handlers
+    molecule.onChanged(() => update());
+    subName.onChanged(() => update());
+    molregno.onChanged(() => findByMolregno());
+    ro5Violation.onChanged(() => update());
+    maxPhase.onChanged(() => update());
+    molecule_type.onChanged(() => update());
+
+
+    async function initView() {
+
+        let parser = document.createElement('a');
+        parser.href = window.location;
+        let pathSegments = parser.href.split("?");
+
+
+        // if we came to app by link with molregno in URL
+        if (pathSegments.length === 2 && pathSegments[1].includes("molregno")) {
+            let parsedMolregno = parseInt(pathSegments[1].split("=")[1]);
+            molregno.value = parsedMolregno;
+            let data = await grok.data.query(`${packageName}:FindByMolregno`, {'molregno': parsedMolregno});
+            data.col('canonical_smiles').semType = DG.SEMTYPE.MOLECULE;
+            v = grok.shell.newView('Chembl Browser');
+            r = DG.Viewer.fromType(DG.VIEWER.TILE_VIEWER, data);
+            v.append(ui.divV([r]));
+            v.toolbox = ui.div(controlPanel);
+            v.box = true;
+
         }
-        return df;
-    } catch (e) {
-        console.error("In SubstructureSearch: " + e.toString());
-        throw e;
-    }
-}
+        // if we came to app by link with set of parameters in URL
+        else if (pathSegments.length > 2 && pathSegments[1].includes("substructure")) {
 
-//name: ChemblSimilaritySearch
-//input: string molecule {semType: Molecule}
-//output: dataframe drbResult
-export async function chemblSimilaritySearch(molecule) {
-    try {
-        let df = await grok.data.query('Chemblapi:SimilaritySmileScore', {'smile': molecule, 'score': 40})
-        df = df.clone(null, ['canonical_smiles', 'molecule_chembl_id']);
-        if (df == null) {
-            return DG.DataFrame.create();
+            let parsedSubstructure = pathSegments[1].split("=")[1];
+            let parsedSubname = pathSegments[2].split("=")[1];
+            let parsedRo5 = parseInt(pathSegments[3].split("=")[1]);
+            let parsedMaxPhase = parseInt(pathSegments[4].split("=")[1]);
+            let parsedMoleculeType = pathSegments[5].split("=")[1];
+
+            let queryParameters = {
+                'substructure': parsedSubstructure,
+                'subname': parsedSubname,
+                'num_ro5_violations': parsedRo5,
+                'max_phase': parsedMaxPhase,
+                'molecule_type': parsedMoleculeType
+            };
+
+            molecule.value = parsedSubstructure;
+            subName.value = parsedSubname;
+            ro5Violation.value = parsedRo5;
+            maxPhase = parsedMaxPhase;
+            molecule_type.value = parsedMoleculeType;
+
+            let data = await grok.data.query(`${packageName}:ChemblBrowserQuery`, queryParameters);
+            data.col('canonical_smiles').semType = DG.SEMTYPE.MOLECULE;
+            v = grok.shell.newView('Chembl Browser');
+            r = DG.Viewer.fromType(DG.VIEWER.TILE_VIEWER, data);
+            v.append(ui.divV([r]));
+            v.toolbox = ui.div(controlPanel);
+            v.box = true;
+        } else {
+            let data = await grok.data.query(`${packageName}:allChemblStructures`, {});
+            data.col('canonical_smiles').semType = DG.SEMTYPE.MOLECULE;
+            v = grok.shell.newView('Chembl Browser');
+            r = DG.Viewer.fromType(DG.VIEWER.TILE_VIEWER, data);
+            v.append(ui.divV([r]));
+            v.toolbox = ui.div(controlPanel);
+            v.box = true;
         }
-        return df;
-
-    } catch (e) {
-        console.error(e);
-        return null;
     }
 
-}
-
-//name: Chembl Search Widget
-//tags: widgets
-//input: string mol {semType: Molecule}
-//input: string searchType
-//output: widget result
-export function ChemblSearchWidget(mol, searchType) {
-    let headerHost = ui.divH([]);
-    let compsHost = ui.divH([ui.loader(), headerHost]);
-    let panel = ui.divV([compsHost]);
-    let search = {
-        'similarity': async () => chemblSimilaritySearch(mol),
-        'substructure': async () => chemblSubstructureSearch(mol)
-    }
-    if (!searchType in search) {
-
-        throw "DrugBankSearch: No such search type" + searchType;
-    }
-
-    search[searchType]().then(t => {
-          compsHost.removeChild(compsHost.firstChild);
-          if (t == null) {
-
-              compsHost.appendChild(ui.divText('No matches'));
-              return;
-          }
-          t.col('canonical_smiles').semType = 'Molecule';
-          t.col('canonical_smiles').setTag('cell.renderer', 'Molecule');
-
-
-          let grid = t.plot.grid();
-          let col = grid.columns.byName('molecule_chembl_id');
-          col.cellType = 'html';
-          grid.onCellPrepare(function (gc) {
-              if (gc.isTableCell && gc.gridColumn.name === 'molecule_chembl_id') {
-                  const link = `https://www.ebi.ac.uk/chembl/compound_report_card/${gc.cell.value}/`;
-                  gc.style.element = ui.divV([
-                      ui.link(gc.cell.value, link, link)
-                  ]);
-              }
-          });
-          compsHost.appendChild(grid.root)
-          headerHost.appendChild(ui.iconFA('arrow-square-down', () => {
-              t.name = `"DrugBank Similarity Search"`;
-              grok.shell.addTableView(t);
-          }, 'Open compounds as table'));
-          compsHost.style.overflowY = 'auto';
-      }
-    )
-    .catch(err => {
-        if (compsHost.children.length > 0) {
-            compsHost.removeChild(compsHost.firstChild);
+    async function update() {
+        let ro5 = ro5Violation.value == 'All' ? -1 : parseInt(ro5Violation.value);
+        let max_phase = maxPhase.value == 'All' ? -1 : parseInt(maxPhase.value);
+        let queryParameters = {
+            'substructure': molecule.value,
+            'subname': subName.value,
+            'num_ro5_violations': ro5,
+            'max_phase': max_phase,
+            'molecule_type': molecule_type.value
+        };
+        let query = await grok.data.query(`${packageName}:ChemblBrowserQuery`, queryParameters);
+        if (query.rowCount == 0) {
+            grok.shell.info("No results for this filter were found")
+        } else {
+            query.col('canonical_smiles').semType = DG.SEMTYPE.MOLECULE;
+            v.root.children[0].remove();
+            r = DG.Viewer.fromType(DG.VIEWER.TILE_VIEWER, query);
+            r.setOptions({look:look});
+            console.log(r.getOptions());
+            v.toolbox = ui.div(controlPanel);
+            v.append(ui.divV([r]));
+            v.box = true;
+            v.path = '';
+            v.path = `/apps/Chemblbrowser?substructure=${molecule.value}?subname=${subName.value}?num_ro5_violations=${ro5}?max_phase=${max_phase}?molecule_type=${molecule_type.value}`;
         }
-        let div = ui.divText('No matches');
-        ui.tooltip.bind(div, `${err}`);
-        compsHost.appendChild(div);
-    });
-    return new DG.Widget(panel);
-}
-
-//name Chembl Get by Id
-//input string id
-//output dataframe
-export async function getById(id) {
-    if (!id.toLowerCase().startsWith("chembl")) {
-        id = "CHEMBL" + id
-    }
-    try {
-        return await grok.data.query(`${packageName}:MoleculeJson`, {'molecule_chembl_id__exact': id});
-    } catch (e) {
-        console.error(e);
-        return null;
     }
 
+    async function findByMolregno() {
+        let query = await grok.data.query(`${packageName}:FindByMolregno`, {'molregno': parseInt(molregno.value)});
+        if (query.rowCount == 0) {
+            grok.shell.info("No results for this filter were found")
+        } else {
+            query.col('canonical_smiles').semType = DG.SEMTYPE.MOLECULE;
+            v.root.children[0].remove();
+            r = DG.Viewer.fromType(DG.VIEWER.TILE_VIEWER, query);
+            v.toolbox = ui.div(controlPanel);
+            v.append(ui.divV([r]));
+            v.box = true;
+            v.path = '/apps/Chemblbrowser?molregno=' + molregno.value;
+        }
+    }
+
+    async function clearFilters() {
+        molecule.value = '';
+        subName.value = '';
+        ro5Violation.value = 'All';
+        maxPhase.value = 'All';
+        molecule_type.value = 'All';
+        molregno.value = null;
+        v.path = '';
+    }
+
+    await initView();
+
 }
 
-//name: Chembl Substructure Search Widget
-//tags: panel, widgets
-//input: string mol {semType: Molecule}
-//output: widget result
-//condition: true
-export function ChemblSubstructureSearchPanel(mol) {
-    return ChemblSearchWidget(mol, 'substructure');
-}
 
-//name: Chembl Similarity Search Widget
-//tags: panel, widgets
-//input: string mol {semType: Molecule}
-//output: widget result
-//condition: true
-export function ChemblSimilaritySearchPanel(mol) {
-    return ChemblSearchWidget(mol, 'similarity');
-}
+const look = {
+    "sketchState": {
+        "#type": "SketchState",
+        "elementStates": [
+            {
+                "left": 142,
+                "top": 94,
+                "width": 100,
+                "height": 20,
+                "type": "field",
+                "viewerSettings": {
+                    "table": "FindByMolregno",
+                    "column": "molregno",
+                    "format": null
+                }
+            },
+            {
+                "left": 142,
+                "top": 118,
+                "width": 100,
+                "height": 20,
+                "type": "field",
+                "viewerSettings": {
+                    "table": "FindByMolregno",
+                    "column": "compound_name",
+                    "format": null
+                }
+            },
+            {
+                "left": 10,
+                "top": 190,
+                "width": 112,
+                "height": 20,
+                "type": "html",
+                "viewerSettings": {
+                    "markup": "<label class=\"d4-sketch-column-name\">max_phase</label>"
+                }
+            },
+            {
+                "left": 142,
+                "top": 214,
+                "width": 100,
+                "height": 20,
+                "type": "field",
+                "viewerSettings": {
+                    "table": "FindByMolregno",
+                    "column": "chembl_id",
+                    "format": null
+                }
+            },
+            {
+                "left": 10,
+                "top": 118,
+                "width": 112,
+                "height": 20,
+                "type": "html",
+                "viewerSettings": {
+                    "markup": "<label class=\"d4-sketch-column-name\">compound_name</label>"
+                }
+            },
+            {
+                "left": 142,
+                "top": 142,
+                "width": 100,
+                "height": 20,
+                "type": "field",
+                "viewerSettings": {
+                    "table": "FindByMolregno",
+                    "column": "num_ro5_violations",
+                    "format": null
+                }
+            },
+            {
+                "left": 10,
+                "top": 142,
+                "width": 112,
+                "height": 20,
+                "type": "html",
+                "viewerSettings": {
+                    "markup": "<label class=\"d4-sketch-column-name\">num_ro5_violations</label>"
+                }
+            },
+            {
+                "left": 10,
+                "top": 94,
+                "width": 112,
+                "height": 20,
+                "type": "html",
+                "viewerSettings": {
+                    "markup": "<label class=\"d4-sketch-column-name\">molregno</label>"
+                }
+            },
+            {
+                "left": 142,
+                "top": 166,
+                "width": 100,
+                "height": 20,
+                "type": "field",
+                "viewerSettings": {
+                    "table": "FindByMolregno",
+                    "column": "synonyms",
+                    "format": null
+                }
+            },
+            {
+                "left": 10,
+                "top": 166,
+                "width": 112,
+                "height": 20,
+                "type": "html",
+                "viewerSettings": {
+                    "markup": "<label class=\"d4-sketch-column-name\">synonyms</label>"
+                }
+            },
+            {
+                "left": 10,
+                "top": 214,
+                "width": 112,
+                "height": 20,
+                "type": "html",
+                "viewerSettings": {
+                    "markup": "<label class=\"d4-sketch-column-name\">chembl_id</label>"
+                }
+            },
+            {
+                "left": 142,
+                "top": 190,
+                "width": 100,
+                "height": 20,
+                "type": "field",
+                "viewerSettings": {
+                    "table": "FindByMolregno",
+                    "column": "max_phase",
+                    "format": null
+                }
+            },
+            {
+                "left": 7,
+                "top": 20,
+                "width": 235,
+                "height": 70,
+                "type": "field",
+                "viewerSettings": {
+                    "table": "FindByMolregno",
+                    "column": "canonical_smiles",
+                    "format": null
+                }
+            }
+        ]
+    }
+};
+
+
+
+
+
