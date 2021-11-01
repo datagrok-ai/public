c7cba77d75471a6d9e352ec64dd811240c409664
kdoncov <kdoncov@datagrok.ai>
2021-11-01 19:45:45 +0200

    Grok connect: removed test

--- connectors/grok_connect/src/test/java/grok_connect/GrokConnectTest.java
+++ connectors/grok_connect/src/test/java/grok_connect/GrokConnectTest.java
@@ -1,13 +1,6 @@
 package grok_connect;
 
-import grok_connect.utils.ConnectionPool;
 import org.junit.Test;
-
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.Properties;
-
-import static org.junit.Assert.*;
 
 
 public class GrokConnectTest {
@@ -16,46 +9,46 @@ public class GrokConnectTest {
         //GrokConnect.connect("test");
     }
 
-    @Test
-    public void connectionPoolTest() {
-        ConnectionPool cp = ConnectionPool.getInstance();
-        String url = "jdbc:postgresql://localhost:5432/datagrok";
-        String driverName = "org.postgresql.Driver";
-        String query = "select count(1) from entities";
-
-        Properties prop1 = new Properties();
-        prop1.setProperty("user", "datagrok");
-        prop1.setProperty("password", "datagrok");
-        try {
-            cp.getConnection(url, prop1, driverName).createStatement(); // create connection
-        } catch (Exception throwables) {
-            fail();
-        }
-
-        Properties prop2 = new Properties(); // different user
-        prop2.setProperty("user", "test_user");
-        prop2.setProperty("password", "test_user");
-        try {
-            cp.getConnection(url, prop2, driverName).createStatement(); // create connection
-        } catch (Exception throwables) {
-            fail();
-        }
-
-        try {
-            Statement statement = cp.getConnection(url, prop1, driverName).createStatement(); // use connection
-            statement.execute(query);
-        } catch (Exception throwables) {
-            fail();
-        }
-
-        try {
-            Statement statement = cp.getConnection(url, prop2, driverName).createStatement(); // use connection
-            statement.execute(query);
-            fail();
-        } catch (Exception throwables) {
-            assertEquals("ERROR: permission denied for table entities", throwables.getMessage());
-        }
-
-        assertEquals(2, cp.connectionPool.size());
-    }
+//    @Test
+//    public void connectionPoolTest() {
+//        ConnectionPool cp = ConnectionPool.getInstance();
+//        String url = "jdbc:postgresql://localhost:5432/datagrok";
+//        String driverName = "org.postgresql.Driver";
+//        String query = "select count(1) from entities";
+//
+//        Properties prop1 = new Properties();
+//        prop1.setProperty("user", "datagrok");
+//        prop1.setProperty("password", "datagrok");
+//        try {
+//            cp.getConnection(url, prop1, driverName).createStatement(); // create connection
+//        } catch (Exception throwables) {
+//            fail();
+//        }
+//
+//        Properties prop2 = new Properties(); // different user
+//        prop2.setProperty("user", "test_user");
+//        prop2.setProperty("password", "test_user");
+//        try {
+//            cp.getConnection(url, prop2, driverName).createStatement(); // create connection
+//        } catch (Exception throwables) {
+//            fail();
+//        }
+//
+//        try {
+//            Statement statement = cp.getConnection(url, prop1, driverName).createStatement(); // use connection
+//            statement.execute(query);
+//        } catch (Exception throwables) {
+//            fail();
+//        }
+//
+//        try {
+//            Statement statement = cp.getConnection(url, prop2, driverName).createStatement(); // use connection
+//            statement.execute(query);
+//            fail();
+//        } catch (Exception throwables) {
+//            assertEquals("ERROR: permission denied for table entities", throwables.getMessage());
+//        }
+//
+//        assertEquals(2, cp.connectionPool.size());
+//    }
 }
