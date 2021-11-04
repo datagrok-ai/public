9f3d3db80f165951b33062c7a6e364e58f0209fe
skalkin <skalkin@gmail.com>
2021-11-04 17:09:53 -0400

    #180: Compute: UX harmonization - created ModelCatalogView class, added a menu and a toolbox

---
+++ packages/Compute/src/model-catalog-view.ts
@@ -0,0 +1,35 @@
+import * as grok from 'datagrok-api/grok';
+import * as ui from 'datagrok-api/ui';
+import * as DG from 'datagrok-api/dg';
+import {_package} from './package';
+import {ModelHandler} from "./model_handler";
+let api = <any>window;
+
+export class ModelCatalogView extends DG.CardView {
+
+  constructor() {
+    super(api.grok_CardView_Create({dataSource: grok.dapi.scripts, permanentFilter: '#model'}));
+
+    this.meta = new ModelHandler();
+    this.name = 'Models';
+    this.permanentFilter = '#model';
+
+    this.initToolbox();
+    this.initRibbon();
+  }
+
+  initRibbon() {
+    this.ribbonMenu
+      .group('Models')
+      .item('Add New...', () => grok.shell.info('Adding new model'));
+  }
+
+  initToolbox() {
+    grok.dapi.scripts
+      .filter('#model')
+      .list()
+      .then((models) => {
+        this.toolbox = ui.list(models);
+      });
+  }
+}
