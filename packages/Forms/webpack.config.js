2ae745bf99bc4f8d661768582988ccd4d7e7c622
Umid Shahmaliyev <quzayli@gmail.com>
2021-11-06 06:04:24 +0400

    First commit

---
+++ packages/Forms/webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path')
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    package: './src/package.js'
+  },
+  devtool: 'inline-source-map',
+  externals: {
+    'datagrok-api/dg': 'DG',
+    'datagrok-api/grok': 'grok',
+    'datagrok-api/ui': 'ui',
+    'openchemlib/full.js': 'OCL',
+    'rxjs': 'rxjs',
+    'rxjs/operators': 'rxjs.operators',
+    'cash-dom': '$',
+    'dayjs': 'dayjs',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|svg)(\?.*$|$)/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]'
+        },
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js']
+  },
+  output: {
+    filename: '[name].js',
+    library: 'forms',
+    libraryTarget: 'var',
+    path: path.resolve(__dirname, 'dist'),
+  },
+}
