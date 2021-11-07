66fef43cc15a0e87839a2a6e9965b864cfcd0063
skalkin <skalkin@gmail.com>
2021-11-07 13:20:18 -0500

    A snippet that demonstrates different ways to create a viewer

---
+++ packages/ApiSamples/scripts/ui/viewers/create-viewers-dynamically.js
@@ -0,0 +1,11 @@
+// creating viewers dynamically
+
+let t = grok.data.demo.demog();
+
+// dynamically
+let scatterPlot = DG.Viewer.fromType(DG.VIEWER.SCATTER_PLOT, t);
+
+// fluent API
+let barChart = t.plot.bar();
+
+ui.divV([scatterPlot.root, barChart.root])
