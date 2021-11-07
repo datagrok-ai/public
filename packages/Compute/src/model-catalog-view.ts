b6ffc12a0e294a0bf9947f259aa22ce6c13e4a4a
Aufar Zakiev <zaufar@gmail.com>
2021-11-07 19:29:34 +0300

    Compute: renamed the files

--- packages/Compute/src/model-catalog-view.ts
+++ packages/Compute/src/model-catalog-view.ts
@@ -1,12 +1,11 @@
 import * as grok from 'datagrok-api/grok';
 import * as ui from 'datagrok-api/ui';
 import * as DG from 'datagrok-api/dg';
-import {ModelHandler} from "./model_handler";
-import {onboardModel} from "./onboard-model";
-let api = <any>window;
+import {ModelHandler} from './model-handler';
+import {onboardModel} from './onboard-model';
+const api = <any>window;
 
 export class ModelCatalogView extends DG.CardView {
-
   constructor() {
     super(api.grok_CardView_Create({dataSource: grok.dapi.scripts, permanentFilter: '#model'}));
 
@@ -26,10 +25,10 @@ export class ModelCatalogView extends DG.CardView {
   initMenu() {
     this.ribbonMenu
       .group('Models')
-        .item('Add New...', () => onboardModel())
+      .item('Add New...', () => onboardModel())
       .endGroup()
       .group('Help')
-        .item('Compute Engine', () => window.open('https://github.com/datagrok-ai/public/tree/master/packages/Compute', '_blank'))
+      .item('Compute Engine', () => window.open('https://github.com/datagrok-ai/public/tree/master/packages/Compute', '_blank'));
   }
 
   initToolbox() {
@@ -37,13 +36,13 @@ export class ModelCatalogView extends DG.CardView {
       .filter('#model')
       .list()
       .then((models) => {
-        this.toolbox = ui.divV(models.map(model => ui.render(model, {onClick: (_) => this.setCurrentModel(model)})));
+        this.toolbox = ui.divV(models.map((model) => ui.render(model, {onClick: (_) => this.setCurrentModel(model)})));
       });
   }
 
   setCurrentModel(model: DG.Script) {
-    let modelView = DG.View.forObject(model)!;
-    let host: HTMLDivElement = this.root.querySelector('.grok-gallery-grid')!
+    const modelView = DG.View.forObject(model)!;
+    const host: HTMLDivElement = this.root.querySelector('.grok-gallery-grid')!;
     ui.empty(host);
     host.appendChild(modelView.root);
   }
