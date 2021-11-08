6e388569a3e69da72898ce6fc744d15f90ac0580
skalkin <skalkin@gmail.com>
2021-11-08 15:05:48 -0500

    GROK-9945: JS API: TabControl.onTabChanged

--- packages/ApiSamples/scripts/ui/components/tab-control.js
+++ packages/ApiSamples/scripts/ui/components/tab-control.js
@@ -2,14 +2,14 @@
 
 let v = grok.shell.newView('Tabs');
 
+let tabControl = ui.tabControl({
+  'FIRST': ui.panel('First panel', {style :{backgroundColor: '#EFE'}}),           // an element
+  'SECOND': () => ui.panel('Second panel', {style :{backgroundColor: '#EEF'}})    // a function that returns an element
+});
+
 // Simple API
 v.append(ui.h1('Horizontal'));
-v.append(
-    ui.tabControl({
-        'FIRST': ui.panel('First panel', {style :{backgroundColor: '#EFE'}}),           // an element
-        'SECOND': () => ui.panel('Second panel', {style :{backgroundColor: '#EEF'}})    // a function that returns an element
-    })
-);
+v.append(tabControl);
 
 // Detailed API
 v.append(ui.h1('Vertical'));
@@ -17,3 +17,6 @@ let tabsV = ui.tabControl(null, true);
 tabsV.addPane('FIRST', () => ui.panel('First panel', {style :{backgroundColor: '#EFE'}}));
 tabsV.addPane('SECOND', () => ui.panel('Second panel', {style :{backgroundColor: '#EEF'}}));
 v.append(tabsV);
+
+// events
+tabControl.onTabChanged.subscribe((_) => grok.shell.info(tabControl.currentPane.name));
