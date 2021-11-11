3b80dc0bd7137d14a47396def40314e914bd737e
Leonid <stolbovla@yandex.ru>
2021-11-11 21:03:36 +0300

    RepertoireBrowser: fix

--- packages/RepertoireBrowser/src/pViz.js
+++ packages/RepertoireBrowser/src/pViz.js
@@ -2,7 +2,7 @@ import * as grok from 'datagrok-api/grok';
 import * as ui from 'datagrok-api/ui';
 import * as DG from "datagrok-api/dg";
 
-import mutcodes from "./mutcodes.json";
+import mutcodes from "./externalData/mutcodes.json";
 import {MiscMethods} from "./misc.js"
 
 
