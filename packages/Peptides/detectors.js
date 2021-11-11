d39744450d77edc9e83d5a59c5566c9fe9d96e4f
Volodymyr Dyma <emissarys@protonmail.com>
2021-11-11 13:06:12 +0200

    Peptides: RegEx catastrophic backtracking fix

--- packages/Peptides/detectors.js
+++ packages/Peptides/detectors.js
@@ -3,7 +3,7 @@ class PeptidesPackageDetectors extends DG.Package {
   //input: column col
   //output: string semType
   detectAligned(col) {
-    const regexp = new RegExp('^((.+)?-){5,49}(\\w|\\(|\\))+$');
-    return DG.Detector.sampleCategories(col, (s) => s.length < 1000 && regexp.test(s)) ? 'alignedSequence' : null;
+    const regexp = new RegExp(/^([^-^\n]*-){2,49}(\\w|\\(|\\))+$/);
+    return DG.Detector.sampleCategories(col, (s) => regexp.test(s)) ? 'alignedSequence' : null;
   }
 }
