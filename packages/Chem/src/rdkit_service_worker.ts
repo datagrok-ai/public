e0b3cf37fd6ab278f956fc7c40092edabf3fce77
dskatov <dskatov@datagrok.ai>
2021-11-17 14:55:31 +0300

    #181 Chem: Workers. wip: Restructure service and service workers

---
+++ packages/Chem/src/rdkit_service_worker.ts
@@ -0,0 +1,7 @@
+import {RdKitServiceWorkerMisc} from './rdkit_service_worker_misc';
+
+export class RdKitServiceWorker extends RdKitServiceWorkerMisc {
+  constructor(module: Object) {
+    super(module);
+  }
+}
