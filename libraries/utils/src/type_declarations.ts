26ce31b2d7466781c676aae3f2371b51dd56cfde
Nikolay Alemasov <nalemasov@datagrok.ai>
2021-11-18 13:01:03 +0700

    Utils: * Update operations to in-house implementation. * Add JSDoc.
    Peptides:
    * Update similarity space to use new Utils.
    * Add zoom out to fit viewport.

--- libraries/utils/src/type_declarations.ts
+++ libraries/utils/src/type_declarations.ts
@@ -1,8 +1,54 @@
-import * as vec from 'vectorious';
+/**
+ * Denotes a vector of floating poit values.
+ *
+ * @export
+ * @class Vector
+ * @extends {Float32Array}
+ */
+export class Vector extends Float32Array {}
 
-export import Matrix = vec.NDArray;
+/**
+ * Denotes a two-dimensional matrix.
+ *
+ * @export
+ * @class Matrix
+ * @extends {Array<Vector>}
+ */
+export class Matrix extends Array<Vector> {}
 
+/**
+ * Denotes cartesian coordinates.
+ *
+ * @export
+ * @class Coordinates
+ * @extends {Matrix}
+ */
+export class Coordinates extends Matrix {}
+
+/**
+ * Denotes an array of arbitrary-typed vectors.
+ *
+ * @export
+ * @class Vectors
+ * @extends {Array<any>}
+ */
+export class Vectors extends Array<any> {}
+
+/**
+ * Denotes a dictionary containing function options.
+ *
+ * @export
+ * @type Options
+ */
 export type Options = {[name: string]: any};
-export type DistanceMetric = (v1: any, v2: any) => (number);
-export type Coordinates = Array<Float32Array>;
-export type Vectors = Array<any>;
+
+/**
+ * Denotes custom distance metric between the two given vectors.
+ *
+ * @export
+ * @type DistanceMetric
+ * @param {any} v1 The first vector.
+ * @param {any} v2 The second vector.
+ * @return {number} Distance between these two vectors.
+ */
+ export type DistanceMetric = (v1: any, v2: any) => (number);
