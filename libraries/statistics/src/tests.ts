f118c9caa1c49d9f48a39b195fa3e2420fab073d
Volodymyr Dyma <vdyma@its.jnj.com>
2021-11-18 16:38:53 +0200

    Peptides: various fixes and improvements

--- libraries/statistics/src/tests.ts
+++ libraries/statistics/src/tests.ts
@@ -9,7 +9,7 @@ type testStats = {
   'p-value less': number,
 };
 
-export function tTest(arr1: number[], arr2: number[], alpha=0.05, devKnown=false, devEqual=false): testStats {
+export function tTest(arr1: number[], arr2: number[], devKnown=false, devEqual=false): testStats {
   const m1: number = jStat.mean(arr1);
   const m2: number = jStat.mean(arr2);
   const v1: number = jStat.variance(arr1);
