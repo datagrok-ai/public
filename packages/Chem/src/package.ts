20d3c990206a1b83e4dcdde2aa8ef0ecb97f73dc
Alex Paramonov <aparamonov@datagrok.ai>
2021-11-23 15:21:34 +0300

    Fixed build issues

--- packages/Chem/src/package.ts
+++ packages/Chem/src/package.ts
@@ -1,6 +1,7 @@
 import * as grok from 'datagrok-api/grok';
 import * as ui from 'datagrok-api/ui';
 import * as DG from 'datagrok-api/dg';
+// @ts-ignore
 import {createRDKit} from './RDKit_minimal_2021.03_18.js';
 import {getMolColumnPropertyPanel} from './chem_column_property_panel';
 import * as chemSearches from './chem_searches';
