ed13b61805c61f8c2b03892d6d73588253e2fce0
Nikolay Alemasov <nalemasov@datagrok.ai>
2021-11-24 17:14:12 +0700

    Peptides: Remove console.log.

--- packages/Peptides/src/workers/dimensionality-reducer.ts
+++ packages/Peptides/src/workers/dimensionality-reducer.ts
@@ -1,8 +1,6 @@
 import {DimensionalityReducer} from '@datagrok-libraries/utils/src/reduce-dimensionality';
 
 function onMessage(columnData: [], method: string, measure: string, cyclesCount: number): Array<Float32Array> {
-  console.log('onMessage:');
-  console.log(`${columnData}, ${method}, ${measure}, ${cyclesCount}`);
   const reducer = new DimensionalityReducer(
     columnData,
     method,
@@ -14,10 +12,6 @@ function onMessage(columnData: [], method: string, measure: string, cyclesCount:
 
 self.onmessage = ({data: {columnData, method, measure, cyclesCount}}) => {
   const embedding = onMessage(columnData, method, measure, cyclesCount);
-  console.log('onmessage:');
-  console.log(embedding);
-  console.log(embedding.length);
-  console.log(embedding.toString());
   self.postMessage({
     embedding: embedding,
   });
