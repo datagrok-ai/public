717a97386a731e9eeb291faac3eaa1ff1b9eb752
dskatov <dskatov@datagrok.ai>
2021-11-25 00:57:04 +0300

    #181 Chem: Workers. wip: Access locks on grok API chem functions

--- packages/Chem/src/chem_common.ts
+++ packages/Chem/src/chem_common.ts
@@ -1,12 +1,66 @@
+const lockPromiseForKey: any = {};
+const unlockFunctionForKey: any = {};
+
+// By https://github.com/mistval/locko
+
+/**
+ * Take out a lock. When this function returns (asynchronously),
+ * you have the lock.
+ * @param {string} key - The key to lock on. Anyone else who
+ *   tries to lock on the same key will need to wait for it to
+ *   be unlocked.
+ */
+async function lock(key: string) {
+  if (!lockPromiseForKey[key]) {
+    lockPromiseForKey[key] = Promise.resolve();
+  }
+
+  const takeLockPromise = lockPromiseForKey[key];
+  lockPromiseForKey[key] = takeLockPromise.then(() => new Promise((fulfill) => {
+    unlockFunctionForKey[key] = fulfill;
+  }));
+
+  return takeLockPromise;
+}
+
+/**
+ * Release a lock.
+ * @param {string} key - The key to release the lock for.
+ *   The next person in line will now be able to take out
+ *   the lock for that key.
+ */
+function unlock(key: string) {
+  if (unlockFunctionForKey[key]) {
+    unlockFunctionForKey[key]();
+    delete unlockFunctionForKey[key];
+  }
+}
+
+/*
+
+let cntr = 0;
+
+export async function chemLock() {
+  await lock(cntr.toString());
+  cntr++;
+}
+
+export function chemUnlock() {
+  unlock(cntr.toString());
+  cntr--;
+}
+
+*/
+
 let _chemLocked = false;
 
-export function chemLock() {
+export async function chemLock() {
   if (_chemLocked) {
-    throw('RdKit Service usage locked');
+    throw(`RdKit Service usage locked:\n${(new Error()).stack}`);
   }
   _chemLocked = true;
 }
 
-export function chemUnlock() {
+export async function chemUnlock() {
   _chemLocked = false;
 }
