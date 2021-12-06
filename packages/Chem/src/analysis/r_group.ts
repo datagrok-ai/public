a7187d2feb80e6e5419159c02a546743997a0766
Volodymyr Dyma <emissarys@protonmail.com>
2021-12-06 12:00:39 +0200

    Chem: R Group Analysis fix

--- packages/Chem/src/analysis/r_group.ts
+++ packages/Chem/src/analysis/r_group.ts
@@ -5,8 +5,6 @@ import { mcsgetter } from '../scripts-api';
 import { getRGroups } from '../chem_rgroup_analysis';
 
 export function rGroupAnalysis(col: DG.Column) {
-  let sketcherSmile = '';
-
   let sketcher = new grok.chem.Sketcher();
   let columnPrefixInput = ui.stringInput('Column prefix', 'R');
   let visualAnalysisCheck = ui.boolInput('Visual analysis', true);
@@ -14,7 +12,6 @@ export function rGroupAnalysis(col: DG.Column) {
   let mcsButton = ui.button('MCS', async () => {
     let smiles: string = await mcsgetter(col.name, col.dataFrame);
     sketcher.setSmiles(smiles);
-    sketcherSmile = smiles;
   });
   ui.tooltip.bind(mcsButton, "Most Common Substructure");
   let mcsButtonHost = ui.div([mcsButton]);
@@ -32,18 +29,23 @@ export function rGroupAnalysis(col: DG.Column) {
       visualAnalysisCheck
     ]))
     .onOK(async () => {
-      let res = await getRGroups(col, sketcherSmile, columnPrefixInput.value);
-      for (let resCol of res.columns) {
-        resCol.semType = DG.SEMTYPE.MOLECULE;
-        col.dataFrame.columns.add(resCol);
-      }
-      if (res.columns.length == 0)
-        grok.shell.error("None R-Groups were found");
-      let view = grok.shell.getTableView(col.dataFrame.name);
-      if (visualAnalysisCheck.value && view) {
-        view.trellisPlot({
-          xColumnNames: [res.columns[0].name],
-          yColumnNames: [res.columns[1].name]});
+      const core = sketcher.getSmiles();
+      if (core !== null) {
+        let res = await getRGroups(col, core, columnPrefixInput.value);
+        for (let resCol of res.columns) {
+          resCol.semType = DG.SEMTYPE.MOLECULE;
+          col.dataFrame.columns.add(resCol);
+        }
+        if (res.columns.length == 0)
+          grok.shell.error("None R-Groups were found");
+        let view = grok.shell.getTableView(col.dataFrame.name);
+        if (visualAnalysisCheck.value && view) {
+          view.trellisPlot({
+            xColumnNames: [res.columns[0].name],
+            yColumnNames: [res.columns[1].name]});
+        }
+      } else {
+        grok.shell.error('No core was provided');
       }
     });
   dlg.show();
