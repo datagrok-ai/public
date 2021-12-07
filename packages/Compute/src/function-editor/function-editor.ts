ae530f9e7c96647ab1f18260a56f4015897315b7
Aufar Zakiev <zaufar@gmail.com>
2021-12-07 15:42:32 +0300

    Compute: added styling to FE preview

--- packages/Compute/src/function-editor/function-editor.ts
+++ packages/Compute/src/function-editor/function-editor.ts
@@ -2,6 +2,7 @@ import * as grok from 'datagrok-api/grok';
 import * as ui from 'datagrok-api/ui';
 import * as DG from 'datagrok-api/dg';
 import CodeMirror from 'codemirror';
+import 'codemirror/lib/codemirror.css';
 import {_package} from '../package';
 
 const enum FUNC_PROPS_FIELDS {
