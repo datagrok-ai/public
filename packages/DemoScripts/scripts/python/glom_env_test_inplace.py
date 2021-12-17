dabec24d2e652cad5db0f9258ced77e3df05c549
dskatov <dskatov@datagrok.ai>
2021-12-17 21:38:54 +0300

    GROK-10125 Scripting: in-place definition for environments

---
+++ packages/DemoScripts/scripts/python/glom_env_test_inplace.py
@@ -0,0 +1,14 @@
+#name: GlomEnvTestInplace
+#environment: channels: [conda-forge], dependencies: [python=3.7, requests, dataclasses, glom, {pip: [requests]}]
+#language: python
+#top-menu: Test | Environment Test (in-place)
+#output: string result
+
+import json, os, base64, uuid, re, requests
+import sys, getopt, datetime
+from dataclasses import dataclass
+from glom import glom
+import pandas as pd
+
+target = {'a': {'b': {'c': 'd'}}}
+result = glom(target, 'a.b.c')  # returns 'd'
