cf9b2a89a17513ff5eea112f6ae661b4d79d6f10
Aufar Zakiev <zaufar@gmail.com>
2021-12-21 15:22:28 +0300

    Compute: completely removed the outliers selection dialog

--- packages/Compute/src/package.ts
+++ packages/Compute/src/package.ts
@@ -3,7 +3,6 @@ import * as grok from 'datagrok-api/grok';
 import * as ui from 'datagrok-api/ui';
 import * as DG from 'datagrok-api/dg';
 import {ModelHandler} from './model-handler';
-import {selectOutliersManually} from './outliers-selection/outliers-selection';
 import {exportFuncCall} from './export-funccall';
 import {_functionParametersGrid} from './function-views/function-parameters-grid';
 import {ModelCatalogView} from './model-catalog-view';
@@ -17,34 +16,6 @@ export const _package = new DG.Package();
 //name: test
 export function test() {
   grok.shell.info(_package.webRoot);
-}
-
-//name: manualOutlierDetectionDialog
-//input: dataframe inputData
-//output: dataframe augmentedInput
-//output: dataframe editedInput
-export async function manualOutlierSelectionDialog(inputData: DG.DataFrame) {
-  const call = grok.functions.getCurrentCall();
-
-  const IS_OUTLIER_COL_LABEL = 'isOutlier';
-  const OUTLIER_REASON_COL_LABEL = 'Rationale';
-
-  if (call.options['interactive']) {
-    const {augmentedInput, editedInput} = await selectOutliersManually(inputData);
-    return {augmentedInput, editedInput};
-  }
-  return new Promise<{augmentedInput: DG.DataFrame, editedInput: DG.DataFrame}>((resolve, reject) => {
-    if (!inputData.columns.byName(IS_OUTLIER_COL_LABEL)) {
-      inputData.columns
-        .add(DG.Column.fromBitSet(IS_OUTLIER_COL_LABEL, DG.BitSet.create(inputData.rowCount, () => false)));
-    }
-
-    if (!inputData.columns.byName(OUTLIER_REASON_COL_LABEL)) {
-      inputData.columns
-        .add(DG.Column.fromStrings(OUTLIER_REASON_COL_LABEL, Array.from({length: inputData.rowCount}, () => '')));
-    }
-    resolve({augmentedInput: inputData, editedInput: inputData});
-  });
 }
 
 //name: OutliersSelectionViewer
