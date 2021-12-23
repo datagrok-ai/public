a55710b6bf57ef7863610bd8a6eb9d5508b2a47a
Volodymyr Dyma <emissarys@protonmail.com>
2021-12-23 21:40:51 +0200

    SQLite: types, optimizations and naming fix

--- packages/SQLite/src/package.ts
+++ packages/SQLite/src/package.ts
@@ -1,6 +1,7 @@
 import * as DG from 'datagrok-api/dg';
 //@ts-ignore: there are no types
 import initSqlJs from './sql-wasm.js';
+import * as sql from 'sql.js';
 
 export const _package = new DG.Package();
 
@@ -13,23 +14,24 @@ export const _package = new DG.Package();
 export async function importSQLite(bytes: number[]) {
   //TODO: use web worker?
   const progress = DG.TaskBarProgressIndicator.create('Loading SQLite tables...');
-  const SQL = await initSqlJs({locateFile: () => _package.webRoot + 'dist/sql-wasm.wasm'});
+  const SQL: sql.SqlJsStatic = await initSqlJs({locateFile: () => _package.webRoot + 'dist/sql-wasm.wasm'});
   const db = new SQL.Database(Uint8Array.from(bytes));
   const tableList = db.exec('SELECT `name` FROM `sqlite_master` WHERE type=\'table\';');
   const result = [];
 
   for (const tableName of tableList[0].values) {
     const statement = db.prepare(`SELECT * FROM ${tableName[0]};`);
-    const columns = statement.getColumnNames();
     const tableObjects = [];
 
     while (statement.step()) {
-      const row = statement.get();
-      const tableRow = Object.fromEntries(row.map((value: any, index: number) => [columns[index], value]));
-      tableObjects.push(tableRow);
+      const row = statement.getAsObject();
+      tableObjects.push(row);
     }
     statement.reset();
-    result.push(DG.DataFrame.fromObjects(tableObjects));
+
+    const currentDf = DG.DataFrame.fromObjects(tableObjects);
+    currentDf!.name = tableName[0]!.toString();
+    result.push(currentDf);
   }
   db.close();
   progress.close();
