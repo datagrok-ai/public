7850ff7c2159cb7ba9c1a4a9d41b231538be34f2
Alex Paramonov <aparamonov@datagrok.ai>
2021-12-23 16:25:06 +0300

    JS-API testing package

---
+++ packages/JsApiTests/js-api-tests/src/dataframe/df.ts
@@ -0,0 +1,12 @@
+import {category, test} from "../test";
+
+category('DF', () => {
+  test('dataframe 1', async () => {
+    throw 'Hi all';
+  });
+
+
+  test('dataframe 2', async () => {
+    return 'OK';
+  });
+});
---
+++ packages/JsApiTests/js-api-tests/src/package.ts
@@ -0,0 +1,27 @@
+/* Do not change these import lines. Datagrok will import API library in exactly the same manner */
+import * as grok from 'datagrok-api/grok';
+import * as ui from 'datagrok-api/ui';
+import * as DG from 'datagrok-api/dg';
+import './dataframe/df';
+import {category, tests} from "./test";
+
+export let _package = new DG.Package();
+
+//name: test
+export async function test() {
+  grok.shell.info(_package.webRoot);
+  let results = tests.map(async (t) => {
+    let r: {category?: String, name?: String, success: boolean, result: String};
+    try {
+      r = {success: true, result: await t.test()};
+    } catch (x: any) {
+      r = {success: false, result: x.toString()};
+    }
+    r.category = t.category;
+    r.name = t.name;
+    return r;
+  });
+
+  let data = await Promise.all(results);
+  grok.shell.addTableView(DG.DataFrame.fromObjects(data)!);
+}
