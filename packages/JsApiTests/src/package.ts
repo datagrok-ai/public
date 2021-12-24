f0099a52b649bf4501f914fe1200b174f17bf437
skalkin <skalkin@gmail.com>
2021-12-24 14:33:02 -0500

    ApiTests: added DataFrame construction tests

--- packages/JsApiTests/src/package.ts
+++ packages/JsApiTests/src/package.ts
@@ -2,7 +2,7 @@
 import * as grok from 'datagrok-api/grok';
 import * as ui from 'datagrok-api/ui';
 import * as DG from 'datagrok-api/dg';
-import './dataframe/df';
+import './dataframe/data_frame';
 import './shell/shell';
 import './shell/windows';
 import './viewer/viewer';
