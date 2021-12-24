08e1209662f23cc7fc0d4fc7c8c41841ba416e23
Alex Paramonov <aparamonov@datagrok.ai>
2021-12-24 12:50:09 +0300

    JS-API testing package

--- packages/JsApiTests/src/package.ts
+++ packages/JsApiTests/src/package.ts
@@ -3,25 +3,14 @@ import * as grok from 'datagrok-api/grok';
 import * as ui from 'datagrok-api/ui';
 import * as DG from 'datagrok-api/dg';
 import './dataframe/df';
-import {tests} from "./test";
+import './shell/shell';
+import {runTests} from "./test";
 
 export let _package = new DG.Package();
 
+
 //name: test
 export async function test() {
-
-  let results = tests.map(async (t) => {
-    let r: {category?: String, name?: String, success: boolean, result: String};
-    try {
-      r = {success: true, result: await t.test()};
-    } catch (x: any) {
-      r = {success: false, result: x.toString()};
-    }
-    r.category = t.category;
-    r.name = t.name;
-    return r;
-  });
-
-  let data = await Promise.all(results);
+  let data = await runTests();
   grok.shell.addTableView(DG.DataFrame.fromObjects(data)!);
 }
--- packages/JsApiTests/src/test.ts
+++ packages/JsApiTests/src/test.ts
@@ -1,24 +1,65 @@
 
 export let tests: Test[] = [];
-export let currentCategory: String;
+export let preconditions: {[key: string]: {before?: () => Promise<void>, after?: () => Promise<void>}} = {};
+
+export let currentCategory: string;
 
 export class Test {
-  test: Function;
-  name: String;
-  category: String;
+  test: () => Promise<any>;
+  name: string;
+  category: string;
 
-  constructor(category: String, name: String, test: Function) {
+  constructor(category: string, name: string, test: () => Promise<any>) {
     this.category = category;
     this.name = name;
     this.test = test;
   }
 }
 
-export function test(name: String, test: Function): void {
+export function test(name: string, test: () => Promise<any>): void {
   tests.push(new Test(currentCategory, name , test));
 }
 
-export function category(category: String, tests: Function): void {
+export function category(category: string, tests: () => void): void {
   currentCategory = category;
   tests();
+}
+
+export function before(before: () => Promise<void>): void {
+  if (preconditions[currentCategory] == undefined)
+    preconditions[currentCategory] = {};
+  preconditions[currentCategory].before = before;
+}
+
+export function after(after: () => Promise<void>): void {
+  if (preconditions[currentCategory] == undefined)
+    preconditions[currentCategory] = {};
+  preconditions[currentCategory].after = after;
+}
+
+
+export async function runTests() {
+  for (const [key, value] of Object.entries(preconditions)) {
+    if (value.before)
+      await value.before();
+  }
+  let results = tests.map(async (t) => {
+    let r: { category?: string, name?: string, success: boolean, result: string };
+    try {
+      r = {success: true, result: await t.test() ?? 'OK'};
+    } catch (x: any) {
+      r = {success: false, result: x.toString()};
+    }
+    r.category = t.category;
+    r.name = t.name;
+    return r;
+  });
+
+  let data = await Promise.all(results);
+
+  for (const [key, value] of Object.entries(preconditions)) {
+    if (value.after)
+      await value.after();
+  }
+  return data;
 }
