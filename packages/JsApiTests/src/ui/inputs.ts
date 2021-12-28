4630aeae6b38972f9a525aa6899bc88c2594df28
Dmitry Illarionov <illarionow@yahoo.com>
2021-12-28 14:36:38 +0100

    JSApiTests: UI test (Inputs)

--- packages/JsApiTests/src/ui/inputs.ts
+++ packages/JsApiTests/src/ui/inputs.ts
@@ -1,83 +1,87 @@
-import {after, before, category, delay, expect, test} from "../test";
+import { after, before, category, delay, expect, test } from "../test";
 import * as grok from 'datagrok-api/grok';
 import * as ui from 'datagrok-api/ui';
 import * as DG from 'datagrok-api/dg';
+import { caption, enabled, HTMLElement } from './utils';
 
 
-category('UI - Inputs', () => {
-    let v: DG.View;
-    const inputs = [
-        ui.stringInput('',''),
-        ui.intInput('', 0),
-        ui.floatInput('', 0.00),
-        ui.boolInput('',true),
-        ui.dateInput('', DG.DateTime.fromDate(new Date(2000, 1, 1))),
-        ui.textInput('',''),
-        ui.searchInput('','')
-    ];
+category('UI: Inputs', () => {
+  let v: DG.View;
+  let t = grok.data.testData('demog', 100);
+  let tables = grok.shell.tables;
+  const inputs = {
+    'stringInput': ui.stringInput('', ''),
+    'intInput': ui.intInput('', 0),
+    'floatInput': ui.floatInput('', 0.00),
+    'boolInput': ui.boolInput('', true),
+    'switchInput': ui.switchInput('', true),
+    'choiceInput': ui.choiceInput('', '', []),
+    'multiChoiceInput': ui.multiChoiceInput('', [], []),
+    'dateInput': ui.dateInput('', DG.DateTime.fromDate(new Date(2000, 1, 1))),
+    'textInput': ui.textInput('', ''),
+    'searchInput': ui.searchInput('', ''),
+    'columnInput': ui.columnInput('', t, t.col('age')),
+    'tableInput': ui.tableInput('', tables[0], tables, (table: any) => grok.shell.info(table.name))
+  };
 
-    before(async () => {
-        v = grok.shell.newView('');
-    });
+  before(async () => {
+    v = grok.shell.newView('');
+  });
 
-    test('input.root', async () => {
-        for (const input of inputs){
-            checkHTMLElement(input, '.ui-input-root');
-        }
-    })
-
-    test('input.input', async () => {
-        for (const input of inputs){
-            checkHTMLElement(input, '.ui-input-editor');
-        }
-    })
-
-    test('input.captionLabel', async () => {
-        for (const input of inputs){
-            checkHTMLElement(input, '.ui-input-label');
-        }
-    })
-
-    test('input.caption', async () => {
-        for (const input of inputs){
-            checkStringValue(input, '.ui-input- ');
-        }
-    })
-
-    test('input.stringValue', async () => {
-        for (const input of inputs){
-            checkStringValue(input, '.ui-input-editor');
-        }
-    })
-
-
-
-    after(async () => {
-        v.close();
-        grok.shell.closeAll();
-    });
-
-    function checkHTMLElement(input: DG.InputBase, selector: string): void {
-        v.append(input.root);
-        let e = v.root.querySelector(selector);
-        if (e == undefined)
-        throw `Element "${selector}" not found`;
-        input.root.remove();
+  test('input.root', async () => {
+    for (const [key, value] of Object.entries(inputs)) {
+      HTMLElement(key, value, v, '.ui-input-root');
     }
+  })
 
-    function checkStringValue(input: DG.InputBase, selector: string): void {
-        v.append(input.root);
-        let value = (<HTMLInputElement>v.root.querySelector(selector)).innerText;
-        try {
-            expect(input.caption, value)
-        }
-        catch (x){
-            throw x;
-        }
-        finally{
-            input.root.remove();
-        }
+  test('input.input', async () => {
+    for (const [key, value] of Object.entries(inputs)) {
+      HTMLElement(key, value, v, '.ui-input-editor');
     }
+  })
+
+  test('input.captionLabel', async () => {
+    for (const [key, value] of Object.entries(inputs)) {
+      HTMLElement(key, value, v, '.ui-input-label');
+    }
+  })
+
+  test('input.caption', async () => {
+    for (const [key, value] of Object.entries(inputs)) {
+      caption(key, value, v, '.ui-input-label');
+    }
+  })
+
+  test('input.stringValue', async () => {
+    for (const [key, value] of Object.entries(inputs)) {
+      stringValue(key, value, '.ui-input-editor');
+    }
+  })
+
+  test('input.ebabled', async () => {
+    for (const [key, value] of Object.entries(inputs)) {
+      enabled(key, value, v, '.ui-input-root');
+    }
+  })
+
+  after(async () => {
+    v.close();
+    grok.shell.closeAll();
+  });
+
+  function stringValue(name: string, input: DG.InputBase, selector: string): void {
+    v.append(input.root);
+    let value: string;
+    try {
+      value = (<HTMLInputElement>v.root.querySelector(selector)).value;
+      expect(input.stringValue, value)
+    }
+    catch (x) {
+      throw name + ': ' + x;
+    }
+    finally {
+      input.root.remove();
+    }
+  }
 
 });
-
