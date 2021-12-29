8c6432efcb9392a3efdcd9500cc3b133698cac45
Nikolay Alemasov <nalemasov@datagrok.ai>
2021-12-29 10:41:12 +0700

    PDBIDViewer #229: Small fixes and refactoring.
    * Fix comments to follow code convention.
    * Rewrite genRange to be based on typed array.
    * Move genRange into utils.

--- libraries/utils/src/operations.ts
+++ libraries/utils/src/operations.ts
@@ -158,3 +158,20 @@ export function calcDistanceMatrix(data: Vectors, distance: DistanceMetric): Mat
   }
   return matrix;
 }
+
+/**
+ * Generates array from a range.
+ *
+ * @param {number} begin First position to include.
+ * @param {number} end Last position to include.
+ * @return {Int32Array} Array corresponding the range given.
+ */
+export function genRange(begin: number, end: number): Int32Array {
+  const nItems = end - begin;
+  const series = new Int32Array(nItems);
+
+  for (let i = 0; i < nItems; ++i) {
+    series[i] = i;
+  }
+  return series;
+}
