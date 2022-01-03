e5924c59a53ab2940fe2a3a05daad9616f2d9767
skalkin <skalkin@gmail.com>
2022-01-03 17:58:47 -0500

    GROK-10233: Chem: move semantic type detectors to the Chem package

--- packages/Chem/detectors.js
+++ packages/Chem/detectors.js
@@ -1,16 +1,25 @@
 class ChemPackageDetectors extends DG.Package {
 
+  static likelyNames = [
+    'structure', 'mol', 'molecule', 'smiles', 'rdkit',
+    'canonical_smiles', 'core', 'scaffold',
+    'r1', 'r2', 'r3', 'r4', 'r5'];
+
   //tags: semTypeDetector
   //input: column col
   //output: string semType
   detectRDSmiles(col) {
-    if ((
-      col.name.toLowerCase().endsWith('smiles') ||
-      col.name.toLowerCase().endsWith('rdkit') ||
-      col.name.toLowerCase().endsWith('molecule') ||
-      col.name.toLowerCase().endsWith('scaffold')
-    ) && col.type === DG.TYPE.STRING) {
+    if (col.type !== DG.TYPE.STRING)
+      return null;
+
+    let name = col.name.toLowerCase();
+    if (ChemPackageDetectors.likelyNames.some((likelyName) => name.endsWith(likelyName))) {
       col.semType = DG.SEMTYPE.MOLECULE;
+
+      // smiles or molblock?
+      let str = col.length > 0 ? col.get(0) : null;
+      col.tags[DG.TAGS.UNITS] = (str !== null && str.includes('M  END')) ? DG.UNITS.Molecule.MOLBLOCK : DG.UNITS.Molecule.SMILES;
+
       return col.semType;
     }
 
