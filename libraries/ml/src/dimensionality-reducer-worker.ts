9831e913d1be9cd7b2d0c85fa71c2d3ba48be714
Nikolay Alemasov <nalemasov@datagrok.ai>
2022-01-07 13:34:54 +0700

    ML #248: Add worker to dimensionality reducer.

---
+++ libraries/ml/src/dimensionality-reducer-worker.ts
@@ -0,0 +1,28 @@
+import {DimensionalityReducer, KnownMethods} from './reduce-dimensionality';
+import {Coordinates} from '@datagrok-libraries/utils/src/type-declarations';
+
+/**
+ * Worker thread receiving data function.
+ *
+ * @param {any[]} columnData Samples to process.
+ * @param {string} method Embedding method.
+ * @param {string} measure Distance metric.
+ * @param {number} cyclesCount Number of cycles to repeat.
+ * @return {Coordinates} Embedding.
+ */
+function onMessage(columnData: [], method: KnownMethods, measure?: string, cyclesCount?: number): Coordinates {
+  const reducer = new DimensionalityReducer(
+    columnData,
+    method,
+    measure,
+    cyclesCount ? {cycles: cyclesCount} : undefined,
+  );
+  return reducer.transform(true);
+}
+
+self.onmessage = ({data: {columnData, method, measure, cyclesCount}}) => {
+  const embedding = onMessage(columnData, method, measure, cyclesCount);
+  self.postMessage({
+    embedding: embedding,
+  });
+};
