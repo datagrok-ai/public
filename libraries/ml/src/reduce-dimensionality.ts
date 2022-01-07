c977e1e0b1b8d0edb2b6bccdeb918e2a99b8e168
Nikolay Alemasov <nalemasov@datagrok.ai>
2022-01-07 13:34:54 +0700

    ML #248: Add worker to dimensionality reducer.

--- libraries/ml/src/reduce-dimensionality.ts
+++ libraries/ml/src/reduce-dimensionality.ts
@@ -284,3 +284,32 @@ export class DimensionalityReducer {
     return Object.keys(AvailableReducers);
   }
 }
+
+/**
+ * A worker to perform dimensionality reduction.
+ *
+ * @param {any[]} columnData The data to process.
+ * @param {string} method A method of dimensionality reduction.
+ * @param {string} measure A distance metrics.
+ * @param {number} cyclesCount Number of iterations to run.
+ * @return {Promise<unknown>} Resulting embedding.
+ */
+export function createDimensinalityReducingWorker(
+  columnData: any[],
+  method: string,
+  measure?: string,
+  cyclesCount?: number,
+): Promise<unknown> {
+  return new Promise(function(resolve) {
+    const worker = new Worker(new URL('../workers/dimensionality-reducer.ts', import.meta.url));
+    worker.postMessage({
+      columnData: columnData,
+      method: method,
+      measure: measure,
+      cyclesCount: cyclesCount,
+    });
+    worker.onmessage = ({data: {embedding}}) => {
+      resolve(embedding);
+    };
+  });
+}
