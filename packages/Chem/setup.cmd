74e5554ae55250c5f09711893c46379fc654c458
skalkin <skalkin@gmail.com>
2022-01-08 17:45:00 -0500

    Chem: Adjusted the setup script

--- packages/Chem/setup.cmd
+++ packages/Chem/setup.cmd
@@ -1,12 +1,16 @@
 call npm unlink datagrok-api
-call npm unlink @datagrok-tools/utils
+call npm unlink @datagrok-libraries/utils
+call npm unlink @datagrok-libraries/ml
 cd ../../js-api
 call npm install
 call npm link
 cd ../libraries/utils
 call npm install
 call npm link
+cd ../../libraries/ml
+call npm install
+call npm link
 cd ../../packages/Chem
 call npm install
-call npm link datagrok-api @datagrok-libraries/utils
+call npm link datagrok-api @datagrok-libraries/utils @datagrok-libraries/ml
 webpack
