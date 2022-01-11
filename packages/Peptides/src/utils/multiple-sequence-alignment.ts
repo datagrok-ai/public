e0236f909f9bca93e343b920c30b1d097e946311
Nikolay Alemasov <nalemasov@datagrok.ai>
2022-01-11 11:05:35 +0700

    Peptides #159: Fix update include libraries.

--- packages/Peptides/src/utils/multiple-sequence-alignment.ts
+++ packages/Peptides/src/utils/multiple-sequence-alignment.ts
@@ -1,14 +1,10 @@
 /* Do not change these import lines. Datagrok will import API library in exactly the same manner */
-// eslint-disable-next-line no-unused-vars
 import * as grok from 'datagrok-api/grok';
-// eslint-disable-next-line no-unused-vars
-import * as ui from 'datagrok-api/ui';
-// eslint-disable-next-line no-unused-vars
 import * as DG from 'datagrok-api/dg';
 
 import biomsa from 'biomsa';
 
-import {AlignedSequenceEncoder} from '@datagrok-libraries/utils/src/sequence-encoder';
+import {AlignedSequenceEncoder} from '@datagrok-libraries/bio/src/sequence-encoder';
 
 const BLOSUM62 = {
   matrix: [
