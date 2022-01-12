4237cd8370fe252a728cddad608de34e79d2a2ff
dskatov <dskatov@datagrok.ai>
2022-01-13 01:12:48 +0300

    GROK-7774 Chem: Proper error handling. wip

--- packages/Chem/src/chem-common-rdkit.ts
+++ packages/Chem/src/chem-common-rdkit.ts
@@ -64,48 +64,52 @@ export function getRdKitWebRoot() {
 export function drawMoleculeToCanvas(
   x: number, y: number, w: number, h: number,
   onscreenCanvas: HTMLCanvasElement, molString: string, scaffoldMolString: string | null = null) {
-  let mol = getRdKitModule().get_mol(convertToRDKit(molString));
-  const molBlock = mol.get_new_coords(true);
-  mol.delete();
-  mol = getRdKitModule().get_mol(molBlock);
-  mol.normalize_2d_molblock();
-  mol.straighten_2d_layout();
-  const scaffoldMol = scaffoldMolString == null ? null :
-    getRdKitModule().get_qmol(convertToRDKit(scaffoldMolString));
-  let substructJson = '{}';
-  if (scaffoldMol) {
-    substructJson = mol.get_substruct_match(scaffoldMol);
-    if (substructJson === '')
-      substructJson = '{}';
+  let mol = null;
+  try {
+    mol = getRdKitModule().get_mol(convertToRDKit(molString));
+    const molBlock = mol.get_new_coords(true);
+    mol.delete();
+    mol = getRdKitModule().get_mol(molBlock);
+    mol.normalize_2d_molblock();
+    mol.straighten_2d_layout();
+    const scaffoldMol = scaffoldMolString == null ? null :
+      getRdKitModule().get_qmol(convertToRDKit(scaffoldMolString));
+    let substructJson = '{}';
+    if (scaffoldMol) {
+      substructJson = mol.get_substruct_match(scaffoldMol);
+      if (substructJson === '')
+        substructJson = '{}';
+    }
+    // TODO: make this an optional parameter, AND a system-wide setting
+    const opts = {
+      'clearBackground': false,
+      'offsetx': Math.floor(x),
+      'offsety': Math.floor(y),
+      'width': Math.floor(w),
+      'height': Math.floor(h),
+      'bondLineWidth': 1,
+      'fixedScale': 0.07,
+      'minFontSize': 9,
+      'highlightBondWidthMultiplier': 12,
+      'dummyIsotopeLabels': false,
+      'atomColourPalette': {
+        16: [0.498, 0.247, 0.0],
+        9: [0.0, 0.498, 0.0],
+        17: [0.0, 0.498, 0.0],
+      },
+    };
+    if (scaffoldMol) {
+      const substruct = JSON.parse(substructJson);
+      Object.assign(opts, substruct);
+    }
+    // we need the offscreen canvas first to not let the molecule scaffold skew on a real canvas
+    let offscreenCanvas: OffscreenCanvas | null = new OffscreenCanvas(w, h);
+    mol.draw_to_canvas_with_highlights(offscreenCanvas, JSON.stringify(opts));
+    const image = offscreenCanvas!.getContext('2d')!.getImageData(0, 0, w, h);
+    const context = onscreenCanvas.getContext('2d');
+    context!.putImageData(image, x, y);
+    offscreenCanvas = null; // GC?
+  } finally {
+    mol?.delete();
   }
-  // TODO: make this an optional parameter, AND a system-wide setting
-  const opts = {
-    'clearBackground': false,
-    'offsetx': Math.floor(x),
-    'offsety': Math.floor(y),
-    'width': Math.floor(w),
-    'height': Math.floor(h),
-    'bondLineWidth': 1,
-    'fixedScale': 0.07,
-    'minFontSize': 9,
-    'highlightBondWidthMultiplier': 12,
-    'dummyIsotopeLabels': false,
-    'atomColourPalette': {
-      16: [0.498, 0.247, 0.0],
-      9: [0.0, 0.498, 0.0],
-      17: [0.0, 0.498, 0.0],
-    },
-  };
-  if (scaffoldMol) {
-    const substruct = JSON.parse(substructJson);
-    Object.assign(opts, substruct);
-  }
-  // we need the offscreen canvas first to not let the molecule scaffold skew on a real canvas
-  let offscreenCanvas: OffscreenCanvas | null = new OffscreenCanvas(w, h);
-  mol.draw_to_canvas_with_highlights(offscreenCanvas, JSON.stringify(opts));
-  const image = offscreenCanvas!.getContext('2d')!.getImageData(0, 0, w, h);
-  const context = onscreenCanvas.getContext('2d');
-  context!.putImageData(image, x, y);
-  offscreenCanvas = null; // GC?
-  mol.delete();
 }
