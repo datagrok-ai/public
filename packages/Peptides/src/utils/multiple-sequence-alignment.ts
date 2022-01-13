d951f9b2a4579089622c9b531336d2de58706d8d
Nikolay Alemasov <nalemasov@datagrok.ai>
2022-01-13 14:22:46 +0700

    Peptides #159: Fix kalign wasm and glue js.
    * Rebuild both files concurrently using the following command:
    export EMCC_CFLAGS="-s ENVIRONMENT='web' -s MODULARIZE=1 -s 'EXTRA_EXPORTED_RUNTIME_METHODS=[\"FS\", \"callMain\"]' -s EXPORT_NAME='initKalign' -s FORCE_FILESYSTEM=1" && emmake make
    * Limitation: webpack with the current config does not pack .wasm file correctly.

--- packages/Peptides/src/utils/multiple-sequence-alignment.ts
+++ packages/Peptides/src/utils/multiple-sequence-alignment.ts
@@ -1,42 +1,46 @@
-/* Do not change these import lines. Datagrok will import API library in exactly the same manner */
 import * as DG from 'datagrok-api/dg';
 
 import {AlignedSequenceEncoder} from '@datagrok-libraries/bio/src/sequence-encoder';
-import {FSInjector} from './fs-injector';
 
 // @ts-ignore
 import initKalign from '../wasm/kalign';
 
+/**
+ * Converts array of sequences into simple fasta string.
+ *
+ * @param {string[]} sequences Input list of sequences.
+ * @return {string} Fasta-formatted string.
+ */
+function _stringsToFasta(sequences: string[]): string {
+  return sequences.reduce((a, v, i) => a + `>sample${i + 1}\n${v}\n`, '');
+}
+
+/**
+ * Extracts array of sequences from simple fasta string.
+ *
+ * @param {string} fasta Fasta-formatted string.
+ * @return {string[]} Output list of sequences.
+ */
+function _fastaToStrings(fasta: string): string[] {
+  return fasta.replace(/>sample\d+(\r\n|\r|\n)/g, '').split('\n');
+}
+
 export async function runKalign(col: DG.Column, webRootValue: string) : Promise<DG.Column> {
+  // eslint-disable-next-line max-len
+  // export EMCC_CFLAGS="-s ENVIRONMENT='web' -s MODULARIZE=1 -s 'EXTRA_EXPORTED_RUNTIME_METHODS=[\"FS\", \"callMain\"]' -s EXPORT_NAME='initKalign' -s FORCE_FILESYSTEM=1" && emmake make
   const _webPath = `${webRootValue}dist/kalign.wasm`;
   const sequences = col.toList().map((v: string, _) => AlignedSequenceEncoder.clean(v).replace(/\-/g, ''));
-  const stdin = sequences.reduce((a, v, i) => a + `>sample${i}\n${v}\n`);
-  const inj = new FSInjector(stdin);
+  const fasta = _stringsToFasta(sequences);
 
-  console.log(initKalign);
-  console.log(_webPath);
-
-  let kalign = await initKalign({
-    locateFile: () => _webPath,
+  const args = '-i input.fa -o output.fa'.split(' ');
+  const kalign = await initKalign({
     noInitialRun: true,
-  });
-
-  const Module = {
-    preRun: function() {
-      console.log('preRun');
-      inj.reset();
-      kalign.FS.init(inj.stdin.bind(inj), inj.stdout.bind(inj), inj.stderr.bind(inj));
-    },
     locateFile: () => _webPath,
-  };
-
-  console.log(kalign);
-  console.log(Module);
-
-  kalign = await initKalign(Module);
-  console.log(kalign);
-  await kalign.run();
-  console.log(inj);
-
-  return DG.Column.fromStrings('aligned', inj.stdoutBuffer.split(/>sample\d+\n/g));
+  });
+  kalign.FS.writeFile('input.fa', fasta, {encoding: 'utf8'});
+  const ret = kalign.callMain(args);
+  console.log(`main(argc, argv) returned ${ret}`);
+  const buf = kalign.FS.readFile('output.fa', {encoding: 'utf8'}) as string;
+  const aligned = _fastaToStrings(buf).slice(0, sequences.length);
+  return DG.Column.fromStrings('aligned', aligned);
 }
