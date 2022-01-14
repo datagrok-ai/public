53f898acda88c3542b66e9bc8606257f52c46532
skalkin <skalkin@gmail.com>
2022-01-14 16:11:44 -0500

    PowerPack: added tests for search templates

--- packages/PowerPack/src/package-test.ts
+++ packages/PowerPack/src/package-test.ts
@@ -1,7 +1,8 @@
 import * as DG from "datagrok-api/dg";
 import * as grok from "datagrok-api/grok";
 import {runTests} from "@datagrok-libraries/utils/src/test";
-import "./widgets/widgets-tests.ts";
+import "./widgets/widgets-test.ts";
+import "./search/search-test.ts";
 
 export let _package = new DG.Package();
 
---
+++ packages/PowerPack/src/search/search-test.ts
@@ -0,0 +1,11 @@
+import {after, before, category, expect, test} from "@datagrok-libraries/utils/src/test";
+import * as grok from 'datagrok-api/grok';
+import * as ui from 'datagrok-api/ui';
+import * as DG from 'datagrok-api/dg';
+import {initTemplates} from "./templates-search";
+
+category('Widgets', () => {
+
+  test('InitTemplates', async () => initTemplates());
+
+});
