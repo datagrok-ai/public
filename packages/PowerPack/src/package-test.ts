688f256f58d2a5d7af06f99fe1b91d55647445d6
skalkin <skalkin@gmail.com>
2022-01-14 15:58:05 -0500

    PowerPack: added tests for widgets

---
+++ packages/PowerPack/src/package-test.ts
@@ -0,0 +1,13 @@
+import * as DG from "datagrok-api/dg";
+import * as grok from "datagrok-api/grok";
+import {runTests} from "@datagrok-libraries/utils/src/test";
+import "./widgets/widgets-tests.ts";
+
+export let _package = new DG.Package();
+
+//name: test
+//output: dataframe result
+export async function test(): Promise<DG.DataFrame> {
+  let data = await runTests();
+  return DG.DataFrame.fromObjects(data)!;
+}
