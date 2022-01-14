b0c9b9d24217a97b132675c7aaeb5a2c78920e0a
Nikolay Alemasov <nalemasov@datagrok.ai>
2022-01-14 18:06:32 +0700

    BiostructureViewer #229: Add sample PDB id test.

--- packages/BiostructureViewer/src/tests/pdb-tests.ts
+++ packages/BiostructureViewer/src/tests/pdb-tests.ts
@@ -1,6 +1,7 @@
 import {category/*, expect*/, test} from '@datagrok-libraries/utils/src/test';
 import {
   _testRCSBAlive,
+  _testParse2V0A,
   /*requireText,*/
 } from './utils';
 /*
@@ -12,5 +13,9 @@ import * as ui from 'datagrok-api/ui';
 category('biosv', () => {
   test('biosv.pdb_entry.RSCB_is_alive', async () => {
     await _testRCSBAlive();
+  });
+
+  test('biosv.pdb_entry.Parse_sample_2V0A', async () => {
+    await _testParse2V0A();
   });
 });
