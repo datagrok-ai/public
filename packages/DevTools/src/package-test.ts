2979e7aac2d4af9e477ce7423eb98c3201104c6a
Aufar Zakiev <zaufar@gmail.com>
2022-01-17 15:48:58 +0300

    DevTools: added tests

---
+++ packages/DevTools/src/package-test.ts
@@ -0,0 +1,17 @@
+import * as DG from "datagrok-api/dg";
+import * as grok from "datagrok-api/grok";
+import {runTests} from "@datagrok-libraries/utils/src/test";
+
+import "./tests/function-signature-editor-test";
+
+export let _package = new DG.Package();
+
+//name: test
+//input: string category {optional: true}
+//input: string test {optional: true}
+//output: dataframe result
+//top-menu: Tools | Dev | JS API Tests
+export async function test(category: string, test: string): Promise<DG.DataFrame> {
+  let data = await runTests({category, test});
+  return DG.DataFrame.fromObjects(data)!;
+}
--- packages/DevTools/webpack.config.js
+++ packages/DevTools/webpack.config.js
@@ -1,9 +1,11 @@
 const path = require('path');
+const packageName = path.parse(require('./package.json').name).name.toLowerCase().replace(/-/g, '');
 
 module.exports = {
   mode: 'development',
   entry: {
-    package: './src/package.ts'
+    package: './src/package.ts',
+    test: {filename: 'package-test.js', library: {type: 'var', name:`${packageName}_test`}, import: './src/package-test.ts'},
   },
   resolve: {
     extensions: ['.js', '.json', '.ts'],
