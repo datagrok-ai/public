34b062ce0bdeaba13c79cca1c36260d71e05f4f6
Nuradil <mr.nuradil@gmail.com>
2022-01-18 18:04:47 +0600

    fix similarity metrics

--- packages/Chem/src/chem-common.ts
+++ packages/Chem/src/chem-common.ts
@@ -43,7 +43,7 @@ export function chemEndCriticalSection(token = CHEM_TOKEN) {
   criticalSectionEnd(token);
 }
 
-export function tanimoto(x: BitArray, y: BitArray) {
+export function tanimotoSimilarity(x: BitArray, y: BitArray) {
   const total = x.trueCount() + y.trueCount();
   if (total == 0)
     return 1.0;
@@ -51,78 +51,79 @@ export function tanimoto(x: BitArray, y: BitArray) {
   return common / (total - common);
 }
 
-export function dice(x: BitArray, y: BitArray) {
+export function diceSimilarity(x: BitArray, y: BitArray) {
   const total = x.trueCount() + y.trueCount();
   if (total == 0)
-    return 1.0;
-
+    return 0.0;
   const common = x.andWithCountBits(y, true);
   return 2 * common / total;
 }
 
-export function cosine(x: BitArray, y: BitArray) {
+export function cosineSimilarity(x: BitArray, y: BitArray) {
   const total = x.trueCount() * y.trueCount();
   if (total == 0)
-    return 1.0;
-
+    return 0.0;
   const common = x.andWithCountBits(y, true);
   return common / Math.sqrt(total);
 }
 
-export function sokal(x: BitArray, y: BitArray) {
+export function sokalSimilarity(x: BitArray, y: BitArray) {
   const total = x.trueCount() + y.trueCount();
-  if (total == 0)
-    return 1.0;
-
   const common = x.andWithCountBits(y, true);
   return common / (2 * total - 3 * common);
 }
 
-export function kulczynski(x: BitArray, y: BitArray) {
-  const totalProd = x.trueCount() * y.trueCount();
-  if (totalProd == 0)
-    return 1.0;
-
-  const common = x.andWithCountBits(y, true);
-  return (common * totalProd) / (2 * totalProd);
-}
-
-export function mcConnaughey(x: BitArray, y: BitArray) {
+export function kulczynskiSimilarity(x: BitArray, y: BitArray) {
   const total = x.trueCount() + y.trueCount();
   const totalProd = x.trueCount() * y.trueCount();
   if (totalProd == 0)
-    return 1.0;
+    return 0.0;
+  const common = x.andWithCountBits(y, true);
+  return (common * total) / (2 * totalProd);
+}
 
+export function mcConnaugheySimilarity(x: BitArray, y: BitArray) {
+  const total = x.trueCount() + y.trueCount();
+  const totalProd = x.trueCount() * y.trueCount();
+  if (totalProd == 0)
+    return 0.0;
   const common = x.andWithCountBits(y, true);
   return (common * total - totalProd) / totalProd;
 }
 
-export function asymmetric(x: BitArray, y: BitArray) {
+export function asymmetricSimilarity(x: BitArray, y: BitArray) {
   const min = Math.min(x.trueCount(), y.trueCount());
   if (min == 0)
-    return 1.0;
-
+    return 0.0;
   const common = x.andWithCountBits(y, true);
   return common / min;
 }
 
-export function braunBlanquet(x: BitArray, y: BitArray) {
+export function braunBlanquetSimilarity(x: BitArray, y: BitArray) {
   const max = Math.max(x.trueCount(), y.trueCount());
   if (max == 0)
-    return 1.0;
-
+    return 0.0;
   const common = x.andWithCountBits(y, true);
   return common / max;
 }
 
-export function russel(x: BitArray, y: BitArray) {
-  if (x.trueCount() == 0)
-    return 1.0;
-
+export function russelSimilarity(x: BitArray, y: BitArray) {
+  if (x.length == 0)
+    return 0.0;
   const common = x.andWithCountBits(y, true);
-  return common / x.trueCount();
+  return common / x.length;
 }
 
+export function rogotGoldbergSimilarity(x: BitArray, y: BitArray) {
+  let common = x.andWithCountBits(y, true);
+  let total = x.countBits(true) + y.countBits(true);
+  let len = x.length;
+  let diff = len - total + common;
+  if ((common == len) || (diff == len))
+    return 1.0;
+  else
+    return common / total + diff / (2 * len - total);
+}
 
 export function rdKitFingerprintToBitArray(fp: string, fpLength: number) {
   const arr = new BitArray(fpLength);
