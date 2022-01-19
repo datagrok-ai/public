bb6ea3f4d973b57f3017ef64499f32c4a6772d38
dskatov <dskatov@datagrok.ai>
2022-01-19 12:54:38 +0300

    #269 Chem: Progress state in Substructure Filter

--- packages/Chem/src/chem-substructure-filter.ts
+++ packages/Chem/src/chem-substructure-filter.ts
@@ -15,7 +15,12 @@ import Sketcher = chem.Sketcher;
 export class SubstructureFilter extends DG.Filter {
   column: DG.Column | null = null;
   sketcher: Sketcher = new Sketcher();
+  loader = ui.loader();
   readonly WHITE_MOL = '\n  0  0  0  0  0  0  0  0  0  0999 V2000\nM  END\n';
+
+  _indicateProgress(on = true) {
+    this.loader.style.display = on ? 'block' : 'none';
+  }
 
   get filterSummary(): string {
     return this.sketcher.getSmiles();
@@ -29,8 +34,13 @@ export class SubstructureFilter extends DG.Filter {
     super();
     /* No await! */ initRdKitService();
     this.root = ui.divV([]);
+    this._indicateProgress(false);
+    this.loader.style.position = 'absolute';
+    this.loader.style.right = '60px';
+    this.loader.style.top = '4px';
     this.sketcher.onChanged.subscribe((_) => this.dataFrame?.rows.requestFilter());
     this.root.appendChild(this.sketcher.root);
+    this.root!.firstChild!.firstChild!.firstChild!.appendChild(this.loader);
   }
 
   attach(dataFrame: DG.DataFrame) {
@@ -55,14 +65,16 @@ export class SubstructureFilter extends DG.Filter {
       this.reset();
       return;
     }
-    console.log('Substructure filtering is to be performed');
+
+    this._indicateProgress();
     (async() => {
       await chemSubstructureSearchLibrary(this.column!, this.sketcher.getMolFile(), await this.sketcher.getSmarts())
         .then((bitset) => {
-          console.log('Substructure filtering produced results');
+          // console.log('Substructure filtering produced results');
           this.dataFrame?.filter.and(bitset);
           this.column!.temp['chem-scaffold-filter'] = this.sketcher.getMolFile();
           this.dataFrame?.filter.fireChanged();
+          this._indicateProgress(false);
         }).catch((e) => {
         console.warn(e);
         this.reset();
