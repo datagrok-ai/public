45bb11885f49a240aa42ee0c55623f63be9b1f4c
dskatov <dskatov@datagrok.ai>
2022-01-20 01:42:07 +0300

    #269 Chem: Progress state in Substructure Filter #277 Chem: Substructure filter to DG.Filter

--- packages/Chem/src/chem-substructure-filter.ts
+++ packages/Chem/src/chem-substructure-filter.ts
@@ -4,12 +4,13 @@
  * 2. Call dataFrame.rows.requestFilter when filtering criteria changes.
  * */
 
-import * as grok from 'datagrok-api/grok';
 import * as ui from 'datagrok-api/ui';
 import * as DG from 'datagrok-api/dg';
 import {chem} from 'datagrok-api/grok';
 import {chemSubstructureSearchLibrary} from "./chem-searches";
 import {initRdKitService} from "./chem-common-rdkit";
+import {Observable, Subscription} from "rxjs";
+import {debounceTime} from 'rxjs/operators';
 import Sketcher = chem.Sketcher;
 
 export class SubstructureFilter extends DG.Filter {
@@ -17,6 +18,7 @@ export class SubstructureFilter extends DG.Filter {
   bitset: DG.BitSet | null = null;
   loader = ui.loader();
   readonly WHITE_MOL = '\n  0  0  0  0  0  0  0  0  0  0999 V2000\nM  END\n';
+  onSketcherChangedSubs?: Subscription;
 
   _indicateProgress(on = true) {
     this.loader.style.display = on ? 'block' : 'none';
@@ -38,18 +40,31 @@ export class SubstructureFilter extends DG.Filter {
     this.loader.style.position = 'absolute';
     this.loader.style.right = '60px';
     this.loader.style.top = '4px';
-
-    this.sketcher.onChanged.subscribe(async (_) => await this._onSketchChanged());
-
     this.root.appendChild(this.sketcher.root);
     this.root.firstChild!.firstChild!.firstChild!.appendChild(this.loader);
   }
 
-  attach(dataFrame: DG.DataFrame): void {
-    this.column = dataFrame.columns.bySemType(DG.SEMTYPE.MOLECULE);
-    this.columnName = this.column?.name;
-    super.attach(dataFrame);
+  get _debounceTime(): number {
+    const sz = this.column!.length;
+    const szMin = 500, szMax = 10000, msecMax = 1000;
+    if (sz < szMin) { return 0; }
+    if (sz > szMax) { return msecMax; }
+    return Math.floor(msecMax * ((sz - szMin) / (szMax - szMin)));
+  }
 
+  attach(dataFrame: DG.DataFrame): void {
+    if (this.onSketcherChangedSubs)
+      this.onSketcherChangedSubs.unsubscribe();
+    this.column = dataFrame.columns.bySemType(DG.SEMTYPE.MOLECULE);
+    chemSubstructureSearchLibrary(this.column!, '', ''); // No await
+    this.columnName = this.column?.name;
+    let onChangedEvent: Observable<any> = this.sketcher.onChanged;
+    const t = this._debounceTime;
+    if (t > 0)
+      onChangedEvent = onChangedEvent.pipe(debounceTime(t));
+    this.onSketcherChangedSubs = onChangedEvent
+      .subscribe(async _ => await this._onSketchChanged());
+    super.attach(dataFrame);
     if (this.column?.temp['chem-scaffold-filter'])
       this.sketcher.setMolFile(this.column?.temp['chem-scaffold-filter']);
   }
@@ -69,13 +84,13 @@ export class SubstructureFilter extends DG.Filter {
     } else {
       this._indicateProgress();
       try {
-        this.bitset = await chemSubstructureSearchLibrary(this.column!, this.sketcher.getMolFile(), await this.sketcher.getSmarts());
+        this.bitset = await chemSubstructureSearchLibrary(
+          this.column!, this.sketcher.getMolFile(), await this.sketcher.getSmarts());
       }
       finally {
         this._indicateProgress(false);
       }
     }
-
     this.dataFrame?.rows.requestFilter();
   }
 }
