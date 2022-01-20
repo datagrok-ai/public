9532cdfe92957341399f2bc5d46fb07d9524c8c4
Dmitry Illarionov <illarionow@yahoo.com>
2022-01-20 14:49:24 +0100

    Update format style

--- packages/ApiTests/src/ui/icons.ts
+++ packages/ApiTests/src/ui/icons.ts
@@ -5,38 +5,38 @@ import * as DG from 'datagrok-api/dg';
 import { checkHTMLElement } from './utils';
 
 category('UI: Icons', () => {
-    let v: DG.View;
+  let v: DG.View;
 
-    let icons = ui.icons;
-    let iconFA = ui.iconFA('cog');
-    let iconSVG = ui.iconSvg('column');
-    let iconImage = ui.iconImage('logo', 'http://datagrok.ai/img/logo.svg');
+  let icons = ui.icons;
+  let iconFA = ui.iconFA('cog');
+  let iconSVG = ui.iconSvg('column');
+  let iconImage = ui.iconImage('logo', 'http://datagrok.ai/img/logo.svg');
 
-    before(async () => {
-        v = grok.shell.newView('');
-      });
+  before(async () => {
+    v = grok.shell.newView('');
+  });
 
-    test('icons.root', async () => {
-        for (const [key, value] of Object.entries(icons)){
-            checkHTMLElement(key, value(()=>{}), v , '.grok-icon');
-        }
-    });
+  test('icons.root', async () => {
+    for (const [key, value] of Object.entries(icons)) {
+      checkHTMLElement(key, value(() => { }), v, '.grok-icon');
+    }
+  });
 
-    test('iconFA.root', async () => {
-        checkHTMLElement('iconFA', iconFA, v , ['.grok-icon', '.fal']);
-    });
+  test('iconFA.root', async () => {
+    checkHTMLElement('iconFA', iconFA, v, ['.grok-icon', '.fal']);
+  });
 
-    test('iconSVG.root', async () => {
-        checkHTMLElement('iconSVG', iconSVG, v , '.svg-icon');
-    });
+  test('iconSVG.root', async () => {
+    checkHTMLElement('iconSVG', iconSVG, v, '.svg-icon');
+  });
 
-    test('iconImage.root', async () => {
-        checkHTMLElement('iconImage', iconImage, v , '.image-icon');
-    });
+  test('iconImage.root', async () => {
+    checkHTMLElement('iconImage', iconImage, v, '.image-icon');
+  });
 
-    after(async () => {
-        v.close();
-        grok.shell.closeAll();
-    });
+  after(async () => {
+    v.close();
+    grok.shell.closeAll();
+  });
 
 });
--- packages/ApiTests/src/ui/tables.ts
+++ packages/ApiTests/src/ui/tables.ts
@@ -5,37 +5,37 @@ import * as DG from 'datagrok-api/dg';
 import { checkHTMLElement } from './utils';
 
 category('UI: Tables', () => {
-    let v: DG.View;
+  let v: DG.View;
 
-    let arr = [
-        {key: 'First', value: true},
-        {key: 'Second', value: false},
-        {key: 'Third', value: false},
-    ];
+  let arr = [
+    { key: 'First', value: true },
+    { key: 'Second', value: false },
+    { key: 'Third', value: false },
+  ];
 
-    let table = ui.table(arr, (item, idx)=>[item.key, item.value]);
-    let tableFromMap = ui.tableFromMap(arr);
-    let HTMLTable = DG.HtmlTable.create(arr,  (item:any, idx:any)=>[item.key, item.value]);
+  let table = ui.table(arr, (item, idx) => [item.key, item.value]);
+  let tableFromMap = ui.tableFromMap(arr);
+  let HTMLTable = DG.HtmlTable.create(arr, (item: any, idx: any) => [item.key, item.value]);
 
-    before(async () => {
-        v = grok.shell.newView('');
-      });
+  before(async () => {
+    v = grok.shell.newView('');
+  });
 
-    test('table.root', async () => {
-        checkHTMLElement('Table', table, v , 'table');
-    });
+  test('table.root', async () => {
+    checkHTMLElement('Table', table, v, 'table');
+  });
 
-    test('tableFromMap.root', async () => {
-        checkHTMLElement('tableFromMap', tableFromMap, v , 'table');
-    });
+  test('tableFromMap.root', async () => {
+    checkHTMLElement('tableFromMap', tableFromMap, v, 'table');
+  });
 
-    test('HTMLTable.root', async () => {
-        checkHTMLElement('HTMLTable', HTMLTable.root, v , 'table');
-    });
+  test('HTMLTable.root', async () => {
+    checkHTMLElement('HTMLTable', HTMLTable.root, v, 'table');
+  });
 
-    after(async () => {
-        v.close();
-        grok.shell.closeAll();
-    });
-    
+  after(async () => {
+    v.close();
+    grok.shell.closeAll();
+  });
+
 });
