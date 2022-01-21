7b91bd5eec811a7b44a53aa1334b0c88e56c76f1
Nuradil <mr.nuradil@gmail.com>
2022-01-21 18:20:15 +0600

    Resolve conflict

--- packages/Chem/src/chem-similarity-search.ts
+++ packages/Chem/src/chem-similarity-search.ts
@@ -1,82 +1,92 @@
 import * as grok from 'datagrok-api/grok';
 import * as ui from 'datagrok-api/ui';
 import * as DG from 'datagrok-api/dg';
-import {GridCellRenderArgs, Property, Widget} from 'datagrok-api/dg';
-// import {getMorganFingerprint, getMorganFingerprints} from './package';
+import { GridCellRenderArgs, Property, SIMILARITY_METRIC, Widget } from 'datagrok-api/dg';
 import * as chemSearches from './chem-searches';
 import {tanimotoSimilarity} from '@datagrok-libraries/utils/src/similarity-metrics';
+import * as metric from './chem-common';
+import $ from 'cash-dom';
 
-export class MoleculeViewer extends DG.JsViewer {
+export class SimilaritySearch extends DG.JsViewer {
   private moleculeColumnName: string;
   private initialized: boolean;
   private isEditedFromSketcher: boolean = false;
-  private hotSearch: boolean = true;
+  private hotSearch: boolean;
   private sketchButton: HTMLButtonElement;
+  private sketchedMolecule: string = "";
+  private distanceMetric: string;
+  private curIdx: number;
+  private molCol: DG.Column;
+  private idxs: DG.Column;
+  private scores: DG.Column;
+  private limit: number;
+  private minScore: number;
 
   constructor() {
     super();
 
-    // Register properties and define fields initialized to properties' default values
-    // Properties that represent columns should end with the 'ColumnName' postfix
-    this.moleculeColumnName = this.string('moleculeColumnName', 'smiles');
+    this.moleculeColumnName = this.string('moleculeColumnName');
+    this.limit = this.int('limit', 10);
+    this.minScore = this.float('minScore', 0.1);
+    this.distanceMetric = this.string('distanceMetric', 'tanimoto', {choices: Object.values(SIMILARITY_METRIC)});
+    this.hotSearch = this.bool('hotSearch', true);
     this.initialized = false;
     this.sketchButton = ui.button('Sketch', () => {
       let mol = '';
       this.isEditedFromSketcher = true;
-      const sketcher = grok.chem.sketcher((_: any, molfile: string) => {
-        mol = molfile;
+      const sketcher = grok.chem.sketcher((smiles: string, molfile: string) => {
+        mol = smiles;
         if (this.hotSearch) {
-          // this._search(mol).then();
+          this.sketchedMolecule = mol;
+          this.render();
         }
-      },
-      );
-      const dialog = ui.dialog()
-        .add(sketcher);
-      if (!this.hotSearch) {
-        dialog.onOK(() => {
-          // this._search(mol).then();
-        });
-      }
+      });
+      const dialog = ui.dialog().add(sketcher);
+      dialog.onOK(() => {
+        this.sketchedMolecule = mol;
+        this.render();
+      });
       dialog.show();
     });
     this.sketchButton.id = 'reference';
+    this.curIdx = 0;
+    this.molCol = DG.Column.fromList('string', 'foo', ['foo', 'boo']);
+    this.idxs = DG.Column.fromList('string', 'foo', ['foo', 'boo']);
+    this.scores = DG.Column.fromList('string', 'foo', ['foo', 'boo']);
   }
 
-  // Additional chart settings
   init(): void {
     this.initialized = true;
     this.isEditedFromSketcher = false;
     this.hotSearch = true;
   }
 
-  // Stream subscriptions
   async onTableAttached(): Promise<void> {
     this.init();
 
     if (this.dataFrame) {
-      this.subs.push(DG.debounce(this.dataFrame.onCurrentRowChanged, 50).subscribe(async (_) => await this.render()));
+      this.subs.push(DG.debounce(this.dataFrame.onMouseOverRowChanged, 50).subscribe(async (_) => await this.render()));
+      this.subs.push(DG.debounce(this.dataFrame.onCurrentRowChanged, 50).subscribe(async (_) => await this.render(false)));
       this.subs.push(DG.debounce(this.dataFrame.selection.onChanged, 50).subscribe(async (_) => await this.render(false)));
-      this.subs.push(DG.debounce(this.dataFrame.filter.onChanged, 50).subscribe(async (_) => await this.render()));
       this.subs.push(DG.debounce(ui.onSizeChanged(this.root), 50).subscribe(async (_) => await this.render(false)));
+      for (const col of this.dataFrame.columns) {
+        if (col.semType == 'Molecule') {
+          this.moleculeColumnName = col.name;
+        }
+      }
     }
 
     await this.render();
   }
 
-  // Cancel subscriptions when the viewer is detached
   detach() {
     this.subs.forEach((sub) => sub.unsubscribe());
   }
 
   onPropertyChanged(property: Property): void {
     super.onPropertyChanged(property);
-    if (this.initialized) {
-      if (property.name === 'moleculeColumnName' &&
-          this.dataFrame?.getCol(this.moleculeColumnName).type !== property.propertyType) {
-        grok.shell.error('Wrong property type');
-        return;
-      }
-    }
+    if (!this.initialized)
+      return;
     this.render();
   }
 
@@ -84,75 +94,96 @@ export class MoleculeViewer extends DG.JsViewer {
     if (!this.initialized)
       return;
 
-    if (this.dataFrame && computeData) {
+    if (this.dataFrame) {
+      this.curIdx = this.dataFrame.currentRowIdx;
+      const targetMolecule = (this.isEditedFromSketcher ? this.sketchedMolecule : 
+        this.dataFrame?.getCol(this.moleculeColumnName).get(this.curIdx));
+
+      if (computeData) {
+        const df = await chemSimilaritySearch(this.dataFrame, this.dataFrame?.getCol(this.moleculeColumnName),
+                   targetMolecule, this.distanceMetric, this.limit, this.minScore);
+
+        this.molCol = df.getCol('smiles');
+        this.idxs = df.getCol('indexes');
+        this.scores = df.getCol('score');
+      }
+
       if (this.root.hasChildNodes())
         this.root.removeChild(this.root.childNodes[0]);
+      const panel = [];
+      const grids = []; 
+      let cnt = 0, cnt2 = 0;
+      panel[cnt++] = ui.h1('Reference');
+      panel[cnt++] = grok.chem.svgMol(targetMolecule, 200, 100);
+      panel[cnt++] = this.sketchButton;
+      panel[cnt++] = ui.h1('Similar Structure');
+      for (let i = 0; i < this.molCol.length; ++i) {
+        const mol = grok.chem.svgMol(this.molCol?.get(i), 200, 100);
+        const text = ui.label(`${this.scores.get(i).toPrecision(2)}`);
+        let grid = ui.div([mol, text], {style: {width: '200px', height: '120px', margin: '5px'}});
+        let divClass = 'd4-flex-col';
 
+        if (this.idxs.get(i) == this.curIdx) {
+          divClass += ' d4-current';
+          grid.style.backgroundColor = '#ddffd9';
+        } 
+        if (this.dataFrame.selection.get(this.idxs.get(i))) {
+          divClass += ' d4-selected';
+          if (divClass == 'd4-flex-col d4-selected')
+            grid.style.backgroundColor = '#f8f8df';
+          else 
+            grid.style.backgroundColor = '#d3f8bd';
+        }
 
-      const curIdx = this.dataFrame.currentRowIdx;
-      const df = await chemSimilaritySearch(this.dataFrame, this.dataFrame?.getCol(this.moleculeColumnName),
-        this.dataFrame?.getCol(this.moleculeColumnName).get(curIdx), 'tanimoto', 10, 0.1);
-
-      const molCol = df.getCol('smiles');
-      const idxs = df.getCol('indexes');
-      const scores = df.getCol('score');
-      const g = []; let cnt = 0;
-      g[cnt++] = ui.h1('SVG rendering');
-      g[cnt++] = this.sketchButton;
-      for (let i = 0; i < molCol.length; ++i) {
-        const mol = grok.chem.svgMol(molCol?.get(i), 250, 100);
-        const text = ui.p(`${scores.get(i).toPrecision(3)}`);
-
-        const grid = ui.div([mol, text]);
-        grid.addEventListener('click', (event: Event) => {
-          if (this.dataFrame)
-            this.dataFrame.currentRowIdx = idxs.get(i);
+        $(grid).addClass(divClass);
+        grid.addEventListener('click', (event: MouseEvent) => {
+          if (this.dataFrame) {
+            if (event.shiftKey || event.altKey) {
+              this.dataFrame.selection.set(this.idxs.get(i), true);
+            } else if (event.metaKey) {
+              let selected = this.dataFrame.selection;
+              this.dataFrame.selection.set(this.idxs.get(i), selected.get(this.idxs.get(i)) != true);
+            } else {
+              this.dataFrame.currentRowIdx = this.idxs.get(i);
+              this.isEditedFromSketcher = false;
+            }
+          }
         });
-        g[cnt++] = grid;
+        grids[cnt2++] = grid;
       }
-      this.root.appendChild(ui.div(g));
+      const gridsDiv = ui.div(grids, {classes: 'd4-flex-wrap'});
+      panel[cnt++] = gridsDiv;
+      this.root.appendChild(ui.div(panel, {style: {margin: '5px'}}));
     }
   }
 }
 
-//name: chemSimilaritySearch
-//input: dataframe table
-//input: column smiles
-//input: string molecule
-//input: string metric = tanimoto
-//input: int limit = 10
-//input: double minScore = 0.7
 export async function chemSimilaritySearch(
   table: DG.DataFrame,
   smiles: DG.Column,
   molecule: string,
-  metric: string,
+  metricName: string,
   limit: number,
   minScore: number,
 ) {
-  const options = {
-    'minPath': 1,
-    'maxPath': 7,
-    'fpSize': 2048,
-    'bitsPerHash': 2,
-    'useHs': true,
-    'tgtDensity': 0.0,
-    'minSize': 128,
-  };
+  const metrics: {[Key: string]: any} = {
+    'tanimoto': metric.tanimoto,
+    'dice': metric.dice,
+    'cosine': metric.cosine,
+    'sokal': metric.sokal,
+    'kulczynski': metric.kulczynski,
+    'mc-connaughey': metric.mcConnaughey,
+    'asymmetric': metric.asymmetric,
+    'braun-blanquet': metric.braunBlanquet,
+    'russel': metric.russel,
+    'rogot-goldberg': metric.russel,
+  }
   limit = Math.min(limit, smiles.length);
   const fingerprint = chemSearches.chemGetMorganFingerprint(molecule);
   const fingerprintCol = await chemSearches.chemGetMorganFingerprints(smiles);
   const distances: number[] = [];
 
-  let fpSim = tanimotoSimilarity;
-  const webWorker = false;
-  if (webWorker) {
-    //todo: implement
-    fpSim = () => {
-      throw new Error('Not Impemented yet');
-    };
-  }
-
+  let fpSim = metrics[metricName];
   for (let row = 0; row < fingerprintCol.length; row++) {
     const fp = fingerprintCol[row];
     distances[row] = fp == null ? 100.0 : fpSim(fingerprint, fp);
