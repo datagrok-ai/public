17a49b4c86330c308ac152622ff036b7be5281e6
dskatov <dskatov@datagrok.ai>
2022-01-21 17:56:06 +0300

    Chem: Refactoring (to RDMol)

--- packages/Chem/src/rdkit-service-worker-base.ts
+++ packages/Chem/src/rdkit-service-worker-base.ts
@@ -1,6 +1,9 @@
+import {RDMol} from "./rdkit-api";
+
 export class RdKitServiceWorkerBase {
-  _rdKitModule: any | null;
-  _webRoot: string | null;
+  _rdKitModule: any;
+  _webRoot: string;
+  _rdKitMols: RDMol[] | null = null;
   constructor(module: Object, webRoot: string) {
     this._rdKitModule = module;
     this._webRoot = webRoot;
