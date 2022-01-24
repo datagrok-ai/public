988660e64e1a6dfaefb752cd4347d433b8220f5b
dskatov <dskatov@datagrok.ai>
2022-01-24 17:51:42 +0300

    Chem: Move to RdKitMolecule and RdKitModule interfaces

--- packages/Chem/src/rdkit-service/rdkit-service-worker-substructure.ts
+++ packages/Chem/src/rdkit-service/rdkit-service-worker-substructure.ts
@@ -2,12 +2,13 @@ import {RdKitServiceWorkerSimilarity} from './rdkit-service-worker-similarity';
 import {rdKitFingerprintToBitArray} from '../utils/chem-common';
 import BitArray from '@datagrok-libraries/utils/src/bit-array';
 import {isMolBlock} from '../utils/chem-utils';
+import {RDModule} from "../rdkit-api";
 
 export class RdKitServiceWorkerSubstructure extends RdKitServiceWorkerSimilarity {
   _patternFps: BitArray[] | null = null;
    readonly _patternFpLength = 2048;
 
-   constructor(module: Object, webRoot: string) {
+   constructor(module: RDModule, webRoot: string) {
     super(module, webRoot);
   }
 
