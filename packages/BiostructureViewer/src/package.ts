4f503429a300a14f2431891952232edab42391a2
Nikolay Alemasov <nalemasov@datagrok.ai>
2022-01-24 16:37:47 +0700

    BiostructureViewer #283: Turn off Mol* additional panels.
    * Turn off the viewer's additional panels.
    * Add test for the viewer is opening.
    * Small fixes of test procedure names.

--- packages/BiostructureViewer/src/package.ts
+++ packages/BiostructureViewer/src/package.ts
@@ -1,12 +1,10 @@
 /* Do not change these import lines to match external modules in webpack configuration */
-import * as grok from 'datagrok-api/grok';
+//import * as grok from 'datagrok-api/grok';
 //import * as ui from 'datagrok-api/ui';
 import * as DG from 'datagrok-api/dg';
 
-//@ts-ignore
-import {Viewer} from 'molstar/build/viewer/molstar';
-
 import {BioStructureViewer} from './biostructure-viewer';
+import {createViewer} from './viewers/molstar-viewer';
 
 export const _package = new DG.Package();
 
@@ -22,9 +20,7 @@ export async function biostructureApp() {
 //name: Mol* BioStructure Viewer.
 //tags: app
 export async function molstarView() {
-  const view = grok.shell.newView('Mol*');
-  const viewerContainer = view.root;
-  const v = new Viewer(viewerContainer);//Viewer();
-  //await v.loadPdb('2v0a');
-  v.handleResize();
+  const pi = DG.TaskBarProgressIndicator.create('Opening BioStructure* Viewer');
+  await createViewer();
+  pi.close();
 }
