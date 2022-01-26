0f83fca9310dbcac1856fa74d0060e5be9e783a6
Maria Dolotova <mahouny85@mail.ru>
2022-01-26 18:43:52 +0300

    Clinical Case: refactoring, volcano plot

--- packages/ClinicalCase/src/views/ae-risk-assessment-view.ts
+++ packages/ClinicalCase/src/views/ae-risk-assessment-view.ts
@@ -1,23 +1,26 @@
 import * as grok from 'datagrok-api/grok';
 import * as DG from "datagrok-api/dg";
 import * as ui from "datagrok-api/ui";
-import { study, ClinRow } from "../clinical-study";
+import { study } from "../clinical-study";
 import { createAERiskAssessmentDataframe } from '../data-preparation/data-preparation';
-import { checkMissingDomains, updateDivInnerHTML } from './utils';
+import { updateDivInnerHTML } from './utils';
 import { TREATMENT_ARM } from '../columns-constants';
 import { ClinicalCaseViewBase } from '../model/ClinicalCaseViewBase';
+import { AE_COUNT, LOG2_RISK_DIFFERENCE, NEG_LOG10_P_VALUE, RISK_DIFFERENCE, SE_RD_WITH_SIGN_LEVEL } from '../constants';
 
 export class AERiskAssessmentView extends ClinicalCaseViewBase {
 
   riskAssessmentDataframe: DG.DataFrame;
   volcanoPlotXAxis = '';
-  volcanoPlotYAxis = '-log10(p-value)';
+  volcanoPlotYAxis = NEG_LOG10_P_VALUE;
+  volcanoPlotMarkerSize = '';
   placeboArm = [];
   activeArm = [];
   treatmentArmOptions: string[];
   riskAssessmentDiv = ui.box();
   initialGuide: any;
-  xScaleOptions = { 'Linear': 'RD', 'Log2': 'log2(RD)' };
+  xScaleOptions = { 'Linear': RISK_DIFFERENCE, 'Log': LOG2_RISK_DIFFERENCE };
+  sizeOptions = { 'AE total count': AE_COUNT, 'Significance value': SE_RD_WITH_SIGN_LEVEL};
   volcanoPlot: any;
   pValueLimit = 0.05;
   incorrectTreatmentArms = false;
@@ -30,6 +33,7 @@ export class AERiskAssessmentView extends ClinicalCaseViewBase {
 
   createView(): void {
     this.volcanoPlotXAxis = Object.values(this.xScaleOptions)[0];
+    this.volcanoPlotMarkerSize = Object.values(this.sizeOptions)[0];
     this.treatmentArmOptions = study.domains.dm.col(TREATMENT_ARM).categories;
     this.initialGuide = ui.info('Please select values for treatment/placebo arms in a property panel', '', false);
     updateDivInnerHTML(this.riskAssessmentDiv, this.initialGuide);
@@ -38,7 +42,8 @@ export class AERiskAssessmentView extends ClinicalCaseViewBase {
 
   createVolcanoPlotDiv() {
     if (this.placeboArm.length && this.activeArm.length && !this.incorrectTreatmentArms) {
-      this.riskAssessmentDataframe = createAERiskAssessmentDataframe(study.domains.ae, study.domains.dm.clone(), this.placeboArm, this.activeArm);
+      this.riskAssessmentDataframe = createAERiskAssessmentDataframe(study.domains.ae, study.domains.dm.clone(), this.placeboArm, this.activeArm, this.pValueLimit);
+      console.log(this.riskAssessmentDataframe.rowCount);
       this.riskAssessmentDataframe.col(this.volcanoPlotXAxis).tags[DG.TAGS.COLOR_CODING_TYPE] = 'Linear';
       this.riskAssessmentDataframe.col(this.volcanoPlotXAxis).tags[DG.TAGS.COLOR_CODING_LINEAR] = `[${DG.Color.blue}, ${DG.Color.red}]`;
       this.updateVolcanoPlot();
@@ -53,13 +58,10 @@ export class AERiskAssessmentView extends ClinicalCaseViewBase {
       x: this.volcanoPlotXAxis,
       y: this.volcanoPlotYAxis,
       color: this.volcanoPlotXAxis,
-      showViewerFormulaLines: true
+      showViewerFormulaLines: true,
+      size: this.volcanoPlotMarkerSize
     });
-    this.volcanoPlot.meta.addFormulaLine({
-      formula: `\${${this.volcanoPlotYAxis}} = ${-Math.log10(this.pValueLimit)}`,
-      color: "#ff0000",
-      width: 0.5
-    });
+    this.updatePValueLine();
     updateDivInnerHTML(this.riskAssessmentDiv, ui.splitV([this.volcanoPlot.root]))
   }
 
@@ -101,21 +103,37 @@ export class AERiskAssessmentView extends ClinicalCaseViewBase {
       let xChoices = ui.choiceInput('X axis type', Object.keys(this.xScaleOptions)[0], Object.keys(this.xScaleOptions));
       xChoices.onChanged((v) => {
         this.volcanoPlotXAxis = this.xScaleOptions[xChoices.value];
-        this.updateVolcanoPlot();
+        this.volcanoPlot.setOptions({xColumnName: this.volcanoPlotXAxis});
+        this.volcanoPlot.setOptions({color: this.volcanoPlotXAxis});
       });
       let pValue = ui.stringInput('Significance level', this.pValueLimit.toString());
       pValue.onChanged((v) => {
         this.pValueLimit = parseFloat(pValue.value);
         if (!isNaN(this.pValueLimit)) {
-          this.updateVolcanoPlot();
+          this.volcanoPlot.meta.formulaLines = [];
+          this.updatePValueLine();
         }
+      });
+      let sizeChoices = ui.choiceInput('Marker size', Object.keys(this.sizeOptions)[0], Object.keys(this.sizeOptions));
+      sizeChoices.onChanged((v) => {
+        this.volcanoPlotMarkerSize = this.sizeOptions[sizeChoices.value];
+        this.volcanoPlot.setOptions({size: this.volcanoPlotMarkerSize});
       });
       return ui.inputs([
         xChoices,
+        sizeChoices,
         pValue,
       ]);
     })
     return acc.root;
+  }
+
+  private updatePValueLine(){
+    this.volcanoPlot.meta.addFormulaLine({
+      formula: `\${${this.volcanoPlotYAxis}} = ${-Math.log10(this.pValueLimit)}`,
+      color: "#ff0000",
+      width: 0.5
+    });
   }
 
 }
