8c8779e2702cf9606785d2fe11cb5304ccdc7847
Nuradil <mr.nuradil@gmail.com>
2022-01-27 15:34:27 +0600

    Remove loops in isDataTypeMetric functions

--- packages/Chem/src/analysis/chem-space.ts
+++ packages/Chem/src/analysis/chem-space.ts
@@ -6,14 +6,14 @@ import {chemGetFingerprints} from '../chem-searches';
 //import {getRdKitWebRoot} from '../chem-common-rdkit';
 import {Coordinates} from '@datagrok-libraries/utils/src/type-declarations';
 import {createDimensinalityReducingWorker,} from '@datagrok-libraries/ml/src/workers/dimensionality-reducing-worker-creator';
-import {BitArrayMetrics, isBitArrayMetric, StringMetrics} from '@datagrok-libraries/ml/src/typed-metrics';
+import {BitArrayMetrics, isBitArrayMetric, KnownMetrics, StringMetrics} from '@datagrok-libraries/ml/src/typed-metrics';
 import {Fingerprint} from "../utils/chem-common";
 import BitArray from '@datagrok-libraries/utils/src/bit-array';
 
 export async function chemSpace(table: DG.DataFrame, molColumn: DG.Column, methodName: string, similarityMetric: string) {
   let fpColumn = molColumn.toList();
   let coordinates: Coordinates;
-  if (isBitArrayMetric(similarityMetric)) {
+  if (isBitArrayMetric(similarityMetric as KnownMetrics)) {
     fpColumn = await chemGetFingerprints(molColumn, Fingerprint.Morgan);
     coordinates = await createDimensinalityReducingWorker({data: fpColumn as BitArray[], metric: similarityMetric as BitArrayMetrics}, methodName) as Coordinates;
   } else {
