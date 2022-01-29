0513ed55985b2cf42ff5628945424f8ac3fb53bf
skalkin <skalkin@gmail.com>
2022-01-28 20:27:14 -0500

    Chem: refactoring

--- packages/Chem/src/panels/inchi.ts
+++ packages/Chem/src/panels/inchi.ts
@@ -1,59 +1,30 @@
 import * as ui from 'datagrok-api/ui';
 import * as grok from 'datagrok-api/grok';
 import * as DG from 'datagrok-api/dg';
-import {getRdKitModule} from '../utils/chem-common-rdkit';
+import {_rdKitModule, getRdKitModule} from '../utils/chem-common-rdkit';
+import {StringUtils} from "@datagrok-libraries/utils/src/string-utils";
+import {RDMol} from "../rdkit-api";
 
-/**
- * Adds InchI identifiers to dataframe.
- * @export
- * @param {DG.Column} col column with smiles.
- */
-export function addInchis(col: DG.Column): void {
-  const pi = DG.TaskBarProgressIndicator.create('Getting Inchi');
-  const rdKitModule = getRdKitModule();
-  const inchis = new Array(col.length);
-  for (let i = 0; i < inchis.length; i++) {
-    const mol = rdKitModule.get_mol(col.get(i));
-    inchis[i] = mol.get_inchi();
+function addDerived(col: DG.Column, description: string, extract: (mol: RDMol) => string) {
+  const pi = DG.TaskBarProgressIndicator.create(description);
+  const result = new Array(col.length);
+  for (let i = 0; i < result.length; i++) {
+    const mol = _rdKitModule.get_mol(col.get(i));
+    result[i] = extract(mol);
     mol.delete();
   }
 
-  const name = getName('inchi', col.dataFrame.columns.names());
-
-  col.dataFrame.columns.add(DG.Column.fromList('string', name, inchis));
+  const name = StringUtils.getUniqueName('inchi', col.dataFrame.columns.names());
+  col.dataFrame.columns.add(DG.Column.fromList('string', name, result));
   pi.close();
 }
 
-/**
- * Adds InchI Keys to dataframe.
- * @export
- * @param {DG.Column} col column with smiles.
- */
+/** Adds InchI identifiers for the specified molecular column. */
+export function addInchis(col: DG.Column): void {
+  addDerived(col, 'Getting Inchi', (m) => m.get_inchi());
+}
+
+/** Adds InchI keys identifiers for the specified molecular column. */
 export function addInchiKeys(col: DG.Column): void {
-  const pi = DG.TaskBarProgressIndicator.create('Getting Inchi Keys');
-  const rdKitModule = getRdKitModule();
-  const inchiKeys = new Array(col.length);
-  for (let i = 0; i < inchiKeys.length; i++)
-    inchiKeys[i] = rdKitModule.get_inchikey_for_inchi(rdKitModule.get_mol(col.get(i)).get_inchi());
-
-
-  const name = getName('inchi_key', col.dataFrame.columns.names());
-
-  col.dataFrame.columns.add(DG.Column.fromList('string', name, inchiKeys));
-  pi.close();
-}
-
-function getName(initialName: string, existingNames: string[]) {
-  if (!existingNames.includes(initialName))
-    return initialName;
-  else {
-    let counter: number = 1;
-    let newName: string = (' ' + initialName + '_' + counter).slice(1);
-    while (existingNames.includes(newName)) {
-      counter++;
-      newName = (' ' + initialName + '_' + counter).slice(1);
-    }
-
-    return newName;
-  }
+  addDerived(col, 'Getting Inchi', (m) => _rdKitModule.get_inchikey_for_inchi(m.get_inchi()));
 }
