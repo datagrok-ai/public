0b5d38d0bb8a7c2972265ae996d49b49fbb9cb28
Volodymyr Dyma <emissaryprog@gmail.com>
2022-01-31 17:05:37 +0200

    SQLite #300: tests fix and package refactoring

--- packages/SQLite/src/tests/sqlite-tests.ts
+++ packages/SQLite/src/tests/sqlite-tests.ts
@@ -1,19 +1,23 @@
-import * as DG from 'datagrok-api/dg';
 import {before, category, expect, test} from '@datagrok-libraries/utils/src/test';
-import {importSQLite} from '../package';
-import {symmetricDifference} from 'set-operations';
 
-export const _package = new DG.Package();
+import * as sql from 'sql.js';
+
+//@ts-ignore: there are no types
+import initSqlJs from '../sql-wasm.js';
+import {_package} from '../package-test';
+import {importSQLiteImpl} from '../import';
 
 category('SQLite', () => {
   let chinookBytes: Uint8Array;
+  let SQL: sql.SqlJsStatic;
 
   before(async () => {
+    SQL = await initSqlJs({locateFile: () => _package.webRoot + 'dist/sql-wasm.wasm'});
     chinookBytes = await _package.files.readAsBytes('chinook.sqlite');
   });
 
   test('import', async () => {
-    const chinookTables = (await importSQLite(chinookBytes)).map((t) => t!.name);
+    const chinookTables = importSQLiteImpl(chinookBytes, SQL).map((t) => t!.name);
     const expectedTables = [
       'albums', 'sqlite_sequence', 'artists', 'customers', 'employees', 'genres', 'invoices',
       'invoice_items', 'media_types', 'playlists', 'playlist_track', 'tracks', 'sqlite_stat1',
