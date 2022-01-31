e5b671a124f6cb1c39dc4aebdcd7b496e6d1e795
onuf <56073962+onuf@users.noreply.github.com>
2022-01-31 16:01:15 +0200

    Tutorials: tests (WIP)

---
+++ packages/Tutorials/src/package-test.ts
@@ -0,0 +1,15 @@
+import * as DG from 'datagrok-api/dg';
+import {runTests} from '@datagrok-libraries/utils/src/test';
+import './tests/app-test';
+
+export let _package = new DG.Package();
+
+//name: test
+//input: string category {optional: true}
+//input: string test {optional: true}
+//output: dataframe result
+//top-menu: Tools | Dev | JS API Tests
+export async function test(category: string, test: string): Promise<DG.DataFrame> {
+  let data = await runTests({category, test});
+  return DG.DataFrame.fromObjects(data)!;
+}
