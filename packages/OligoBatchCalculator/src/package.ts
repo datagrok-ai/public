5aebbdcb30d62482e73150624f1546ed06088cde
Vadym Kovadlo <vadym.kovadlo@gmail.com>
2022-02-09 14:18:28 +0200

    GROK-10249 OligoBatchCalculator: add/delete/edit modifications 2.0 (WIP)

--- packages/OligoBatchCalculator/src/package.ts
+++ packages/OligoBatchCalculator/src/package.ts
@@ -8,6 +8,14 @@ import {map} from "./map";
 export let _package = new DG.Package();
 
 const STORAGE_NAME = 'oligo-batch-calculator-storage';
+const OVERHANG_COL_NAMES = {
+  LONG_NAMES: 'Long name',
+  ABBREVIATION: 'Abbreviation',
+  MOLECULAR_WEIGHT: 'Molecular weight',
+  BASE_MODIFICATION: 'Base modification',
+  EXTINCTION_COEFFICIENT: 'Ext. coefficient',
+  ACTION: 'Action'
+}
 
 let weightsObj: {[code: string]: number} = {};
 let normalizedObj: {[code: string]: string} = {};
@@ -18,8 +26,37 @@ for (let synthesizer of Object.keys(map))
       normalizedObj[code] = map[synthesizer][technology][code].normalized;
     }
 
+function saveAsCsv(table: DG.DataFrame) {
+  let link = document.createElement("a");
+  link.setAttribute("href", "data:text/csv;charset=utf-8,\uFEFF" + encodeURI(table.toCsv()));
+  link.setAttribute("download", "Oligo Properties.csv");
+  link.click();
+}
+
 function sortByStringLengthInDescendingOrder(array: string[]): string[] {
   return array.sort(function(a, b) { return b.length - a.length; });
+}
+
+async function getModificationsDf() {
+  // //@ts-ignore
+  // await grok.dapi.userDataStorage.remove(STORAGE_NAME, null, false);
+  let modifications: any[] = [];
+  let entries = await grok.dapi.userDataStorage.get(STORAGE_NAME, false);
+  if (entries !== null && Object.keys(entries).length === 0)
+    grok.shell.info('Storage is empty. Try to post something to the storage');
+  else {
+    Object.keys(entries).forEach((key) => {
+      modifications.push(JSON.parse(entries[key]));
+    });
+  }
+  return DG.DataFrame.fromColumns([
+    DG.Column.fromStrings(OVERHANG_COL_NAMES.LONG_NAMES, modifications.map((e) => e.longName)),
+    DG.Column.fromStrings(OVERHANG_COL_NAMES.ABBREVIATION, modifications.map((e) => e.abbreviation)),    // @ts-ignore
+    DG.Column.fromFloat32Array(OVERHANG_COL_NAMES.MOLECULAR_WEIGHT, modifications.map((e) => (e.molecularWeight == undefined) ? 0 : e.molecularWeight)),
+    DG.Column.fromStrings(OVERHANG_COL_NAMES.BASE_MODIFICATION, modifications.map((e) => e.baseModification)),
+    DG.Column.fromStrings(OVERHANG_COL_NAMES.EXTINCTION_COEFFICIENT, modifications.map((e) => e.extinctionCoefficient)),
+    DG.Column.fromStrings(OVERHANG_COL_NAMES.ACTION, Array(modifications.length).fill(''))
+  ])!;
 }
 
 //name: Oligo Batch Calculator
@@ -113,7 +150,7 @@ export function extinctionCoefficient(sequence: string): number {
   };
   let ec1 = 0, ec2 = 0;
   for (let i = 0; i < sequence.length - 2; i += 2)
-    ec1 += (sequence[i] == sequence[i + 2]) ? 
+    ec1 += (sequence[i] == sequence[i + 2]) ?
       nearestNeighbour[sequence.slice(i, i + 2)][sequence.slice(i + 2, i + 4)] :
       (
         nearestNeighbour['r' + ((sequence[i + 1] == 'T') ? 'U' : sequence[i + 1])]['r' + ((sequence[i + 3] == 'T') ? 'U' : sequence[i + 3])]
@@ -135,7 +172,7 @@ function getAllCodesOfSynthesizer(synthesizer: string) {
   let codes: string[] = [];
   for (let technology of Object.keys(map[synthesizer]))
     codes = codes.concat(Object.keys(map[synthesizer][technology]));
-  return codes;    
+  return codes;
 }
 
 function getListOfPossibleSynthesizersByFirstMatchedCode(sequence: string): string[] {
@@ -196,7 +233,7 @@ function isValid(sequence: string) {
 
 //name: Oligo Batch Calculator
 //tags: app
-export function OligoBatchCalculatorApp() {
+export async function OligoBatchCalculatorApp() {
 
   function updateTable(text: string) {
     gridDiv.innerHTML = '';
@@ -297,16 +334,8 @@ export function OligoBatchCalculatorApp() {
   let inputSequences = ui.textInput("", defaultInput, async (txt: string) => updateTable(txt));
   let yieldAmount = ui.floatInput('', 1, () => updateTable(inputSequences.value));
   let units = ui.choiceInput('', 'OD', ['OD', 'µg', 'mg', 'µmole', 'nmole'], () => updateTable(inputSequences.value));
-  let clearSequences = ui.button('CLEAR', () => inputSequences.value = '');
 
   updateTable(defaultInput);
-
-  let saveAsButton = ui.bigButton('SAVE AS CSV', () => {
-    let link = document.createElement("a");
-    link.setAttribute("href", "data:text/csv;charset=utf-8,\uFEFF" + encodeURI(table.toCsv()));
-    link.setAttribute("download", "Oligo Properties.csv");
-    link.click();
-  });
 
   let tables = ui.divV([]);
   for (let synthesizer of Object.keys(map)) {
@@ -325,109 +354,202 @@ export function OligoBatchCalculatorApp() {
     }
   }
 
-  let showCodesButton = ui.button('SHOW CODES', () => {
-    ui.dialog('Codes')
-      .add(tables)
-      .show();
-  });
-
-  async function loadAll() {
-    let modifications: any[] = [];
-    let entries = await grok.dapi.userDataStorage.get(STORAGE_NAME, false);
-    if (entries !== null && Object.keys(entries).length === 0)
-      grok.shell.info('Storage is empty. Try to post something to the storage');
-    else {
-      Object.keys(entries).forEach((key) => {
-        modifications.push(JSON.parse(entries[key]));
-      });
-    }
-    return modifications;
+  async function addModificationButton() {
+    grok.dapi.users.current().then((user) => {
+      if (adminUsers.includes(user.firstName + ' ' + user.lastName)) {
+        let longName = ui.stringInput(OVERHANG_COL_NAMES.LONG_NAMES, '');
+        ui.tooltip.bind(longName.root, "Examples: 'Inverted Abasic', 'Cyanine 3 CPG', '5-Methyl dC'");
+        let abbreviation = ui.stringInput(OVERHANG_COL_NAMES.ABBREVIATION, '');
+        ui.tooltip.bind(abbreviation.root, "Examples: 'invabasic', 'Cy3', '5MedC'");
+        let molecularWeight = ui.floatInput(OVERHANG_COL_NAMES.MOLECULAR_WEIGHT, 0);
+        let baseModification = ui.choiceInput(OVERHANG_COL_NAMES.BASE_MODIFICATION, 'NO', ['NO', 'rU', 'rA', 'rC', 'rG', 'dA', 'dC', 'dG', 'dT'], (v: string) => {
+          if (v != 'NO')
+            extinctionCoefficient.value = 'Base';
+          extinctionCoefficient.enabled = (v == 'NO');
+        });
+        let extinctionCoefficient = ui.stringInput(OVERHANG_COL_NAMES.EXTINCTION_COEFFICIENT, '');
+        ui.dialog('Add Modification')
+          .add(ui.block([
+            longName.root,
+            abbreviation.root,
+            molecularWeight.root,
+            baseModification.root,
+            extinctionCoefficient.root,
+          ]))
+          .onOK(() => {
+            if (longName.value.length > 300)
+              return grok.shell.warning('Long Name shouldn\'t contain more than 300 characters')
+            if (abbreviation.value.length > 100)
+              return grok.shell.warning('Abbreviation shouldn\'t contain more than 100 characters')
+            grok.dapi.userDataStorage.postValue(
+              STORAGE_NAME,
+              longName.value,
+              JSON.stringify({
+                longName: longName.value,
+                abbreviation: abbreviation.value,
+                molecularWeight: molecularWeight.value,
+                extinctionCoefficient: extinctionCoefficient.value,
+                baseModification: baseModification.value,
+              }),
+              false
+            ).then(() => grok.shell.info('Posted'));
+            modificationsDf.rows.addNew([
+              longName.value, abbreviation.value, molecularWeight.value, extinctionCoefficient.value, baseModification.value
+            ]);
+          })
+          .show();
+      } else
+        grok.shell.info('You don\'t have permission for this action');
+    });
   }
 
-  const baseModificationItems = ['NO', 'rU', 'rA', 'rC', 'rG', 'dA', 'dC', 'dG', 'dT'];
-  let addModificationButton = ui.button('ADD MODIFICATION',  async() => {
-    let modifications = await loadAll();
-    let longName = ui.stringInput('Long name', '');
-    ui.tooltip.bind(longName.root, "Examples: 'Inverted Abasic', 'Cyanine 3 CPG', '5-Methyl dC'");
-    let abbreviation = ui.stringInput('Abbreviation', '');
-    ui.tooltip.bind(abbreviation.root, "Examples: 'invabasic', 'Cy3', '5MedC'");
-    let molecularWeight = ui.floatInput('Molecular weight', 0);
-    let baseModification = ui.choiceInput('Base modification', 'NO', baseModificationItems, (v: string) => {
-      if (v != 'NO')
-        extinctionCoefficient.value = 'Base';
-      extinctionCoefficient.enabled = (v == 'NO');
-    });
-    let extinctionCoefficient = ui.stringInput('Extinction coefficient', '');
-    ui.dialog('Add Modification')
-      .add(ui.block([
-        DG.Viewer.grid(
-          DG.DataFrame.fromColumns([
-            DG.Column.fromStrings(longName.caption, modifications.map((e) => e.longName)),
-            DG.Column.fromStrings(abbreviation.caption, modifications.map((e) => e.abbreviation)),    // @ts-ignore
-            DG.Column.fromFloat32Array(molecularWeight.caption, modifications.map((e) => (e.molecularWeight == undefined) ? 0 : e.molecularWeight)),
-            DG.Column.fromStrings(baseModification.caption, modifications.map((e) => e.baseModification)),
-            DG.Column.fromStrings(extinctionCoefficient.caption, modifications.map((e) => e.extinctionCoefficient))
-          ])
-        ).root,
-        longName.root,
-        abbreviation.root,
-        molecularWeight.root,
-        baseModification.root,
-        extinctionCoefficient.root,
-      ]))
-      .onOK(() => {
-        grok.dapi.userDataStorage.postValue(
-          STORAGE_NAME,
-          longName.value,
-          JSON.stringify({
-            longName: longName.value,
-            abbreviation: abbreviation.value,
-            molecularWeight: molecularWeight.value,
-            extinctionCoefficient: extinctionCoefficient.value,
-            baseModification: baseModification.value,
-          }),
-          false
-        ).then(() => grok.shell.info('Posted'));
-      })
-      .showModal(true);
-  });
+  let title = ui.panel([ui.h2('Oligo Properties')], 'ui-panel ui-box');
+  title.style.maxHeight = '40px';
+  $(title).children('h2').css('margin', '0px');
 
-  let title = ui.panel([ui.h1('Oligo Properties')], 'ui-panel ui-box');
-  title.style.maxHeight = '45px';
-  $(title).children('h1').css('margin', '0px');
-
+  const asoGapmersDf = DG.DataFrame.fromObjects([
+    { Name: "2'MOE-5Me-rU", BioSpring: "5", "Janssen GCRS": "moeT", Weight: 378.27 },
+    { Name: "2'MOE-rA", BioSpring: "6", "Janssen GCRS": "moeA", Weight: 387.29 },
+    { Name: "2'MOE-5Me-rC", BioSpring: "7", "Janssen GCRS": "moe5mC", Weight: 377.29 },
+    { Name: "2'MOE-rG", BioSpring: "8", "Janssen GCRS": "moeG", Weight: 403.28 },
+    { Name: "5-Methyl-dC", BioSpring: "9", "Janssen GCRS": "5mC", Weight: 303.21 },
+    { Name: "ps linkage", BioSpring: "*", "Janssen GCRS": "ps", Weight: 16.07 },
+    { Name: "dA", BioSpring: "A", "Janssen GCRS": "A, dA", Weight: 313.21 },
+    { Name: "dC", BioSpring: "C", "Janssen GCRS": "C, dC", Weight: 289.18 },
+    { Name: "dG", BioSpring: "G", "Janssen GCRS": "G, dG", Weight: 329.21 },
+    { Name: "dT", BioSpring: "T", "Janssen GCRS": "T, dT", Weight: 304.2 },
+    { Name: "rA", BioSpring: "", "Janssen GCRS": "rA", Weight: 329.21 },
+    { Name: "rC", BioSpring: "", "Janssen GCRS": "rC", Weight: 305.18 },
+    { Name: "rG", BioSpring: "", "Janssen GCRS": "rG", Weight: 345.21 },
+    { Name: "rU", BioSpring: "", "Janssen GCRS": "rU", Weight: 306.17 }
+  ]);
+  const omeAndFluoroDf = DG.DataFrame.fromObjects([
+    { Name: "2'-fluoro-U", BioSpring: "1", Axolabs: "Uf", "Janssen GCRS": "fU", Weight: 308.16 },
+    { Name: "2'-fluoro-A", BioSpring: "2", Axolabs: "Af", "Janssen GCRS": "fA", Weight: 331.2 },
+    { Name: "2'-fluoro-C", BioSpring: "3", Axolabs: "Cf", "Janssen GCRS": "fC", Weight: 307.18 },
+    { Name: "2'-fluoro-G", BioSpring: "4", Axolabs: "Gf", "Janssen GCRS": "fG", Weight: 347.19 },
+    { Name: "2'OMe-rU", BioSpring: "5", Axolabs: "u", "Janssen GCRS": "mU", Weight: 320.2 },
+    { Name: "2'OMe-rA", BioSpring: "6", Axolabs: "a", "Janssen GCRS": "mA", Weight: 343.24 },
+    { Name: "2'OMe-rC", BioSpring: "7", Axolabs: "c", "Janssen GCRS": "mC", Weight: 319.21 },
+    { Name: "2'OMe-rG", BioSpring: "8", Axolabs: "g", "Janssen GCRS": "mG", Weight: 359.24 },
+    { Name: "ps linkage", BioSpring: "*", Axolabs: "s", "Janssen GCRS": "ps", Weight: 16.07 }
+  ]);
+  const modificationsDf = await getModificationsDf();
+  let modifGrid = DG.Viewer.grid(modificationsDf, { showRowHeader: false, showCellTooltip: false });
+  modifGrid.col(OVERHANG_COL_NAMES.LONG_NAMES)!.width = 110;
+  modifGrid.col(OVERHANG_COL_NAMES.ABBREVIATION)!.width = 80;
+  modifGrid.col(OVERHANG_COL_NAMES.MOLECULAR_WEIGHT)!.width = 105;
+  modifGrid.col(OVERHANG_COL_NAMES.BASE_MODIFICATION)!.width = 110;
+  modifGrid.col(OVERHANG_COL_NAMES.EXTINCTION_COEFFICIENT)!.width = 100;
   let view = grok.shell.newView('Oligo Batch Calculator', [
-    ui.splitV([
-      ui.box(
-        ui.panel([
-          ui.h1('Yield Amount & Units'),
-          ui.divH([
-            yieldAmount.root,
-            units.root,
-          ]),
-          ui.h1('Input Sequences'),
-          ui.div([
-            inputSequences.root
-          ],'inputSequence'),
-          ui.divH([
-            clearSequences,
-            showCodesButton,
-            addModificationButton,
-          ])
-        ]), {style:{maxHeight:'270px'}}
-      ),
+    ui.splitH([
       ui.splitV([
-        title,
-        ui.panel([gridDiv], 'ui-box')
+        ui.box(
+          ui.panel([
+            ui.h2('Yield Amount & Units'),
+            ui.divH([
+              yieldAmount.root,
+              units.root,
+            ]),
+            ui.h2('Input Sequences'),
+            ui.div([
+              inputSequences.root
+            ],'inputSequence')
+          ]), { style: { maxHeight: '230px' } }
+        ),
+        ui.splitV([
+          title,
+          gridDiv
+        ])
       ]),
-      ui.box(
-        ui.panel([
-          saveAsButton,
-        ]), {style:{maxHeight:'60px'}}
-      )
+      ui.splitV([
+        ui.box(ui.h2('ASO Gapmers'), { style: {maxHeight: '40px'} }),
+        DG.Viewer.grid(asoGapmersDf!, { showRowHeader: false, showCellTooltip: false }).root,
+        ui.box(ui.h2("2'-OMe and 2'-F modifications"), { style: {maxHeight: '40px'} }),
+        DG.Viewer.grid(omeAndFluoroDf!, { showRowHeader: false, showCellTooltip: false }).root,
+        ui.box(ui.h2('Overhang modifications'), { style: {maxHeight: '40px'} }),
+        modifGrid.root
+      ], { style: { maxWidth: '600px' } })
     ])
   ]);
+  const adminUsers = ['Baozhong Zhao', 'Sijin Guo', 'Saika Siddiqui', 'Vadym Kovadlo'];
   view.box = true;
+  let col = modifGrid.col('Action')!;
+  col.cellType = 'html';
+  modifGrid.onCellPrepare(function (gc) {
+    if (gc.isTableCell && gc.gridColumn.name == OVERHANG_COL_NAMES.ACTION) {
+      gc.style.element = ui.divH([
+        ui.button(ui.iconFA('trash-alt'), () => {
+          grok.dapi.users.current().then(async(user) => {
+            if (adminUsers.includes(user.firstName + ' ' + user.lastName)) {
+              modificationsDf.rows.removeAt(gc.gridRow, 1, true);
+              await grok.dapi.userDataStorage.remove(STORAGE_NAME, modificationsDf.col(OVERHANG_COL_NAMES.LONG_NAMES)!.get(gc.gridRow), false);
+            } else
+              grok.shell.info('You don\'t have permission for this action');
+          });
+        }),
+        ui.button(ui.iconFA('edit'), () => {
+          grok.dapi.users.current().then((user) => {
+            if (adminUsers.includes(user.firstName + ' ' + user.lastName)) {
+              let oldLongName = modificationsDf.col(OVERHANG_COL_NAMES.LONG_NAMES)!.get(gc.gridRow);
+              let longName = ui.stringInput(OVERHANG_COL_NAMES.LONG_NAMES, oldLongName);
+              ui.tooltip.bind(longName.root, "Examples: 'Inverted Abasic', 'Cyanine 3 CPG', '5-Methyl dC'");
+              let abbreviation = ui.stringInput(OVERHANG_COL_NAMES.ABBREVIATION, modificationsDf.col(OVERHANG_COL_NAMES.ABBREVIATION)!.get(gc.gridRow));
+              ui.tooltip.bind(abbreviation.root, "Examples: 'invabasic', 'Cy3', '5MedC'");
+              let molecularWeight = ui.floatInput(OVERHANG_COL_NAMES.MOLECULAR_WEIGHT, modificationsDf.col(OVERHANG_COL_NAMES.MOLECULAR_WEIGHT)!.get(gc.gridRow));
+              let baseModification = ui.choiceInput(OVERHANG_COL_NAMES.BASE_MODIFICATION, 'NO', ['NO', 'rU', 'rA', 'rC', 'rG', 'dA', 'dC', 'dG', 'dT'], (v: string) => {
+                if (v != 'NO')
+                  extinctionCoefficient.value = 'Base';
+                extinctionCoefficient.enabled = (v == 'NO');
+              });
+              let extinctionCoefficient = ui.stringInput(OVERHANG_COL_NAMES.EXTINCTION_COEFFICIENT, modificationsDf.col(OVERHANG_COL_NAMES.EXTINCTION_COEFFICIENT)!.get(gc.gridRow));
+              ui.dialog('Add Modification')
+                .add(ui.block([
+                  longName.root,
+                  abbreviation.root,
+                  molecularWeight.root,
+                  baseModification.root,
+                  extinctionCoefficient.root,
+                ]))
+                .onOK(async () => {
+                  if (longName.value.length > 300)
+                    return grok.shell.warning('Long Name shouldn\'t contain more than 300 characters')
+                  if (abbreviation.value.length > 100)
+                    return grok.shell.warning('Abbreviation shouldn\'t contain more than 100 characters')
+                  await grok.dapi.userDataStorage.postValue(
+                    STORAGE_NAME,
+                    longName.value,
+                    JSON.stringify({
+                      longName: longName.value,
+                      abbreviation: abbreviation.value,
+                      molecularWeight: molecularWeight.value,
+                      extinctionCoefficient: extinctionCoefficient.value,
+                      baseModification: baseModification.value,
+                    }),
+                    false
+                  );
+                  if (oldLongName != longName.value)
+                    await grok.dapi.userDataStorage.remove(STORAGE_NAME, oldLongName, false);
+                  modificationsDf.set(OVERHANG_COL_NAMES.LONG_NAMES, gc.gridRow, longName.value);
+                  modificationsDf.set(OVERHANG_COL_NAMES.ABBREVIATION, gc.gridRow, abbreviation.value);
+                  modificationsDf.set(OVERHANG_COL_NAMES.MOLECULAR_WEIGHT, gc.gridRow, molecularWeight.value);
+                  modificationsDf.set(OVERHANG_COL_NAMES.EXTINCTION_COEFFICIENT, gc.gridRow, extinctionCoefficient.value);
+                  modificationsDf.set(OVERHANG_COL_NAMES.BASE_MODIFICATION, gc.gridRow, baseModification.value);
+                })
+                .show();
+            } else
+              grok.shell.info('You don\'t have permission for this action');
+          });
+        })
+      ]);
+    }
+  });
+
+  view.setRibbonPanels([[
+    ui.iconFA('redo', () => inputSequences.value = ''),
+    ui.iconFA('plus', () => addModificationButton()),
+    ui.iconFA('arrow-to-bottom', () => saveAsCsv(table))
+  ]]);
 
   $('.inputSequence textarea')
     .css('resize','none')
