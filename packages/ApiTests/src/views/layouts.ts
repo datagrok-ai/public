ccd38d213877b7284d9843c79e8be2aaadd58b21
Volodymyr Dyma <emissaryprog@gmail.com>
2022-02-09 19:38:30 +0200

    ApiTests: TableView.dockManager.dock test

--- packages/ApiTests/src/views/layouts.ts
+++ packages/ApiTests/src/views/layouts.ts
@@ -1,6 +1,7 @@
 import * as grok from 'datagrok-api/grok';
 import * as DG from 'datagrok-api/dg';
 import { after, before, category, expect, test } from "@datagrok-libraries/utils/src/test";
+import wu from 'wu';
 
 category('Layouts', () => {
   let df: DG.DataFrame;
@@ -26,6 +27,14 @@ category('Layouts', () => {
     const layout = tv.saveLayout();
     const json = layout.toJson();
     expect(DG.ViewLayout.fromJson(json) instanceof DG.ViewLayout, true);
+  });
+
+  test('TableView.dockManager.dock', async () => {
+    const viewer = df.plot.scatter();
+    viewer.temp['test'] = true;
+    expect(wu(tv.viewers).find((v) => v.temp['test']), undefined);
+    tv.dockManager.dock(viewer, DG.DOCK_TYPE.DOWN);
+    expect(wu(tv.viewers).find((v) => v.temp['test'])?.temp['test'], true);
   });
 
   after(async () => {
