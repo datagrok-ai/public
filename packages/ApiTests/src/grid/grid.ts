f790d3096eab6de92d44c566a5473417cf35f007
osakhniuk <osakhniuk@datagrok.ai>
2022-02-10 17:21:55 +0200

    GROK-10418 API Test: multi-values-column

--- packages/ApiTests/src/grid/grid.ts
+++ packages/ApiTests/src/grid/grid.ts
@@ -2,10 +2,10 @@ import { after, before, category, delay, expect, test } from "@datagrok-librarie
 import * as grok from 'datagrok-api/grok';
 import * as ui from 'datagrok-api/ui';
 import * as DG from 'datagrok-api/dg';
-import { checkHTMLElement } from './utils';
+import { checkHTMLElement } from '../ui/utils';
 import {Grid} from "datagrok-api/dg";
 
-category('UI: Grid', () => {
+category('Grid', () => {
     let v: DG.TableView;
     let  demog = grok.data.demo.demog(1000);
     demog.columns.byName('study').name = '~study';
@@ -57,27 +57,27 @@ category('UI: Grid', () => {
         if (raceTags[0][0] != '.color-coding-categorical' || raceTags[0][1] != '{"Asian":4278190335,"Black":4286578816,"Caucasian":4278547786,"Other":4293188935}')
             throw 'Categorical Color Coding error'
 
-        //numerical height column check for Conditional ColorCoding
+        //numerical HEIGHT column check for Conditional ColorCoding
         let heightTags:string[] = Array.from(demog.col('height')!.tags);
         if (heightTags[0][0] != '.color-coding-type' || heightTags[0][1] != 'Conditional' || heightTags[1][0] != '.color-coding-conditional' || heightTags[1][1] != '{"20-170":"#00FF00","170-190":"#220505"}')
             throw 'Conditional Color Coding error'
 
-        //numerical height column check for Linear ColorCoding
+        //numerical AGE column check for Linear ColorCoding
         let ageTags:string[] = Array.from(demog.col('age')!.tags);
         if (ageTags[0][0] != '.color-coding-type' || ageTags[0][1] != 'Linear' || ageTags[1][0] != '.color-coding-linear' || ageTags[1][1] != '[4294944000, 4278255360]')
             throw 'Linear Color Coding error'
     });
 
     test('grid.columnVisibility', async () => {
-        let studyColVisiable = v.grid.columns.byName('~study')!.visible;
+        let studyColVisible = v.grid.columns.byName('~study')!.visible;
 
         v.grid.columns.setVisible(['age', 'sex', 'race', 'height', 'weight', 'site', 'subj', 'started']);
-        let diseaseColVisiable = v.grid.columns.byName('disease')!.visible;
+        let diseaseColVisible = v.grid.columns.byName('disease')!.visible;
 
-        if (studyColVisiable != false)
+        if (studyColVisible != false)
             throw 'Hiding a column by adding ~ to the name doesn\'t work'
 
-        if (diseaseColVisiable != false)
+        if (diseaseColVisible != false)
             throw 'Hiding a column by using columns.setVisible doesn\'t work'
     });
 
@@ -87,9 +87,6 @@ category('UI: Grid', () => {
         demog.col('site')!.tags[DG.TAGS.AUTO_CHOICES] = 'New York';
 
         let siteTags:string[] = Array.from(demog.col('site')!.tags);
-
-        console.log('site00' + siteTags[0][0])
-        console.log('site01' + siteTags[0][1])
 
         if (siteTags[0][0] != '.choices' || siteTags[0][1] != '["New York", "Buffalo"]' || siteTags[1][0] !='.auto-choices' || siteTags[1][1] !='New York')
             throw 'Column Controlled Values (Choices) error'
