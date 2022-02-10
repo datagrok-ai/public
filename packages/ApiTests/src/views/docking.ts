7abc71989330556642a3df91e2b725fa81221574
onuf <56073962+onuf@users.noreply.github.com>
2022-02-10 12:23:23 +0200

    ApiTests: layout tests

---
+++ packages/ApiTests/src/views/docking.ts
@@ -0,0 +1,28 @@
+import * as grok from 'datagrok-api/grok';
+import * as DG from 'datagrok-api/dg';
+import { after, before, category, expect, test } from "@datagrok-libraries/utils/src/test";
+import wu from 'wu';
+
+
+category('Docking', () => {
+  let df: DG.DataFrame;
+  let tv: DG.TableView;
+
+  before(async () => {
+    df = grok.data.demo.demog();
+    tv = grok.shell.addTableView(df);
+  });
+
+  test('TableView.dockManager.dock', async () => {
+    const viewer = df.plot.scatter();
+    viewer.temp['test'] = true;
+    expect(wu(tv.viewers).find((v) => v.temp['test']), undefined);
+    tv.dockManager.dock(viewer, DG.DOCK_TYPE.DOWN);
+    expect(wu(tv.viewers).find((v) => v.temp['test'])?.temp['test'], true);
+  });
+
+  after(async () => {
+    tv.close();
+    grok.shell.closeTable(df);
+  });
+});
