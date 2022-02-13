cad046506a73d7a02f2ac933ec8f0aacbd6cf017
Leonid <stolbovla@yandex.ru>
2022-02-13 16:27:21 +0300

    Chem: high resolution for molecules in non-table rendering

--- packages/Chem/src/rendering/rdkit-cell-renderer.ts
+++ packages/Chem/src/rendering/rdkit-cell-renderer.ts
@@ -170,12 +170,17 @@ M  END
 
   _drawMolecule(x: number, y: number, w: number, h: number, onscreenCanvas: HTMLCanvasElement,
     molString: string, scaffoldMolString: string, highlightScaffold: boolean,
-    molRegenerateCoords: boolean, scaffoldRegenerateCoords: boolean, pixelRatioRecalculation: boolean = true) {
-    if (pixelRatioRecalculation) {
-      const r = window.devicePixelRatio;
-      x = r * x; y = r * y;
-      w = r * w; h = r * h;
+    molRegenerateCoords: boolean, scaffoldRegenerateCoords: boolean, modifyCanvas: boolean = false) {
+    const r = window.devicePixelRatio;
+    if (modifyCanvas) {
+      onscreenCanvas.width = w * r;
+      onscreenCanvas.height = h * r;
+      onscreenCanvas.style.width = w + 'px';
+      onscreenCanvas.style.height = h + 'px';
     }
+
+    x = r * x; y = r * y;
+    w = r * w; h = r * h;
 
     const renderObj = this._fetchRender(w, h,
       molString, scaffoldMolString, highlightScaffold, molRegenerateCoords, scaffoldRegenerateCoords);
@@ -203,7 +208,7 @@ M  END
 
     // value-based drawing (coming from HtmlCellRenderer.renderValue)
     if (gridCell.cell.column == null) {
-      this._drawMolecule(x, y, w, h, g.canvas, molString, '', false, false, false, false);
+      this._drawMolecule(x, y, w, h, g.canvas, molString, '', false, false, false, true);
       return;
     }
 
