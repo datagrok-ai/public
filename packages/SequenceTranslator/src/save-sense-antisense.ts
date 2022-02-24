f9ecec76a4fb8319d1587f5718753a891945a523
Leonid <stolbovla@yandex.ru>
2022-02-24 18:45:57 +0300

    SequenceTranslatar: saving as one chem entity

--- packages/SequenceTranslator/src/save-sense-antisense.ts
+++ packages/SequenceTranslator/src/save-sense-antisense.ts
@@ -5,14 +5,21 @@ import * as OCL from 'openchemlib/full.js';
 export function saveSenseAntiSense() {
   const ssInput = ui.textInput('Sense Strand 5\' ->3\'', '');
   const asInput = ui.textInput('Anti Sense 3\' ->5\'', '');
+  const saveOption = ui.switchInput('save as one entity', false);
   const saveBtn = ui.button('Save SDF', () => {
     const smiSS = sequenceToSmiles(ssInput.value);
     const smiAS = sequenceToSmiles(asInput.value, true);
-    const result =
+    let result: string;
+    if (saveOption.value)
+      result = `${OCL.Molecule.fromSmiles(smiSS + '.' + smiAS).toMolfile()}\n\n$$$$\n`;
+    else {
+      result =
       `${OCL.Molecule.fromSmiles(smiSS).toMolfile()}\n` +
       `>  <Sequence>\nSense Strand\n\n$$$$\n` +
       `${OCL.Molecule.fromSmiles(smiAS).toMolfile()}\n` +
       `>  <Sequence>\nAnti Sense\n\n$$$$\n`;
+    }
+
     const element = document.createElement('a');
     element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(result));
     element.setAttribute('download', ssInput.value.replace(/\s/g, '') + '.sdf');
@@ -26,6 +33,7 @@ export function saveSenseAntiSense() {
         ui.divV([
           ui.div([ssInput.root]),
           ui.div([asInput.root]),
+          saveOption,
           ui.buttonsInput([saveBtn]),
         ], 'ui-form'),
       ], 'ui-form'),
