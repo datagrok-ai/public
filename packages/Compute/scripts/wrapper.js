f0b4f33ea693c7df73bc47ff9e1999843c848c9e
Alex Paramonov <aparamonov@datagrok.ai>
2022-03-18 17:01:23 +0200

    GROK-10562 Ability to search JS-models in Model Catalog

---
+++ packages/Compute/scripts/wrapper.js
@@ -0,0 +1,8 @@
+//name: wrapper
+//language: javascript
+//output: view v
+//tags: model
+//meta.domain: math
+
+v = grok.shell.newView('Wrapper', [ui.div(['hello world'])]);
+// here, launch the app
