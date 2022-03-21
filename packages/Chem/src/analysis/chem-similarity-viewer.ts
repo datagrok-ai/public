dd0b327c8c1869c37e9143a3b6c10a923664e62f
Maria Dolotova <mahouny85@mail.ru>
2022-03-21 18:56:50 +0300

    Chem: added choice inputs for fingerprint and distance metric, excluded target molecule from search results

--- packages/Chem/src/analysis/chem-similarity-viewer.ts
+++ packages/Chem/src/analysis/chem-similarity-viewer.ts
@@ -9,6 +9,7 @@ import {Fingerprint} from "../utils/chem-common";
 import {chem} from "datagrok-api/grok";
 import Sketcher = chem.Sketcher;
 import {renderMolecule} from "../rendering/render-molecule";
+import { createChoiceInputProperty, createChoiceInputsDiv, updateChoiceInputValue } from '../utils/ui-utils';
 
 export class ChemSimilarityViewer extends DG.JsViewer {
   moleculeColumn?: DG.Column;
@@ -16,16 +17,21 @@ export class ChemSimilarityViewer extends DG.JsViewer {
   hotSearch: boolean;
   sketchButton: HTMLButtonElement;
   sketchedMolecule: string = "";
-  distanceMetric: string;
+  distanceMetric: string = '';
   curIdx: number = 0;
   molCol: DG.Column | null = null;
   idxs: DG.Column | null = null;
   scores: DG.Column | null = null;
   limit: number;
   minScore: number;
-  fingerprint: string;
+  fingerprint: string = '';
   gridSelect: boolean = false;
   targetMoleculeIdx: number = 0;
+  choiceInputPostfix = 'Choice';
+  choiceInputProps = {
+    'distanceMetric': {choiceInputName: 'Distance Metric', options: Object.keys(similarityMetric)},
+    'fingerprint': {choiceInputName: 'Fingerprint', options: ['Morgan', 'RDKit', 'Pattern']}
+  };
 
   get targetMolecule(): string {
     return this.isEditedFromSketcher
@@ -37,8 +43,9 @@ export class ChemSimilarityViewer extends DG.JsViewer {
     super();
     this.limit = this.int('limit', 10);
     this.minScore = this.float('minScore', 0.1);
-    this.distanceMetric = this.string('distanceMetric', 'Tanimoto', {choices: Object.keys(similarityMetric)});
-    this.fingerprint = this.string('fingerprint', 'Morgan', {choices: ['Morgan', 'RDKit', 'Pattern']});
+    Object.keys(this.choiceInputProps).forEach(key => {
+      createChoiceInputProperty(key, this, this.choiceInputProps, this.choiceInputPostfix);
+    });
     this.hotSearch = this.bool('hotSearch', true);
     this.sketchButton = ui.button('Sketch', () => {
       const sketcher = new Sketcher();
@@ -72,6 +79,7 @@ export class ChemSimilarityViewer extends DG.JsViewer {
 
   onPropertyChanged(property: Property): void {
     super.onPropertyChanged(property);
+    updateChoiceInputValue(this, property.name, this.choiceInputPostfix);
     this.render();
   }
 
@@ -97,46 +105,52 @@ export class ChemSimilarityViewer extends DG.JsViewer {
       const grids = []; 
       let cnt = 0, cnt2 = 0;
       panel[cnt++] = ui.h1('Reference');
-      panel[cnt++] = renderMolecule(this.targetMolecule);
+      panel[cnt++] = ui.divH([
+        renderMolecule(this.targetMolecule),
+        createChoiceInputsDiv(this, this.choiceInputProps, this.choiceInputPostfix)
+      ])
       panel[cnt++] = this.sketchButton;
       if (this.molCol && this.idxs && this.scores) {
         for (let i = 0; i < this.molCol.length; ++i) {
-          let grid = ui.div([
-            renderMolecule(this.molCol?.get(i)),
-            ui.label(`${this.scores.get(i).toPrecision(2)}`)],
-            {style: {width: '200px', height: '120px', margin: '5px'}}
-          );
-          let divClass = 'd4-flex-col';
-          if (this.idxs.get(i) == this.curIdx) {
-            divClass += ' d4-current';
-            grid.style.backgroundColor = '#ddffd9';
-          } 
-          if (this.dataFrame!.selection.get(this.idxs.get(i))) {
-            divClass += ' d4-selected';
-            if (divClass == 'd4-flex-col d4-selected')
-              grid.style.backgroundColor = '#f8f8df';
-            else 
-              grid.style.backgroundColor = '#d3f8bd';
-          }
-          $(grid).addClass(divClass);
-          grid.addEventListener('click', (event: MouseEvent) => {
-            if (this.dataFrame && this.idxs) {
-              if (event.shiftKey || event.altKey) {
-                this.dataFrame.selection.set(this.idxs.get(i), true);
-              } else if (event.metaKey) {
-                let selected = this.dataFrame.selection;
-                this.dataFrame.selection.set(this.idxs.get(i), !selected.get(this.idxs.get(i)));
-              } else {
-                this.dataFrame.currentRowIdx = this.idxs.get(i);
-                this.gridSelect = true;
-              }
+          const idx = this.idxs.get(i);
+          if (idx !== this.targetMoleculeIdx) {
+            let grid = ui.div([
+              renderMolecule(this.molCol?.get(i)),
+              ui.label(`${this.scores.get(i).toPrecision(2)}`)],
+              { style: { width: '200px', height: '120px', margin: '5px' } }
+            );
+            let divClass = 'd4-flex-col';
+            if (idx == this.curIdx) {
+              divClass += ' d4-current';
+              grid.style.backgroundColor = '#ddffd9';
             }
-          });
-          grids[cnt2++] = grid;
+            if (this.dataFrame!.selection.get(idx)) {
+              divClass += ' d4-selected';
+              if (divClass == 'd4-flex-col d4-selected')
+                grid.style.backgroundColor = '#f8f8df';
+              else
+                grid.style.backgroundColor = '#d3f8bd';
+            }
+            $(grid).addClass(divClass);
+            grid.addEventListener('click', (event: MouseEvent) => {
+              if (this.dataFrame && this.idxs) {
+                if (event.shiftKey || event.altKey) {
+                  this.dataFrame.selection.set(idx, true);
+                } else if (event.metaKey) {
+                  let selected = this.dataFrame.selection;
+                  this.dataFrame.selection.set(idx, !selected.get(idx));
+                } else {
+                  this.dataFrame.currentRowIdx = idx;
+                  this.gridSelect = true;
+                }
+              }
+            });
+            grids[cnt2++] = grid;
+          }
         }
       }
-      panel[cnt++] = ui.div(grids, {classes: 'd4-flex-wrap'});
-      this.root.appendChild(ui.div(panel, {style: {margin: '5px'}}));
+      panel[cnt++] = ui.div(grids, { classes: 'd4-flex-wrap' });
+      this.root.appendChild(ui.div(panel, { style: { margin: '5px' } }));
     }
   }
 }
