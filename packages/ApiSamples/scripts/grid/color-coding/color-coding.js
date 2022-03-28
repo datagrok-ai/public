a51fe75a9867a763a918a17070a764332c3aeb0f
skalkin <skalkin@gmail.com>
2022-03-28 12:17:34 -0400

    Added an example of turning the color-coding off

--- packages/ApiSamples/scripts/grid/color-coding/color-coding.js
+++ packages/ApiSamples/scripts/grid/color-coding/color-coding.js
@@ -13,3 +13,7 @@ t.col('race').tags[DG.TAGS.COLOR_CODING_TYPE] = 'Categorical';
 t.col('race').tags[DG.TAGS.COLOR_CODING_CATEGORICAL] = `{"Asian":4278190335,"Black":4286578816}`;
 
 grok.shell.addTableView(t);
+
+// To turn the color-coding off, use either approach:
+//t.col('height').tags[DG.TAGS.COLOR_CODING_TYPE] = 'Off';
+//delete t.col('height').tags[DG.TAGS.COLOR_CODING_TYPE];
