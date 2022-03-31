2bdd3ef360e928749801893df56d91c3c14d1c30
Vadym Kovadlo <vadym.kovadlo@gmail.com>
2022-03-31 22:29:13 +0300

    SequenceTranslator: removed 3dx saving option

--- packages/SequenceTranslator/src/structures-works/save-sense-antisense.ts
+++ packages/SequenceTranslator/src/structures-works/save-sense-antisense.ts
@@ -2,12 +2,12 @@ import * as ui from 'datagrok-api/ui';
 import {sequenceToMolV3000} from '../structures-works/from-monomers';
 import {linkV3000} from '../structures-works/mol-transformations';
 
-export function saveSdf(as: string, ss: string, oneEntity: boolean, for3dx: boolean) {
+export function saveSdf(as: string, ss: string, oneEntity: boolean) {
   const molSS = sequenceToMolV3000(ss);
   const molAS = sequenceToMolV3000(as, true);
   let result: string;
   if (oneEntity)
-    result = linkV3000([molSS, molAS], for3dx, true) + '\n\n$$$$\n';
+    result = linkV3000([molSS, molAS], true) + '\n\n$$$$\n';
   else {
     result =
     molSS + '\n' +
@@ -26,9 +26,8 @@ export function saveSenseAntiSense() {
   const moleculeSvgDiv = ui.block([]);
   const ssInput = ui.textInput('Sense Strand 5\' ->3\'', '');
   const asInput = ui.textInput('Anti Sense 3\' ->5\'', '');
-  const saveOption = ui.switchInput('save as one entity', true);
-  const save3dx = ui.switchInput('save for 3dx', true);
-  const saveBtn = ui.button('Save SDF', () => saveSdf(asInput.value, ssInput.value, saveOption.value, save3dx.value));
+  const saveOption = ui.switchInput('Save as one entity', true);
+  const saveBtn = ui.button('Save SDF', () => saveSdf(asInput.value, ssInput.value, saveOption.value));
 
   const saveSection = ui.panel([
     ui.div([
@@ -38,7 +37,6 @@ export function saveSenseAntiSense() {
           ui.div([ssInput.root]),
           ui.div([asInput.root]),
           saveOption,
-          save3dx,
           ui.buttonsInput([saveBtn]),
         ], 'ui-form'),
       ], 'ui-form'),
