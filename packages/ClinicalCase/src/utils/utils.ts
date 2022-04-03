3359e06ce1fe784b3389d084c98a2cf6f40e33e1
Maria Dolotova <mahouny85@mail.ru>
2022-04-03 19:25:21 +0300

    Clinical Case: implemented p-value calculating for categorical variables

--- packages/ClinicalCase/src/utils/utils.ts
+++ packages/ClinicalCase/src/utils/utils.ts
@@ -1,4 +1,12 @@
+import * as DG from 'datagrok-api/dg';
+
 export function updateDivInnerHTML(div: HTMLElement, content: any) {
   div.innerHTML = '';
   div.append(content);
+}
+
+export function createFilters(df: DG.DataFrame) {
+  return DG.Viewer.fromType('Filters', df, {
+    'showContextMenu': false,
+  });
 }
