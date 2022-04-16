a217857ee7d6cc6dd7b9e14692193ee6d62ab9d4
skalkin <skalkin@gmail.com>
2022-04-16 15:09:58 -0400

    Retired the obsolete SDTM package. All hail ClinicalCase!

--- packages/Sdtm/queries/mh.sql
+++
@@ -1,9 +0,0 @@
---name: MedicalHistory
---input: string study
---input: string subj
---connection: clin
-select mhterm, mhenrf
-from mh
-where studyid = @study and usubjid = @subj and mhoccur = 'Y'
-order by mhterm
---end
