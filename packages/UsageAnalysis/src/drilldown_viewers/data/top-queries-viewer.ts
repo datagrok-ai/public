de8013c5b7e5fdd575ae07b4f47dfac383aa8a2b
kdoncov <KDoncov@its.jnj.com>
2022-04-20 17:59:26 +0300

    Usage analysis: lazy tabs, removed re-creation of tabs, etc.

---
+++ packages/UsageAnalysis/src/drilldown_viewers/data/top-queries-viewer.ts
@@ -0,0 +1,47 @@
+import {UaFilterableQueryViewer} from "../../viewers/ua-filterable-query-viewer";
+import * as DG from "datagrok-api/dg";
+import {UaQueryViewer} from "../../viewers/abstract/ua-query-viewer";
+import {TopQueriesUsingDataSource} from "../top-queries-using-data-source";
+import * as grok from "datagrok-api/grok";
+import * as ui from "datagrok-api/ui";
+import {UaFilter} from "../../filter2";
+import {PropertyPanel} from "../../property-panel";
+import {UaDataFrameQueryViewer} from "../../viewers/ua-data-frame-query-viewer";
+import {BehaviorSubject} from "rxjs"
+import {TopFunctionErrorsViewer} from "../function_errors/top-function-errors-viewer";
+
+export class TopQueriesViewer extends UaFilterableQueryViewer {
+
+  public constructor(filterStream: BehaviorSubject<UaFilter>) {
+    super(
+        filterStream,
+        'Queries',
+        'TopQueries',
+        (t: DG.DataFrame) => {
+          let viewer = DG.Viewer.barChart(t, UaQueryViewer.defaultBarchartOptions);
+          viewer.onEvent('d4-bar-chart-on-category-clicked').subscribe(async (args) => {
+            let entity = await grok.dapi.queries.filter(`shortName = "${args.args.categories[0]}"`).first();
+            let pp = new PropertyPanel(
+                entity,
+                [
+                new UaDataFrameQueryViewer(
+                    'Users Of Query',
+                    'TopUsersOfQuery',
+                    (t: DG.DataFrame) => DG.Viewer.barChart(t, UaQueryViewer.defaultBarchartOptions).root,
+                    null as any,
+                    {name: args.args.categories[0]},
+                    filterStream.getValue(),
+                    false
+                )
+            ],
+            `Query: ${args.args.categories[0]}`,
+            'Query');
+
+            grok.shell.o = pp.getRoot();
+          });
+          return viewer.root;
+        }
+    );
+  }
+
+}
