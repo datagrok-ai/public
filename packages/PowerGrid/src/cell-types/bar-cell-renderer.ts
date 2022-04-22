1119b430a512cfca8f26dd4f6eaa031e16dce559
tanas80 <atanas@datagrok.ai>
2022-04-22 16:20:18 +0400

    Closes #491: Make typescript renderers for PowerGrid package

--- packages/PowerGrid/src/cell-types/bar-cell-renderer.ts
+++ packages/PowerGrid/src/cell-types/bar-cell-renderer.ts
@@ -9,18 +9,23 @@ interface BarCellSettings {
 const BarCellSettingsProperties = {
   color: DG.Property.js('color', TYPE.STRING),
   radius: DG.Property.js('radius', TYPE.STRING),
-}
+};
 
 export class BarCellRenderer extends DG.GridCellRenderer {
-  get name() { return 'bar'; }
-  get cellType() { return 'bar'; }
+  get name() {return 'bar';}
 
-  render(g: CanvasRenderingContext2D, x: number, y: number, w: number, h: number, gridCell: DG.GridCell, cellStyle: DG.GridCellStyle) {
+  get cellType() {return 'bar';}
+
+  render(
+    g: CanvasRenderingContext2D,
+    x: number, y: number, w: number, h: number,
+    gridCell: DG.GridCell, cellStyle: DG.GridCellStyle,
+  ) {
     if (gridCell.cell.isNone())
       return;
 
     const ratio = gridCell.cell.value / gridCell.cell.column.max;
     g.fillStyle = '#006400';
-    g.roundRect(x + 2, y + 4 , (w - 4) * ratio, h - 8, 4).fill();
+    g.roundRect(x + 2, y + 4, (w - 4) * ratio, h - 8, 4).fill();
   }
 }
--- packages/PowerGrid/src/sparklines/sparklines-lines.ts
+++ packages/PowerGrid/src/sparklines/sparklines-lines.ts
@@ -1,74 +1,82 @@
 import * as DG from 'datagrok-api/dg';
 import {InputBase, Property, TYPE} from 'datagrok-api/dg';
 import * as ui from 'datagrok-api/ui';
-import {GridCell, Point} from "datagrok-api/src/grid";
-import {Paint} from "datagrok-api/src/utils";
-import {Color} from "datagrok-api/src/widgets";
-import {MARKER_TYPE} from "datagrok-api/src/const";
+import {GridCell, Point, Rect} from 'datagrok-api/src/grid';
+import {Paint} from 'datagrok-api/src/utils';
+import {Color} from 'datagrok-api/src/widgets';
+import {MARKER_TYPE} from 'datagrok-api/src/const';
+import {getSettingsBase, names, SummarySettingsBase} from './base';
 
-interface SparklineSettings {
+
+interface SparklineSettings extends SummarySettingsBase {
   normalize: boolean;
-  columnNames: string[];
 }
 
-function names(columns: Iterable<DG.Column>): string[] {
-  return Array.from(columns).map((c: any) => c.name)
-}
 
 function getSettings(gc: DG.GridColumn): SparklineSettings {
   return gc.settings ??= {
-    normalize: true,
-    columnNames: names(gc.grid.dataFrame.columns.numerical)
-  }
+    ...getSettingsBase(gc),
+    ...{normalize: true},
+  };
 }
 
-function getDataColumns(gc: DG.GridColumn): DG.Column[] {
-  return gc.grid.dataFrame.columns.byNames(getSettings(gc).columnNames);
-}
 
 export class SparklineCellRenderer extends DG.GridCellRenderer {
-  get name() { return 'sparkline'; }
-  get cellType() { return 'sparkline'; }
+  get name() {return 'sparkline';}
 
-  render(g: CanvasRenderingContext2D, x: number, y: number, w: number, h: number, gridCell: GridCell, cellStyle: DG.GridCellStyle) {
+  get cellType() {return 'sparkline_ts';}
+
+  render(
+    g: CanvasRenderingContext2D,
+    x: number, y: number, w: number, h: number,
+    gridCell: GridCell, cellStyle: DG.GridCellStyle,
+  ) {
+    const df = gridCell.grid.dataFrame;
+
+    if (w < 20 || h < 10 || df === void 0) return;
 
     const settings = getSettings(gridCell.gridColumn);
-    x += 4; y += 2; w -= 6; h -= 4;
-
-    if (w < 20 || h < 10) return;
-
+    const b = new Rect(x, y, w, h).inflate(-2, -2);
     g.strokeStyle = 'lightgrey';
     g.lineWidth = 1;
 
-    let row = gridCell.cell.row.idx;
-    let cols = gridCell.grid.dataFrame.columns.byNames(settings.columnNames);
-    let gmin = settings.normalize ? 0 : Math.min(...cols.map((c: DG.Column) => c.min));
-    let gmax = settings.normalize ? 0 : Math.max(...cols.map((c: DG.Column) => c.max));
+    const row = gridCell.cell.row.idx;
+    const cols = df.columns.byNames(settings.columnNames);
+    const gmin = settings.normalize ? 0 : Math.min(...cols.map((c: DG.Column) => c.min));
+    const gmax = settings.normalize ? 0 : Math.max(...cols.map((c: DG.Column) => c.max));
 
     function getPos(col: number, row: number): Point {
       const r: number = settings.normalize ? cols[col].scale(row) : (cols[col].get(row) - gmin) / (gmax - gmin);
-      return new Point(x + w * (cols.length == 1 ? 0 : col / (cols.length - 1)), (y + h) - h * r);
+      return new Point(
+        b.left + b.width * (cols.length == 1 ? 0 : col / (cols.length - 1)),
+        (b.top + b.height) - b.height * r);
     }
 
     g.beginPath();
+    let started = false;
     for (let i = 0; i < cols.length; i++) {
-      let p = getPos(i, row);
+      if (!cols[i].isNone(row)) {
+        const p = getPos(i, row);
 
-      if (i == 0 || cols[i].isNone(row))
-        g.moveTo(p.x, p.y);
-      else
-        g.lineTo(p.x, p.y);
+        if (!started) {
+          g.moveTo(p.x, p.y);
+          started = true;
+        } else
+          g.lineTo(p.x, p.y);
+      }
     }
     g.stroke();
 
     for (let i = 0; i < cols.length; i++) {
-      let p = getPos(i, row);
-      Paint.marker(g, MARKER_TYPE.CIRCLE, p.x, p.y, Color.red, 3);
+      if (!cols[i].isNone(row)) {
+        const p = getPos(i, row);
+        Paint.marker(g, MARKER_TYPE.CIRCLE, p.x, p.y, Color.blue, 3);
+      }
     }
   }
 
   renderSettings(gridColumn: DG.GridColumn): HTMLElement {
-    gridColumn.settings ??= { normalize: true };
+    gridColumn.settings ??= {normalize: true};
     const settings: SparklineSettings = gridColumn.settings;
 
     const normalizeInput = InputBase.forProperty(Property.js('normalize', TYPE.BOOL), settings);
@@ -82,8 +90,8 @@ export class SparklineCellRenderer extends DG.GridCellRenderer {
         console.log(JSON.stringify(gridColumn.settings));
       }, {
         available: names(gridColumn.grid.dataFrame.columns.numerical),
-        checked: settings?.columnNames ?? names(gridColumn.grid.dataFrame.columns.numerical)
-      })
+        checked: settings?.columnNames ?? names(gridColumn.grid.dataFrame.columns.numerical),
+      }),
     ]);
   }
 }
