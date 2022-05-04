f924ee9d285cee12c3923b4e7c8a9c00ee7eb6dd
Volodymyr Dyma <emissaryprog@gmail.com>
2022-05-04 22:29:23 +0300

    #534: TableQuery & DbTableQueryBuilder tests WIP

--- packages/ApiTests/src/dapi/entities.ts
+++ packages/ApiTests/src/dapi/entities.ts
@@ -1,8 +1,8 @@
-// import {after, before, category, expect, test} from "@datagrok-libraries/utils/src/test";
-// import * as grok from 'datagrok-api/grok';
-// import * as ui from 'datagrok-api/ui';
-// import * as DG from 'datagrok-api/dg';
-//
+import {after, before, category, expect, test} from "@datagrok-libraries/utils/src/test";
+import * as grok from 'datagrok-api/grok';
+import * as ui from 'datagrok-api/ui';
+import * as DG from 'datagrok-api/dg';
+
 // category('Dapi: properties', () => {
 //
 //     test('Dapi: properties - save, get, delete', async () => {
@@ -21,3 +21,104 @@
 //     });
 //
 // });
+
+category('DataConnection', () => {
+  
+});
+
+category('TableQuery', () => {
+  let dc: DG.DataConnection;
+  const tableName = 'public.chats';
+  const fields = ['id', 'name'];
+  const fieldsAll = ['id', 'name', 'direct', 'firendly_name', 'group_id', 'private', 'views'];
+
+  before(async () => {
+    const dcParams = {
+      dataSource: 'PostgreSQL', server: 'localhost:5432', db: 'datagrok_dev', login: 'datagrok_dev', password: '123'};
+    dc = DG.DataConnection.createDB('test', dcParams);
+    dc = await grok.dapi.connections.save(dc);
+  });
+
+  test('Create', async () => {
+    const tq = DG.TableQuery.create(dc);
+    expect(tq instanceof DG.TableQuery, true);
+  });
+
+  test('Table', async () => {
+    const tq = DG.TableQuery.create(dc);
+    tq.table = tableName;
+    expect(tq.table, tableName);
+  });
+
+  test('Fields', async () => {
+    const tq = DG.TableQuery.create(dc);
+    tq.fields = fields;
+    expectArray(tq.fields, fields);
+  });
+
+  test('Where clauses', async () => {
+
+  });
+
+  test('Aggregations', async () => {
+
+  });
+
+  test('Having', async () => {
+
+  });
+
+  test('Order by', async () => {
+
+  });
+
+  test('From table', async () => {
+
+  });
+
+  test('From', async () => {
+
+  });
+});
+
+category('DbTableQueryBuilder', () => {
+  test('From table', async () => {
+
+  });
+
+  test('From', async () => {
+
+  });
+
+  test('Select all', async () => {
+
+  });
+  
+  test('Select', async () => {
+
+  });
+
+  test('Group by', async () => {
+
+  });
+
+  test('Pivot on', async () => {
+
+  });
+
+  test('where', async () => {
+
+  });
+
+  test('Sort by', async () => {
+
+  });
+
+  test('Limit', async () => {
+
+  });
+
+  test('Build', async () => {
+
+  });
+});
