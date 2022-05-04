193ea8c660fcd0863dbb640a5ee869622acb041d
onuf <56073962+onuf@users.noreply.github.com>
2022-05-04 14:47:15 +0200

    DevTools: fix the use of ui.split, linting

--- packages/DevTools/src/viewers-gallery.ts
+++ packages/DevTools/src/viewers-gallery.ts
@@ -67,34 +67,34 @@ const viewerOptions = {
     "multiAxis": true,
     "showSplitSelector": false,
     "showColumnSelectionPanel": false,
-    "legendVisibility":"Never",
+    "legendVisibility": "Never",
     "marginLeft": 10,
     "marginRight": 10,
     "marginTop": 10,
     "marginBottom": 10,
-  }
-  let tempName = ''; 
+}
+let tempName = '';
 
-  let contentBox = ui.box();
-  contentBox.classList.add('vg-root-content');
+let contentBox = ui.box();
+contentBox.classList.add('vg-root-content');
 
-  let descriptionBox = ui.box();
-  descriptionBox.classList.add('vg-root-description');
+let descriptionBox = ui.box();
+descriptionBox.classList.add('vg-root-description');
 
-  let filterBox = ui.box();
-  filterBox.classList.add('vg-root-filters');
-  
-  filterBox.style.maxWidth = '230px';
+let filterBox = ui.box();
+filterBox.classList.add('vg-root-filters');
 
-  let dlgTitle;
-  let dlgFooter;
-  let dlg;
-  let view: DG.TableView;
+filterBox.style.maxWidth = '230px';
 
-  let okBtn;
-  //let okBtn = ui.bigButton('ADD',()=>{});
+let dlgTitle;
+let dlgFooter;
+let dlg;
+let view: DG.TableView;
 
-  let backBtn = ui.iconFA('arrow-left',()=>{
+let okBtn;
+//let okBtn = ui.bigButton('ADD',()=>{});
+
+let backBtn = ui.iconFA('arrow-left', () => {
     dlgFooter.hide();
     $('.vg-selection-text').html('Selected: ');
     dlgTitle.text('Add viewer');
@@ -103,11 +103,11 @@ const viewerOptions = {
     $(descriptionBox).hide();
     $(contentBox).show();
     $(filterBox).show();
-  })
-  backBtn.style.marginRight = '10px';
-  backBtn.style.fontSize = '16px';
-  backBtn.classList.remove('fal');
-  backBtn.classList.add('far');
+})
+backBtn.style.marginRight = '10px';
+backBtn.style.fontSize = '16px';
+backBtn.classList.remove('fal');
+backBtn.classList.add('far');
 
 export async function viewersGallery() {
 
@@ -115,22 +115,22 @@ export async function viewersGallery() {
     const table = grok.shell.t;
     view = grok.shell.tableView(table.name);
     let root = ui.box();
-    
-    let recommend = ui.divH([],'viewer-gallery');
-    let selectioText = ui.div(['Selected: '],'vg-selection-text');
-    let cards = ui.divH([],'viewer-gallery');
 
-    $(recommend).css('min-height','360px');
-    $(recommend).css('overflow','hidden');
-    
+    let recommend = ui.divH([], 'viewer-gallery');
+    let selectioText = ui.div(['Selected: '], 'vg-selection-text');
+    let cards = ui.divH([], 'viewer-gallery');
 
-    let showMore = ui.link('Show more', ()=>{
-        if($(showMore).hasClass('vg-link-expand')){
+    $(recommend).css('min-height', '360px');
+    $(recommend).css('overflow', 'hidden');
+
+
+    let showMore = ui.link('Show more', () => {
+        if ($(showMore).hasClass('vg-link-expand')) {
             $(showMore).text('Show less');
             $(showMore).addClass('vg-link-collapse');
             $(showMore).removeClass('vg-link-expand');
             console.log($(showMore))
-        }else if($(showMore).hasClass('vg-link-collapse')){
+        } else if ($(showMore).hasClass('vg-link-collapse')) {
             $(showMore).text('Show more');
             $(showMore).addClass('vg-link-expand');
             $(showMore).removeClass('vg-link-collapse');
@@ -138,128 +138,130 @@ export async function viewersGallery() {
         }
 
         $(recommend).toggleClass('vg-expanded');
-        
-        if($(recommend).hasClass('vg-expanded')){
-            $(recommend).css('min-height','auto');
-            $(recommend).css('overflow','initial');
-        }else{
-            $(recommend).css('min-height','360px');
-            $(recommend).css('overflow','hidden'); 
+
+        if ($(recommend).hasClass('vg-expanded')) {
+            $(recommend).css('min-height', 'auto');
+            $(recommend).css('overflow', 'initial');
+        } else {
+            $(recommend).css('min-height', '360px');
+            $(recommend).css('overflow', 'hidden');
         }
     });
     showMore.className = 'ui-link vg-link vg-link-expand';
     $(showMore).attr('data-content', '\f078');
 
-    let search = ui.searchInput('','',(value)=>{
+    let search = ui.searchInput('', '', (value) => {
         clearRoot([recommend]);
         clearRoot([cards]);
         $(okBtn).hide();
         tempName = '';
         let filter_value = getFilter([filters_type, filters_cat], viewers);
 
-        if (filter_value.length === 0){
-            for (let i in viewers){
+        if (filter_value.length === 0) {
+            for (let i in viewers) {
                 filter_value.push(viewers[i].name)
             }
         }
 
-        if (value != ''){
-            for (let i in viewers){
-                if (filter_value.indexOf(viewers[i].name) != -1 && viewers[i].name.toLowerCase().includes(value.toLowerCase())){
+        if (value != '') {
+            for (let i in viewers) {
+                if (filter_value.indexOf(viewers[i].name) != -1 && viewers[i].name.toLowerCase().includes(value.toLowerCase())) {
                     if (viewers[i].recommend)
-                        recommend.append(render(viewers[i],table))
+                        recommend.append(render(viewers[i], table))
                     else
-                        cards.append(render(viewers[i],table));
+                        cards.append(render(viewers[i], table));
                 }
             }
-        } 
+        }
         else {
-            for (let i in viewers){
-                if (filter_value.indexOf(viewers[i].name) != -1){
+            for (let i in viewers) {
+                if (filter_value.indexOf(viewers[i].name) != -1) {
                     if (viewers[i].recommend)
-                        recommend.append(render(viewers[i],table))
+                        recommend.append(render(viewers[i], table))
                     else
-                        cards.append(render(viewers[i],table));
+                        cards.append(render(viewers[i], table));
                 }
             }
         }
         console.log($(recommend).children());
-        if ($(recommend).children().length == 0){
-            $(recommend).css('min-height','0px');
+        if ($(recommend).children().length == 0) {
+            $(recommend).css('min-height', '0px');
             $(showMore).hide();
-        }else{
-            $(recommend).css('min-height','360px');
-            $(showMore).show(); 
+        } else {
+            $(recommend).css('min-height', '360px');
+            $(showMore).show();
         }
-        $('.vg-selection-text').html('Selected: '+ $('.viewer-gallery').find('.fa-minus').length);   
+        $('.vg-selection-text').html('Selected: ' + $('.viewer-gallery').find('.fa-minus').length);
     });
 
     //@ts-ignore
-    let filters_type = ui.multiChoiceInput('', [''], ['Viewer','Widget'], (value)=>{
+    let filters_type = ui.multiChoiceInput('', [''], ['Viewer', 'Widget'], (value) => {
         getFilter([filters_type, filters_cat], viewers);
         search.fireChanged();
     });
 
     //@ts-ignore
-    let filters_cat = ui.multiChoiceInput('', [''], ['Comparisons','Trends','Correlations','Relationships', 'Maps', 'Others'], (value)=>{
+    let filters_cat = ui.multiChoiceInput('', [''], ['Comparisons', 'Trends', 'Correlations', 'Relationships', 'Maps', 'Others'], (value) => {
         getFilter([filters_type, filters_cat], viewers);
         search.fireChanged();
     })
 
     //@ts-ignore
     search.input.placeholder = 'Search by name or type';
-    
-    let searchBlock = ui.block([ui.div([search.input],'d4-search-ba')], 'vg-controls grok-gallery-search-bar');
-    
-    clearRoot([filterBox,contentBox,descriptionBox]);
-    
-    for (let i=0; i<DG.Viewer.getViewerTypes().length; i++){
 
-        Object.assign(viewers, {[i]:{
-            icon:'grok-icon svg-icon svg-'+DG.Viewer.getViewerTypes()[i].toLowerCase().replace(/(\s)/g,'-'),
-            name: insertSpaces(DG.Viewer.getViewerTypes()[i]),
-            type: 'Viewer',
-            recommend: false,
-            category: 'Other',
-            disabled: false
-        }});
+    let searchBlock = ui.block([ui.div([search.input], 'd4-search-ba')], 'vg-controls grok-gallery-search-bar');
 
-        if (i<7){
+    clearRoot([filterBox, contentBox, descriptionBox]);
+
+    for (let i = 0; i < DG.Viewer.getViewerTypes().length; i++) {
+
+        Object.assign(viewers, {
+            [i]: {
+                icon: 'grok-icon svg-icon svg-' + DG.Viewer.getViewerTypes()[i].toLowerCase().replace(/(\s)/g, '-'),
+                name: insertSpaces(DG.Viewer.getViewerTypes()[i]),
+                type: 'Viewer',
+                recommend: false,
+                category: 'Other',
+                disabled: false
+            }
+        });
+
+        if (i < 7) {
             viewers[i].recommend = true;
         }
-        
-        if (cat_comparisons.indexOf(viewers[i].name) != -1){
+
+        if (cat_comparisons.indexOf(viewers[i].name) != -1) {
             viewers[i].category = 'Comparisons';
-        }else if (cat_trends.indexOf(viewers[i].name) != -1){
+        } else if (cat_trends.indexOf(viewers[i].name) != -1) {
             viewers[i].category = 'Trends';
-        }else if (cat_correlations.indexOf(viewers[i].name) != -1){
+        } else if (cat_correlations.indexOf(viewers[i].name) != -1) {
             viewers[i].category = 'Correlations';
-        }else if (cat_relationships.indexOf(viewers[i].name) != -1){
+        } else if (cat_relationships.indexOf(viewers[i].name) != -1) {
             viewers[i].category = 'Relationships';
-        }else if (cat_maps.indexOf(viewers[i].name) != -1){
+        } else if (cat_maps.indexOf(viewers[i].name) != -1) {
             viewers[i].category = 'Maps';
-        }else{
+        } else {
             viewers[i].category = 'Others';
         }
 
-        if(viewers[i].name.includes('widget')){
+        if (viewers[i].name.includes('widget')) {
             viewers[i].type = 'Widget';
             viewers[i].icon = 'grok-icon svg-icon svg-project';
         }
 
-        if (i>40){
+        if (i > 40) {
             viewers[i].disabled = true;
         }
 
     }
-    
+
     filterBox.append(ui.divV([
         ui.h3('Type'),
         filters_type.root,
         ui.h3('Category'),
         filters_cat.root
     ], 'vg-filter-panel'));
-    
+
 
     contentBox.append(
         ui.divV([
@@ -289,9 +291,9 @@ export async function viewersGallery() {
             //viewerRoot
         )
         .onOK(() => {
-            if(tempName!=''){view.addViewer(DG.Viewer.fromType(tempName, table))}
-            clearRoot([filterBox,contentBox,descriptionBox]);
-            clearRoot([root]) 
+            if (tempName != '') { view.addViewer(DG.Viewer.fromType(tempName, table)) }
+            clearRoot([filterBox, contentBox, descriptionBox]);
+            clearRoot([root])
             clearRoot([recommend]);
             clearRoot([cards]);
             tempName = '';
@@ -301,12 +303,12 @@ export async function viewersGallery() {
     $(dlg.root).find('.d4-dialog-contents').removeClass('ui-panel');
     $(dlg.root).find('.d4-dialog-contents').addClass('ui-box');
     $(dlg.root).find('.d4-command-bar').append(selectioText);
-    
-    $(dlg.root).find('.d4-dialog-contents').css('padding','0px');
-    $(dlg.root).find('.d4-dialog-header').css('border-bottom','1px solid var(--grey-2)');
+
+    $(dlg.root).find('.d4-dialog-contents').css('padding', '0px');
+    $(dlg.root).find('.d4-dialog-header').css('border-bottom', '1px solid var(--grey-2)');
     $(dlg.root).find('.d4-dialog-header').prepend(backBtn);
-    
-    $(dlg.root).find('.d4-command-bar').css('border-top','1px solid var(--grey-2)');
+
+    $(dlg.root).find('.d4-command-bar').css('border-top', '1px solid var(--grey-2)');
 
     okBtn = $(dlg.root).find('.d4-command-bar > button').first();
     okBtn.addClass('ui-btn-raised');
@@ -317,41 +319,41 @@ export async function viewersGallery() {
     dlgFooter = $(dlg.root).find('.d4-dialog-footer');
     dlgFooter.hide();
 
-    if (grok.shell.v.type == 'TableView'){
+    if (grok.shell.v.type == 'TableView') {
         dlg.showModal(true)
     }
 
-    for (let i in viewers){
+    for (let i in viewers) {
         if (viewers[i].recommend)
-            recommend.append(render(viewers[i],table))
-        else{
-            cards.append(render(viewers[i],table));
-        }  
+            recommend.append(render(viewers[i], table))
+        else {
+            cards.append(render(viewers[i], table));
+        }
     }
 
-    if ($(recommend).children().length == 0){
-        $(recommend).css('min-height','0px');
+    if ($(recommend).children().length == 0) {
+        $(recommend).css('min-height', '0px');
         $(showMore).hide();
-    }else{
-        $(recommend).css('min-height','360px');
-        $(showMore).show(); 
+    } else {
+        $(recommend).css('min-height', '360px');
+        $(showMore).show();
     }
 
 }
 
-function getFilter(inputs:InputBase[], viewers:any){
+function getFilter(inputs: InputBase[], viewers: any) {
     let arr = [];
     let values = [];
-    for (let i in inputs){
+    for (let i in inputs) {
         arr = arr.concat(inputs[i].value);
     }
 
-    if (arr.length>0){
-        for (let i in viewers){
-            if (arr.indexOf(viewers[i].category) != -1){
+    if (arr.length > 0) {
+        for (let i in viewers) {
+            if (arr.indexOf(viewers[i].category) != -1) {
                 values.push(viewers[i].name);
             }
-            if (arr.indexOf(viewers[i].type) != -1){
+            if (arr.indexOf(viewers[i].type) != -1) {
                 values.push(viewers[i].name);
             }
         }
@@ -360,9 +362,9 @@ function getFilter(inputs:InputBase[], viewers:any){
     return values;
 };
 
-function insertSpaces(string:string) {
+function insertSpaces(string: string) {
     string = string.replace(/^(_|-)/g, '')
-	string = string.replace(/(_|-)/g, ' ');
+    string = string.replace(/(_|-)/g, ' ');
     string = string.replace(/([a-z]\s)/g, m => m.toLowerCase());
     string = string.replace(/([a-z])([A-Z])/g, '$1 $2');
     string = string.replace(/([A-Z])([A-Z][a-z])/g, '$1 $2');
@@ -371,31 +373,31 @@ function insertSpaces(string:string) {
     return string
 }
 
-function clearRoot(root: HTMLDivElement[]){
-    for (const i in root){
+function clearRoot(root: HTMLDivElement[]) {
+    for (const i in root) {
         root[i].innerHTML = '';
     }
 }
 
-function render(viewer:any, table:DG.DataFrame){
+function render(viewer: any, table: DG.DataFrame) {
     let root = ui.div([])
     let icon = ui.iconFA('');
-    icon.className = 'grok-icon svg-icon '+viewer.icon;
+    icon.className = 'grok-icon svg-icon ' + viewer.icon;
     let label = ui.div([viewer.name], 'card-label');
-    let add = ui.button(ui.iconFA('plus'), ()=>{
+    let add = ui.button(ui.iconFA('plus'), () => {
         root.click();
     });
 
-    let details = ui.button(ui.icons.help(()=>{}, 'Click to read more about '+viewer.name), ()=>{
+    let details = ui.button(ui.icons.help(() => { }, 'Click to read more about ' + viewer.name), () => {
         dlgFooter.show();
 
         tempName = viewer.name;
-        $('.vg-selection-text').html('Selected: '+ viewer.name);
+        $('.vg-selection-text').html('Selected: ' + viewer.name);
 
         dlgTitle.text(viewer.name);
         //show details block
         $(okBtn).show();
-        $('.vg-selection-text').html('Selected: '+ viewer.name);
+        $('.vg-selection-text').html('Selected: ' + viewer.name);
 
         $(backBtn).show();
         $(descriptionBox).show();
@@ -408,51 +410,51 @@ function render(viewer:any, table:DG.DataFrame){
         viewerbox.append(DG.Viewer.fromType(viewer.name, table).root);
         let tabs = ui.box();
         tabs.append(ui.tabControl({
-            'Description': ()=> markup,
-            'Options': ()=> options
+            'Description': () => markup,
+            'Options': () => options
         }).root);
         tabs.style.paddingLeft = '20px';
-        
+
         clearRoot([descriptionBox])
-        descriptionBox.append(ui.splitH([viewerbox, tabs], false, { style: { height: '100%' } }));   
+        descriptionBox.append(ui.splitH([viewerbox, tabs], { style: { height: '100%' } }));
 
         //@ts-ignore
         let link = DG.Viewer.fromType(viewer.name, table).helpUrl;
-        grok.dapi.fetchProxy('https://raw.githubusercontent.com/datagrok-ai/public/master'+link)
+        grok.dapi.fetchProxy('https://raw.githubusercontent.com/datagrok-ai/public/master' + link)
             .then(response => response.text())
             .then(data => {
                 let res = data.replace(new RegExp('../../', 'g'), 'https://raw.githubusercontent.com/datagrok-ai/public/master/help/')
                 markup.append(ui.markdown(res));
-                $(markup).find('img').css('width','300px');
+                $(markup).find('img').css('width', '300px');
             });
     });
 
     $(add).hide();
 
-    if (viewer.recommend){
+    if (viewer.recommend) {
         let viewerRoot = ui.box(DG.Viewer.fromType(viewer.name, table, viewerOptions).root);
         root.className = 'd4-item-card viewer-gallery vg-card';
-        root.append(ui.block([viewerRoot,ui.divH([label,add, details])]));
-    }else{
+        root.append(ui.block([viewerRoot, ui.divH([label, add, details])]));
+    } else {
         root.className = 'd4-item-card viewer-gallery vg-card-small';
-        if(viewer.disabled)
+        if (viewer.disabled)
             root.classList.add('disabled')
-        root.append(ui.divH([icon,label,add, details]));
+        root.append(ui.divH([icon, label, add, details]));
     }
-    root.addEventListener('click', ()=>{
+    root.addEventListener('click', () => {
         //view.addViewer(DG.Viewer.fromType(tempName, table))
-        if (root.classList.contains('disabled')!=true){
-        dockViewers(viewer.name, table, view);
-        dlg.close();
+        if (root.classList.contains('disabled') != true) {
+            dockViewers(viewer.name, table, view);
+            dlg.close();
         }
     });
 
-    if(viewer.disabled!=true){
-        root.addEventListener('mouseover', ()=>{
+    if (viewer.disabled != true) {
+        root.addEventListener('mouseover', () => {
             $(add).show();
-            ui.tooltip.bind(add, 'Click to add '+viewer.name)
+            ui.tooltip.bind(add, 'Click to add ' + viewer.name)
         });
-        root.addEventListener('mouseout', ()=>{
+        root.addEventListener('mouseout', () => {
             $(add).hide();
         });
     }
@@ -460,6 +462,6 @@ function render(viewer:any, table:DG.DataFrame){
     return root
 }
 
-function dockViewers(viewer:string, table:DG.DataFrame, view: DG.TableView){
+function dockViewers(viewer: string, table: DG.DataFrame, view: DG.TableView) {
     view.addViewer(DG.Viewer.fromType(viewer, table));
 }
