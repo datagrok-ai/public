59a84df3ef0021c6567e1eef66011be8e7ade894
Volodymyr Dyma <emissaryprog@gmail.com>
2022-05-04 14:20:08 +0300

    Closes #551: package template fixes

--- tools/package-template/src/package-test.ts
+++ tools/package-template/src/package-test.ts
@@ -1,7 +1,6 @@
 import * as DG from "datagrok-api/dg";
 import * as grok from "datagrok-api/grok";
 import {runTests, tests} from '@datagrok-libraries/utils/src/test';
-import "./tests/chem-tests";
 
 export let _package = new DG.Package();
 export {tests}
