cb4f0f304787253e796b9f6dd73b3d659d6c7f9a
Volodymyr Dyma <emissaryprog@gmail.com>
2022-05-05 15:50:18 +0300

    #534: TableQuery & DbTableQueryBuilder tests & fixes WIP

--- packages/ApiTests/src/dapi/entities.ts
+++ packages/ApiTests/src/dapi/entities.ts
@@ -1,4 +1,4 @@
-import {after, before, category, expect, test} from "@datagrok-libraries/utils/src/test";
+import {after, before, category, expect, test, expectArray} from "@datagrok-libraries/utils/src/test";
 import * as grok from 'datagrok-api/grok';
 import * as ui from 'datagrok-api/ui';
 import * as DG from 'datagrok-api/dg';
@@ -26,15 +26,20 @@ category('DataConnection', () => {
   
 });
 
-category('TableQuery', () => {
+category('TableQuery', async () => {
   let dc: DG.DataConnection;
-  const tableName = 'public.chats';
-  const fields = ['id', 'name'];
-  const fieldsAll = ['id', 'name', 'direct', 'firendly_name', 'group_id', 'private', 'views'];
+  const tableName = 'public.orders';
+  const fields = ['orderid', 'freight'];
+  const whereClauses = [{orderid: '10250'}];
+  const aggregationsDb = [{orderid: '10250'}];
+  const havingDb = [{'COUNT(shipcountry)': '2'}];
+  const orderByDb = [{orderid: 'ASC'}];
+  const fromTable = await grok.dapi.tables.first();
+  const from = fromTable.name;
 
   before(async () => {
-    const dcParams = {
-      dataSource: 'PostgreSQL', server: 'localhost:5432', db: 'datagrok_dev', login: 'datagrok_dev', password: '123'};
+    const dcParams = {dataSource: 'PostgresNet', server: 'dev.datagrok.ai:54322', db: 'northwind',
+      login: 'datagrok', password: 'datagrok'};
     dc = DG.DataConnection.createDB('test', dcParams);
     dc = await grok.dapi.connections.save(dc);
   });
@@ -57,65 +62,87 @@ category('TableQuery', () => {
   });
 
   test('Where clauses', async () => {
-
+    const tq = DG.TableQuery.create(dc);
+    tq.whereClauses = whereClauses;
+    expectArray(tq.whereClauses, whereClauses);
   });
 
   test('Aggregations', async () => {
-
+    const tq = DG.TableQuery.create(dc);
+    tq.aggregationsDb = aggregationsDb;
+    expectArray(tq.aggregationsDb, aggregationsDb);
   });
 
   test('Having', async () => {
-
+    const tq = DG.TableQuery.create(dc);
+    tq.havingDb = havingDb;
+    expectArray(tq.havingDb, havingDb);
   });
 
   test('Order by', async () => {
-
+    const tq = DG.TableQuery.create(dc);
+    tq.orderByDb = orderByDb;
+    expectArray(tq.orderByDb, orderByDb);
   });
 
   test('From table', async () => {
-
+    const dtqb = DG.TableQuery.fromTable(fromTable);
+    expect(dtqb instanceof DG.DbTableQueryBuilder, true);
   });
 
   test('From', async () => {
-
+    const dtqb = DG.TableQuery.from(from);
+    expect(dtqb instanceof DG.DbTableQueryBuilder, true);
   });
 });
 
-category('DbTableQueryBuilder', () => {
-  test('From table', async () => {
+category('DbTableQueryBuilder', async () => {
+  const fromTable = await grok.dapi.tables.first();
+  const from = fromTable.name;
 
+  test('From table', async () => {
+    const dtqb = DG.DbTableQueryBuilder.fromTable(fromTable);
+    expect(dtqb instanceof DG.DbTableQueryBuilder, true);
   });
 
   test('From', async () => {
-
+    const dtqb = DG.DbTableQueryBuilder.from(from);
+    expect(dtqb instanceof DG.DbTableQueryBuilder, true);
   });
 
   test('Select all', async () => {
-
+    let dtqb = DG.DbTableQueryBuilder.fromTable(fromTable);
+    dtqb.selectAll();
   });
   
   test('Select', async () => {
-
+    let dtqb = DG.DbTableQueryBuilder.fromTable(fromTable);
+    dtqb.select([]);
   });
 
   test('Group by', async () => {
-
+    let dtqb = DG.DbTableQueryBuilder.fromTable(fromTable);
+    dtqb.groupBy([]);
   });
 
   test('Pivot on', async () => {
-
+    let dtqb = DG.DbTableQueryBuilder.fromTable(fromTable);
+    dtqb.pivotOn([]);
   });
 
   test('where', async () => {
-
+    let dtqb = DG.DbTableQueryBuilder.fromTable(fromTable);
+    dtqb.where('', '');
   });
 
   test('Sort by', async () => {
-
+    let dtqb = DG.DbTableQueryBuilder.fromTable(fromTable);
+    dtqb.sortBy('');
   });
 
   test('Limit', async () => {
-
+    let dtqb = DG.DbTableQueryBuilder.fromTable(fromTable);
+    dtqb.limit(10);
   });
 
   test('Build', async () => {
