bca0fdd8ff7dd796a1f1571e9142e2129c8573f7
tanas80 <atanas@datagrok.ai>
2022-05-13 17:52:44 +0400

    bio WebLogo startPosition endPosition attributes and positionNames column tag

--- libraries/bio/src/index.ts
+++ libraries/bio/src/index.ts
@@ -1,8 +1,8 @@
 import {SeqPalette} from './seq-palettes';
 import {Aminoacids, AminoacidsPalettes} from './aminoacids';
 import {Nucleotides, NucleotidesPalettes} from './nucleotides';
-import {WebLogo} from './viewers/web-logo';
+import {WebLogo, PositionInfo, PositionMonomerInfo} from './viewers/web-logo';
 
 export {SeqPalette, AminoacidsPalettes, Aminoacids, NucleotidesPalettes, Nucleotides};
-export {WebLogo};
+export {WebLogo, PositionInfo, PositionMonomerInfo};
 
