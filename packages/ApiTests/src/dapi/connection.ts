b19d96d7e853fed6631a22abda6c59e0e1090020
Volodymyr Dyma <emissaryprog@gmail.com>
2022-05-18 15:40:16 +0300

    GROK-10727: connection test: create, save, delete

---
+++ packages/ApiTests/src/dapi/connection.ts
@@ -0,0 +1,22 @@
+import * as grok from 'datagrok-api/grok';
+import * as DG from 'datagrok-api/dg';
+
+import { category, expectObject, expect } from "@datagrok-libraries/utils/src/test";
+
+const GDC = grok.dapi.connections;
+
+category('Dapi: connection', () => {
+  const dcParams = {
+    dataSource: 'PostgreSQL', server: 'localhost:5432', db: 'datagrok_dev', login: 'datagrok_dev', password: '123'};
+
+  test('Create, save, delete', async () => {
+    let dc = DG.DataConnection.createDB('Local DG Test', dcParams);
+    dc = await GDC.save(dc);
+    expectObject(dc.parameters, {server: 'localhost:5432', db: 'datagrok_dev'});
+    expect(await dc.test(), 'ok');
+    expect(dc.friendlyName, 'Local D G Test');
+    expect((await GDC.find(dc.id)).id, dc.id);
+    await GDC.delete(dc);
+    expect(await GDC.find(dc.id), undefined);
+  });
+});
