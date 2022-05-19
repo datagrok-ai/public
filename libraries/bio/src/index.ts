1e3f179b8a7bb880c2fc8664b94eefa0d370a036
tanas80 <atanas@datagrok.ai>
2022-05-19 16:24:31 +0400

    Remove index.ts

--- libraries/bio/src/index.ts
+++
@@ -1,8 +0,0 @@
-import {SeqPalette} from './seq-palettes';
-import {Aminoacids, AminoacidsPalettes} from './aminoacids';
-import {Nucleotides, NucleotidesPalettes} from './nucleotides';
-import {WebLogo, PositionInfo, PositionMonomerInfo} from './viewers/web-logo';
-
-export {SeqPalette, AminoacidsPalettes, Aminoacids, NucleotidesPalettes, Nucleotides};
-export {WebLogo, PositionInfo, PositionMonomerInfo};
-
