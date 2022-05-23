06f4d3d2740feb27ce5d000b89df5f12712c937d
dpetrov <dpetrov@gnf.org>
2022-05-22 18:59:53 -0700

    Added the ability to register/unregister RendererUIManager

--- libraries/gridext/src/utils/GridUtils.ts
+++ libraries/gridext/src/utils/GridUtils.ts
@@ -1,6 +1,28 @@
 import * as DG from 'datagrok-api/dg';
 import {GridCellRendererEx} from "../renderer/GridCellRendererEx";
 
+
+export function getInstalledGridForColumn(colGrid : DG.GridColumn) : DG.Grid | null {
+  const dart : any = DG.toDart(colGrid);
+  if(!(dart.m_grid instanceof DG.Grid))
+    return null;
+
+  return dart.m_grid;
+}
+
+export function installGridForColumn(grid : DG.Grid, colGrid : DG.GridColumn) : boolean {
+  if(colGrid.grid instanceof DG.Grid)
+    return false;
+
+  const dart : any = DG.toDart(colGrid);
+  if(dart.m_grid instanceof DG.Grid)
+    return false;
+
+  dart.m_grid = grid;
+  return true;
+}
+
+
 export function setGridColumnRenderer(colGrid : DG.GridColumn, renderer : GridCellRendererEx) : void {
   const dart : any = DG.toDart(colGrid);
   dart.m_renderer = renderer;
