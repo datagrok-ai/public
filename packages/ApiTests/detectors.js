df28cfb1142e461de53be43b2a7d01ec38cc480c
Oleksandra Serhiienko <59337516+Aleksashka11@users.noreply.github.com>
2022-05-23 20:00:39 +0300

    File needed to detect SemType for df tests

---
+++ packages/ApiTests/detectors.js
@@ -0,0 +1,20 @@
+/**
+ * The class contains semantic type detectors.
+ * Detectors are functions tagged with `DG.FUNC_TYPES.SEM_TYPE_DETECTOR`.
+ * See also: https://datagrok.ai/help/develop/how-to/define-semantic-type-detectors
+ * The class name is comprised of <PackageName> and the `PackageDetectors` suffix.
+ * Follow this naming convention to ensure that your detectors are properly loaded.
+ */
+class ApiTestsPackageDetectors extends DG.Package {
+
+  //tags: semTypeDetector
+  //input: column col
+  //output: string semType
+  detectCountry(col) {
+    if (col.name === 'countries' && col.type === DG.TYPE.STRING) {
+      col.semType = 'country';
+      return col.semType;
+    }
+    return null;
+  }
+}
