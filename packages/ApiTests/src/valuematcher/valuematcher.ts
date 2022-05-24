16ae069e150240a0975c0dd94ac22d590fd54c01
Oleksandra Serhiienko <59337516+Aleksashka11@users.noreply.github.com>
2022-05-24 20:03:42 +0300

    JS API Tests: Tests for string and numerical patterns

---
+++ packages/ApiTests/src/valuematcher/valuematcher.ts
@@ -0,0 +1,110 @@
+import * as DG from 'datagrok-api/dg';
+import * as grok from 'datagrok-api/grok';
+import {category, expect, test} from "@datagrok-libraries/utils/src/test";
+
+category('ValueMatcher', () => {
+  test('numerical pattern equals', async () => {
+    let matcher = DG.ValueMatcher.numerical('5');
+    expect(matcher.operator, '=');
+    expect(matcher.match(5).toString(), 'true');
+    expect(matcher.validate(10).toString(), '"10" does not match "5"');
+  });
+
+  test('numerical pattern equals =', async () => {
+    let matcher = DG.ValueMatcher.numerical('= 5');
+    expect(matcher.operator, '=');
+    expect(matcher.match(5).toString(), 'true');
+    expect(matcher.validate(10).toString(), '"10" does not match "= 5"');
+  });
+
+  test('numerical pattern not equals', async () => {
+    let matcher = DG.ValueMatcher.numerical('!= 5');
+    expect(matcher.operator, '!=');
+    expect(matcher.match(5).toString(), 'false');
+    expect(matcher.validate(5).toString(), '"5" does not match "!= 5"');
+  });
+
+  test('numerical pattern greater than', async () => {
+    let matcher = DG.ValueMatcher.numerical('> 30');
+    expect(matcher.operator, '>');
+    expect(matcher.match(40).toString(), 'true');
+    expect(matcher.validate(20).toString(), '"20" does not match "> 30"');
+  });
+
+  test('numerical pattern greater than or equals', async () => {
+    let matcher = DG.ValueMatcher.numerical('>= 5');
+    expect(matcher.operator, '>=');
+    expect(matcher.match(10).toString(), 'true');
+    expect(matcher.validate(1).toString(), '"1" does not match ">= 5"');
+  });
+
+  test('numerical pattern less than', async () => {
+    let matcher = DG.ValueMatcher.numerical('< 10');
+    expect(matcher.operator, '<');
+    expect(matcher.match(15).toString(), 'false');
+    expect(matcher.validate(20).toString(), '"20" does not match "< 10"');
+  });
+
+  test('numerical pattern less than or equals', async () => {
+    let matcher = DG.ValueMatcher.numerical('<= 10');
+    expect(matcher.operator, '<=');
+    expect(matcher.match(5).toString(), 'true');
+    expect(matcher.validate(15).toString(), '"15" does not match "<= 10"');
+  });
+
+  test('numerical pattern range(inclusive)', async () => {
+    let matcher = DG.ValueMatcher.numerical('10-20');
+    expect(matcher.operator, '-');
+    expect(matcher.match(25).toString(), 'false');
+    expect(matcher.validate(30).toString(), '"30" does not match "10-20"');
+  });
+
+  test('numerical pattern in', async () => {
+    let matcher = DG.ValueMatcher.numerical('in(5,10)');
+    expect(matcher.operator, 'in');
+    expect(matcher.match(5).toString(), 'true');
+    expect(matcher.validate(11).toString(), '"11" does not match "in(5,10)"');
+  });
+
+  test('numerical pattern not in', async () => {
+    let matcher = DG.ValueMatcher.numerical('not in(5,10)');
+    expect(matcher.operator, 'not in');
+    expect(matcher.match(12).toString(), 'true');
+    expect(matcher.validate(5).toString(), '"5" does not match "not in(5,10)"');
+  });
+
+  test('string pattern contains', async () => {
+    let matcher = DG.ValueMatcher.string('contains foo');
+    expect(matcher.operator, 'contains');
+    expect(matcher.match('foofd').toString(), 'true');
+    expect(matcher.validate('ffff').toString(), '"ffff" does not match "contains foo"');
+  });
+
+  test('string pattern starts with', async () => {
+    let matcher = DG.ValueMatcher.string('starts with Charles');
+    expect(matcher.operator, 'starts with');
+    expect(matcher.match('hhhCharles').toString(), 'false');
+    expect(matcher.validate('fggdgdgCharles').toString(), '"fggdgdgCharles" does not match "starts with Charles"');
+  });
+
+  test('string pattern ends with', async () => {
+    let matcher = DG.ValueMatcher.string('ends with District');
+    expect(matcher.operator, 'ends with');
+    expect(matcher.match('helloDistrict').toString(), 'true');
+    expect(matcher.validate('ffff').toString(), '"ffff" does not match "ends with District"'); 
+  });
+
+  test('string pattern in', async () => {
+    let matcher = DG.ValueMatcher.string('in (asian, other)');
+    expect(matcher.operator, 'in');
+    expect(matcher.match('asian').toString(), 'true');
+    expect(matcher.validate('nnnn').toString(), '"nnnn" does not match "in (asian, other)"');
+  });
+
+  test('string pattern not in', async () => {
+    let matcher = DG.ValueMatcher.string('not in (m, f)');
+    expect(matcher.operator, 'not in');
+    expect(matcher.match('m').toString(), 'false');
+    expect(matcher.validate('f').toString(), '"f" does not match "not in (m, f)"');
+  });
+});
