3f64a6a600faf843a30c8f7d111e0b3cd808b16f
Sofia Podolsky <spodolskaya@datagrok.ai>
2022-05-24 15:21:37 +0300

    Meta package upgrade flow

---
+++ packages/Meta/bin/commands/add.ts
@@ -0,0 +1,45 @@
+import fs from 'fs';
+import path from 'path';
+
+function addPackageVersion(name: string, packageVersion: string, dependency: string, dependencyVersion: string) {
+
+  const jsonTemplateLoc = path.join(path.dirname(path.dirname(__dirname)), 'package.json');
+  let jsonContent = JSON.parse(fs.readFileSync(jsonTemplateLoc, 'utf8'));
+
+
+  var data = {
+    [name]: {
+      dependencies: {
+        [packageVersion]: {
+          [dependency]: dependencyVersion
+        }
+      }
+    }
+  }
+  if (jsonContent.data.hasOwnProperty(name)){
+    var result = {...jsonContent['data'][name]['dependencies'][packageVersion], ...data[name]['dependencies'][packageVersion]};
+    jsonContent['data'][name]['dependencies'][packageVersion] = result
+  } else {
+    var result = {...jsonContent['data'], ...data};
+    jsonContent.data = result
+  }
+
+  fs.writeFileSync(jsonTemplateLoc, JSON.stringify(jsonContent, null, 2) + '\n');
+}
+
+export function add(args: CreateArgs) {
+  const nOptions = Object.keys(args).length - 1;
+  if (nOptions < 4) return false;
+  if (nOptions && !Object.keys(args).slice(1).every(op => ['package', 'ver', 'dep', 'depver'].includes(op))) return false;
+
+  addPackageVersion(args.package, args.ver, args.dep, args.depver);
+  return true;
+}
+
+interface CreateArgs {
+  _: string[],
+  package: string,
+  ver: string,
+  dep: string,
+  depver: string,
+}
---
+++ packages/Meta/bin/commands/help.ts
@@ -0,0 +1,25 @@
+const HELP = `
+Usage: grok-meta <COMMAND>
+
+Datagrok's Meta package management tool
+
+Commands:
+    add       Add a package change to Meta package
+
+To get help on a particular command, use:
+    grok <COMMAND> --help
+`
+
+const HELP_ADD = `
+Usage: grok add --package <name> --ver <semver> --dep <dependency_name> --depver <semver>
+
+--package      Set package name to add/change in Meta package
+--ver          Set package version to add/change in Meta package
+--dep          Set package dependency to add/change in Meta package
+--depver       Set package dependency version to add to Meta package
+`;
+
+export const help = {
+  add: HELP_ADD,
+  help: HELP
+};
