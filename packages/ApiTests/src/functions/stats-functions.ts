9e66a32462252c9a145eae860c6d6a622abf339f
onuf <56073962+onuf@users.noreply.github.com>
2022-06-01 16:02:21 +0200

    GROK-10724 JS API Tests: Functions: tests for all built-in arithmetic/logical/date functions
    added statistical functions

--- packages/ApiTests/src/functions/stats-functions.ts
+++ packages/ApiTests/src/functions/stats-functions.ts
@@ -7,10 +7,18 @@ category('Statistical functions', () => {
   test('Avg', () => check({
     'Avg([1, 2, 3, 4])': 2.5,
     'Avg([null, 0.3, 0.7])': 0.5,
+    'Avg([5.123])': 5.123,
     'Avg([-1, -2, 3])': 0,
     'Avg([1, null, 2, 3])': 2,
     'Avg([])': undefined,
     'Avg([null, null])': DG.FLOAT_NULL,
+  }));
+
+  test('Kurt', () => check({
+    'Kurt([1, 2, 3])': -1.5,
+    'Kurt([1, 2, 6])': -1.5,
+    'Kurt([1, null, 2, 3])': -1.5,
+    'Kurt([null, null])': 0,
   }));
 
   test('Max', () => check({
@@ -21,4 +29,103 @@ category('Statistical functions', () => {
     'Max([null, null])': DG.FLOAT_NULL,
   }));
 
+  test('Med', () => check({
+    'Med([1, 2, 3])': 2,
+    'Med([1, 2, 3, 4])': 2.5,
+    'Med([1, null, 2, 3])': 2,
+    'Med([-100, null, 100])': 0,
+    'Med([null, 0.3, 0.7])': 0.5,
+    'Med([null, 1, 0.7, 0.3])': 0.7,
+    'Med([null, null])': 0,
+  }));
+
+  test('Min', () => check({
+    'Min([1.5, -2, 1.9])': -2,
+    'Min([1, 2, 4, 3])': 1,
+    'Min([2, null, 0, 3])': 0,
+    'Min([null, 1, 0.7, 0.3])': 0.3,
+    'Min([null, null])': DG.FLOAT_NULL,
+  }));
+
+  test('MissingValueCount', () => check({
+    'MissingValueCount([1, 2, 3])': 0,
+    'MissingValueCount([10, null, 7])': 1,
+    'MissingValueCount([2, null, 0, null, 3])': 2,
+    'MissingValueCount([null, null, null])': 3,
+  }));
+
+  test('Q1', () => check({
+    'Q1([-5, -3, -1, 0, 1, 3, 5])': -3,
+    'Q1([5, -5, 1, -1, 3, -3, 0])': -3,
+    'Q1([1, 2, 3])': 1,
+    'Q1([1, null, 2, 3])': 1,
+    'Q1([null, null])': 0,
+  }));
+
+  test('Q2', () => check({
+    'Q2([1, 2, 3])': 2,
+    'Q2([1, 2, 3, 4])': 2.5,
+    'Q2([1, null, 2, 3])': 2,
+    'Q2([-100, null, 100])': 0,
+    'Q2([null, 0.3, 0.7])': 0.5,
+    'Q2([null, 1, 0.7, 0.3])': 0.7,
+    'Q2([null, null])': 0,
+  }));
+
+  test('Q3', () => check({
+    'Q3([-5, -3, -1, 0, 1, 3, 5])': 3,
+    'Q3([5, -5, 1, -1, 3, -3, 0])': 3,
+    'Q3([1, 2, 3])': 3,
+    'Q3([1, null, 2, 3])': 3,
+    'Q3([null, null])': 0,
+  }));
+
+  test('Skew', () => check({
+    'Skew([1, 2, 3])': 0,
+    'Skew([1, 2, 6])': 0.595,
+    'Skew([1, null, 2, 3])': 0,
+    'Skew([null, null])': 0,
+  }));
+
+  test('StDev', () => check({
+    'StDev([1, 2, 3])': 1,
+    'StDev([1, null, 2, 3])': 1,
+    'StDev([null, null])': DG.FLOAT_NULL,
+    'StDev([7, 14, 21])': 7,
+    'StDev([-15, -5, 5, 15])': 12.91,
+  }));
+
+  test('Sum', () => check({
+    'Sum([1, 2, 4, 3])': 10,
+    'Sum([-1, 4, 12, 5])': 20,
+    'Sum([2, null, 0, 3])': 5,
+    'Sum([null, 1, 0.7, 0.3])': 2,
+    'Sum([null, null])': DG.FLOAT_NULL,
+    'Sum([-0])': 0,
+  }));
+
+  test('TotalCount', () => check({
+    'TotalCount([2, null, 0, 3])': 4,
+    'TotalCount([1, 2, 4])': 3,
+    'TotalCount([null, null])': 2,
+    'TotalCount([100])': 1,
+    'TotalCount([])': undefined,
+  }));
+
+  test('ValueCount', () => check({
+    'ValueCount([1, 2, 4, 3])': 4,
+    'ValueCount([2, null, 0, 3])': 3,
+    'ValueCount([1, 2, 4])': 3,
+    'ValueCount([null, null])': 0,
+    'ValueCount([100])': 1,
+    'ValueCount([])': undefined,
+  }));
+
+  test('Variance', () => check({
+    'Variance([1, 2, 3])': 1,
+    'Variance([1, null, 2, 3])': 1,
+    'Variance([null, null])': DG.FLOAT_NULL,
+    'Variance([7, 14, 21])': 49,
+    'Variance([-15, -5, 5, 15])': 166.667,
+  }));
 });
