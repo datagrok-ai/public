611d38076a9b183b84d5e34aadab1bb7a6aed28a
gav(Denis_Kryvchuk) <dendenysyk@gmail.com>
2022-06-02 01:09:26 +0300

    new tests fro sss

--- packages/Chem/src/tests/chem-tests.ts
+++ packages/Chem/src/tests/chem-tests.ts
@@ -15,6 +15,14 @@ category('chem', () => {
       _testSearchSubstructureSARSmall);
   });
 
+  test('searchSustructureColWithoutDf', async () => {
+    const col = DG.Column.fromStrings('smiles', [
+      'CCOC(=O)c1oc2cccc(OCCNCc3cccnc3)c2c1C4CC4',
+      'Fc1cc2C(=O)C(=CN(C3CC3)c2cc1N4CCNCC4)c5oc(COc6ccccc6)nn5',
+    ]);
+    await grok.chem.searchSubstructure(col, 'C1CC1');
+  });
+
   //Number of molecules is smaller than a number of threads
   test('searchSustructure.5_rows', async () => {
     const targetSmiles = [
@@ -30,11 +38,83 @@ ${targetSmiles[1]}
 CC(C(=O)NCCS)c1cccc(c1)C(=O)c2ccccc2
 COc1ccc2c(c1)c(CC(=O)N3CCCC3C(=O)Oc4ccc(C)cc4OC)c(C)n2C(=O)c5ccc(Cl)cc5
 `;
-    await _testSearchSubstructureAllParameters(
-      async (params: any) => await _testSearchSubstructure(
-        csv, substructureSmiles, [0, 2], params,
-      ),
-    );
+    const df = DG.DataFrame.fromCsv(csv);
+    await _testSearchSubstructure(df, 'smiles', substructureSmiles, [0, 2]);
+  });
+
+  test('searchSustructureWithMalformedMolString', async () => {
+    const targetSmiles = [
+      'CCOC(=O)c1oc2cccc(OCCNCc3cccnc3)c2c1C4CC4',
+      'Fc1cc2C(=O)C(=CN(C3CC3)c2cc1N4CCNCC4)c5oc(COc6ccccc6)nn5',
+    ];
+    const substructureSmiles = 'C1CC1';
+    const csv =
+      `smiles
+${targetSmiles[0]}
+COc1ccc2cc(ccc2c1)C(C)C(=O)Oc3ccc(C)cc3OC
+${targetSmiles[1]}
+CC(C(=O)NCCS)c1cccc(c1)C(=O)c2ccccc2
+qq
+COc1ccc2c(c1)c(CC(=O)N3CCCC3C(=O)Oc4ccc(C)cc4OC)c(C)n2C(=O)c5ccc(Cl)cc5
+`;
+    const df = DG.DataFrame.fromCsv(csv);
+    await _testSearchSubstructure(df, 'smiles', substructureSmiles, [0, 2]);
+  });
+
+  test('searchSustructurePerformance', async () => {
+    const df = grok.data.demo.molecules(30000);
+
+    const startTime = performance.now();
+    await grok.chem.searchSubstructure(df.columns.byName('smiles'), 'c1ccccc1')!;
+    const midTime1 = performance.now();
+    for (let i = 0; i < 20; i++)
+      await grok.chem.searchSubstructure(df.columns.byName('smiles'), 'c1ccccc1')!;
+    const midTime2 = performance.now();
+    await grok.chem.searchSubstructure(df.columns.byName('smiles'), 'C1CC1')!;
+    const endTime = performance.now();
+
+    console.log(`first Call to WithLibrary took ${midTime1 - startTime} milliseconds`);
+    console.log(`loop Call to WithLibrary took ${midTime2 - midTime1} milliseconds`);
+    console.log(`last Call to WithLibrary took ${endTime - midTime2} milliseconds`);
+  });
+
+  test('searchSustructureAfterChanges', async () => {
+    const targetSmiles = [
+      'CCOC(=O)c1oc2cccc(OCCNCc3cccnc3)c2c1C4CC4',
+      'Fc1cc2C(=O)C(=CN(C3CC3)c2cc1N4CCNCC4)c5oc(COc6ccccc6)nn5',
+    ];
+    const substructureSmiles = 'C1CC1';
+    const csv =
+      `smiles
+${targetSmiles[0]}
+COc1ccc2cc(ccc2c1)C(C)C(=O)Oc3ccc(C)cc3OC
+${targetSmiles[1]}
+CC(C(=O)NCCS)c1cccc(c1)C(=O)c2ccccc2
+COc1ccc2c(c1)c(CC(=O)N3CCCC3C(=O)Oc4ccc(C)cc4OC)c(C)n2C(=O)c5ccc(Cl)cc5
+`;
+    const df = DG.DataFrame.fromCsv(csv);
+    await _testSearchSubstructure(df, 'smiles', substructureSmiles, [0, 2]);
+    df.columns.byName('smiles').set(0, 'COc1ccc2cc(ccc2c1)C(C)C(=O)Oc3ccc(C)cc3OC');
+    await _testSearchSubstructure(df, 'smiles', substructureSmiles, [2]);
+  });
+
+  test('searchSustructureWith2Columns', async () => {
+    const targetSmiles = [
+      'CCOC(=O)c1oc2cccc(OCCNCc3cccnc3)c2c1C4CC4',
+      'Fc1cc2C(=O)C(=CN(C3CC3)c2cc1N4CCNCC4)c5oc(COc6ccccc6)nn5',
+    ];
+    const substructureSmiles = 'C1CC1';
+    const csv =
+      `smiles, smiles1
+${targetSmiles[0]}, COc1ccc2cc(ccc2c1)C(C)C(=O)Oc3ccc(C)cc3OC
+COc1ccc2cc(ccc2c1)C(C)C(=O)Oc3ccc(C)cc3OC, ${targetSmiles[0]}
+${targetSmiles[1]}, COc1ccc2c(c1)c(CC(=O)N3CCCC3C(=O)Oc4ccc(C)cc4OC)c(C)n2C(=O)c5ccc(Cl)cc5
+CC(C(=O)NCCS)c1cccc(c1)C(=O)c2ccccc2, COc1ccc2c(c1)c(CC(=O)N3CCCC3C(=O)Oc4ccc(C)cc4OC)c(C)n2C(=O)c5ccc(Cl)cc5
+COc1ccc2c(c1)c(CC(=O)N3CCCC3C(=O)Oc4ccc(C)cc4OC)c(C)n2C(=O)c5ccc(Cl)cc5, ${targetSmiles[1]}
+`;
+    const df = DG.DataFrame.fromCsv(csv);
+    await _testSearchSubstructure(df, 'smiles', substructureSmiles, [0, 2]);
+    await _testSearchSubstructure(df, 'smiles1', substructureSmiles, [1, 4]);
   });
 
   test('findSimilar.sar-small', async () => {
