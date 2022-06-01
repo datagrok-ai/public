7bf22afdce708a28ac4e1c330ca3782f932f2c15
kdoncov <kdoncov@datagrok.ai>
2022-06-01 17:50:14 +0300

    Usage analysis: fixed NewUsersSummaryByDate query

--- packages/UsageAnalysis/queries/new_users_summary_by_date.sql
+++ packages/UsageAnalysis/queries/new_users_summary_by_date.sql
@@ -1,11 +1,14 @@
 --name: NewUsersSummaryByDate
 --input: string days
 --connection: System:Datagrok
-SELECT
-    DATE(joined) as day, 
-	COUNT(*)
-FROM users
-WHERE DATE(joined) BETWEEN (current_timestamp -  (CONCAT(@days, ' day'))::interval)
-AND (current_timestamp - interval '0 day')
-GROUP BY DATE(joined)
-ORDER BY day;
+select t.date::date, count(t.id) as user_count from (
+	select distinct on (date(e.event_time), u.id) u.id, e.event_time as date
+	from events e
+	inner join users_sessions s on e.session_id = s.id
+	inner join users u on u.id = s.user_id
+	WHERE DATE(joined) BETWEEN (current_timestamp - (CONCAT(@days, ' day'))::interval)
+    AND (current_timestamp - interval '0 day')
+) t
+group by t.date::date
+order by t.date::date;
+--end
