56cbcf801c144ce5103f925752f2b695833ae913
Oleksandra Serhiienko <59337516+Aleksashka11@users.noreply.github.com>
2022-06-04 14:10:30 +0300

    Adding unit tests for parquet file format

--- packages/Arrow/src/tests/arrow-tests.ts
+++ packages/Arrow/src/tests/arrow-tests.ts
@@ -6,14 +6,19 @@ import { default as init, readParquet, writeParquet, WriterPropertiesBuilder, Co
 
 
 category('Arrow', () => {
-  let bytes;
+  let bytes_arrow;
   let arrow;
-  let table;
+  let table_arrow;
+  let bytes_parquet;
+  let table_parquet;
 
   before(async () => {
-    bytes = await _package.files.readAsBytes('titanic.feather');
-    arrow = Buffer.from(bytes);
-    table = tableFromIPC(arrow);
+    bytes_arrow = await _package.files.readAsBytes('titanic.feather');
+    arrow = Buffer.from(bytes_arrow);
+    table_arrow = tableFromIPC(arrow);
+    await init(_package.webRoot + 'dist/arrow1_bg.wasm');
+    bytes_parquet = await _package.files.readAsBytes('titanic.parquet');
+    table_parquet = tableFromIPC(readParquet(bytes_parquet));
   });
 
   test('feather file column names', async () => {
@@ -21,18 +26,36 @@ category('Arrow', () => {
                             'sibsp', 'parch', 'ticket', 'fare', 'cabin',
                             'embarked', 'boat', 'body', 'home.dest'
                           ];
-    expect(table.schema.fields.map((table) => table.name).toString(), expectedTables.toString());
+    expect(table_arrow.schema.fields.map((table_arrow) => table_arrow.name).toString(), expectedTables.toString());
   });
 
   test('feather file column types', async () => {
-    expect(table.getChild('pclass').type.toString(), 'Int32');
-    expect(table.getChild('name').type.toString(), 'Dictionary<Int32, Utf8>');
-    expect(table.getChild('age').type.toString(), 'Float64');
+    expect(table_arrow.getChild('pclass').type.toString(), 'Int32');
+    expect(table_arrow.getChild('name').type.toString(), 'Dictionary<Int32, Utf8>');
+    expect(table_arrow.getChild('age').type.toString(), 'Float64');
   });
 
   test('feather file number of rows and columns', async () => {
-    expect(table.numCols, 14);
-    expect(table.numRows, 1310);
+    expect(table_arrow.numCols, 14);
+    expect(table_arrow.numRows, 1310);
+  });
+  
+  test('parquet file column names', async () =>{
+    const expectedTables = ['pclass', 'survived', 'name', 'sex', 'age', 
+                            'sibsp', 'parch', 'ticket', 'fare', 'cabin',
+                            'embarked', 'boat', 'body', 'home.dest'
+                          ];
+    expect(table_parquet.schema.fields.map((table_parquet) => table_parquet.name).toString(), expectedTables.toString());
   });
 
+  test('parquet file column types', async () => {
+    expect(table_parquet.getChild('pclass').type.toString(), 'Int32');
+    expect(table_parquet.getChild('name').type.toString(), 'Dictionary<Int32, Utf8>');
+    expect(table_parquet.getChild('age').type.toString(), 'Float64');
+  });
+
+  test('parquet file number of rows and columns', async () => {
+    expect(table_parquet.numCols, 14);
+    expect(table_parquet.numRows, 1310);
+  });
 });
