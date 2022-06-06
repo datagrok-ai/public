5eeeffbd774133bc2967d9ce7d8d7182dbfc5042
dpetrov <dpetrov@gnf.org>
2022-06-05 20:52:24 -0700

    Addressed color issues in pinned columns

---
+++ libraries/gridext/src/utils/ColorUtils.ts
@@ -0,0 +1,33 @@
+export class ColorUtils {
+
+  static a(c: number): number {
+    return (c >> 24) & 0xFF;
+  }
+
+  /** Returns the Red component of the color represented as ARGB-formatted integer. */
+  static r(c: number): number {
+    return (c >> 16) & 0xFF;
+  }
+
+  /** Returns the Green component of the color represented as ARGB-formatted integer. */
+  static g(c: number): number {
+    return (c >> 8) & 0xFF;
+  }
+
+  /** Returns the Blue component of the color represented as ARGB-formatted integer. */
+  static b(c: number): number {
+    return c & 0xFF;
+  }
+
+  static toRgb(color: number): string {
+    return color === null ? '' : `rgb(${ColorUtils.r(color)},${ColorUtils.g(color)},${ColorUtils.b(color)})`;
+  }
+
+  static get currentRow(): number {
+    return 0xFF38B738;
+  }
+
+  static get colSelection(): number {
+    return 0x60dcdca0;
+  }
+}
