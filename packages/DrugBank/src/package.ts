18c72716e9ba1f81abe00c4f6ff36b5d0639be0d
Oleksandra Serhiienko <59337516+Aleksashka11@users.noreply.github.com>
2022-06-06 17:07:56 +0300

    Fixing issues with drugNameMolecule function

--- packages/DrugBank/src/package.ts
+++ packages/DrugBank/src/package.ts
@@ -35,11 +35,11 @@ export async function drugBankSimilaritySearchPanel(mol: string) {
 //connection: DrugBank
 //input: string id
 //output: string smiles { semType: Molecule }
-export async function drugNameMolecule(id:string){
-  const drug_name = id.slice(3);
-  for (var i = 0; i < dbdf.rowCount; i++){
-    if (dbdf.get('SYNONYMS', i).toLowerCase().includes(drug_name)){
-      return  dbdf.get('Smiles', i);
+export async function drugNameMolecule(id: string) {
+  const drugName = id.slice(3);
+  for (var i = 0; i < dbdf.rowCount; i++) {
+    if (dbdf.get('SYNONYMS', i).toLowerCase().includes(drugName)) {
+      return dbdf.get('Smiles', i);
     }
   }
 }
