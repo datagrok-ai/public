c07eb5eb9f92de7428014acaf2ede88fd44ce063
onuf <56073962+onuf@users.noreply.github.com>
2022-06-16 11:23:08 +0200

    ApiTests: patch version bump
    - publish a new version with the datagrok-api dependency ^1.0.0 to npm
    - uncomment parts of the 'Time' func test (the fix arrives with DG 1.3.6)

--- packages/ApiTests/src/functions/date-functions.ts
+++ packages/ApiTests/src/functions/date-functions.ts
@@ -99,11 +99,12 @@ category('Date functions', () => {
   }));
 
   test('Time', () => check({
+    // The function returns a datetime with a special insignificant date `01-01-01`.
     // 2-digit year is replaced in expected dates with 4-digit format to address offset inconsistency
     'Time(23, 59, 45, 999)': dayjs(new Date(2001, 0, 1, 23, 59, 45, 999)),
     'Time(10, 35, 15, 500)': dayjs(new Date(2001, 0, 1, 10, 35, 15, 500)),
-    // 'Time(5, 5, 5, 5)': dayjs(new Date(2001, 0, 1, 5, 5, 5, 5)),
-    // 'Time(0, 0, 0, 0)': dayjs(new Date(2001, 0, 1)),
+    'Time(5, 5, 5, 5)': dayjs(new Date(2001, 0, 1, 5, 5, 5, 5)),
+    'Time(0, 0, 0, 0)': dayjs(new Date(2001, 0, 1)),
   }));
 
   test('Today', () => check({
