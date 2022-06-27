41ffaf2166f856c8f105a4aee32672926ba9cea9
Oleksandra Serhiienko <59337516+Aleksashka11@users.noreply.github.com>
2022-06-27 12:14:07 +0300

    Helm: Making changes in package-test file

--- packages/Helm/src/package-test.ts
+++ packages/Helm/src/package-test.ts
@@ -1,13 +1,18 @@
 import * as DG from "datagrok-api/dg";
 import * as grok from "datagrok-api/grok";
 import {runTests, tests} from '@datagrok-libraries/utils/src/test';
+import './tests/helm-tests.ts'
 
-export let _package = new DG.Package();
-export {tests}
+export const _package = new DG.Package();
+export {tests};
+
 
 //name: test
+//input: string category {optional: true}
+//input: string test {optional: true}
 //output: dataframe result
-export async function test(): Promise<DG.DataFrame> {
-  let data = await runTests();
+//top-menu: Tools | Dev | JS API Tests
+export async function test(category: string, test: string): Promise<DG.DataFrame> {
+  const data = await runTests({category, test});
   return DG.DataFrame.fromObjects(data)!;
 }
