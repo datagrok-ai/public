188cfcb39f1ab1cf0b8f56f6a8b09e82d5b1863b
skalkin <skalkin@gmail.com>
2022-06-27 08:36:01 -0400

    GROK-10899: Status Bar - WIP

--- packages/ApiSamples/scripts/functions/progress.js
+++ packages/ApiSamples/scripts/functions/progress.js
@@ -5,6 +5,8 @@
 let p = DG.ProgressIndicator.create();
 p.onProgressUpdated.subscribe((p) => grok.shell.info(p.percent));
 p.onLogUpdated.subscribe((line) => console.log(line));
+p.update(50, "Loading foo.csv");
+
 
 grok.functions.call("Discovery:countToTen",
   {s: "a"}, true, p)
