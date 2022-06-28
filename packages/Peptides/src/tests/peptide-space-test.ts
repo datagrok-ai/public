99177a7ac3b871090ce3471918611ee386e855f8
Volodymyr Dyma <emissaryprog@gmail.com>
2022-06-28 22:12:47 +0300

    Peptide space tests fix and performance benchmark

--- packages/Peptides/src/tests/peptide-space-test.ts
+++ packages/Peptides/src/tests/peptide-space-test.ts
@@ -1,4 +1,4 @@
-import {/*before, after, */category, test} from '@datagrok-libraries/utils/src/test';
+import {/*before, after, */after, category, test} from '@datagrok-libraries/utils/src/test';
 import {
   _testViewerIsDrawing,
   _testDimensionalityReducer,
@@ -12,66 +12,67 @@ import {aligned1} from './test-data';
 import * as DG from 'datagrok-api/dg';
 import * as grok from 'datagrok-api/grok';
 import {StringMetrics} from '@datagrok-libraries/ml/src/typed-metrics';
+import { computeWeights } from '../viewers/peptide-space-viewer';
 
 export const _package = new DG.Package();
 
 let table: DG.DataFrame;
 let view: DG.TableView;
 
-//const table = await grok.data.loadTable(`${_package.webRoot}files/aligned.csv`);
-//'/home/www/data/dev/packages/data/peptides/aligned.csv');
-//console.log(table);
-//const table = await grok.data.files.openTable('Demo:Files/bio/peptides.csv');
-
-category('peptides', async () => {
-  /*before(async () => {
-    console.log(['before']);
-    // const text = await _package.files.readAsText('aligned.csv');
-    // console.log([text]);
-    // table = DG.DataFrame.fromCsv(text);
-
-    // const path = `${_package.webRoot}files/aligned.csv`;
-    // console.log([path]);
-    // table = await grok.data.loadTable(path);
-    // console.log([table]);
-
-    table = await grok.data.files.openTable('Demo:Files/bio/peptides.csv');
-    view = grok.shell.addTableView(table);
-  });*/
-
-  //table = await grok.data.files.openTable('Demo:Files/bio/peptides.csv');
+category('Peptide space', async () => {
   table = DG.DataFrame.fromCsv(aligned1);
-  view = grok.shell.addTableView(table);
-
-  test('peptide_space.test_table.is_not_empty', async () => {
+  
+  test('test_table.is_not_empty', async () => {
     _testTableIsNotEmpty(table);
   });
 
-  test('peptide_space.PeptideSimilaritySpaceWidget.is_drawing', async () => {
+  test('PeptideSimilaritySpaceWidget.is_drawing', async () => {
+    view = grok.shell.addTableView(table);
     await _testViewerIsDrawing(table, view);
   });
 
   const alignedSequencesColumn = table.getCol('AlignedSequence');
-  const columnData = cleanAlignedSequencesColumn(alignedSequencesColumn);
-
-  for (const method of DimensionalityReducer.availableMethods) {
-    for (const measure of DimensionalityReducer.availableMetricsByType('String')) {
-      test(`peptide_space.DimensinalityReducer.${method}.${measure}.is_numeric`, async () => {
-        await _testDimensionalityReducer(columnData, method as StringMetrics, measure);
-      });
+  
+  test('test_deminsionality_reducer', async () => {
+    const columnData = cleanAlignedSequencesColumn(alignedSequencesColumn);
+    for (const method of DimensionalityReducer.availableMethods) {
+      for (const measure of DimensionalityReducer.availableMetricsByType('String')) {
+        test(`peptide_space.DimensinalityReducer.${method}.${measure}.is_numeric`, async () => {
+          await _testDimensionalityReducer(columnData, method as StringMetrics, measure);
+        });
+      }
     }
-  }
+  })
 
-  for (const method of DimensionalityReducer.availableMethods) {
-    for (const measure of DimensionalityReducer.availableMetricsByType('String')) {
-      test(`peptide_space.PeptideSimilaritySpaceViewer.${method}.${measure}.is_proper`, async () => {
-        await _testPeptideSimilaritySpaceViewer(table, alignedSequencesColumn, method, measure, 100);//, view);
-      });
+  test('test_peptide_similarity_space_viewer', async () => {
+    for (const method of DimensionalityReducer.availableMethods) {
+      for (const measure of DimensionalityReducer.availableMetricsByType('String')) {
+        test(`peptide_space.PeptideSimilaritySpaceViewer.${method}.${measure}.is_proper`, async () => {
+          await _testPeptideSimilaritySpaceViewer(table, alignedSequencesColumn, method, measure, 100);//, view);
+        });
+      }
     }
-  }
+  });
 
-  /*after(async () => {
-    view.close();
-    grok.shell.closeTable(table!);
-  });*/
+  test('test_compute_weights_performance', async () => {
+    const table = DG.DataFrame.fromCsv(await _package.files.readAsText('steven_temp.csv'));
+    for (const slice of [1, 2, 3]) {
+      const bitset = DG.BitSet.create(table.rowCount, (i) => i < slice * 1000);
+      const table_slice = table.clone(bitset);
+      const col = table_slice.getCol('sequence');
+      for (const method of DimensionalityReducer.availableMethods) {
+        for (const measure of DimensionalityReducer.availableMetricsByType('String')) {
+          const start = new Date();
+          await computeWeights(table_slice, method, measure, 100, col);
+          const stop = new Date();
+          console.warn(`Table slice: ${slice}k; method: ${method}; measure: ${measure}; ` + 
+            `Resulted in ${stop.getMilliseconds() - start.getMilliseconds()} ms`);
+        }
+      }
+    }
+  });
+
+  after(async () => {
+    view?.close();
+  });
 });
