d78f5dfef3a3ffaec1a73c685b14c71b9763ca23
simpleprofi <simpleprofi@gmail.com>
2022-07-01 09:28:10 +0300

    Added other conversion tests except molV3

--- packages/Chem/src/tests/ketcher_tests.ts
+++ packages/Chem/src/tests/ketcher_tests.ts
@@ -1,13 +1,13 @@
 import {category, expect, expectFloat, test} from '@datagrok-libraries/utils/src/test';
 import {Func} from "datagrok-api/src/entities";
 import * as grok from 'datagrok-api/grok';
-
+const exampleSmiles = 'CC(C(=O)OCCCc1cccnc1)c2cccc(c2)C(=O)c3ccccc3';
 
 category('sketcher testing', () => {
     test('smiles-to-mol', async () => {
         const funcs = Func.find({tags: ['moleculeSketcher']});
         for (let f of funcs) {
-            const smilesString = 'CC(C(=O)OCCCc1cccnc1)c2cccc(c2)C(=O)c3ccccc3'
+            const smilesString = exampleSmiles;
             // @ts-ignore
             const sketcher = await f!.apply();
             sketcher.smiles = smilesString;
@@ -19,7 +19,40 @@ category('sketcher testing', () => {
     });
 
     test('mol-to-smiles', async () => {
-     
+        const funcs = Func.find({tags: ['moleculeSketcher']});
+        for (let f of funcs) {
+            const smilesString = exampleSmiles;
+            // @ts-ignore
+            const sketcher = await f!.apply();
+            const startMol = grok.chem.convert(smilesString, 'smiles', 'mol');
+            sketcher.mol = startMol;
+            const resultSmiles = sketcher.smiles;
+            expect(resultSmiles, smilesString);
+        }
+    });
+    test('mol-to-smarts', async () => {
+        const funcs = Func.find({tags: ['moleculeSketcher']});
+        for (let f of funcs) {
+            const smilesString = exampleSmiles;
+            // @ts-ignore
+            const sketcher = await f!.apply();
+            const startMol = grok.chem.convert(smilesString, 'smiles', 'mol');
+            sketcher.mol = startMol;
+            const resultSmarts = await sketcher.getSmarts();
+            expect(resultSmarts != null, true);
+        }
+    });
+    test('smiles-to-smarts', async () => {
+        const funcs = Func.find({tags: ['moleculeSketcher']});
+        for (let f of funcs) {
+            const smilesString = exampleSmiles;
+            // @ts-ignore
+            const sketcher = await f!.apply();
+            sketcher.smiles = smilesString;
+            const resultSmarts = await sketcher.getSmarts();
+            expect(resultSmarts != null, true);
+        }
+
     });
 
 });
