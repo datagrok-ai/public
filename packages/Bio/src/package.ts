66f3c9b1ea6f1ddf5dae976c0e8d72140ac20785
Leonid <stolbovla@yandex.ru>
2022-07-04 15:14:04 +0300

    Bio: updates for top-menu

--- packages/Bio/src/package.ts
+++ packages/Bio/src/package.ts
@@ -9,6 +9,7 @@ export const _package = new DG.Package();
 import {WebLogo} from '@datagrok-libraries/bio/src/viewers/web-logo';
 import {VdRegionsViewer} from './viewers/vd-regions-viewer';
 import {runKalign, testMSAEnoughMemory} from './utils/multiple-sequence-alignment';
+import { TableView } from 'datagrok-api/dg';
 
 //name: sequenceAlignment
 //input: string alignType {choices: ['Local alignment', 'Global alignment']}
@@ -44,7 +45,7 @@ export function vdRegionViewer() {
 //name: Activity Cliffs
 //description: detect activity cliffs
 //input: dataframe df [Input data table]
-//input: column smiles {type:categorical; semType: MacroMolecule}
+//input: column smiles {type:categorical; semType: Macromolecule}
 //input: column activities
 //input: double similarity = 80 [Similarity cutoff]
 //input: string methodName { choices:["UMAP", "t-SNE", "SPE"] }
@@ -55,7 +56,7 @@ export async function activityCliffs(df: DG.DataFrame, smiles: DG.Column, activi
 //top-menu: Bio | Sequence Space...
 //name: Sequence Space
 //input: dataframe table
-//input: column smiles { semType: MacroMolecule }
+//input: column smiles { semType: Macromolecule }
 //input: string methodName { choices:["UMAP", "t-SNE", "SPE", "pSPE", "OriginalSPE"] }
 //input: string similarityMetric { choices:["Tanimoto", "Asymmetric", "Cosine", "Sokal"] }
 //input: bool plotEmbeddings = true
@@ -66,8 +67,30 @@ export async function chemSpaceTopMenu(table: DG.DataFrame, smiles: DG.Column, m
 //top-menu: Bio | MSA...
 //name: MSA
 //input: dataframe table
-//input: column sequence { semType: MacroMolecule }
+//input: column sequence { semType: Macromolecule }
 export async function multipleSequenceAlignmentAny(table: DG.DataFrame, col: DG.Column): Promise<void> {
   const msaCol = await runKalign(col, false);
   table.columns.add(msaCol);
+}
+
+//name: Composition Analysis
+//top-menu: Bio | Composition Analysis
+//output: viewer result
+export async function compositionAnalysis(): Promise<void> {
+  const col = grok.shell.t.columns.bySemType("Macromolecule");//DG.SEMTYPE.MACROMOLECULE);
+  if (col === null) {
+    grok.shell.error('Current table does not contain sequences');
+    return;
+  }
+
+  const wl = await col.dataFrame.plot.fromType("WebLogo", {});
+
+  for (let v of grok.shell.views) {
+    if (v instanceof TableView && (v as  DG.TableView).dataFrame.name === col.dataFrame.name){
+      (v as DG.TableView).dockManager.dock(wl.root, "down");
+      break;
+    }
+  }
+
+
 }
