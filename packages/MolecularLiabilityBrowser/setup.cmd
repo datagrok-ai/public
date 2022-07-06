a54236285e2d765c287186d9a02f0a2d1314033a
tanas80 <atanas@datagrok.ai>
2022-07-06 15:39:18 +0400

    MLB: Bump version to use new bio/Bio

--- packages/MolecularLiabilityBrowser/setup.cmd
+++ packages/MolecularLiabilityBrowser/setup.cmd
@@ -2,55 +2,34 @@ set package_dir=%cd%
 
 call setup-unlink-clean.cmd
 
-cd %package_dir%/../../js-api
-call npm install
-call npm link
+cd "%package_dir%/../../js-api/"                                 & call npm install
+cd "%package_dir%/../../libraries/utils/"                        & call npm install
+cd "%package_dir%/../../libraries/ml/"                           & call npm install
+cd "%package_dir%/../../libraries/bio/"                          & call npm install
+cd "%package_dir%/../../packages/Bio/"                           & call npm install
+cd "%package_dir%/../../packages/MolecularLiabilityBrowserData"  & call npm install
+cd "%package_dir%/"                                              & call npm install
 
-cd %package_dir%/../../libraries/utils
-call npm install
-call npm run link-all
-call npm link
+cd "%package_dir%/../../js-api/"                                 & call npm link
+cd "%package_dir%/../../libraries/utils/"                        & call npm link
+cd "%package_dir%/../../libraries/ml/"                           & call npm link
+cd "%package_dir%/../../libraries/bio/"                          & call npm link
+cd "%package_dir%/../../packages/Bio/"                           & call npm link
+cd "%package_dir%/../../packages/MolecularLiabilityBrowserData"  & call npm link
+cd "%package_dir%/"                                              & call npm link
 
-cd %package_dir%/../../libraries/bio
-call npm install
-call npm run link-all
-call npm link
+cd "%package_dir%/../../js-api/"                                 & call npm run link-all
+cd "%package_dir%/../../libraries/utils/"                        & call npm run link-all
+cd "%package_dir%/../../libraries/ml/"                           & call npm run link-all
+cd "%package_dir%/../../libraries/bio/"                          & call npm run link-all
+cd "%package_dir%/../../packages/Bio/"                           & call npm run link-all
+cd "%package_dir%/../../packages/MolecularLiabilityBrowserData"  & call npm run link-all
+cd "%package_dir%/"                                              & call npm run link-all
 
-cd %package_dir%/../../packages/Bio
-call npm install
-call npm run link-all
-call npm link
-
-cd %package_dir%/../../packages/MolecularLiabilityBrowserData
-call npm install
-call npm run link-all
-call npm link
-
-cd %package_dir%
-call npm install
-call npm run link-all
-
-
-echo Build js-api ...
-cd %package_dir%/../../js-api
-call npm run build
-
-echo Build  utils ...
-cd %package_dir%/../../libraries/utils
-call npm run build
-
-echo Build bio ...
-cd %package_dir%/../../libraries/bio
-call npm run build
-
-echo Build Bio ...
-cd %package_dir%/../../packages/Bio
-call npm run build
-
-echo Build MLB-Data ...
-cd %package_dir%/../../packages/MolecularLiabilityBrowserData
-call npm run build
-
-echo Build MLB ...
-cd %package_dir%
-call npm run build
+cd "%package_dir%/../../js-api/"                                 & call npm run build
+cd "%package_dir%/../../libraries/utils/"                        & call npm run build
+cd "%package_dir%/../../libraries/ml/"                           & call npm run build
+cd "%package_dir%/../../libraries/bio/"                          & call npm run build
+cd "%package_dir%/../../packages/Bio/"                           & call npm run build
+cd "%package_dir%/../../packages/MolecularLiabilityBrowserData"  & call npm run build
+cd "%package_dir%/"                                              & call npm run build
