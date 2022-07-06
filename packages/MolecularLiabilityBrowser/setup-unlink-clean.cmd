598b13d2a9bebdd49d486a60b1f695e8ce2de0ff
tanas80 <atanas@datagrok.ai>
2022-07-06 15:39:18 +0400

    MLB: Bump version to use new bio/Bio

--- packages/MolecularLiabilityBrowser/setup-unlink-clean.cmd
+++ packages/MolecularLiabilityBrowser/setup-unlink-clean.cmd
@@ -4,6 +4,7 @@ call npm uninstall -g datagrok-api @datagrok-libraries/utils @datagrok-libraries
 
 cd "%package_dir%/../../js-api/"                                 & rmdir /s /q "node_modules"
 cd "%package_dir%/../../libraries/utils/"                        & rmdir /s /q "node_modules"
+cd "%package_dir%/../../libraries/ml/"                           & rmdir /s /q "node_modules"
 cd "%package_dir%/../../libraries/bio/"                          & rmdir /s /q "node_modules"
 cd "%package_dir%/../../packages/Bio/"                           & rmdir /s /q "node_modules"
 cd "%package_dir%/../../packages/MolecularLiabilityBrowserData"  & rmdir /s /q "node_modules"
@@ -11,6 +12,7 @@ cd "%package_dir%/"                                              & rmdir /s /q "
 
 rem cd "%package_dir%/../../js-api/"                                 & del "package-lock.json"
 rem cd "%package_dir%/../../libraries/utils/"                        & del "package-lock.json"
+rem cd "%package_dir%/../../libraries/ml/"                           & del "package-lock.json"
 rem cd "%package_dir%/../../libraries/bio/"                          & del "package-lock.json"
 rem cd "%package_dir%/../../packages/Bio/"                           & del "package-lock.json"
 rem cd "%package_dir%/../../packages/MolecularLiabilityBrowserData"  & del "package-lock.json"
