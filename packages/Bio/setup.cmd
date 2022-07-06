8383455236619e63a4b0dc9431d4541c634bb091
Leonid <stolbovla@yandex.ru>
2022-07-06 15:39:03 +0300

    Bio: setup

--- packages/Bio/setup.cmd
+++ packages/Bio/setup.cmd
@@ -1,10 +1,19 @@
 cd ../../js-api
 call npm install
 call npm link
+cd ../libraries/utils
+call npm install
+call npm link
+call npm link datagrok-api
+cd ../libraries/ml
+call npm install
+call npm link
+call npm link @datagrok-libraries/utils
 cd ../libraries/bio
 call npm install
 call npm link
+call npm link @datagrok-libraries/utils
 cd ../../packages/Bio
 call npm install
-call npm link datagrok-api @datagrok-libraries/bio
+call npm link datagrok-api @datagrok-libraries/bio @datagrok-libraries/utils @datagrok-libraries/ml
 webpack
