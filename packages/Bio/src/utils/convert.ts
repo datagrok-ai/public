c8ebd861a7323676d714369ea76d8b45bc69253d
Alexey <achopovsky@datagrok.ai>
2022-07-07 08:40:46 +0300

    Bio: notations converter WIP

--- packages/Bio/src/utils/convert.ts
+++ packages/Bio/src/utils/convert.ts
@@ -1,11 +1,27 @@
 import * as DG from 'datagrok-api/dg';
 import * as ui from 'datagrok-api/ui';
+import {NotationConverter} from './notation-converter';
 
+/**
+ * Converts notations of a Macromolecule column
+ *
+ * @param {DG.column} col Column with 'Macromolecule' semantic type
+ */
 export function convert(col: DG.Column): void {
   const current = col.tags[DG.TAGS.UNITS];
   //TODO: read all notations
-  const notations = ['fasta:SEQ:NT', 'fasta:SEQ:PT', 'fasta:SEQ.MSA:NT', 'fasta:SEQ.MSA:PT', 'HELM'];
-  const choices = ui.choiceInput('convert to', '', notations.filter((e) => e !== current));
+  const units = [
+    'fasta:SEQ:NT',
+    'fasta:SEQ:PT',
+    'fasta:SEQ.MSA:NT',
+    'fasta:SEQ.MSA:PT',
+    'separator:SEQ:NT',
+    'separator:SEQ:PT',
+    'separator:SEQ.MSA:NT',
+    'separator:SEQ.MSA:PT',
+    'HELM'
+  ];
+  const choices = ui.choiceInput('convert to', '', units.filter((e) => e !== current));
 
   ui.dialog('Convert sequence')
     .add(
