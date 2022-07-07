1aeb74012957a3a25257e351d985d0e7570f3eb2
Alexey <achopovsky@datagrok.ai>
2022-07-07 10:04:32 +0300

    Bio: notation converter dummy

---
+++ packages/Bio/src/utils/split-to-monomers.ts
@@ -0,0 +1,8 @@
+/* Do not change these import lines to match external modules in webpack configuration */
+import * as grok from 'datagrok-api/grok';
+import * as ui from 'datagrok-api/ui';
+import * as DG from 'datagrok-api/dg';
+
+export function splitToMonomers() {
+
+}
