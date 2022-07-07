25591514d31d8faaa7f036dd9b9cf9ebabf7f5de
Maria Dolotova <mahouny85@mail.ru>
2022-07-07 08:28:11 +0300

    Bio: added explicit types to sequence space

--- packages/Bio/src/utils/sequence-space.ts
+++ packages/Bio/src/utils/sequence-space.ts
@@ -18,9 +18,9 @@ export async function sequenceSpace(spaceParams: ISequenceSpaceParams): Promise<
     const sepFinal = sep ? sep === '.' ? '\\\.' : sep : '-';
     const regex = new RegExp(sepFinal, 'g');
     if (Object.keys(AvailableMetrics['String']).includes(spaceParams.similarityMetric))
-      preparedData = spaceParams.seqCol.toList().map((v) => v.replace(regex, '')) as string[];
+      preparedData = spaceParams.seqCol.toList().map((v: string) => v.replace(regex, '')) as string[];
     else
-      preparedData = spaceParams.seqCol.toList().map((v) => v.replace(regex, '')) as string[];
+      preparedData = spaceParams.seqCol.toList().map((v: string) => v.replace(regex, '')) as string[];
   } else {
     preparedData = spaceParams.seqCol.toList();
   }
@@ -31,13 +31,13 @@ export async function sequenceSpace(spaceParams: ISequenceSpaceParams): Promise<
     spaceParams.similarityMetric as StringMetrics | BitArrayMetrics,
     spaceParams.options);
   const cols: DG.Column[] = spaceParams.embedAxesNames.map(
-    (name, index) => DG.Column.fromFloat32Array(name, sequenceSpaceResult.embedding[index]));
+    (name: string, index: string) => DG.Column.fromFloat32Array(name, sequenceSpaceResult.embedding[index]));
   return {distance: sequenceSpaceResult.distance, coordinates: new DG.ColumnList(cols)};
 }
 
 
 export function getEmbeddingColsNames(df: DG.DataFrame) {
   const axes = ['Embed_X', 'Embed_Y'];
-  const colNameInd = df.columns.names().filter((it) => it.includes(axes[0])).length + 1;
+  const colNameInd = df.columns.names().filter((it: string) => it.includes(axes[0])).length + 1;
   return axes.map((it) => `${it}_${colNameInd}`);
 }
