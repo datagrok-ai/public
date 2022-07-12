ddf49f2bb7053266aa6cd9724ffa92ed2d56e45f
tanas80 <atanas@datagrok.ai>
2022-07-12 18:30:58 +0400

    Bio pckg: script generate_fasta_csv_for_alphabets.R

---
+++ packages/Bio/scripts/generate_fasta_csv_for_alphabets.R
@@ -0,0 +1,70 @@
+require(tidyverse)
+library(random)
+
+alphabetDna <- c('A','C','G','T')
+alphabetRna <- c('A','C','G','U')
+alphabetPt <- c('G', 'L', 'Y', 'S', 'E', 'Q', 'D', 'N', 'F', 'A',
+                'K', 'R', 'H', 'C', 'V', 'P', 'W', 'I', 'M', 'T',)
+
+toAlphabet <- function(v, a;ph){
+  paste(sapply(v, function(ci){ alph[ci]; }), collapse = '')
+}
+
+mutateString <- function(s, p){
+
+}
+
+seq <- toAlphabet(sample.int(4, 35, replace=TRUE), alphabet);
+seqPt <- toAlphabet(sample.int(20, 35, replace=TRUE), alphabetPt);
+seqDna <- toAlphabet(sample.int(4, 35, replace=TRUE), alphabetDna);
+seqRna <- toAlphabet(sample.int(4, 35, replace=TRUE), alphabetRna);
+# probability to mutate
+seq_p1 <- c(100,  100,  100, 100, 100,   5,  2,  2,  50, 3,
+           100,  100,   7,  2,  2,   7,  2,  33,  100, 100,
+           100,  100,  100, 100, 100, 100, 100, 100, 100, 2,
+           100,  100,  100, 100, 100)
+seq_p2 <- c(100,  100,   7,  2,  2,   7,  2,  33,  100, 100,
+           100,  100,  100, 100, 100, 100, 100, 100, 100, 2,
+           100,  100,  100, 100, 100,   5,  2,  2,  50, 3,
+           100,  100,  100, 100, 100)
+
+# mutate string s with probability p and alphabet
+seq_mutate <- function(s, p, alphabet){
+  # s <- seqDna
+  # p <- seq_p
+  # alphabet <- alphabetDna
+  res_s <- s
+  res_p <- p
+  for (i in 1:(str_length(res_s)*2)) {
+    pos <- sample.int(str_length(res_s), 1)
+    if (sample.int(100, 1) < res_p[pos]) {
+      cast <- sample.int(100, 1) # mutation type probabilty
+      if (0 < cast && cast <= 2 ) {
+        #insertion
+        res_s <- paste(substr(res_s, 1, pos), alphabet[sample.int(4, 1)], substr(res_s, pos+1, str_length(res_s)), collapse='', sep='')
+        res_p <- c(res_p[1:pos], c(100), res_p[(pos+1):length(res_p)])
+        #cat('insertion');
+      } else if (2 < cast && cast <= 4 ) {
+        # deletion
+        res_s <- paste(substr(res_s, 1, pos-1), substr(res_s, pos+1, str_length(res_s)), collapse = '', sep='')
+        res_p <- c(res_p[1: (pos-1)], res_p[(pos+1):length(res_p)])
+        #cat('deletion');
+      } else {
+        # replace
+        res_s <- paste(substr(res_s, 1, pos-1), alphabet[sample.int(4, 1)], substr(res_s, pos+1, str_length(res_s)), collapse='', sep='')
+        #cat('replace')
+      }
+      #cat(res, '\n')
+    }
+  }
+  res_s;
+}
+
+fastaDna_df <- data.frame(id = 1:100, sequence = sapply(1:100, function(id){ seq_mutate(seqDna, seq_p1, alphabetDna)}));
+write_csv(fastaDna_df, 'D:/HOME/atanas/Datagrok/projs/public/packages/Bio/files/samples/sample_FASTA_DNA.csv');
+
+fastaRna_df <- data.frame(id = 1:100, sequence = sapply(1:100, function(id){ seq_mutate(seqRna, seq_p2, alphabetRna)}));
+write_csv(fastaRna_df, 'D:/HOME/atanas/Datagrok/projs/public/packages/Bio/files/samples/sample_FASTA_RNA.csv');
+
+fastaPt_df <- data.frame(id = 1:100, sequence = sapply(1:100, function(id){ seq_mutate(seqPt, seq_p2, alphabetPt)}));
+write_csv(fastaPt_df, 'D:/HOME/atanas/Datagrok/projs/public/packages/Bio/files/samples/sample_FASTA_PT.csv');
