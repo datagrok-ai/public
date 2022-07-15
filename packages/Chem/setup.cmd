cf5ed101c9014bf7b67cfd7b9b7ac78e6cfc1902
Leonid <stolbovla@yandex.ru>
2022-07-15 10:59:52 +0300

    Chem: setup fix

--- packages/Chem/setup.cmd
+++ packages/Chem/setup.cmd
@@ -8,7 +8,7 @@ call npm link datagrok-api
 cd ../../libraries/ml
 call npm install
 call npm link
-call npm link @datagrok-libraries/utils
+call npm link datagrok-api @datagrok-libraries/utils
 cd ../../packages/Chem
 call npm install
 call npm link datagrok-api @datagrok-libraries/utils @datagrok-libraries/ml
