c1488a5c46c7e5660217e5ebdfe529f9ad3a8b5d
Vadym Kovadlo <vadym.kovadlo@gmail.com>
2022-07-18 19:26:39 +0300

    SequenceTranslator: detectors

--- packages/SequenceTranslator/detectors.js
+++ packages/SequenceTranslator/detectors.js
@@ -1,2 +1,26 @@
 class SequenceTranslatorPackageDetectors extends DG.Package {
+  isDnaNucleotides(sequence) {return /^[ATGC]{6,}$/.test(sequence);}
+  isRnaNucleotides(sequence) {return /^[AUGC]{6,}$/.test(sequence);}
+  isAsoGapmerBioSpring(sequence) {return /^[*56789ATGC]{6,}$/.test(sequence);}
+  isAsoGapmerGcrs(sequence) {return /^(?=.*moe)(?=.*5mC)(?=.*ps){6,}/.test(sequence);}
+  isSiRnaBioSpring(sequence) {return /^[*1-8]{6,}$/.test(sequence);}
+  isSiRnaAxolabs(sequence) {return /^[fsACGUacgu]{6,}$/.test(sequence);}
+  isSiRnaGcrs(sequence) {return /^[fmpsACGU]{6,}$/.test(sequence);} // TODO: insert into detectNucleotides
+  isGcrs(sequence) {return /^[fmpsACGU]{6,}$/.test(sequence);}
+  isMermade12(sequence) {return /^[IiJjKkLlEeFfGgHhQq]{6,}$/.test(sequence);}
+  //tags: semTypeDetector
+  //input: column col
+  //output: string semType
+  detectNucleotides(col) {
+    if (col.type === DG.TYPE.STRING) {
+      if (DG.Detector.sampleCategories(col, (s) => this.isDnaNucleotides(s))) return 'DNA nucleotides';
+      if (DG.Detector.sampleCategories(col, (s) => this.isRnaNucleotides(s))) return 'RNA nucleotides';
+      if (DG.Detector.sampleCategories(col, (s) => this.isAsoGapmerBioSpring(s))) return 'BioSpring / Gapmers';
+      if (DG.Detector.sampleCategories(col, (s) => this.isAsoGapmerGcrs(s))) return 'GCRS / Gapmers';
+      if (DG.Detector.sampleCategories(col, (s) => this.isSiRnaBioSpring(s))) return 'BioSpring / siRNA';
+      if (DG.Detector.sampleCategories(col, (s) => this.isSiRnaAxolabs(s))) return 'Axolabs / siRNA';
+      if (DG.Detector.sampleCategories(col, (s) => this.isGcrs(s))) return 'GCRS';
+      if (DG.Detector.sampleCategories(col, (s) => this.isMermade12(s))) return 'Mermade 12 / siRNA';
+    }
+  }
 }
