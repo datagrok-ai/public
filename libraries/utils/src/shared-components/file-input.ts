37da18a1381c78117674ff24649aeca2b5aa7e4b
Maria Dolotova <mahouny85@mail.ru>
2022-07-18 14:48:22 +0300

    Libraries/utils: fixed bug in getting diverse subset, bump version

--- libraries/utils/src/shared-components/file-input.ts
+++ libraries/utils/src/shared-components/file-input.ts
@@ -25,7 +25,7 @@ export class FileInput {
     this.draw();
 
     if (onValueChanged)
-      this.uploadedFile$.subscribe((newValue) => onValueChanged(newValue));
+      this.uploadedFile$.subscribe((newValue: any) => onValueChanged(newValue));
   }
 
   private draw() {
