7f465da85626f2ed96cca2b79a4b4d6d4589b1b2
Volodymyr Dyma <emissaryprog@gmail.com>
2022-07-20 10:47:24 +0300

    Peptides: code cleanup

--- packages/Peptides/src/utils/misc.ts
+++ packages/Peptides/src/utils/misc.ts
@@ -1,14 +1,3 @@
-export function stringToBool(str: string): boolean {
-  switch (str) {
-    case 'true':
-      return true;
-    case 'false':
-      return false;
-    default:
-      throw new Error(`StrToBoolError: cannot convert string '${str}' to boolean`);
-  }
-}
-
 export function getTypedArrayConstructor(
   maxNum: number): Uint8ArrayConstructor | Uint16ArrayConstructor | Uint32ArrayConstructor {
   return maxNum < 256 ? Uint8Array :
