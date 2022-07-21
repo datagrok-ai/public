8fabc871ba7a3b1dcbc63e43ff2c6a61cd51bcfd
tanas80 <atanas@datagrok.ai>
2022-07-21 15:35:38 +0400

    MLB: Fix filters on reloading data, fix for semType Macromolecule

--- packages/MolecularLiabilityBrowser/setup.cmd
+++ packages/MolecularLiabilityBrowser/setup.cmd
@@ -1,35 +1,18 @@
-set package_dir=%cd%
-
 call setup-unlink-clean.cmd
 
-cd "%package_dir%/../../js-api/"                                 & call npm install
-cd "%package_dir%/../../libraries/utils/"                        & call npm install
-cd "%package_dir%/../../libraries/ml/"                           & call npm install
-cd "%package_dir%/../../libraries/bio/"                          & call npm install
-cd "%package_dir%/../../packages/Bio/"                           & call npm install
-cd "%package_dir%/../../packages/MolecularLiabilityBrowserData"  & call npm install
-cd "%package_dir%/"                                              & call npm install
+set package_dir=%cd%
 
-cd "%package_dir%/../../js-api/"                                 & call npm link
-cd "%package_dir%/../../libraries/utils/"                        & call npm link
-cd "%package_dir%/../../libraries/ml/"                           & call npm link
-cd "%package_dir%/../../libraries/bio/"                          & call npm link
-cd "%package_dir%/../../packages/Bio/"                           & call npm link
-cd "%package_dir%/../../packages/MolecularLiabilityBrowserData"  & call npm link
-cd "%package_dir%/"                                              & call npm link
+set dirs=^
+\..\..\js-api\ ^
+\..\..\libraries\ml\ ^
+\..\..\libraries\utils\ ^
+\..\..\libraries\bio\ ^
+\..\..\packages\Bio\ ^
+\..\..\packages\Helm\ ^
+\..\..\packages\MolecularLiabilityBrowserData ^
+\
 
-cd "%package_dir%/../../js-api/"                                 & call npm run link-all
-cd "%package_dir%/../../libraries/utils/"                        & call npm run link-all
-cd "%package_dir%/../../libraries/ml/"                           & call npm run link-all
-cd "%package_dir%/../../libraries/bio/"                          & call npm run link-all
-cd "%package_dir%/../../packages/Bio/"                           & call npm run link-all
-cd "%package_dir%/../../packages/MolecularLiabilityBrowserData"  & call npm run link-all
-cd "%package_dir%/"                                              & call npm run link-all
-
-cd "%package_dir%/../../js-api/"                                 & call npm run build
-cd "%package_dir%/../../libraries/utils/"                        & call npm run build
-cd "%package_dir%/../../libraries/ml/"                           & call npm run build
-cd "%package_dir%/../../libraries/bio/"                          & call npm run build
-cd "%package_dir%/../../packages/Bio/"                           & call npm run build
-cd "%package_dir%/../../packages/MolecularLiabilityBrowserData"  & call npm run build
-cd "%package_dir%/"                                              & call npm run build
+for %%p in (%dirs%) do cd %package_dir%\%%p & call npm install
+for %%p in (%dirs%) do cd %package_dir%\%%p & call npm link
+for %%p in (%dirs%) do cd %package_dir%\%%p & call npm run link-all
+for %%p in (%dirs%) do cd %package_dir%\%%p & call npm run build
