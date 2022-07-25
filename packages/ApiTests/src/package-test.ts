87f310855842520d3ae971cc7ea7f97676996ae8
osakhniuk <osakhniuk@datagrok.ai>
2022-07-25 13:47:04 +0300

    GROK-10966  JS UI Test: Similarity Search

--- packages/ApiTests/src/package-test.ts
+++ packages/ApiTests/src/package-test.ts
@@ -64,6 +64,8 @@ import './gui/scatter-plot-3d.ts'
 import './gui/word-cloud'
 import './gui/network-diagram'
 import './gui/box-plot'
+import './gui/chem-similarity-search'
+import './gui/r-groups-analysis'
 
 import {runTests, tests} from '@datagrok-libraries/utils/src/test';
 export const _package = new DG.Package();
