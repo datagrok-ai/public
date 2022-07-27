971bf0274264da370825a3bb70ad195f0d462c15
Maksym Shvyrlo <64889448+ShvyrloM@users.noreply.github.com>
2022-07-27 22:24:31 +0300

    GIS: hot map, multi values, weighted markers

--- packages/GIS/src/package.ts
+++ packages/GIS/src/package.ts
@@ -4,6 +4,8 @@ import * as ui from 'datagrok-api/ui';
 import * as DG from 'datagrok-api/dg';
 
 import {GisViewer} from '../src/gis-viewer';
+//OpenLayers functionality import
+import {OpenLayers} from '../src/gis-openlayer'; //TODO: remobve it further (it needs just for preview)
 
 export const _package = new DG.Package();
 
@@ -18,4 +20,37 @@ export function info() {
 //output: viewer result
 export function gisViewer(): GisViewer {
   return new GisViewer();
+}
+
+//name: gisKMLFileViewer
+//tags: fileViewer, fileViewer-kml
+//input: file file
+//output: view v
+export function gisKMLFileViewer(file: DG.FileInfo): DG.View {
+  const viewFile = DG.View.create();
+  let htmlStyle: DG.ElementOptions = {style: {'width': '100%', 'height': '100%', 'border': 'solid 1px yellow'}};
+  // const boxMap = ui.box(null, htmlStyle);
+  const boxMap = ui.box(ui.div(file.fullPath), htmlStyle);
+  //file.path
+
+  boxMap.id = 'map-container'; //boxMap - div that contains map
+
+
+  viewFile.append(boxMap);
+  // const ol = new OpenLayers();
+  // ol.initMap('map-container');
+
+  return viewFile;
+}
+
+//name: openGISViewer
+export function openGISViewer(): void {
+  let htmlStyle: DG.ElementOptions = { };
+  htmlStyle = {style: {'width': '100%', 'height': '100%', 'border': 'solid 1px yellow'}};
+  const boxMap = ui.box(null, htmlStyle);
+  boxMap.id = 'map-container'; //boxMap - div that contains map
+  grok.shell.newView('Leaflet preview view', [boxMap]);
+
+  const ol = new OpenLayers();
+  ol.initMap('map-container');
 }
