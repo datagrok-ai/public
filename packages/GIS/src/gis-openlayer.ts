b11238aecd190282cdd01b95be77db2df59628d0
Maksym Shvyrlo <64889448+ShvyrloM@users.noreply.github.com>
2022-07-27 22:24:31 +0300

    GIS: hot map, multi values, weighted markers

--- packages/GIS/src/gis-openlayer.ts
+++ packages/GIS/src/gis-openlayer.ts
@@ -1,7 +1,9 @@
-import {Map, View as OLView} from 'ol';
+import {Map as OLMap, MapBrowserEvent, View as OLView} from 'ol';
+import HeatmapLayer from 'ol/layer/Heatmap';
+import BaseLayer from 'ol/layer/Base';
 import TileLayer from 'ol/layer/Tile';
-import TileImage from 'ol/source/TileImage'; //this is the base class for XYZ, BingMaps etc..
 import VectorLayer from 'ol/layer/Vector';
+import TileImage from 'ol/source/TileImage'; //this is the base class for XYZ, BingMaps etc..
 import VectorSource from 'ol/source/Vector';
 //Projections working itilities
 import * as OLProj from 'ol/proj';
@@ -10,44 +12,59 @@ import {Coordinate} from 'ol/coordinate';
 import * as OLPolygon from 'ol/geom/Polygon';
 import Feature from 'ol/Feature';
 import Point from 'ol/geom/Point';
+import * as OLStyle from 'ol/style';
 //Sources import
 import OSM from 'ol/source/OSM';
 import BingMaps from 'ol/source/BingMaps';
-import BaseLayer from 'ol/layer/Base';
+import KML from 'ol/format/KML';
+import Style, {StyleLike} from 'ol/style/Style';
+
+export {Coordinate} from 'ol/coordinate';
+//interface for callback functions parameter
+export interface OLCallbackParam {
+  coord: Coordinate; //[number, number];
+  pixel: [number, number];
+}
 
 export class OpenLayers {
-  olMap: Map;
-  olLayers: TileLayer<TileImage>[]; //array or map layers TODO: change it to Collection ??
+  olMap: OLMap;
   olCurrentView: OLView;
-  olCurrentLayer: BaseLayer | VectorLayer<VectorSource> | null;
+  olCurrentLayer: BaseLayer | VectorLayer<VectorSource> | HeatmapLayer | null;
   olBaseLayer: BaseLayer | null;
   olMarkersLayer: VectorLayer<VectorSource> | null;
+
+  //event handlers map
+  eventsMap = new Map<string, ()=>any>(); //TODO: solve this puzzle
+  onClickCallback: Function | null = null;
+  onPointermoveCallback: Function | null = null;
+  public labelStatus: HTMLElement | null = null;
+
   constructor() {
-    this.olMap = new Map({});
-    this.olLayers = [];
+    this.olMap = new OLMap({});
     this.olCurrentView = new OLView({});
     this.olCurrentLayer = null;
     this.olBaseLayer = null;
     this.olMarkersLayer = null;
   }
+
   initMap(targetName: string) {
     if (targetName === '') return;
 
-    this.olMap = new Map({
+    this.olMap = new OLMap({
       target: targetName,
-      //layers: this.olLayers,
       view: new OLView({
-        center: OLProj.fromLonLat([34.109565029604006, 45.45296242157734]),
+        center: OLProj.fromLonLat([34.109565, 45.452962]),
         zoom: 7,
       }),
     });
 
-    // this.addNewView({
-    //   center: OLProj.fromLonLat([34.109565029604006, 45.45296242157734]),
-    //   zoom: 7,
-    // });
+    //add layers>>
     this.olBaseLayer = this.addNewOSMLayer('BaseLayer');
-    this.olMarkersLayer = this.addNewVectorLayer('Markers');
+    this.olMarkersLayer = this.addNewVectorLayer('Markers');//, this.genStyleMarker);
+
+    //add base event handlers>>
+    this.olMap.on('click', this.onMapClick);
+    this.olMap.on('pointermove', this.onMapPointermove);
   }
 
   addNewView(options?: Object | undefined) {
@@ -63,15 +80,30 @@ export class OpenLayers {
     this.olCurrentLayer = layerToAdd;
   }
 
+  getLayersList(): string[] {
+    const arrayNames: string[] = [];
+
+    if (this.olMap) {
+      let layersArr = this.olMap.getAllLayers();
+      for (let i = 0; i < layersArr.length; i++) {
+        let layerName = i + ' ' + layersArr[i].get('layerName');
+        //if (!layerName) layerName = i + ':';
+        arrayNames.push(layerName);
+      }
+    }
+    return arrayNames;
+  }
+
   // addNewTileLayer(layerToAdd: BaseLayer) //TODO: add
 
   //adds arbitrary Vector layer
-  addNewVectorLayer(layerName?: string | undefined, options?: Object | undefined): VectorLayer<VectorSource> {
+  addNewVectorLayer(lrName?: string, opt?: Object, style?: StyleLike): VectorLayer<VectorSource> {
     const sourceVector = new VectorSource();
     const newLayer = new VectorLayer({source: sourceVector});
 
-    if (layerName) newLayer.set('layerName', layerName);
-    if (options) newLayer.setProperties(options);
+    if (lrName) newLayer.set('layerName', lrName);
+    if (opt) newLayer.setProperties(opt);
+    if (style) newLayer.setStyle(style);
     this.olMap.addLayer(newLayer);
     return newLayer;
   }
@@ -89,14 +121,139 @@ export class OpenLayers {
     return newLayer;
   }
 
+  addNewHeatMap(layerName?: string | undefined, options?: Object | undefined): HeatmapLayer {
+    const newLayer = new HeatmapLayer({
+      source: new VectorSource({}),
+      blur: 25,
+      radius: 10,
+      weight: function(feature: Feature): number {
+        let val = feature.get('fieldValue');
+        if (typeof(val) !== 'number') val = 1;
+        return val;
+      },
+    });
+    if (layerName) newLayer.set('layerName', layerName);
+    if (options) newLayer.setProperties(options);
+    this.olMap.addLayer(newLayer);
+    return newLayer;
+  }
+
+  //map marker style function>>
+  genStyleMarker(feature: Feature): Style {
+    let val = feature.get('fieldValue');
+    if (typeof(val) !== 'number') val = 1;
+
+    const style = new Style({
+      image: new OLStyle.Circle({
+        radius: val*20,
+        fill: new OLStyle.Fill({
+          color: 'rgba(255, 153, 0, 0.4)',
+        }),
+        stroke: new OLStyle.Stroke({
+          color: 'rgba(255, 204, 0, 0.2)',
+          width: 1,
+        }),
+      }),
+    });
+    return style;
+  }
+
+
+  //map base events handlers>>
+  onMapClick(evt: MapBrowserEvent<any>) {
+    // evt.coordinate
+    //evt.pixel
+    const res: OLCallbackParam = {
+      coord: evt.coordinate,
+      pixel: [evt.pixel[0], evt.pixel[1]]
+    };
+
+    if (this.onClickCallback)
+      this.onClickCallback(res);
+  }
+  onMapPointermove(evt: MapBrowserEvent<any>) {
+    if (evt.dragging) return;
+
+    const res: OLCallbackParam = {
+      coord: evt.coordinate,
+      pixel: [evt.pixel[0], evt.pixel[1]]
+    };
+
+    //TODO: remove this crutch - only callback fn
+    if (this.labelStatus)
+      this.labelStatus.innerText = evt.coordinate[0] + ', ' + evt.coordinate[1];
+    else {
+      //TODO: remove this crutch
+      let lbl = document.getElementById('lbl-coord');
+      if (lbl) lbl.innerHTML = evt.coordinate[0] + ', ' + evt.coordinate[1];
+    }
+
+    if (this.onPointermoveCallback)
+      this.onPointermoveCallback(res);
+  }
+
+  //map events management functions>>
+  setMapEventHandler() {
+    // this.olMap.on('click', function (evt) {
+    //   displayFeatureInfo(evt.pixel);
+    // });
+  }
+  setMapClickCallback(f: Function) {
+    this.onClickCallback = f;
+  }
+  setMapPointermoveCallback(f: Function) {
+    this.onPointermoveCallback = f;
+  }
+
   //map elements management functions>>
-  addPoint(coord: Coordinate, layer?: VectorLayer<VectorSource> | undefined) {
-    let aLayer = this.olMarkersLayer;
+  clearLayer(layer?: VectorLayer<VectorSource> | HeatmapLayer | undefined) {
+    let aLayer: VectorLayer<VectorSource> | HeatmapLayer | undefined | null;
+    aLayer = this.olMarkersLayer;
+    if (layer) aLayer = layer;
+    if (aLayer) {
+      const src = aLayer.getSource();
+      if (src) src.clear();
+    }
+  }
+
+  addPoint(coord: Coordinate, value?: string|number|undefined,
+    layer?: VectorLayer<VectorSource>|HeatmapLayer|undefined) {
+    //
+    let aLayer: VectorLayer<VectorSource>|HeatmapLayer|undefined|null;
+    aLayer = this.olMarkersLayer;
     if (layer) aLayer = layer;
 
     if (aLayer) {
+      let val = value;
+      if (typeof(val) !== 'number') val = 1;
+
+      const marker = new Feature(new Point(OLProj.fromLonLat(coord)));
+      const style = new Style({
+        image: new OLStyle.Circle({
+          radius: val*1,
+          fill: new OLStyle.Fill({
+            color: 'rgba(255, 153, 0, 0.4)',
+          }),
+          stroke: new OLStyle.Stroke({
+            color: 'rgba(255, 204, 0, 0.2)',
+            width: 1,
+          }),
+        }),
+      });
+      marker.setStyle(style);
+
       const src = aLayer.getSource();
-      src?.addFeature(new Feature(new Point(OLProj.fromLonLat(coord))));
+      if (src) src.addFeature(marker);
     }
   }
+  //   addPointsSet(coord: Coordinate, layer?: VectorLayer<VectorSource> | undefined) {
+  //     let aLayer = this.olMarkersLayer;
+  //     if (layer) aLayer = layer;
+
+//     if (aLayer) {
+//       const src = aLayer.getSource();
+// //      src?.addFeatures()
+//       src?.addFeature(new Feature(new Point(OLProj.fromLonLat(coord))));
+//     }
+//   }
 }
--- packages/GIS/src/gis-viewer.ts
+++ packages/GIS/src/gis-viewer.ts
@@ -1,28 +1,46 @@
 /*
-  GIS VIEWER
-  TODO: add description here
+ * GIS VIEWER
+ * TODO: add description here
 */
 //base import
 //import * as grok from 'datagrok-api/grok';
 import * as ui from 'datagrok-api/ui';
 import * as DG from 'datagrok-api/dg';
+//QJuery import
+import $ from 'cash-dom';
+
 //OpenLayers functionality import
 import {OpenLayers} from '../src/gis-openlayer';
+import {Coordinate} from '../src/gis-openlayer';
+import {OLCallbackParam} from '../src/gis-openlayer';
+
 
 type Pair<T, K> = [T, K];
 
 export class GisViewer extends DG.JsViewer {
   latitudeColumnName: string;
   longitudeColumnName: string;
-  // renderType = this.string('renderType', 'heat map');
-  // markerSize = this.int('markerSize', 10);
-  // markerOpacity = this.float('markerOpacity', 0.8);
-  //getProperty('renderType').choices = ['markers', 'heat map'];
+  valuesColumnName: string;
+  renderType: string;
+  markerSize: number;
+  markerOpacity: number;
+  weightedMarkers: boolean;
+
+  //ui elements
+  panelLeft: HTMLElement | null = null;
+  panelRight: HTMLElement | null = null;
+  panelTop: HTMLElement | null = null;
+  panelBottom: HTMLElement | null = null;
+  viewerContainer: HTMLElement | null = null;
+  lblStatusCoords: HTMLElement | null = null;
+  divLayersList: HTMLElement | null = null;
 
   initialized: boolean;
   ol: OpenLayers;
   layers = [];
-  coordinates: Pair<number, number>[] = [];
+  coordinates: Coordinate[] = [];
+  values: Array<string | number> = [];
+  // coordinates: Pair<number, number>[] = [];
 
   constructor() {
     super();
@@ -33,48 +51,138 @@ export class GisViewer extends DG.JsViewer {
     // properties
     this.latitudeColumnName = this.string('latitudeColumnName');
     this.longitudeColumnName = this.string('longitudeColumnName');
-    // this.renderType = this.string('renderType', 'heat map');
-    // this.markerSize = this.int('markerSize', 10);
-    // this.markerOpacity = this.float('markerOpacity', 0.8);
-    // this.getProperty('renderType').choices = ['markers', 'heat map'];
+    this.valuesColumnName = this.string('valuesColumnName');
+    this.markerSize = this.int('markerSize', 10);
+    this.markerOpacity = this.float('markerOpacity', 0.8);
+    this.weightedMarkers = this.bool('weightedMarkers', true);
 
-    // this.layers = [];
-    // this.coordinates = [];
-
-    // ui.onSizeChanged(this.root).subscribe((_) => this.map.invalidateSize());
+    this.renderType = this.string('renderType', 'markers');
+    const renderTypeProp = this.getProperty('renderType');
+    if (renderTypeProp) renderTypeProp.choices = ['markers', 'heat map'];
   }
 
-  initUi() {
-    let htmlStyle: DG.ElementOptions = { };
-    htmlStyle = {style: {'border': 'solid 1px darkgray'}};
-    const panelTop = ui.divH([ui.div('MENU')], htmlStyle);
-    const panelLeft = ui.divV([], htmlStyle);
-    const panelRight = ui.divV([], htmlStyle);
-    const panelBottom = ui.divH([ui.div('status bar')], htmlStyle);
-    // panelTop.setAttribute('id', 'gis-panel-top'); //TODO: maybe we need to use id further
-    panelBottom.style.background = '#f2f2f5';
+  initUi(shortUI: boolean = false): HTMLElement {
+    //create UI>>
+    this.lblStatusCoords = ui.div('long, lat');
+    this.lblStatusCoords.id = 'lbl-coord';
+    const divStatusBody = ui.divH([ui.div('status bar: '), this.lblStatusCoords]);
+    this.panelBottom = ui.box(divStatusBody);
+    this.panelBottom.style.maxHeight = '20px';
+    this.panelBottom.style.background = '#f2f2f5';
+    this.panelBottom.style.border = 'solid 1px darkgray';
 
-    //const boxMap = ui.box(null, 'd4-viewer-host');
-    htmlStyle = {style: {'width': '100%', 'height': '100%', 'border': 'solid 1px red'}};
-    //const boxMap = ui.box(null, htmlStyle);
-    //const boxMap = ui.div([], 'd4-viewer-host');
-    const boxMap = ui.div([], htmlStyle);
-    boxMap.id = 'map-container';
-    //htmlStyle = {style: {'border': 'solid 1px blue'}};
-    htmlStyle = {style: {'width': '100%', 'height': '100%', 'border': 'solid 1px blue'}};
-    const boxCenter = ui.divH([panelLeft, boxMap, panelRight], htmlStyle);
-    //const boxCenter = ui.divH([panelLeft, boxMap, panelRight], 'd4-viewer-host');
+    this.panelTop = ui.box();
+    this.panelTop.style.maxHeight = '36px';
 
-    const divViewerContainer = ui.divV([panelTop, boxCenter, panelBottom], 'd4-viewer-host');
+    this.panelLeft = ui.box();
+    this.panelLeft.style.maxWidth = '90px';
+    this.panelLeft.style.border = 'solid 1px darkgray';
 
-    this.root.appendChild(divViewerContainer);
+    this.panelRight = ui.box();
+    this.panelRight.style.maxWidth = '10px';
+    //this.panelRight.style.border = 'solid 1px darkgray';
+    //menu bar icons>>
+    const leftPanelBtn = ui.button('Left panel', ()=>{
+      // this.panelLeft.style.visibility = (this.panelLeft.style.visibility == 'visible') ? 'hidden' : 'visible';
+      if (this.panelLeft) {
+        if (this.panelLeft.style.visibility == 'visible') {
+          this.panelLeft.style.visibility = 'hidden';
+          this.panelLeft.style.maxWidth = '2px';
+        } else {
+          this.panelLeft.style.visibility = 'visible';
+          this.panelLeft.style.maxWidth = '90px';
+        }
+      }
+      this.rootOnSizeChanged(null);
+      //$(this.panelLeft).hide();
+    });
+    const rightPanelBtn = ui.button('Right panel', ()=>{
+      // if (this.panelRight)
+      //   this.panelRight.style.visibility = (this.panelRight.style.visibility == 'visible') ? 'hidden' : 'visible';
+      if (this.panelRight) {
+        if (this.panelRight.style.visibility == 'visible') {
+          this.panelRight.style.visibility = 'hidden';
+          this.panelRight.style.maxWidth = '2px';
+        } else {
+          this.panelRight.style.visibility = 'visible';
+          this.panelRight.style.maxWidth = '10px';
+        }
+      }
+      this.rootOnSizeChanged(null);
+      //$(this.panelRight).hide();
+    });
+    const heatmapBtn = ui.button('HM', ()=>{
+      this.renderHeat();
+      //$(this.panelRight).hide();
+    });
+    //add buttont to top menu panel
+    this.panelTop.append(ui.divH([leftPanelBtn, rightPanelBtn, heatmapBtn]));
+
+    //left panel icons>>
+    const l1 = ui.div(' <l1>');
+    const l2 = ui.div(' <l2>');
+    this.divLayersList = ui.divV([l1, l2]);
+    this.panelLeft.append(this.divLayersList);
+
+    this.panelLeft.style.visibility = 'visible';
+    this.panelRight.style.visibility = 'visible';
+    // $(this.panelLeft).hide();
+    // $(this.panelRight).hide();
+
+    const body = ui.box();
+    body.id = 'map-container';
+
+    this.viewerContainer = ui.splitV(
+      [this.panelTop,
+        ui.splitH([this.panelLeft, body, this.panelRight]),
+        this.panelBottom]);
+
+    this.viewerContainer.style.border = 'solid 2px lightgray';
+    this.root.appendChild(this.viewerContainer);
+
+    return this.viewerContainer;
   }
 
   init() {
     this.initUi();
     this.ol.initMap('map-container');
 
+    this.subs.push(ui.onSizeChanged(this.root).subscribe(this.rootOnSizeChanged.bind(this)));
+
+    this.ol.setMapPointermoveCallback(this.showCoordsInStatus);
+    this.ol.labelStatus = this.lblStatusCoords; //TODO: remove this construction - all interactions through callback!!
+
+    this.updateLayersList();
+
     this.initialized = true;
+  }
+
+  updateLayersList() {
+    if (!this.ol) return;
+    if (!this.panelLeft) return;
+    if (!this.divLayersList) return;
+
+    //while (this.divLayersList.lastChild) this.divLayersList.lastChild.remove;
+    while (this.divLayersList.lastChild) this.divLayersList.removeChild(this.divLayersList.lastChild);
+    const layersNames = this.ol.getLayersList();
+
+    for (let i = 0; i < layersNames.length; i++) {
+      // const l2 = ui.div(layersNames[i]);
+      this.divLayersList.append(ui.div(layersNames[i], {style: {'border': 'solid 1px blue'}}));
+    }
+  }
+
+  showCoordsInStatus(p: OLCallbackParam): void {
+    if (this.lblStatusCoords) {
+      if (p)
+        this.lblStatusCoords.innerText = p.coord[0] + ', ' + p.coord[1];
+    }
+  }
+
+  private rootOnSizeChanged(args: any): void {
+    //this.ol.olMap.setSize(this.root.clientWidth, this.root.clientHeight);
+    //this.ol.olMap.updateSize();
+    setTimeout( function(m) {m.updateSize();}, 200, this.ol.olMap);
   }
 
   onTableAttached() {
@@ -97,6 +205,7 @@ export class GisViewer extends DG.JsViewer {
   }
 
   detach() {
+    //TODO - release map
     // this.map.remove();
     this.subs.forEach((sub) => sub.unsubscribe());
   }
@@ -109,37 +218,63 @@ export class GisViewer extends DG.JsViewer {
     if (this.latitudeColumnName == null || this.longitudeColumnName == null)
       return;
 
-    this.getCoordinates();
-    this.renderMarkers();
+    this.getCoordinates(); //not only coordinates but a data at all
+    // this.renderMarkers();
 
-    // if (this.renderType === 'heat map')
-    //   this.renderHeat();
-    // else if (this.renderType === 'markers')
-    //   this.renderMarkers();
+    if (this.renderType === 'heat map')
+      this.renderHeat();
+    else if (this.renderType === 'markers')
+      this.renderMarkers();
 
     // if (fit)
     //   this.map.fitBounds(this.coordinates);
+    this.updateLayersList();
   }
 
   getCoordinates() {
     this.coordinates.length = 0;
+    this.values.length = 0;
     const indexes = this.dataFrame.filter.getSelectedIndexes();
-    const lat = this.dataFrame.getCol(this.latitudeColumnName).getRawData();
-    const lon = this.dataFrame.getCol(this.longitudeColumnName).getRawData();
+    let lat: Int32Array | Float32Array | Float64Array | Uint32Array;
+    let lon: Int32Array | Float32Array | Float64Array | Uint32Array;
+    let val: Int32Array | Float32Array | Float64Array | Uint32Array;
+
+    lat = this.dataFrame.getCol(this.latitudeColumnName).getRawData();
+    lon = this.dataFrame.getCol(this.longitudeColumnName).getRawData();
+    // val = this.dataFrame.getCol(this.valuesColumnName).getRawData();
+
+    // col = this.dataFrame.getCol(this.latitudeColumnName);
+    // if (col) lat = col.getRawData();
+    // col = this.dataFrame.getCol(this.longitudeColumnName);
+    // if (col) lon = col.getRawData();
+    const col = this.dataFrame.getCol(this.valuesColumnName);
+    //TODO: change it to filling array of objects with all table data
+    if (col) val = col.getRawData();
+    else {
+      val = new Int32Array(lat.length); //lat.copyWithin(0, 0);
+      val.fill(1);
+    }
 
     for (let i = 0; i < indexes.length; i++) {
-      if ((i < lat.length ) && (i < lon.length ))
+      if ((i < lat.length ) && (i < lon.length )) {
         this.coordinates.push([lon[indexes[i]], lat[indexes[i]]]);
+        this.values.push(val[indexes[i]]);
+      }
     }
   }
 
   renderHeat() {
     // this.layers.push(L.heatLayer(this.coordinates, {radius: this.markerSize}).addTo(this.map));
+    this.getCoordinates();
+    const layer = this.ol.addNewHeatMap('heatLayer');
+    for (let i = 0; i < this.coordinates.length; i++)
+      this.ol.addPoint(this.coordinates[i], this.values[i], layer);
   }
 
   renderMarkers() {
+    this.ol.clearLayer(); //TODO: clear exact layer
     for (let i = 0; i < this.coordinates.length; i++)
-      this.ol.addPoint(this.coordinates[i]);
+      this.ol.addPoint(this.coordinates[i], this.values[i]);
 
     // let markerOptions = {
     //   radius: this.markerSize,
