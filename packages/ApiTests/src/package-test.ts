f2bbe483353d16bf4fc50b1e2538c554a7beb90c
Aleksandr Tanas <atanas@datagrok.ai>
2022-07-28 18:44:11 +0400

    ApiTests: Add test Detector/SingleEmptyCategory

--- packages/ApiTests/src/package-test.ts
+++ packages/ApiTests/src/package-test.ts
@@ -2,6 +2,7 @@ import * as DG from 'datagrok-api/dg';
 import * as grok from 'datagrok-api/grok';
 import * as ui from 'datagrok-api/ui';
 import './dataframe/dataframe';
+import './dataframe/detector';
 import './dataframe/calculated-columns';
 import './dataframe/events';
 import './functions/functions';
