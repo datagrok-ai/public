488366dd3d9303b49a8d4d052eba2857b5b18609
Aufar Zakiev <zaufar@gmail.com>
2022-08-05 11:14:22 +0300

    Compute: removed outdated files

--- packages/Compute/src/view-samples/computation-test-view.ts
+++
@@ -1,8 +0,0 @@
-import * as grok from 'datagrok-api/grok';
-import * as ui from 'datagrok-api/ui';
-import * as DG from 'datagrok-api/dg';
-import {ComputationView} from "@datagrok-libraries/utils/src/computation-view";
-
-export class ComputationTestView extends ComputationView {
-
-}
