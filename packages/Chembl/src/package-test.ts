e6e16f9ce67a73ac314ba3c37404356d670344ca
Leonid <stolbovla@yandex.ru>
2022-08-08 16:23:47 +0300

    Chmebl: polishing, fit new cartridge requirements, test initiated

---
+++ packages/Chembl/src/package-test.ts
@@ -0,0 +1,17 @@
+import * as DG from 'datagrok-api/dg';
+import {runTests, tests} from '@datagrok-libraries/utils/src/test';
+
+import './tests/queries';
+
+export const _package = new DG.Package();
+export {tests};
+
+//name: test
+//input: string category {optional: true}
+//input: string test {optional: true}
+//output: dataframe result
+//top-menu: Tools | Dev | JS API Tests
+export async function test(category: string, test: string): Promise<DG.DataFrame> {
+  const data = await runTests({category, test: test});
+  return DG.DataFrame.fromObjects(data)!;
+}
