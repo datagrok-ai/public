0ddea90eeaad155de3898430caf26bce091c8a20
Volodymyr Dyma <emissaryprog@gmail.com>
2022-08-10 11:56:59 +0300

    Libary/bio: MonomerWorks class

---
+++ libraries/bio/src/monomer-works.ts
@@ -0,0 +1,32 @@
+import * as grok from 'datagrok-api/grok';
+import * as ui from 'datagrok-api/ui';
+import * as DG from 'datagrok-api/dg';
+import {SeqPalette} from './seq-palettes';
+
+class MonomerWorks {
+  monomerLibs: string[] | null = null;
+  acceptedMonomers: Set<string> | null = null;
+
+  constructor() {
+
+  }
+
+  /*
+  getColors(seqCol: DG.Column<string>, method?: string): SeqPalette {
+    if (!method) {
+      // return Unknown
+      if () {} //monomer not in acceptedMonomers color it red
+
+    } else {
+      switch (method) {
+        case :
+          break;
+        default:
+          throw new Error('Not implemented');
+      }
+    }
+
+    return ;
+  }
+  */
+}
