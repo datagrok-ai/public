ba4141f43e2d6505f7d6289a6796f20961265ade
Maria Dolotova <mahouny85@mail.ru>
2022-08-10 17:59:23 +0300

    Bio: added sequence space and activity cliffs tests with empty rows

--- packages/Bio/src/tests/sequence-space-test.ts
+++ packages/Bio/src/tests/sequence-space-test.ts
@@ -1,30 +1,35 @@
-import {after, before, category, test, expect} from '@datagrok-libraries/utils/src/test';
+import {after, before, category, test, expect, delay} from '@datagrok-libraries/utils/src/test';
 import * as DG from 'datagrok-api/dg';
-import {sequenceSpace} from '../utils/sequence-space';
 import {readDataframe} from './utils';
 import * as grok from 'datagrok-api/grok';
+import { _testSequenceSpaceReturnsResult } from './sequence-space-utils';
 
 category('sequenceSpace', async () => {
   let testFastaDf: DG.DataFrame;
+  let testFastaTableView: DG.TableView;
+  let testHelmWithEmptyRows: DG.DataFrame;
+  let testHelmWithEmptyRowsTableView: DG.TableView;
 
   before(async () => {
     testFastaDf = await readDataframe('samples/sample_FASTA.csv');
-    // await grok.data.detectSemanticTypes(testFastaDf);
+    testFastaTableView = grok.shell.addTableView(testFastaDf);
+    testHelmWithEmptyRows = await readDataframe('samples/sample_HELM_empty_vals.csv');
+    testHelmWithEmptyRowsTableView = grok.shell.addTableView(testHelmWithEmptyRows);
   });
 
   after(async () => {
     grok.shell.closeTable(testFastaDf);
+    testFastaTableView.close();
+    grok.shell.closeTable(testHelmWithEmptyRows);
+    testHelmWithEmptyRowsTableView.close();
   });
 
   test('sequenceSpaceOpens', async () => {
-    const sequenceSpaceParams = {
-      seqCol: testFastaDf.col('Sequence')!,
-      methodName: 't-SNE',
-      similarityMetric: 'Levenshtein',
-      embedAxesNames: ['Embed_X', 'Embed_Y']
-    };
-    const res = await sequenceSpace(sequenceSpaceParams);
-    expect(res.coordinates != undefined, true);
-    expect(res.distance != undefined, true);
+    await _testSequenceSpaceReturnsResult(testFastaDf, 'UMAP', 'Sequence');
   });
+
+  test('sequenceSpaceOpensWithEmptyRows', async () => {
+    await _testSequenceSpaceReturnsResult(testHelmWithEmptyRows, 'UMAP', 'HELM');
+  });
+
 });
