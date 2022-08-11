cdbb3683f32f576f2b09a75e2a292c1568bd8abc
Leonid <stolbovla@yandex.ru>
2022-08-11 14:50:17 +0300

    ST: any sequences directions for saving

--- packages/SequenceTranslator/src/structures-works/save-sense-antisense.ts
+++ packages/SequenceTranslator/src/structures-works/save-sense-antisense.ts
@@ -3,14 +3,14 @@ import {sequenceToMolV3000} from '../structures-works/from-monomers';
 import {linkV3000} from '../structures-works/mol-transformations';
 import {getFormat} from '../structures-works/sequence-codes-tools';
 
-export function saveSdf(as: string, ss: string, oneEntity: boolean, fit3dx: boolean) {
+export function saveSdf(as: string, ss: string, oneEntity: boolean, invertSS: boolean, invertAS: boolean) {
   const formatAs = getFormat(as);
   const formatSs = getFormat(ss);
-  const molSS = sequenceToMolV3000(ss, false, false, formatSs!);
-  const molAS = sequenceToMolV3000(as, true, false, formatAs!);
+  const molSS = sequenceToMolV3000(ss, invertSS, false, formatSs!);
+  const molAS = sequenceToMolV3000(as, invertAS, false, formatAs!);
   let result: string;
   if (oneEntity)
-    result = linkV3000([molSS, molAS], true, !fit3dx) + '\n\n$$$$\n';
+    result = linkV3000([molSS, molAS], true) + '\n\n$$$$\n';
   else {
     result =
     molSS + '\n' +
@@ -27,22 +27,33 @@ export function saveSdf(as: string, ss: string, oneEntity: boolean, fit3dx: bool
 
 export function saveSenseAntiSense() {
   const moleculeSvgDiv = ui.block([]);
-  const ssInput = ui.textInput('Sense Strand 5\' ->3\'', '');
-  const asInput = ui.textInput('Anti Sense 3\' ->5\'', '');
+  const ssInput = ui.textInput('Sense Strand', '');
+  const asInput = ui.textInput('Anti Sense', '');
+  const straight = '5\' ->3\'';
+  const inverse = '3\' ->5\'';
+  let ssInverse = false;
+  let asInverse = false;
+
+  const changeSense = ui.choiceInput('SS direction', straight, [straight, inverse]);
+  changeSense.onChanged(() =>{ ssInverse = changeSense.value == inverse; });
+  const changeAntiSense = ui.choiceInput('AS direction', straight, [straight, inverse]);
+  changeAntiSense.onChanged(() =>{ asInverse = changeAntiSense.value == inverse; });
+
   const saveOption = ui.switchInput('Save as one entity', true);
-  const save3dx = ui.switchInput('Save 3dx', true);
   const saveBtn = ui.button('Save SDF', () =>
-    saveSdf(asInput.value, ssInput.value, saveOption.value, save3dx.value));
+    saveSdf(asInput.value, ssInput.value, saveOption.value, ssInverse, asInverse));
 
   const saveSection = ui.panel([
     ui.div([
       ui.div([
         ui.divH([ui.h1('Inputs')]),
         ui.divV([
-          ui.div([ssInput.root]),
-          ui.div([asInput.root]),
+          ssInput,
+          asInput,
+          ui.div([changeSense], {style: {width: '40'}}),
+          changeSense,
+          changeAntiSense,
           saveOption,
-          save3dx,
           ui.buttonsInput([saveBtn]),
         ], 'ui-form'),
       ], 'ui-form'),
