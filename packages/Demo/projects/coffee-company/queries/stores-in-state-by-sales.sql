8cdfa69d83547d055b3c2a13b9fc56d0dcd7760f
Yuliia <yulya.lavrinenko.2000@gmail.com>
2022-08-12 13:37:24 +0200

    changed folder to package in scripts

--- packages/Demo/projects/coffee-company/queries/stores-in-state-by-sales.sql
+++ packages/Demo/projects/coffee-company/queries/stores-in-state-by-sales.sql
@@ -1,5 +1,5 @@
 --name: stores in @state with sales greater than @sales
---input: string state
+--input: string state = 'NY' 
 --input: int sales
 select * from starbucks_us where state = @state and sales > @sales
 --end
