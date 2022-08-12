8c8cfabfb92dc57301482f654119816b80613203
osakhniuk <osakhniuk@datagrok.ai>
2022-08-12 17:05:19 +0300

    GROK-10709 Implement test DB testing

--- packages/ApiTests/queries/postgresnet_test_queries.sql
+++ packages/ApiTests/queries/postgresnet_test_queries.sql
@@ -1,52 +1,59 @@
 --name: PostgresNetAll
---connection: PostgresNetNorthwind
+--connection: PostgresNetTest
+--meta.testExpectedRows: 830
 select * from orders;
 --end
 
 --name: PostgresNetByInt
---connection: PostgresNetNorthwind
---input: int orderid
+--connection: PostgresNetTest
+--input: int orderid = 10330
+--meta.testExpectedRows: 1
 select * from orders where orderid = @orderid;
 --end
 
 --name: PostgresNetByStringPatternInt
---connection: PostgresNetNorthwind
---input: string shipVia {pattern: int}
+--connection: PostgresNetTest
+--input: string shipVia = '>2' {pattern: int}
+--meta.testExpectedRows: 255
 SELECT * FROM orders WHERE @shipVia(shipVia);
 --end
 
 --name: PostgresNetByDouble
---connection: PostgresNetNorthwind
---input: double freight
+--connection: PostgresNetTest
+--input: double freight = 100.1
+--meta.testExpectedRows: 187
 SELECT * FROM orders WHERE freight >= @freight;
 --end
 
 --name: PostgresNetByStringChoices
---connection: PostgresNetNorthwind
---input: string shipCountry {choices: ["France", "Germany", "USA", "Finland"]}
+--connection: PostgresNetTest
+--input: string shipCountry = 'France' {choices: ["France", "Germany", "USA", "Finland"]}
+--meta.testExpectedRows: 77
 SELECT * FROM orders WHERE shipCountry = @shipCountry;
 --end
 
 --name: PostgresNetByStringPatternString
---connection: PostgresNetNorthwind
---input: string shipCity {pattern: string}
+--connection: PostgresNetTest
+--input: string shipCity = 'contains ran' {pattern: string}
+--meta.testExpectedRows: 33
 SELECT * FROM orders WHERE @shipCity(shipCity);
 --end
 
 --name: PostgresNetByBool
---connection: PostgresNetNorthwind
---input: bool freightLess100
+--connection: PostgresNetTest
+--input: bool freightLess100 = true
+--meta.testExpectedRows: 643
 SELECT * FROM orders WHERE ((freight < 100) OR NOT @freightLess100);
 --end
 
 --name: PostgresNetByDatetime
---connection: PostgresNetNorthwind
+--connection: PostgresNetTest
 --input: datetime requiredDate
 SELECT * FROM orders WHERE requiredDate >= @requiredDate;
 --end
 
 --name: PostgresNetByStringPatternDatetime
---connection: PostgresNetNorthwind
+--connection: PostgresNetTest
 --input: string orderDate {pattern: datetime}
 SELECT * FROM orders WHERE @orderDate(orderDate);
 --end
--- packages/ApiTests/queries/postgresql_test_queries.sql
+++ packages/ApiTests/queries/postgresql_test_queries.sql
@@ -1,52 +1,59 @@
 --name: PostgresqlAll
---connection: PostgreSQLNorthwind
+--connection: PostgreSQLTest
+--meta.testExpectedRows: 830
 select * from orders;
 --end
 
 --name: PostgresqlByInt
---connection: PostgreSQLNorthwind
---input: int orderid
+--connection: PostgreSQLTest
+--input: int orderid = 10330
+--meta.testExpectedRows: 1
 select * from orders where orderid = @orderid;
 --end
 
 --name: PostgresqlByStringPatternInt
---connection: PostgreSQLNorthwind
---input: string shipVia {pattern: int}
+--connection: PostgreSQLTest
+--input: string shipVia = '>2' {pattern: int}
+--meta.testExpectedRows: 255
 SELECT * FROM orders WHERE @shipVia(shipVia);
 --end
 
 --name: PostgresqlByDouble
---connection: PostgreSQLNorthwind
---input: double freight
+--connection: PostgreSQLTest
+--input: double freight = 100.1
+--meta.testExpectedRows: 187
 SELECT * FROM orders WHERE freight >= @freight;
 --end
 
 --name: PostgresqlByStringChoices
---connection: PostgreSQLNorthwind
---input: string shipCountry {choices: ["France", "Germany", "USA", "Finland"]}
+--connection: PostgreSQLTest
+--input: string shipCountry = 'France' {choices: ["France", "Germany", "USA", "Finland"]}
+--meta.testExpectedRows: 77
 SELECT * FROM orders WHERE shipCountry = @shipCountry;
 --end
 
 --name: PostgresqlByStringPatternString
---connection: PostgreSQLNorthwind
---input: string shipCity {pattern: string}
+--connection: PostgreSQLTest
+--input: string shipCity = 'contains ran' {pattern: string}
+--meta.testExpectedRows: 33
 SELECT * FROM orders WHERE @shipCity(shipCity);
 --end
 
 --name: PostgresqlByBool
---connection: PostgreSQLNorthwind
---input: bool freightLess100
+--connection: PostgreSQLTest
+--input: bool freightLess100 = true
+--meta.testExpectedRows: 643
 SELECT * FROM orders WHERE ((freight < 100) OR NOT @freightLess100);
 --end
 
 --name: PostgresqlByDatetime
---connection: PostgreSQLNorthwind
+--connection: PostgreSQLTest
 --input: datetime requiredDate
 SELECT * FROM orders WHERE requiredDate >= @requiredDate;
 --end
 
 --name: PostgresqlByStringPatternDatetime
---connection: PostgreSQLNorthwind
+--connection: PostgreSQLTest
 --input: string orderDate {pattern: datetime}
 SELECT * FROM orders WHERE @orderDate(orderDate);
 --end
