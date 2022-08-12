e834f6e5eb55f0afb0feab59cd6559e5053d7798
Yuliia <yulya.lavrinenko.2000@gmail.com>
2022-08-12 19:22:18 +0200

    update structure of demo packages

--- packages/Demo/projects/coffee-company/queries/stores-in-state-by-sales.sql
+++ packages/Demo/projects/coffee-company/queries/stores-in-state-by-sales.sql
@@ -1,5 +1,5 @@
 --name: stores in @state with sales greater than @sales
---input: string state = 'NY' 
+--input: string state
 --input: int sales
 select * from starbucks_us where state = @state and sales > @sales
 --end
