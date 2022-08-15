df834473d7a636f30c03077d687d639ba65629ad
Alex Paramonov <alex.aprm@gmail.com>
2022-08-15 12:31:03 +0600

    DataFrame hash test

--- packages/ApiTests/src/dataframe/dataframe.ts
+++ packages/ApiTests/src/dataframe/dataframe.ts
@@ -1,7 +1,8 @@
 import * as DG from 'datagrok-api/dg';
 import * as grok from 'datagrok-api/grok';
-import {category, expect, test} from '@datagrok-libraries/utils/src/test';
+import {category, expect, expectArray, test} from '@datagrok-libraries/utils/src/test';
 import dayjs from 'dayjs';
+import {hashDataFrame} from '@datagrok-libraries/utils/src/dataframe-utils';
 
 category('DataFrame', () => {
   const createDf = (): DG.DataFrame => {
@@ -295,5 +296,18 @@ Tesla, Model S,  ,          1.6,    120000`);
     expect(c.get(1), null);
     const v = grok.shell.addTableView(t);
     v.close();
+  });
+
+  test('hash', async () => {
+    const df = grok.data.demo.demog(100000);
+    expect(hashDataFrame(df).length, 32);
+
+    const df1 = DG.DataFrame.fromCsv(`a,b\n1,0\n2,0\n3,0`);
+    const df2 = DG.DataFrame.fromCsv(`a,b\n2,0\n1,0\n3,0`);
+    expectArray(hashDataFrame(df1), hashDataFrame(df2));
+
+    const df3 = DG.DataFrame.fromCsv(`a,b\n"abc",0\n"dce",0\n"xyz",0`);
+    const df4 = DG.DataFrame.fromCsv(`a,b\n"dce",0\n"abc",0\n"xyz",0`);
+    expectArray(hashDataFrame(df3), hashDataFrame(df4));
   });
 });
