c6b8dad13c771ace5fc9362479db58ee11fc20c7
Yuliia <yulya.lavrinenko.2000@gmail.com>
2022-08-15 13:56:25 +0200

    moved queries to ApiTests

--- packages/ApiTests/queries/postgresnet-test-queries.sql
+++ packages/ApiTests/queries/postgresnet-test-queries.sql
@@ -1,26 +1,29 @@
---name: MSSQLOrders
+--name: PostgresNetOrders
 --friendlyName: Orders
---connection: MSSQLNorthwind
+--connection: PostgresNetNorthwind
 --input: int employeeId = 5
---input: string shipVia = "=3" {pattern: int}
+--input: string shipVia = "= 3" {pattern: int}
 --input: double freight = 10.0
 --input: string shipCountry = "France" {choices: Query("SELECT DISTINCT shipCountry FROM Orders")}
 --input: string shipCity = "starts with r" {pattern: string}
 --input: bool freightLess1000 = true
 --input: datetime requiredDate = "1/1/1995"
 --input: string orderDate = "after 1/1/1995" {pattern: datetime}
+--meta.testExpectedRows: 1
 SELECT * FROM Orders WHERE (employeeId = @employeeId)
     AND (freight >= @freight)
     AND @shipVia(shipVia)
-    AND ((freight < 1000) OR @freightLess1000 = 0)
+    AND ((freight < 1000) OR NOT @freightLess1000)
     AND (shipCountry = @shipCountry)
     AND @shipCity(shipCity)
     AND @orderDate(orderDate)
     AND (requiredDate >= @requiredDate)
 --end
 
---name: MSSQLProducts
+--name: PostgresNetProducts
 --friendlyName: Products
---connection: MSSQLNorthwind
+--connection: PostgresNetNorthwind
+--input: int ProductID = "7"
+--meta.testExpectedRows: 1
 select * from Products
 --end
