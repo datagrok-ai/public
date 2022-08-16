ae5cab8354d6105b8eaff37ba558439afc853b1b
Oleksandra Serhiienko <59337516+Aleksashka11@users.noreply.github.com>
2022-08-16 13:25:20 +0300

    Helm: loading monomer libraries uploaded before

--- packages/Helm/src/package.ts
+++ packages/Helm/src/package.ts
@@ -17,15 +17,16 @@ const LIB_PATH = 'libraries/';
 
 //tags: init
 export async function initHelm(): Promise<void> {
+  let grid = grok.shell.tv.grid;
+  for (let j = 0; j <= i; j++) {
+    await monomerManager(await grok.dapi.userDataStorage.getValue(STORAGE_NAME, j.toString(), true));
+  }
+  grid.invalidate();
+  
   return new Promise((resolve, reject) => {
     // @ts-ignore
     dojo.ready(function() { resolve(null); });
   });
-  /*let grid = grok.shell.tv.grid;
-  for (let j = 0; j <= i; j++) {
-    await monomerManager(await grok.dapi.userDataStorage.getValue(STORAGE_NAME, j.toString(), true));
-  }
-  grid.invalidate();*/
 }
 
 //name: helmCellRenderer
