6011b105d2cf8ad55e7183a201cdc367da2a600a
Leonid <stolbovla@yandex.ru>
2022-08-16 19:24:42 +0300

    SimPKPD: garmonization

--- packages/SimPKPD/src/package.ts
+++ packages/SimPKPD/src/package.ts
@@ -2,19 +2,63 @@
 import * as grok from 'datagrok-api/grok';
 import * as ui from 'datagrok-api/ui';
 import * as DG from 'datagrok-api/dg';
-//import { ODEview } from './views/ode_view';
+import { ODEview } from './views/ode-view';
 
 export const _package = new DG.Package();
 
 //name: SimPKPD
 //tags: app
 export async function sim() {
-  grok.shell.info("Hello world");
+  const dosage = ui.floatInput('Dosage, um', 1000);
+  const doseInterval = ui.floatInput('Dose intrval, h', 12);
+  const compartments = ui.choiceInput('Model ', '2 compartment PK', ['2 compartment PK', '1 compartment PK']);
+  const clearance = ui.floatInput('Clearance', 2);
+  const rateConstant = ui.floatInput('Rate', 0.3);
+  const centralV = ui.floatInput('Central volume', 4);
+  const perV = ui.floatInput('Peripheral volume', 30);
+  const interRate = ui.floatInput('Intercompartmental rate', 1);
+  const effRate = ui.floatInput('Effective rate', 0.2);
+  const effect = ui.floatInput('EC50', 8);
 
-  let result = await grok.functions.call(
-    "Simpkpd:rxodeCommandReal", {
-    "inputSD": 1
+  const moleculeSvgDiv = ui.block([]);
+  
+  const v = grok.shell.newView('Sequence Translator', [
+    ui.panel([
+      ui.div([
+        ui.div([
+          ui.divH([ui.h1('Inputs')]),
+          ui.divV([
+            dosage,
+            doseInterval,
+            compartments,
+            clearance,
+            rateConstant,
+            centralV,
+            perV,
+            interRate,
+            effRate,
+            effect
+          ], 'ui-form'),
+        ], 'ui-form'),
+      ], 'ui-form'),
+      moleculeSvgDiv,
+    ])
+  ]);
+  v.box = true;
+
+  grok.functions.call(
+    "Simpkpd:pkpd", {
+    'dosage': dosage.value, 
+    'doseInterval': doseInterval.value,
+    'compartments': compartments.value,
+    'clearance': clearance.value, 
+    'rateConstant': rateConstant.value,
+    'centralV': centralV.value, 
+    'perV': perV.value,
+    'interRate': interRate.value,
+    'effRate': effRate.value,
+    'effect': effect.value
+  }).then(res => {
+    moleculeSvgDiv.append(res);
   });
-
-  const view = grok.shell.newView('Custom View', [ui.image(`data:image/png;base64,${result}`, 500, 500)]);
 }
---
+++ packages/SimPKPD/src/tests/simulation.ts
@@ -0,0 +1,13 @@
+import * as grok from 'datagrok-api/grok';
+import * as DG from 'datagrok-api/dg';
+
+import { category, test, expect, delay } from '@datagrok-libraries/utils/src/test';
+import { _package } from '../package-test';
+
+category('Simulation', () => {
+  test('Simulatiom', async () => {
+    expect(1, 1);
+
+
+  });
+});
