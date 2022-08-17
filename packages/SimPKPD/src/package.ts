20d1cf1b2f421688bea38fa425deaa3cc94d28fc
Leonid <stolbovla@yandex.ru>
2022-08-17 15:29:44 +0300

    SimPKPD: polishing and test

--- packages/SimPKPD/src/package.ts
+++ packages/SimPKPD/src/package.ts
@@ -9,18 +9,31 @@ export const _package = new DG.Package();
 //name: SimPKPD
 //tags: app
 export async function sim() {
-  const dosage = ui.floatInput('Dosage, um', 1000);
-  const doseInterval = ui.floatInput('Dose intrval, h', 12);
-  const compartments = ui.choiceInput('Model ', '2 compartment PK', ['2 compartment PK', '1 compartment PK']);
-  const clearance = ui.floatInput('Clearance', 2);
-  const rateConstant = ui.floatInput('Rate', 0.3);
-  const centralV = ui.floatInput('Central volume', 4);
-  const perV = ui.floatInput('Peripheral volume', 30);
-  const interRate = ui.floatInput('Intercompartmental rate', 1);
-  const effRate = ui.floatInput('Effective rate', 0.2);
-  const effect = ui.floatInput('EC50', 8);
+  const dosage = ui.floatInput('Dosage, um', 1000, () => recalculate());
+  const doseInterval = ui.floatInput('Dose intrval, h', 12, () => recalculate());
+  const compartments = ui.choiceInput('Model ', '2 compartment PK', ['2 compartment PK', '1 compartment PK'], () => recalculate());
+  const clearance = ui.floatInput('Clearance', 2, () => recalculate());
+  const rateConstant = ui.floatInput('Rate', 0.3, () => recalculate());
+  const centralV = ui.floatInput('Central volume', 4, () => recalculate());
+  const perV = ui.floatInput('Peripheral volume', 30, () => recalculate());
+  const interRate = ui.floatInput('Intercompartmental rate', 1, () => recalculate());
+  const effRate = ui.floatInput('Effective rate', 0.2, () => recalculate());
+  const effect = ui.floatInput('EC50', 8, () => recalculate());
 
-  const moleculeSvgDiv = ui.block([]);
+  let t = await simulate(dosage.value!, doseInterval.value!, compartments.value!, 
+  clearance.value!, rateConstant.value!, centralV.value!,
+  perV.value!, interRate.value!, effRate.value!, effect.value!);
+
+  let lc = DG.Viewer.lineChart(t);
+  
+  let recalculate = async (): Promise<void> => {
+    t = await simulate(
+      dosage.value!, doseInterval.value!, compartments.value!, 
+      clearance.value!, rateConstant.value!, centralV.value!,
+      perV.value!, interRate.value!, effRate.value!, effect.value!);
+    
+    lc.dataFrame = t;
+  } 
   
   const v = grok.shell.newView('Sequence Translator', [
     ui.panel([
@@ -41,24 +54,28 @@ export async function sim() {
           ], 'ui-form'),
         ], 'ui-form'),
       ], 'ui-form'),
-      moleculeSvgDiv,
     ])
   ]);
   v.box = true;
 
-  grok.functions.call(
+  v.append(lc);
+}
+
+export async function simulate(
+  dosage: number, doseInterval: number, compartments: string,
+  clearance: number, rateConstant: number, centralV: number,
+  perV: number, interRate: number, effRate: number, effect: number): Promise<DG.DataFrame> {
+  return await grok.functions.call(
     "Simpkpd:pkpd", {
-    'dosage': dosage.value, 
-    'doseInterval': doseInterval.value,
-    'compartments': compartments.value,
-    'clearance': clearance.value, 
-    'rateConstant': rateConstant.value,
-    'centralV': centralV.value, 
-    'perV': perV.value,
-    'interRate': interRate.value,
-    'effRate': effRate.value,
-    'effect': effect.value
-  }).then(res => {
-    moleculeSvgDiv.append(res);
+    'dosage': dosage, 
+    'doseInterval': doseInterval,
+    'compartments': compartments,
+    'clearance': clearance, 
+    'rateConstant': rateConstant,
+    'centralV': centralV, 
+    'perV': perV,
+    'interRate': interRate,
+    'effRate': effRate,
+    'effect': effect
   });
 }
