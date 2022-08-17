30def897feed610e9cfb0e7c1810c8af67120058
Leonid <stolbovla@yandex.ru>
2022-08-17 15:29:44 +0300

    SimPKPD: polishing and test

--- packages/SimPKPD/src/tests/simulation.ts
+++ packages/SimPKPD/src/tests/simulation.ts
@@ -3,11 +3,16 @@ import * as DG from 'datagrok-api/dg';
 
 import { category, test, expect, delay } from '@datagrok-libraries/utils/src/test';
 import { _package } from '../package-test';
+import { simulate } from '../package';
 
 category('Simulation', () => {
-  test('Simulatiom', async () => {
-    expect(1, 1);
+  test('Simulation', async () => {
 
+    const df = await simulate(1000, 12, '2 compartment PK', 2, 0.3, 4, 30, 1, 0.2, 8);
 
+    expect(df!.rowCount, 200);
+
+    if (df != null)
+      grok.shell.closeTable(df);
   });
 });
