7a899c48ac890936db40b73f3cad735eccc1f3bc
Maria Dolotova <mahouny85@mail.ru>
2022-08-22 18:42:26 +0300

    Chem: fixed tests

--- packages/Chem/src/tests/activity-cliffs-tests.ts
+++ packages/Chem/src/tests/activity-cliffs-tests.ts
@@ -11,33 +11,23 @@ import { _testActivityCliffsOpen } from './activity-cliffs-utils';
 
 category('activityCliffs', async () => {
 
-  let actCliffsTableView: DG.TableView;
-  let actCliffsDf: DG.DataFrame;
-  let actCliffsEmptyRowsTableView: DG.TableView;
-  let actCliffsEmptyRowsDf: DG.DataFrame
 
   before(async () => {
     if (!chemCommonRdKit.moduleInitialized) {
       chemCommonRdKit.setRdKitWebRoot(_package.webRoot);
       await chemCommonRdKit.initRdKitModuleLocal();
     }
-    actCliffsTableView = await createTableView('activity_cliffs.csv');
-    actCliffsDf = await readDataframe('activity_cliffs.csv');
-    actCliffsEmptyRowsTableView = await createTableView('tests/activity_cliffs_empty_rows.csv');
-    actCliffsEmptyRowsDf = await readDataframe('tests/activity_cliffs_empty_rows.csv');
   });
 
   test('activityCliffsOpen', async () => {
-    await _testActivityCliffsOpen(actCliffsDf, 92);
+    await _testActivityCliffsOpen('activity_cliffs.csv', 92);
   });
 
   test('activityCliffsWithEmptyRows', async () => {
-    await _testActivityCliffsOpen(actCliffsEmptyRowsDf, 91);
+    await _testActivityCliffsOpen('tests/activity_cliffs_empty_rows.csv', 91);
   });
 
   after(async () => {
-    actCliffsTableView.close();
-    actCliffsEmptyRowsTableView.close();
   });
 
 });
--- packages/Chem/src/tests/activity-cliffs-utils.ts
+++ packages/Chem/src/tests/activity-cliffs-utils.ts
@@ -2,13 +2,16 @@ import * as DG from 'datagrok-api/dg';
 import {delay, expect} from '@datagrok-libraries/utils/src/test';
 import {_package} from '../package-test';
 import { activityCliffs } from '../package';
+import { createTableView, readDataframe } from './utils';
 
 
-export async function _testActivityCliffsOpen(df: DG.DataFrame, numberCliffs: number) {
+export async function _testActivityCliffsOpen(dfName: string, numberCliffs: number) {
+
+  const actCliffsTableView = await createTableView(dfName);
    const scatterPlot = await activityCliffs(
-     df, 
-     df.col('smiles')!, 
-     df.col('Activity')!, 
+    actCliffsTableView.dataFrame, 
+    actCliffsTableView.dataFrame.col('smiles')!, 
+    actCliffsTableView.dataFrame.col('Activity')!, 
      80, 
      't-SNE');
 
@@ -16,4 +19,5 @@ export async function _testActivityCliffsOpen(df: DG.DataFrame, numberCliffs: nu
 
     const cliffsLink = Array.from(scatterPlot.root.children).filter(it => it.className === 'ui-btn ui-btn-ok');
     expect((cliffsLink[0] as HTMLElement).innerText, `${numberCliffs} cliffs`);
+    actCliffsTableView.close();
 }
