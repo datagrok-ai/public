cba0f8a5f3a24d52a1c2d5473ce861d8a15f5461
Dmitry Illarionov <illarionow@yahoo.com>
2022-08-22 15:10:06 +0200

    update UsageAnalysis UI

--- packages/UsageAnalysis/src/viewers/abstract/ua-query-viewer.ts
+++ packages/UsageAnalysis/src/viewers/abstract/ua-query-viewer.ts
@@ -1,10 +1,10 @@
 import * as ui from "datagrok-api/ui";
-import {UaFilter} from "../../filter2";
+import { UaFilter } from "../../filter2";
 import * as grok from "datagrok-api/grok";
-import {UaViewer} from "./ua-viewer";
+import { UaViewer } from "./ua-viewer";
 import * as DG from "datagrok-api/dg";
 
-export abstract class UaQueryViewer extends UaViewer{
+export abstract class UaQueryViewer extends UaViewer {
 
   queryName: string;
   viewerFunction: Function;
@@ -12,7 +12,7 @@ export abstract class UaQueryViewer extends UaViewer{
   filter: Object = {};
 
   protected constructor(name: string, queryName: string, viewerFunction: Function,
-                        setStyle?: Function | null, staticFilter?: Object | null, filter?: UaFilter | null, showName: boolean = true) {
+    setStyle?: Function | null, staticFilter?: Object | null, filter?: UaFilter | null, showName: boolean = true) {
     super(name, setStyle, showName);
 
     this.queryName = queryName;
@@ -27,7 +27,7 @@ export abstract class UaQueryViewer extends UaViewer{
   }
 
   setViewer(loader: any, host: HTMLDivElement, nameDiv: HTMLElement) {
-    let filter = {...this.filter, ...this.staticFilter}
+    let filter = { ...this.filter, ...this.staticFilter }
 
     grok.data.query('UsageAnalysis:' + this.queryName, filter).then((dataFrame) => {
       if (dataFrame.columns.byName('count') != null)
@@ -37,7 +37,7 @@ export abstract class UaQueryViewer extends UaViewer{
       if (dataFrame.rowCount > 0)
         viewer = this.viewerFunction(dataFrame);
       else
-        viewer = ui.divText('No data', {style:{color:'var(--red-3)', paddingBottom:'25px'}})
+        viewer = ui.divText('No data', { style: { color: 'var(--red-3)', paddingBottom: '25px' } })
 
       let grid = DG.Viewer.grid(dataFrame);
       grid.props.allowColSelection = false;
@@ -52,26 +52,48 @@ export abstract class UaQueryViewer extends UaViewer{
         raw = !raw;
       });
 
+      tableIcon.style.display = 'none';
       tableIcon.style.padding = '3px';
       tableIcon.style.margin = '0 3px';
       tableIcon.style.color = 'var(--grey-4)';
 
-      tableIcon.addEventListener("mouseover",function(){tableIcon.style.color = 'var(--blue-1)'});
-      tableIcon.addEventListener("mouseleave",function(){tableIcon.style.color = 'var(--grey-4)'});
-
-      
+      tableIcon.addEventListener("mouseover", function () { tableIcon.style.color = 'var(--blue-1)' });
+      tableIcon.addEventListener("mouseleave", function () { tableIcon.style.color = 'var(--grey-4)' });
 
       nameDiv.append(ui.tooltip.bind(tableIcon, 'Show grid'));
 
+      let tableViewIcon = ui.iconFA('external-link-square', () => {
+        grok.shell.v = grok.shell.addTableView(dataFrame);
+      });
+
+      tableViewIcon.style.display = 'none';
+      tableViewIcon.style.padding = '3px';
+      tableViewIcon.style.margin = '0 3px';
+      tableViewIcon.style.color = 'var(--grey-4)';
+
+      tableViewIcon.addEventListener("mouseover", function () { tableViewIcon.style.color = 'var(--blue-1)' });
+      tableViewIcon.addEventListener("mouseleave", function () { tableViewIcon.style.color = 'var(--grey-4)' });
+
+      nameDiv.append(ui.tooltip.bind(tableViewIcon, 'Open grid'));
+
       host.appendChild(viewer);
       host.removeChild(loader);
+
+      host.addEventListener("mouseover", function () {
+        tableIcon.style.display = "block";
+        tableViewIcon.style.display = "block";
+      });
+      host.addEventListener("mouseleave", function () {
+        tableIcon.style.display = "none";
+        tableViewIcon.style.display = "none";
+      });
     });
   }
 
   reload(filter: UaFilter) {
   };
 
-  init() : void {
+  init(): void {
   }
 
 }
