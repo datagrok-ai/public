b6e4b642ae4c7460bdf0c923915a67daab230051
onuf <56073962+onuf@users.noreply.github.com>
2022-08-27 19:45:13 +0200

    Resolves #986 datagrok-tools: grok add tests

--- tools/bin/commands/add.ts
+++ tools/bin/commands/add.ts
@@ -17,6 +17,7 @@ export function add(args: { _: string[] }) {
   const jsPath = path.join(srcDir, 'package.js');
   const tsPath = path.join(srcDir, 'package.ts');
   const detectorsPath = path.join(curDir, 'detectors.js');
+  const webpackConfigPath = path.join(curDir, 'webpack.config.js');
   const scriptsDir = path.join(curDir, 'scripts');
   const queryDir = path.join(curDir, 'queries');
   const queryPath = path.join(queryDir, 'queries.sql');
@@ -292,6 +293,61 @@ export function add(args: { _: string[] }) {
 
       fs.writeFileSync(detectorsPath, contents, 'utf8');
       console.log(help.detector(name));
+      break;
+    case 'tests':
+      if (!fs.existsSync(webpackConfigPath) || !fs.existsSync(tsPath))
+        return false;
+
+      const config = fs.readFileSync(webpackConfigPath, 'utf8');
+      if (!/(?<=entry:\s*{\s*(\r\n|\r|\n))[^}]*test:/.test(config)) {
+        const entryIdx = config.search(/(?<=entry:\s*{\s*(\r\n|\r|\n)).*/);
+        if (entryIdx === -1)
+          return color.error('Entry point not found during config parsing');
+  
+        const testEntry = "    test: {filename: 'package-test.js', library: " +
+          "{type: 'var', name:`${packageName}_test`}, import: './src/package-test.ts'},\n";
+        fs.writeFileSync(webpackConfigPath, config.slice(0, entryIdx) + testEntry +
+          config.slice(entryIdx), 'utf8');
+      }
+
+      const packageObj = JSON.parse(fs.readFileSync(packagePath, 'utf8'));
+      Object.assign(packageObj.devDependencies, {
+        'jest-html-reporter': '^3.5.0',
+        'jest': '^27.0.0',
+        '@types/jest': '^27.0.0',
+        'js-yaml': '^4.1.0',
+        '@types/js-yaml': "^4.0.5",
+        '@types/node-fetch': '^2.6.2',
+        'node-fetch': '^2.6.7'
+      }, ts ? {
+        'ts-jest': '^27.0.0',
+        'puppeteer': '^13.7.0'
+      } : {});
+      Object.assign(packageObj.scripts, {
+        'test': 'jest',
+      });
+      fs.writeFileSync(packagePath, JSON.stringify(packageObj, null, 2), 'utf8');
+
+      if (!fs.existsSync(path.join(curDir, 'jest.config.js')))
+        fs.writeFileSync(path.join(curDir, 'jest.config.js'), fs.readFileSync(
+          path.join(path.dirname(path.dirname(__dirname)), 'package-template',
+          'jest.config.js')));
+
+      if (!fs.existsSync(path.join(srcDir, '__jest__', 'remote.test.ts')))
+        fs.writeFileSync(path.join(srcDir, '__jest__', 'remote.test.ts'),
+          fs.readFileSync(path.join(path.dirname(path.dirname(__dirname)),
+            'package-template', 'src', '__jest__', 'remote.test.ts')));
+
+      if (!fs.existsSync(path.join(srcDir, '__jest__', 'test-node.ts')))
+        fs.writeFileSync(path.join(srcDir, '__jest__', 'test-node.ts'),
+          fs.readFileSync(path.join(path.dirname(path.dirname(__dirname)),
+            'package-template', 'src', '__jest__', 'test-node.ts')));
+
+      if (!fs.existsSync(path.join(srcDir, 'package-test.ts')))
+        fs.writeFileSync(path.join(srcDir, 'package-test.ts'),
+          fs.readFileSync(path.join(path.dirname(path.dirname(__dirname)),
+            'package-template', 'src', 'package-test.ts')));
+      console.log('Run `npm install` to get newly added packages');
       break;
     default:
       return false;
