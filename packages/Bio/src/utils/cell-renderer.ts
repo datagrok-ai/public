e7fb6343a7ac93b928bf6ad9cca9e1d16ead60b2
simpleprofi <simpleprofi@gmail.com>
2022-08-29 10:38:12 +0300

    Bio: Optimized renderer, added sampling

--- packages/Bio/src/utils/cell-renderer.ts
+++ packages/Bio/src/utils/cell-renderer.ts
@@ -7,11 +7,9 @@ import {SplitterFunc, WebLogo} from '@datagrok-libraries/bio/src/viewers/web-log
 import {SeqPalette} from '@datagrok-libraries/bio/src/seq-palettes';
 import * as ui from 'datagrok-api/ui';
 import {printLeftOrCentered} from '@datagrok-libraries/bio/src/utils/cell-renderer';
-import {GridCell} from 'datagrok-api/dg';
 
 const undefinedColor = 'rgb(100,100,100)';
 const monomerToShortFunction: (amino: string, maxLengthOfMonomer: number) => string = WebLogo.monomerToShort;
-const grayColor = '#808080';
 const gapRenderer = 5;
 
 function getPalleteByType(paletteType: string): SeqPalette {
@@ -45,81 +43,6 @@ export function processSequence(subParts: string[]): [string[], boolean] {
     text.push(amino);
   });
   return [text, simplified];
-}
-
-
-/**
- * A function that prints a string aligned to left or centered.
- *
- * @param {number} x x coordinate.
- * @param {number} y y coordinate.
- * @param {number} w Width.
- * @param {number} h Height.
- * @param {CanvasRenderingContext2D} g Canvas rendering context.
- * @param {string} s String to print.
- * @param {string} [color=undefinedColor] String color.
- * @param {number} [pivot=0] Pirvot.
- * @param {boolean} [left=false] Is left aligned.
- * @param {number} [transparencyRate=0.0] Transparency rate where 1.0 is fully transparent
- * @param {string} [separator=''] Is separator for sequence.
- * @param {boolean} [last=false] Is checker if element last or not.
- * @return {number} x coordinate to start printing at.
- */
-export function printLeftOrCentered(
-  x: number, y: number, w: number, h: number,
-  g: CanvasRenderingContext2D, s: string, color = undefinedColor,
-  pivot: number = 0, left = false, transparencyRate: number = 1.0,
-  separator: string = '', last: boolean = false, drawStyle: string = 'classic', maxWord: any = {}, maxWordIdx: number = 0, gridCell: any = {}): number {
-  g.textAlign = 'start';
-  const colorPart = s.substring(0);
-  let grayPart = last ? '' : separator;
-  if (drawStyle === 'msa') {
-    grayPart = '';
-  }
-
-  let textSize: any = g.measureText(colorPart + grayPart);
-  const indent = 5;
-
-  let maxColorTextSize = g.measureText(colorPart).width;
-  let colorTextSize = g.measureText(colorPart).width;
-  const dy = (textSize.fontBoundingBoxAscent + textSize.fontBoundingBoxDescent) / 2;
-  textSize = textSize.width;
-  if (drawStyle === 'msa') {
-    maxColorTextSize = maxWord[maxWordIdx];
-    textSize = maxWord[maxWordIdx];
-    if (maxColorTextSize > maxWord) {
-      maxWord[maxWordIdx] = maxColorTextSize;
-      gridCell.cell.column.temp = maxWord;
-    }
-    if (maxWordIdx > (maxWord['bio-maxIndex'] ?? 0)) {
-      maxWord['bio-maxIndex'] = maxWordIdx;
-      gridCell.cell.column.temp = maxWord;
-    }
-  }
-
-  function draw(dx1: number, dx2: number): void {
-    g.fillStyle = color;
-    g.globalAlpha = transparencyRate;
-    if (drawStyle === 'classic') {
-      g.fillText(colorPart, x + dx1, y + dy);
-      g.fillStyle = grayColor;
-      g.fillText(grayPart, x + dx2, y + dy);
-    }
-    if (drawStyle === 'msa') {
-      g.fillStyle = color;
-      g.fillText(colorPart, x + dx1 + ((maxWord[maxWordIdx] - colorTextSize) / 2), y + dy);
-    }
-  }
-
-  if (left || textSize > w) {
-    draw(indent, indent + maxColorTextSize);
-    return x + maxColorTextSize + g.measureText(grayPart).width;
-
-  } else {
-    const dx = (w - textSize) / 2;
-    draw(dx, dx + maxColorTextSize);
-    return x + dx + maxColorTextSize;
-  }
 }
 
 
@@ -256,7 +179,7 @@ export class MacromoleculeSequenceCellRenderer extends DG.GridCellRenderer {
       color = palette.get(amino);
       g.fillStyle = undefinedColor;
       let last = index === subParts.length - 1;
-      x1 = printLeftOrCentered(x1, y, w, h, g, amino, color, 0, true, 1.0, separator, last, drawStyle, maxLengthWords, index, gridCell);
+      x1 = printLeftOrCentered(x1, y, w, h, g, monomerToShortFunction(amino, maxLengthOfMonomer), color, 0, true, 1.0, separator, last, drawStyle, maxLengthWords, index, gridCell);
     });
 
     g.restore();
