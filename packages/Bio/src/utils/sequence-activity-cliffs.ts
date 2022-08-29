55e8fbd0dcedc285c7910845af1955fae90720ef
Maria Dolotova <mahouny85@mail.ru>
2022-08-29 07:48:09 +0300

    Bio: activity cliffs property panel, updaed libraries/ml dependency, bump version

--- packages/Bio/src/utils/sequence-activity-cliffs.ts
+++ packages/Bio/src/utils/sequence-activity-cliffs.ts
@@ -1,4 +1,4 @@
-import {IDrawTooltipParams} from '@datagrok-libraries/ml/src/viewers/activity-cliffs';
+import {ITooltipAndPanelParams} from '@datagrok-libraries/ml/src/viewers/activity-cliffs';
 import * as DG from 'datagrok-api/dg';
 import * as ui from 'datagrok-api/ui';
 import {getSimilarityFromDistance} from '@datagrok-libraries/utils/src/similarity-metrics';
@@ -12,25 +12,9 @@ export async function sequenceGetSimilarities(col: DG.Column, seq: string): Prom
   return DG.Column.fromList(DG.COLUMN_TYPE.FLOAT, 'distances', distances);
 }
 
-export function drawTooltip(params: IDrawTooltipParams) {
-  params.tooltips[params.line.id] = ui.divH([]);
-  const columnNames = ui.divV([
-    ui.divText('sequense'),
-    ui.divText(params.activity.name),
-  ]);
-  columnNames.style.fontWeight = 'bold';
-  columnNames.style.display = 'flex';
-  columnNames.style.justifyContent = 'space-between';
-  params.tooltips[params.line.id].append(columnNames);
-  params.line.mols.forEach((mol: number) => {
-    const seq = ui.divText(params.df.get(params.seqCol.name, mol));
-    const activity = ui.divText(params.df.get(params.activity.name, mol).toFixed(2));
-    activity.style.display = 'flex';
-    activity.style.justifyContent = 'left';
-    activity.style.paddingLeft = '30px';
-    params.tooltips[params.line.id].append(ui.divV([
-      seq,
-      activity,
-    ], {style: {paddingLeft: '5px'}}));
+export function drawSequences(params: ITooltipAndPanelParams) {
+  params.line.mols.forEach((mol: number, index: number) => {
+    ui.empty(params.hosts[index]);
+    params.hosts[index].append(ui.divText(params.seqCol.get(mol)));
   });
 }
