cd00e790bd5552d8c87c9db7b78ab6ea47748163
Leonid <stolbovla@yandex.ru>
2022-08-30 20:49:13 +0300

    Chem: small fixes

--- packages/Chem/scripts/smiles_to_3d_coordinates.py
+++ packages/Chem/scripts/smiles_to_3d_coordinates.py
@@ -1,11 +1,11 @@
 #name: SmilesTo3DCoordinates
 #language: python
-#input: string smiles
+#input: string molecule
 #output: string sdf
 
 from rdkit.Chem import AllChem
 from rdkit import Chem
 
-mol = Chem.MolFromSmiles(smiles)
+mol = Chem.MolFromSmiles(molecule)
 AllChem.EmbedMolecule(mol, AllChem.ETKDG())
 sdf = Chem.MolToMolBlock(mol)
