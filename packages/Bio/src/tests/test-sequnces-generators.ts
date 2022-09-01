8f37330bcd9383b1b1ca6627205589d18ea5632a
simpleprofi <simpleprofi@gmail.com>
2022-09-01 10:22:12 +0300

    Bio #999: speed up cell renderer (render only visible area)

--- packages/Bio/src/tests/test-sequnces-generators.ts
+++ packages/Bio/src/tests/test-sequnces-generators.ts
@@ -9,13 +9,12 @@ meI/hHis/Aca/N/T/dE/Thr_PO3H2/Aca/D-Tyr_Et/Tyr_ab-dehydroMe/dV/E/N/D-Orn/D-aThr/
 
 export function generateLongSequence(): string {
   let longSequence = `meI/hHis/Aca/N/T/dE/Thr_PO3H2/Aca/D-Tyr_Et/Tyr_ab-dehydroMe/dV/E/N/D-Orn/D-aThr`;
-  for (let i = 0; i < 10 ** 6; i++) {
+  for (let i = 0; i < 10 ** 5; i++) {
     longSequence += `/Aca/N/T/dE/Thr_PO3H2/Aca/D-Tyr_Et/Tyr_ab-dehydroMe/dV/dv`;
   }
   longSequence += `//Phe_4Me,5.30751`;
-  let csvData = `MSA,Activity
-  \n ${longSequence}`;
-  for (let i = 0; i <= 10 ** 2; i++) {
+  let csvData = `MSA,Activity `;
+  for (let i = 0; i <= 10 ** 1 * 4; i++) {
     csvData += `\n ${longSequence}`;
   }
   return csvData;
