f89825d96ac7e70283fe871e4ec5526146b4ff1f
Leonid <stolbovla@yandex.ru>
2022-09-02 15:31:38 +0300

    Chem: #978 curate, mutate tests

--- packages/Chem/src/tests/menu-tests-script-based.ts
+++ packages/Chem/src/tests/menu-tests-script-based.ts
@@ -8,10 +8,11 @@ import {_testSearchSubstructure,
   loadFileAsText} from './utils';
 
 import {_importSdf} from '../open-chem/sdf-importer';
+import { SEMTYPE } from 'datagrok-api/dg';
 
 category('top menu script based', () => {
   test('curate', async () => {
-    const t = DG.DataFrame.fromCsv(`Name,smiles
+    let t = DG.DataFrame.fromCsv(`Name,smiles
     metal_non,CCC(=O)O[Na]
     metal_st,CCC(=O)[O-].[Na+]
     parent_non,[Na]OC(=O)c1ccccc1
@@ -28,9 +29,37 @@ category('top menu script based', () => {
     main_component_non_st,CCC1=C(C)C=CC(O)=N1`);
     const v = grok.shell.addTableView(t);
 
-    await grok.functions.call('Chem:CurateChemStructures', {'data': t, 'molecules': 'smiles',
+    t = await grok.functions.call('Chem:CurateChemStructures', {'data': t, 'molecules': 'smiles',
       'kekulization': false, 'normalization': true, 'reionization': true,
       'neutralization': true, 'tautomerization': true, 'mainFragment': true});
     v.close();
+    expect(t.getCol('curated_molecule').get(0), 'CCC(=O)[O-]');
+    expect(t.getCol('curated_molecule').get(1), 'CCC(=O)O');
+    expect(t.getCol('curated_molecule').get(2), 'O=C([O-])c1ccccc1');
+    expect(t.getCol('curated_molecule').get(3), 'O=C(O)c1ccccc1');
+    expect(t.getCol('curated_molecule').get(4), 'CN(C)C=CC=O');
+    expect(t.getCol('curated_molecule').get(5), 'CN(C)C=CC=O');
+    expect(t.getCol('curated_molecule').get(6), 'O=S(O)c1ccc(S(=O)(=O)O)cc1');
+    expect(t.getCol('curated_molecule').get(7), 'O=S(O)c1ccc(S(=O)(=O)O)cc1');
+    expect(t.getCol('curated_molecule').get(8), 'O=C(O)c1ccccc1');
+    expect(t.getCol('curated_molecule').get(9), 'O=C(O)c1ccccc1');
+    expect(t.getCol('curated_molecule').get(10), 'O=C1CCCCC1');
+    expect(t.getCol('curated_molecule').get(11), 'O=C1CCCCC1');
+    expect(t.getCol('curated_molecule').get(12), 'CCc1[nH]c(=O)ccc1C');
+    expect(t.getCol('curated_molecule').get(13), 'CCc1[nH]c(=O)ccc1C');
   });
+
+  test('mutate', async () => {
+    const mutations = 100;
+    const t: DG.DataFrame = await grok.functions.call('Chem:Mutate', {
+      'smiles': 'CN1C(CC(O)C1=O)C1=CN=CC=C1', 
+      'steps': 1,
+      'randomize': true, 
+      'maxRandomResults': mutations
+    });
+
+    expect(t.rowCount, mutations);
+    expect(t.getCol('mutations').semType, SEMTYPE.MOLECULE);
+  });
+
 });
