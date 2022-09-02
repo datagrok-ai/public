ce481631b02174e8e9bcdacc76736abb24ae7f65
simpleprofi <simpleprofi@gmail.com>
2022-09-02 13:29:22 +0300

    Bio, cell renderer: add enums for DrawStyle

--- libraries/bio/src/utils/cell-renderer.ts
+++ libraries/bio/src/utils/cell-renderer.ts
@@ -1,6 +1,11 @@
 const undefinedColor = 'rgb(100,100,100)';
 const grayColor = '#808080';
 
+export enum DrawStyle {
+  MSA = 'MSA',
+  classic = 'classic',
+}
+
 /**
  * A function that prints a string aligned to left or centered.
  *
@@ -18,56 +23,56 @@ const grayColor = '#808080';
  * @param {boolean} [last=false] Is checker if element last or not.
  * @return {number} x coordinate to start printing at.
  */
- export function printLeftOrCentered(
-    x: number, y: number, w: number, h: number,
-    g: CanvasRenderingContext2D, s: string, color = undefinedColor,
-    pivot: number = 0, left = false, transparencyRate: number = 1.0,
-    separator: string = '', last: boolean = false, drawStyle: string = 'classic', maxWord: any = {}, maxWordIdx: number = 0, gridCell: any = {}): number {
-    g.textAlign = 'start';
-    const colorPart = s.substring(0);
-    let grayPart = last ? '' : separator;
-    if (drawStyle === 'msa') {
-      grayPart = '';
-    }
+export function printLeftOrCentered(
+  x: number, y: number, w: number, h: number,
+  g: CanvasRenderingContext2D, s: string, color = undefinedColor,
+  pivot: number = 0, left = false, transparencyRate: number = 1.0,
+  separator: string = '', last: boolean = false, drawStyle: string = 'classic', maxWord: any = {}, maxWordIdx: number = 0, gridCell: any = {}): number {
+  g.textAlign = 'start';
+  const colorPart = s.substring(0);
+  let grayPart = last ? '' : separator;
+  if (drawStyle === DrawStyle.MSA) {
+    grayPart = '';
+  }
 
-    let textSize: any = g.measureText(colorPart + grayPart);
-    const indent = 5;
+  let textSize: any = g.measureText(colorPart + grayPart);
+  const indent = 5;
 
-    let maxColorTextSize = g.measureText(colorPart).width;
-    let colorTextSize = g.measureText(colorPart).width;
-    const dy = (textSize.fontBoundingBoxAscent + textSize.fontBoundingBoxDescent) / 2;
-    textSize = textSize.width;
-    if (drawStyle === 'msa') {
-      maxColorTextSize = maxWord[maxWordIdx];
-      textSize = maxWord[maxWordIdx];
-      if (maxWordIdx > (maxWord['bio-maxIndex'] ?? 0)) {
-        maxWord['bio-maxIndex'] = maxWordIdx;
-        gridCell.cell.column.temp = maxWord;
-      }
-    }
-
-    function draw(dx1: number, dx2: number): void {
-      g.fillStyle = color;
-      g.globalAlpha = transparencyRate;
-      if (drawStyle === 'classic') {
-        g.fillText(colorPart, x + dx1, y + dy);
-        g.fillStyle = grayColor;
-        g.fillText(grayPart, x + dx2, y + dy);
-      }
-      if (drawStyle === 'msa') {
-        g.fillStyle = color;
-        g.fillText(colorPart, x + dx1 + ((maxWord[maxWordIdx] - colorTextSize) / 2), y + dy);
-      }
-    }
-
-    if (left || textSize > w) {
-      draw(indent, indent + maxColorTextSize);
-      return x + maxColorTextSize + g.measureText(grayPart).width;
-
-    } else {
-      const dx = (w - textSize) / 2;
-      draw(dx, dx + maxColorTextSize);
-      return x + dx + maxColorTextSize;
+  let maxColorTextSize = g.measureText(colorPart).width;
+  let colorTextSize = g.measureText(colorPart).width;
+  const dy = (textSize.fontBoundingBoxAscent + textSize.fontBoundingBoxDescent) / 2;
+  textSize = textSize.width;
+  if (drawStyle === DrawStyle.MSA) {
+    maxColorTextSize = maxWord[maxWordIdx];
+    textSize = maxWord[maxWordIdx];
+    if (maxWordIdx > (maxWord['bio-maxIndex'] ?? 0)) {
+      maxWord['bio-maxIndex'] = maxWordIdx;
+      gridCell.cell.column.temp = maxWord;
     }
   }
+
+  function draw(dx1: number, dx2: number): void {
+    g.fillStyle = color;
+    g.globalAlpha = transparencyRate;
+    if (drawStyle === DrawStyle.classic) {
+      g.fillText(colorPart, x + dx1, y + dy);
+      g.fillStyle = grayColor;
+      g.fillText(grayPart, x + dx2, y + dy);
+    }
+    if (drawStyle === DrawStyle.MSA) {
+      g.fillStyle = color;
+      g.fillText(colorPart, x + dx1 + ((maxWord[maxWordIdx] - colorTextSize) / 2), y + dy);
+    }
+  }
+
+  if (left || textSize > w) {
+    draw(indent, indent + maxColorTextSize);
+    return x + maxColorTextSize + g.measureText(grayPart).width;
+
+  } else {
+    const dx = (w - textSize) / 2;
+    draw(dx, dx + maxColorTextSize);
+    return x + dx + maxColorTextSize;
+  }
+}
 
