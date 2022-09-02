71edd1e6c3b69452783192bc482f78488471be9c
Oleksandra Serhiienko <59337516+Aleksashka11@users.noreply.github.com>
2022-09-02 10:11:54 +0300

    Chem: Issue #976 Chem: elemental analysis code refactoring WIP

--- packages/Chem/src/package.ts
+++ packages/Chem/src/package.ts
@@ -356,56 +356,54 @@ export function addInchisKeysPanel(col: DG.Column): void {
   addInchiKeys(col);
 }
 
-//name: getAtoms
-//input: string molfile
-export function getAtoms(molfile: string) {
-  let elemental_table = {"R" : 0, "H" : 0, "He" : 0, "Li" : 0, "Be" : 0, "B" : 0, "C" : 0,
-  	                     "N" : 0, "O" : 0, "F" : 0, "Ne" : 0, "Na" : 0, "Mg" : 0, "Al" : 0,
-                         "Si" : 0, "P" : 0, "S" : 0, "Cl" : 0, "Ar" : 0, "K" : 0, "Ca" : 0,
-  	                     "Sc" : 0, "Ti" : 0, "V" : 0, "Cr" : 0, "Mn" : 0, "Fe" : 0, "Co" : 0,
-  	                     "Ni" : 0, "Cu" : 0, "Zn" : 0, "Ga" : 0, "Ge" : 0, "As" : 0, "Se" : 0,
-  	                     "Br" : 0, "Kr" : 0, "Rb" : 0, "Sr" : 0, "Y" : 0, "Zr" : 0, "Nb" : 0,
-  	                     "Mo" : 0, "Tc" : 0, "Ru" : 0, "Rh" : 0, "Pd" : 0, "Ag" : 0, "Cd" : 0,
-  	                     "In" : 0, "Sn" : 0, "Sb" : 0, "Te" : 0, "I" : 0, "Xe" : 0, "Cs" : 0,
-  	                     "Ba" : 0, "La" : 0, "Ce" : 0, "Pr" : 0, "Nd" : 0, "Pm" : 0, "Sm" : 0,
-  	                     "Eu" : 0, "Gd" : 0, "Tb" : 0, "Dy" : 0, "Ho" : 0, "Er" : 0, "Tm" : 0,
-  	                     "Yb" : 0, "Lu" : 0, "Hf" : 0, "Ta" : 0, "W" : 0, "Re" : 0, "Os" : 0,
-  	                     "Ir" : 0, "Pt" : 0, "Au" : 0, "Hg" : 0, "Tl" : 0, "Pb" : 0, "Bi" : 0,
-  	                     "Po" : 0, "At" : 0, "Rn" : 0, "Fr" : 0, "Ra" : 0, "Ac" : 0, "Th" : 0,
-  	                     "Pa" : 0, "U" : 0, "Np" : 0, "Pu" : 0, "Am" : 0, "Cm" : 0, "Bk" : 0,
-  	                     "Cf" : 0, "Es" : 0, "Fm" : 0, "Md" : 0, "No" : 0, "Lr" : 0, "Rf" : 0,
-  	                     "Db" : 0, "Sg" : 0, "Bh" : 0, "Hs" : 0, "Mt" : 0, "Ds" : 0, "Rg" : 0,
-  	                     "Cn" : 0, "Nh" : 0, "Fl" : 0, "Mc" : 0, "Lv" : 0, "Ts" : 0, "Og" : 0};
-  let rows = molfile.split('\n');
-  let atom_counts = rows[3].split(' ')[1];
-  for (let i = 4; i < parseInt(atom_counts) + 4; i++) {
-    let element = rows[i].match(/[a-zA-Z]+/g)?.toString();
-    //@ts-ignore
-    elemental_table[element] += 1;
+//name: getAtomsColumn
+//input: column molecule { semType: Molecule }
+export function getAtomsColumn(molecule: DG.Column) {
+  let elemental_table = {"R" : [], "H" : [], "He" : [], "Li" : [], "Be" : [], "B" : [], "C" : [],
+  	                     "N" : [], "O" : [], "F" : [], "Ne" : [], "Na" : [], "Mg" : [], "Al" : [],
+                         "Si" : [], "P" : [], "S" : [], "Cl" : [], "Ar" : [], "K" : [], "Ca" : [],
+  	                     "Sc" : [], "Ti" : [], "V" : [], "Cr" : [], "Mn" : [], "Fe" : [], "Co" : [],
+  	                     "Ni" : [], "Cu" : [], "Zn" : [], "Ga" : [], "Ge" : [], "As" : [], "Se" : [],
+  	                     "Br" : [], "Kr" : [], "Rb" : [], "Sr" : [], "Y" : [], "Zr" : [], "Nb" : [],
+  	                     "Mo" : [], "Tc" : [], "Ru" : [], "Rh" : [], "Pd" : [], "Ag" : [], "Cd" : [],
+  	                     "In" : [], "Sn" : [], "Sb" : [], "Te" : [], "I" : [], "Xe" : [], "Cs" : [],
+  	                     "Ba" : [], "La" : [], "Ce" : [], "Pr" : [], "Nd" : [], "Pm" : [], "Sm" : [],
+  	                     "Eu" : [], "Gd" : [], "Tb" : [], "Dy" : [], "Ho" : [], "Er" : [], "Tm" : [],
+  	                     "Yb" : [], "Lu" : [], "Hf" : [], "Ta" : [], "W" : [], "Re" : [], "Os" : [],
+  	                     "Ir" : [], "Pt" : [], "Au" : [], "Hg" : [], "Tl" : [], "Pb" : [], "Bi" : [],
+  	                     "Po" : [], "At" : [], "Rn" : [], "Fr" : [], "Ra" : [], "Ac" : [], "Th" : [],
+  	                     "Pa" : [], "U" : [], "Np" : [], "Pu" : [], "Am" : [], "Cm" : [], "Bk" : [],
+  	                     "Cf" : [], "Es" : [], "Fm" : [], "Md" : [], "No" : [], "Lr" : [], "Rf" : [],
+  	                     "Db" : [], "Sg" : [], "Bh" : [], "Hs" : [], "Mt" : [], "Ds" : [], "Rg" : [],
+  	                     "Cn" : [], "Nh" : [], "Fl" : [], "Mc" : [], "Lv" : [], "Ts" : [], "Og" : []};
+  for (let i = 0; i < molecule.length; i++) {
+    Object.entries(elemental_table).forEach(([key, value]) => {
+      let rows = molecule.get(i).split('\n');
+      let atom_counts = rows[3].split(' ')[1];
+      let new_mol = rows.slice(4, parseInt(atom_counts) + 4).toString();
+      if (new_mol.includes(key)) {
+        const re = new RegExp(key, 'g');
+        const count = new_mol.match(re).length;
+        //@ts-ignore
+        value.push(count);
+      } else {
+        //@ts-ignore
+        value.push(0);
+      }
+    })
   }
-  //@ts-ignore
-  Object.keys(elemental_table).forEach((k) => elemental_table[k] == 0 && delete elemental_table[k]);
   return elemental_table;
 }
-
 
 //top-menu: Chem | Elemental analysis...
 //name: Elemental analysis
 //input: dataframe table
 //input: column molecule { semType: Molecule }
 export async function elementalAnalysis(table: DG.DataFrame, molecule: DG.Column) {
-  for (let i = 0; i < table.rowCount; i++) {
-    let atoms = molecule.get(i);
-    Object.entries(getAtoms(atoms)).forEach(([key, value]) => {
-      let column_name = key;
-      if (table.columns.names().includes(column_name)) {
-        table.set(column_name, i, value);
-      } else {
-        table.columns.addNewInt(column_name);
-        table.set(column_name, i, value);
-      }  
-    })
-  }
+  Object.entries(getAtomsColumn(molecule)).forEach(([key, value]) => {
+    //@ts-ignore
+    value.every(el => el == 0) ? 0: table.columns.add(DG.Column.fromInt32Array(key, value));
+  })
 } 
 
 //name: Chem
