b7cc3f863c708545a71e7905ccba6b63bcd9ffea
Maria Dolotova <maria.dolotova@arcadia.spb.ru>
2022-09-08 20:38:13 +0300

    Bio: fixed Levenshtein distance normalization

--- packages/Bio/src/utils/sequence-activity-cliffs.ts
+++ packages/Bio/src/utils/sequence-activity-cliffs.ts
@@ -4,47 +4,32 @@ import * as ui from 'datagrok-api/ui';
 import {getSimilarityFromDistance} from '@datagrok-libraries/utils/src/similarity-metrics';
 import {AvailableMetrics} from '@datagrok-libraries/ml/src/typed-metrics';
 import * as grok from 'datagrok-api/grok';
+import { SplitterFunc, WebLogo } from '@datagrok-libraries/bio/src/viewers/web-logo';
+import { UnitsHandler } from '@datagrok-libraries/bio/src/utils/units-handler';
 
 export async function getDistances(col: DG.Column, seq: string): Promise<Array<number>> {
   const stringArray = col.toList();
   const distances = new Array(stringArray.length).fill(0);
-  for (let i = 0; i < stringArray.length; ++i)
-    distances[i] = stringArray[i] ? AvailableMetrics['String']['Levenshtein'](stringArray[i], seq) : null;
+  for (let i = 0; i < stringArray.length; ++i) {
+    const distance = stringArray[i] ? AvailableMetrics['String']['Levenshtein'](stringArray[i], seq) : null;
+    distances[i] = distance ? distance/Math.max((stringArray[i] as string).length, seq.length) : null;
+  }
   return distances;
 }
 
 export async function getSimilaritiesMarix(dim: number, seqCol: DG.Column, df: DG.DataFrame, colName: string, simArr: DG.Column[])
   : Promise<DG.Column[]> {
 
-  function arrayMin(arr: number[]) {
-    return arr.reduce(function (p, v) {
-      return (p < v ? p : v);
-    });
-  }
-
-  function arrayMax(arr: number[]) {
-    return arr.reduce(function (p, v) {
-      return (p > v ? p : v);
-    });
-  }
   const distances = new Array(simArr.length).fill(null);
-  let min = Infinity;
-  let max = -Infinity;
   for (let i = 0; i != dim - 1; ++i) {
-    const seq = seqCol.get(i);
+    const seq: string = seqCol.get(i);  
     df.rows.removeAt(0, 1, false);
     distances[i] = (await getDistances(df.col(colName)!, seq))!;
-    const newMin = arrayMin(distances[i]);
-    const newMax = arrayMax(distances[i]);
-    if (newMin < min)
-      min = newMin;
-    if (newMax > max)
-      max = newMax;
   }
 
   for (let i = 0; i < distances.length; i++) {
     for (let j = 0; j < distances[i].length; j++) {
-      distances[i][j] = getSimilarityFromDistance((distances[i][j] - min)/(max - min));
+      distances[i][j] = getSimilarityFromDistance(distances[i][j]);
     }
     simArr[i] = DG.Column.fromList(DG.COLUMN_TYPE.FLOAT, 'distances', distances[i]);
   }
