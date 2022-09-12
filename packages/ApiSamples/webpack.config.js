d195e6935aad0d848f206d4830a45d72d197f442
Alex Paramonov <alex.aprm@gmail.com>
2022-09-12 14:16:04 +0300

    Added tests to ApiSamples

---
+++ packages/ApiSamples/webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    test: {filename: 'package-test.js', library: {type: 'var', name: `apisamples_test`}, import: './src/package-test.ts'},
+    package: './src/package.ts',
+  },
+  resolve: {
+    extensions: ['.wasm', '.mjs', '.js', '.json', '.ts', '.tsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  devtool: 'inline-source-map',
+  externals: {
+    'datagrok-api/dg': 'DG',
+    'datagrok-api/grok': 'grok',
+    'datagrok-api/ui': 'ui',
+    'openchemlib/full.js': 'OCL',
+    'rxjs': 'rxjs',
+    'rxjs/operators': 'rxjs.operators',
+  },
+  output: {
+    filename: '[name].js',
+    library: 'apisamples',
+    libraryTarget: 'var',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+};
