8536f56ddbbe480bb8dfc3482bd17ca736c898c8
Volodymyr Dyma <vdyma@datagrok.ai>
2022-09-15 10:56:55 +0300

    Lib/bio: splitAlignedSequences refactoring

--- libraries/bio/src/utils/splitter.ts
+++ libraries/bio/src/utils/splitter.ts
@@ -2,25 +2,31 @@ import * as DG from 'datagrok-api/dg';
 
 import {WebLogo} from '../viewers/web-logo';
 
-export function splitAlignedSequences(peptideColumn: DG.Column<string>): DG.DataFrame {
-  const splitter = WebLogo.getSplitterForColumn(peptideColumn);
-  const colLen = peptideColumn.length;
-  const resultDf = DG.DataFrame.create(colLen);
-  let monomerList = splitter(peptideColumn.get(0)!);
-  const columnList: DG.Column<string>[] = [];
-
-  // create columns and fill the first row for faster values filling in the next loop
-  for (let i = 0; i < monomerList.length; i++) {
-    const col = resultDf.columns.addNewString((i + 1).toString());
-    col.set(0, monomerList[i] || '-', false);
-    columnList.push(col);
+export function splitAlignedSequences(sequenceColumn: DG.Column<string>): DG.DataFrame {
+  const splitter = WebLogo.getSplitterForColumn(sequenceColumn);
+  const getCol = (index: number): DG.Column<string> | null => columnList[index] ?? null;
+  const createCol = (index: number): DG.Column<string> => {
+    const positionCol = resultDf.columns.addNewString((index + 1).toString());
+    columnList.push(positionCol);
+    return positionCol;
   }
 
-  for (let rowIndex = 1; rowIndex < colLen; rowIndex++) {
-    monomerList = splitter(peptideColumn.get(rowIndex)!);
-    monomerList.forEach((monomer, colIndex) => columnList[colIndex].set(rowIndex, monomer || '-', false));
+  let currentMonomerList = splitter(sequenceColumn.get(0)!);
+  const columnList: DG.Column<string>[] = [];
+  const rowCount = sequenceColumn.length;
+  const resultDf = DG.DataFrame.create(rowCount);
+
+  for (let rowIndex = 0; rowIndex < rowCount; ++rowIndex) {
+    const sequence = sequenceColumn.get(rowIndex);
+    if (sequence == null)
+      continue;
+
+    currentMonomerList = splitter(sequence);
+    currentMonomerList.forEach((monomer, positionIndex) => {
+      const col = getCol(positionIndex) || createCol(positionIndex);
+      col.set(rowIndex, monomer || '-', false);
+    });
   }
 
   return resultDf;
 }
-  
