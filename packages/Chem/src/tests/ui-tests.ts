1c57568a75cad0db00d5bdc16fdb8a099abe45db
Leonid <stolbovla@yandex.ru>
2022-09-19 22:17:00 +0300

    Chem: ui tests fixed

--- packages/Chem/src/tests/ui-tests.ts
+++ packages/Chem/src/tests/ui-tests.ts
@@ -14,7 +14,7 @@ category('UI', () => {
 
   test('similarity search', async () => {
     v = grok.shell.addTableView(smiles);
-    await delay(3000);
+    await delay(4000);
     smiles.currentRowIdx = 0;
 
     grok.shell.topMenu.find('Chem').find('Similarity Search...').click(); await delay(2000);
@@ -133,11 +133,13 @@ category('UI', () => {
     smiles.currentRowIdx = 5; await delay(1000);
     checkHTMLElementbyInnerText('d4-accordion-pane-header', 'Gasteiger Partial Charges');
 
-    let gastaigerPanel = getHTMLElementbyInnerText('d4-accordion-pane-header', 'Gasteiger Partial Charges')
-    gastaigerPanel!.click(); await delay(4000);
-
-    if (document.getElementsByClassName('grok-scripting-image-container-info-panel').length != 1)
-        throw 'script output was not rendered in the panel'
+    await delay(4000);
+    if (document.getElementsByClassName('grok-scripting-image-container-info-panel').length != 1) {
+      let gastaigerPanel = getHTMLElementbyInnerText('d4-accordion-pane-header', 'Gasteiger Partial Charges')
+      gastaigerPanel!.click(); await delay(4000);
+      if (document.getElementsByClassName('grok-scripting-image-container-info-panel').length != 1)
+        throw 'Gasteiger charges script output was not rendered in the panel'
+    }
 
     let pecilIcon = document.getElementsByClassName('grok-icon fal fa-pencil')[0] as HTMLElement;
     pecilIcon!.click(); await delay(500);
@@ -148,6 +150,7 @@ category('UI', () => {
     let applyBtn = document.getElementsByClassName('d4-accordion-pane-content ui-div d4-pane-gasteiger_partial_charges')[0].getElementsByClassName('ui-btn ui-btn-ok')[0] as HTMLElement;
     applyBtn!.click(); await delay(2000);
 
+    let gastaigerPanel = getHTMLElementbyInnerText('d4-accordion-pane-header', 'Gasteiger Partial Charges')
     gastaigerPanel!.click(); await delay(500);
 
     grok.shell.closeAll(); 
@@ -161,15 +164,21 @@ category('UI', () => {
     smiles.currentRowIdx = 5; await delay(500);
     checkHTMLElementbyInnerText('d4-accordion-pane-header', 'Identifiers');
 
-    let identifiersPanel = getHTMLElementbyInnerText('d4-accordion-pane-header', 'Identifiers')
-    identifiersPanel!.click(); await delay(2000);
+    await delay(3000);
+    if (document.getElementsByClassName('ui-link d4-link-external').length < 1) {
+      let identifiersPanel = getHTMLElementbyInnerText('d4-accordion-pane-header', 'Identifiers')
+      identifiersPanel!.click(); await delay(3000);
+      if (document.getElementsByClassName('ui-link d4-link-external').length < 1)
+        throw 'no identifiers found'; 
+    }
+
 
     checkHTMLElementbyInnerText('ui-link d4-link-external', 'DTXSID00639834');
     checkHTMLElementbyInnerText('ui-link d4-link-external', '24204979');
     checkHTMLElementbyInnerText('ui-link d4-link-external', 'CHEMBL2262434');
-        
+    
+    let identifiersPanel = getHTMLElementbyInnerText('d4-accordion-pane-header', 'Identifiers')
     identifiersPanel!.click(); await delay(500);
-
     grok.shell.closeAll(); 
   }); 
 
@@ -181,12 +190,15 @@ category('UI', () => {
     smiles.currentRowIdx = 5; await delay(500);
     checkHTMLElementbyInnerText('d4-accordion-pane-header', 'Structure 2D');
 
-    let structure2DPanel = getHTMLElementbyInnerText('d4-accordion-pane-header', 'Structure 2D')
-    structure2DPanel!.click(); await delay(1000);
+    await delay(3000);
+    if (document.getElementsByClassName('d4-flex-col ui-div chem-mol-box').length != 1) {
+      let structure2DPanel = getHTMLElementbyInnerText('d4-accordion-pane-header', 'Structure 2D')
+      structure2DPanel!.click(); await delay(3000);
+      if (document.getElementsByClassName('d4-flex-col ui-div chem-mol-box').length != 1)
+        throw 'canvas with structure was not rendered in the panel' 
+    }
 
-    if (document.getElementsByClassName('d4-flex-col ui-div chem-mol-box').length != 1)
-        throw 'canvas with structure was not rendered in the panel'    
-        
+    let structure2DPanel = getHTMLElementbyInnerText('d4-accordion-pane-header', 'Structure 2D')
     structure2DPanel!.click(); await delay(500);
 
     grok.shell.closeAll(); 
@@ -200,12 +212,15 @@ category('UI', () => {
     smiles.currentRowIdx = 5; await delay(500);
     checkHTMLElementbyInnerText('d4-accordion-pane-header', 'Structure 3D');
 
-    let structure3DPanel = getHTMLElementbyInnerText('d4-accordion-pane-header', 'Structure 3D')
-    structure3DPanel!.click(); await delay(2000);
-
-    if (document.getElementsByClassName('d4-ngl-viewer ui-div').length != 1)
-        throw 'canvas with 3D structure was not rendered in the panel'    
-        
+    await delay(4000);
+    if (document.getElementsByClassName('d4-ngl-viewer ui-div').length != 1) {
+      let structure3DPanel = getHTMLElementbyInnerText('d4-accordion-pane-header', 'Structure 3D');
+      structure3DPanel!.click(); await delay(3000);
+      if (document.getElementsByClassName('d4-ngl-viewer ui-div').length != 1)
+        throw 'canvas with 3D structure was not rendered in the panel'; 
+    }
+    
+    let structure3DPanel = getHTMLElementbyInnerText('d4-accordion-pane-header', 'Structure 3D');
     structure3DPanel!.click(); await delay(500);
 
     grok.shell.closeAll();
@@ -218,13 +233,18 @@ category('UI', () => {
 
     smiles.currentRowIdx = 5; await delay(500);
     checkHTMLElementbyInnerText('d4-accordion-pane-header', 'Properties');
-
-    let propertiesPanel = getHTMLElementbyInnerText('d4-accordion-pane-header', 'Properties')
-    propertiesPanel!.click(); await delay(2000);
-
-    if (document.getElementsByClassName('d4-accordion-pane-content ui-div d4-pane-properties')[0].getElementsByClassName('d4-table d4-item-table d4-info-table').length != 1)
-        throw 'table with properties was not rendered in the panel'    
         
+    await delay(3000);
+    if (document.getElementsByClassName('d4-accordion-pane-content ui-div d4-pane-properties')[0]
+      .getElementsByClassName('d4-table d4-item-table d4-info-table').length != 1) {
+      let propertiesPanel = getHTMLElementbyInnerText('d4-accordion-pane-header', 'Properties')
+      propertiesPanel!.click(); await delay(2000);
+      if (document.getElementsByClassName('d4-accordion-pane-content ui-div d4-pane-properties')[0]
+        .getElementsByClassName('d4-table d4-item-table d4-info-table').length != 1)
+        throw 'table with properties was not rendered in the panel';
+    }
+    
+    let propertiesPanel = getHTMLElementbyInnerText('d4-accordion-pane-header', 'Properties')
     propertiesPanel!.click(); await delay(500);
 
     grok.shell.closeAll();
@@ -238,12 +258,17 @@ category('UI', () => {
     smiles.currentRowIdx = 5; await delay(500);
     checkHTMLElementbyInnerText('d4-accordion-pane-header', 'Toxicity');
 
-    let toxicityPanel = getHTMLElementbyInnerText('d4-accordion-pane-header', 'Toxicity')
-    toxicityPanel!.click(); await delay(2000);
+    await delay(3000);
+    if (document.getElementsByClassName('d4-accordion-pane-content ui-div d4-pane-toxicity')[0].
+      getElementsByClassName('d4-table d4-item-table d4-info-table').length != 1) {
+      let toxicityPanel = getHTMLElementbyInnerText('d4-accordion-pane-header', 'Toxicity')
+      toxicityPanel!.click(); await delay(2000);
+      if (document.getElementsByClassName('d4-accordion-pane-content ui-div d4-pane-toxicity')[0]
+        .getElementsByClassName('d4-table d4-item-table d4-info-table').length != 1)
+      throw 'table with toxicity was not rendered in the panel';
+    }
 
-    if (document.getElementsByClassName('d4-accordion-pane-content ui-div d4-pane-toxicity')[0].getElementsByClassName('d4-table d4-item-table d4-info-table').length != 1)
-        throw 'table with toxicity was not rendered in the panel'    
-        
+    let toxicityPanel = getHTMLElementbyInnerText('d4-accordion-pane-header', 'Toxicity')
     toxicityPanel!.click(); await delay(500);
 
     grok.shell.closeAll();
@@ -256,13 +281,18 @@ category('UI', () => {
 
     smiles.currentRowIdx = 5; await delay(500);
     checkHTMLElementbyInnerText('d4-accordion-pane-header', 'Drug Likeness');
+        
+    await delay(3000);
+    if (document.getElementsByClassName('d4-accordion-pane-content ui-div d4-pane-drug_likeness')[0]
+      .getElementsByClassName('d4-flex-col ui-div').length != 19) {
+      let drugLikenessPanel = getHTMLElementbyInnerText('d4-accordion-pane-header', 'Drug Likeness')
+      drugLikenessPanel!.click(); await delay(2000);
+      if (document.getElementsByClassName('d4-accordion-pane-content ui-div d4-pane-drug_likeness')[0]
+        .getElementsByClassName('d4-flex-col ui-div').length != 19)
+        throw 'number of displayed canvases with molecules does not match the expected'  ; 
+    }
 
     let drugLikenessPanel = getHTMLElementbyInnerText('d4-accordion-pane-header', 'Drug Likeness')
-    drugLikenessPanel!.click(); await delay(2000);
-
-    if (document.getElementsByClassName('d4-accordion-pane-content ui-div d4-pane-drug_likeness')[0].getElementsByClassName('d4-flex-col ui-div').length != 19)
-        throw 'number of displayed canvases with molecules does not match the expected'    
-        
     drugLikenessPanel!.click(); await delay(500);
 
     grok.shell.closeAll();
@@ -275,13 +305,18 @@ category('UI', () => {
     
     smiles.currentRowIdx = 5; await delay(500);
     checkHTMLElementbyInnerText('d4-accordion-pane-header', 'Structural Alerts');
+        
+    await delay(3000);
+    if (document.getElementsByClassName('d4-accordion-pane-content ui-div d4-pane-structural_alerts')[0]
+      .getElementsByClassName('d4-flex-col ui-div').length != 10) {
+      let structuralAlertsPanel = getHTMLElementbyInnerText('d4-accordion-pane-header', 'Structural Alerts')
+      structuralAlertsPanel!.click(); await delay(2000);
+      if (document.getElementsByClassName('d4-accordion-pane-content ui-div d4-pane-structural_alerts')[0]
+        .getElementsByClassName('d4-flex-col ui-div').length != 10)
+        throw 'number of displayed canvases with alerts does not match the expected'; 
+    }
 
     let structuralAlertsPanel = getHTMLElementbyInnerText('d4-accordion-pane-header', 'Structural Alerts')
-    structuralAlertsPanel!.click(); await delay(2000);
-
-    if (document.getElementsByClassName('d4-accordion-pane-content ui-div d4-pane-structural_alerts')[0].getElementsByClassName('d4-flex-col ui-div').length != 10)
-        throw 'number of displayed canvases with alerts does not match the expected'    
-        
     structuralAlertsPanel!.click(); await delay(500);
 
     grok.shell.closeAll();
@@ -361,7 +396,7 @@ category('UI', () => {
     await callDialog();
     setDialogInputValue('Chem Map Identifiers', 'To Source', 'pubchem');
     okButton = document.getElementsByClassName('ui-btn ui-btn-ok enabled')[0] as HTMLElement;
-    okButton!.click(); await delay(10000);
+    okButton!.click(); await delay(12000);
     isColumnPresent(grok.shell.t.columns, 'pubchem');
 
     v.close();
