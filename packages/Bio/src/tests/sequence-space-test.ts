f955a7eb51102528843ab394d898f0ec93018873
tanas80 <atanas@datagrok.ai>
2022-09-20 12:14:05 +0400

    Bio: Fix sequenceSpace test for moved files

--- packages/Bio/src/tests/sequence-space-test.ts
+++ packages/Bio/src/tests/sequence-space-test.ts
@@ -2,7 +2,7 @@ import {after, before, category, test, expect, delay} from '@datagrok-libraries/
 import * as DG from 'datagrok-api/dg';
 import {readDataframe} from './utils';
 import * as grok from 'datagrok-api/grok';
-import { _testSequenceSpaceReturnsResult } from './sequence-space-utils';
+import {_testSequenceSpaceReturnsResult} from './sequence-space-utils';
 
 category('sequenceSpace', async () => {
   let testFastaDf: DG.DataFrame;
@@ -13,7 +13,7 @@ category('sequenceSpace', async () => {
   before(async () => {
     testFastaDf = await readDataframe('samples/sample_FASTA.csv');
     testFastaTableView = grok.shell.addTableView(testFastaDf);
-    testHelmWithEmptyRows = await readDataframe('samples/sample_HELM_empty_vals.csv');
+    testHelmWithEmptyRows = await readDataframe('data/sample_HELM_empty_vals.csv');
     testHelmWithEmptyRowsTableView = grok.shell.addTableView(testHelmWithEmptyRows);
   });
 
