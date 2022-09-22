8f41ab9fdb33e195d27f966de466f99ac45d209a
Alexey <achopovsky@datagrok.ai>
2022-09-22 14:41:52 +0300

    Chem: saveAsSdf fixes

--- packages/Chem/src/utils/sdf-utils.ts
+++ packages/Chem/src/utils/sdf-utils.ts
@@ -1,16 +1,16 @@
 import * as grok from 'datagrok-api/grok';
 import * as DG from 'datagrok-api/dg';
 import * as ui from 'datagrok-api/ui';
-import * as OCL from 'openchemlib/full';
+import * as chemCommonRdKit from './chem-common-rdkit';
+import {MolNotation, _convertMolNotation} from './convert-notation-utils';
 import $ from 'cash-dom';
 
 /**  Dialog for SDF file exporter */
 export function saveAsSdfDialog() {
   const table = grok.shell.t;
-  let cols = table.columns.bySemTypeAll('Molecule');
-  cols = cols.concat(table.columns.bySemTypeAll('Macromolecule'));
+  const cols = table.columns.bySemTypeAll(DG.SEMTYPE.MOLECULE);
   if (cols.length === 0)
-    grok.shell.warning('This table does not contain Molecule columns, unable to save as SDF');
+    grok.shell.warning(`This table does not contain ${DG.SEMTYPE.MOLECULE} columns, unable to save as SDF`);
   else if (cols.length === 1)
     _saveAsSdf(table, cols[0]);
   else {
@@ -24,6 +24,12 @@ export function saveAsSdfDialog() {
         const structureColumn = colsInput.value;
         _saveAsSdf(table, structureColumn!);
       });
+    if (cols.length > 1) {
+      const text = ui.divText(`Other ${DG.SEMTYPE.MOLECULE} colums saved as SMILES`);
+      sdfDialog.add(text);
+      $(text).css('color', 'gray');
+      $(text).css('font-size', '12px');
+    }
     sdfDialog.show({x: 350, y: 100});
     $(sdfDialog.root).find('.grok-font-icon-help').remove();
     $(sdfDialog.root).find('.ui-input-label').css('max-width', 'fit-content');
@@ -35,7 +41,6 @@ export function _saveAsSdf(
   structureColumn: DG.Column,
 ): void {
   //todo: load OpenChemLib (or use RDKit?)
-  //todo: open dialog
   //todo: UI for choosing columns with properties
 
   const pi = DG.TaskBarProgressIndicator.create('Saving as SDF...');
@@ -48,14 +53,22 @@ export function _saveAsSdf(
   for (let i = 0; i < table.rowCount; i++) {
     try {
       const molecule: string = structureColumn.get(i);
-      const mol = molecule.includes('M  END') ? molecule : OCL.Molecule.fromSmiles(molecule).toMolfile();
+      const mol = molecule.includes('M  END') ? molecule :
+        _convertMolNotation(molecule, MolNotation.Unknown, MolNotation.MolBlock, chemCommonRdKit.getRdKitModule());
       result += i == 0 ? '' : '\n';
       result += `${mol}\n`;
 
       // properties
       for (const col of table.columns) {
-        if (col !== structureColumn)
-          result += `>  <${col.name}>\n${col.get(i)}\n\n`;
+        if (col !== structureColumn) {
+          let cellValue = col.get(i);
+          // convert to SMILES if necessary
+          if (col.semType === DG.SEMTYPE.MOLECULE) {
+            cellValue = _convertMolNotation(cellValue, MolNotation.Unknown, 
+              MolNotation.Smiles, chemCommonRdKit.getRdKitModule());
+          }
+          result += `>  <${col.name}>\n${cellValue}\n\n`;
+        }
       }
 
       result += '$$$$';
