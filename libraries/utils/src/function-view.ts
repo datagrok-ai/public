0fb7c780a74ba1d6d02b80430832879cfc4f9733
Aufar Zakiev <zaufar@gmail.com>
2022-09-23 12:34:24 +0300

    #466 ComputationView: fixed bug with readonly state

--- libraries/utils/src/function-view.ts
+++ libraries/utils/src/function-view.ts
@@ -690,7 +690,7 @@ export class FunctionView extends DG.ViewBase {
 
   private interactiveEventListeners = [
     () =>this.root.removeEventListener('click', this.rootReadonlyEventListeners[2]),
-    () =>this.root.addEventListener('click', this.rootReadonlyEventListeners[2])
+    () =>setTimeout(() => this.root.addEventListener('click', this.rootReadonlyEventListeners[2]), 100)
   ];
 
   private setRunViewReadonly(): void {
@@ -701,7 +701,7 @@ export class FunctionView extends DG.ViewBase {
     this.root.querySelectorAll(`.${INTERACTIVE_CSS_CLASS}`).forEach((el) => {
       (el as HTMLElement).style.zIndex = '2';
       (el as HTMLElement).addEventListener('mousedown', this.interactiveEventListeners[0]);
-      (el as HTMLElement).addEventListener('mouseup', () => setTimeout(this.interactiveEventListeners[1], 100));
+      (el as HTMLElement).addEventListener('mouseup', this.interactiveEventListeners[1]);
     });
   }
 
