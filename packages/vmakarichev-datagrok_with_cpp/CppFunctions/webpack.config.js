bad2034e2529a1d1453d2ab3eb55ef7c778c655f
vmakarichev <vmakarichev@datagrok.ai>
2022-09-26 12:30:22 +0300

    New package is create

---
+++ packages/vmakarichev-datagrok_with_cpp/CppFunctions/webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const packageName = path.parse(require('./package.json').name).name.toLowerCase().replace(/-/g, '');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    package: './src/package.js'
+  },
+  devtool: 'inline-source-map',
+  externals: {
+    'datagrok-api/dg': 'DG',
+    'datagrok-api/grok': 'grok',
+    'datagrok-api/ui': 'ui',
+    'openchemlib/full.js': 'OCL',
+    'rxjs': 'rxjs',
+    'rxjs/operators': 'rxjs.operators',
+    'cash-dom': '$',
+    'dayjs': 'dayjs',
+    'wu': 'wu',
+    'exceljs': 'ExcelJS',
+  },
+  output: {
+    filename: '[name].js',
+    library: packageName,
+    libraryTarget: 'var',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
