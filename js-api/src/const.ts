cbd09071a51d72bce909cae6fb9fd9c64a422666
Aufar Zakiev <zaufar@gmail.com>
2022-10-05 17:43:38 +0300

    JS API: add default users IDs

--- js-api/src/const.ts
+++ js-api/src/const.ts
@@ -547,3 +547,19 @@ export interface SemTypeInfo {
    * Used for the automatic detection of semantic data types. */
   valueRegexp?: string;
 }
+
+export const defaultUsersIds = {
+  "Test": "ca1e672e-e3be-40e0-b79b-d2c68e68d380", 
+  "Admin": "878c42b0-9a50-11e6-c537-6bf8e9ab02ee", 
+  "System": "3e32c5fa-ac9c-4d39-8b4b-4db3e576b3c3", 
+}
+
+export const defaultGroupsIds = {
+  "All users": "a4b45840-9a50-11e6-9cc9-8546b8bf62e6", 
+  "Developers": "ba9cd191-9a50-11e6-9cc9-910bf827f0ab",
+  "Need to create": "00000000-0000-0000-0000-000000000000",
+  "Test": "ca1e672e-e3be-40e0-b79b-8546b8bf62e6", 
+  "Admin": "a4b45840-9a50-11e6-c537-6bf8e9ab02ee", 
+  "System": "a4b45840-ac9c-4d39-8b4b-4db3e576b3c3", 
+  "Administrators": "1ab8b38d-9c4e-4b1e-81c3-ae2bde3e12c5",
+}
