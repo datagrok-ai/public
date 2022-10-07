7a3525b683e1d2a615479262f9609df982b5056c
tanas80 <atanas@datagrok.ai>
2022-10-07 13:38:09 +0400

    ApiTests: Add test for assigning string property of JsViewer with number value and vice versa

--- packages/ApiTests/src/package.ts
+++ packages/ApiTests/src/package.ts
@@ -2,3 +2,12 @@
 import * as grok from 'datagrok-api/grok';
 import * as ui from 'datagrok-api/ui';
 import * as DG from 'datagrok-api/dg';
+import {TestViewerForProperties} from './viewers/test-viewer-for-properties';
+
+//name: TestViewerForProperties
+//description: Viewer to test properties and others
+//tags: viewer, panel
+//output: viewer result
+export function testViewerForProperties() {
+  return new TestViewerForProperties();
+}
