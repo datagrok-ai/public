f9295c0c9f4b0ece5657ddbf365ecf522b31f9d9
vmakarichev <vmakarichev@datagrok.ai>
2022-10-17 17:06:36 +0300

    Shorter C-funcs preambula is implemented

--- packages/vmakarichev-datagrok_with_cpp/CppFunctions/wasm/Release 1.0.0, Oct. 13, 2022/lib1.c
+++ packages/vmakarichev-datagrok_with_cpp/CppFunctions/wasm/Release 1.0.0, Oct. 13, 2022/lib1.c
@@ -4,6 +4,8 @@
 //input: int x
 //input: int y
 //output: int z
+//map: a is x
+//map: b is y
 EMSCRIPTEN_KEEPALIVE
 int sumOfInts(int a, int b)
 {
@@ -14,12 +16,14 @@ int sumOfInts(int a, int b)
 //input: dataframe df
 //input: column col
 //output: int num
+//map: array is col
+//map: length is col.length
 EMSCRIPTEN_KEEPALIVE
-int minOfArray(int * array, int arrayLength)
+int minOfArray(int * array, int length)
 {
     int result = array[0];
 
-    for(int i = 1; i < arrayLength; i++)
+    for(int i = 1; i < length; i++)
         if( result > array[i] )
             result = array[i];
     
