e89f3dbedc6d55b246d8c2196bbcd78fbd9ef269
Maria Dolotova <maria.dolotova@arcadia.spb.ru>
2022-10-18 12:47:42 +0300

    Bio: updated utils library version, bump package version

--- packages/Bio/src/package-test.ts
+++ packages/Bio/src/package-test.ts
@@ -1,6 +1,6 @@
 import * as DG from 'datagrok-api/dg';
 
-import {runTests, tests} from '@datagrok-libraries/utils/src/test';
+import {runTests, TestContext, tests} from '@datagrok-libraries/utils/src/test';
 
 import './tests/WebLogo-test';
 import './tests/Palettes-test';
@@ -24,9 +24,9 @@ export {tests};
 //name: test
 //input: string category {optional: true}
 //input: string test {optional: true}
-//input: bool catchUnhandled {optional: true}
+//input: object testContext {optional: true}
 //output: dataframe result
-export async function test(category: string, test: string, catchUnhandled: boolean): Promise<DG.DataFrame> {
-  const data = await runTests({category, test, catchUnhandled});
+export async function test(category: string, test: string, testContext: TestContext): Promise<DG.DataFrame> {
+  const data = await runTests({category, test, testContext});
   return DG.DataFrame.fromObjects(data)!;
 }
