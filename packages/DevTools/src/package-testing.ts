fbdf0542260457961bc4e1e37ee9b23f2551a744
Maria Dolotova <maria.dolotova@arcadia.spb.ru>
2022-10-18 12:45:17 +0300

    DevTools: updated utills version, bump version

--- packages/DevTools/src/package-testing.ts
+++ packages/DevTools/src/package-testing.ts
@@ -1,7 +1,7 @@
 import * as grok from 'datagrok-api/grok';
 import * as ui from 'datagrok-api/ui';
 import * as DG from 'datagrok-api/dg';
-import {Test} from '@datagrok-libraries/utils/src/test';
+import {Test, TestContext} from '@datagrok-libraries/utils/src/test';
 import {c, testFunc} from './package';
 import {Menu} from 'datagrok-api/dg';
 
@@ -335,7 +335,7 @@ export class TestManager extends DG.ViewBase {
         `${t.packageName}:test`, {
           'category': t.test.category,
           'test': t.test.name,
-          'catchUnhandled': false,
+          'testContext': new TestContext(false),
         });
       res.columns.addNewString('funcTest').init((i) => '');
       testSucceeded = res.get('success', 0);
