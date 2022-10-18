7bbc06c9786089685491ebd2ebb5597b1c2c6622
Denis Zhirkov <denyzhirkov@gmail.com>
2022-10-18 14:36:39 +0300

    extended tests with example of datetime column type and join (#1106)
    Co-authored-by: Denis Zhirkov <denis.zhirkov_ext@novartis.com>

---
+++ packages/ApiTests/src/dataframe/datetime-columns-join.ts
@@ -0,0 +1,35 @@
+import * as grok from 'datagrok-api/grok';
+import * as DG from 'datagrok-api/dg';
+import {category, expect, expectArray, test} from '@datagrok-libraries/utils/src/test';
+
+
+category('DataFrame', () => {
+
+  test('Join DF with DF (with datetime columns) should be without errors', async () => {
+    const df1 = DG.DataFrame.fromColumns([
+      DG.Column.fromList(DG.TYPE.INT, 'A', [1, 2, 3]),
+      DG.Column.fromList(DG.TYPE.INT, 'B', [4, 5, 6]),
+      DG.Column.fromList(DG.TYPE.INT, 'C', [7, 8, 9])
+    ]);
+
+    const df2 = DG.DataFrame.fromColumns([
+      DG.Column.fromList(DG.TYPE.INT, 'AA', [1, 2, 3]),
+      DG.Column.fromList(DG.TYPE.INT, 'BB', [4, 5, 6]),
+      DG.Column.fromList(DG.TYPE.INT, 'CC', [7, 8, 9])
+    ]);
+
+    df1.columns.addNew('D', DG.COLUMN_TYPE.DATE_TIME);
+    df1.set('D', 0, ''); // <- will be a problem on join
+    df1.set('D', 1, '2017-10-18T00:00:00.000Z');
+    df1.set('D', 2, '2017-11-18T00:00:00.000Z');
+    
+    const result = DG.DataFrame.fromColumns([
+      df2.columns.byName('AA'),
+      df2.columns.byName('BB'),
+      df2.columns.byName('CC')
+    ]).join(df1, ['AA'], ['A'], ['AA','BB','CC'],['B','C','D'], 'inner', false);
+
+    expect(result);
+  });
+
+});
