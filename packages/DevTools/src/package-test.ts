2ffa32c778facbefe3be3de3d21fef93b12ec306
Maria Dolotova <maria.dolotova@arcadia.spb.ru>
2022-10-18 12:45:17 +0300

    DevTools: updated utills version, bump version

--- packages/DevTools/src/package-test.ts
+++ packages/DevTools/src/package-test.ts
@@ -1,6 +1,6 @@
 import * as DG from 'datagrok-api/dg';
 import * as grok from 'datagrok-api/grok';
-import {runTests, tests} from '@datagrok-libraries/utils/src/test';
+import {runTests, TestContext, tests} from '@datagrok-libraries/utils/src/test';
 
 import './tests/function-signature-editor-test';
 import './tests/dev-panel-test';
@@ -12,9 +12,9 @@ export {tests};
 //name: test
 //input: string category {optional: true}
 //input: string test {optional: true}
-//input: bool catchUnhandled {optional: true}
+//input: object testContext {optional: true}
 //output: dataframe result
-export async function test(category: string, test: string, catchUnhandled: boolean): Promise<DG.DataFrame> {
-  const data = await runTests({category, test, catchUnhandled});
+export async function test(category: string, test: string, testContext: TestContext): Promise<DG.DataFrame> {
+  const data = await runTests({category, test, testContext});
   return DG.DataFrame.fromObjects(data)!;
 }
