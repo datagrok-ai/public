84b6c1684e457414ef552751113c65de8c173c05
Maksym Shvyrlo <64889448+ShvyrloM@users.noreply.github.com>
2022-10-20 13:40:48 +0300

    GIS: selection map points/grid rows (update)

--- packages/GIS/src/package-test.ts
+++ packages/GIS/src/package-test.ts
@@ -1,6 +1,7 @@
 import * as DG from "datagrok-api/dg";
 import * as grok from "datagrok-api/grok";
 import {runTests, tests, TestContext} from '@datagrok-libraries/utils/src/test';
+// import {runTests, tests} from '@datagrok-libraries/utils/src/test';
 
 export let _package = new DG.Package();
 export {tests}
