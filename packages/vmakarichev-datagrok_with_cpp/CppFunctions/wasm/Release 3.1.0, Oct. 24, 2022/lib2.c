9ff68f34ce336e1116d54bab420352ecf73e8aec
vmakarichev <vmakarichev@datagrok.ai>
2022-10-24 16:15:52 +0300

    C-functions export via updated runtime system

---
+++ packages/vmakarichev-datagrok_with_cpp/CppFunctions/wasm/Release 3.1.0, Oct. 24, 2022/lib2.c
@@ -0,0 +1,49 @@
+#include <emscripten.h>
+
+//name: doubleColumn
+//input: dataframe df
+//input: column col
+//EMSCRIPTEN_KEEPALIVE
+void doubleArray(float * array, int arrayLength)
+{
+    for(int i = 0; i < arrayLength; i++)
+        array[i] = array[i] * 2;
+
+}
+
+//name: sumOfColumns (non-export)
+//input: column col1
+//input: column col2
+//output: column result [sum new(col1.length)]
+EMSCRIPTEN_KEEPALIVE
+void sumOfArrays(float * arr1, int arr1Length, float * arr2, int arr2Length, float * sum, int sumLength)
+{
+    for(int i = 0; i < sumLength; i++) {
+        sum[i] = arr1[i] + arr2[i];
+    }
+} 
+
+//name: sumAndProdOfColumns (non-export)
+//input: column col1
+//input: column col2
+//output: columns result [sum new(col1.length), prod new(...)]
+EMSCRIPTEN_KEEPALIVE
+void sumAndProdOfArrays(float * arr1, int arr1Length, float * arr2, int arr2Length, float * sum, int sumLength, float * prod, int prodLength)
+{
+    for(int i = 0; i < sumLength; i++) {
+        sum[i] = arr1[i] + arr2[i];
+        prod[i] = arr1[i] * arr2[i];
+    }
+}
+
+//name: powersOfInts (non-export)
+//input: column col
+//output: columns result [squared new(col.length), cubic new(...), fourth new(...)]
+EMSCRIPTEN_KEEPALIVE
+void powers(int * arr, int arrLength, int * squared, int squaredLength, int * cubic, int cubicLength, int * fourth, int fourthLength) {
+    for(int i = 0; i < arrLength; i++) {
+        squared[i] = arr[i] * arr[i];
+        cubic[i] = squared[i] * arr[i];
+        fourth[i] = cubic[i] * arr[i];
+    }
+}
---
+++ packages/vmakarichev-datagrok_with_cpp/CppFunctions/wasm/Release 3.1.0, Oct. 24, 2022/runtime.js
@@ -0,0 +1,111 @@
+
+// Wrapper for exported C-functions
+function cFuncWrapper(module, cFuncName, returnType, argTypes, args, argsToUpdate=[]){    
+
+    // type-to-heap correspondence
+    let heapMap = {"i32a": "HEAP32", // Int32Array
+                   "f32a": "HEAPF32", // Float32Array
+                   "i32c": "HEAP32", // column that contains Int32Array
+                   "f32c": "HEAPF32"}; // column that contains Float32Array
+                   // TODO: add other items
+  
+    let params = []; // parameters that are further substituted to C-function
+    let paramTypes = []; // types of parameters that are further substituted to C-function
+    let bufs = []; // buffers that are used for arrays    
+    let buffer; // the current buffer 
+    let extendedTypeOfReturn = (returnType == 'num') ? 'number' : null; // C-function return type
+  
+    // creation of parameters that are substituted to the exported C-function
+    for(let i = 0; i < args.length; i++){          
+        let argType = argTypes[i];
+  
+        if(argType == 'num') {
+            params.push(args[i]);
+            paramTypes.push('number');
+            bufs.push(null); // i.e. no allocated memory is used
+        }
+  
+        else { // the current argument is an array, so some buffer/heap routine shoude be performed
+            let arr; // raw data array
+            
+            // set arr taking into account the current structure feature: array or column
+            switch(argType[argType.length - 1]) { // check the last symbol: 'a' - array, 'c' - column
+              case 'a':
+                arr = args[i];
+                break;
+              case 'c':
+                arr = args[i].getRawData();
+                break;
+            }
+            
+            buffer = module._malloc(arr.BYTES_PER_ELEMENT * arr.length); // allocate memory
+  
+            let heap = heapMap[argType]; // choose the required heap            
+            let shift = 0; // current shift
+            
+            // compute shift
+            switch(heap) {
+                case "HEAP32": case "HEAPF32": 
+                    shift = 2; // this shift is specified by EMSCRIPTEN 
+                    break;
+                //TODO: add processing with respect to other types
+            }
+  
+            // assign the data to the heap
+            module[heap].set(arr, buffer >> shift);
+  
+            // append arrays that are passed to C-function
+            params.push(buffer);
+            paramTypes.push('number'); // in the case of array, 'number' is also used
+            bufs.push(buffer);
+  
+            // append length of array
+            params.push(arr.length);
+            paramTypes.push('number');
+        }
+    }
+  
+    // call C-function
+    let result;   
+    if(extendedTypeOfReturn)
+        result = module.ccall(cFuncName, extendedTypeOfReturn, paramTypes, params);
+    else
+        module.ccall(cFuncName, extendedTypeOfReturn, paramTypes, params);
+  
+    // process arrays that are modified, when executing C-function
+    for(let j = 0; j < argsToUpdate.length; j++){
+        let m = argsToUpdate[j];
+        
+        let argType = argTypes[m];
+  
+        if(argType != 'num') {     
+            let arr; // raw data array
+  
+            // set arr taking into account the current structure feature: array or column
+            switch(argType[argType.length - 1]) { // check the last symbol: 'a' - array, 'c' - column
+              case 'a':
+                arr = args[m];
+                break;
+              case 'c':
+                arr = args[m].getRawData();
+                break;
+            }
+  
+            let heap = heapMap[argType]; 
+            let buffer = bufs[m];
+            let bytes = arr.BYTES_PER_ELEMENT;
+            
+            // update the current array
+            for(let n = 0; n < arr.length; n++)
+                arr[n] = module[heap][buffer/bytes + n];                
+        } 
+    }
+    
+    // clear allocated memory
+    for(let k = 0; k < bufs.length; k++)
+        if(bufs[k])
+            module._free(bufs[k]);
+    
+    if(returnType)
+        return result;
+  }
