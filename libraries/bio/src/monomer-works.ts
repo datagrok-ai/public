aedb03e2a99adeb9811b28b434709b4d41ef1730
tanas80 <atanas@datagrok.ai>
2022-10-25 19:08:09 +0400

    bio lib: Export MonomerWorks change getCappedMonomer(type, name), remove async init()

--- libraries/bio/src/monomer-works.ts
+++ libraries/bio/src/monomer-works.ts
@@ -2,32 +2,24 @@ import * as grok from 'datagrok-api/grok';
 import * as ui from 'datagrok-api/ui';
 import * as DG from 'datagrok-api/dg';
 
-import {IMonomerLib} from './types/index';
+import {IMonomerLib, Monomer} from './types/index';
 import {capTheMonomer} from './utils/to-atomic-level';
-import {getMonomerLib} from './utils/monomer-lib';
 
 
 export class MonomerWorks {
-  monomerLib: IMonomerLib | null = null;
+  monomerLib: IMonomerLib;
 
-  public async init(): Promise<void> {
-    const funcList: DG.Func[] = DG.Func.find({package: 'Helm', name: 'getAllLibsData'});
-    if (funcList.length === 0)
-      await grok.functions.call('Helm:initHelm');
-
-    this.monomerLib = await getMonomerLib();
-    this.monomerLib.onChanged.subscribe(() => {
-      // invalidate something
-    });
+  constructor(monomerLib: IMonomerLib) {
+    this.monomerLib = monomerLib;
   }
 
   //types according to Monomer possible
-  public getCappedMonomer(name: string, type: string): string {
+  public getCappedMonomer(monomerType: string, monomerName: string): Monomer | null {
     const types = Object.keys(this.monomerLib!);
-    if (!types.includes(type))
+    if (!types.includes(monomerType))
       throw '';
 
     //return capTheMonomer(this.monomerLib!.get(type, name));
-    return this.monomerLib!.get(type, name)!.m;
+    return this.monomerLib.get(monomerType, monomerName);
   }
 }
