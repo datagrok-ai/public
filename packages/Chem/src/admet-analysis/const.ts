df99f484bf4ef667d181330c1d4ea4022dd57b61
Oleksandra Serhiienko <59337516+Aleksashka11@users.noreply.github.com>
2022-10-25 16:51:23 +0300

    Issue #946: Adding ui for admet

---
+++ packages/Chem/src/admet-analysis/const.ts
@@ -0,0 +1,105 @@
+export const properties: any = {
+    "Absorption": {
+        "name": "Absorption",
+        "description": "Models to predict absorbtion",
+        "models": [
+        {
+            "name": "Caco-2",
+        },
+        {
+            "name": "Pgp-Inhibitor",
+        },
+        {
+            "name": "Pgp-Substrate",
+        },
+        {
+            "name": "HIA",
+        },
+        {
+            "name": "F(20%)",
+        },
+        {
+            "name": "F(30%)",
+        }]
+    },
+    "Distribution": {
+        "name": "Distribution",
+        "description": "Models to predict distribution",
+        "models": [
+        {
+            "name": "PPB",
+        },
+        {
+            "name": "VD",
+        },
+        {
+            "name": "BBB",
+        }]
+    },
+    "Metabolism": {
+        "name": "Metabolism",
+        "description": "Models to predict metabolism",
+        "models": [
+        {
+            "name": "CYP1A2-Inhibitor",
+        },
+        {
+            "name": "CYP1A2-Substrate",
+        },
+        {
+            "name": "CYP3A4-Inhibitor",
+        },
+        {
+            "name": "CYP3A4-Substrate",
+        },
+        {
+            "name": "CYP2C19-Inhibitor",
+        },
+        {
+            "name": "CYP2C19-Substrate",
+        },
+        {
+            "name": "CYP2C9-Inhibitor",
+        },
+        {
+            "name": "CYP2C9-Substrate",
+        },
+        {
+            "name": "CYP2D6-Inhibitor",
+        },
+        {
+            "name": "CYP2D6-Substrate",
+        }]
+    },
+    "Excretion": {
+        "name": "Excretion",
+        "description": "Models to predict excretion",
+        "models": [
+        {
+            "name": "Clearance",
+        },
+        {
+            "name": "T1/2",
+        }]
+    },
+    "Toxicity": {
+        "name": "Toxicity",
+        "description": "Models to predict toxicity",
+        "models": [
+        {
+            "name": "hERG",
+        },
+        {
+            "name": "H-HT",
+        },
+        {
+            "name": "Ames",
+        },
+        {
+            "name": "SkinSen",
+        },
+        {
+            "name": "LD50 of acute toxicity",
+        }]
+    }
+}
