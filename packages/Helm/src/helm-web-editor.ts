6570e59219473f926475b7d4b6c016a14a80a667
Maria Dolotova <maria.dolotova@arcadia.spb.ru>
2022-10-28 17:34:28 +0300

    Helm: fixed freezing in substructure filter

--- packages/Helm/src/helm-web-editor.ts
+++ packages/Helm/src/helm-web-editor.ts
@@ -3,16 +3,23 @@ import * as ui from 'datagrok-api/ui';
 import * as DG from 'datagrok-api/dg';
 
 export class HelmWebEditor {
-  editorView: any;
-  webEditor: any;
   host: any;
   editor: any;
   w = 200;
   h = 100;
   constructor() {
-    this.editorView = ui.div();
+    this.host = ui.div([], {style: {width: `${this.w}px`, height: `${this.h}px`}});
+    this.editor = new JSDraw2.Editor(this.host, {width: this.w, height: this.h, viewonly: true});
+  }
+
+  resizeEditor(w: number, h: number) {
+    this.editor.setSize(w, h);
+  }
+
+  createWebEditor(value: string) {
+    const editorView = ui.div();
     org.helm.webeditor.MolViewer.molscale = 0.8;
-    this.webEditor = new scil.helm.App(this.editorView, {
+    const webEditor = new scil.helm.App(editorView, {
       showabout: false,
       mexfontsize: '90%',
       mexrnapinontab: true,
@@ -22,24 +29,22 @@ export class HelmWebEditor {
       mexfavoritefirst: true,
       mexfilter: true
     });
-    const sizes = this.webEditor.calculateSizes();
-    this.webEditor.canvas.resize(sizes.rightwidth - 100, sizes.topheight - 210);
+    const sizes = webEditor.calculateSizes();
+    webEditor.canvas.resize(sizes.rightwidth - 100, sizes.topheight - 210);
     let s = {width: sizes.rightwidth - 100 + 'px', height: sizes.bottomheight + 'px'};
     //@ts-ignore
-    scil.apply(this.webEditor.sequence.style, s);
+    scil.apply(webEditor.sequence.style, s);
     //@ts-ignore
-    scil.apply(this.webEditor.notation.style, s);
-    s = {width: sizes.rightwidth + 'px', height: (sizes.bottomheight + this.webEditor.toolbarheight) + 'px'};
+    scil.apply(webEditor.notation.style, s);
+    s = {width: sizes.rightwidth + 'px', height: (sizes.bottomheight + webEditor.toolbarheight) + 'px'};
     //@ts-ignore
-    scil.apply(this.webEditor.properties.parent.style, s);
-    this.webEditor.structureview.resize(sizes.rightwidth, sizes.bottomheight + this.webEditor.toolbarheight);
-    this.webEditor.mex.resize(sizes.topheight - 80);
-    this.host = ui.div([], {style: {width: `${this.w}px`, height: `${this.h}px`}});
-    this.editor = new JSDraw2.Editor(this.host, {width: this.w, height: this.h, viewonly: true});
-  }
-
-  resizeEditor(w: number, h: number) {
-    this.editor.setSize(w, h);
+    scil.apply(webEditor.properties.parent.style, s);
+    webEditor.structureview.resize(sizes.rightwidth, sizes.bottomheight + webEditor.toolbarheight);
+    webEditor.mex.resize(sizes.topheight - 80);
+    setTimeout(function() {
+      webEditor.canvas.helm.setSequence(value, 'HELM');
+    }, 200);
+    return {editorDiv: editorView, webEditor: webEditor};
   }
 }
 
