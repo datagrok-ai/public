ce5ebb1d431e7fea395830a4a4cd0b9dafdf868d
tanas80 <atanas@datagrok.ai>
2022-10-31 21:24:44 +0400

    Bio: Fix test substructureFilters/helm

--- packages/Bio/src/tests/activity-cliffs-utils.ts
+++ packages/Bio/src/tests/activity-cliffs-utils.ts
@@ -1,20 +1,21 @@
 import * as DG from 'datagrok-api/dg';
+import * as grok from 'datagrok-api/grok';
+
 import {delay, expect} from '@datagrok-libraries/utils/src/test';
 import {_package} from '../package-test';
-import { activityCliffs } from '../package';
-import * as grok from 'datagrok-api/grok';
+import {activityCliffs} from '../package';
 
 export async function _testActivityCliffsOpen(df: DG.DataFrame, numberCliffs: number, method: string, colName: string) {
   await grok.data.detectSemanticTypes(df);
   const scatterPlot = await activityCliffs(
-     df, 
-     df.col(colName)!, 
-     df.col('Activity')!, 
-     90, 
-     method);
+    df,
+    df.col(colName)!,
+    df.col('Activity')!,
+    90,
+    method);
 
-    expect(scatterPlot != null, true);
+  expect(scatterPlot != null, true);
 
-    const cliffsLink = Array.from(scatterPlot!.root.children).filter(it => it.className === 'ui-btn ui-btn-ok');
-    expect((cliffsLink[0] as HTMLElement).innerText, `${numberCliffs} cliffs`);
+  const cliffsLink = Array.from(scatterPlot!.root.children).filter(it => it.className === 'ui-btn ui-btn-ok');
+  expect((cliffsLink[0] as HTMLElement).innerText.toLowerCase(), `${numberCliffs} cliffs`);
 }
