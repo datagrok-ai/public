683ea0227797333b3367fac7514fbe10318427f8
Alex Paramonov <alex.aprm@gmail.com>
2022-10-31 18:23:51 +0300

    Bump ApiTests version

--- packages/ApiTests/src/dataframe/calculated-columns.ts
+++ packages/ApiTests/src/dataframe/calculated-columns.ts
@@ -28,43 +28,47 @@ category('DataFrame', () => {
 
   test('Add new column dialog', () => new Promise(async (resolve, reject) => {
     if ((await grok.dapi.packages.filter('PowerPack').list()).length > 0)
-      return;
-    let tv: DG.TableView;
-    subs.push(grok.events.onDialogShown.subscribe((d) => {
-      if (d.title == 'Add New Column')
-        resolve('OK');
-      dialogs.push(d);
-    }));
-    setTimeout(() => {
-      // eslint-disable-next-line prefer-promise-reject-errors
-      reject('Dialog not found');
-    }, 1000);
-    try {
-      tv = grok.shell.addTableView(df);
-      await df.dialogs.addNewColumn();
-    } finally {
-      tv!.close();
-      grok.shell.closeTable(df);
+      resolve('Skipped because PowerPack is installed');
+    else {
+      let tv: DG.TableView;
+      subs.push(grok.events.onDialogShown.subscribe((d) => {
+        if (d.title == 'Add New Column')
+          resolve('OK');
+        dialogs.push(d);
+      }));
+      setTimeout(() => {
+        // eslint-disable-next-line prefer-promise-reject-errors
+        reject('Dialog not found');
+      }, 1000);
+      try {
+        tv = grok.shell.addTableView(df);
+        await df.dialogs.addNewColumn();
+      } finally {
+        tv!.close();
+        grok.shell.closeTable(df);
+      }
     }
   }));
 
   test('Edit formula dialog', () => new Promise(async (resolve, reject) => {
     if ((await grok.dapi.packages.filter('PowerPack').list()).length > 0)
-      return;
-    subs.push(grok.events.onDialogShown.subscribe((d) => {
-      if (d.title == 'Add New Column')
-        resolve('OK');
-      dialogs.push(d);
-    }));
-    try {
-      setTimeout(() => {
-        // eslint-disable-next-line prefer-promise-reject-errors
-        reject('Dialog not found');
-      }, 1000);
-      const column = await df.columns.addNewCalculated('editable', '0');
-      column.dialogs.editFormula();
-    } finally {
-      df.columns.remove('editable');
+      resolve('Skipped because PowerPack is installed');
+    else {
+      subs.push(grok.events.onDialogShown.subscribe((d) => {
+        if (d.title == 'Add New Column')
+          resolve('OK');
+        dialogs.push(d);
+      }));
+      try {
+        setTimeout(() => {
+          // eslint-disable-next-line prefer-promise-reject-errors
+          reject('Dialog not found');
+        }, 1000);
+        const column = await df.columns.addNewCalculated('editable', '0');
+        column.dialogs.editFormula();
+      } finally {
+        df.columns.remove('editable');
+      }
     }
   }));
 
