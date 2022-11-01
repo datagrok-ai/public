5fb4d2c8c2bfee2e4f7e970e31486afd0e22cef6
tanas80 <atanas@datagrok.ai>
2022-11-01 16:20:11 +0400

    utils lib: Fix expectedObject for float tolerance

---
+++ libraries/utils/src/to-console.ts
@@ -0,0 +1,18 @@
+import * as ui from 'datagrok-api/ui';
+import * as grok from 'datagrok-api/grok';
+import * as DG from 'datagrok-api/dg';
+
+
+export function errorToConsole(err: any): string {
+  if ('$thrownJsError' in err) {
+    return errorToConsole(err['$thrownJsError']);
+  } else if (err instanceof Error) {
+    return (err as Error).stack ?? (err as Error).message;
+  } else {
+    return err.toString();
+  }
+}
+
+export function rectToConsole(rect: DG.Rect): string {
+  return `(x=${rect.x}, y=${rect.y}, w=${rect.width}, h=${rect.height})`;
+}
