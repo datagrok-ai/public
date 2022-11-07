ed761c047942383da4cee41e45d3a9bcf78f1c01
vmakarichev <vmakarichev@datagrok.ai>
2022-11-07 19:07:36 +0300

    Updated PCA

--- packages/vmakarichev-datagrok_with_cpp/eigenPCA/src/package.js
+++ packages/vmakarichev-datagrok_with_cpp/eigenPCA/src/package.js
@@ -29,17 +29,88 @@ function pca(module, sourceArrays, numOfPrincipalComponents, principalComponents
   let shift = 2;
   let heap = module.HEAPF32;
 
+  // BUFFERS ALLOCATION
+
+  let buffers = [];
+  let excpectedNumOfBuffers = 3; // 1st - for source data, 
+                                 // 2nd - for principal components
+                                 // 3rd - for approximated data
+
+  let bufForData = module._malloc(sizeOfData * numOfBytes);
+  if(bufForData != 0)
+      buffers.push(bufForData);
+
+  let bufForPrincComp = module._malloc(sizeOfPrincComp * numOfBytes);
+  if(bufForPrincComp != 0)
+      buffers.push(bufForPrincComp);
+
+  let bufForApprox = module._malloc(sizeOfData * numOfBytes);
+  if(bufForApprox != 0)
+      buffers.push(bufForApprox);
+  
+  console.log("Allocated buffers:");
+  console.log(buffers);
+
+  // check memory allocation
+  if(buffers.length == excpectedNumOfBuffers) {
+
+    console.log('Success: the required memory is allocated!');
+
+      // assign allocated buffer with source data
+      for(let i = 0; i < height; i++)
+          heap.set(sourceArrays[i], (bufForData + i * width * numOfBytes) >> shift);
+    
+      // EXPORTED METHOD CALL
+      let res = module.ccall('principalComponentAnalysis', 
+      'number', ['number', 'number', 'number', 'number', 'number', 'number'], 
+      [bufForData, height, width, numOfPrincipalComponents, bufForPrincComp, bufForApprox]);
+
+      // get results: principal components    
+      for(let i = 0; i < numOfPrincipalComponents; i++)
+          principalComponents.push(new Float32Array(heap.buffer, 
+              bufForPrincComp + i * width * numOfBytes, width));
+
+      // get results: approximation of source data
+      for(let i = 0; i < height; i++)
+          approxData.push(new Float32Array(heap.buffer, 
+              bufForApprox + i * width * numOfBytes, width));
+  
+      // evaluate error - maximum absolute deviation  <-- OPTIONAL
+      console.log('Maximum absolute deviation: ' + module._error(bufForData, bufForApprox, sizeOfData));
+
+  } // if
+  else { // i.e. memory allocation fail
+      console.log('FAIL:the required memory is not allocated!');
+  }
+  
+  // clear allocted memory
+  for(let i = 0; i < buffers.length; i++)
+      module._free(buffers[i]);  
+}
+
+/*// A wrapper for PCA by Eigen
+function pca(module, sourceArrays, numOfPrincipalComponents, principalComponents, approxData ){
+ 
+  // sizes
+  let height = sourceArrays.length;
+  let width = sourceArrays[0].length;
+  let sizeOfData = height * width;
+  let sizeOfPrincComp = numOfPrincipalComponents * width; 
+  let numOfBytes = sourceArrays[0].BYTES_PER_ELEMENT;
+
+  // heap
+  let shift = 2;
+  let heap = module.HEAPF32;
+
   // allocate buffer for source data
   let bufForData = module._malloc(sizeOfData * numOfBytes);
+  let bufForPrincComp = module._malloc(sizeOfPrincComp * numOfBytes);
+  let bufForApprox = module._malloc(sizeOfData * numOfBytes);
 
   // assign allocated buffer with source data
   for(let i = 0; i < height; i++)
       heap.set(sourceArrays[i], (bufForData + i * width * numOfBytes) >> shift);
-  
-  // allocate buffers
-  let bufForPrincComp = module._malloc(sizeOfPrincComp * numOfBytes);
-  let bufForApprox = module._malloc(sizeOfData * numOfBytes);
-
+    
   // EXPORTED METHOD CALL
   let res = module.ccall('principalComponentAnalysis', 
   'number', ['number', 'number', 'number', 'number', 'number', 'number'], 
@@ -56,11 +127,14 @@ function pca(module, sourceArrays, numOfPrincipalComponents, principalComponents
       approxData.push(new Float32Array(heap.buffer, 
           bufForApprox + i * width * numOfBytes, width));
   
+  // evaluate error - maximum absolute deviation  <-- OPTIONAL
+  console.log("Maximum absolute deviation: " + module._error(bufForData, bufForApprox, sizeOfData));
+  
   // clear allocted memory
   module._free(bufForData);
   module._free(bufForPrincComp);
   module._free(bufForApprox);
-}
+}*/
 
 //name: testOfPCA
 //input: dataframe df
@@ -88,11 +162,24 @@ export function testOfPCA(df, numOfComponents) {
     }
     names.push(col.name);
   }
-
+ 
   // PRINCIPAL COMPONENT ANALYSIS
+  console.log('PCA ...');
+
+  let start = new Date().getTime(); 
+
   pca(NewModule, data, numOfComponents, princComp, approx);
 
+  let finish = new Date().getTime();
+
+  console.log('Principal components and approximation of source data have been computed!');
+  console.log('Time for PCA execution: ' + (finish - start) + ' ms.');
+
   // Create dataframe with approximation
+  console.log('Creating dataframes from the arrays computed ...');
+
+  start = new Date().getTime();
+  
   let approxCols = [];  
   for(let i = 0; i < data.length; i++)
     approxCols.push(DG.Column.fromFloat32Array(names[i] + '(approx)', approx[i]));  
@@ -107,4 +194,9 @@ export function testOfPCA(df, numOfComponents) {
   let tableWithPrincComp = DG.DataFrame.fromColumns(princCompCols);
   tableWithPrincComp.name = 'PrincipalComponents';
   grok.shell.addTableView(tableWithPrincComp);
+
+  finish = new Date().getTime();
+
+  console.log('Dataframes and their views have been created!');
+  console.log('Time for creating dataframes and their views: ' + (finish - start) + ' ms.');
 }
