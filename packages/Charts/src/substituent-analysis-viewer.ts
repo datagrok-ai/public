95a15597b209c49dd7e7f4d4effb269dcb481aeb
Maria Dolotova <maria.dolotova@arcadia.spb.ru>
2022-11-08 12:41:54 +0300

    Charts: substituent analysis viewer in progress

--- packages/Charts/src/substituent-analysis-viewer.ts
+++ packages/Charts/src/substituent-analysis-viewer.ts
@@ -1,7 +1,15 @@
 import * as ui from 'datagrok-api/ui';
 import * as DG from 'datagrok-api/dg';
 import * as grok from 'datagrok-api/grok';
+import { Grid } from 'datagrok-api/dg';
 
+const CHART_TYPES = {
+  histogram: DG.VIEWER.HISTOGRAM,
+  barchart: DG.VIEWER.BAR_CHART,
+  linechart: DG.VIEWER.LINE_CHART,
+  piechart: DG.VIEWER.PIE_CHART,
+};
+const CHART_TYPE_TAG = 'chart_type';
 export class SubstituentAnalysisViewer extends DG.JsViewer {
   initialized: boolean = false;
   name = 'substituent';
@@ -11,7 +19,6 @@ export class SubstituentAnalysisViewer extends DG.JsViewer {
   grouppedDf: DG.DataFrame | undefined = undefined;
   grouppedGridDiv = ui.div();
   mainView = ui.splitV([]);
-  historgamMode: DG.InputBase<boolean> | undefined = undefined;
 
   constructor() {
     super();
@@ -34,11 +41,6 @@ export class SubstituentAnalysisViewer extends DG.JsViewer {
     this.updateColumnChoices(this.colsToAnalyze, 'Values', colsToAnalyzeDiv);
     this.mainView.append(ui.box(ui.panel([grouppingColsDiv], {style: {padding: '0px'}}), {style: {maxHeight: '30px'}}));
     this.mainView.append(ui.box(ui.panel([colsToAnalyzeDiv], {style: {padding: '0px'}}), {style: {maxHeight: '30px'}}));
-    this.historgamMode = ui.switchInput('Historgam', true, () => {
-      this.updateGrid();
-    });
-    this.mainView.append(
-      ui.box(ui.panel([this.historgamMode.root], {style: {padding: '0px'}}), {style: {maxHeight: '30px'}}));
     this.mainView.append(this.grouppedGridDiv);
     this.root.append(this.mainView);
   }
@@ -81,35 +83,49 @@ export class SubstituentAnalysisViewer extends DG.JsViewer {
       grid.col(col)!.cellType = 'html';
       grid.col(col)!.width = 150;
     }
+
     grid.onCellPrepare((gc) => {
       if (gc.isTableCell && gc.cell.value != null && this.colsToAnalyze.includes(gc.gridColumn.name)) {
         const condition: {[key: string]: any} = {};
         for (const col of this.grouppingCols)
           condition[col] = this.grouppedDf!.get(col, gc.tableRowIndex!);
-        const df = this.dataFrame.clone(null, this.grouppingCols.concat([gc.gridColumn.name]));
-        if (this.historgamMode!.value) {
-          gc.style.element = df.plot
-            .histogram({value: gc.gridColumn.name,
-              showColumnSelector: false, showFilteredOutRows: true, filteringEnabled: false}).root;
-          df.rows.match(condition).filter();
-          console.log(`filter count: ${df.filter.trueCount}`);
-          const test = df.rows.match(condition).toDataFrame();
-          console.log(`df rowCount: ${test.rowCount}`);
-        } else {
-          df.columns.addNewString('group').init((j) => {
-            for (const group of this.grouppingCols) {
-              if (df.get(group, j) !== this.grouppedDf!.get(group, gc.tableRowIndex!))
-                return 'other';
-            }
-            return 'current_group';
-          });
-          gc.style.element = gc.style.element = df.plot
-            .histogram({value: gc.gridColumn.name, split: 'group', bins: 5,
-              showColumnSelector: false, showFilteredOutRows: true, filteringEnabled: false}).root;
+        const colList = [];
+        this.grouppingCols.forEach((col) => colList.push(this.dataFrame.col(col)!));
+        colList.push(this.dataFrame.col(gc.gridColumn.name)!);
+        const df = DG.DataFrame.fromColumns(colList);
+        df.columns.addNewString('group').init((j) => {
+          for (const group of this.grouppingCols) {
+            if (df.get(group, j) !== this.grouppedDf!.get(group, gc.tableRowIndex!))
+              return 'other';
+          }
+          return 'current_group';
+        });
+        let chartType = this.dataFrame.col(gc.gridColumn.name)!.getTag(CHART_TYPE_TAG);
+        if (!chartType) {
+          chartType = Object.keys(CHART_TYPES)[0];
+          this.dataFrame.col(gc.gridColumn.name)!.setTag(CHART_TYPE_TAG, Object.keys(CHART_TYPES)[0]);
         }
+        const viewer = DG.Viewer.fromType((CHART_TYPES as any)[chartType], df);
+        gc.element = viewer.root;
+        df.rows.match(condition).filter();
       }
     });
+
+    grid.onCellClick.subscribe((gc) => {
+      if (!gc.isTableCell && this.colsToAnalyze.includes(gc.gridColumn.name))
+        this.createPropertyPanel(gc.gridColumn.name, this.dataFrame.col(gc.gridColumn.name)!.getTag(CHART_TYPE_TAG), grid);
+    });
+
     ui.empty(this.grouppedGridDiv);
     this.grouppedGridDiv.append(grid.root);
+  }
+
+  createPropertyPanel(colName: string, chartType: string, grid: DG.Grid) {
+    const chartChoice = ui.choiceInput('Chart', chartType, Object.keys(CHART_TYPES));
+    chartChoice.onChanged(() => {
+      this.dataFrame.col(colName)!.setTag(CHART_TYPE_TAG, chartChoice.value!);
+      grid.invalidate();
+    });
+    grok.shell.o = chartChoice.root;
   }
 }
