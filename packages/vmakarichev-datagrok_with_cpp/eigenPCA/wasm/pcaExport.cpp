cecf5c2202400d7c1fe9cd3b007430d3b802c12d
vmakarichev <vmakarichev@datagrok.ai>
2022-11-09 13:51:11 +0300

    Max. absolute deviation error computing is added.

--- packages/vmakarichev-datagrok_with_cpp/eigenPCA/wasm/pcaExport.cpp
+++ packages/vmakarichev-datagrok_with_cpp/eigenPCA/wasm/pcaExport.cpp
@@ -5,20 +5,45 @@ extern "C" {
 	int height,
 	int width,
 	int numOfPrincipalComponents,
+	float * principalComponents);
+
+	int pcaWithApproximation(float * data,
+	int height,
+	int width,
+	int numOfPrincipalComponents,
 	float * principalComponents,
-	float * approxData = 0);
+	float * approximation);
+
+	float error(float * data1, float * data2, int length);
 }
 
 #include "PCA\PCA.h"
-//#include "PCA\pca.cpp"
 
 EMSCRIPTEN_KEEPALIVE
 int principalComponentAnalysis(float * data,
 	int height,
 	int width,
 	int numOfPrincipalComponents,
-	float * principalComponents,
-	float * approxData)
+	float * principalComponents)
 {
-	return pca::pcaUsingCorrelationMatrix(data, height, width, numOfPrincipalComponents, principalComponents, approxData);
-} // principalComponentAnalysis
+	return pca::pcaUsingCorrelationMatrix(data, height, width, numOfPrincipalComponents, principalComponents, 0);
+}
+
+EMSCRIPTEN_KEEPALIVE
+int pcaWithApproximation(float * data,
+	int height,
+	int width,
+	int numOfPrincipalComponents,
+	float * principalComponents,
+	float * approximation)
+{
+	return pca::pcaUsingCorrelationMatrix(data, height, width, numOfPrincipalComponents, principalComponents, approximation);
+} 
+
+EMSCRIPTEN_KEEPALIVE
+float error(float * data1, float * data2, int length)
+{
+	return pca::mad(data1, data2, length);
+} 
+
+
