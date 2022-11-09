8211923014331b77b7c4d53e06097209fd5894ed
Volodymyr Dyma <vdyma@datagrok.ai>
2022-11-09 16:20:16 +0200

    #1119: moved out mutation cliffs calculation

--- packages/Peptides/src/model.ts
+++ packages/Peptides/src/model.ts
@@ -9,7 +9,7 @@ import * as rxjs from 'rxjs';
 
 import * as C from './utils/constants';
 import * as type from './utils/types';
-import {getTypedArrayConstructor, isGridCellInvalid, scaleActivity} from './utils/misc';
+import {isGridCellInvalid, scaleActivity} from './utils/misc';
 import {MutationCliffsViewer, MostPotentResiduesViewer} from './viewers/sar-viewer';
 import * as CR from './utils/cell-renderer';
 import {mutationCliffsWidget} from './widgets/mutation-cliffs';
@@ -19,6 +19,7 @@ import {LogoSummary} from './viewers/logo-summary';
 import {getSettingsDialog} from './widgets/settings';
 import {getMonomerWorks} from './package';
 import * as bio from '@datagrok-libraries/bio';
+import {findMutations} from './utils/algorithms';
 
 export class PeptidesModel {
   static modelName = 'peptidesModel';
@@ -247,7 +248,9 @@ export class PeptidesModel {
     // SAR vertical table (naive, choose best Mean difference from pVals <= 0.01)
     const sequenceDf = this.createVerticalTable();
 
-    this.calcSubstitutions();
+    const scaledActivityCol = this.df.getCol(C.COLUMNS_NAMES.ACTIVITY_SCALED);
+    const monomerColumns = this.df.columns.bySemTypeAll(C.SEM_TYPES.MONOMER);
+    this.substitutionsInfo = findMutations(scaledActivityCol, monomerColumns, this.settings);
 
     [this.mutationCliffsGrid, this.mostPotentResiduesGrid] =
       this.createGrids(matrixDf, sequenceDf, positionColumns, alphabet);
@@ -274,91 +277,6 @@ export class PeptidesModel {
     this.setBitsetCallback();
 
     this.postProcessGrids();
-  }
-
-  //TODO: move out
-  calcSubstitutions(): void {
-    const activityValues: DG.Column<number> = this.df.columns.bySemType(C.SEM_TYPES.ACTIVITY_SCALED)!;
-    const columnList: DG.Column<string>[] = this.df.columns.bySemTypeAll(C.SEM_TYPES.MONOMER);
-    const nCols = columnList.length;
-    if (nCols == 0)
-      throw new Error(`Couldn't find any column of semType '${C.SEM_TYPES.MONOMER}'`);
-
-    this.substitutionsInfo = new Map();
-    const nRows = this.df.rowCount;
-    for (let seq1Idx = 0; seq1Idx < nRows - 1; seq1Idx++) {
-      for (let seq2Idx = seq1Idx + 1; seq2Idx < nRows; seq2Idx++) {
-        let substCounter = 0;
-        const activityValSeq1 = activityValues.get(seq1Idx)!;
-        const activityValSeq2 = activityValues.get(seq2Idx)!;
-        const delta = activityValSeq1 - activityValSeq2;
-        if (Math.abs(delta) < (this.settings.minActivityDelta ?? 0))
-          continue;
-
-        let substCounterFlag = false;
-        const tempData: { pos: string, seq1monomer: string, seq2monomer: string, seq1Idx: number, seq2Idx: number }[] =
-          [];
-        for (const currentPosCol of columnList) {
-          const seq1monomer = currentPosCol.get(seq1Idx)!;
-          const seq2monomer = currentPosCol.get(seq2Idx)!;
-          if (seq1monomer == seq2monomer)
-            continue;
-
-          substCounter++;
-          substCounterFlag = substCounter > (this.settings.maxMutations ?? 1);
-          if (substCounterFlag)
-            break;
-
-          tempData.push({
-            pos: currentPosCol.name,
-            seq1monomer: seq1monomer,
-            seq2monomer: seq2monomer,
-            seq1Idx: seq1Idx,
-            seq2Idx: seq2Idx,
-          });
-        }
-
-        if (substCounterFlag || substCounter == 0)
-          continue;
-
-        for (const tempDataElement of tempData) {
-          const position = tempDataElement.pos;
-
-          //Working with seq1monomer
-          const seq1monomer = tempDataElement.seq1monomer;
-          if (!this.substitutionsInfo.has(seq1monomer))
-            this.substitutionsInfo.set(seq1monomer, new Map());
-
-          let positionsMap = this.substitutionsInfo.get(seq1monomer)!;
-          if (!positionsMap.has(position))
-            positionsMap.set(position, new Map());
-
-          let indexes = positionsMap.get(position)!;
-
-          !indexes.has(seq1Idx) ? indexes.set(seq1Idx, [seq2Idx]) : (indexes.get(seq1Idx)! as number[]).push(seq2Idx);
-
-          //Working with seq2monomer
-          const seq2monomer = tempDataElement.seq2monomer;
-          if (!this.substitutionsInfo.has(seq2monomer))
-            this.substitutionsInfo.set(seq2monomer, new Map());
-
-          positionsMap = this.substitutionsInfo.get(seq2monomer)!;
-          if (!positionsMap.has(position))
-            positionsMap.set(position, new Map());
-
-          indexes = positionsMap.get(position)!;
-          !indexes.has(seq2Idx) ? indexes.set(seq2Idx, [seq1Idx]) : (indexes.get(seq2Idx)! as number[]).push(seq1Idx);
-        }
-      }
-    }
-
-    const TypedArray = getTypedArrayConstructor(nRows);
-    for (const positionMap of this.substitutionsInfo.values()) {
-      for (const indexMap of positionMap.values()) {
-        for (const [index, indexArray] of indexMap.entries())
-          indexMap.set(index, new TypedArray(indexArray));
-      }
-    }
   }
 
   initSelections(positionColumns: string[]): void {
