81489f2d9b08da1086ecb07a20b485b3de3a94ea
Oleksandra Serhiienko <59337516+Aleksashka11@users.noreply.github.com>
2022-11-15 17:09:41 +0200

    GROK-11410: Charts | Radar viewer: toggling 'Show Min' and 'Show Max' issues (show percentiles issue + show values issue fixed)

--- packages/Charts/src/radar/radar-viewer.ts
+++ packages/Charts/src/radar/radar-viewer.ts
@@ -1,5 +1,6 @@
 import * as DG from 'datagrok-api/dg';
 import * as ui from 'datagrok-api/ui';
+import * as grok from 'datagrok-api/grok';
 import * as echarts from 'echarts';
 import {option} from '../radar/constants';
 
@@ -35,35 +36,34 @@ export class RadarViewer extends DG.JsViewer {
     this.showValues = this.bool('showValues', true);
     this.columnNames = this.stringList('columnNames', null);
   
-    const chartDiv = ui.div([], { style: { position: 'absolute', left: '0', right: '0', top: '0', bottom: '0'}} );
+    const chartDiv = ui.div([], { id: 'chart' ,style: { position: 'absolute', left: '0', right: '0', top: '0', bottom: '0'}} );
     this.root.appendChild(chartDiv);
     this.myChart = echarts.init(chartDiv);
     this.subs.push(ui.onSizeChanged(chartDiv).subscribe((_) => this.myChart.resize()));
   }
 
   init() {
-    option.radar.indicator = [];
-    const columns = this.getColumns();
-    for (const c of columns) {
-      option.radar.indicator.push({name: c.name, max: c.max});
-    }
-    this.updateMin();
-    this.updateMax();
-    this.updateRow();
-    //@ts-ignore
-    this.myChart.on('mouseover', function(params) {
-      if (params.componentType === 'series') {
-        if (params.seriesIndex === 2) {
-          let divs: HTMLElement[] = [];
-          for (let i = 0; i < columns.length; ++i) {
-            divs[i] = ui.divText(`${columns[i].name} : ${params.data.value[i]}`);
-          } 
-          ui.tooltip.show(ui.div(divs), params.event.event.x,  params.event.event.y);
-        }
+      option.radar.indicator = [];
+      const columns = this.getColumns();
+      for (const c of columns) {
+        option.radar.indicator.push({name: c.name, max: c.max});
       }
-    });
-    this.myChart.on('mouseout', () => ui.tooltip.hide());
-    this.helpUrl = 'https://raw.githubusercontent.com/datagrok-ai/public/master/help/visualize/viewers/radar-viewer.md';
+      this.updateMin();
+      this.updateMax();
+      this.updateRow();
+      //@ts-ignore
+      this.myChart.on('mouseover', function(params) {
+        if (params.componentType === 'series') {
+          if (params.seriesIndex === 2) {
+            let divs: HTMLElement[] = [];
+            for (let i = 0; i < columns.length; ++i) {
+              divs[i] = ui.divText(`${columns[i].name} : ${params.data.value[i]}`);
+            } 
+            ui.tooltip.show(ui.div(divs), params.event.event.x,  params.event.event.y);
+          }
+        }
+      });
+      this.myChart.on('mouseout', () => ui.tooltip.hide());
   }
 
   onTableAttached() {
@@ -86,25 +86,27 @@ export class RadarViewer extends DG.JsViewer {
     super.onPropertyChanged(property);
     switch (property.name) {
       case 'min':
-        this.updateMin();
+        if (this.showMin === true) {
+          this.updateMin();
+        }
         break;
       case 'max':
-        this.updateMax();
+        if (this.showMax === true) {
+          this.updateMax();
+        }
         break;
       case 'showMin':
         if (this.showMin != true) {
           this.clearData([0]);
         } else {
-          this.clearData([1, 2]);
-          this.init();
+          this.updateMin();
         }
         break;
       case 'showMax':
         if (this.showMax != true) {
           this.clearData([1]);
         } else {
-          this.clearData([0, 2]);
-          this.init();
+          this.updateMax();
         }
         break;
       case 'showCurrentRow':
@@ -171,7 +173,6 @@ export class RadarViewer extends DG.JsViewer {
         this.init();
         break;
     }
-
     this.render();
   }
 
