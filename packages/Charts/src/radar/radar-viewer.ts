5413925f93ab1c140ebe2affdb483b752214e279
Oleksandra Serhiienko <59337516+Aleksashka11@users.noreply.github.com>
2022-11-15 17:16:12 +0200

    RadarViewer: Updating init function

--- packages/Charts/src/radar/radar-viewer.ts
+++ packages/Charts/src/radar/radar-viewer.ts
@@ -36,34 +36,35 @@ export class RadarViewer extends DG.JsViewer {
     this.showValues = this.bool('showValues', true);
     this.columnNames = this.stringList('columnNames', null);
   
-    const chartDiv = ui.div([], { id: 'chart' ,style: { position: 'absolute', left: '0', right: '0', top: '0', bottom: '0'}} );
+    const chartDiv = ui.div([], { style: { position: 'absolute', left: '0', right: '0', top: '0', bottom: '0'}} );
     this.root.appendChild(chartDiv);
     this.myChart = echarts.init(chartDiv);
     this.subs.push(ui.onSizeChanged(chartDiv).subscribe((_) => this.myChart.resize()));
   }
 
   init() {
-      option.radar.indicator = [];
-      const columns = this.getColumns();
-      for (const c of columns) {
-        option.radar.indicator.push({name: c.name, max: c.max});
-      }
-      this.updateMin();
-      this.updateMax();
-      this.updateRow();
-      //@ts-ignore
-      this.myChart.on('mouseover', function(params) {
-        if (params.componentType === 'series') {
-          if (params.seriesIndex === 2) {
-            let divs: HTMLElement[] = [];
-            for (let i = 0; i < columns.length; ++i) {
-              divs[i] = ui.divText(`${columns[i].name} : ${params.data.value[i]}`);
-            } 
-            ui.tooltip.show(ui.div(divs), params.event.event.x,  params.event.event.y);
-          }
+    option.radar.indicator = [];
+    const columns = this.getColumns();
+    for (const c of columns) {
+      option.radar.indicator.push({name: c.name, max: c.max});
+    }
+    this.updateMin();
+    this.updateMax();
+    this.updateRow();
+    //@ts-ignore
+    this.myChart.on('mouseover', function(params) {
+      if (params.componentType === 'series') {
+        if (params.seriesIndex === 2) {
+          let divs: HTMLElement[] = [];
+          for (let i = 0; i < columns.length; ++i) {
+            divs[i] = ui.divText(`${columns[i].name} : ${params.data.value[i]}`);
+          } 
+          ui.tooltip.show(ui.div(divs), params.event.event.x,  params.event.event.y);
         }
-      });
-      this.myChart.on('mouseout', () => ui.tooltip.hide());
+      }
+    });
+    this.myChart.on('mouseout', () => ui.tooltip.hide());
+    this.helpUrl = 'https://raw.githubusercontent.com/datagrok-ai/public/master/help/visualize/viewers/radar-viewer.md';
   }
 
   onTableAttached() {
