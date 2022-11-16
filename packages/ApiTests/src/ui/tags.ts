e83f64e6c8f41e4bf5717b2c231f8c54fe5b0845
Anna Muzychyna <annamuz69@gmail.com>
2022-11-16 22:00:48 +0200

    UI Tests fixes

--- packages/ApiTests/src/ui/tags.ts
+++ packages/ApiTests/src/ui/tags.ts
@@ -13,7 +13,6 @@ async function testTags(obj: any, apiPath: any, error: string) {
   if (len !== 1) throw new Error(`Expected 1 ${error}, got ${len}`);
 }
 
-
 category('UI: Tags', () => {
   let v: DG.ViewBase;
   let v1: DG.View;
@@ -25,38 +24,30 @@ category('UI: Tags', () => {
     v = grok.shell.v;
   });
 
-
   test('filter.projects', async () => {
     const prapi = await grok.dapi.projects
       .filter('#demo')
       .list()
       .then(projects => projects.length);
-
     await waitForHTMLElement('.grok-items-view-counts', /[0-9]+ \/ [0-9]+/g, 'Error: cannot load Projects!')
     const search = v.root.querySelector('.ui-input-editor') as HTMLInputElement;
     search.value = '#demo';
     search.dispatchEvent(new Event('input'));
     const regex = new RegExp(`[0-9]+ / ${prapi}`, 'g');
-
     await waitForHTMLElement('.grok-items-view-counts', regex, 'Error: Number of projects does not match!');
   });
 
-
   test('tag.editor', async () => {
     v1 = grok.shell.newView('Tag Editor');
-
     const editor = DG.TagEditor.create();
     editor.addTag('demo');
     editor.addTag('test');
     editor.addTag('1234');
-    
     v1.append(editor.root);
     const tags = v1.root.querySelectorAll('.d4-tag').length;
     expect(tags, 3);
-
     v1.close();
   });
-
 
   test('tag.add', async () => {
     await testTags(DG.Project.create(), grok.dapi.projects, 'project');
@@ -67,7 +58,6 @@ category('UI: Tags', () => {
     }), grok.dapi.connections, 'connection');
     await testTags(DG.Script.create('apitests'), grok.dapi.scripts, 'script');
   });
-
 
   after(async () => {
     v.close();
--- packages/ApiTests/src/ui/users.ts
+++ packages/ApiTests/src/ui/users.ts
@@ -15,31 +15,25 @@ category('UI: Users', () => {
     await usel.click();
     v = grok.shell.v;
     tb = v.toolbox;
-
     const filters = Array.from(tb.querySelectorAll('div.d4-accordion-pane-header'))
       .find(el => el.textContent === 'Filters') as HTMLElement;
     if (!filters.classList.contains('expanded')) await filters.click();
-
     const actions = Array.from(tb.querySelectorAll('div.d4-accordion-pane-header'))
       .find(el => el.textContent === 'Actions') as HTMLElement;
     if (!actions.classList.contains('expanded')) actions.click();
   });
-
 
   test('filters.all', async () => {
     const usapi = await grok.dapi.users
       .list()
       .then(users => users.length);
     const regex = new RegExp(`[0-9]+ / ${usapi}`, 'g');
-
     const all = Array.from(tb.querySelectorAll('label'))
       .find(el => el.textContent === 'All');
     if (all === undefined) throw 'Error: cannot find All!';
     await all.click();
-
     await waitForHTMLElement('.grok-items-view-counts', regex, 'Number of users does not match!');
   });
-
 
   test('filters.recentlyJoined', async () => {
     const usapi = await grok.dapi.users
@@ -47,17 +41,14 @@ category('UI: Users', () => {
       .list()
       .then(users => users.length);
     const regex = new RegExp(`[0-9]+ / ${usapi}`, 'g');
-
     const rj = Array.from(tb.querySelectorAll('label'))
       .find(el => el.textContent === 'Recently joined');
     if (rj === undefined) throw 'Error: cannot find Recently Joined!';
     await rj.click();
-
     setTimeout(async () => {
       await waitForHTMLElement('.grok-items-view-counts', regex, 'Number of users does not match!');
     }, 10)
   });
-
 
   test('actions.addUser', async () => {
     const user = DG.User.create();
@@ -65,42 +56,39 @@ category('UI: Users', () => {
     user.status = DG.USER_STATUS.STATUS_NEW;
     user.firstName = 'new';
     user.lastName = 'user';
-
     // TODO: add save and delete when will work
   });
-
 
   test('actions.addServiceUser', async () => {
     const asu = Array.from(tb.querySelectorAll('label'))
       .find(el => el.textContent === 'Add Service User...');
     if (asu === undefined) throw 'Error: cannot find Add Service User!';
     await asu.click();
-
     const diag = document.getElementsByClassName('d4-dialog');
     if (diag.length === 0) throw 'Error: Add Service User does not work!';
     const cancel = diag[0].querySelector('[class="ui-btn ui-btn-ok"]') as HTMLElement;
     cancel.click();
   });
 
-
   test('actions.inviteFriend', async () => {
     const iaf = Array.from(tb.querySelectorAll('label'))
       .find(el => el.textContent === 'Invite a Friend...');
     if (iaf === undefined) throw 'Error: cannot find Invite a Friend!';
     await iaf.click();
-
     const diag = document.getElementsByClassName('d4-dialog');
     if (diag.length === 0) throw 'Error: Invite a Friend does not work!';
     const cancel = diag[0].querySelector('[class="ui-btn ui-btn-ok"]') as HTMLElement;
     cancel.click();
   });
 
-
   test('user.panel', async () => {
+    const all = Array.from(tb.querySelectorAll('label'))
+      .find(el => el.textContent === 'All');
+    if (all === undefined) throw 'Error: cannot find All!';
+    await all.click();
     const user = (await waitForHTMLCollection('.grok-gallery-grid'))[0] as HTMLElement;
     grok.shell.windows.showProperties = true;
     user.click();
-
     const regex = new RegExp('Groups', 'g');
     const userInfo = await waitForHTMLElement('.grok-entity-prop-panel', regex, 'Error in .grok-entity-prop-panel!');
     const pict = userInfo.querySelector('.grok-user-profile-picture');
@@ -108,7 +96,6 @@ category('UI: Users', () => {
     const b = (pict !== null) && desc.includes('Groups') && desc.includes('Joined');
     expect(b, true);
   });
-
 
   after(async () => {
     v.close();
