b1d51aab9431d24a2ca25585614e30dc8dabf3b5
Alex Paramonov <alex.aprm@gmail.com>
2022-11-17 15:55:03 +0300

    Fixed categories

--- packages/ApiTests/src/package-test.ts
+++ packages/ApiTests/src/package-test.ts
@@ -49,6 +49,7 @@ import './viewers/viewers';
 //import './gui/chem-sketcher';
 import './grid/grid';
 import './connections/queries-test';
+import './scripts/scripts-params';
 
 import {runTests, tests, TestContext} from '@datagrok-libraries/utils/src/test';
 export const _package = new DG.Package();
