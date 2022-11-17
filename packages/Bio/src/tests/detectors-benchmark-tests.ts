cba5b16c3a66200089b66d5d8a9a59461670d128
tanas80@gmail.com <tanas80@gmail.com>
2022-11-17 14:35:02 +0400

    Bio: detectMacromolecule benchmark tests skipReason for FASTA notation tests

--- packages/Bio/src/tests/detectors-benchmark-tests.ts
+++ packages/Bio/src/tests/detectors-benchmark-tests.ts
@@ -23,34 +23,36 @@ category('detectorsBenchmark', () => {
   // -- fasta --
 
   test('fastaDnaShorts50Few50', async () => {
-    const et: number = await benchmarkDetectMacromolecule(10, bio.NOTATION.FASTA, bio.ALPHABET.DNA, 50, 50);
-  });
+      const et: number = await detectMacromoleculeBenchmark(10, bio.NOTATION.FASTA, bio.ALPHABET.DNA, 50, 50);
+    },
+    {skipReason: '#1192'});
 
   test('fastaDnaShorts50Many1E6', async () => {
-      const et: number = await benchmarkDetectMacromolecule(10, bio.NOTATION.FASTA, bio.ALPHABET.DNA, 50, 1E6);
+      const et: number = await detectMacromoleculeBenchmark(10, bio.NOTATION.FASTA, bio.ALPHABET.DNA, 50, 1E6);
     },
-    { /* skipReason: 'slow transmit large dataset to detector' */});
+    {skipReason: '#1192'});
 
   test('fastaDnaLong1e6Few50', async () => {
-    const et: number = await benchmarkDetectMacromolecule(10, bio.NOTATION.FASTA, bio.ALPHABET.DNA, 1E6, 50);
-  });
+      const et: number = await detectMacromoleculeBenchmark(10, bio.NOTATION.FASTA, bio.ALPHABET.DNA, 1E6, 50);
+    },
+    {skipReason: '#1192'});
 
   // -- separator --
 
   test('separatorDnaShorts50Few50', async () => {
-    const et: number = await benchmarkDetectMacromolecule(10, bio.NOTATION.SEPARATOR, bio.ALPHABET.DNA, 50, 50, '/');
+    const et: number = await detectMacromoleculeBenchmark(10, bio.NOTATION.SEPARATOR, bio.ALPHABET.DNA, 50, 50, '/');
   });
 
   test('separatorDnaShorts50Many1E6', async () => {
-      const et: number = await benchmarkDetectMacromolecule(10, bio.NOTATION.SEPARATOR, bio.ALPHABET.DNA, 50, 1E6, '/');
+      const et: number = await detectMacromoleculeBenchmark(10, bio.NOTATION.SEPARATOR, bio.ALPHABET.DNA, 50, 1E6, '/');
     },
     { /* skipReason: 'slow transmit large dataset to detector' */});
 
   test('separatorDnaLong1e6Few50', async () => {
-    const et: number = await benchmarkDetectMacromolecule(10, bio.NOTATION.SEPARATOR, bio.ALPHABET.DNA, 1E6, 50, '/');
+    const et: number = await detectMacromoleculeBenchmark(10, bio.NOTATION.SEPARATOR, bio.ALPHABET.DNA, 1E6, 50, '/');
   });
 
-  async function benchmarkDetectMacromolecule(
+  async function detectMacromoleculeBenchmark(
     maxET: number, notation: bio.NOTATION, alphabet: bio.ALPHABET, length: number, count: number, separator?: string
   ): Promise<number> {
     return await benchmark<DG.FuncCall, DG.Column>(10,
