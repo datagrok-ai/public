99bffd0ba7c036dbc72e97a14467c9b7c31979d1
Leonid <stolbovla@yandex.ru>
2022-11-17 19:12:30 +0300

    Curve fit demo

--- packages/Chem/src/package.ts
+++ packages/Chem/src/package.ts
@@ -756,3 +756,61 @@ export async function callChemDiversitySearch(
 
 
 
+//name: Curves
+//tags: app
+export async function sim() {
+  const dataX = new Float32Array([4, 4.25, 4.5, 4.75, 5, 5.25, 5.5, 5.75, 6, 6.25, 6.5, 6.75, 7, 7.25, 7.5, 7.75, 8]);
+  const dataY = new Float32Array([1.005, 0.981, 0.921, 0.987,
+                                  0.948, 0.810, 0.730, 0.558,
+                                  0.501, 0.381, 0.271, 0.148, 
+                                  0.139, 0.078, 0.095, -0.037, 0.01]);
+
+  let t = DG.DataFrame.fromColumns([
+    DG.Column.fromFloat32Array('X', dataX),
+    DG.Column.fromFloat32Array('Y', dataY)
+  ]);
+
+  let lc = DG.Viewer.scatterPlot(t);
+  let coeffA = 1;
+  let coeffB = 20;
+  let coeffC = 2;
+  let coeffD = 0;
+
+  let btn = ui.bigButton('FIT', () => {
+    lc.meta.formulaLines.addLine({
+      title: 'Fitted',
+      formula: '${Y} = 0 + (1 - 0)/(1 + Pow(10, 1*(${X} - Log10(1000000))))',
+      zIndex: -30,
+      color: "#FF7F00",
+      width: 2,
+      visible: true,
+    });
+  });
+
+  lc.meta.formulaLines.addLine({
+    title: 'Original',
+    formula: '${Y} = 0 + (0.7 - 0.3)/(0.7 + Pow(10, 1*(${X} - Log10(900000))))',
+    zIndex: -30,
+    color: "#FFA500",
+    width: 2,
+    visible: true,
+  });
+
+  const v = grok.shell.newView('Curve fit', [
+    ui.panel([
+      ui.div([
+        ui.div([
+          ui.divH([ui.h1('Inputs')]),
+          ui.divV([
+            btn
+          ], 'ui-form'),
+        ], 'ui-form'),
+      ], 'ui-form'),
+    ])
+  ]);
+  v.box = true;
+
+
+
+  v.append(lc);
+}
