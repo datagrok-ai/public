5cf899a109dc4833ec34169257c0a6556f9db5c3
Alex Paramonov <alex.aprm@gmail.com>
2022-11-17 14:43:54 +0300

    Skipped tests

--- packages/ApiTests/src/viewers/viewers.ts
+++ packages/ApiTests/src/viewers/viewers.ts
@@ -117,7 +117,7 @@ category('Viewers', () => {
     expect(viewer.onTableAttachedCounter, 2);
 
     // TODO: Check onTableAttached has been called
-  });
+  }, {skipReason: 'GROK-11484'});
 
   test('setPropertyStringWithNumber', async () => {
     // const v: TestViewerForProperties = tv.addViewer('TestViewerForProperties', {}) as TestViewerForProperties;
@@ -141,7 +141,7 @@ category('Viewers', () => {
     if ((typeof propValueFromObject !== typeof 'str' || typeof propValueFromProps !== typeof 'str') && !exCaught)
       throw new Error('JsViewer string property assigned with number value ' +
         `become value of type '${typeof propValueFromObject}' without an exception or type conversion.`);
-  });
+  }, {skipReason: 'GROK-11485'});
 
   test('setPropertyIntWithString', async () => {
     // const v: TestViewerForProperties = tv.addViewer('TestViewerForProperties', {}) as TestViewerForProperties;
@@ -165,7 +165,7 @@ category('Viewers', () => {
     if ((typeof propValueFromObject !== typeof 1 || typeof propValueFromProps !== typeof 1) && !exCaught)
       throw new Error('JsViewer int property assigned with string value ' +
         `become value of type '${typeof propValueFromObject}' without an exception or type conversion.`);
-  });
+  }, {skipReason: 'GROK-11485'});
 
 
   test('testViewersLayout', async () => {
