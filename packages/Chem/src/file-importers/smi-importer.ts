91c10af55a95955489a27757f3f4569cc394f69c
Leonid <stolbovla@yandex.ru>
2022-11-20 13:00:20 +0300

    Chem: smi read small fix

--- packages/Chem/src/file-importers/smi-importer.ts
+++ packages/Chem/src/file-importers/smi-importer.ts
@@ -5,6 +5,9 @@ import * as OCL from 'openchemlib/full';
 export function _importSmi(bytes: Uint8Array): DG.DataFrame[] {
   let str = new TextDecoder().decode(bytes);
   str = str.replace('#SMILES', 'SMILES');
+  if(!str.includes('SMILES'))
+    str = 'SMILES\n' + str;
+
   let df = DG.DataFrame.fromCsv(str);
 
   return [df];
