75c97d121d1e2c31eb4a4c623e30107ff7ce4812
Volodymyr Dyma <vdyma@datagrok.ai>
2022-11-22 12:42:38 +0200

    DrugBank: code style fixes

--- packages/DrugBank/src/searches.ts
+++ packages/DrugBank/src/searches.ts
@@ -1,27 +1,25 @@
 import * as grok from 'datagrok-api/grok';
 import * as DG from 'datagrok-api/dg';
 
-import {drugBankSearchResult} from './utils';
-
-export async function drugBankSubstructureSearch(molStr: string, dbdf: DG.DataFrame): Promise<drugBankSearchResult> {
-  const bitset = await grok.chem.searchSubstructure(dbdf!.getCol('molecule'), molStr);
+export async function searchSubstructure(molStr: string, dbdf: DG.DataFrame): Promise<DG.DataFrame | null> {
+  const bitset = await grok.chem.searchSubstructure(dbdf.getCol('molecule'), molStr);
 
   if (bitset === null)
     return null;
 
-  dbdf!.filter.copyFrom(bitset);
-  return dbdf!;
+  dbdf.filter.copyFrom(bitset);
+  return dbdf;
 }
 
-export async function drugBankSimilaritySearch(molStr: string, limit: number, cutoff: number, dbdf: DG.DataFrame,
-): Promise<drugBankSearchResult> {
-  const searchdf = await grok.chem.findSimilar(dbdf!.getCol('molecule'), molStr, {'limit': limit, 'cutoff': cutoff});
+export async function findSimilar(molStr: string, limit: number, cutoff: number, dbdf: DG.DataFrame,
+): Promise<DG.DataFrame | null> {
+  const searchdf = await grok.chem.findSimilar(dbdf.getCol('molecule'), molStr, {'limit': limit, 'cutoff': cutoff});
 
   if (searchdf == null)
     return null;
 
-  if (dbdf!.col('index') === null)
-    (dbdf!.columns as DG.ColumnList).addNewInt('index').init((i) => i);
+  if (dbdf.col('index') === null)
+    dbdf.columns.addNewInt('index').init((i) => i);
 
-  return dbdf!.join(searchdf, ['index'], ['index'], ['molecule'], ['molecule'], DG.JOIN_TYPE.INNER, true);
+  return dbdf.join(searchdf, ['index'], ['index'], ['molecule'], ['molecule'], DG.JOIN_TYPE.INNER, true);
 }
