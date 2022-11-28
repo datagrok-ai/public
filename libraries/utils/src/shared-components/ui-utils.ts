e88c85eaf6634184ba731fac8aec5cdf754b55cf
Aufar Zakiev <zaufar@gmail.com>
2022-11-28 17:56:50 +0300

    Utils: added HistoryInput component

--- libraries/utils/src/shared-components/ui-utils.ts
+++ libraries/utils/src/shared-components/ui-utils.ts
@@ -1,4 +1,6 @@
+import * as DG from 'datagrok-api/dg';
 import {EXCEL_BLOB_TYPE, FileInput} from './file-input';
+import {HistoryInput} from './history-input';
 
 export namespace UiUtils {
   export function fileInput(
@@ -8,5 +10,15 @@ export namespace UiUtils {
     fileType = EXCEL_BLOB_TYPE
   ) {
     return new FileInput(initialText, initialValue, onValueChanged, fileType);
+  }
+
+  export function historyInput(
+    label: string,
+    _funcName: string,
+    _stringValueFunc: (currentRun: DG.FuncCall) => string,
+    _visibleColumnsForGrid: Record<string, (currentRun: DG.FuncCall) => string>,
+    _visibleColumnsForFilter: string[] = []
+  ) {
+    return new HistoryInput(label, _funcName, _stringValueFunc, _visibleColumnsForGrid, _visibleColumnsForFilter);
   }
 }
