6602c70ea454f8edffe0abeef048a36da8a37692
Alex Paramonov <alex.aprm@gmail.com>
2022-11-28 21:36:44 +0300

    Added missing type information

--- js-api/dg.ts
+++ js-api/dg.ts
@@ -34,7 +34,7 @@ $(function () {
   (<any>window).$ = $;
   (<any>window).dayjs = dayjs;
   (<any>window).wu = wu;
-  (<any>window).addEventListener("unhandledrejection", function(e) {
+  (<any>window).addEventListener("unhandledrejection", function(e: PromiseRejectionEvent) {
     (<any>window).grok_Unhandled_Error(e.reason);
   });
 });
