4a26505c1be22d8aed407fe3d2617ddfda81711f
Volodymyr Dyma <vdyma@datagrok.ai>
2022-12-02 22:30:54 +0200

    Fixes #1268, fixes #1249: tests and error handling

--- packages/PubChemApi/src/pubchem.ts
+++ packages/PubChemApi/src/pubchem.ts
@@ -1,104 +1,8 @@
 import * as grok from 'datagrok-api/grok';
-import * as ui from 'datagrok-api/ui';
-import {anyObject, paramsType, pubChemIdType, pubChemSearchType, urlParamsFromObject} from './utils';
+
+import {anyObject, getSmiles, paramsType, pubChemIdType, pubChemSearchType, urlParamsFromObject} from './utils';
 import {delay} from '@datagrok-libraries/utils/src/test';
-
-const pubChemBaseURL = 'https://pubchem.ncbi.nlm.nih.gov';
-const pubChemRest = `${pubChemBaseURL}/rest`;
-const pubChemPug = `${pubChemRest}/pug`;
-
-export function renderInfoValue(info: anyObject, refs: anyObject): HTMLElement {
-  const infoValue: {[key: string]: any[]} = info['Value'];
-  let infoValueList = Object.values(infoValue)[0];
-  infoValueList = infoValueList.length ? Object.values(infoValueList[0]) : infoValueList;
-  let text: string = infoValueList.length ? Object.values(infoValueList[0]).toString() : '';
-
-  if (text && info['ValueUnit'])
-    text += ` ${info['ValueUnit']}`;
-
-  if (info['Table']) {
-    const columnNames: string[] = info['Table']['ColumnName'];
-    const rows: any[] = info['Table']['Row'] || [];
-
-    return ui.table(rows, (row, _) => {
-      row['Cell'].map((x: any) => renderInfoValue(x, refs)).toList();
-    }, columnNames);
-  }
-
-  let result;
-  if (infoValue['String'] && info['URL'])
-    result = ui.div(text.replaceAll('<img src="/', `<img src="${pubChemBaseURL}`));
-  else if (text && info['URL'])
-    result = ui.link(text, info['URL']);
-  else if (infoValue['StringList'])
-    result = ui.list(infoValue['StringList']);
-  else if (text)
-    result = ui.divText(text);
-  else if (info['ExternalDataMimeType'] === 'image/png' && info['ExternalDataURL'])
-    result = ui.image(info['ExternalDataURL'], 200, 150);
-  else
-    result = null;
-
-
-  if (result instanceof HTMLElement && info['ReferenceNumber']) {
-    //@ts-ignore: api types are wrong
-    ui.tooltip.bind(result, () => ui.tableFromMap(refs[info['ReferenceNumber']]));
-  }
-
-  return result ?? ui.divText('unknown');
-}
-
-export function renderSection(section: anyObject, refs: anyObject): HTMLDivElement {
-  const content = ui.divV([]);
-
-  const description = section['Description'];
-  if (description)
-    content.append(ui.div(description));
-
-
-  const information: any[] = section['Information'];
-  if (information) {
-    const table = ui.table(information, (inf, _) => [inf['Description'] || inf['Name'], renderInfoValue(inf, refs)]);
-    content.append(table);
-  }
-
-  const sections: anyObject[] = section['Section'];
-  if (sections) {
-    const acc = ui.accordion(`pubChem/${section['TOCHeading']}`);
-    for (const section of sections) {
-      const paneName = section['TOCHeading'];
-      acc.addPane(paneName, () => renderSection(section, refs));
-      if (section['Description']) {
-        ui.tooltip.bind(ui.divText(paneName), () => ui.div(section['Description']));
-      }
-    }
-    content.append(acc.root);
-  }
-  return content;
-}
-
-export async function init(id: pubChemIdType): Promise<HTMLElement> {
-  if (id === '0' || id === null)
-    return ui.div('Not found in PubChem.');
-
-  const url = `${pubChemRest}/pug_view/data/compound/${id}/JSON`;
-  const response = await grok.dapi.fetchProxy(url);
-  const json = await response.json();
-  const acc = ui.accordion('pubChem');
-  acc.header = ui.label(`pubchem: ${id}`);
-
-  const references: anyObject = {};
-  const recordJson = json['Record'];
-  for (const ref of recordJson['Reference'])
-    references[ref['ReferenceNumber']] = ref;
-
-  const sections = recordJson['Section'];
-  for (const section of sections)
-    acc.addPane(section['TOCHeading'], () => renderSection(section, references));
-
-  return acc.root;
-}
-
+import {pubChemPug, pubChemRest} from './tests/const';
 
 export async function similaritySearch(
   idType: string, id: pubChemIdType, params?: paramsType): Promise<anyObject[] | null> {
@@ -133,9 +37,14 @@ export async function substructureSearch(
   return Array.isArray(json) ? json : null;
 }
 
-export async function smilesToPubChem(smiles: string) {
-  const s = await getBy('smiles', 'cids', smiles);
-  const cids = s['IdentifierList']['CID'][0];
+export async function smilesToPubChem(molString: string): Promise<number | null> {
+  try {
+    molString = await getSmiles(molString);
+  } catch (e) {
+    return null;
+  }
+  const s = await getBy('smiles', 'cids', molString);
+  const cids = s.IdentifierList?.CID[0];
   return cids;
 }
 
@@ -163,16 +72,22 @@ export async function _getListById(
     json = await response.json();
   } while (json.hasOwnProperty('Waiting') && maxRequests > 0);
 
-  return json.PropertyTable?.Properties ?? json['PC_Compounds'] ?? null;
+  return json.PropertyTable?.Properties ?? json.PC_Compounds ?? null;
 }
 
 export async function _asyncSearchId(
-  searchType: pubChemSearchType, idType: string, id: pubChemIdType, params?: paramsType): Promise<string | undefined> {
+  searchType: pubChemSearchType, idType: string, id: pubChemIdType, params?: paramsType): Promise<string | null> {
   params ??= {};
-  params['MaxRecords'] ??= 20;
+  params.MaxRecords ??= 20;
   const url =
     `${pubChemPug}/compound/${searchType}/${idType}/${encodeURIComponent(id)}/JSON?${urlParamsFromObject(params)}`;
   const response = await grok.dapi.fetchProxy(url);
   const json: anyObject = await response.json();
-  return json.Waiting?.ListKey;
+  return json.Waiting?.ListKey ?? null;
+}
+
+export async function getCompoundInfo(id: pubChemIdType): Promise<anyObject> {
+  const url = `${pubChemRest}/pug_view/data/compound/${id}/JSON`;
+  const response = await grok.dapi.fetchProxy(url);
+  return response.json();
 }
