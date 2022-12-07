161ccc5a2fb0ba4e63084e2e003f6c2d00ce3dae
osakhniuk <osakhniuk@datagrok.ai>
2022-12-07 14:21:33 +0200

    GROK-11088 Remove delays in JS UI Tests

--- packages/ApiTests/src/gui/demo-files-connection.ts
+++ packages/ApiTests/src/gui/demo-files-connection.ts
@@ -1,37 +1,26 @@
-import {after, before, category, delay, expect, test} from '@datagrok-libraries/utils/src/test';
+import {category, delay, test, awaitCheck} from '@datagrok-libraries/utils/src/test';
 import * as grok from 'datagrok-api/grok';
-import * as ui from 'datagrok-api/ui';
-import * as DG from 'datagrok-api/dg';
-import {checkHTMLElement} from '../ui/utils';
-import {isColumnPresent, isViewerPresent, isDialogPresent, returnDialog, setDialogInputValue, isErrorBallon} from './gui-utils';
+import {isDialogPresent, setDialogInputValue, isErrorBallon} from './gui-utils';
 
-category('Files: Demo Files Connection', () => {
+category('GUI: Files', () => {
 
   test('demoFiles.createConnectionToS3', async () => {
     let openSection = document.getElementsByClassName('grok-icon fal fa-folder-open')[0] as HTMLElement;
     openSection.click();
 
-    let menus = document.getElementsByClassName('d4-toggle-button');
-    let filesMenu:HTMLElement;
-    for (let i = 0; i < menus.length; i++ ) {        
-      filesMenu = menus[i] as HTMLElement;
-      if (filesMenu.innerText == 'Files')
-          break;
-      }
-    filesMenu!.click(); 
+    const filesMenuItem = Array.from(document.querySelectorAll('.d4-toggle-button'))
+    .find((el) => el.textContent == 'Files') as HTMLElement;
+    filesMenuItem!.click();
+    await awaitCheck(() => {return Array.from(grok.shell.views).find((fv) => fv.type == 'files') != undefined});
 
-    await delay(3000);  
-
-    let addBnt = document.getElementsByClassName('tab-handle-list-container')[0].getElementsByClassName('grok-icon fal fa-plus')[0] as HTMLElement;
+    let addBnt = document.getElementsByClassName('d4-ribbon')[0].getElementsByClassName('grok-icon fal fa-plus')[0] as HTMLElement;
     addBnt.click();      
 
-    await delay(1000);  
-
-    isDialogPresent('New file share');
+    await awaitCheck(() => {return isDialogPresent('New file share');}); 
     
     setDialogInputValue('New file share', 'Data Source', 'S3');
 
-    await delay(1000);
+    await delay(500); 
     
     setDialogInputValue('New file share', 'Name', 'Test Connection to S3');
     setDialogInputValue('New file share', 'Region', 'us-east-2');
@@ -40,27 +29,23 @@ category('Files: Demo Files Connection', () => {
     setDialogInputValue('New file share', 'Access Key', '');
     setDialogInputValue('New file share', 'Secret Key', '');
 
-    await delay(500);
+    await delay(300);
     
-    let buttons = document.getElementsByClassName('ui-btn ui-btn-ok');
-    let testBtn:HTMLElement;
-    for (let i = 0; i < buttons.length; i++ ) {        
-      testBtn = buttons[i] as HTMLElement;
-      if (testBtn.innerText == 'TEST')
-          break;
-      }
-    testBtn!.click(); await delay(1000);
+    const testBtn = Array.from(document.querySelectorAll('.ui-btn-ok'))
+    .find((el) => el.textContent == 'TEST') as HTMLElement;
+    testBtn.click();
+
+    await delay(1000); // not enough timeout inside awaitCheck(). I will remove it after the parameterization of the function
+    await awaitCheck(() => {return document.getElementsByClassName('d4-balloon-content').length > 0}); 
 
     isErrorBallon('"Test Connection to S3": connected successfully');
     
-    buttons = document.getElementsByClassName('ui-btn ui-btn-ok');
-    let okBtn:HTMLElement;
-    for (let i = 0; i < buttons.length; i++ ) {        
-      okBtn = buttons[i] as HTMLElement;
-      if (okBtn.innerText == 'OK')
-          break;
-      }
-    okBtn!.click(); await delay(5000);
+    const okBtn = Array.from(document.querySelectorAll('.ui-btn-ok'))
+    .find((el) => el.textContent == 'OK') as HTMLElement;
+    okBtn.click();
+    
+    await delay(1000);
+    await awaitCheck(() => {return Array.from(document.querySelectorAll('.d4-tree-view-group-label')) .find((el) => el.textContent == 'Test Connection to S3') != undefined})
    
     let testConnection = await grok.dapi.connections.filter('dataSource in ("Dropbox","Files","Git","GitHub","GoogleCloud","S3") and friendlyName="Test Connection to S3"').first();
 
@@ -70,7 +55,7 @@ category('Files: Demo Files Connection', () => {
     await grok.dapi.connections.delete(testConnection);
 
     testConnection = await grok.dapi.connections.filter('dataSource in ("Dropbox","Files","Git","GitHub","GoogleCloud","S3") and friendlyName="Test Connection to S3"').first();
-    if (testConnection == undefined)
+    if (testConnection != undefined)
       throw 'Test connection has not been deleted'  
     }); 
 });
---
+++ packages/ApiTests/src/gui/grid.ts
@@ -0,0 +1,108 @@
+import {after, before, category, delay, test, awaitCheck} from '@datagrok-libraries/utils/src/test';
+import * as grok from 'datagrok-api/grok';
+import * as DG from 'datagrok-api/dg';
+import { getHTMLElementbyInnerText } from './gui-utils';
+import {checkDialog} from './gui-utils';
+
+category('GUI: Grid', () => {
+
+  test('grid.dataSearch', async () => {
+    let v: DG.TableView;
+    const demog = grok.data.demo.demog(1000);
+    v = grok.shell.addTableView(demog);
+    await awaitCheck(() => {return grok.shell.v == v});
+
+    let searchTab = getHTMLElementbyInnerText('d4-accordion-pane-header', 'Search');
+    searchTab!.click();
+    
+    let searchInput:HTMLInputElement | undefined;
+    let input;
+    for (let i=0; i<document.getElementsByClassName('ui-input-editor').length; i++) {
+        input = document.getElementsByClassName('ui-input-editor')[i] as HTMLInputElement;
+        if (input.placeholder == 'Search'){
+            searchInput = input;
+            break;
+        }
+    }
+
+    await awaitCheck(() => {return searchInput != undefined});
+    searchInput!.value = 'Asian'; await delay(100);
+    searchInput!.dispatchEvent(new Event('input'));
+
+    let searchOptionsBtn = document.getElementsByClassName('d4-flex-row d4-flew-nowrap d4-search')[0].getElementsByClassName('grok-icon grok-font-icon-menu')[0] as HTMLElement;
+    searchOptionsBtn.click(); 
+    
+    await awaitCheck(() => {return getHTMLElementbyInnerText('d4-menu-item-label', 'Filter matching') != undefined});    
+    
+    let filterMatchingAction = getHTMLElementbyInnerText('d4-menu-item-label', 'Filter matching'); 
+    filterMatchingAction!.click();
+    
+    await awaitCheck(() => {return demog.filter.trueCount == 762});
+
+    v.close();
+    grok.shell.tables.forEach((t) => grok.shell.closeTable(t));
+  });
+
+  test('grid.deleteRows', async () => {
+    let v: DG.TableView;
+    const demog = grok.data.demo.demog(1000);
+    v = grok.shell.addTableView(demog);
+
+    grok.shell.topMenu.find('Select').find('Random...').click(); await delay(2000);
+
+    await awaitCheck(() => {return checkDialog('Select Random Rows')});    
+  
+    const input25 = Array.from(document.querySelectorAll('.d4-link-label'))
+    .find((el) => el.textContent === '25%') as HTMLElement;
+    input25!.click();
+
+    await awaitCheck(() => {return demog.selection.trueCount == 250})
+
+    let okButton = Array.from(document.querySelectorAll('.ui-btn.ui-btn-ok'))
+      .find((el) => el.textContent === 'OK') as HTMLElement;
+    okButton.click();
+    await delay(200);
+
+    let removeRowsBtn = document.getElementsByClassName('svg-remove-selected-rows')[0] as HTMLElement;
+    removeRowsBtn.click(); 
+
+    await awaitCheck(() => {return demog.rowCount == 750})
+
+    if (demog.rowCount == 1000)
+        throw 'rows are not deleted';
+    
+    grok.shell.closeTable(demog);
+    v.close();
+  });
+
+  test('grid.deleteCols', async () => {
+    let v: DG.TableView;
+    const demog = grok.data.demo.demog(1000);
+    v = grok.shell.addTableView(demog);
+    await awaitCheck(() => {return grok.shell.v == v});
+
+    grok.shell.o = grok.shell.t.col('disease');
+    await awaitCheck(() => {return Array.from(document.querySelectorAll('.d4-accordion-title'))
+    .find((el) => el.textContent == 'disease') != undefined});
+
+    let actionsSectionOnPP = Array.from(document.getElementsByClassName('grok-entity-prop-panel')[0].querySelectorAll('.d4-accordion-pane-header'))
+    .find((el) => el.textContent === 'Actions') as HTMLElement;
+    if (Array.from(actionsSectionOnPP.classList).find((el) => el == 'expanded') == undefined){
+      actionsSectionOnPP.click();
+    }
+    await awaitCheck(() => {return Array.from(actionsSectionOnPP.classList).find((el) => el == 'expanded') != undefined });
+  
+    let removeLinkAction = Array.from(document.querySelectorAll('.d4-link-action'))
+    .find((el) => el.textContent === 'Remove') as HTMLElement;
+    removeLinkAction!.click(); 
+    
+    await awaitCheck(() => {return demog.columns.byName('disease') == null}); 
+    
+    if (demog.columns.byName('disease') != null)
+      throw 'disease column was not deleted'
+    
+    grok.shell.closeTable(demog);
+    v.close();  
+  }); 
+
+});
