a3ac15c1bf22f6adf439f4a0174363f6f9ded1ca
Alexey <achopovsky@datagrok.ai>
2022-12-08 23:28:46 +0200

    ST: improvements to the SDF tab UI, WIP

--- packages/SequenceTranslator/src/structures-works/save-sense-antisense.ts
+++ packages/SequenceTranslator/src/structures-works/save-sense-antisense.ts
@@ -2,15 +2,17 @@ import * as grok from 'datagrok-api/grok';
 import * as ui from 'datagrok-api/ui';
 import * as DG from 'datagrok-api/dg';
 
+import $ from 'cash-dom';
+
 import {download} from '../helpers';
 import {sequenceToMolV3000} from '../structures-works/from-monomers';
 import {linkStrandsV3000} from '../structures-works/mol-transformations';
 import {getFormat} from '../structures-works/sequence-codes-tools';
 
 export function saveSdf(as: string, ss: string,
-                        oneEntity: boolean, useChirality: boolean,
-                        invertSS: boolean, invertAS: boolean,
-                        as2: string | null = null, invertAS2: boolean | null) {
+  oneEntity: boolean, useChirality: boolean,
+  invertSS: boolean, invertAS: boolean,
+  as2: string | null = null, invertAS2: boolean | null) {
   const formatAs = getFormat(as);
   const formatSs = getFormat(ss);
   let formatAs2: string | null = null;
@@ -28,7 +30,6 @@ export function saveSdf(as: string, ss: string,
   if (oneEntity) {
     const antiStrands = molAS2 == null ? [molAS] : [molAS, molAS2];
     result = linkStrandsV3000({senseStrands: [molSS], antiStrands: antiStrands}, useChirality) + '\n$$$$\n';
-
   } else {
     result =
     molSS + '\n' +
@@ -36,56 +37,94 @@ export function saveSdf(as: string, ss: string,
     molAS + '\n' +
     `> <Sequence>\nAnti Sense\n$$$$\n`;
 
-    if (molAS2)
+    if (molAS2) {
       result += molAS2+ '\n' +
       `> <Sequence>\nAnti Sense 2\n$$$$\n`;
+    }
   }
   download(ss.replace(/\s/g, '') + '.sdf', encodeURIComponent(result));
 }
 
 export function saveSenseAntiSense() {
+  //// const v = grok.shell.newView('SDF');
+
+  //let ssDirection = ui.choiceInput('SS direction', '5 prime -> 3 prime', ['5 prime -> 3 prime']);
+  //let asDirection = ui.choiceInput('AS direction', '5 prime -> 3 prime', ['5 prime -> 3 prime']);
+  //let as2Direction = ui.choiceInput('AS 2 direction', '5 prime -> 3 prime', ['5 prime -> 3 prime']);
+  //let saveEntity = ui.boolInput('Save as one entity', true);
+  //let useChiral = ui.boolInput('Use chiral', true);
+
+  //let form2 = ui.form([ssDirection, asDirection, as2Direction, saveEntity, useChiral]);
+  //form2.className = 'ui-form ui-form-wide';
+
+  //let body = ui.divH([ui.block([form1]), form2]);
+  //v.append(body);
+  //$(form1).find('textarea').css('flex-grow','1');
+  //$(form1).find('label').css('max-width','140px');
+
+  // end of comment
+
   const moleculeSvgDiv = ui.block([]);
+  const inputColHeader = ui.h1('Sequences');
   const ssInput = ui.textInput('Sense Strand', '');
+  ssInput.root.style.color = 'red';
   const asInput = ui.textInput('Anti Sense', '');
   const asInput2 = ui.textInput('Anti Sense 2', '');
-  const straight = "5 prime -> 3 prime";
-  const inverse = "3 prime -> 5 prime";
+  const saveEntity = ui.boolInput('Save as one entity', true);
+  const useChiral = ui.boolInput('Use chiral', true);
+
+  const straight = '5 prime -> 3 prime';
+  const inverse = '3 prime -> 5 prime';
   let ssInverse = false;
   let asInverse = false;
-  let as2Inverse = false;
+  const as2Inverse = false;
 
-  const changeSense = ui.choiceInput('SS direction', straight, [straight, inverse]);
-  changeSense.onChanged(() => {ssInverse = changeSense.value == inverse;});
-  const changeAntiSense = ui.choiceInput('AS direction', straight, [straight, inverse]);
-  changeAntiSense.onChanged(() => {asInverse = changeAntiSense.value == inverse;});
-  const changeAntiSense2 = ui.choiceInput('AS 2 direction', straight, [straight, inverse]);
-  changeAntiSense2.onChanged(() => {asInverse = changeAntiSense.value == inverse;});
+  const ssDirection = ui.choiceInput('SS direction', straight, [straight, inverse]);
+  ssDirection.onChanged(() => { ssInverse = ssDirection.value == inverse; });
+  const asDirection = ui.choiceInput('AS direction', straight, [straight, inverse]);
+  asDirection.onChanged(() => { asInverse = asDirection.value == inverse; });
+  const as2Direction = ui.choiceInput('AS 2 direction', straight, [straight, inverse]);
+  as2Direction.onChanged(() => { asInverse = asDirection.value == inverse; });
 
-  const saveOption = ui.switchInput('Save as one entity', true);
-  const chirality = ui.switchInput('Use chiral', true);
-  const saveBtn = ui.button('Save SDF', () =>
-    saveSdf(asInput.value, ssInput.value, saveOption.value, chirality.value, ssInverse, asInverse, asInput2.value, as2Inverse));
-
-  const saveSection = ui.panel([
-    ui.div([
-      ui.div([
-        ui.divH([ui.h1('Inputs')]),
-        ui.divV([
-          ssInput,
-          asInput,
-          asInput2,
-          ui.div([changeSense], {style: {width: '40'}}),
-          changeSense,
-          changeAntiSense,
-          changeAntiSense2,
-          saveOption,
-          chirality,
-          ui.buttonsInput([saveBtn]),
-        ], 'ui-form'),
-      ], 'ui-form'),
-    ], 'ui-form'),
-    moleculeSvgDiv,
+  const saveButton = ui.buttonsInput([
+    ui.bigButton('Save SDF', () =>
+      saveSdf(
+        asInput.value, ssInput.value, saveEntity.value!,
+        useChiral.value!, ssInverse, asInverse, asInput2.value, as2Inverse)
+    )
   ]);
+  //@ts-ignore
+  // the above line is recommended by Dmitry because saveButton has wrong return
+  // type
+  const form1 = ui.form([ssInput, asInput, asInput2, saveButton]);
+  form1.className = 'ui-form ui-form-wide';
+  const form2 = ui.form([ssDirection, asDirection, as2Direction, saveEntity, useChiral]);
+  form2.className = 'ui-form ui-form-wide';
 
-  return saveSection;
+  const body = ui.divH([ui.block([form1]), form2]);
+  $(form1).find('textarea').css('flex-grow', '1');
+  $(form1).find('label').css('max-width','140px');
+  // const saveSection = ui.panel([
+  //   ui.div([
+  //     ui.divH([
+  //       // ui.divH([ui.h1('Inputs')]),
+  //       ui.divV([
+  //         inputColHeader,
+  //         ssInput,
+  //         asInput,
+  //         asInput2,
+  //         ui.div([changeSense], {style: {width: '40'}}),
+  //         changeSense,
+  //         changeAntiSense,
+  //         changeAntiSense2,
+  //         saveOption,
+  //         chirality,
+  //         ui.buttonsInput([saveBtn]),
+  //       ], 'ui-form'),
+  //     ], 'ui-form'),
+  //   ], 'ui-form'),
+  //   moleculeSvgDiv,
+  // ]);
+
+  return body;
 }
