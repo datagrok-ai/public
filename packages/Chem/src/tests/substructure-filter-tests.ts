a69b9990f199515a3c69c0a2a50a65cce99c9731
Maria Dolotova <maria.dolotova@arcadia.spb.ru>
2022-12-08 15:16:34 +0300

    Chem: fixing substructure filters tests

--- packages/Chem/src/tests/substructure-filter-tests.ts
+++ packages/Chem/src/tests/substructure-filter-tests.ts
@@ -18,54 +18,73 @@ category('substructure filters', async () => {
   test('filterBy2Columns', async () => {
     const df = await readDataframe('tests/smiles_2_columns.csv');
     await grok.data.detectSemanticTypes(df);
+    const sketcherDialogs: DG.Dialog[] = [];
 
-    const filter1 = new SubstructureFilter();
-    const filter2 = new SubstructureFilter();
-    filter1.attach(df);
-    filter2.attach(df);
-    filter1.column = df.col('smiles1');
-    filter1.columnName = 'smiles1';
-    filter2.column = df.col('smiles2');
-    filter2.columnName = 'smiles2';
-    
-    filter1.sketcher.setMolFile(`
-  Ketcher 12 72223422D 1   1.00000     0.00000     0
+    async function initSketcher(sw: DG.chem.Sketcher) {
+      return new Promise(async (resolve, reject) => {
+        sw.sketcherCreated.subscribe(async (_: any) => {
+          try {
+            resolve(true);
+          } catch (error) {
+            reject(error);
+          }
+        });
+      });
+    }
 
-  5  5  0  0  0  0            999 V2000
-    -1.4062   -0.8866    0.0000 N   0  0  0  0  0  0  0  0  0  0  0  0
-    -2.1207   -0.4741    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0
-    -2.1207    0.3508    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0
-    -0.6918    0.3508    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0
-    -0.6918   -0.4741    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0
+    async function createFilter(colName: string): Promise<SubstructureFilter> {
+      const filter = new SubstructureFilter();
+      sketcherDialogs.push(ui.dialog().add(filter.sketcher).show());
+      filter.attach(df);
+      filter.column = df.col(colName);
+      filter.columnName = colName;
+      const waitForSketcher = initSketcher(filter.sketcher);
+      if (!filter.sketcher.sketcher)
+        await waitForSketcher;
+      return filter;
+    }
+
+    const filter1 = await createFilter('smiles1');
+    const filter2 = await createFilter('smiles2');
+    const molfile1 = `
+      MJ201900                      
+
+  5  5  0  0  0  0  0  0  0  0999 V2000
+   -0.1785   -0.1946    0.0000 N   0  0  0  0  0  0  0  0  0  0  0  0
+   -0.8930    0.2178    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0
+   -0.8930    1.0428    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0
+    0.5358    1.0428    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0
+    0.5358    0.2178    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0
   1  2  1  0  0  0  0
   1  5  1  0  0  0  0
   2  3  2  0  0  0  0
   3  4  1  0  0  0  0
   4  5  2  0  0  0  0
-M  END`);
-    filter2.sketcher.setMolFile(`
-  Ketcher 12 72223422D 1   1.00000     0.00000     0
+M  END`;
+    const molfile2 = `
+      MJ201900                      
 
-  5  5  0  0  0  0            999 V2000
-    -1.4062   -0.8866    0.0000 O   0  0  0  0  0  0  0  0  0  0  0  0
-    -2.1207   -0.4741    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0
-    -2.1207    0.3508    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0
-    -0.6918    0.3508    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0
-    -0.6918   -0.4741    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0
-  1  2  1  0  0  0  0
-  1  5  1  0  0  0  0
+  5  5  0  0  0  0  0  0  0  0999 V2000
+   -0.1785   -0.1946    0.0000 O   0  0  0  0  0  0  0  0  0  0  0  0
+   -0.8930    0.2178    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0
+   -0.8930    1.0428    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0
+    0.5358    1.0428    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0
+    0.5358    0.2178    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0
   2  3  2  0  0  0  0
   3  4  1  0  0  0  0
   4  5  2  0  0  0  0
+  1  2  1  0  0  0  0
+  1  5  1  0  0  0  0
 M  END
-`); 
-    await testEvent(df.onFilterChanged, (_) => {}, () => { filter1._onSketchChanged() }, 5000);
+`; 
+    await testEvent(df.onFilterChanged, (_) => {}, () => { filter1.sketcher.setMolFile(molfile1) }, 7000);
     await testEvent(df.onFilterChanged, (_) => {
       expect(df.filter.trueCount, 2);
       expect(df.filter.get(4), true);
       expect(df.filter.get(5), true);
       expect(df.filter.get(0), false);
-    }, () => { filter2._onSketchChanged() }, 5000);
+    }, () => { filter2.sketcher.setMolFile(molfile2) }, 7000);
+    sketcherDialogs.forEach((it) => it.close());
   });
 });
   
