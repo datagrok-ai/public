eeb3313c0b0606b94f342a6cac33d66f291af199
osakhniuk <osakhniuk@datagrok.ai>
2022-12-09 16:17:37 +0200

    GROK-11088 Remove delays in JS UI Tests

--- packages/ApiTests/src/gui/dialogs.ts
+++ packages/ApiTests/src/gui/dialogs.ts
@@ -64,16 +64,14 @@ category('GUI: Dialogs', () => {
     isDialogPresent('Cluster Data');
 
     returnDialog('Cluster Data')!.input('Show scatter plot').input.click();    
-    await delay(2000); // not enough timeout inside awaitCheck(). I will remove it after the parameterization of the function
-    await awaitCheck(() => {return checkViewer(Array.from(v.viewers), 'Scatter plot');}); 
-    isViewerPresent(Array.from(v.viewers), 'Scatter plot');
+    await awaitCheck((() => {return checkViewer(Array.from(v.viewers), 'Scatter plot')}), 'Scatter plot did not appear', 2500); 
     isColumnPresent(demog.columns, 'clusters (2)');
 
     const cancelButton = Array.from(document.querySelectorAll('.ui-btn.ui-btn-ok'))
       .find((el) => el.textContent === 'CANCEL') as HTMLElement;
     cancelButton.click();
 
-    await awaitCheck(() => {return !checkViewer(Array.from(v.viewers), 'Scatter plot');}); 
+    await awaitCheck((() => {return !checkViewer(Array.from(v.viewers), 'Scatter plot')}), 'Scatter plot still displayed', 1000); 
 
     for (let i:number = 0; i < Array.from(v.viewers).length; i++) {
       if (Array.from(v.viewers)[i].type == 'Scatter plot')
@@ -91,8 +89,7 @@ category('GUI: Dialogs', () => {
     setDialogInputValue('Cluster Data', 'Clusters', 4); await delay(100);
     setDialogInputValue('Cluster Data', 'Metric', 'Manhattan'); await delay(100);
     returnDialog('Cluster Data')!.input('Show scatter plot').input.click();    
-    await delay(2000); // not enough timeout inside awaitCheck(). I will remove it after the parameterization of the function
-    await awaitCheck(() => {return checkViewer(Array.from(v.viewers), 'Scatter plot');});
+    await awaitCheck((() => {return checkViewer(Array.from(v.viewers), 'Scatter plot')}), 'Scatter plot did not appear', 2500); 
 
     okButton = document.getElementsByClassName('ui-btn ui-btn-ok enabled')[0] as HTMLElement;
     okButton!.click(); 
@@ -141,7 +138,7 @@ category('GUI: Dialogs', () => {
     const okButton = Array.from(document.querySelectorAll('.ui-btn.ui-btn-ok'))
         .find((el) => el.textContent === 'OK') as HTMLElement;
     okButton.click();
-    await delay(8000); // not enough timeout inside awaitCheck(). I will remove it after the parameterization of the function. Impute missed values takes a long time
+    await awaitCheck((() => {return grok.shell.t.col('AGE')?.stats.missingValueCount == 0}), 'column AGE still contains empty rows!', 9000);
 
     if (((await demog).col('AGE')?.stats.missingValueCount) != 0)
       throw 'column AGE contains empty rows!';
@@ -191,8 +188,7 @@ category('GUI: Dialogs', () => {
     okButton = Array.from(document.querySelectorAll('.ui-btn.ui-btn-ok'))
     .find((el) => el.textContent === 'OK') as HTMLElement;
     okButton!.click(); 
-    await delay(2000); // not enough timeout inside awaitCheck(). I will remove it after the parameterization of the function. PCA function takes more time
-    await awaitCheck(() => {return DG.Dialog.getOpenDialogs().length == 0});
+    await awaitCheck((() => {return DG.Dialog.getOpenDialogs().length == 0}), 'PCA Timeout', 2500);
 
     isColumnPresent(demog.columns, 'PCA0');
     isColumnPresent(demog.columns, 'PCA1');
@@ -215,8 +211,7 @@ category('GUI: Dialogs', () => {
     let okButton = Array.from(document.querySelectorAll('.ui-btn.ui-btn-ok'))
         .find((el) => el.textContent === 'OK') as HTMLElement;
     okButton.click();
-    await delay(2000); // not enough timeout inside awaitCheck(). I will remove it after the parameterization of the function
-    await awaitCheck(() => {return grok.shell.t.col('normal') != null});
+    await awaitCheck((() => {return grok.shell.t.col('normal') != null}), '"normal" col not added to table', 2500);
 
     grok.shell.topMenu.find('Tools').find('Data Science').find('Random Data...').click(); 
     await awaitCheck(() => {return checkDialog('Random Data');});
@@ -225,8 +220,7 @@ category('GUI: Dialogs', () => {
     okButton = Array.from(document.querySelectorAll('.ui-btn.ui-btn-ok'))
         .find((el) => el.textContent === 'OK') as HTMLElement;
     okButton.click(); 
-    await delay(2000); // not enough timeout inside awaitCheck(). I will remove it after the parameterization of the function
-    await awaitCheck(() => {return grok.shell.t.col('log-normal') != null});
+    await awaitCheck((() => {return grok.shell.t.col('log-normal') != null}), '"log-normal" col not added to table', 2500);
 
     grok.shell.topMenu.find('Tools').find('Data Science').find('Random Data...').click(); 
     await awaitCheck(() => {return checkDialog('Random Data');});
@@ -234,16 +228,14 @@ category('GUI: Dialogs', () => {
     okButton = Array.from(document.querySelectorAll('.ui-btn.ui-btn-ok'))
         .find((el) => el.textContent === 'OK') as HTMLElement;
     okButton.click();
-    await delay(2000); // not enough timeout inside awaitCheck(). I will remove it after the parameterization of the function
-    await awaitCheck(() => {return grok.shell.t.col('binomial') != null});
+    await awaitCheck((() => {return grok.shell.t.col('binomial') != null}), '"binomial" col not added to table', 2500);
 
     grok.shell.topMenu.find('Tools').find('Data Science').find('Random Data...').click(); 
     await awaitCheck(() => {return checkDialog('Random Data');});
-    setDialogInputValue('Random Data', 'Distribution', 'poisson'); await delay(500);
+    setDialogInputValue('Random Data', 'Distribution', 'poisson'); await delay(200);
     okButton = document.getElementsByClassName('ui-btn ui-btn-ok enabled')[0] as HTMLElement;
     okButton!.click(); 
-    await delay(2000); // not enough timeout inside awaitCheck(). I will remove it after the parameterization of the function
-    await awaitCheck(() => {return grok.shell.t.col('poisson') != null});
+    await awaitCheck((() => {return grok.shell.t.col('poisson') != null}), '"poisson" col not added to table',2500);
 
     grok.shell.topMenu.find('Tools').find('Data Science').find('Random Data...').click(); 
     await awaitCheck(() => {return checkDialog('Random Data');});
@@ -251,8 +243,7 @@ category('GUI: Dialogs', () => {
     okButton = Array.from(document.querySelectorAll('.ui-btn.ui-btn-ok'))
         .find((el) => el.textContent === 'OK') as HTMLElement;
     okButton!.click(); 
-    await delay(2000); // not enough timeout inside awaitCheck(). I will remove it after the parameterization of the function
-    await awaitCheck(() => {return grok.shell.t.col('uniform') != null});
+    await awaitCheck((() => {return grok.shell.t.col('uniform') != null}),'"uniform" col not added to table', 2500);
 
     isColumnPresent(demog.columns, 'normal');
     isColumnPresent(demog.columns, 'binomial');
@@ -263,8 +254,8 @@ category('GUI: Dialogs', () => {
     grok.shell.topMenu.find('Tools').find('Data Science').find('Random Data...').click(); 
     await awaitCheck(() => {return checkDialog('Random Data');});
     isDialogPresent('Random Data');
-    returnDialog('Random Data')!.input('Show histogram').input.click(); await delay(3000); // not enough timeout inside awaitCheck(). I will remove it after the parameterization of the function
-    await awaitCheck(() => {return Array.from(v.viewers).find((el) => el.type == 'Histogram') != undefined});
+    returnDialog('Random Data')!.input('Show histogram').input.click();
+    await awaitCheck((() => {return Array.from(v.viewers).find((el) => el.type == 'Histogram') != undefined}), 'Histogram not added', 3500);
 
     let cancelButton = Array.from(document.querySelectorAll('.ui-btn.ui-btn-ok'))
     .find((el) => el.textContent === 'CANCEL') as HTMLElement;
@@ -279,8 +270,8 @@ category('GUI: Dialogs', () => {
     grok.shell.topMenu.find('Tools').find('Data Science').find('Random Data...').click(); 
     await awaitCheck(() => {return checkDialog('Random Data');});
     isDialogPresent('Random Data');
-    returnDialog('Random Data')!.input('Show histogram').input.click(); await delay(3000); // not enough timeout inside awaitCheck(). I will remove it after the parameterization of the function
-    await awaitCheck(() => {return Array.from(v.viewers).find((el) => el.type == 'Histogram') != undefined});
+    returnDialog('Random Data')!.input('Show histogram').input.click();
+    await awaitCheck((() => {return Array.from(v.viewers).find((el) => el.type == 'Histogram') != undefined}), 'Histogram not added', 3500);
     isViewerPresent(Array.from(v.viewers), 'Histogram');
     okButton = Array.from(document.querySelectorAll('.ui-btn.ui-btn-ok'))
         .find((el) => el.textContent === 'OK') as HTMLElement;
@@ -289,6 +280,87 @@ category('GUI: Dialogs', () => {
 
     isViewerPresent(Array.from(v.viewers), 'Histogram');
 
+    v.close();
+    grok.shell.tables.forEach((t) => grok.shell.closeTable(t));
+  });  
+
+  test('dialogs.multivariateAnalysis', async () => {
+    let v: DG.TableView;
+    const demog = grok.data.demo.demog(1000);
+    v = grok.shell.addTableView(demog);
+    await awaitCheck(() => {return grok.shell.v == v});
+
+    grok.shell.topMenu.find('Tools').find('Data Science').find('Multivariate Analysis (PLS)...').click();
+    await awaitCheck((() => {return document.querySelector('.d4-dialog') != undefined}), 'Dialog did not open', 1000);
+
+    let okButton = Array.from(document.querySelectorAll('.ui-btn.ui-btn-ok'))
+      .find((el) => el.textContent == 'OK') as HTMLElement;
+    
+    if (okButton != undefined) {
+      if (okButton.classList[2] != 'disabled')
+        throw 'OK Button does not have disabled class';
+    }
+
+    returnDialog('Multivariate Analysis (PLS)')!.input('Features').input.dispatchEvent(new MouseEvent('click')); 
+    await awaitCheck((() => {return DG.Dialog.getOpenDialogs().length == 2}), 'Select columns dialog did not open', 1000);
+
+    let allButtonInSelectColumns = Array.from(document.querySelectorAll('.d4-link-label'))
+    .find((el) => el.textContent === 'All') as HTMLElement;
+
+    let okButtonInSelectColumns = Array.from(returnDialog('Select columns...')!.root.querySelectorAll('.ui-btn.ui-btn-ok'))
+    .find((el) => el.textContent === 'OK') as HTMLElement;
+
+    if (allButtonInSelectColumns != undefined) {
+      allButtonInSelectColumns.click(); await delay(200);
+    }
+    if (okButtonInSelectColumns != undefined) {
+      okButtonInSelectColumns.click(); await delay(200);
+    }
+    okButton!.click(); 
+    await awaitCheck((() => {return Array.from(v.dockManager.rootNode.children).length > 1}), 'Multivariate Analysis is not done', 12000);
+
+    v.close();
+    grok.shell.tables.forEach((t) => grok.shell.closeTable(t));
+  });
+  
+  test('dialogs.spe', async () => {
+    let v: DG.TableView;
+    const demog = grok.data.demo.demog(1000);
+    v = grok.shell.addTableView(demog);
+    await awaitCheck(() => {return grok.shell.v == v});
+
+    grok.shell.topMenu.find('Tools').find('Data Science').find('Stochastic Proximity Embedding...').click();
+    await awaitCheck(() => {return checkDialog('SPE');});
+
+    let okButton = Array.from(document.querySelectorAll('.ui-btn.ui-btn-ok'))
+        .find((el) => el.textContent === 'OK') as HTMLElement;
+    okButton.click();
+    //WIP, SPE Feature currently not working (GROK-10250)
+
+    v.close();
+    grok.shell.tables.forEach((t) => grok.shell.closeTable(t));
+  }, {skipReason: 'GROK-10250'});  
+
+  test('dialogs.splitColumn', async () => {
+    let v: DG.TableView;
+    const demog = grok.data.demo.demog(1000);
+    v = grok.shell.addTableView(demog);
+    await awaitCheck(() => {return grok.shell.v == v});
+
+    grok.shell.topMenu.find('Data').find('Split Column...').click();
+    await awaitCheck(() => {return checkDialog('Text to columns');});
+
+    setDialogInputValue('Text to columns', 'Split by', ' '); await delay(200);
+    setDialogInputValue('Text to columns', 'Prefix', 'TestCol'); await delay(200);
+
+    let okButton = Array.from(document.querySelectorAll('.ui-btn.ui-btn-ok'))
+        .find((el) => el.textContent === 'OK') as HTMLElement;
+    okButton.click();
+    await awaitCheck((() => demog.col('TestCol1') != undefined && demog.col('TestCol2') != undefined), '', 3000);
+
+    isColumnPresent(demog.columns, 'TestCol1');
+    isColumnPresent(demog.columns, 'TestCol2');
+    
     v.close();
     grok.shell.tables.forEach((t) => grok.shell.closeTable(t));
   });  
