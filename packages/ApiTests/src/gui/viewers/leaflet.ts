e36903db3cb2d3db11b73f4163a3f88c61cc25bc
Anna Muzychyna <annamuz69@gmail.com>
2022-12-12 10:00:05 +0200

    fix UI tests (awaitCheck)

--- packages/ApiTests/src/gui/viewers/leaflet.ts
+++ packages/ApiTests/src/gui/viewers/leaflet.ts
@@ -1,8 +1,8 @@
-import {after, before, category, delay, test} from '@datagrok-libraries/utils/src/test';
+import {after, before, category, delay, test, awaitCheck} from '@datagrok-libraries/utils/src/test';
 import * as grok from 'datagrok-api/grok';
 //import * as ui from 'datagrok-api/ui';
 import * as DG from 'datagrok-api/dg';
-import {isViewerPresent, waitForElement} from '../gui-utils';
+import {isViewerPresent} from '../gui-utils';
 
 
 category('Viewers: Leaflet', () => {
@@ -13,34 +13,29 @@ category('Viewers: Leaflet', () => {
     demog = grok.data.demo.demog(1000);
     tv = grok.shell.addTableView(demog);
   });
-
   
   test('leaflet.visual', async () => {
     tv.addViewer('Leaflet');
-    await waitForElement('.d4-leaflet', 'cannot find Leaflet');
+    await awaitCheck(() => document.querySelector('.d4-leaflet') !== null, 'cannot find leaflet', 3000);
     isViewerPresent(Array.from(tv.viewers), 'Leaflet');
-      
     const leaflet = document.querySelector('[name=viewer-Leaflet]') as HTMLElement;
     const zoomIn = leaflet.getElementsByClassName('leaflet-control-zoom-in')[0] as HTMLElement;
     const zoomOut = leaflet.getElementsByClassName('leaflet-control-zoom-out')[0] as HTMLElement;
-    
     zoomIn.click();
     await delay(500);
     zoomOut.click();
     await delay(500);
   });
 
-
   test('leaflet.serialization', async () => {
     tv.addViewer('Leaflet');
-    await waitForElement('.d4-leaflet', 'cannot find Leaflet');
+    await awaitCheck(() => document.querySelector('.d4-leaflet') !== null, 'cannot find leaflet', 3000);
     const layout = tv.saveLayout();
     tv.resetLayout();
     tv.loadLayout(layout);
-    await waitForElement('.d4-leaflet', 'cannot find Leaflet');
+    await awaitCheck(() => document.querySelector('.d4-leaflet') !== null, 'cannot find leaflet', 3000);
     isViewerPresent(Array.from(tv.viewers), 'Leaflet');
   });
-
 
   after(async () => {
     tv.close();
