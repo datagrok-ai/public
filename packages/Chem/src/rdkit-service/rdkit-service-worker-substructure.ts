310f402a31f8f2013f4935f1527b8703293a5eec
Leonid <stolbovla@yandex.ru>
2022-12-13 21:37:05 +0300

    Chem: #1325 fix queries alignement

--- packages/Chem/src/rdkit-service/rdkit-service-worker-substructure.ts
+++ packages/Chem/src/rdkit-service/rdkit-service-worker-substructure.ts
@@ -43,7 +43,7 @@ export class RdKitServiceWorkerSubstructure extends RdKitServiceWorkerSimilarity
       let queryMol: RDMol | null = null;
       try {
         if (isMolBlock(queryMolString))
-          queryMol = this.getMol(queryMolString);
+          queryMol = this.getQMol(queryMolString);
         else {
           let createdQMol = false;
           try {
