08af79a3e1928ed3999c959b9ecd04e72538be7c
Oleksandra Serhiienko <59337516+Aleksashka11@users.noreply.github.com>
2022-12-15 18:43:27 +0200

    Issue #946: Removing ADME/T widget from Chem and adding a function to run it

--- packages/ADMETox/src/package.ts
+++ packages/ADMETox/src/package.ts
@@ -2,10 +2,19 @@
 import * as grok from 'datagrok-api/grok';
 import * as ui from 'datagrok-api/ui';
 import * as DG from 'datagrok-api/dg';
+import { getModelsSingle } from './admet-analysis/admet-calculation';
 
 export const _package = new DG.Package();
 
 //name: info
 export function info() {
   grok.shell.info(_package.webRoot);
+}
+
+//name: ADME/T Calculators
+//tags: panel, chem, widgets
+//input: string smiles { semType: Molecule }
+//output: widget result
+export function admetWidget(smiles: string): DG.Widget<any> {
+  return getModelsSingle(smiles);
 }
