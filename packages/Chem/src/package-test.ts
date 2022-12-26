425f7cc0547dcb20ef6f64acbb2452bec0457726
Anna Muzychyna <annamuz69@gmail.com>
2022-12-26 11:48:18 +0200

    GROK-11401 Chem

--- packages/Chem/src/package-test.ts
+++ packages/Chem/src/package-test.ts
@@ -23,6 +23,8 @@ import './tests/screening-tools';
 import './tests/save-as-sdf-tests';
 import './tests/substructure-filter-tests';
 
+import './tests/viewers';
+
 export const _package = new DG.Package();
 export {tests};
 
