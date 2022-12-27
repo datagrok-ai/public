9141e18a905a1930efa56ff397f58eeedc3be170
Dmitry Illarionov <illarionow@yahoo.com>
2022-12-27 17:44:56 +0100

    Fix layers UI

--- packages/GIS/src/gis-mapcontrols.ts
+++ packages/GIS/src/gis-mapcontrols.ts
@@ -2,7 +2,7 @@
 import * as grok from 'datagrok-api/grok';
 import * as ui from 'datagrok-api/ui';
 import * as DG from 'datagrok-api/dg';
-
+import $ from 'cash-dom';
 import {OpenLayers} from '../src/gis-openlayer';
 import {Control, defaults as defaultControls} from 'ol/control'; //Attribution include?
 
@@ -43,7 +43,7 @@ export class PanelLayersControl extends Control {
     const df = grok.data.demo.demog(6);
     // const layersGrid = DG.Viewer.grid(this.dfLayersList, layersGridStyle);
     const layersGrid = DG.Viewer.grid(df, layersGridStyle);
-    layersGrid.autoSize(200, 170);
+    layersGrid.autoSize(200, 200, 200, 30, true);
 
     // const element = ui.box(layersGrid.root);
     const element = ui.div(layersGrid.root);
@@ -114,17 +114,21 @@ export class PanelLayersControl extends Control {
       } //setup visible button
 
       if (gc.isTableCell && gc.gridColumn.name === 'del') {
-        const btnDel = ui.iconFA('trash-alt', (evt) => {
+        const btnDel = ui.button(ui.iconFA('trash-alt'), () => {
+          grok.shell.info('Delete')
         }, 'Delete layer');
-        btnDel.style.margin = '3px';
-        gc.style.element = ui.divV([btnDel]);
+        btnDel.style.width = '18px';
+        btnDel.style.height = '18px';
+        gc.style.element = btnDel;
       } //setup delete button
 
       if (gc.isTableCell && gc.gridColumn.name === 'exp') {
-        const btnExp = ui.iconFA(gc.cell.value ? 'download' : '', (evt) => {
+        const btnExp = ui.button(ui.iconFA(gc.cell.value ? 'arrow-to-bottom' : ''), () => {
+          grok.shell.info('Export')
         }, 'Export layer data to table');
-        btnExp.style.margin = '3px';
-        gc.style.element = ui.divV([btnExp]);
+        btnExp.style.width = '18px';
+        btnExp.style.height = '18px';
+        gc.style.element = btnExp;
       } //setup export button
     });
   }
@@ -159,9 +163,9 @@ export class BtnLayersControl extends Control {
     const options = opt || {};
 
     // const button = document.createElement('button');
-    const button = ui.iconFA('layer-group');
+    const button = ui.button(ui.iconFA('layer-group'),()=>{}, 'Map layers');
     // button.innerHTML = 'L';
-    // button.className = 'btn-layers ol-unselectable ol-control';
+     button.className = 'btn-layers ol-unselectable ol-control';
 
     const element = document.createElement('div');
     // const element = ui.iconFA('layer-group');
