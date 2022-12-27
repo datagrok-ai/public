3ad37b9e0ede5ad5fdac54cc47020038389558f3
Oleksandra Serhiienko <59337516+Aleksashka11@users.noreply.github.com>
2022-12-27 10:22:54 +0200

    Issue #946: Connect web service logic with ui (added columns with predictions + predictions in the property panel)

--- packages/ADMETox/src/admet-analysis/admet-calculation.ts
+++ packages/ADMETox/src/admet-analysis/admet-calculation.ts
@@ -6,6 +6,51 @@ import { properties } from './const';
 const _STORAGE_NAME = 'admet_models';
 const _KEY = 'selected';
 
+async function accessServer(url: string, csvString: string) {
+  const params: RequestInit = {
+    method: 'POST',
+    headers: {
+      'Accept': 'text/csv',
+      'Content-type': 'text/csv'
+    },
+    body: csvString
+  };
+  const response = await fetch(url, params);
+  const csv = await response.text();
+  return csv;
+}
+
+export async function addPredictions(smilesCol: DG.Column, viewTable: DG.DataFrame): Promise<void> {
+  openModelsDialog(await getSelected(), async (selected: any) => {
+    await grok.dapi.userDataStorage.postValue(_STORAGE_NAME, _KEY, JSON.stringify(selected));
+    selected = await getSelected();
+    const queryParams = selected.join(',');
+    const pi = DG.TaskBarProgressIndicator.create('Evaluating predictions');
+    let csvString = await accessServer(`http://localhost:1111/smiles/df_upload/?models=${queryParams}`, DG.DataFrame.fromColumns([smilesCol]).toCsv());
+    csvString = csvString.replaceAll('"', '');
+    let table = DG.DataFrame.fromCsv(csvString);
+    table.rows.removeAt(table.rowCount - 1);
+    addResultColumns(table, viewTable);
+    pi.close();
+  });
+}
+
+function addResultColumns(table: DG.DataFrame, viewTable: DG.DataFrame): void {
+  if (table.columns.length > 0) {
+    const modelNames: string[] = [];
+    const prevColNames = table.columns.names();
+    for (let i = 0; i < prevColNames.length; ++i) {
+      modelNames[i] = table.get(prevColNames[i], 0);
+    }
+    table.rows.removeAt(0);
+    for (let i = 0; i < prevColNames.length; ++i) {
+      const column: DG.Column = table.columns.byName(prevColNames[i]);
+      column.name = viewTable.columns.getUnusedName(modelNames[i]);
+      viewTable.columns.add(column);
+    }
+  }
+}
+
 export function getModelsSingle(smiles: string): DG.Widget {
   const widget = new DG.Widget(ui.div());
   const result = ui.div();
@@ -15,8 +60,27 @@ export function getModelsSingle(smiles: string): DG.Widget {
     });
   });
   selectButton.style.marginTop = '20px';
+  
+  const update = () => {
+    removeChildren(result);
+    result.appendChild(ui.loader());
+    getSelected().then((selected) => {
+      const queryParams = selected.join(',');
+      accessServer(`http://localhost:1111/smiles/df_upload/?models=${queryParams}`,
+       `smiles
+        ${smiles}`
+      ).then((csvString: any) => {
+        removeChildren(result);
+        result.appendChild(ui.divText(csvString));
+      });
+    });
+  };
+
   widget.root.appendChild(result);
   widget.root.appendChild(selectButton);
+
+  update();
+
   return widget;
 }
 
@@ -78,5 +142,11 @@ async function getSelected() : Promise<any> {
   const str = await grok.dapi.userDataStorage.getValue(_STORAGE_NAME, _KEY);
   let selected = (str != null && str !== '') ? JSON.parse(str) : [];
   return selected;
+}
+
+//description: Removes all children from node
+function removeChildren(node: any): void {
+  while (node.firstChild)
+    node.removeChild(node.firstChild);
 }
 
--- packages/ADMETox/src/package.ts
+++ packages/ADMETox/src/package.ts
@@ -2,6 +2,7 @@
 import * as grok from 'datagrok-api/grok';
 import * as ui from 'datagrok-api/ui';
 import * as DG from 'datagrok-api/dg';
+import { addPredictions } from './admet-analysis/admet-calculation';
 import { getModelsSingle } from './admet-analysis/admet-calculation';
 
 export const _package = new DG.Package();
@@ -17,4 +18,11 @@ export function info() {
 //output: widget result
 export function admetWidget(smiles: string): DG.Widget<any> {
   return getModelsSingle(smiles);
+}
+
+//name: ADMETox | ADMETox Calculators
+//input: column col {semType: Molecule}
+export async function admetoxCalculators(col: DG.Column) {
+  let table = col.dataFrame;
+  return await addPredictions(col, table);
 }
