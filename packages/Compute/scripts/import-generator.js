ea9923cdd1d7e9433c909d6e0ccdf3d46e16250d
Aufar Zakiev <zaufar@gmail.com>
2022-12-29 17:10:28 +0300

    Compute: import script refinment

--- packages/Compute/scripts/import-generator.js
+++ packages/Compute/scripts/import-generator.js
@@ -10,14 +10,27 @@
 const dfOutputsLines = dfSheetsToSeek.split(',').map((dfName) => `//output: dataframe ${dfName.trim()} { viewer: Grid(); }`).join("\n");
 const scalarsLines = scalarsToSeek.split(',').map((scalarName) => `//output: string ${scalarName.trim()}`).join("\n");
 
+const fileParsingCode = `const wb = new ExcelJS.workbook();
+await wb.xlsx.load(await uploadedFile.arrayBuffer());
+`
+
+const dfParsingCode = (dfName) => `const ${dfName}_ws = wb.getWorksheet(${dfName});
+let data = await ${dfName}_ws.workbook.csv.writeBuffer() as Buffer;
+${dfName} = grok.data.parseCsv(data.toString());
+`;
+
 let scriptText = `//name: ${scriptName}` + "\n";
 scriptText += `//description: ${description}` + "\n";
 scriptText += `//language: javascript` + "\n";
-scriptText += `//input: string file {type: file}` + "\n";
+scriptText += `//input: string uploadedFile {type: file}` + "\n";
 scriptText += scalarsLines + "\n";
 scriptText += dfOutputsLines + "\n";
 scriptText += `//editor: Compute:FunctionViewEditor` + "\n";
-scriptText += 
+scriptText += fileParsingCode + "\n";
+
+dfSheetsToSeek.split(',').forEach((dfName) => {
+    scriptText += dfParsingCode(dfName.trim()) + "\n";
+});
 
 const duplicates = await grok.dapi.scripts.filter(`name="${scriptName}"`).list();
 
