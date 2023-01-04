76dd249639871bf4a436858cb8e9bfaa94ae47ca
Anna Muzychyna <annamuz69@gmail.com>
2023-01-04 16:58:30 +0200

    GROK-11088 Remove delays in JS UI Tests

--- packages/ApiTests/src/gui/viewers/matrix-plot.ts
+++ packages/ApiTests/src/gui/viewers/matrix-plot.ts
@@ -1,90 +1,85 @@
-import {after, before, category, delay, expect, test} from '@datagrok-libraries/utils/src/test';
+/* eslint-disable no-throw-literal */
 import * as grok from 'datagrok-api/grok';
-import * as ui from 'datagrok-api/ui';
+// import * as ui from 'datagrok-api/ui';
 import * as DG from 'datagrok-api/dg';
-import {isColumnPresent, isViewerPresent, isDialogPresent, returnDialog, setDialogInputValue, uploadProject, findViewer, waitForElement} from '../gui-utils';
-import { Viewer } from 'datagrok-api/dg';
+
+import {after, before, category, test, awaitCheck} from '@datagrok-libraries/utils/src/test';
+import {isViewerPresent, uploadProject, findViewer} from '../gui-utils';
+
 
 category('Viewers: Matrix Plot', () => {
   let v: DG.TableView;
-  const demog = grok.data.demo.demog(1000);
+  let demog: DG.DataFrame;
 
   before(async () => {
+    demog = grok.data.demo.demog(1000);
     v = grok.shell.addTableView(demog);
   });
 
   test('matrixPlot.visual', async () => {
-    let matrixPlotIcon = document.getElementsByClassName('svg-matrix-plot')[0] as HTMLElement;
+    const matrixPlotIcon = document.getElementsByClassName('svg-matrix-plot')[0] as HTMLElement;
     matrixPlotIcon.click(); 
-    await waitForElement('.d4-matrix-plot', 'matrix plot not found');
-
+    await awaitCheck(() => document.querySelector('.d4-matrix-plot') !== null, 'matrix plot not found', 3000);
     isViewerPresent(Array.from(v.viewers), 'Matrix plot');
+    const protpertiesBtn = document.getElementsByClassName('panel-titlebar disable-selection panel-titlebar-tabhost')[0]
+      .getElementsByClassName('grok-icon grok-font-icon-settings')[0] as HTMLElement;
+    protpertiesBtn.click();
+    await awaitCheck(() => document.querySelector('.grok-prop-panel') !== null,
+      'matrix plot properties not found', 1000);
+    const hamburgerBtn = document.getElementsByClassName('panel-titlebar disable-selection panel-titlebar-tabhost')[0]
+      .getElementsByClassName('grok-icon grok-font-icon-menu')[0] as HTMLElement;
+    hamburgerBtn.click();
+    await awaitCheck(() => document.querySelector('.d4-menu-item-container.d4-vert-menu.d4-menu-popup') !== null,
+      'hamburger menu not found', 1000);
+    const closeBtn = document.getElementsByClassName('panel-titlebar disable-selection panel-titlebar-tabhost')[0]
+      .getElementsByClassName('grok-icon grok-font-icon-close')[0] as HTMLElement;
+    closeBtn.click();
+    await awaitCheck(() => Array.from(v.viewers).length === 1, 'Matrix plot viewer was not closed', 1000);
+  });
 
-    let protpertiesBtn = document.getElementsByClassName('panel-titlebar disable-selection panel-titlebar-tabhost')[0].getElementsByClassName('grok-icon grok-font-icon-settings')[0] as HTMLElement;
-    protpertiesBtn.click(); 
-    await waitForElement('.grok-prop-panel', 'matrix plot properties not found');
-
-    if (document.getElementsByClassName('property-grid-base property-grid-disable-selection').length == 0)
-        throw 'Properties table does not open'
-
-    let hamburgerBtn = document.getElementsByClassName('panel-titlebar disable-selection panel-titlebar-tabhost')[0].getElementsByClassName('grok-icon grok-font-icon-menu')[0] as HTMLElement;
-    hamburgerBtn.click(); await delay(1000);
-
-    let closeBtn = document.getElementsByClassName('panel-titlebar disable-selection panel-titlebar-tabhost')[0].getElementsByClassName('grok-icon grok-font-icon-close')[0] as HTMLElement;
-    closeBtn.click(); await delay(1000);
-
-    for (let i:number = 0; i < Array.from(v.viewers).length; i++) {
-        if (Array.from(v.viewers)[i].type == 'Matrix Plot') {
-            throw 'Matrix Plot viewer was not closed'
-        }
-    }
-  }); 
   test('matrixPlot.api', async () => {
-    let matrixPlot = v.matrixPlot({
-        xColumnNames: ['subj', 'age', 'started'],
-        yColumnNames: ['subj', 'age', 'started']
-        }); await delay(500);
+    const matrixPlot = v.matrixPlot({
+      xColumnNames: ['subj', 'age', 'started'],
+      yColumnNames: ['subj', 'age', 'started'],
+    });
+    await awaitCheck(() => document.querySelector('.d4-matrix-plot') !== null, 'matrix plot not found', 3000);
 
     if (matrixPlot.props.xColumnNames.length != 3)
-        throw 'xColumnNames property was set incorrectly' 
+      throw 'xColumnNames property was set incorrectly'; 
     if (matrixPlot.props.yColumnNames.length != 3)
-        throw 'yColumnNames property was set incorrectly'     
+      throw 'yColumnNames property was set incorrectly';     
   
     matrixPlot.setOptions({
-        title: 'Test Matrix Plot',
-        cellPlotType: 'scatter',
-    }); await delay(500);
+      title: 'Test Matrix Plot',
+      cellPlotType: 'scatter',
+    });
 
-    if (matrixPlot.props.title != 'Test Matrix Plot')
-        throw 'title property has not been set'
+    await awaitCheck(() => matrixPlot.props.title === 'Test Matrix Plot', 'title property has not been set', 1000);
     if (matrixPlot.props.cellPlotType != 'scatter')
-        throw 'cellPlotType property has not been set to "scatter"' 
-  });  
+      throw 'cellPlotType property has not been set to "scatter"'; 
+  });
+
+  // Does not work through Test Manager
   test('matrixPlot.serialization', async () => {
     await uploadProject('Test project with Matrix Plot', demog.getTableInfo(), v, demog);
-
-    grok.shell.closeAll(); await delay(500);
-
+    grok.shell.closeAll();
     await grok.dapi.projects.open('Test project with Matrix Plot');
-
     v = grok.shell.getTableView('demog 1000');
-
     isViewerPresent(Array.from(v.viewers), 'Matrix plot');
-
-    let matrixPlot = findViewer('Matrix plot', v);
+    const matrixPlot = findViewer('Matrix plot', v);
     
     if (matrixPlot!.props.xColumnNames.length != 3)
-        throw 'xColumnNames property has not been deserialized incorrectly' 
+      throw 'xColumnNames property has not been deserialized incorrectly'; 
     if (matrixPlot!.props.yColumnNames.length != 3)
-        throw 'yColumnNames property has not been deserialized incorrectly'
-        if (matrixPlot!.props.title != 'Test Matrix Plot')
-        throw 'title property has not been deserialized'
+      throw 'yColumnNames property has not been deserialized incorrectly';
+    if (matrixPlot!.props.title != 'Test Matrix Plot')
+      throw 'title property has not been deserialized';
     if (matrixPlot!.props.cellPlotType != 'scatter')
-        throw 'cellPlotType property has not been deserialized' 
+      throw 'cellPlotType property has not been deserialized'; 
   }); 
+
   after(async () => {
-    v.close();
     grok.shell.closeAll();
-    await grok.dapi.projects.delete(await grok.dapi.projects.filter('Test project with Matrix Plot').first())
+    await grok.dapi.projects.delete(await grok.dapi.projects.filter('Test project with Matrix Plot').first());
   }); 
 });
