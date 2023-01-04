64aad8d590e2b080abcc832ed9932bbeb3d81420
Alexey <achopovsky@datagrok.ai>
2023-01-04 16:22:42 +0200

    ST: monomer lib viewer, WIP

--- packages/SequenceTranslator/src/utils/monomer-lib-viewer.ts
+++ packages/SequenceTranslator/src/utils/monomer-lib-viewer.ts
@@ -4,6 +4,35 @@ import * as ui from 'datagrok-api/ui';
 import * as DG from 'datagrok-api/dg';
 
 import {LIB_PATH, DEFAULT_LIB_FILENAME} from './const';
+import {SYNTHESIZERS, TECHNOLOGIES} from '../hardcode-to-be-eliminated/map';
+
+import {Monomer} from '@datagrok-libraries/bio/src/types';
+
+type CodesField = {
+  [synthesizer: string]: {
+    [technology: string]: string[]
+  }
+}
+
+type MonomerExtension = {
+  [key: string]: string | CodesField
+}
+
+type ExtendedMonomer = Monomer & MonomerExtension;
+
+type FormattedMonomer = {
+  [key: string]: string
+}
+
+const EXTENDED_SYNTHESIZERS = Object.assign({}, SYNTHESIZERS, {'Modification': 'Modification'});
+
+const EXTENDED_TECHNOLOGIES = Object.assign({}, TECHNOLOGIES, {'modification': 'modification'});
+
+const enum RELEVANT_FIELD {
+  NAME = 'name',
+  MOLFILE = 'molfile',
+  CODES = 'codes',
+}
 
 export async function viewMonomerLib(): Promise<void> {
   const table = await parseMonomerLib(LIB_PATH, DEFAULT_LIB_FILENAME);
@@ -13,7 +42,38 @@ export async function viewMonomerLib(): Promise<void> {
 async function parseMonomerLib(path: string, fileName: string): Promise<DG.DataFrame> {
   const fileSource = new DG.FileSource(path);
   const file = await fileSource.readAsText(fileName);
-  const obj = JSON.parse(file);
-  const df = DG.DataFrame.fromObjects(obj)!;
+  const objList = JSON.parse(file);
+  const formattedObjectsList = new Array(objList.length);
+  for (let i = 0; i < objList.length; i++)
+    formattedObjectsList[i] = formatMonomerObject(objList[i]);
+  const df = DG.DataFrame.fromObjects(formattedObjectsList)!;
+  // for (const key of Object.keys(EXTENDED_SYNTHESIZERS))
+  //   console.log('synth:', key);
+  // for (const key of Object.keys(EXTENDED_TECHNOLOGIES))
+  //   console.log('tech:', key);
   return df;
+}
+
+function formatMonomerObject(sourceObj: ExtendedMonomer): FormattedMonomer {
+  const formattedObject: FormattedMonomer = {};
+  formattedObject[RELEVANT_FIELD.NAME] = sourceObj[RELEVANT_FIELD.NAME];
+  formattedObject[RELEVANT_FIELD.MOLFILE] = sourceObj[RELEVANT_FIELD.MOLFILE];
+  const codes = sourceObj[RELEVANT_FIELD.CODES] as CodesField;
+  for (const synthesizer of Object.values(EXTENDED_SYNTHESIZERS)) {
+    const fieldName = synthesizer;
+    const valuesList = [];
+    const technologySet = new Set();
+    for (const technology of Object.values(EXTENDED_TECHNOLOGIES)) {
+      if (codes[synthesizer] !== undefined) {
+        if (codes[synthesizer][technology] !== undefined) {
+          valuesList.push(codes[synthesizer][technology].toString());
+          technologySet.add(technology);
+        }
+      }
+    }
+    formattedObject['technologies'] = [...technologySet].toString();
+    formattedObject[fieldName] = valuesList.toString();
+  }
+  console.log('formatted:', formattedObject);
+  return formattedObject;
 }
