e4b4f929f92560d8b48e49d09f812f00903b74a6
Alexey <achopovsky@datagrok.ai>
2023-01-09 23:30:08 +0200

    ST: MVC refactoring, initial commit

---
+++ packages/SequenceTranslator/src/view/view-const.ts
@@ -0,0 +1,9 @@
+/* Do not change these import lines to match external modules in webpack configuration */
+import * as grok from 'datagrok-api/grok';
+import * as ui from 'datagrok-api/ui';
+import * as DG from 'datagrok-api/dg';
+
+export const MAIN = 'MAIN';
+export const AXOLABS = 'AXOLABS';
+export const SDF = 'SDF';
+export const DEFAULT_SEQUENCE = 'fAmCmGmAmCpsmU';
