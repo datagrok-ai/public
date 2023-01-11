f23254df79f414cbc18e565c7038ffc62320041e
Oleksandra Serhiienko <59337516+Aleksashka11@users.noreply.github.com>
2023-01-11 13:50:41 +0200

    ScaffoldTree: script changes (return molblocks instead of smiles, json dumps result fixes)

--- packages/Chem/scripts/scaffold_tree.py
+++ packages/Chem/scripts/scaffold_tree.py
@@ -2,35 +2,37 @@
 #description: generation scaffold tree from dataset
 #language: python
 #input: dataframe data [Input data table]
-#input: string smiles
+#input: string smiles 
 #output: string result
 
 import scaffoldgraph as sg
 import networkx as nx
 import json
+from rdkit.Chem import MolToMolBlock, MolFromSmiles
 
 #function that recursively adds child_nodes to hierarchies depending on the prev_scaffold value
-def recurs_append_nodes(key, value, option, node, obj):
-    if key in obj:
+def recurs_append_nodes(key, value, node, obj):
+    if key in obj: 
         if obj[key] == value:
-            obj[option].insert(0, node)
+            obj['child_nodes'].insert(0, node)
             return obj
     for k, v in obj.items():
-        if type(v) == list and k == option:
-            result = recurs_append_nodes(key, value, option, node, v[0])
+        if type(v) == list:
+            result = recurs_append_nodes(key, value, node, v[0])
 
 # function that returns nodes of each scaffold
 def find_nodes(tree, nodes, scaffold):
     scaffold_nodes = []
     for i in range(len(nodes)):
         predecessors = list(nx.bfs_tree(tree, nodes[i], reverse=True))
-        if len(predecessors) > 1 and predecessors[1] == scaffold:
+        if predecessors[1] == scaffold:
             scaffold_nodes.append(nodes[i])
     return scaffold_nodes
 
 #function that returns the scaffold parent
 def get_parent(tree, scaffold):
     return ''.join(tree.get_parent_scaffolds(scaffold, max_levels=1))
+
 
 #function that returns the right order of scaffolds
 def get_sorted_scaffolds(tree, nodes):
@@ -47,79 +49,89 @@ def get_sorted_scaffolds(tree, nodes):
                 prev_node = nodes[i]
                 last = i
         nodes.remove(nodes[last])
+    print(result)
     return result
 
 #function that returns scaffold hierarchies
 def get_hierarchies_list(tree, sorted_scaffolds):
     result = []
     for scaffold in sorted_scaffolds:
-        result.append(tree.nodes[scaffold]['hierarchy'])
+        result.append(scaffold[1]['hierarchy'])
+    print(result)
     return result
 
+#function that returns mols
+def get_mols(sorted_scaffolds):
+    scaffolds = list(tree.get_scaffold_nodes(True))
+    result = []
+    for scaffold in sorted_scaffolds:
+        for i in range(len(scaffolds)):
+            if scaffolds[i][0] == scaffold:
+                result.append(scaffolds[i][1]['realmolecule'])
+    print(result)
+    return result
+    
+
 #function that returns dict for each hierarchy depending on the input data
-def get_hierarchy_dict(scaffold_str, child_nodes_list, orphan_nodes_list):
+def get_hierarchy_dict(scaffold_str, smiles_str, child_nodes_list):
     hierarchy_dict = {
-        'scaffold': scaffold_str,
-        'child_nodes': child_nodes_list,
-        'orphan_nodes': orphan_nodes_list
+        'scaffold': scaffold_str, 
+        'smiles': smiles_str,
+        'child_nodes': child_nodes_list
     }
     return hierarchy_dict
+
 
 #function that returns first hierarchy scaffolds
 def get_first_hierarchy(tree, scaffolds):
     first_hierarchy_scaffolds = []
     for scaffold in scaffolds:
-        if tree.nodes[scaffold]['hierarchy'] == 1:
+        if scaffold[1]['hierarchy'] == 1:
             first_hierarchy_scaffolds.append(scaffold)
+    print(first_hierarchy_scaffolds)
     return first_hierarchy_scaffolds
+
 
 #function that returns the tree for first_hierarchy_scaffolds (if there are multiple mcs)
 def get_tree(tree, scaffold_1):
     json_list = []
     scaffolds = []
-    orphans = []
-    orphans_counter = 0
-    first = True
-    prev_scaffold = ''
     scaffolds.append(scaffold_1)
+    print(list(tree.get_child_scaffolds(scaffold_1)))
     scaffolds.extend(list(tree.get_child_scaffolds(scaffold_1)))
     sorted_scaffolds = get_sorted_scaffolds(tree, scaffolds)
+    sorted_scaffolds_mols = get_mols(sorted_scaffolds)
     nodes = list(tree.get_molecule_nodes())
     for i in range(0, len(sorted_scaffolds)):
-        hierarchy_dict = get_hierarchy_dict(sorted_scaffolds[i], [], [])
-        molecule_nodes = find_nodes(tree, nodes, sorted_scaffolds[i])
-        for j in range(0, len(molecule_nodes)):
-            hierarchy_dict['child_nodes'].append(get_hierarchy_dict(molecule_nodes[j], [], []))
-        if tree.nodes[sorted_scaffolds[i]]['hierarchy'] == 1:
-            json_list.append(hierarchy_dict)
+        try:
+            molbl = MolToMolBlock(sorted_scaffolds_mols[i], kekulize=False)
+        except:
+            molbl = MolToMolBlock(sorted_scaffolds_mols[i])
         else:
-            try:
-                recurs_append_nodes('scaffold', get_parent(tree, sorted_scaffolds[i]), 'child_nodes', hierarchy_dict, json_list[0])
-            except IndexError:
-                orphans_counter += 1
-                if first is True:
-                    prev_scaffold = sorted_scaffolds[i - 1]
-                first = False
-                if get_parent(tree, sorted_scaffolds[i]) == '':
-                    orphans.insert(0, hierarchy_dict)
-                else:
-                    recurs_append_nodes('scaffold', get_parent(tree, sorted_scaffolds[i]), 'child_nodes', hierarchy_dict, orphans[0])
-    if prev_scaffold != '':
-        recurs_append_nodes('scaffold', prev_scaffold, 'orphan_nodes', orphans, json_list[0])
+            hierarchy_dict = get_hierarchy_dict(str(molbl), sorted_scaffolds[i], [])
+            molecule_nodes = find_nodes(tree, nodes, sorted_scaffolds[i])
+            for j in range(0, len(molecule_nodes)):
+                hierarchy_dict['child_nodes'].append(get_hierarchy_dict(MolToMolBlock(MolFromSmiles(molecule_nodes[j])), molecule_nodes[j], []))
+            if tree.nodes[sorted_scaffolds[i]]['hierarchy'] == 1:
+                json_list.append(hierarchy_dict)
+            else:
+                recurs_append_nodes('smiles', ''.join(tree.get_parent_scaffolds(sorted_scaffolds[i], max_levels=1)), hierarchy_dict, json_list[0])
     return json_list[0]
 
 #function to get the json representation
 def get_json_representation(tree):
-    scaffolds = list(tree.get_scaffold_nodes())
+    scaffolds = list(tree.get_scaffold_nodes(True))
     first_scaffolds = get_first_hierarchy(tree, scaffolds)
     json_list = []
     for scaffold in first_scaffolds:
-        json_list.append(get_tree(tree, scaffold))
+        print('scaffold: ' + str(scaffold[0]))
+        json_list.append(get_tree(tree, scaffold[0]))
     return json_list
-
+  
 tree = sg.ScaffoldTree.from_dataframe(
     data, smiles_column=smiles, name_column=smiles, progress=True,
 )
 
 res = get_json_representation(tree)
-result = json.dumps(res)
+json_result = json.dumps(res)
+result = json_result.replace("\\n", "\n")
