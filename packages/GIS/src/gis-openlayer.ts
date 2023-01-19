a5217d309fcd991fc5fed1077ba8a715e4e4dceb
Dmitry Kovalyov <laykdimon278@gmail.com>
2023-01-19 13:07:48 +0200

    GIS: fixed odd adding of Markers GL Selection to layers list

--- packages/GIS/src/gis-openlayer.ts
+++ packages/GIS/src/gis-openlayer.ts
@@ -566,6 +566,11 @@ export class OpenLayers {
 
     if (recreate) {
       let previousLayer = this.olMarkersLayerGL;
+      if (previousLayer) {
+        this.olMap.removeLayer(previousLayer);
+        previousLayer.dispose();
+      }
+
       let src = this.olMarkersLayerGL?.getSource();
       this.olMarkersLayerGL = new WebGLPointsLayer({
         source: src ? src : this.olMarkersSource,
@@ -574,12 +579,13 @@ export class OpenLayers {
       this.olMarkersLayerGL.set('layerName', 'Markers GL');
       this.addLayer(this.olMarkersLayerGL);
 
+      //prepare markers selection layer
+      previousLayer = this.olMarkersSelLayerGL;
       if (previousLayer) {
         this.olMap.removeLayer(previousLayer);
         previousLayer.dispose();
       }
-      //prepare markers selection layer
-      previousLayer = this.olMarkersSelLayerGL;
+
       src = this.olMarkersSelLayerGL?.getSource();
       this.olMarkersSelLayerGL = new WebGLPointsLayer({
         source: src ? src : this.olMarkersSelSource,
@@ -589,11 +595,6 @@ export class OpenLayers {
       this.olMarkersSelLayerGL.set('layerName', 'Markers GL Selection');
       this.addLayer(this.olMarkersSelLayerGL);
       this.olMarkersSelLayerGL.setZIndex(100);
-
-      if (previousLayer) {
-        this.olMap.removeLayer(previousLayer);
-        previousLayer.dispose();
-      }
     } //<<if recreate
   }
 
