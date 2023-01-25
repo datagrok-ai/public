d1a2076434dce3b09f6822fa1c089568e3dedb8c
Leonid Stolbov <57249456+StLeonidas@users.noreply.github.com>
2023-01-25 13:55:09 +0300

    Lstolbov/curves (#1526)
    * Curve fit demo
    
    * Chem: work on curves
    
    * Chem: curve fit
    
    * Chem: curves - review fixes
    
    * SimFit: 11763 basic fitting tools

---
+++ packages/FittingTools/README.md
@@ -0,0 +1,4 @@
+# FittingTools
+
+FittingTools provides simulations and fitting support for all kind of processes. Statistical features are also supported. 
+Package uses optimization [lbfgs](https://github.com/mimno/jsLBFGS) algorithm.
---
+++ packages/FittingTools/webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const packageName = path.parse(require('./package.json').name).name.toLowerCase().replace(/-/g, '');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    test: {filename: 'package-test.js', library: {type: 'var', name:`${packageName}_test`}, import: './src/package-test.ts'},
+    package: './src/package.ts'
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.wasm', '.mjs', '.js', '.json'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts(x?)$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  devtool: 'inline-source-map',
+  externals: {
+    'datagrok-api/dg': 'DG',
+    'datagrok-api/grok': 'grok',
+    'datagrok-api/ui': 'ui',
+    'openchemlib/full.js': 'OCL',
+    'rxjs': 'rxjs',
+    'rxjs/operators': 'rxjs.operators',
+    'cash-dom': '$',
+    'dayjs': 'dayjs',
+    'wu': 'wu',
+    'exceljs': 'ExcelJS',
+  },
+  output: {
+    filename: '[name].js',
+    library: packageName,
+    libraryTarget: 'var',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
