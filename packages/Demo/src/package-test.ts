d8c7436b6da5304a0bb38b16042008d32aa3e317
Alex Paramonov <alex.aprm@gmail.com>
2023-02-01 19:17:54 +0000

    Removed extra menu items

--- packages/Demo/src/package-test.ts
+++ packages/Demo/src/package-test.ts
@@ -15,7 +15,6 @@ export {tests};
 //input: string test {optional: true}
 //input: object testContext {optional: true}
 //output: dataframe result
-//top-menu: Tools | Dev | JS API Tests
 export async function test(category: string, test: string, testContext: TestContext): Promise<DG.DataFrame> {
   const data = await runTests({category, test, testContext});
   return DG.DataFrame.fromObjects(data)!;
