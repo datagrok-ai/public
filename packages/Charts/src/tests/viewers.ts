db6a03ed3d52d209d29fb2012bc50d13246659fa
onuf <56073962+onuf@users.noreply.github.com>
2023-02-03 15:03:22 +0100

    Charts: more specific skip reasons

--- packages/Charts/src/tests/viewers.ts
+++ packages/Charts/src/tests/viewers.ts
@@ -10,6 +10,11 @@ import {energyUK, demog} from './test-data';
 category('Viewers', () => {
   const df = grok.data.demo.demog(100);
   const viewers = DG.Func.find({package: 'Charts', tags: ['viewer']}).map((f) => f.friendlyName);
+  const viewersToSkip: {[v: string]: string} = {
+    'SankeyViewer': '#1570',
+    'GlobeViewer': '#1571',
+    'GroupAnalysisViewer': '#1572',
+  };
   for (const v of viewers) {
     test(v, async () => {
       await testViewer(v, (() => {
@@ -17,6 +22,6 @@ category('Viewers', () => {
         else if (['TreeMapViewer', 'SunburstViewer'].includes(v)) return demog.clone();
         return df.clone();
       })());
-    }, {skipReason: 'GROK-11534'});
+    }, v in viewersToSkip ? { skipReason: viewersToSkip[v] } : {});
   }
 });
