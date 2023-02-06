f321ecf0c7e3ef2d66949edebb6bf04ac5fc9e7f
Anna Muzychyna <annamuz69@gmail.com>
2023-02-06 13:02:16 +0200

    GROK-11670 fixes
    connections, dapi, dataframe, GUI, ui, viewers

--- packages/ApiTests/src/connections/get-all-top100.ts
+++ packages/ApiTests/src/connections/get-all-top100.ts
@@ -1,57 +1,59 @@
-import {after, before, category, test, delay, awaitCheck} from '@datagrok-libraries/utils/src/test';
+import {after, before, category, test, awaitCheck} from '@datagrok-libraries/utils/src/test';
 import * as grok from 'datagrok-api/grok';
 //import * as ui from 'datagrok-api/ui';
 import * as DG from 'datagrok-api/dg';
 
 
 category('Connections', () => {
-  let v: DG.ViewBase;
+  let schema: DG.ViewBase;
   let table: HTMLElement;
+  let nw: HTMLElement;
 
   before(async () => {
-    try {
-      const mng: DG.TabPane = grok.shell.sidebar.getPane('Manage');
-      await (mng.content.querySelector('[data-view=connections]') as HTMLElement).click();
-      await awaitCheck(() => document.querySelector('[data-link="/e/ApiTests.PostgreSQLTest"]') !== null,
-        'cannot find Northwind connection', 3000);
-      const nw = document.querySelector('[data-link="/e/ApiTests.PostgreSQLTest"]');
-      v = grok.shell.v;
-      nw!.dispatchEvent(new MouseEvent('contextmenu', {bubbles: true}));
-      await delay(50);
-      const bs = Array.from(document.querySelectorAll('.d4-menu-item-label'))
-        .find((el) => (el as HTMLElement).innerText.includes('Browse schema')) as HTMLElement;
-      bs.click();
-      v.close();
-      await awaitCheck(() => document.querySelector('.d4-sketch-item') !== null, 'cannot find table', 3000);
-      table = document.querySelector('.d4-sketch-item') as HTMLElement;
-      v = grok.shell.v;
-    } catch (e) {
-      console.log(e);
-    }
+    const mng: DG.TabPane = grok.shell.sidebar.getPane('Manage');
+    await (mng.content.querySelector('[data-view=connections]') as HTMLElement).click();
+    await awaitCheck(() => document.querySelector('[data-link="/e/ApiTests.PostgresTest"]') !== null,
+      'cannot find Northwind connection', 3000);
+    nw = document.querySelector('[data-link="/e/ApiTests.PostgresTest"]') as HTMLElement;
   });
 
   test('getAll', async () => {
+    await browseSchema();
     table.dispatchEvent(new MouseEvent('contextmenu', {bubbles: true}));
-    await delay(50);
+    await awaitCheck(() => document.querySelector('.d4-menu-popup') !== null, 'cannot find context menu', 2000);
     const getAll = Array.from(document.querySelectorAll('.d4-menu-item-label'))
       .find((el) => (el as HTMLElement).innerText.includes('Get All')) as HTMLElement;
     getAll.click();
     await awaitCheck(() => document.querySelector('canvas') !== null, 'Get All does not work', 5000);
     grok.shell.v.close();
+    schema.close();
   });
 
   test('getTop100', async () => {
+    await browseSchema();
     table.dispatchEvent(new MouseEvent('contextmenu', {bubbles: true}));
-    await delay(50);
+    await awaitCheck(() => document.querySelector('.d4-menu-popup') !== null, 'cannot find context menu', 2000);
     const getTop100 = Array.from(document.querySelectorAll('.d4-menu-item-label'))
       .find((el) => (el as HTMLElement).innerText.includes('Get Top 100')) as HTMLElement;
     getTop100.click();
     await awaitCheck(() => document.querySelector('canvas') !== null, 'Get Top 100 does not work', 5000);
     grok.shell.v.close();
+    schema.close();
   });
 
   after(async () => {
-    v.close();
+    grok.shell.closeAll();
     grok.shell.tables.forEach((t) => grok.shell.closeTable(t));
   });
+
+  async function browseSchema() {
+    nw.dispatchEvent(new MouseEvent('contextmenu', {bubbles: true}));
+    await awaitCheck(() => document.querySelector('.d4-menu-popup') !== null, 'cannot find context menu');
+    const bs = Array.from(document.querySelectorAll('.d4-menu-item-label'))
+      .find((el) => (el as HTMLElement).innerText.includes('Browse schema')) as HTMLElement;
+    bs.click();
+    await awaitCheck(() => document.querySelector('.d4-sketch-item') !== null, 'cannot find table', 10000);
+    table = document.querySelector('.d4-sketch-item') as HTMLElement;
+    schema = grok.shell.v;
+  }
 });
