68baff009e55f3330505013b57bc3b8379a6e72d
Denis Kryvchuk <dendenysyk@gmail.com>
2023-02-08 22:46:04 +0200

    fix vertical rendering

--- packages/Chem/src/rendering/rdkit-cell-renderer.ts
+++ packages/Chem/src/rendering/rdkit-cell-renderer.ts
@@ -220,12 +220,13 @@ M  END
       ctx.save();
       const scl = ctx.getTransform();
       ctx.resetTransform();
-      ctx.translate(x, y);
+      ctx.translate(x + h, y);
       ctx.rotate(Math.PI / 2);
       if (scl.m11 < 1 || scl.m22 < 1)
         ctx.scale(scl.m11, scl.m22);
-      const bitmap = this.canvasReused.transferToImageBitmap();
-      ctx.drawImage(bitmap, 0, - (h), w, h);
+      let f = new OffscreenCanvas(imageData.width, imageData.height)!;
+      f.getContext('2d')?.putImageData(imageData, 0, 0);
+      ctx.drawImage(f, 0, 0)
       ctx.restore();
     } else {
       //const image = offscreenCanvas.getContext('2d')!.getImageData(0, 0, w, h);
