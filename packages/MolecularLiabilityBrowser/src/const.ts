a566ea42c7149075b1a23aa16503196cbf91c406
tanas80@gmail.com <tanas80@gmail.com>
2023-02-09 19:29:53 +0400

    MLB: Remove packages MolecularLiabilityBrowser, MolecularLiabilityBrowserData

--- packages/MolecularLiabilityBrowser/src/const.ts
+++
@@ -1,7 +0,0 @@
-import * as ui from 'datagrok-api/ui';
-import * as grok from 'datagrok-api/grok';
-import * as DG from 'datagrok-api/dg';
-
-export enum MlbEvents {
-  CdrChanged = 'cdrChanged',
-}
