fd0e49848ad654e828b4e6063e842a8c38c10ae9
Dmitry Kovalyov <laykdimon278@gmail.com>
2023-02-09 19:14:47 +0200

    Charts: removed unnecessary test-data file

--- packages/Charts/src/package-test.ts
+++ packages/Charts/src/package-test.ts
@@ -2,7 +2,7 @@ import * as DG from 'datagrok-api/dg';
 
 import {runTests, tests, TestContext} from '@datagrok-libraries/utils/src/test';
 
-import './tests/timelines-test';
+import './tests/timelines-viewer-test';
 import './tests/tree-viewer-test';
 import './tests/radar-viewer-test';
 import './tests/viewers';
