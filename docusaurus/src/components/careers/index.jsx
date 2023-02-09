2b4ec0b12ab82ecb087cbbb40f8dc0e5b1e2ced3
Dmitry Illarionov <illarionow@yahoo.com>
2023-02-09 16:52:36 +0100

    careers page update layout

--- docusaurus/src/components/careers/index.jsx
+++ docusaurus/src/components/careers/index.jsx
@@ -6,22 +6,10 @@ export function LayoutHeader ({children, meta}){
         <div className="row">
             <div className="row py-2 align-items-center">
                 <div className="col-lg-12 px-5">
-                    <div className='display-5 text-secondary pb-4'>{meta.title}</div>
-                    <p>{meta.description}</p>
+                    <div className='display-5 text-secondary pb-3'>{meta.title}</div>
+                    <p class="h6">{meta.description}</p>
                 </div>
             </div>
         </div>
     );
-}
-
-export function LayoutBody ({children, meta}){
-    return(
-        <div className="row justify-content-md-center text-center">
-            <div className="row py-3 align-items-center">
-                <div className="col-lg-12">
-                    {children}
-                </div>
-            </div>
-        </div>
-    )
 }
