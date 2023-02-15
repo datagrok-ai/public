58f38f5d67dfc8944fe1a4e2c5e71be70009a6aa
Volodymyr Dyma <vdyma@datagrok.ai>
2023-02-16 00:30:12 +0200

    #1524: fixed pepsea test

--- packages/Bio/src/tests/pepsea-tests.ts
+++ packages/Bio/src/tests/pepsea-tests.ts
@@ -5,11 +5,11 @@ import {runPepsea} from '../utils/pepsea';
 
 category('PepSeA', () => {
   const testCsv = `HELM,MSA
-  "PEPTIDE1{F.L.R.G.W.[MeF].Y.S.N.N.C}$$$$","F.L.R.G.W.MeF.Y.-.S.N.N.C"
-  "PEPTIDE1{F.L.R.G.Y.[MeF].Y.W.N.C}$$$$","F.L.R.G.Y.MeF.Y.W.-.-.N.C"
-  "PEPTIDE1{F.G.Y.[MeF].Y.W.S.D.N.C}$$$$","F.-.-.G.Y.MeF.Y.W.S.D.N.C"
+  "PEPTIDE1{F.L.R.G.W.[MeF].Y.S.N.N.C}$$$$","F.L.R.G.W.MeF.Y..S.N.N.C"
+  "PEPTIDE1{F.L.R.G.Y.[MeF].Y.W.N.C}$$$$","F.L.R.G.Y.MeF.Y.W...N.C"
+  "PEPTIDE1{F.G.Y.[MeF].Y.W.S.D.N.C}$$$$","F...G.Y.MeF.Y.W.S.D.N.C"
   "PEPTIDE1{F.L.R.G.Y.[MeF].Y.W.S.N.D.C}$$$$","F.L.R.G.Y.MeF.Y.W.S.N.D.C"
-  "PEPTIDE1{F.V.R.G.Y.[MeF].Y.W.S.N.C}$$$$","F.V.R.G.Y.MeF.Y.W.S.-.N.C"`;
+  "PEPTIDE1{F.V.R.G.Y.[MeF].Y.W.S.N.C}$$$$","F.V.R.G.Y.MeF.Y.W.S..N.C"`;
 
   test('Basic alignment', async () => {
     const table = DG.DataFrame.fromCsv(testCsv);
