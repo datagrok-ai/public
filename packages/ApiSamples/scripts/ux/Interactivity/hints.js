5c65947dd93645406725ec7a5eb6fa3368db2fc4
Diana Onuf <56073962+onuf@users.noreply.github.com>
2023-02-15 13:52:13 +0100

    Add wizard hints (#1555)
    * Add wizard hints
    
    * added fix to addTextHint
    
    * Hints: minor corrections
    
    * UI hints reworked
    
    * UI hints: updates
    
    ---------
    
    Co-authored-by: Dmitry Illarionov <illarionow@yahoo.com>

--- packages/ApiSamples/scripts/ux/Interactivity/hints.js
+++ packages/ApiSamples/scripts/ux/Interactivity/hints.js
@@ -13,10 +13,23 @@ $(addNCIcon).on('click', () => {
 });
 
 grok.events.onDialogShown.pipe(
-  rxjs.operators.first(),
-  rxjs.operators.filter((dlg) => dlg.title === 'Add New Column'))
+  rxjs.operators.filter((dlg) => dlg.title === 'Add New Column'),
+  rxjs.operators.first())
   .subscribe((dlg) => {
     const input = dlg.inputs[0].root;
     ui.hints.addHint(input);
     setTimeout(() => ui.hints.remove(input), 4000);
   });
+
+// You can also describe a series of visual components in the wizard. Each wizard page is associated
+// with the [showNextTo] element. Provide either [text] or [root] parameter to populate the page, the
+// other parameters are optional. The wizard header is shown only if [title] or [helpUrl] are provided.
+// The user can use the arrow buttons to navigate the set of instructions. The wizard can be closed
+// from the dialog header (the "x" icon), via the "Cancel" button, or via the [Wizard.close()] method.
+const sp = view.scatterPlot();
+const hist = view.histogram();
+
+const wizard = ui.hints.addTextHint({title: 'Viewers', pages: [
+ {caption: 'First Hint', text: 'Welcome! This is the 1st hint about a scatter plot.', showNextTo: sp.root},
+ {caption: 'Second Hint', root: ui.divText('This is the 2nd hint about a histogram.'),  showNextTo: hist.root},
+]});
