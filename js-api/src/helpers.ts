9014d0081f286df1a46614a6930bd9f5f761720f
dpetrov <dpetrov@datagrok.ai>
2023-02-19 19:39:49 -0800

    js-api: Removing accidental error

--- js-api/src/helpers.ts
+++ js-api/src/helpers.ts
@@ -17,7 +17,6 @@ export interface FormulaLine {
   min?: number;
   max?: number;
   formula?: string;
-  funcFormula?: string;
   showOnPlot?:boolean;
   showOnTooltip?:boolean;
   // Specific to lines:
