8124365e3f7e27c61467bf5679d0f839a3405274
Leonid Stolbov <57249456+StLeonidas@users.noreply.github.com>
2023-02-20 15:07:06 +0300

    Docking Demo

--- packages/BiostructureViewer/src/apps/ngl-viewer-app.ts
+++ packages/BiostructureViewer/src/apps/ngl-viewer-app.ts
@@ -7,20 +7,27 @@ import {PROPS as pdbPROPS} from '../viewers/ngl-viewer';
 
 export class NglViewerApp {
   private readonly appFuncName: string;
+  private pdb: string;
+  private df: DG.DataFrame;
 
   constructor(appFuncName: string) {
     this.appFuncName = appFuncName;
   }
 
-  async init(): Promise<void> {
-    await this.loadData();
+  async init(data?:{ligands: DG.DataFrame, macromolecule: string}): Promise<void> {
+    if(data) {
+      this.pdb = data.macromolecule;
+      this.setData(data.ligands);
+    }
+    else 
+      await this.loadData();
   }
 
   async loadData(): Promise<void> {
-    const sdfBytes: Uint8Array = await _package.files.readAsBytes('samples/mol1K.sdf');
+    const sdfBytes: Uint8Array = await _package.files.readAsBytes('samples/1bdq.sdf');
     const df: DG.DataFrame = (await grok.functions.call(
       'Chem:importSdf', {bytes: sdfBytes}))[0];
-
+    this.pdb = await _package.files.readAsText('samples/protease.pdb');
     await this.setData(df);
   }
 
@@ -30,8 +37,6 @@ export class NglViewerApp {
     await this.buildView();
   }
 
-  private df: DG.DataFrame;
-
   // -- View --
 
   private view: DG.TableView;
@@ -40,10 +45,8 @@ export class NglViewerApp {
     this.view = grok.shell.addTableView(this.df);
     this.view.path = this.view.basePath = `func/${_package.name}.${this.appFuncName}`;
 
-    const pdbStr: string = await _package.files.readAsText('samples/protease.pdb');
-
     const viewer: DG.JsViewer = (await this.view.dataFrame.plot.fromType('NglViewer', {
-      [pdbPROPS.pdb]: pdbStr,
+      [pdbPROPS.pdb]: this.pdb,
       [pdbPROPS.ligandColumnName]: 'molecule',
     })) as DG.JsViewer;
     this.view.dockManager.dock(viewer, DG.DOCK_TYPE.RIGHT, null, 'NGL', 0.4);
