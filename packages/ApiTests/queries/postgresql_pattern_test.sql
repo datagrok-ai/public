167d198dbef21dd5056d6a229c587a71c332561f
Alex Paramonov <alex.aprm@gmail.com>
2023-02-22 18:07:36 +0000

    Auto tests refactoring

--- packages/ApiTests/queries/postgresql_pattern_test.sql
+++ packages/ApiTests/queries/postgresql_pattern_test.sql
@@ -1,6 +1,6 @@
 --name: PostgresqlPatternsAll
 --connection: PostgreSQLApiTests
---test: ApiTests:compareTables(PostgresqlPatternsAll(), OpenFile('System:AppData/ApiTests/datasets/tests/data1-30.csv'))
+--test: ApiTests:expectTable(PostgresqlPatternsAll(), OpenFile('System:AppData/ApiTests/datasets/tests/data1-30.csv'))
 SELECT * FROM mock_data;
 --end
 
@@ -9,63 +9,63 @@ SELECT * FROM mock_data;
 --name: PostgresqlIntTypePatternNone
 --connection: PostgreSQLApiTests
 --input: int id = 20
---test: ApiTests:compareTables(PostgresqlIntTypePatternNone(), OpenFile('System:AppData/ApiTests/datasets/tests/data20.csv'))
+--test: ApiTests:expectTable(PostgresqlIntTypePatternNone(), OpenFile('System:AppData/ApiTests/datasets/tests/data20.csv'))
 SELECT * FROM mock_data WHERE id = @id;
 --end
 
 --name: PostgresqlStringTypeIntPatternOpMore
 --connection: PostgreSQLApiTests
 --input: string id = ">28" {pattern: int}
---test: ApiTests:compareTables(PostgresqlStringTypeIntPatternOpMore(), OpenFile('System:AppData/ApiTests/datasets/tests/data29-30.csv'))
+--test: ApiTests:expectTable(PostgresqlStringTypeIntPatternOpMore(), OpenFile('System:AppData/ApiTests/datasets/tests/data29-30.csv'))
 SELECT * FROM mock_data WHERE @id(id)
 --end
 
 --name: PostgresqlStringTypeIntPatternOpMoreEq
 --connection: PostgreSQLApiTests
 --input: string id = ">=29" {pattern: int}
---test: ApiTests:compareTables(PostgresqlStringTypeIntPatternOpMoreEq(), OpenFile('System:AppData/ApiTests/datasets/tests/data29-30.csv'))
+--test: ApiTests:expectTable(PostgresqlStringTypeIntPatternOpMoreEq(), OpenFile('System:AppData/ApiTests/datasets/tests/data29-30.csv'))
 SELECT * FROM mock_data WHERE @id(id)
 --end
 
 --name: PostgresqlStringTypeIntPatternOpLessEq
 --connection: PostgreSQLApiTests
 --input: string id = "<=1" {pattern: int}
---test: ApiTests:compareTables(PostgresqlStringTypeIntPatternOpLessEq(), OpenFile('System:AppData/ApiTests/datasets/tests/data1.csv'))
+--test: ApiTests:expectTable(PostgresqlStringTypeIntPatternOpLessEq(), OpenFile('System:AppData/ApiTests/datasets/tests/data1.csv'))
 SELECT * FROM mock_data WHERE @id(id)
 --end
 
 --name: PostgresqlStringTypeIntPatternOpLess
 --connection: PostgreSQLApiTests
 --input: string id = "<2" {pattern: int}
---test: ApiTests:compareTables(PostgresqlStringTypeIntPatternOpLess(), OpenFile('System:AppData/ApiTests/datasets/tests/data1.csv'))
+--test: ApiTests:expectTable(PostgresqlStringTypeIntPatternOpLess(), OpenFile('System:AppData/ApiTests/datasets/tests/data1.csv'))
 SELECT * FROM mock_data WHERE @id(id)
 --end
 
 --name: PostgresqlStringTypeIntPatternOpIn
 --connection: PostgreSQLApiTests
 --input: string id = "in(29, 30)" {pattern: int}
---test: ApiTests:compareTables(PostgresqlStringTypeIntPatternOpIn(), OpenFile('System:AppData/ApiTests/datasets/tests/data29-30.csv'))
+--test: ApiTests:expectTable(PostgresqlStringTypeIntPatternOpIn(), OpenFile('System:AppData/ApiTests/datasets/tests/data29-30.d42'))
 SELECT * FROM mock_data WHERE @id(id)
 --end
 
 --name: PostgresqlStringTypeIntPatternOpNotIn
 --connection: PostgreSQLApiTests
 --input: string id = "not in(21, 22, 23, 24, 25, 26, 27, 28, 29, 30)" {pattern: int}
---test: ApiTests:compareTables(PostgresqlStringTypeIntPatternOpNotIn(), OpenFile('System:AppData/ApiTests/datasets/tests/data1-20.csv'))
+--test: ApiTests:expectTable(PostgresqlStringTypeIntPatternOpNotIn(), OpenFile('System:AppData/ApiTests/datasets/tests/data1-20.csv'))
 SELECT * FROM mock_data WHERE @id(id)
 --end
 
 --name: PostgresqlStringTypeIntPatternOpMinMax
 --connection: PostgreSQLApiTests
 --input: string id = "min-max 29-30" {pattern: int}
---test: ApiTests:compareTables(PostgresqlStringTypeIntPatternOpMinMax(), OpenFile('System:AppData/ApiTests/datasets/tests/data29-30.csv'))
+--test: ApiTests:expectTable(PostgresqlStringTypeIntPatternOpMinMax(), OpenFile('System:AppData/ApiTests/datasets/tests/data29-30.csv'))
 SELECT * FROM mock_data WHERE @id(id)
 --end
 
 --name: PostgresqlStringTypeIntPatternOpNotEq
 --connection: PostgreSQLApiTests
 --input: string id = "!=1" {pattern: int}
---test: ApiTests:compareTables(PostgresqlStringTypeIntPatternOpNotEq(), OpenFile('System:AppData/ApiTests/datasets/tests/data2-30.csv'))
+--test: ApiTests:expectTable(PostgresqlStringTypeIntPatternOpNotEq(), OpenFile('System:AppData/ApiTests/datasets/tests/data2-30.csv'))
 SELECT * FROM mock_data WHERE @id(id)
 --end
 
@@ -75,35 +75,35 @@ SELECT * FROM mock_data WHERE @id(id)
 --name: PostgresqlDoubleTypePatternNone
 --connection: PostgreSQLApiTests
 --input: double some_number = 510.32
---test: ApiTests:compareTables(PostgresqlDoubleTypePatternNone(), OpenFile('System:AppData/ApiTests/datasets/tests/data1.csv'))
+--test: ApiTests:expectTable(PostgresqlDoubleTypePatternNone(), OpenFile('System:AppData/ApiTests/datasets/tests/data1.csv'))
 SELECT * FROM mock_data WHERE some_number = @some_number;
 --end
 
 --name: PostgresqlStringTypePatternDoubleOpMore
 --connection: PostgreSQLApiTests
 --input: string some_number = ">975" {pattern: double}
---test: ApiTests:compareTables(PostgresqlStringTypePatternDoubleOpMore(), OpenFile('System:AppData/ApiTests/datasets/tests/data10,26.csv'))
+--test: ApiTests:expectTable(PostgresqlStringTypePatternDoubleOpMore(), OpenFile('System:AppData/ApiTests/datasets/tests/data10,26.csv'))
 SELECT * FROM mock_data WHERE @some_number(some_number);
 --end
 
 --name: PostgresqlStringTypePatternDoubleOpMoreEq
 --connection: PostgreSQLApiTests
 --input: string some_number = ">=975" {pattern: double}
---test: ApiTests:compareTables(PostgresqlStringTypePatternDoubleOpMoreEq(), OpenFile('System:AppData/ApiTests/datasets/tests/data10,26.csv'))
+--test: ApiTests:expectTable(PostgresqlStringTypePatternDoubleOpMoreEq(), OpenFile('System:AppData/ApiTests/datasets/tests/data10,26.csv'))
 SELECT * FROM mock_data WHERE @some_number(some_number);
 --end
 
 --name: PostgresqlStringTypePatternDoubleOpLess
 --connection: PostgreSQLApiTests
 --input: string some_number = "<20" {pattern: double}
---test: ApiTests:compareTables(PostgresqlStringTypePatternDoubleOpLess(), OpenFile('System:AppData/ApiTests/datasets/tests/data5.csv'))
+--test: ApiTests:expectTable(PostgresqlStringTypePatternDoubleOpLess(), OpenFile('System:AppData/ApiTests/datasets/tests/data5.csv'))
 SELECT * FROM mock_data WHERE @some_number(some_number);
 --end
 
 --name: PostgresqlStringTypePatternDoubleOpLessEq
 --connection: PostgreSQLApiTests
 --input: string some_number = "<=20" {pattern: double}
---test: ApiTests:compareTables(PostgresqlStringTypePatternDoubleOpLessEq(), OpenFile('System:AppData/ApiTests/datasets/tests/data5.csv'))
+--test: ApiTests:expectTable(PostgresqlStringTypePatternDoubleOpLessEq(), OpenFile('System:AppData/ApiTests/datasets/tests/data5.csv'))
 SELECT * FROM mock_data WHERE @some_number(some_number);
 --end
 
@@ -124,35 +124,35 @@ SELECT * FROM mock_data WHERE country = @country;
 --name: PostgresqlStringTypePatternStringOpContains
 --connection: PostgreSQLApiTests
 --input: string first_name = "contains Z" {pattern: string}
---test: ApiTests:compareTables(PostgresqlStringTypePatternStringOpContains(), OpenFile('System:AppData/ApiTests/datasets/tests/data25.csv'))
+--test: ApiTests:expectTable(PostgresqlStringTypePatternStringOpContains(), OpenFile('System:AppData/ApiTests/datasets/tests/data25.csv'))
 SELECT * FROM mock_data WHERE @first_name(first_name);
 --end
 
 --name: PostgresqlStringTypePatternStringOpStartsWith
 --connection: PostgreSQLApiTests
 --input: string first_name = "starts with W" {pattern: string}
---test: ApiTests:compareTables(PostgresqlStringTypePatternStringOpStartsWith(), OpenFile('System:AppData/ApiTests/datasets/tests/data23.csv'))
+--test: ApiTests:expectTable(PostgresqlStringTypePatternStringOpStartsWith(), OpenFile('System:AppData/ApiTests/datasets/tests/data23.csv'))
 SELECT * FROM mock_data WHERE @first_name(first_name);
 --end
 
 --name: PostgresqlStringTypePatternStringOpEndsWith
 --connection: PostgreSQLApiTests
 --input: string first_name = "ends with y" {pattern: string}
---test: ApiTests:compareTables(PostgresqlStringTypePatternStringOpEndsWith(), OpenFile('System:AppData/ApiTests/datasets/tests/data6,23,25.csv'))
+--test: ApiTests:expectTable(PostgresqlStringTypePatternStringOpEndsWith(), OpenFile('System:AppData/ApiTests/datasets/tests/data6,23,25.csv'))
 SELECT * FROM mock_data WHERE @first_name(first_name);
 --end
 
 --name: PostgresqlStringTypePatternStringOpIn
 --connection: PostgreSQLApiTests
 --input: string country = "in (Poland, Brazil)" {pattern: string}
---test: ApiTests:compareTables(PostgresqlStringTypePatternStringOpIn(), OpenFile('System:AppData/ApiTests/datasets/tests/data2,5,20.csv'))
+--test: ApiTests:expectTable(PostgresqlStringTypePatternStringOpIn(), OpenFile('System:AppData/ApiTests/datasets/tests/data2,5,20.csv'))
 SELECT * FROM mock_data WHERE @country(country);
 --end
 
 --name: PostgresqlStringTypePatternStringOpRegex
 --connection: PostgreSQLApiTests
 --input: string email = "regex ^([A-Za-z0-9_]+@google.com.au)$" {pattern: string}
---test: ApiTests:compareTables(PostgresqlStringTypePatternStringOpRegex(), OpenFile('System:AppData/ApiTests/datasets/tests/data9.csv'))
+--test: ApiTests:expectTable(PostgresqlStringTypePatternStringOpRegex(), OpenFile('System:AppData/ApiTests/datasets/tests/data9.csv'))
 SELECT * FROM mock_data WHERE @email(email);
 --end
 
@@ -166,7 +166,7 @@ SELECT * FROM mock_data WHERE @email(email);
 --input: string some_number = ">20" {pattern: double}
 --input: string country = "in (Indonesia)" {pattern: string}
 --input: string date = "before 1/1/2022" {pattern: datetime}
---test: ApiTests:compareTables(PostgresqlPatternsAllParams(), ApiTests:getTable("data13.csv", path="ApiTests/datasets/tests"))
+--test: ApiTests:expectTable(PostgresqlPatternsAllParams(), OpenFile("System:AppData/ApiTests/datasets/tests/data13.csv"))
 SELECT * FROM mock_data
 WHERE @first_name(first_name)
   AND @id(id)
