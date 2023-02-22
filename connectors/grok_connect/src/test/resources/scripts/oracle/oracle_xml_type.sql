8ded4d9f1a87bbbe8a146ba9de1ea1c7e56e0976
PavloPolovyi <pasha32662@gmail.com>
2023-02-22 12:32:53 +0200

    GROK12210: Edited test queries

--- connectors/grok_connect/src/test/resources/scripts/oracle/oracle_xml_type.sql
+++ connectors/grok_connect/src/test/resources/scripts/oracle/oracle_xml_type.sql
@@ -3,5 +3,4 @@ CREATE TABLE XML_DATA (
 );
 
 INSERT INTO XML_DATA (data) VALUES ('<foo>Hello World!</foo>');
-INSERT INTO XML_DATA (data) VALUES ('abc<foo>bar</foo><bar>foo</bar>');
 INSERT INTO XML_DATA (data) VALUES ('<book><title>Manual</title><chapter>...</chapter></book>');
