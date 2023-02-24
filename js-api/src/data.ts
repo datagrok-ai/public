2fd9e506399954bc9696b598b2dea3f26ab0c6e9
Alex Paramonov <alex.aprm@gmail.com>
2023-02-24 17:47:27 +0000

    GROK-12254 JS: Expose CompareTables

--- js-api/src/data.ts
+++ js-api/src/data.ts
@@ -153,8 +153,18 @@ export class Data {
    * Opens the visual table comparison tool.
    * Sample: {@link https://public.datagrok.ai/js/samples/data-frame/compare-tables}
    * */
-  compareTables(t1: DataFrame, t2: DataFrame, keyColumns1: string[], keyColumns2: string[], valueColumns1: string[], valueColumns2: string[]): void {
-    api.grok_CompareTables(t1.dart, t2.dart, keyColumns1, keyColumns2, valueColumns1, valueColumns2);
+  compareTables(t1: DataFrame, t2: DataFrame, keyColumns1: string[], keyColumns2: string[], valueColumns1: string[], valueColumns2: string[]):
+    { changedColumns: number,
+      changedValues: number,
+      diffTable: DataFrame,
+      missingRows: number,
+      t1MissingRows: number,
+      t2MissingRows: number } {
+    console.log(t1.toCsv());
+    console.log(t2.toCsv());
+    let r = toJs(api.grok_CompareTables(t1.dart, t2.dart, keyColumns1, keyColumns2, valueColumns1, valueColumns2));
+    console.log(r.diffTable);
+    return r;
   };
 
   /**
