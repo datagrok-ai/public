873573ef741d9a5037acba6fa231699c3831a982
Dmitry Kovalyov <laykdimon278@gmail.com>
2023-02-28 12:25:55 +0200

    PowerGrid: syntax fixes

--- packages/PowerGrid/src/fit/fit-grid-cell-handler.ts
+++ packages/PowerGrid/src/fit/fit-grid-cell-handler.ts
@@ -2,16 +2,16 @@ import * as ui from 'datagrok-api/ui';
 import * as DG from 'datagrok-api/dg';
 import * as fit from './fit-data';
 import {FitResult, fitResultProperties} from '@datagrok-libraries/statistics/src/parameter-estimation/fit-curve';
-import {fitSeries, IFitChartData} from "./fit-data";
-import {divV} from "datagrok-api/ui";
-import {GridCell} from "datagrok-api/dg";
+import {fitSeries, IFitChartData} from './fit-data';
+import {divV} from 'datagrok-api/ui';
+import {GridCell} from 'datagrok-api/dg';
 
 
 function addStatisticsColumn(chartColumn: DG.GridColumn, p: DG.Property): void {
   const grid = chartColumn.grid;
   grid.dataFrame.columns
     .addNew(p.name, p.propertyType as DG.ColumnType)
-    .init(i => {
+    .init((i) => {
       const chartData = fit.getChartData(
         GridCell.fromColumnRow(grid, chartColumn.name, grid.tableRowToGrid(i)));
       const fitResult = fitSeries(chartData.series![0], true);
@@ -31,15 +31,15 @@ export class FitGridCellHandler extends DG.ObjectHandler {
 
     acc.addPane('Options', () => ui.input.form(chartData.chartOptions, fit.fitChartDataProperties));
     acc.addPane('Results', () => {
-      let host = divV([]);
+      const host = divV([]);
 
       for (let i = 0; i < chartData.series!.length; i++) {
-        let series = chartData.series![i];
-        let fitResult = fitSeries(series, true);
+        const series = chartData.series![i];
+        const fitResult = fitSeries(series, true);
         host.appendChild(ui.panel([
           ui.h1(series.name ?? 'series ' + i),
           ui.input.form(fitResult, fitResultProperties, {
-            onCreated: input => input.root.appendChild(
+            onCreated: (input) => input.root.appendChild(
               ui.iconFA('plus',
                 () => addStatisticsColumn(gridCell.gridColumn, input.property),
                 `Calculate ${input.property.name} for the whole column`))
