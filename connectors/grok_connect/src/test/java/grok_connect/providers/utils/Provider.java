cb38ff1e3c97af29cbca954fe331b0d8a5b7414c
PavloPolovyi <pasha32662@gmail.com>
2023-02-28 11:35:57 +0200

    GROK-12103: Added test queries, added test class and cases, fixed some issues

---
+++ connectors/grok_connect/src/test/java/grok_connect/providers/utils/Provider.java
@@ -0,0 +1,73 @@
+package grok_connect.providers.utils;
+
+import org.testcontainers.containers.BindMode;
+import org.testcontainers.containers.JdbcDatabaseContainer;
+import org.testcontainers.containers.OracleContainer;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ * Enum that contains all necessary data related to specific provider and it's container
+ */
+public enum Provider {
+    SNOWFLAKE("src/test/resources/properties/snowflake.properties"),
+
+    ORACLE("src/test/resources/properties/oracle.properties") {
+        @Override
+        protected JdbcDatabaseContainer<?> newJdbcContainer() {
+            if (this.container == null) {
+                container = new OracleContainer(properties.get("image").toString())
+                        .withDatabaseName(properties.get("database").toString())
+                        .withUsername(properties.get("user").toString())
+                        .withPassword(properties.get("password").toString())
+                        .withInitScript(properties.get("initScript").toString());
+                container.start();
+            }
+            return container;
+        }
+    },
+
+    POSTGRESQL("src/test/resources/properties/postgresql.properties") {
+        @Override
+        protected JdbcDatabaseContainer<?> newJdbcContainer() {
+            if (this.container == null) {
+                container = new PostgreSQLContainer<>(properties.get("image").toString())
+                        .withDatabaseName(properties.get("database").toString())
+                        .withUsername(properties.get("user").toString())
+                        .withPassword(properties.get("password").toString())
+                        .withClasspathResourceMapping(properties.get("volumePath").toString(),
+                                "/etc/", BindMode.READ_ONLY);
+                container.start();
+            }
+            return container;
+        }
+    };
+
+    protected final Properties properties;
+    protected JdbcDatabaseContainer<?> container;
+
+    Provider(String propertyPath) {
+        Properties properties = new Properties();
+        try {
+            properties.load(new FileReader(propertyPath));
+        } catch (IOException e) {
+            throw new RuntimeException("Something went wrong when reading from " + propertyPath, e);
+        }
+        this.properties = properties;
+    }
+
+    protected JdbcDatabaseContainer<?> newJdbcContainer() {
+        throw new UnsupportedOperationException("Override method newJdbcContainer()");
+    }
+
+    public JdbcDatabaseContainer<?> getContainer() {
+        return newJdbcContainer();
+    }
+
+    public Properties getProperties() {
+        return properties;
+    }
+}
--- connectors/grok_connect/src/test/resources/scripts/oracle/all_data.sql
+++ connectors/grok_connect/src/test/resources/scripts/oracle/all_data.sql
@@ -62,7 +62,7 @@ CREATE TABLE dates_patterns (
 INSERT INTO dates_patterns(dat) VALUES (SYSDATE); --TODAY
 INSERT INTO dates_patterns(dat) VALUES (SYSDATE - 1); --YESTERDAY
 INSERT INTO dates_patterns(dat) VALUES (TRUNC(SYSDATE + 6, 'DAY')); --THIS WEEK
-INSERT INTO dates_patterns(dat) VALUES (SYSDATE - 150); --THIS YEAR
+INSERT INTO dates_patterns(dat) VALUES (SYSDATE - 150);
 INSERT INTO dates_patterns(dat) VALUES (TO_DATE('2021-04-09', 'YYYY-MM-DD'));
 
 CREATE TABLE dates_type (
