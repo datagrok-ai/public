978a3c1dd392c20b447524d655777e155c02493e
PavloPolovyi <pasha32662@gmail.com>
2023-03-01 21:08:53 +0200

    GROK-12344: created sql queries for mssql provider

---
+++ connectors/grok_connect/src/test/resources/scripts/mssql/mssql_character_types.sql
@@ -0,0 +1,5 @@
+CREATE TABLE character_types (char_data char(50), varchar_data varchar(max), text_data text, nchar_data nchar, nvarchar_data nvarchar(max), ntext_data ntext);
+
+INSERT INTO character_types(char_data, varchar_data, text_data, nchar_data, nvarchar_data, ntext_data)
+VALUES ('Datagrok', 'Hello, World!', 'Lorem ipsum', 'Hello, World!', 'Hello, Datagrok!', 'Hello, Datagrok!');
+
