338c3269355ce05cc1fe8526237d35819aeec118
Oleksandra Serhiienko <59337516+Aleksashka11@users.noreply.github.com>
2023-03-01 13:25:06 +0200

    GROK-12343: Elemental analysis: add column with the molecule charge calculated

--- packages/Chem/src/constants.ts
+++ packages/Chem/src/constants.ts
@@ -13,7 +13,7 @@ export const elementsTable: Array<string> = [
   'Au', 'Hg', 'Tl', 'Pb', 'Bi', 'Po', 'At', 'Rn', 'Fr', 'Ra', 'Ac',
   'Th', 'Pa', 'U', 'Np', 'Pu', 'Am', 'Cm', 'Bk', 'Cf', 'Es', 'Fm', 'Md',
   'No', 'Lr', 'Rf', 'Db', 'Sg', 'Bh', 'Hs', 'Mt', 'Ds', 'Rg', 'Cn',
-  'Nh', 'Fl', 'Mc', 'Lv', 'Ts', 'Og'];
+  'Nh', 'Fl', 'Mc', 'Lv', 'Ts', 'Og', 'Molecule Charge'];
 
 export enum MOL_FORMAT {
   SMILES = 'smiles',
