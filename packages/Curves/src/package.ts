dffb9f28d1875f44fba42779ab26e80070232729
Dmytro <61393026+LaykDimon@users.noreply.github.com>
2023-03-01 20:32:12 +0200

    dkovalyov/curves (#1623)
    * created Curves package
    
    * Curves: fixed name in package.json
    
    * Curves: added temporary test
    
    * Curves: returned gridColumn.tags instead of temp in fit-data.ts

---
+++ packages/Curves/src/package.ts
@@ -0,0 +1,50 @@
+/* Do not change these import lines to match external modules in webpack configuration */
+import * as grok from 'datagrok-api/grok';
+import * as ui from 'datagrok-api/ui';
+import * as DG from 'datagrok-api/dg';
+
+import {FitCellRenderer} from './fit/fit-curve';
+import {FitGridCellHandler} from './fit/fit-grid-cell-handler';
+import {FitChartCellRenderer} from './fit/fit-renderer';
+import {FitViewer} from './fit/fit-viewer';
+import {curveFitDemo} from './fit/fit-demo';
+
+
+export const _package = new DG.Package();
+
+
+//name: Fit
+//tags: cellRenderer
+//meta.cellType: fit
+//meta.virtual: true
+//output: grid_cell_renderer result
+export function fitCellRenderer() {
+  return new FitChartCellRenderer();
+}
+
+//tags: cellRenderer
+//meta.cellType: fit-old
+//meta.virtual: true
+//output: grid_cell_renderer result
+export function fitOldCellRenderer() {
+  return new FitCellRenderer();
+}
+
+//name: FitViewer
+//description: Creates a fit viewer
+//tags: viewer
+//output: viewer result
+export function _FitViewer() {
+  return new FitViewer();
+}
+
+//tags: app
+//name: Curve Fit Demo
+export function curveFitDemoApp() {
+  curveFitDemo();
+}
+
+//tags: autostart
+export function _autoCurves(): void {
+  DG.ObjectHandler.register(new FitGridCellHandler());
+}
