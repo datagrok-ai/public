dcfe9f7ae277ad2c053b03809376de2e76290218
Anna Muzychyna <annamuz69@gmail.com>
2023-03-01 08:55:34 +0200

    GROK-11605 ApiTests: scripts tests & help func

--- packages/ApiTests/scripts/python-test.py
+++ packages/ApiTests/scripts/python-test.py
@@ -1,22 +1,27 @@
 #name: Python Params Test
 #language: python
 #tags: test
-#input: int i
-#input: double d
-#input: bool b
-#input: string s
-#input: datetime dt
-#input: dataframe df
-#input: column col
+#input: int i = 10
+#input: double d = -20.1
+#input: bool b = false
+#input: string s = 'abc'
+#input_: datetime dt = '1992-09-20 00:00:00'
+#input_: dataframe df {optional: true}
+#input_: column col {optional: true}
 #output: int ri
 #output: double rd
 #output: bool rb
 #output: string rs
-#output: datetime rdt
-#output: dataframe rdf
+#output_: datetime rdt
+#output_: dataframe rdf
+#test: ApiTests:getOutput('PythonParamsTest', 'ri').val == 5
+#test: ApiTests:getOutput('PythonParamsTest', 'rd').val == 39.9
+#test: ApiTests:getOutput('PythonParamsTest', 'rb').val == true
+#test: ApiTests:getOutput('PythonParamsTest', 'rs').val == 'abcabc'
+
 ri = i / 2
 rd = d + 60
 rb = not b
 rs = s + s
-rdf = df[col]
-rdt = dt - timedelta(days=10)
+#rdf = df[col]
+#rdt = dt - timedelta(days=10)
--- packages/ApiTests/src/package.ts
+++ packages/ApiTests/src/package.ts
@@ -1,6 +1,6 @@
 /* Do not change these import lines. Datagrok will import API library in exactly the same manner */
 import * as grok from 'datagrok-api/grok';
-import * as ui from 'datagrok-api/ui';
+// import * as ui from 'datagrok-api/ui';
 import * as DG from 'datagrok-api/dg';
 
 import {TestViewerForProperties} from './viewers/test-viewer-for-properties';
@@ -24,4 +24,19 @@ export function testViewerForProperties() {
 export function expectTable(actual: DG.DataFrame, expected: DG.DataFrame): boolean {
   _expectTable(actual, expected);
   return true;
+}
+
+//name: getOutput
+//input: string func
+//input: string param
+//output: map res
+export async function getOutput(func: string, param: string): Promise<object> {
+  const f = await grok.functions.eval(func);
+  const params: {[key: string]: any} = {};
+  f.inputs.forEach((i: any) => params[i.name] = typeof i.defaultValue === 'string' ?
+    i.defaultValue.slice(1, -1) : i.defaultValue);
+  const call = await f.prepare(params);
+  await call.call();
+  const res = {val: call.getParamValue(param)};
+  return res;
 }
