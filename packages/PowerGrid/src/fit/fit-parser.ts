90b08dd65741190cd40827eb66f411586c74503a
Dmitry Kovalyov <laykdimon278@gmail.com>
2023-03-01 11:18:25 +0200

    PowerGrid: added getSeriesArray() to xml conversion and wrote documentation

--- packages/PowerGrid/src/fit/fit-parser.ts
+++ packages/PowerGrid/src/fit/fit-parser.ts
@@ -4,6 +4,10 @@ const AXES = {x: 'xAxis', y: 'yAxis'};
 const EXTREMUMS = {min: 'min', max: 'max'};
 
 
+/** Constructs {@link IFitChartOptions} from the grid xml tag.
+ * @param {Element} grid XML grid tag
+ * @return {IFitChartOptions} IFitChartOptions for the fitted curve
+*/
 function getChartOptions(grid: Element): IFitChartOptions {
   const fitChartOptions: IFitChartOptions = {
     minX: +grid.getElementsByTagName(AXES.x)[0].getAttribute(EXTREMUMS.min)!,
@@ -15,6 +19,10 @@ function getChartOptions(grid: Element): IFitChartOptions {
   return fitChartOptions;
 }
 
+/** Constructs {@link IFitSeriesOptions} from the series xml tag.
+ * @param {Element} series XML series tag
+ * @return {IFitSeriesOptions} IFitSeriesOptions for the fitted curve
+*/
 function getSeriesOptions(series: Element): IFitSeriesOptions {
   const params = (series.getElementsByTagName('params')[0].childNodes[0].nodeValue)?.split(',')!.map(Number)!;
   let funcType = series.getElementsByTagName('function')[0].getAttribute('type')!;
@@ -36,6 +44,10 @@ function getSeriesOptions(series: Element): IFitSeriesOptions {
   return fitSeriesOptions;
 }
 
+/** Constructs {@link IFitPoint} array from the grid series tag.
+ * @param {Element} series XML series tag
+ * @return {IFitPoint[]} IFitPoint array for current series
+*/
 function getPoints(series: Element): IFitPoint[] {
   const xCoords = (series.getElementsByTagName('x')[0].childNodes[0].nodeValue)?.split(',')!;
   const yCoords = (series.getElementsByTagName('y')[0].childNodes[0].nodeValue)?.split(',')!;
@@ -53,6 +65,10 @@ function getPoints(series: Element): IFitPoint[] {
   return points;
 }
 
+/** Constructs {@link IFitSeries} from the grid series tag.
+ * @param {Element} series XML series tag
+ * @return {IFitSeries} IFitSeries for the current series
+*/
 function getSeries(series: Element): IFitSeries {
   const points: IFitPoint[] = getPoints(series);
   const currentFitSeriesOptions: IFitSeriesOptions = getSeriesOptions(series);
@@ -65,23 +81,44 @@ function getSeries(series: Element): IFitSeries {
   return returnSeries;
 }
 
+/** Constructs {@link IFitSeries} array from the seriesCollection xml tag.
+ * @param {Element} seriesCollection XML seriesCollection tag
+ * @return {IFitSeries[]} IFitSeries array for the fitted curve
+*/
+function getSeriesArray(seriesCollection: Element): IFitSeries[] {
+  const fitSeries: IFitSeries[] = [];
 
+  const seriesCollectionLength = seriesCollection.getElementsByTagName('series').length;
+  for (let i = 0; i < seriesCollectionLength; i++) {
+    const currentSeries = seriesCollection.getElementsByTagName('series')[i];
+    fitSeries[i] = getSeries(currentSeries);
+  }
+
+  return fitSeries;
+}
+
+
+/** Converts XML fitted curve chart document into {@link IFitChartData} interface.
+ * @param {string} xmlText XML document
+ * @return {IFitChartData} IFitChartData intefrace for the fitted curve
+*/
 export function convertXMLToIFitChartData(xmlText: string): IFitChartData {
   const parser = new DOMParser();
   const xmlDoc = parser.parseFromString(xmlText, 'text/xml');
 
+  // get IFitChartOptions from grid
   const gridElement = xmlDoc.getElementsByTagName('grid')[0];
   const fitChartOptions: IFitChartOptions = getChartOptions(gridElement);
 
-  // get first series to make it default options
-  const firstSeries = xmlDoc.getElementsByTagName('series')[0];
+  // get the whole series collection
+  const seriesCollection = xmlDoc.getElementsByTagName('seriesCollection')[0];
+
+  // get first series to make it the default options (IFitSeriesOptions)
+  const firstSeries = seriesCollection.getElementsByTagName('series')[0];
   const fitSeriesOptions: IFitSeriesOptions = getSeriesOptions(firstSeries);
 
-  const fitSeries: IFitSeries[] = [];
-  for (let i = 0; i < xmlDoc.getElementsByTagName('series').length; i++) {
-    const currentSeries = xmlDoc.getElementsByTagName('series')[i];
-    fitSeries[i] = getSeries(currentSeries);
-  }
+  // get IFitSeries[]
+  const fitSeries = getSeriesArray(seriesCollection);
 
   const fitChartData: IFitChartData = {
     chartOptions: fitChartOptions,
