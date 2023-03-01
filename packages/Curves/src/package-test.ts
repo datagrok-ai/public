aaada39108beb4a10dc885f49e2d43f9a55f9610
Dmitry Kovalyov <laykdimon278@gmail.com>
2023-03-01 19:52:22 +0200

    Curves: added temporary test

--- packages/Curves/src/package-test.ts
+++ packages/Curves/src/package-test.ts
@@ -1,6 +1,8 @@
 import * as DG from "datagrok-api/dg";
 import {runTests, tests} from '@datagrok-libraries/utils/src/test';
 
+import './tests/fit-viewer-test';
+
 export let _package = new DG.Package();
 export {tests};
 
