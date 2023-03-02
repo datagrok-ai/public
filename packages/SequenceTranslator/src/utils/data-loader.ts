38d64dbf77e2774a4ea39dc7baa5eed53fcac8c4
Alexey <achopovsky@datagrok.ai>
2023-03-02 19:44:57 +0200

    ST: GROK-12351: minor corrections, WIP

--- packages/SequenceTranslator/src/utils/data-loader.ts
+++ packages/SequenceTranslator/src/utils/data-loader.ts
@@ -5,6 +5,14 @@ import * as DG from 'datagrok-api/dg';
 
 // import {_package} from '../package';
 
+const enum QUERY_NAME {
+  START = 'Sequence',
+  USERS = 'Users',
+  ICDS = 'ICDs',
+  SOURCES = 'Sources',
+  SALTS = 'Salts'
+
+}
 export abstract class DataLoaderBase {
   abstract getUsers(): Promise<DG.DataFrame>;
   // abstract get users(): DG.DataFrame;
@@ -14,21 +22,14 @@ export abstract class DataLoaderBase {
 }
 
 /** Load data for users, salts, ICDs, IDPs and sources. In order for that to
- * wrok, we suppose that the queries have standard names of the form "Sequence
- * Translator " */
+ * work, we suppose that the queries have standard names of the form 'Sequence
+ * Translator <name>' */
 export class DataLoaderDB extends DataLoaderBase {
-  private async getDatafame(query: DG.DataQuery) {
-    const c = await query.prepare().call();
-    console.log('inside getDataframe: c:', c.toString());
-    const df: DG.DataFrame = c.getOutputParamValue();
-    console.log('inside getDataframe: df:', df.toString());
-    return df;
-  }
-
   public async init(): Promise<void> {
-    const dataQueryList = (await grok.dapi.queries.list()).filter((dq) => dq.name.startsWith('Sequence'));
+    const dataQueryList = (await grok.dapi.queries.list())
+      .filter((dq) => dq.name.startsWith(QUERY_NAME.START));
     const usersQuery = dataQueryList.find(
-      (dq) => dq.name.endsWith('Users')
+      (dq) => dq.name.endsWith(QUERY_NAME.USERS)
     )!;
     this.usersDf = await this.getDatafame(usersQuery);
     if (this.usersDf === undefined)
@@ -36,6 +37,13 @@ export class DataLoaderDB extends DataLoaderBase {
   };
 
   private usersDf?: DG.DataFrame;
+
+  private async getDatafame(query: DG.DataQuery) {
+    const df: DG.DataFrame = (await query.prepare().call())
+      .getOutputParamValue();
+    console.log('inside getDataframe: df:', df.toString());
+    return df;
+  }
 
   public async getUsers(): Promise<DG.DataFrame> {
     if (this.usersDf === undefined)
