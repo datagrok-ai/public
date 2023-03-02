5887d14f0b1b62b714a40cac8ccbaf7ff410b616
PavloPolovyi <pasha32662@gmail.com>
2023-03-02 19:27:23 +0200

    GrokConnect:GROK-12352: created test class, test cases for pattern support, updated sql queries

--- connectors/grok_connect/src/test/resources/scripts/mssql/mssql_xml_type.sql
+++ connectors/grok_connect/src/test/resources/scripts/mssql/mssql_xml_type.sql
@@ -1,3 +1,3 @@
 CREATE TABLE xml_type (xml_data xml);
-INSERT INTO xml_type(xml_data) VALUES (XML('<foo>Hello World!</foo>'));
-INSERT INTO xml_type(xml_data) VALUES (XML('<?xml version="1.0"?><book><title>Manual</title><chapter>...</chapter></book>'));
+INSERT INTO xml_type(xml_data) VALUES ('<foo>Hello World!</foo>');
+INSERT INTO xml_type(xml_data) VALUES ('<?xml version="1.0"?><book><title>Manual</title><chapter>...</chapter></book>');
