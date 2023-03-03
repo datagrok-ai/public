ccb04488667cbe7e5cc4d9ebec52111e25a41483
dpetrov <dpetrov@datagrok.ai>
2023-03-02 20:18:43 -0800

    gridext: #1610: Blank pinned columns

--- libraries/gridext/src/renderer/RendUtils.d.ts
+++
@@ -1,3 +0,0 @@
-export declare function renderXYCenteredText(str: string, g: CanvasRenderingContext2D, nX: number, nY: number, nW: number, nH: number, strFont: string, strCrFore: string): void;
-export declare function renderXCenteredText(str: string, g: CanvasRenderingContext2D, nX: number, nY: number, nW: number, nH: number, strFont: string, strCrFore: string): void;
-//# sourceMappingURL=RendUtils.d.ts.map
