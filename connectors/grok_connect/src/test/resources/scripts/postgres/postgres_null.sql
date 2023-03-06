b713bb6b22e86257cb071900e554e4d17946a7cb
PavloPolovyi <pasha32662@gmail.com>
2023-03-06 12:22:04 +0200

    Grok Connect: GROK-12090: fixed issues, added null and operator @ test cases

---
+++ connectors/grok_connect/src/test/resources/scripts/postgres/postgres_null.sql
@@ -0,0 +1,4 @@
+CREATE TABLE null_safety (array_type integer[], varchar_type varchar(50), cidr_type cidr, bool_type bool,
+bit_type bit(3), bit_var_type bit varying(5), bytea_type bytea, date_type date, time_type time, stamp_type timestamp,
+interval_type interval, json_type jsonb, numeric_type numeric, double_type double precision, real_type real,
+bigint_type bigint, uuid_type uuid, xml_type xml);
