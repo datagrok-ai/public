2e23f0e9e7cf9d4457f8e6c5e11283e534100fe3
Alexey Chopovsky <achopovsky@datagrok.ai>
2023-03-06 13:28:03 +0200

    Chem: GROK-12331: add molfile parser
    * Chem: GROK-12231: port molfile parsing utilities to chem-meta
    
    * Chem: GROK-12231: port molfile parsing utilities to chem-meta, WIP
    
    * Chem: GROK-12231: port molfile parsing utilities to chem-meta, WIP
    
    * Chem: GROK-12231: port molfile parsing utilities to chem-meta, WIP
    
    * Chem: GROK-12331: emend base singleton, WIP
    
    * ST: GROK-12331: WIP
    
    * Chem: GROK-12331: WIP
    
    * Chem: GROK-12331: WIP
    
    * Chem: GROK-12331: fix wrong abstract singleton implementation
    
    * Chem: GROK-12331: correct coords and atom types for V2K
    
    * Chem: GROK-12331: fix init, correct atom types for V3K
    
    * Chem: GROK-12331: molfile handler fix coordinates for V3K

---
+++ libraries/chem-meta/src/parsing-utils/molfile-parsing-const.ts
@@ -0,0 +1,22 @@
+export enum MOLFILE_VERSION {
+  V2000 = 'V2000',
+  V3000 = 'V3000',
+}
+
+/** Constants relevant for parsing of Molfile V3K */
+export const enum V3K {
+  HEADER = '999 V3000\n',
+  BEGIN_COUNTS_LINE = 'M  V30 COUNTS ',
+  /** Index shift from the beginning of the 'COUNTS' line to the number of atoms  */
+  COUNTS_SHIFT = 14,
+  BEGIN_ATOM_BLOCK = 'M  V30 BEGIN ATOM',
+  BEGIN_BOND_BLOCK = 'M  V30 BEGIN BOND',
+  END = 'M  END',
+}
+
+/** Constants relevant for parsing of Molfile V2K */
+export enum V2K {
+  HEADER = '999 V2000\n',
+  END = 'M  END',
+}
+
