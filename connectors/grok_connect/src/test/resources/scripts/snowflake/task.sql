eb1f440395a67305c98b7bee4d0c6dfe83ea3b81
PavloPolovyi <pasha32662@gmail.com>
2023-03-06 12:20:32 +0200

    Grok Connect: GROK-12103: fixed issues, added null type test case

--- connectors/grok_connect/src/test/resources/scripts/snowflake/task.sql
+++ connectors/grok_connect/src/test/resources/scripts/snowflake/task.sql
@@ -1,10 +1,15 @@
-create table DATES_PATTERNS (
-    date DATE
-);
-
+create or replace task TEST.PUBLIC.UPDATE_DATES_PATTERNS
+	warehouse=COMPUTE_WH
+	schedule='USING CRON 0 2 * * * Europe/Kiev'
+	as execute immediate
+$$
+BEGIN
+DELETE FROM dates_patterns;
 insert into DATES_PATTERNS (date) values (CURRENT_DATE);
 insert into DATES_PATTERNS (date) values (CURRENT_DATE - 1); -- yesterday
 insert into DATES_PATTERNS (date) select (LAST_DAY(CURRENT_DATE, 'WEEK'))
-where not exists (select * from DATES_PATTERNS where date = LAST_DAY(CURRENT_DATE, 'WEEK')); -- last day of this week
+    where not exists (select * from DATES_PATTERNS where date = LAST_DAY(CURRENT_DATE, 'WEEK')); -- last day of this week
 insert into DATES_PATTERNS (date) values (CURRENT_DATE - 150);
 insert into DATES_PATTERNS (date) values ('2021-04-09');
+END;
+$$;
