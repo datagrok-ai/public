04e091f46dcb2424907701cd79d2dfe5db2558b0
PavloPolovyi <pasha32662@gmail.com>
2023-03-06 12:13:34 +0200

    Grok Connect: GROK-12092: fixed queries, added test case for null values

--- connectors/grok_connect/src/test/java/grok_connect/providers/MsSqlDataProviderTest.java
+++ connectors/grok_connect/src/test/java/grok_connect/providers/MsSqlDataProviderTest.java
@@ -176,6 +176,16 @@ class MsSqlDataProviderTest extends ContainerizedProviderBaseTest {
         Assertions.assertTrue(dataFrameComparator.isDataFramesEqual(expected, actual));
     }
 
+    @DisplayName("SQL SERVER null safety")
+    @ParameterizedTest(name = "{index} : {0}")
+    @MethodSource("grok_connect.providers.arguments_provider.CommonObjectsMother#checkNullSupport_ok")
+    @Sql(path = "scripts/mssql/mssql_null.sql",
+            restorePath = "scripts/mssql/drop.sql")
+    public void checkNullSupport_ok(FuncCall funcCall) {
+        funcCall.func.connection = connection;
+        Assertions.assertDoesNotThrow(() -> provider.execute(funcCall));
+    }
+
     private void prepareDataFrame(DataFrame dataFrame) {
         // in order to save time reuse some common's
         dataFrame.columns.removeIf(column -> column.name.equals("bool")); // mssql doesn't have boolean type
