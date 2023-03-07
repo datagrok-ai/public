e8728d80d6699b78231ffe06104502c594c936ad
Anna Muzychyna <annamuz69@gmail.com>
2023-03-07 12:47:39 +0200

    ApiTests: GROK-12353: fix & add script tests

--- packages/ApiTests/scripts/python-test.py
+++ packages/ApiTests/scripts/python-test.py
@@ -1,27 +1,23 @@
 #name: Python Params Test
 #language: python
 #tags: test
-#input: int i = 10
-#input: double d = -20.1
-#input: bool b = false
-#input: string s = 'abc'
-#input_: datetime dt = '1992-09-20 00:00:00'
-#input_: dataframe df {optional: true}
-#input_: column col {optional: true}
+#input: int i
+#input: double d 
+#input: bool b
+#input: string s
+#input: datetime dt
+#input: dataframe df
+#input: column col
 #output: int ri
 #output: double rd
 #output: bool rb
 #output: string rs
-#output_: datetime rdt
-#output_: dataframe rdf
-#test: ApiTests:getOutput('PythonParamsTest', 'ri').val == 5
-#test: ApiTests:getOutput('PythonParamsTest', 'rd').val == 39.9
-#test: ApiTests:getOutput('PythonParamsTest', 'rb').val == true
-#test: ApiTests:getOutput('PythonParamsTest', 'rs').val == 'abcabc'
-
+#output: datetime rdt
+#output: dataframe rdf
+#output_: column rcol
 ri = i / 2
 rd = d + 60
 rb = not b
 rs = s + s
-#rdf = df[col]
-#rdt = dt - timedelta(days=10)
+rdt = dt + timedelta(days=10)
+rdf = df[col]
--- packages/ApiTests/scripts/r-test.R
+++ packages/ApiTests/scripts/r-test.R
@@ -1,27 +1,23 @@
 #name: R Params Test
 #language: r
 #tags: test
-#input: int i = 10
-#input: double d = -20.1
-#input: bool b = false
-#input: string s = 'abc'
-#input_: datetime dt
-#input_: dataframe df
-#input_: column col
+#input: int i
+#input: double d 
+#input: bool b
+#input: string s
+#input: datetime dt
+#input: dataframe df
+#input: column col
 #output: int ri
 #output: double rd
 #output: bool rb
 #output: string rs
-#output_: datetime rdt
-#output_: dataframe rdf
-#test: ApiTests:getOutput('RParamsTest', 'ri').val == 5
-#test: ApiTests:getOutput('RParamsTest', 'rd').val == 39.9
-#test: ApiTests:getOutput('RParamsTest', 'rb').val == true
-#test: ApiTests:getOutput('RParamsTest', 'rs').val == 'abcabc'
-
+#output: datetime rdt
+#output: dataframe rdf
+#output_: column rcol
 ri = i / 2
 rd = d + 60
 rb = !b
 rs = paste(s, s, sep = '')
-#rdt = as.POSIXlt(dt) - 10*86400
-#rdf = df[c(col)]
+rdt = as.Date(dt) + 10
+rdf = df[c(col)]
