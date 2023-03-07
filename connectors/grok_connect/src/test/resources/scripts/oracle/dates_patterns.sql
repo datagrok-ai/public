8c72e810940208c6f6c39901da9f2be87f2ce73c
PavloPolovyi <pasha32662@gmail.com>
2023-03-07 13:50:11 +0200

    Grok Connect: GROK-12210: small changes

--- connectors/grok_connect/src/test/resources/scripts/oracle/dates_patterns.sql
+++
@@ -1,7 +0,0 @@
-DELETE FROM dates_patterns;
-
-INSERT INTO dates_patterns(dat) VALUES (SYSDATE); --TODAY
-INSERT INTO dates_patterns(dat) VALUES (SYSDATE - 1); --YESTERDAY
-INSERT INTO dates_patterns(dat) VALUES (TRUNC(SYSDATE + 6, 'DAY')); --THIS WEEK
-INSERT INTO dates_patterns(dat) VALUES (SYSDATE - 150);
-INSERT INTO dates_patterns(dat) VALUES (TO_DATE('2021-04-09', 'YYYY-MM-DD'));
