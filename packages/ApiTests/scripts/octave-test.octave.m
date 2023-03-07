cc0e3f61901259d733106c867ff331eb891173c1
Anna Muzychyna <annamuz69@gmail.com>
2023-03-07 12:47:39 +0200

    ApiTests: GROK-12353: fix & add script tests

--- packages/ApiTests/scripts/octave-test.octave.m
+++ packages/ApiTests/scripts/octave-test.octave.m
@@ -1,25 +1,23 @@
 #name: Octave Params Test
 #language: octave
 #tags: test
-#input: int i = 10
-#input: double d = -20.1
-#input: bool b = false
-#input: string s = 'abc'
-#input_: dataframe df [Data table]
-#input_: column col
-#input_: column_list col_list
+#input: int i
+#input: double d 
+#input: bool b
+#input: string s
+#input: datetime dt
+#input: dataframe df
+#input: column col
 #output: int ri
 #output: double rd
 #output: bool rb
 #output: string rs
-#output_: dataframe rdf
-#test: ApiTests:getOutput('OctaveParamsTest', 'ri').val == 5
-#test: ApiTests:getOutput('OctaveParamsTest', 'rd').val == 39.9
-#test: ApiTests:getOutput('OctaveParamsTest', 'rb').val == true
-#test: ApiTests:getOutput('OctaveParamsTest', 'rs').val == 'abcabc'
-
+#output: datetime rdt
+#output: dataframe rdf
+#output_: column rcol
 ri = i / 2
 rd = d + 60
 rb = ~b
 rs = strcat(s, s)
-#rdf = {"first_col", "second_col"; 1, 'two'}
+rdt = datestr(addtodate(dt, 10, 'days'), 'yyyy-mm-dd')
+rdf = df[col]
