e913df169ec94713ad68264c0c1de0817041e7e8
MariaDolotova <44060410+MariaDolotova@users.noreply.github.com>
2023-03-07 11:26:06 +0300

    #1608 chem render molecule in sketcher tooltip (#1630)
    * #1608: render molecule in sketcher tooltip in progress
    
    * Chem: #1608: render molecule in filter tooltip
    
    * JS API: Add .js to webpacke resolve extensions
    
    * Chem: #1608: rendering molecule in sketcher tooltip
    
    ---------
    
    Co-authored-by: Maria Dolotova <maria.dolotova@softwarecountry.com>
    Co-authored-by: Sofia Podolsky <spodolskaya@datagrok.ai>

--- libraries/chem-meta/src/parsing-utils/chemical-table-parser.ts
+++ libraries/chem-meta/src/parsing-utils/chemical-table-parser.ts
@@ -1,8 +1,3 @@
-/* Do not change these import lines to match external modules in webpack configuration */
-import * as grok from 'datagrok-api/grok';
-import * as ui from 'datagrok-api/ui';
-import * as DG from 'datagrok-api/dg';
-
 export interface ChemicalTableParser {
   init(file: string): void;
   atomCount: number;
