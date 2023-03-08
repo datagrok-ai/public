cf5d5fa1f1894afe7cb9bf224d1a3bc1e0de7f94
Anna Muzychyna <annamuz69@gmail.com>
2023-03-08 10:50:37 +0200

    Usage Analysis: GROK-12111: Packages tab

--- packages/UsageAnalysis/src/view-handler.ts
+++ packages/UsageAnalysis/src/view-handler.ts
@@ -3,12 +3,12 @@ import * as ui from 'datagrok-api/ui';
 
 import {UaView} from './views/ua-view';
 import {UaToolbox} from './ua-toolbox';
-import {OverviewView} from './views/overview-view';
-import {EventsView} from './views/events-view';
-import {ErrorsView} from './views/errors-view';
-import {FunctionsView} from './views/function-errors-view';
-import {UsersView} from './views/users-view';
-import {DataView} from './views/data-view';
+// import {OverviewView} from './views/overview-view';
+// import {EventsView} from './views/events-view';
+// import {ErrorsView} from './views/errors-view';
+// import {FunctionsView} from './views/function-errors-view';
+// import {UsersView} from './views/users-view';
+// import {DataView} from './views/data-view';
 import {PackagesView} from './views/packages-view';
 
 const APP_PREFIX: string = `/apps/UsageAnalysis/`;
@@ -36,8 +36,8 @@ export class ViewHandler {
     tabs.root.style.width = 'inherit';
     tabs.root.style.height = 'inherit';
 
-    const viewClasses: (typeof UaView)[] = [OverviewView, EventsView,
-      ErrorsView, FunctionsView, UsersView, DataView, PackagesView];
+    // [OverviewView, EventsView, ErrorsView, FunctionsView, UsersView, DataView];
+    const viewClasses: (typeof UaView)[] = [PackagesView];
 
     for (let i = 0; i < viewClasses.length; ++i) {
       tabs.addPane(viewClasses[i].viewName, () => {
