3b916b8e5663aaa7bc9ca8bc2cb1ea0f5841236c
Anna Muzychyna <annamuz69@gmail.com>
2023-03-09 10:42:50 +0200

    GROK-12111: UsageAnalysis: Filters

--- packages/UsageAnalysis/queries/packages.sql
+++ packages/UsageAnalysis/queries/packages.sql
@@ -1,11 +1,12 @@
 --name: PackagesUsage
---input_: string time = "today" {pattern: datetime}
+--input: string date {pattern: datetime}
+--input: list users
 --meta.cache: true
 --connection: System:Datagrok
 select t.package, t.user, sum(t.count)::bigint as count, t.time
 from (
 (select pp.name as package, u.friendly_name as user, count(*),
-to_timestamp(floor((extract('epoch' from e.event_time) / 600 )) * 600) 
+to_timestamp(floor((extract('epoch' from e.event_time) / 600 )) * 600)
 AT TIME ZONE 'UTC' as time
 from events e
 inner join event_types et on e.event_type_id = et.id
@@ -13,8 +14,9 @@ inner join entities en on et.id = en.id
 inner join published_packages pp on en.package_id = pp.id
 inner join users_sessions s on e.session_id = s.id
 inner join users u on u.id = s.user_id
---where time(e.event_time)
-where e.event_time::TIMESTAMP between 'now'::TIMESTAMP - interval '7 day' and 'now'::TIMESTAMP
+where @date(e.event_time)
+and (u.login = any(@users) or @users = ARRAY['all'])
+-- where e.event_time::TIMESTAMP between 'now'::TIMESTAMP - interval '7 day' and 'now'::TIMESTAMP
 group by pp.name, u.friendly_name, time
 limit 100000)
 union all
@@ -26,8 +28,9 @@ inner join event_parameter_values epv inner join event_parameters ep on epv.para
 inner join published_packages pp on pp.id::text = epv.value
 inner join users_sessions s on e.session_id = s.id
 inner join users u on u.id = s.user_id
---where time(e.event_time)
-where e.event_time::TIMESTAMP between 'now'::TIMESTAMP - interval '7 day' and 'now'::TIMESTAMP
+where @date(e.event_time)
+and (u.login = any(@users) or @users = ARRAY['all'])
+-- where e.event_time::TIMESTAMP between 'now'::TIMESTAMP - interval '7 day' and 'now'::TIMESTAMP
 group by pp.name, u.friendly_name, time
 limit 100000)
 ) t
