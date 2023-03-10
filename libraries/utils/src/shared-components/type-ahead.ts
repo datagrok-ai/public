d028e7ca5eb9f45f1c051766db6159f2c411f20d
Dmytro Kovalyov <laykdimon278@gmail.com>
2023-03-10 16:31:39 +0200

    GROK-12432: Library utils: Implemented TypeAhead control

--- libraries/utils/src/shared-components/type-ahead.ts
+++ libraries/utils/src/shared-components/type-ahead.ts
@@ -1,28 +1,30 @@
+import * as DG from 'datagrok-api/dg';
 import * as ui from 'datagrok-api/ui';
 
 import typeahead from 'typeahead-standalone'; // imports library (js)
 import '../../css/typeahead-input.css';
 
 export namespace u2 {
-  export function typeAhead(label: string = '', source: string[] = [], minLength: number = 1, limit: number = 5):
-    HTMLDivElement {
-    const inputElement = ui.stringInput(label, '');
-    (inputElement.input as HTMLInputElement).placeholder = 'Search';
+  export class TypeAhead extends DG.InputBase {
+    constructor(label: string = '', source: string[] = [],
+      options?: {minLength?: number, limit?: number}) {
+      const inputElement = ui.stringInput(label, '');
+      super(inputElement.dart);
 
-    typeahead({
-      input: <HTMLInputElement>inputElement.input,
-      source: {
-        local: source,
-      },
-      minLength: minLength,
-      limit: limit,
-      hint: false,
-    });
+      (this.input as HTMLInputElement).placeholder = 'Search';
 
-    // set classes
-    inputElement.root.getElementsByClassName('tt-list')[0].className = 'ui-input-list';
-    inputElement.root.getElementsByClassName('tt-input')[0].className = 'ui-input-editor';
+      typeahead({
+        input: <HTMLInputElement> this.input,
+        source: {
+          local: source,
+        },
+        minLength: options?.minLength ?? 1,
+        limit: options?.limit ?? 5,
+        hint: false,
+      });
 
-    return inputElement.root as HTMLDivElement;
+      this.root.getElementsByClassName('tt-list')[0].className = 'ui-input-list';
+      this.root.getElementsByClassName('tt-input')[0].className = 'ui-input-editor';
+    }
   }
 }
