44a5836461913161628101f52db3d7330c1700c3
PavloPolovyi <pasha32662@gmail.com>
2023-03-13 19:02:30 +0200

    GROK-12474: Grok Connect: Created test queries for MySQL provider

---
+++ connectors/grok_connect/src/test/resources/scripts/mysql/msql_bit_type.sql
@@ -0,0 +1,5 @@
+CREATE TABLE BIT_TYPE(bit_type1 bit(64), bit_type2 bit(1));
+
+INSERT INTO BIT_TYPE(bit_type1, bit_type2) VALUES (b'10000000', b'1');
+
+INSERT INTO BIT_TYPE(bit_type1, bit_type2) VALUES (b'1', b'0');
