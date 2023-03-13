eccb3f6ef55aa1d54b865f1e43be89034754080a
Dmytro Kovalyov <laykdimon278@gmail.com>
2023-03-13 14:31:35 +0200

    GROK-12433: Library utils: Implemented dropdown for 1 element

--- libraries/utils/src/shared-components/drop-down.ts
+++ libraries/utils/src/shared-components/drop-down.ts
@@ -2,23 +2,32 @@ import * as ui from 'datagrok-api/ui';
 import '../../css/drop-down.css';
 
 export class DropDown {
-  private _rootElement: HTMLElement;
-  private _elements: HTMLElement[];
-  private _dropDownElements: HTMLDivElement;
+  // private _elements: HTMLElement[];
+  private _element: HTMLElement;
+  private _dropDownElement: HTMLDivElement;
+
   private _isMouseOverElement: boolean;
   private _expanded: boolean;
 
   root: HTMLDivElement;
+  private _rootElement: HTMLElement;
+  private _icon: HTMLElement;
+  private _label: HTMLElement;
 
-  constructor(rootElement: HTMLElement, elements: HTMLElement[]) {
-    this._rootElement = rootElement;
-    this._elements = [];
-    this._dropDownElements = document.createElement('div');
+  constructor(iconName: string = '', label: string = '', element: HTMLElement) {
+    this._element = document.createElement('div');
+    this._dropDownElement = document.createElement('div');
+
     this._isMouseOverElement = false;
     this._expanded = false;
+
+    this._rootElement = document.createElement('div');
     this.root = document.createElement('div');
 
-    this._updateElements(elements);
+    this._icon = iconName ? ui.iconFA(iconName) : document.createElement('div');
+    this._label = label ? ui.divText(label, 'ui-drop-down-item-label') : document.createElement('div');
+
+    this._updateElements(element);
     this._initEventListeners();
   }
 
@@ -32,11 +41,11 @@ export class DropDown {
       this._setExpandedState(this._expanded);
     });
 
-    this._dropDownElements.addEventListener('mouseover', () => {
+    this._dropDownElement.addEventListener('mouseover', () => {
       this._isMouseOverElement = true;
     }, false);
 
-    this._dropDownElements.addEventListener('mouseleave', () => {
+    this._dropDownElement.addEventListener('mouseleave', () => {
       this._isMouseOverElement = false;
     }, false);
   }
@@ -45,25 +54,27 @@ export class DropDown {
     this._expanded = !expanded;
     if (expanded) {
       this.root.classList.remove('ui-drop-down-expanded');
-      this._dropDownElements.style.visibility = 'hidden';
+      this._dropDownElement.style.visibility = 'hidden';
       return;
     }
 
     this.root.classList.add('ui-drop-down-expanded');
-    this._dropDownElements.style.visibility = 'visible';
+    this._dropDownElement.style.visibility = 'visible';
   }
 
-  private _updateElements(elements: HTMLElement[]) {
-    this._elements = elements;
+  private _updateElements(element: HTMLElement) {
+    this._element = element;
 
-    this._dropDownElements = ui.div(this._elements.map((item) => ui.div(item, 'ui-drop-down-menu-list-item')));
-    this._dropDownElements.style.visibility = 'hidden';
-    this._dropDownElements.className = 'ui-drop-down-menu';
+    // this._dropDownElements = ui.div(this._elements.map((item)=> ui.div(item, 'ui-drop-down-menu-list-item')));
+    this._dropDownElement = ui.div(ui.div(this._element, 'ui-drop-down-menu-list-item'));
+    this._dropDownElement.style.visibility = 'hidden';
+    this._dropDownElement.className = 'ui-drop-down-menu';
 
-    const dropDown = ui.div(this._dropDownElements);
+    const dropDown = ui.div(this._dropDownElement);
     dropDown.className = 'ui-drop-down-menu-fixed';
 
-    this._rootElement.classList.add('ui-drop-down-item');
+    this._rootElement = ui.div([this._icon, this._label]);
+    this._rootElement.className = 'ui-drop-down-item';
 
     this.root = ui.div([this._rootElement, dropDown]);
     this.root.className = 'ui-drop-down-root';
