91382ae043eb697346124d5b132d6cc78a0ca9ee
PavloPolovyi <pasha32662@gmail.com>
2023-03-13 19:02:30 +0200

    GROK-12474: Grok Connect: Created test queries for MySQL provider

---
+++ connectors/grok_connect/src/test/resources/scripts/mysql/mysql_date_types.sql
@@ -0,0 +1,5 @@
+CREATE TABLE DATE_TYPES(date_type date, time_type time, timestamp_type timestamp,
+datetime_type datetime, year_type year);
+
+INSERT INTO DATE_TYPES(date_type, time_type, timestamp_type, datetime_type, year_type)
+VALUES ('9999-12-31', '838:59:59.000000', '1970-01-01 00:00:01.000000', '1000-01-01 00:00:00.000000', '2023');
