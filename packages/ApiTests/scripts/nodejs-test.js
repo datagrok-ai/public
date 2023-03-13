f840651ffe7781c4ff8f39921fa747fa16760c92
Anna Muzychyna <annamuz69@gmail.com>
2023-03-14 01:23:08 +0200

    GROK-12353: ApiTests: new tests

--- packages/ApiTests/scripts/nodejs-test.js
+++ packages/ApiTests/scripts/nodejs-test.js
@@ -6,14 +6,16 @@
 //input: bool b
 //input: string s
 //input: datetime dt
+//input_: map m
 //input: dataframe df
-//input: column col
+//input_: column col
 //output: int ri
 //output: double rd
 //output: bool rb
 //output: string rs
 //output: datetime rdt
-//output_: dataframe rdf
+//output_: map rm
+//output: dataframe rdf
 //output_: column rcol
 ri = i / 2;
 rd = d + 60;
@@ -21,4 +23,9 @@ rb = !b;
 rs = s + s;
 rdt = new Date(dt);
 rdt.setDate(rdt.getDate() + 10);
-// rdf = df.col(col.name);
+rm = m;
+rm['b'] = ri;
+col = df.clone().col('height');
+col.name = 'column';
+df.columns.add(col);
+rdf = df;
