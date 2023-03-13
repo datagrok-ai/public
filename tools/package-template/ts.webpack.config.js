473e3dba3f1727e085de77c8415eddbbbc14eaed
onuf <56073962+onuf@users.noreply.github.com>
2023-03-13 14:37:17 +0100

    Tools: GROK-12406: "grok check" enhancements
    Update sourcemap options in package template to simplify package debugging

--- tools/package-template/ts.webpack.config.js
+++ tools/package-template/ts.webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
       { test: /\.tsx?$/, loader: 'ts-loader' }
     ],
   },
-  devtool: 'inline-source-map',
+  devtool: 'source-map',
   externals: {
     'datagrok-api/dg': 'DG',
     'datagrok-api/grok': 'grok',
