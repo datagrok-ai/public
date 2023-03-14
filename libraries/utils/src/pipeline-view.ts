d3f46f7c32353f3a4d1de88146c541c7bf791503
Aufar Zakiev <zaufar@gmail.com>
2023-03-14 12:14:02 +0300

    Utils: Added historical runs to PipelineView

--- libraries/utils/src/pipeline-view.ts
+++ libraries/utils/src/pipeline-view.ts
@@ -2,13 +2,14 @@
 import * as grok from 'datagrok-api/grok';
 import * as ui from 'datagrok-api/ui';
 import * as DG from 'datagrok-api/dg';
-import {RichFunctionView} from './rich-function-view';
-import {UiUtils} from './shared-components/ui-utils';
+import {from} from 'rxjs';
+import {filter} from 'rxjs/operators';
+import JSZip from 'jszip';
 import {historyUtils} from './history-utils';
 import {FunctionView} from './function-view';
-import JSZip from 'jszip';
+import {ComputationView} from './computation-view';
 
-export class PipelineView extends RichFunctionView {
+export class PipelineView extends ComputationView {
   public stepViews: { [nqName: string]: FunctionView } = {};
   public stepTabs: DG.TabControl | null = null;
 
@@ -48,10 +49,17 @@ export class PipelineView extends RichFunctionView {
     return await zip.generateAsync({type: 'blob'});
   };
 
+  scriptCalls = {} as {[scriptNqName: string]: DG.FuncCall};
+  editorFuncs = {} as {[editor: string]: DG.Func};
+  scriptEditors = {} as {[scriptName: string]: string};
+  views = {} as {[script: string]: HTMLElement};
+  scripts: DG.Script[] = [];
+
   constructor(
-    funcCall: DG.FuncCall
+    funcName: string,
+    private scriptStepNames: string[]
   ) {
-    super(funcCall, {exportEnabled: true, historyEnabled: false, isTabbed: true});
+    super(funcName);
 
     this.exportConfig = {
       supportedExtensions: this.defaultSupportedExportExtensions(),
@@ -61,31 +69,88 @@ export class PipelineView extends RichFunctionView {
     };
   }
 
-  // REVIEW INHERITANCE HIERARCHY
+  public override async init() {
+    const scriptStepNames = this.scriptStepNames;
+
+    const stepScripts = scriptStepNames.map((stepScriptName) => {
+      const stepScript = (grok.functions.eval(stepScriptName) as Promise<DG.Func>);
+      return stepScript;
+    });
+    const allStepsLoaded = Promise.all(stepScripts) as Promise<DG.Script[]>;
+
+    this.root.classList.remove('ui-panel');
+
+    this.scripts = await allStepsLoaded;
+
+    const editorTag = 'editor:' as const;
+    const newlineSymbol = '\n' as const;
+    const defaultEditor = 'Compute:PipelineStepEditor';
+    const extractEditor = (script: DG.Script) => {
+      const scriptCode = script.script;
+      const editorTagIndex = scriptCode.indexOf(editorTag);
+      if (editorTagIndex < 0)
+        return defaultEditor;
+
+      const newlineIndex = scriptCode.indexOf(newlineSymbol, editorTagIndex);
+      const editorFuncName = scriptCode.substring(editorTagIndex + editorTag.length, newlineIndex).trim();
+
+      return editorFuncName;
+    };
+
+    for (const script of this.scripts) {
+      // TO DO: replace for type guard
+      const editorName = (script.script) ? extractEditor(script): defaultEditor;
+
+      if (!this.editorFuncs[editorName])
+        this.editorFuncs[editorName] = await(grok.functions.eval(editorName.split(' ').join('')) as Promise<DG.Func>);
+
+      this.scriptEditors[script.nqName] = editorName;
+    };
+
+    for (const script of this.scripts) {
+      const scriptCall = script.prepare();
+      const view: FunctionView = await this.editorFuncs[this.scriptEditors[script.nqName]].apply({'call': scriptCall});
+
+      // scriptCall.options['parentCallId'] = wrapperCall.id;
+      this.scriptCalls[script.nqName] = scriptCall;
+      this.views[script.nqName] = view.root;
+
+      // TEMP WAY TO DEAL WITH IT
+      this.stepViews[script.nqName] = view;
+    }
+  }
+
   public override buildIO() {
-    return ui.div();
+    const pipelineTabs = ui.tabControl(this.views);
+    for (let i = 0; i < pipelineTabs.panes.length - 1; i++) {
+      pipelineTabs.panes[i].header.classList.add('arrow-tab');
+      pipelineTabs.panes[i].header.insertAdjacentElement('afterend', ui.div(undefined, 'empty-box'));
+    }
+    pipelineTabs.root.style.height = '100%';
+    pipelineTabs.root.style.width = '100%';
+
+    this.stepTabs = pipelineTabs;
+
+    return pipelineTabs.root;
   }
 
-  // Should override to prevent saving right after model loading
-  // Actually, it does nothing
-  public async run(): Promise<void> {
-  }
+  public override async run(): Promise<void> {
+    if (!this.funcCall) throw new Error('The correspoding function is not specified');
 
-  /**
-   * Override to create a custom historical runs control.
-   * @returns The HTMLElement with history block UI
-  */
-  // Overrrided since we should load child funcCalls
-  public buildHistoryBlock(): HTMLElement {
-    const newHistoryBlock = UiUtils.historyPanel(this.func!);
+    await this.onBeforeRun(this.funcCall);
+    const pi = DG.TaskBarProgressIndicator.create('Calculating...');
+    this.funcCall.newId();
+    await this.funcCall.call(); // mutates the funcCall field
+    pi.close();
 
-    newHistoryBlock.onRunChosen.subscribe(async (id) => this.linkFunccall(await this.loadRun(id)));
+    Object.values(this.scriptCalls).forEach(async (scriptCall) => {
+      scriptCall.options['parentCallId'] = this.funcCall!.id;
+      await this.stepViews[scriptCall.func.nqName].saveRun(scriptCall);
+    });
 
-    ui.empty(this.historyRoot);
-    this.historyRoot.style.removeProperty('justify-content');
-    this.historyRoot.style.width = '100%';
-    this.historyRoot.append(newHistoryBlock.root);
-    return newHistoryBlock.root;
+    await this.onAfterRun(this.funcCall);
+
+    this.lastCall = await this.saveRun(this.funcCall);
   }
 
   /**
@@ -98,8 +163,9 @@ export class PipelineView extends RichFunctionView {
     await this.onBeforeLoadRun();
     const {parentRun: pulledParentRun, childRuns: pulledChildRuns} = await historyUtils.loadChildRuns(funcCallId);
 
-    pulledChildRuns.forEach((pulledChildRun) => {
-      this.stepViews[pulledChildRun.nqName].linkFunccall(pulledChildRun);
+    pulledChildRuns.forEach(async (pulledChildRun) => {
+      await this.stepViews[pulledChildRun.func.nqName].onAfterLoadRun(pulledChildRun);
+      this.stepViews[pulledChildRun.func.nqName].linkFunccall(pulledChildRun);
     });
 
     await this.onAfterLoadRun(pulledParentRun);
