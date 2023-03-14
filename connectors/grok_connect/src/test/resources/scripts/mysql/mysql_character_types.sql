fc2703ae237c88ab4355acef3807a01b69f4d95c
PavloPolovyi <pasha32662@gmail.com>
2023-03-14 12:57:54 +0200

    GROK-12473: Grok Connect: Finished test cases, refactored some code

--- connectors/grok_connect/src/test/resources/scripts/mysql/mysql_character_types.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE CHARACTER_TYPES (char_type1 char(8), char_type2 char, varchar_type varchar, text_type text,
-mediumtext_type mediumtext, longtext_type longtext);
-
-INSERT INTO CHARACTER_TYPES(char_type1, char_type2, varchar_type, text_type, mediumtext_type, longtext_type)
-VALUES ('datagrok', 'A', 'Hello World', 'Hello Datagrok', 'HelloolleH', 'Datagrok');
