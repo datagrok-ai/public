aab3bc32c70b60e945740a974d4f3a4f45b730a4
PavloPolovyi <pasha32662@gmail.com>
2023-03-15 15:30:19 +0200

    GROK-12500: Grok Connect: Created test queries

---
+++ connectors/grok_connect/src/test/resources/scripts/redshift/redshift_character_types.sql
@@ -0,0 +1,4 @@
+CREATE TABLE CHARACTER_TYPES(char_type char(5), varchar_type varchar(8), nchar_type nchar(5),
+nvarchar_type nvarchar(8), text_type text, bpchar_type bpchar);
+INSERT INTO CHARACTER_TYPES(char_type, varchar_type, nchar_type, nvarchar_type, text_type, bpchar_type)
+VALUES ('Hello', 'Datagrok', 'Groks', 'Datagrok', 'Hello World!', 'Groking');
