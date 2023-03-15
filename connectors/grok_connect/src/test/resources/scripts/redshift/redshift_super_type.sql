a5380298424caeb1583f5f62fa2cc156c9c3967b
PavloPolovyi <pasha32662@gmail.com>
2023-03-15 15:30:19 +0200

    GROK-12500: Grok Connect: Created test queries

---
+++ connectors/grok_connect/src/test/resources/scripts/redshift/redshift_super_type.sql
@@ -0,0 +1,3 @@
+CREATE TABLE super_type(super_type super);
+INSERT INTO super_type(super_type) VALUES (JSON_PARSE('{"bar": "baz", "balance": 7.77, "active":false}'));
+INSERT INTO super_type(super_type) VALUES (JSON_PARSE('[10,100,1000]'));
