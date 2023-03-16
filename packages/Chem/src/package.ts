49d347500597f3966999fb102d5e1943206ed601
Maria Dolotova <maria.dolotova@softwarecountry.com>
2023-03-16 17:19:42 +0300

    Chem: Bump version

--- packages/Chem/src/package.ts
+++ packages/Chem/src/package.ts
@@ -356,7 +356,7 @@ export function similaritySearchViewer(): ChemSimilarityViewer {
 //name: similaritySearch
 //description: finds the most similar molecule
 export function similaritySearchTopMenu(): void {
-  (grok.shell.v as DG.TableView).addViewer('SimilaritySearchViewer');
+  (grok.shell.v as DG.TableView).addViewer('Chem Similarity Search');
 }
 
 //name: Chem Diversity Search
@@ -371,7 +371,7 @@ export function diversitySearchViewer(): ChemDiversityViewer {
 //name: diversitySearch
 //description: finds the most diverse molecules
 export function diversitySearchTopMenu(): void {
-  (grok.shell.v as DG.TableView).addViewer('DiversitySearchViewer');
+  (grok.shell.v as DG.TableView).addViewer('Chem Diversity Search');
 }
 
 
