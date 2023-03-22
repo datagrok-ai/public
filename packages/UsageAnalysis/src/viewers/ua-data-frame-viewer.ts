916f50b680b1b90a20a180e254a5553aa705d137
Anna Muzychyna <annamuz69@gmail.com>
2023-03-22 09:59:10 +0200

    GROK-12111 UsageAnalysis: Fixes & improvements

--- packages/UsageAnalysis/src/viewers/ua-data-frame-viewer.ts
+++ packages/UsageAnalysis/src/viewers/ua-data-frame-viewer.ts
@@ -5,14 +5,14 @@ export class UaDataFrameViewer extends UaViewer {
   dataFrame: DG.DataFrame;
   viewerFunction: Function;
 
-  public constructor(name: string, dataFrame: DG.DataFrame, viewerFunction: Function, showName: boolean = true) {
-    super(name, null, showName);
+  public constructor(name: string, dataFrame: DG.DataFrame, viewerFunction: Function) {
+    super(name, null);
     this.dataFrame = dataFrame;
     this.viewerFunction = viewerFunction;
     this.reloadViewer();
   }
 
-  setViewer(loader: any, host: HTMLDivElement, nameDiv: HTMLElement) {
+  setViewer(loader: any, host: HTMLDivElement) {
     const grid = DG.Viewer.grid(this.dataFrame);
     host.appendChild(grid.root);
     grid.autoSize(600, 400, 200, 100);
