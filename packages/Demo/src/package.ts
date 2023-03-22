6f89533e8cd09771721a553b1a05c54d64e00c85
Dmytro Kovalyov <laykdimon278@gmail.com>
2023-03-22 15:48:04 +0200

    Tutorials | Demo: Moved demo app to Tutorials package

--- packages/Demo/src/package.ts
+++ packages/Demo/src/package.ts
@@ -3,26 +3,8 @@ import * as grok from 'datagrok-api/grok';
 // import * as ui from 'datagrok-api/ui';
 import * as DG from 'datagrok-api/dg';
 
-import {DemoView} from './demo-app/demo-app';
 // eslint-disable-next-line no-unused-vars
 import {expect, expectArray, expectFloat, expectObject} from '@datagrok-libraries/utils/src/test';
 
 
 export const _package = new DG.Package();
-
-
-//name: Demo
-//tags: app
-//description: Interactive demo of major Datagrok capabilities
-export function demoApp() {
-  grok.shell.addView(new DemoView());
-  const pathSegments = window.location.pathname.split('/');
-  if (pathSegments.length > 3) {
-    const category = pathSegments[3];
-    if (category === 'Viewers') {
-      const viewerName = pathSegments[4];
-      const f = DemoView.findDemoFunc(`${category} | ${viewerName}`);
-      f?.apply();
-    }
-  }
-}
