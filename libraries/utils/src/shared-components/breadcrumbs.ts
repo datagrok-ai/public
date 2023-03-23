8d06410aa687e0de6610b98a55250ee525204f9d
Dmytro Kovalyov <laykdimon278@gmail.com>
2023-03-23 16:45:25 +0200

    GROK-12434: Library utils: Added onClick event for Breacdrumbs path parts

--- libraries/utils/src/shared-components/breadcrumbs.ts
+++ libraries/utils/src/shared-components/breadcrumbs.ts
@@ -12,10 +12,12 @@ export class Breadcrumbs {
     this.root = ui.div();
     this.path = path;
 
-    this.root = ui.div(path.map((elem) => ui.span([elem], 'list-elem')), 'list');
+    this.root = ui.divH(path.map((element) =>
+      ui.div(ui.span([element], `${element}`), `breadcrumbs-element`)), 'breadcrumbs');
   }
 
-  get onPathClick(): Observable<MouseEvent> {
-    return fromEvent<MouseEvent>(this.root, 'click');
+  onPathClick(pathElementName: string): Observable<MouseEvent> {
+    const pathElement = this.root.getElementsByClassName(`${pathElementName}`);
+    return fromEvent<MouseEvent>(pathElement, 'click');
   }
 }
