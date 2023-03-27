73a077632a95a8f06b7a03f9473ea0d24ffc232e
Dmytro Kovalyov <laykdimon278@gmail.com>
2023-03-27 13:50:09 +0300

    GROK-12434: Library utils: Implemented Breadcrumbs onPathClick event

--- libraries/utils/src/shared-components/breadcrumbs.ts
+++ libraries/utils/src/shared-components/breadcrumbs.ts
@@ -1,5 +1,6 @@
 import * as ui from 'datagrok-api/ui';
 import {Observable, fromEvent} from 'rxjs';
+import {map} from 'rxjs/operators';
 
 import '../../css/breadcrumbs.css';
 
@@ -13,11 +14,20 @@ export class Breadcrumbs {
     this.path = path;
 
     this.root = ui.divH(path.map((element) =>
-      ui.div(ui.span([element], `${element}`), `breadcrumbs-element`)), 'breadcrumbs');
+      ui.div(ui.span([element], `breadcrumbs-text-element ${element}`), `breadcrumbs-element`)), 'breadcrumbs');
   }
 
-  onPathClick(pathElementName: string): Observable<MouseEvent> {
-    const pathElement = this.root.getElementsByClassName(`${pathElementName}`);
-    return fromEvent<MouseEvent>(pathElement, 'click');
+  get onPathClick(): Observable<string[]> {
+    const pathElements = this.root.getElementsByClassName('breadcrumbs-text-element');
+
+    return fromEvent<MouseEvent>(pathElements, 'click')
+      .pipe(
+        map((event) => {
+          const currentElement = (event.target as Element).innerHTML;
+          const currentPath = this.path.slice(0, this.path.indexOf(currentElement) + 1);
+
+          return currentPath;
+        })
+      );
   }
 }
