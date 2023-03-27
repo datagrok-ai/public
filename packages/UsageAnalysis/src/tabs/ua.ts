f4ef3811b7edab0bdf0bb5472dcc3674f2117b1a
Anna Muzychyna <annamuz69@gmail.com>
2023-03-27 03:04:56 +0300

    GROK-12111 UsageAnalysis: Improvements
    - drilldown filter
    - new viewer loader
    - changing only dataframe, not recreating viewer
    - new global filter ui

--- packages/UsageAnalysis/src/tabs/ua.ts
+++ packages/UsageAnalysis/src/tabs/ua.ts
@@ -1,21 +1,41 @@
 import * as DG from 'datagrok-api/dg';
+// import * as ui from 'datagrok-api/ui';
 
 import {UaToolbox} from '../ua-toolbox';
 import {UaQueryViewer} from '../viewers/abstract/ua-query-viewer';
+// import {BehaviorSubject} from 'rxjs';
+// import {UaFilter} from '../filter';
+
 
 export class UaView extends DG.ViewBase {
-  static viewName = 'Usage Analysis View';
+  viewName = 'Usage Analysis View';
   uaToolbox: UaToolbox;
   viewers: UaQueryViewer[] = [];
   initialized: boolean = false;
-  view: DG.View | undefined;
+  viewer: DG.Viewer;
 
-  constructor(uaToolbox: UaToolbox, view?: DG.View) {
+  constructor(uaToolbox: UaToolbox, y?: string) {
     super();
     this.uaToolbox = uaToolbox;
     this.toolbox = uaToolbox.rootAccordion.root;
     this.box = true;
-    this.view = view;
+    const df = DG.DataFrame.fromColumns([DG.Column.fromList('string', 'time_start', []),
+      DG.Column.fromList('string', y ?? '', []),
+      DG.Column.fromList('string', 'count', []),
+      DG.Column.fromList('string', 'user', [])]);
+    this.viewer = DG.Viewer.scatterPlot(df, {
+      x: 'time_start',
+      y: y,
+      size: 'count',
+      // color: 'user',
+      jitterSize: 5,
+      markerMinSize: 10,
+      markerMaxSize: 30,
+      showColorSelector: false,
+      showSizeSelector: false,
+      showXSelector: false,
+      showYSelector: false,
+    });
   }
 
   tryToinitViewers() {
@@ -24,6 +44,11 @@ export class UaView extends DG.ViewBase {
       this.initViewers();
     }
   }
+
+  // public static checkLabels() {
+  //   return [UaView.dateFromDD, UaView.dateToDD,
+  //     UaView.usersDD, UaView.packagesDD].some((val) => val != null);
+  // }
 
   async initViewers(): Promise<void> {}
 }
