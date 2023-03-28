6228962fcf5fa75a3d116c263cd0659f2527a9b1
Aleksandr Tanas <atanas@datagrok.ai>
2023-03-28 12:12:13 +0400

    GROK-12644: Bio | ST: Fix absolute stereochemistry for some monomers
    Fix monomer library builder to preserve molfile
    collection STEABS from source mol

--- packages/SequenceTranslator/scripts/build-monomer-lib.py
+++ packages/SequenceTranslator/scripts/build-monomer-lib.py
@@ -1,5 +1,6 @@
 # pylint: disable=no-member
 import os.path
+import sys
 from io import TextIOWrapper
 from typing import Optional
 
@@ -16,6 +17,9 @@ BEGIN_ATOM_LINE = 'M  V30 BEGIN ATOM'
 END_ATOM_LINE = 'M  V30 END ATOM'
 BEGIN_BOND_LINE = 'M  V30 BEGIN BOND'
 END_BOND_LINE = 'M  V30 END BOND'
+BEGIN_COLLECTION_LINE = 'M  V30 BEGIN COLLECTION'
+END_COLLECTION_LINE = 'M  V30 END COLLECTION'
+COLLECTION_STEABS_LINE = 'M  V30 MDLV30/STEABS'
 
 
 def mol_add_collection(mol: Mol,
@@ -39,38 +43,64 @@ def mol_add_collection(mol: Mol,
     if name and name not in mb_line_list[1]:
         mb_line_list[1] += '|' + name
 
+    chirality = [atom.GetChiralTag() for atom in mol.GetAtoms()]
+
     # preserve chirality for bonds from src_mol
     tgt_mol_file_map = MolFileMap.parse(res)
+    steabs = []
     if src_mol:
         src_mol_file_map = MolFileMap.parse(src_mol)
-        for (tgt_bond_idx0, (bond_key, tgt_bond)) in enumerate(tgt_mol_file_map.mol_file.bonds.items()):
-            if bond_key in src_mol_file_map.mol_file.bonds:
-                src_bond = src_mol_file_map.mol_file.bonds[bond_key]
-                if src_bond.cfg:
-                    # replace line with CFG from mol_src
-                    mb_line_list[tgt_mol_file_map.begin_bond_idx + tgt_bond_idx0 + 1] = src_bond.src
+        if len(tgt_mol_file_map.mol_file.atom_list) != len(src_mol_file_map.mol_file.atom_list):
+            raise ValueError(f"Atoms count of src and tgt differs for monomer '{name}'.")
+        for (src_bond_idx0, (bond_key, src_bond)) in enumerate(src_mol_file_map.mol_file.bonds.items()):
+            if src_bond.cfg:
+                if bond_key not in tgt_mol_file_map.mol_file.bonds:
+                    raise KeyError(f"Bond key '{bond_key}' not found in tgt bonds.")
+                tgt_bond: MolFileBond = tgt_mol_file_map.mol_file.bonds[bond_key]
+                tgt_bond_cfg_str: str = ' '.join(tgt_bond.cfg)
+                src_bond_cfg_str: str = ' '.join(src_bond.cfg)
+                if tgt_bond_cfg_str != src_bond_cfg_str:
+                    mb_line_list[tgt_mol_file_map.begin_bond_idx + tgt_bond.number] += f" {src_bond_cfg_str}"
 
-    chirality = [atom.GetChiralTag() for atom in mol.GetAtoms()]
-    begin_atom_idx = mb_line_list.index('M  V30 BEGIN ATOM')
-    end_atom_idx = mb_line_list.index('M  V30 END ATOM')
-    for atom_idx in range(1, end_atom_idx - begin_atom_idx):
-        line_idx = begin_atom_idx + atom_idx
-        atom_ch = chirality[atom_idx - 1]
-        if atom_ch != Chem.rdchem.CHI_UNSPECIFIED:
-            mb_line_list[line_idx] += " CFG={0}".format(int(atom_ch))
+        for (tgt_atom_idx0, tgt_atom) in enumerate(tgt_mol_file_map.mol_file.atom_list):
+            src_atom = src_mol_file_map.mol_file.atom_list[tgt_atom_idx0]
+            atom_ch = chirality[tgt_atom_idx0]
+            if src_atom.cfg:
+                mb_line_list[tgt_mol_file_map.begin_atom_idx + tgt_atom_idx0 + 1] += " {0}".format(
+                    ' '.join(src_atom.cfg))
+                steabs.append(tgt_atom_idx0 + 1)
+            elif atom_ch != Chem.rdchem.CHI_UNSPECIFIED:
+                mb_line_list[tgt_mol_file_map.begin_atom_idx + tgt_atom_idx0 + 1] += " CFG={0}".format(int(atom_ch))
+                steabs.append(tgt_atom_idx0 + 1)
+            elif src_atom.number in src_mol_file_map.mol_file.collection_steabs:
+                raise KeyError(f"Source STEABS atom '{src_atom}' not accounted")
+            elif tgt_atom.number in tgt_mol_file_map.mol_file.collection_steabs:
+                raise KeyError(f"Target STEABS atom '{tgt_atom}' not accounted")
+    else:
+        # from Smiles (without src_mol)
+        for (tgt_atom_idx0, tgt_atom) in enumerate(tgt_mol_file_map.mol_file.atom_list):
+            line_idx = tgt_mol_file_map.begin_atom_idx + tgt_atom_idx0 + 1
+            atom_ch = chirality[tgt_atom_idx0]
+            if atom_ch != Chem.rdchem.CHI_UNSPECIFIED:
+                mb_line_list[line_idx] += " CFG={0}".format(int(atom_ch))
+                steabs.append(tgt_atom_idx0 + 1)
 
-    steabs: list[int] = [
-        i + 1 for (i, ch) in enumerate(chirality)
-        if ch != Chem.rdchem.CHI_UNSPECIFIED
-    ]
     if len(steabs) > 0:
-        steabs_str: str = "M  V30 MDLV30/STEABS ATOMS=({count} {list})".format(
+        steabs_str: str = COLLECTION_STEABS_LINE + " ATOMS=({count} {list})".format(
             count=len(steabs),
             list=' '.join([str(idx) for idx in steabs]))
-
-        mb_line_list = mb_line_list[:(tgt_mol_file_map.end_bond_idx + 1)] + \
-                       ["M  V30 BEGIN COLLECTION", steabs_str, "M  V30 END COLLECTION"] + \
-                       mb_line_list[(tgt_mol_file_map.end_bond_idx + 1):]
+        if tgt_mol_file_map.collection_steabs_idx:
+            mb_line_list[tgt_mol_file_map.collection_steabs_idx] = steabs_str
+        elif tgt_mol_file_map.begin_collection_idx is not None:
+            tgt_collection_steabs_idx = tgt_mol_file_map.begin_collection_idx + 1
+            mb_line_list = mb_line_list[:tgt_collection_steabs_idx] + \
+                           [steabs_str] + \
+                           mb_line_list[tgt_collection_steabs_idx:]
+        else:
+            tgt_collection_idx = tgt_mol_file_map.end_bond_idx + 1
+            mb_line_list = mb_line_list[:tgt_collection_idx] + \
+                           [BEGIN_COLLECTION_LINE, steabs_str, END_COLLECTION_LINE] + \
+                           mb_line_list[tgt_collection_idx:]
 
     return '\n'.join(mb_line_list)
 
@@ -78,7 +108,7 @@ def mol_add_collection(mol: Mol,
 def molfile2molfile(src_mol: str, name: str) -> str:
     """Loads mol from src_mol str. Fixed title, adds chirality to atoms and preserves chirality for bonds."""
     # Using sanitize=False leads to unwanted moving stereo (invalid?) CFGs to other bonds
-    mol: Mol = Chem.MolFromMolBlock(src_mol)
+    mol: Mol = Chem.MolFromMolBlock(src_mol, removeHs=False)
     src_mf_lines = src_mol.split('\n')
     title = src_mf_lines[1]
     return mol_add_collection(mol, name, title=title, src_mol=src_mol)
@@ -147,11 +177,25 @@ class Monomer:
 class MolFileAtom:
     def __init__(self, src: str):
         self._src = src
-        self._number = int(src[7:].split(' ')[0].strip())
+        self._parts: [] = self._src[7:].split(' ')
+        self._number = int(self._parts[0].strip())
+        self._cfg = self._parts[6:]
+
+    @property
+    def src(self) -> str:
+        return self._src
 
     @property
     def number(self):
         return self._number
+
+    @property
+    def parts(self) -> list[str]:
+        return self.parts
+
+    @property
+    def cfg(self) -> list[str]:
+        return self._cfg
 
     def __str__(self):
         return self._src
@@ -163,8 +207,8 @@ class MolFileAtom:
 class MolFileBond:
     def __init__(self, src: str):
         self._src = src
-        self._number = int(src[7:].split(' ')[0].strip())
         self._parts: [] = self._src[7:].split(' ')
+        self._number = int(self._parts[0].strip())
         self._key = self._parts[0:4]
         self._cfg = self._parts[4:]
 
@@ -196,10 +240,12 @@ class MolFileBond:
 
 
 class MolFile:
-    def __init__(self, title: str, atom_list: list[MolFileAtom], bond_list: list[MolFileBond]):
+    def __init__(self, title: str, atom_list: list[MolFileAtom], bond_list: list[MolFileBond],
+                 collection_steabs: list[int] = []):
         self._title = title
         self._atom_list = atom_list
         self._bond_list = bond_list
+        self.collection_steabs = collection_steabs
 
         self._bonds: dict = {}
         for bond in self._bond_list:
@@ -224,13 +270,18 @@ class MolFile:
 
 class MolFileMap:
     def __init__(self, src: str, mol_file: MolFile,
-                 begin_atom_idx: int, end_atom_idx: int, begin_bond_idx: int, end_bond_idx: int):
+                 begin_atom_idx: int, end_atom_idx: int, begin_bond_idx: int, end_bond_idx: int,
+                 begin_collection_idx: int = None, end_collection_idx: int = None,
+                 collection_steabs_idx: int = None):
         self._src = src
         self._mol_file = mol_file
         self.begin_atom_idx = begin_atom_idx
         self.end_atom_idx = end_atom_idx
         self.begin_bond_idx = begin_bond_idx
         self.end_bond_idx = end_bond_idx
+        self.begin_collection_idx = begin_collection_idx
+        self.end_collection_idx = end_collection_idx
+        self.collection_steabs_idx = collection_steabs_idx
 
     @property
     def src(self):
@@ -265,8 +316,29 @@ class MolFileMap:
             bond = MolFileBond(bond_line)
             bond_list[bond_idx - 1] = bond
 
-        mol_file = MolFile(title, atom_list, bond_list)
-        return MolFileMap(mol_src, mol_file, begin_atom_idx, end_atom_idx, begin_bond_idx, end_bond_idx)
+        begin_collection_idx = None
+        end_collection_idx = None
+        collection_steabs_idx = None
+        collection_steabs: list[int] = []
+        if BEGIN_COLLECTION_LINE in mb_line_list and END_COLLECTION_LINE in mb_line_list:
+            begin_collection_idx = mb_line_list.index(BEGIN_COLLECTION_LINE)
+            end_collection_idx = mb_line_list.index(END_COLLECTION_LINE)
+            collection_count: int = end_collection_idx - begin_collection_idx - 1
+            for collection_idx in range(1, collection_count + 1):
+                line_idx = begin_collection_idx + collection_idx
+                collection_line = mb_line_list[line_idx]
+                if collection_line.startswith(COLLECTION_STEABS_LINE):
+                    steabs_str = collection_line[len(COLLECTION_STEABS_LINE + " ATOMS=("):-1]
+                    collection_steabs = [int(atom_num_str.strip()) for atom_num_str in steabs_str.split(' ')[1:]]
+                    collection_steabs_idx = line_idx
+                else:
+                    raise ValueError(f"Unexpected collection line '{collection_line}'.")
+
+        mol_file = MolFile(title, atom_list, bond_list, collection_steabs)
+        return MolFileMap(mol_src, mol_file,
+                          begin_atom_idx, end_atom_idx, begin_bond_idx, end_bond_idx,
+                          begin_collection_idx, end_collection_idx,
+                          collection_steabs_idx)
 
 
 def codes2monomers(codes_json: {}) -> dict[str, Monomer]:
@@ -332,18 +404,22 @@ def main(ctx, initial_f: TextIOWrapper, lib_f: TextIOWrapper,
             add_json_str = add_json_f.read()
             add_json = orjson.loads(add_json_str)
             for add_m in add_json:
-                if not add_m['molfile']:
-                    monomer_mol_fn = os.path.join(os.path.dirname(add_json_fn), add_m['name'] + '.mol')
-                    if not os.path.isfile(monomer_mol_fn):
-                        raise FileNotFoundError(monomer_mol_fn)
-                    else:
-                        with open(monomer_mol_fn, 'r') as monomer_mol_f:
-                            monomer_mol_lines = [line.rstrip() for line in monomer_mol_f.readlines()]
-                            monomer_mol_txt = '\n'.join(monomer_mol_lines)
-                            add_m['molfile'] = monomer_mol_txt
+                name = add_m['name']
+                try:
+                    if not add_m['molfile']:
+                        monomer_mol_fn = os.path.join(os.path.dirname(add_json_fn), add_m['name'] + '.mol')
+                        if not os.path.isfile(monomer_mol_fn):
+                            raise FileNotFoundError(monomer_mol_fn)
+                        else:
+                            with open(monomer_mol_fn, 'r') as monomer_mol_f:
+                                monomer_mol_lines = [line.rstrip() for line in monomer_mol_f.readlines()]
+                                monomer_mol_txt = '\n'.join(monomer_mol_lines)
+                                add_m['molfile'] = monomer_mol_txt
 
-                m = Monomer.from_json(add_m)
-                monomers[m.name] = m
+                    m = Monomer.from_json(add_m)
+                    monomers[m.name] = m
+                except Exception as ex:
+                    sys.stderr.write(f"Invalid monomer '{add_m['name']}' error:\n{str(ex)}")
 
     add_json = [m.to_json() for m in monomers.values()]
 
