9bb475f911f397fddf9f5305a133e3777a1fb2fe
MariaDolotova <44060410+MariaDolotova@users.noreply.github.com>
2023-03-28 12:42:56 +0300

    Mdolotova/grok 12459/handling malformed data (#1743)
    * GROK-12459: Chem: Handle malformed and empty data
    
    * GROK-12459: Chem: Malformed data handling
    
    * GROK-12459: Chem: Malformed data selection
    
    * GROK-12459: Chem: Fixed failing on broken data in _chemGetSimilarities
    
    * GROK-12459: Chem: Separating empty and malformed data
    
    * GROK-12459: Chem: Malformed data handling
    
    * GROK-12459: Chem: Updated utils and ml dependencies
    
    * GROK-12459: Chem: Malformed data handling
    
    ---------
    
    Co-authored-by: Maria Dolotova <maria.dolotova@softwarecountry.com>

--- packages/Chem/src/analysis/chem-similarity-viewer.ts
+++ packages/Chem/src/analysis/chem-similarity-viewer.ts
@@ -8,6 +8,7 @@ import {Fingerprint} from '../utils/chem-common';
 import {renderMolecule} from '../rendering/render-molecule';
 import {ChemSearchBaseViewer} from './chem-search-base-viewer';
 import {getRdKitModule} from '../utils/chem-common-rdkit';
+import { malformedDataWarning } from '../utils/malformed-data-utils';
 
 export class ChemSimilarityViewer extends ChemSearchBaseViewer {
   isEditedFromSketcher: boolean = false;
@@ -46,7 +47,7 @@ export class ChemSimilarityViewer extends ChemSearchBaseViewer {
             this.sketchedMolecule = savedMolecule;
           } else {
             this.sketchedMolecule = sketcher.getMolFile();
-            this.gridSelect = false; 
+            this.gridSelect = false;
             this.render();
           }
         })
@@ -65,20 +66,27 @@ export class ChemSimilarityViewer extends ChemSearchBaseViewer {
   async render(computeData = true): Promise<void> {
     if (!this.beforeRender())
       return;
-    if (this.moleculeColumn) {   
+    if (this.moleculeColumn) {
       const progressBar = DG.TaskBarProgressIndicator.create(`Similarity search running...`);
       this.curIdx = this.dataFrame!.currentRowIdx == -1 ? 0 : this.dataFrame!.currentRowIdx;
       if (computeData && !this.gridSelect) {
         this.targetMoleculeIdx = this.dataFrame!.currentRowIdx == -1 ? 0 : this.dataFrame!.currentRowIdx;
-        if (this.isEmptyValue() || this.checkMalformedTargetMolecule()) {
+        if (this.isEmptyValue()) {
           progressBar.close();
           return;
         }
-        const df = await chemSimilaritySearch(this.dataFrame!, this.moleculeColumn!,
-          this.targetMolecule, this.distanceMetric, this.limit, this.cutoff, this.fingerprint as Fingerprint);
-        this.molCol = df.getCol('smiles');
-        this.idxs = df.getCol('indexes');
-        this.scores = df.getCol('score');
+        try {
+          const df = await chemSimilaritySearch(this.dataFrame!, this.moleculeColumn!,
+            this.targetMolecule, this.distanceMetric, this.limit, this.cutoff, this.fingerprint as Fingerprint);
+          this.molCol = df.getCol('smiles');
+          this.idxs = df.getCol('indexes');
+          this.scores = df.getCol('score');
+        } catch (e: any){
+          grok.shell.error(e.message);
+          return;
+        } finally {
+          progressBar.close();
+        }
       } else if (this.gridSelect)
         this.gridSelect = false;
       this.clearResults();
@@ -163,20 +171,6 @@ export class ChemSimilarityViewer extends ChemSearchBaseViewer {
     return false;
   }
 
-  checkMalformedTargetMolecule(): boolean {
-    let mol;
-    try {
-      mol = getRdKitModule().get_mol(this.targetMolecule);
-      return false;
-    } catch (e: any) {
-      grok.shell.error(`Possibly malformed target molecule`);
-      this.clearResults();
-      return true;
-    } finally {
-      if (mol) mol.delete();
-    }
-  }
-
   clearResults() {
     if (this.root.hasChildNodes())
       this.root.removeChild(this.root.childNodes[0]);
@@ -192,15 +186,15 @@ export async function chemSimilaritySearch(
   minScore: number,
   fingerprint: Fingerprint,
 ) : Promise<DG.DataFrame> {
-  limit = Math.min(limit, smiles.length);
   const targetFingerprint = chemSearches.chemGetFingerprint(molecule, fingerprint);
   const fingerprintCol = await chemSearches.chemGetFingerprints(smiles, fingerprint);
+  malformedDataWarning(fingerprintCol, table);
   const distances: number[] = [];
 
   const fpSim = similarityMetric[metricName];
   for (let row = 0; row < fingerprintCol.length; row++) {
     const fp = fingerprintCol[row];
-    distances[row] = fp == null ? 100.0 : fpSim(targetFingerprint, fp);
+    distances[row] = (!fp || fp!.allFalse) ? 100.0 : fpSim(targetFingerprint, fp!);
   }
 
   function range(end: number) {
@@ -218,12 +212,12 @@ export async function chemSimilaritySearch(
   }
 
   const indexes = range(table.rowCount)
-    .filter((idx) => fingerprintCol[idx] != null)
+    .filter((idx) => fingerprintCol[idx] && !fingerprintCol[idx]!.allFalse)
     .sort(compare);
   const molsList = [];
   const scoresList = [];
   const molsIdxs = [];
-
+  limit = Math.min(indexes.length, limit);
   for (let n = 0; n < limit; n++) {
     const idx = indexes[n];
     const score = distances[idx];
