a7ce75f9c116c193f1d8cfd4c61f41a213b9a562
MariaDolotova <44060410+MariaDolotova@users.noreply.github.com>
2023-03-28 09:49:03 +0300

    Mdolotova/grok 12459/handling malformed data (#1679)
    * GROK-12459: Chem: Handle malformed and empty data
    
    * GROK-12459: Chem: Malformed data handling
    
    * GROK-12459: Chem: Malformed data selection
    
    * GROK-12459: Chem: Fixed failing on broken data in _chemGetSimilarities
    
    * GROK-12459: Chem: Separating empty and malformed data
    
    * GROK-12459: Chem: Malformed data handling
    
    ---------
    
    Co-authored-by: Maria Dolotova <maria.dolotova@softwarecountry.com>

--- packages/Chem/src/package.ts
+++ packages/Chem/src/package.ts
@@ -240,9 +240,9 @@ export async function getMorganFingerprints(molColumn: DG.Column): Promise<DG.Co
 
   try {
     const fingerprints = await chemSearches.chemGetFingerprints(molColumn, Fingerprint.Morgan);
-    const fingerprintsBitsets: DG.BitSet[] = [];
+    const fingerprintsBitsets: (DG.BitSet | null)[] = [];
     for (let i = 0; i < fingerprints.length; ++i) {
-      const fingerprint = DG.BitSet.fromBytes(fingerprints[i].getRawData().buffer, fingerprints[i].length);
+      const fingerprint = fingerprints[i] ? DG.BitSet.fromBytes(fingerprints[i]!.getRawData().buffer, fingerprints[i]!.length) : null;
       fingerprintsBitsets.push(fingerprint);
     }
     return DG.Column.fromList('object', 'fingerprints', fingerprintsBitsets);
@@ -412,11 +412,8 @@ export async function chemSpaceTopMenu(table: DG.DataFrame, molecules: DG.Column
 
   const embedColsNames = getEmbeddingColsNames(table);
 
-  // dimensionality reducing algorithm doesn't handle empty values correctly so remove empty values at this step
-  const withoutEmptyValues = DG.DataFrame.fromColumns([molecules]).clone();
-  const emptyValsIdxs = removeEmptyStringRows(withoutEmptyValues, molecules);
   const chemSpaceParams = {
-    seqCol: withoutEmptyValues.col(molecules.name)!,
+    seqCol: molecules,
     methodName: methodName,
     similarityMetric: similarityMetric,
     embedAxesNames: [embedColsNames[0], embedColsNames[1]],
@@ -425,11 +422,8 @@ export async function chemSpaceTopMenu(table: DG.DataFrame, molecules: DG.Column
   const chemSpaceRes = await chemSpace(chemSpaceParams);
   const embeddings = chemSpaceRes.coordinates;
 
-  //inserting empty values back into results
   for (const col of embeddings) {
-    const listValues = col.toList();
-    emptyValsIdxs.forEach((ind: number) => listValues.splice(ind, 0, null));
-    table.columns.add(DG.Column.float(col.name, table.rowCount).init((i)=> listValues[i]));
+    table.columns.add(col);
   }
   if (plotEmbeddings)
     return grok.shell
