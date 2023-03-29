4ae46f2ade1bd3d5c01f9f14094be8cefe2dbddb
Dmytro Kovalyov <laykdimon278@gmail.com>
2023-03-29 15:52:25 +0300

    GROK-12548: Tutorials | Demo: Fixed charts demos

--- packages/Tutorials/src/demo-app/demo-app.ts
+++ packages/Tutorials/src/demo-app/demo-app.ts
@@ -1,7 +1,6 @@
 import * as DG from 'datagrok-api/dg';
 import * as ui from 'datagrok-api/ui';
 import * as grok from 'datagrok-api/grok';
-// import {filter} from "rxjs/operators";
 
 export class DemoView extends DG.ViewBase {
   dockPanel: DG.DockNode = new DG.DockNode(undefined);
@@ -10,7 +9,6 @@ export class DemoView extends DG.ViewBase {
   constructor() {
     super();
 
-    // this._initToolbox();
     this._initDockPanel();
     this._initContent();
   }
@@ -23,21 +21,6 @@ export class DemoView extends DG.ViewBase {
     this.root.appendChild(ui.divText('Select a demo from the toolbox on the right'));
   }
 
-  // _initToolbox() {
-    // for (const f of DG.Func.find({meta: {'demoPath': null}})) {
-    //   const pathOption = <string>f.options[DG.FUNC_OPTIONS.DEMO_PATH];
-    //   const path = pathOption.split('|').map((s) => s.trim());
-    //   const folder = this.tree.getOrCreateGroup(path.slice(0, path.length - 1).join(' | '));
-    //   const item = folder.item(path[path.length - 1]);
-    //   item.root.onmousedown = (_) => {
-    //     // grok.shell.closeAll();
-    //     f.apply().then((_) => { });
-    //   };
-    // }
-
-  //   this.toolbox = this.tree.root;
-  // }
-
   _initDockPanel() {
     for (const f of DG.Func.find({meta: {'demoPath': null}})) {
       const pathOption = <string>f.options[DG.FUNC_OPTIONS.DEMO_PATH];
@@ -49,7 +32,8 @@ export class DemoView extends DG.ViewBase {
         f.apply().then((_) => { });
       };
     }
-    this.dockPanel = grok.shell.dockManager.dock(ui.div([ui.searchInput('', ''), this.tree]), 'left', null, 'Categories');
+    this.dockPanel = grok.shell.dockManager.dock(ui.div(
+      [ui.searchInput('', ''), this.tree]), 'left', null, 'Categories');
     this.dockPanel.container.containerElement.style.maxWidth = '250px';
   }
 }
