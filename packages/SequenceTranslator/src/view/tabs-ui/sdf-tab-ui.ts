cb4ff0f3ba57b6d0fe47702cce91c40d74e89776
Alexey <achopovsky@datagrok.ai>
2023-04-04 19:22:56 +0300

    GROK-12481: Bio | ST: refactor tabs ui, WIP

--- packages/SequenceTranslator/src/view/tabs-ui/sdf-tab-ui.ts
+++ packages/SequenceTranslator/src/view/tabs-ui/sdf-tab-ui.ts
@@ -12,142 +12,145 @@ import {drawMolecule} from '../../utils/structures-works/draw-molecule';
 import {highlightInvalidSubsequence} from '../input-painters';
 import {getLinkedMolfile, saveSdf} from '../../sdf-tab/sdf-tab';
 import {ColoredTextInput} from '../../utils/colored-text-input';
+import {TabUI} from './tab-ui';
 
-export class SdfTabUI {
-  constructor() {
-    this._htmlDivElement = getSdfTab();
+export class SdfTabUI implements TabUI {
+  private constructor() { }
+
+  public static init() {
+    if (!SdfTabUI.instance)
+      SdfTabUI.instance = new SdfTabUI();
+    return SdfTabUI.instance;
   }
 
-  private _htmlDivElement;
+  private static instance: SdfTabUI;
 
-  get htmlDivElement() { return this._htmlDivElement; }
-}
+  get htmlDivElement(): HTMLDivElement {
+    const onInput: rxjs.Subject<string> = new rxjs.Subject<string>();
 
-/** UI of the SDF tab on the application's view */
-function getSdfTab(): HTMLDivElement {
-  const onInput: rxjs.Subject<string> = new rxjs.Subject<string>();
-
-  // default input values
-  const straight = '5′ → 3′';
-  const inverse = '3′ → 5′';
-  let invertSS = false;
-  let invertAS = false;
-  let invertAS2 = false;
-
-  // text inputs
-  const ssInputBase = ui.textInput('', '', () => { onInput.next(); });
-  const asInputBase = ui.textInput('', '', () => { onInput.next(); });
-  const as2InputBase = ui.textInput('', '', () => { onInput.next(); });
-
-  const ssColoredInput = new ColoredTextInput(ssInputBase, highlightInvalidSubsequence);
-  const asColoredInput = new ColoredTextInput(asInputBase, highlightInvalidSubsequence);
-  const as2ColoredInput = new ColoredTextInput(as2InputBase, highlightInvalidSubsequence);
-
-  // bool inputs
-  const saveEntity = ui.boolInput('Save as one entity', true);
-  ui.tooltip.bind(saveEntity.root, 'Save SDF with all strands in one molfile');
-  const useChiralInput = ui.boolInput('Use chiral', true);
-  // todo: compose tooltip message:
-  // ui.tooltip.bind(useChiralInput.root, '');
-
-  // choice inputs
-  const ssDirection = ui.choiceInput('SS direction', straight, [straight, inverse]);
-  ssDirection.onChanged(() => {
-    invertSS = ssDirection.value === inverse;
-    onInput.next();
-  });
-
-  const asDirection = ui.choiceInput('AS direction', straight, [straight, inverse]);
-  asDirection.onChanged(() => {
-    invertAS = asDirection.value === inverse;
-    onInput.next();
-  });
-
-  const as2Direction = ui.choiceInput('AS2 direction', straight, [straight, inverse]);
-  as2Direction.onChanged(() => {
-    invertAS2 = as2Direction.value === inverse;
-    onInput.next();
-  });
-
-  // labels
-  const ssLabel = ui.label('Sense Strand');
-  const asLabel = ui.label('Anti Sense');
-  const as2Label = ui.label('Anti Sense 2');
-
-  const tableLayout = ui.table(
-    ['ss', 'as1', 'as2'], (row) => {
-      switch (row) {
-      case 'ss':
-        return [ssLabel, ssColoredInput.root, ssDirection.root];
-      case 'as1':
-        return [asLabel, asColoredInput.root, asDirection.root];
-      case 'as2':
-        return [as2Label, as2ColoredInput.root, as2Direction.root];
-      }
-    }, ['', '', '']
-  );
-
-  // text input label style
-  for (const item of [ssLabel, asLabel, as2Label]) {
-    item.parentElement!.classList.add('sdf-input-form', 'sdf-text-input-label');
-    $(item.parentElement!).css('padding-top', '3px'); // otherwise overridden
-  }
-
-  // choice input label style
-  for (const item of [ssDirection.root, asDirection.root, as2Direction.root])
-    item.parentElement!.classList.add('sdf-input-form', 'sdf-choice-input-label');
-
-  for (const item of [ssInputBase, asInputBase, as2InputBase]) {
-    // text area's parent td
-    item.root.parentElement!.classList.add('sdf-text-input-td');
-  }
-
-  // molecule image container
-  const moleculeImgDiv = ui.block([]);
-  $(moleculeImgDiv).addClass('sdf-mol-img');
-
-  DG.debounce<string>(onInput, 300).subscribe(async () => {
-    let molfile = '';
-    try {
-      molfile = getLinkedMolfile(
-        {strand: ssInputBase.value.replace(/\s*/g, ''), invert: invertSS},
-        {strand: asInputBase.value.replace(/\s*/g, ''), invert: invertAS},
-        {strand: as2InputBase.value.replace(/\s*/g, ''), invert: invertAS2}, useChiralInput.value!
-      );
-    } catch (err) {
-      const errStr = errorToConsole(err);
-      console.error(errStr);
+    // default input values
+    const enum DIRECTION {
+      STRAIGHT = '5′ → 3′',
+      INVERSE = '3′ → 5′',
     }
-    // todo: calculate relative numbers
-    const canvasWidth = 650;
-    const canvasHeight = 150;
-    await drawMolecule(moleculeImgDiv, canvasWidth, canvasHeight, molfile);
-    // should the canvas be returned from the above function?
-    $(moleculeImgDiv).find('canvas').css('float', 'inherit');
-  });
+    let invertSS = false;
+    let invertAS = false;
+    let invertAS2 = false;
 
-  const saveButton = ui.buttonsInput([
-    ui.bigButton('Save SDF', () =>
-      saveSdf(
-        {strand: ssInputBase.value, invert: invertSS},
-        {strand: asInputBase.value, invert: invertAS},
-        {strand: as2InputBase.value, invert: invertAS2},
-        useChiralInput.value!,
-        saveEntity.value!
+    // text inputs
+    const ssInputBase = ui.textInput('', '', () => { onInput.next(); });
+    const asInputBase = ui.textInput('', '', () => { onInput.next(); });
+    const as2InputBase = ui.textInput('', '', () => { onInput.next(); });
+
+    const ssColoredInput = new ColoredTextInput(ssInputBase, highlightInvalidSubsequence);
+    const asColoredInput = new ColoredTextInput(asInputBase, highlightInvalidSubsequence);
+    const as2ColoredInput = new ColoredTextInput(as2InputBase, highlightInvalidSubsequence);
+
+    // bool inputs
+    const saveEntity = ui.boolInput('Save as one entity', true);
+    ui.tooltip.bind(saveEntity.root, 'Save SDF with all strands in one molfile');
+    const useChiralInput = ui.boolInput('Use chiral', true);
+    // todo: compose tooltip message:
+
+    // choice inputs
+    const ssDirection = ui.choiceInput('SS direction', DIRECTION.STRAIGHT, [DIRECTION.STRAIGHT, DIRECTION.INVERSE]);
+    ssDirection.onChanged(() => {
+      invertSS = ssDirection.value === DIRECTION.INVERSE;
+      onInput.next();
+    });
+
+    const asDirection = ui.choiceInput('AS direction', DIRECTION.STRAIGHT, [DIRECTION.STRAIGHT, DIRECTION.INVERSE]);
+    asDirection.onChanged(() => {
+      invertAS = asDirection.value === DIRECTION.INVERSE;
+      onInput.next();
+    });
+
+    const as2Direction = ui.choiceInput('AS2 direction', DIRECTION.STRAIGHT, [DIRECTION.STRAIGHT, DIRECTION.INVERSE]);
+    as2Direction.onChanged(() => {
+      invertAS2 = as2Direction.value === DIRECTION.INVERSE;
+      onInput.next();
+    });
+
+    // labels
+    const ssLabel = ui.label('Sense Strand');
+    const asLabel = ui.label('Anti Sense');
+    const as2Label = ui.label('Anti Sense 2');
+
+    const tableLayout = ui.table(
+      ['ss', 'as1', 'as2'], (row) => {
+        switch (row) {
+        case 'ss':
+          return [ssLabel, ssColoredInput.root, ssDirection.root];
+        case 'as1':
+          return [asLabel, asColoredInput.root, asDirection.root];
+        case 'as2':
+          return [as2Label, as2ColoredInput.root, as2Direction.root];
+        }
+      }, ['', '', '']
+    );
+
+    // text input label style
+    for (const item of [ssLabel, asLabel, as2Label]) {
+      item.parentElement!.classList.add('sdf-input-form', 'sdf-text-input-label');
+      $(item.parentElement!).css('padding-top', '3px'); // otherwise overridden
+    }
+
+    // choice input label style
+    for (const item of [ssDirection.root, asDirection.root, as2Direction.root])
+      item.parentElement!.classList.add('sdf-input-form', 'sdf-choice-input-label');
+
+    for (const item of [ssInputBase, asInputBase, as2InputBase]) {
+      // text area's parent td
+      item.root.parentElement!.classList.add('sdf-text-input-td');
+    }
+
+    // molecule image container
+    const moleculeImgDiv = ui.block([]);
+    $(moleculeImgDiv).addClass('sdf-mol-img');
+
+    DG.debounce<string>(onInput, 300).subscribe(async () => {
+      let molfile = '';
+      try {
+        molfile = getLinkedMolfile(
+          {strand: ssInputBase.value.replace(/\s*/g, ''), invert: invertSS},
+          {strand: asInputBase.value.replace(/\s*/g, ''), invert: invertAS},
+          {strand: as2InputBase.value.replace(/\s*/g, ''), invert: invertAS2}, useChiralInput.value!
+        );
+      } catch (err) {
+        const errStr = errorToConsole(err);
+        console.error(errStr);
+      }
+      // todo: calculate relative numbers
+      const canvasWidth = 650;
+      const canvasHeight = 150;
+      await drawMolecule(moleculeImgDiv, canvasWidth, canvasHeight, molfile);
+      // should the canvas be returned from the above function?
+      $(moleculeImgDiv).find('canvas').css('float', 'inherit');
+    });
+
+    const saveButton = ui.buttonsInput([
+      ui.bigButton('Save SDF', () =>
+        saveSdf(
+          {strand: ssInputBase.value, invert: invertSS},
+          {strand: asInputBase.value, invert: invertAS},
+          {strand: as2InputBase.value, invert: invertAS2},
+          useChiralInput.value!,
+          saveEntity.value!
+        )
       )
-    )
-  ]);
+    ]);
 
-  const boolInputsAndButtonArray = [saveEntity.root, useChiralInput.root, saveButton];
-  const boolInputsAndButton = ui.divV(boolInputsAndButtonArray);
-  for (const item of boolInputsAndButtonArray)
-    $(item).addClass('sdf-bool-button-block');
+    const boolInputsAndButtonArray = [saveEntity.root, useChiralInput.root, saveButton];
+    const boolInputsAndButton = ui.divV(boolInputsAndButtonArray);
+    for (const item of boolInputsAndButtonArray)
+      $(item).addClass('sdf-bool-button-block');
 
-  const bottomDiv = ui.divH([boolInputsAndButton, moleculeImgDiv]);
-  $(bottomDiv).addClass('sdf-bottom');
+    const bottomDiv = ui.divH([boolInputsAndButton, moleculeImgDiv]);
+    $(bottomDiv).addClass('sdf-bottom');
 
-  const sdfTabBody = ui.divV([tableLayout, bottomDiv]);
-  $(sdfTabBody).addClass('sdf-body');
+    const sdfTabBody = ui.divV([tableLayout, bottomDiv]);
+    $(sdfTabBody).addClass('sdf-body');
 
-  return sdfTabBody;
+    return sdfTabBody;
+  }
 }
