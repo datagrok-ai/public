7ae27efb405b92170ff79a1876a02bfa86bc6f58
PavloPolovyi <pasha32662@gmail.com>
2023-04-04 18:50:22 +0300

    GROK-12728: Grok Connect: Fixed provider

--- connectors/grok_connect/src/main/java/grok_connect/providers/MongoDbDataProvider.java
+++ connectors/grok_connect/src/main/java/grok_connect/providers/MongoDbDataProvider.java
@@ -1,18 +1,30 @@
 package grok_connect.providers;
 
+import grok_connect.connectors_info.DataConnection;
+import grok_connect.connectors_info.DataQuery;
+import grok_connect.connectors_info.DataSource;
+import grok_connect.connectors_info.DbCredentials;
+import grok_connect.connectors_info.FuncCall;
 import grok_connect.utils.GrokConnectException;
 import grok_connect.utils.ProviderManager;
 import grok_connect.utils.QueryCancelledByUser;
 import grok_connect.utils.SchemeInfo;
+import org.bson.Document;
+import serialization.BoolColumn;
+import serialization.Column;
+import serialization.DataFrame;
+import serialization.FloatColumn;
+import serialization.IntColumn;
+import serialization.StringColumn;
 import serialization.Types;
-import org.bson.*;
-
 import java.io.IOException;
-import java.sql.*;
-import java.util.*;
-import serialization.*;
-import grok_connect.connectors_info.*;
-
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 public class MongoDbDataProvider extends JdbcDataProvider {
     public MongoDbDataProvider(ProviderManager providerManager) {
@@ -27,11 +39,13 @@ public class MongoDbDataProvider extends JdbcDataProvider {
         descriptor.aggregations = null;
     }
 
+    @Override
     public String getConnectionStringImpl(DataConnection conn) {
         String port = (conn.getPort() == null) ? "" : ":" + conn.getPort();
         return "jdbc:mongodb://" + conn.getServer() + port + "/" + conn.getDb();
     }
 
+    @Override
     @SuppressWarnings("unchecked")
     public DataFrame execute(FuncCall queryRun)
             throws ClassNotFoundException, SQLException, GrokConnectException {
@@ -105,11 +119,13 @@ public class MongoDbDataProvider extends JdbcDataProvider {
         return dataFrame;
     }
 
+    @Override
     public ResultSet getResultSet(FuncCall queryRun, Connection connection) throws ClassNotFoundException, GrokConnectException, QueryCancelledByUser, SQLException {
         Statement statement = connection.createStatement();
         return statement.executeQuery(queryRun.func.query);
     }
 
+    @Override
     public SchemeInfo resultSetScheme(FuncCall queryRun, ResultSet resultSet) throws QueryCancelledByUser, SQLException {
         int columnCount = 0;
         List<Column> columns = new ArrayList<>(columnCount);
@@ -142,12 +158,15 @@ public class MongoDbDataProvider extends JdbcDataProvider {
         return new SchemeInfo(columns, null, null);
     }
 
+    @Override
     @SuppressWarnings("unchecked")
     public DataFrame getResultSetSubDf(FuncCall queryRun, ResultSet resultSet, List<Column> columns,
-                                    List<Boolean> supportedType,List<Boolean> initColumn, int maxIterations) 
-        throws IOException, SQLException, QueryCancelledByUser {
-        Document collection = (Document)resultSet.getObject(1);
-        while (resultSet != null && resultSet.next()) {
+                                    List<Boolean> supportedType,List<Boolean> initColumn, int maxIterations) throws SQLException {
+        do {
+            Document collection = (Document)resultSet.getObject(1);
+            if (collection == null) {
+                continue;
+            }
             for (Column column : columns) {
                 Object value = collection.get(column.name);
                 String colType = column.getType();
@@ -167,10 +186,9 @@ public class MongoDbDataProvider extends JdbcDataProvider {
                         column.add((value != null) ? value.toString() : "");
                 }
             }
-        }
+        } while (resultSet.next());
         DataFrame dataFrame = new DataFrame();
         dataFrame.addColumns(columns);
-
         return dataFrame;
     }
 }
