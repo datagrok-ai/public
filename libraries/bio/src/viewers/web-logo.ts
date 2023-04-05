36d84a5f07b0c65a949cfd56cf73309a93d8dca0
Aleksandr Tanas <atanas@datagrok.ai>
2023-04-05 13:07:05 +0400

    GROK-12692: MLB | JDOB | Bio: WebLogo VdRegions viewer for TPPs (#1762)
    Add tests ApiTests/DataFrame/join.inPlace
    Add VdRegion.sequenceColumnName attribute per region
    Fix for WebLogo constants and TAGS
    Add VdRegionsPropsDefault and WebLogoPropsDefaults

--- libraries/bio/src/viewers/web-logo.ts
+++ libraries/bio/src/viewers/web-logo.ts
@@ -1,8 +1,32 @@
 import * as grok from 'datagrok-api/grok';
 import * as ui from 'datagrok-api/ui';
 import * as DG from 'datagrok-api/dg';
+import {IViewer} from './viewer';
 
 export enum PositionHeight {
   Entropy = 'Entropy',
   full = '100%',
+}
+
+export class WebLogoPropsDefault {
+  // -- Data --
+  sequenceColumnName: string | null = null;
+  startPositionName: string | null = null;
+  endPositionName: string | null = null;
+  skipEmptySequences: boolean = true;
+  skipEmptyPositions: boolean = false;
+  shrinkEmptyTail: boolean = true;
+}
+
+export type WebLogoProps = Required<WebLogoPropsDefault>;
+
+export interface IWebLogoViewer extends WebLogoProps, IViewer {
+  // Some methods to control the viewer
+}
+
+export const positionSeparator: string = ', ';
+export const positionRe: RegExp = /(\d+)([A-Z]?)/;
+
+export enum TAGS {
+  positionNames = '.positionNames',
 }
