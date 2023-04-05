659397619af359ecbb309a46cd0419f228ef8b9d
Dmitry Illarionov <illarionow@yahoo.com>
2023-04-05 17:14:41 +0200

    Docusaurus: Careers page update

--- docusaurus/src/components/careers/index.jsx
+++ docusaurus/src/components/careers/index.jsx
@@ -3,13 +3,15 @@ import React from 'react';
 
 export function LayoutHeader ({meta}){
     return(
-        <div className="row">
-            <div className="row py-2 align-items-center">
-                <div className="col-lg-12 px-5">
-                    <div className='display-5 text-secondary pb-3'>{meta.title}</div>
-                    <p class="h6">{meta.description}</p>
+        <div className='row'>
+                <div className="col-lg-6 mb-12 px-5 pb-3">
+                    <div className='display-5 text-secondary'>{meta.title}</div>
+                    <h4 className='mt-2 pb-2'>{meta.description}</h4>
                 </div>
-            </div>
+                <div className="col-lg-6 mb-12 my-4 mx-auto">
+                    <iframe width="100%" height="280" className="rounded pt-2" src={meta.video.link}></iframe>
+                </div>
         </div>
+        
     );
 }
