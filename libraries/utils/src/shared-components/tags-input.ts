645387ea3ad21fb2737f84944ff96feb43be998f
Dmytro Kovalyov <laykdimon278@gmail.com>
2023-04-05 14:56:21 +0300

    GROK-12702: JS API: Added onTagAdded and onTagRemoved events

--- libraries/utils/src/shared-components/tags-input.ts
+++ libraries/utils/src/shared-components/tags-input.ts
@@ -1,22 +1,29 @@
 import * as DG from 'datagrok-api/dg';
 import * as ui from 'datagrok-api/ui';
 
-import {Observable, fromEvent} from 'rxjs';
-import {filter} from 'rxjs/operators';
+import {Observable, Subject} from 'rxjs';
 
 import '../../css/tags-input.css';
 
 
 export class TagsInput extends DG.InputBase {
-  tags: string[];
-  _tagsDiv: HTMLDivElement;
+  tags: string[] = [];
+  _tagsDiv: HTMLDivElement = ui.div();
+
+  _onTagAdded: Subject<string> = new Subject<string>();
+  _onTagRemoved: Subject<string> = new Subject<string>();
+
 
   constructor(tags: string[]) {
     const inputElement = ui.stringInput('', '');
     super(inputElement.dart);
 
-    const addTagBtn = ui.iconFA('plus', () => this.addTag((this.input as HTMLInputElement).value));
-    this.addOptions(addTagBtn);
+    this._init(tags);
+  }
+
+  _init(tags: string[]) {
+    const addTagIcon = ui.iconFA('plus', () => this.addTag((this.input as HTMLInputElement).value));
+    this.addOptions(addTagIcon);
 
     this.tags = tags;
     this._tagsDiv = ui.div(tags.map((tag) => { return this._createTag(tag); }), 'ui-tag-list');
@@ -40,31 +47,35 @@ export class TagsInput extends DG.InputBase {
     return currentTag;
   }
 
+  _isProper(tag: string): boolean {
+    return !(tag === '' || this.tags.includes(tag));
+  }
+
 
   addTag(tag: string) {
-    if (tag === '' || this.tags.includes(tag))
+    if (!this._isProper(tag))
       return;
 
     this.tags[this.tags.length] = tag;
     const currentTag = this._createTag(tag);
     this._tagsDiv.append(currentTag);
     (this.input as HTMLInputElement).value = '';
+    this._onTagAdded.next(tag);
   }
 
   removeTag(tag: string) {
     this.tags.splice(this.tags.indexOf(tag), 1);
     const currentTag = this._tagsDiv.querySelector(`[data-tag="${tag}"]`);
     this._tagsDiv.removeChild(currentTag!);
+    this._onTagRemoved.next(tag);
   }
 
-  // TODO: immplement onclick tag add and fix it
-  get onTagAdded(): Observable<KeyboardEvent> {
-    return fromEvent<KeyboardEvent>(this.input, 'keyup').pipe(filter((event: KeyboardEvent) => event.code === 'Enter'));
+
+  get onTagAdded(): Observable<string> {
+    return this._onTagAdded;
   }
 
-  // TODO: fix it
-  get onTagRemoved(): Observable<MouseEvent> {
-    const removeIcons = this._tagsDiv.getElementsByClassName('grok-icon fal fa-times');
-    return fromEvent<MouseEvent>(removeIcons, 'click');
+  get onTagRemoved(): Observable<string> {
+    return this._onTagRemoved;
   }
 }
