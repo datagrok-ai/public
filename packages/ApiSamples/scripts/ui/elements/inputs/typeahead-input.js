173ba7676a147213918c5e42016339f78f8b247c
Dmytro Kovalyov <laykdimon278@gmail.com>
2023-04-06 15:13:04 +0300

    GROK-12717: ApiSamples: Fixed TypeAhead sample

--- packages/ApiSamples/scripts/ui/elements/inputs/typeahead-input.js
+++ packages/ApiSamples/scripts/ui/elements/inputs/typeahead-input.js
@@ -1,4 +1,4 @@
-const typeAhead = ui.typeAhead({source: {local: ['USA', 'Ukraine', 'Antigua', 'United Kingdom', 'Universe']},
+const typeAhead = ui.typeAhead('Countries', {source: {local: ['USA', 'Ukraine', 'Antigua', 'United Kingdom', 'United Arab Emirates']},
     minLength: 2, limit: 3, hint: true, autoSelect: true, highlight: true, diacritics: true,
     onSubmit: (event, value) => console.log(`The value is ${value?.label}`), debounceRemote: 100});
 
