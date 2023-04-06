f57eec6d3fa88430c3147873b179349a80faf58f
Dmytro Kovalyov <laykdimon278@gmail.com>
2023-04-06 15:03:38 +0300

    GROK-12434: JS API: Removed unnecessary class and changed innerHTML to innerText

--- js-api/src/widgets.ts
+++ js-api/src/widgets.ts
@@ -1766,13 +1766,14 @@ export class Breadcrumbs {
     this.root = ui.div();
     this.path = path;
 
-    this.root = ui.divH(path.map((element) => ui.div(ui.link(element, () => {}, '',
-      `ui-breadcrumbs-text-element ${element}`), 'ui-breadcrumbs-element')), 'ui-breadcrumbs');
+    this.root = ui.divH(path.map((element) => ui.div(
+      ui.link(element, () => {}, '', 'ui-breadcrumbs-text-element'), 'ui-breadcrumbs-element')), 'ui-breadcrumbs');
 
     const rootElements = this.root.getElementsByClassName('ui-breadcrumbs-element');
     for (let i = 0; i < rootElements.length - 1; i++)
       rootElements[i].after(ui.iconFA('chevron-right'));
   }
+
 
   get onPathClick(): Observable<string[]> {
     const pathElements = this.root.getElementsByClassName('ui-breadcrumbs-text-element');
@@ -1780,7 +1781,7 @@ export class Breadcrumbs {
     return fromEvent<MouseEvent>(pathElements, 'click')
       .pipe(
         map((event) => {
-          const currentElement = (event.target as Element).innerHTML;
+          const currentElement = (event.target as HTMLElement).innerText;
           const currentPath = this.path.slice(0, this.path.indexOf(currentElement) + 1);
 
           return currentPath;
