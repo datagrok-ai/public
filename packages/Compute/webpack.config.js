4c2351340c0c19c709ec8cfdc7599d322e21fdf4
Aufar Zakiev <zaufar@gmail.com>
2023-04-07 15:16:06 +0300

    Compute: Added debugging options

--- packages/Compute/webpack.config.js
+++ packages/Compute/webpack.config.js
@@ -15,6 +15,11 @@ module.exports = {
         test: /\.css$/,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.js$/,
+        enforce: 'pre',
+        use: ['source-map-loader'],
+      },
     ],
   },
   devtool: 'source-map',
