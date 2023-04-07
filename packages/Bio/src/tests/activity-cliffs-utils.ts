bada0e8501596f3ce4321b50a4ecc1f1fc48dba9
tanas80@gmail.com <tanas80@gmail.com>
2023-04-08 00:51:28 +0400

    GROK-12136: Bio | Tools: Stability and polishing
    Fixing activityCliffs tests, WIP
    investigating _testActivityCliffsOpen
    perform grok.detectSemanticTypes
    test with other Bio package function call getMonomerLibHelper

--- packages/Bio/src/tests/activity-cliffs-utils.ts
+++ packages/Bio/src/tests/activity-cliffs-utils.ts
@@ -2,16 +2,20 @@ import * as DG from 'datagrok-api/dg';
 import * as grok from 'datagrok-api/grok';
 
 import {delay, expect} from '@datagrok-libraries/utils/src/test';
+import {IMonomerLibHelper} from '@datagrok-libraries/bio/src/monomer-works/monomer-utils';
 
 export async function _testActivityCliffsOpen(df: DG.DataFrame, numberCliffs: number, method: string, colName: string) {
   await grok.data.detectSemanticTypes(df);
   // const scatterPlot = await activityCliffs(
   //   df, df.col(colName)!, df.col('Activity')!,
   //   50, method);
-  const scatterPlot = (await grok.functions.call('Bio:activityCliffs', {
-    table: df, molecules: df.getCol(colName), activities: df.getCol('Activity'),
-    similarity: 50, methodName: method
-  })) as DG.Viewer | undefined;
+  // const scatterPlot = (await grok.functions.call('Bio:activityCliffs', {
+  //   table: df, molecules: df.getCol(colName), activities: df.getCol('Activity'),
+  //   similarity: 50, methodName: method
+  // })) as DG.Viewer | undefined;
+  const libHelper: IMonomerLibHelper = (await grok.functions.call('Bio:getMonomerLibHelper'));
+  const k = 11;
+
   // expect(scatterPlot != null, true);
   //
   // const cliffsLink = Array.from(scatterPlot!.root.children).find((el) => {
