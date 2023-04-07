8ff7cd12b4fb0acf933daaf2ff7ab8f79161dcb7
tanas80@gmail.com <tanas80@gmail.com>
2023-04-07 19:30:00 +0400

    GROK-12136: Bio | Tools: Stability and polishing
    Enable second half and detectors of tests for github CI

--- packages/Bio/src/package-test.ts
+++ packages/Bio/src/package-test.ts
@@ -5,7 +5,7 @@ import {runTests, TestContext, tests} from '@datagrok-libraries/utils/src/test';
 import './tests/Palettes-test';
 
 // first quarter
-// import './tests/detectors-tests';
+import './tests/detectors-tests';
 // import './tests/detectors-benchmark-tests';
 // import './tests/msa-tests';
 // import './tests/sequence-space-test';
