84aad52b95d1f613f19753eade32e4e14cdeda82
Dmytro Kovalyov <laykdimon278@gmail.com>
2023-04-07 12:54:17 +0300

    GROK-12702: JS API: Added getTags and setTags methods

--- libraries/utils/src/shared-components/tags-input.ts
+++ libraries/utils/src/shared-components/tags-input.ts
@@ -5,13 +5,14 @@ import {Observable, Subject} from 'rxjs';
 
 import '../../css/tags-input.css';
 
+
 // TODO: make setter and getter for tags (just the getTags and setTags methods and make root private)
 export class TagsInput extends DG.InputBase {
-  tags: string[] = [];
-  _tagsDiv: HTMLDivElement = ui.div();
+  private _tags: string[] = [];
+  private _tagsDiv: HTMLDivElement = ui.div();
 
-  _onTagAdded: Subject<string> = new Subject<string>();
-  _onTagRemoved: Subject<string> = new Subject<string>();
+  private _onTagAdded: Subject<string> = new Subject<string>();
+  private _onTagRemoved: Subject<string> = new Subject<string>();
 
 
   constructor(name: string, tags: string[], showBtn: boolean) {
@@ -21,27 +22,27 @@ export class TagsInput extends DG.InputBase {
     this._init(tags, showBtn);
   }
 
-  _init(tags: string[], showBtn: boolean) {
+  private _init(tags: string[], showBtn: boolean) {
     if (showBtn) {
       const addTagIcon = ui.iconFA('plus', () => this.addTag((this.input as HTMLInputElement).value));
       this.addOptions(addTagIcon);
     }
 
-    this.tags = tags;
+    this._tags = tags;
     this._tagsDiv = ui.div(tags.map((tag) => { return this._createTag(tag); }), 'ui-tag-list');
     this.root.append(this._tagsDiv);
 
     this._initEventListeners();
   }
 
-  _initEventListeners() {
+  private _initEventListeners() {
     this.input.addEventListener('keyup', (event: KeyboardEvent) => {
       if (event.code === 'Enter')
         this.addTag((this.input as HTMLInputElement).value);
     });
   }
 
-  _createTag(tag: string): HTMLElement {
+  private _createTag(tag: string): HTMLElement {
     const icon = ui.iconFA('times', () => this.removeTag(tag));
     const currentTag = ui.span([ui.span([tag]), icon], `ui-tag`);
     currentTag.dataset.tag = tag;
@@ -49,8 +50,8 @@ export class TagsInput extends DG.InputBase {
     return currentTag;
   }
 
-  _isProper(tag: string): boolean {
-    return !(tag === '' || this.tags.includes(tag));
+  private _isProper(tag: string): boolean {
+    return !(tag === '' || this._tags.includes(tag));
   }
 
 
@@ -58,7 +59,7 @@ export class TagsInput extends DG.InputBase {
     if (!this._isProper(tag))
       return;
 
-    this.tags[this.tags.length] = tag;
+    this._tags[this._tags.length] = tag;
     const currentTag = this._createTag(tag);
     this._tagsDiv.append(currentTag);
     (this.input as HTMLInputElement).value = '';
@@ -66,10 +67,21 @@ export class TagsInput extends DG.InputBase {
   }
 
   removeTag(tag: string) {
-    this.tags.splice(this.tags.indexOf(tag), 1);
+    this._tags.splice(this._tags.indexOf(tag), 1);
     const currentTag = this._tagsDiv.querySelector(`[data-tag="${tag}"]`);
     this._tagsDiv.removeChild(currentTag!);
     this._onTagRemoved.next(tag);
+  }
+
+  getTags(): string[] {
+    return this._tags;
+  }
+
+  setTags(tags: string[]) {
+    this._tags = tags;
+    this._tagsDiv = ui.div(tags.map((tag) => { return this._createTag(tag); }), 'ui-tag-list');
+    const currentTagsDiv = this.root.getElementsByClassName('ui-tag-list')[0];
+    currentTagsDiv.replaceWith(this._tagsDiv);
   }
 
 
