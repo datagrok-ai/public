8c312e89321734d0591fb5fcf25d0121fd8239aa
tanas80@gmail.com <tanas80@gmail.com>
2023-04-08 01:13:09 +0400

    GROK-12136: Bio | Tools: Stability and polishing
    Fixing activityCliffs tests, WIP
    investigating _testActivityCliffsOpen
    perform grok.detectSemanticTypes
    test with scatter plot without activityCliffs

--- packages/Bio/src/tests/activity-cliffs-utils.ts
+++ packages/Bio/src/tests/activity-cliffs-utils.ts
@@ -13,7 +13,8 @@ export async function _testActivityCliffsOpen(df: DG.DataFrame, numberCliffs: nu
   //   table: df, molecules: df.getCol(colName), activities: df.getCol('Activity'),
   //   similarity: 50, methodName: method
   // })) as DG.Viewer | undefined;
-  const libHelper: IMonomerLibHelper = (await grok.functions.call('Bio:getMonomerLibHelper'));
+  const scatterPlot = (await df.plot.fromType(DG.VIEWER.SCATTER_PLOT, {})) as DG.Viewer;
+  // const libHelper: IMonomerLibHelper = (await grok.functions.call('Bio:getMonomerLibHelper'));
   const k = 11;
 
   // expect(scatterPlot != null, true);
