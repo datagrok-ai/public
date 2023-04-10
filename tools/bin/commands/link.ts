dde95502eb9ff3c684a89477f8b5760307177a05
onuf <56073962+onuf@users.noreply.github.com>
2023-04-10 19:56:54 +0200

    GROK-12785 Tools: Rework `grok link`

--- tools/bin/commands/link.ts
+++ tools/bin/commands/link.ts
@@ -4,26 +4,25 @@ import { exec } from 'child_process';
 import * as utils from '../utils/utils';
 import * as color from '../utils/color-utils';
 
-/**
- * Link: api > utils > other libs - npm install, npm run build, npm link
- * Unlink: npm install in the package and its dependencies, npm unlink
- */
 
 const curDir = process.cwd();
-const packageDir = path.join(curDir, 'package.json');
+const repositoryDir = path.dirname(path.dirname(curDir));
 
 const apiPackageName = 'datagrok-api';
 const libScope = '@datagrok-libraries';
 const paths = {
-  [apiPackageName]: path.join(path.dirname(path.dirname(curDir)), 'js-api'),
-  [libScope]: path.join(path.dirname(path.dirname(curDir)), 'libraries'),
+  [apiPackageName]: path.join(repositoryDir, 'js-api'),
+  [libScope]: path.join(repositoryDir, 'libraries'),
 };
+
+const packageDependencies: {[packagePath: string]: utils.Indexable} = {};
 
 /** Links local packages. */
 export function link(args: LinkArgs) {
   const nOptions = Object.keys(args).length - 1;
-  if (nOptions > 0 || args['_'].length > 1)
+  if (nOptions > 1 || args['_'].length > 1 || (nOptions === 1 && !args.local && !args.npm))
     return false;
+  const local = args.npm ? false : true;  // args.local is default
 
   if (!utils.isPackageDir(curDir)) {
     color.error('File `package.json` not found. Run the command from the package directory');
@@ -37,132 +36,150 @@ export function link(args: LinkArgs) {
     }
   }
 
-  const dependencies = readDependencies(JSON.parse(fs.readFileSync(packageDir, 'utf-8')));
-  const modulesDir = path.join(curDir, 'node_modules');
-  if (!fs.existsSync(modulesDir)) {
-    console.log('Running `npm install` to get the required dependencies...\n');
-    exec('npm install', (err, stdout, stderr) => {
-      if (err) throw err;
-      else console.log(stderr, stdout);
+  const dependencies = readDependencies(curDir);
+  const packageHierarchy: string[] = getHierarchy(curDir);
+
+  if (local) {
+    // Install dependencies
+    for (const packageName of packageHierarchy) {
+      const packagePath = getPackagePath(packageName);
+      const packageJsonPath = path.join(packagePath, 'package.json');
+      const json = JSON.parse(fs.readFileSync(packageJsonPath, 'utf-8'));
+      json.scripts['link-all'] = generateLinkScript(packagePath, packageHierarchy);
+      json.scripts['build-all'] = generateBuildScript(packagePath, packageHierarchy);
+      console.log(json.scripts['link-all']);
+      console.log(json.scripts['build-all']);
+      // fs.writeFileSync(packageJsonPath, JSON.stringify(json, null, 2), 'utf-8');
+      // TODO: Run link-all, optionally run build-all
+      // exec('npm install && npm link', { cwd: packagePath }, (err, stdout, stderr) => {
+      //   if (err) throw err;
+      //   else console.log(stderr, stdout);
+      // });
+    }
+    // TODO: Add/update package scripts (link-all, build-all) for the main package
+    // Run link-all, optionally run build-all
+    // runScript(curDir, 'npm update && npm link', dependencies, {
+    //   dirMessage: 'Installing dependencies for ',
+    //   successMessage: 'Dependencies have been successfully installed.',
+    // });
+  } else {
+    runScript(curDir, 'npm install', dependencies, {
+      dirMessage: 'Unlinking local packages in ',
+      successMessage: 'Local packages have been successfully unlinked.',
     });
   }
-  // The order should start with js-api, then libraries/utils, then other libraries
-  let apiModule = null;
-  let hasUtils = false;
-  let libs: fs.Dirent[] = [];
-  const allModules = fs.readdirSync(modulesDir, {encoding: 'utf-8', withFileTypes: true});
-  for (const m of allModules) {
-    if (m.name === apiPackageName)
-      apiModule = m;
-    else if (m.name === libScope) {
-      libs = fs.readdirSync(path.join(modulesDir, m.name), {encoding: 'utf-8', withFileTypes: true});
-      for (const lib of libs) {
-        if (lib.name === 'utils')
-          hasUtils = true;
-      }
-    }
-  }
-
-  function installLibs(libs: fs.Dirent[]) {
-    if (!libs.length)
-      return;
-    const f = () => {
-      for (const lib of libs) {
-        if (`${libScope}/${lib.name}` in dependencies) {
-          const libPath = path.join(paths[libScope], lib.name);
-          const isLinked = lib.isSymbolicLink();
-          console.log(isLinked ?
-            `Package "${lib.name}" is linked. Updating dependencies and running the build script...`
-            : `Linking "${lib.name}"...`);
-          exec(isLinked ? 'npm update && npm run build' : 'npm install && npm run link-all && npm run build && npm link',
-            { cwd: libPath }, (err, stdout, stderr) => {
-              if (err) throw err;
-              else console.log(stderr, stdout);
-            });
-        }
-      }
-    };
-    if (hasUtils) {
-      const utilsModule = libs.splice(libs.findIndex((m) => m.name === 'utils'), 1)[0];
-      const libPath = path.join(paths[libScope], 'utils');
-      const isLinked = utilsModule.isSymbolicLink();
-      console.log(isLinked ?
-        `Package "${utilsModule.name}" is linked. Updating dependencies and running the build script...`
-        : `Linking "${utilsModule.name}"...`);
-      exec(isLinked ? 'npm update && npm run build' : 'npm install && npm run link-all && npm run build && npm link',
-        { cwd: libPath }, (err, stdout, stderr) => {
-          if (err) throw err;
-          else {
-            console.log(stderr, stdout);
-            f();
-          }
-        });
-    } else {
-      f();
-    }
-  }
-
-  if (apiModule != null) {
-    const isLinked = apiModule.isSymbolicLink();
-    console.log(isLinked ?
-      `Package "${apiModule.name}" is linked. Updating dependencies and running the build script...`
-      : `Linking "${apiModule.name}"...`);
-    exec(isLinked ? `npm update && npm run build` : `npm install && npm run build && npm link`,
-      { cwd: paths[apiPackageName] }, (err, stdout, stderr) => {
-        if (err) throw err;
-        else {
-          console.log(stderr, stdout);
-          installLibs(libs);
-          const packageNames = Object.keys(dependencies).join(' ');
-          exec(`npm link ${packageNames}`);
-        }
-      });
-    }
 
   return true;
 }
 
-/** Unlinks local packages and runs `npm i`. */
-export function unlink(args: LinkArgs) {
-  const nOptions = Object.keys(args).length - 1;
-  if (nOptions > 0 || args['_'].length > 1)
-    return false;
-
-  if (!utils.isPackageDir(curDir)) {
-    color.error('File `package.json` not found. Run the command from the package directory');
-    return false;
-  }
-
-  const dependencies = readDependencies(JSON.parse(fs.readFileSync(packageDir, 'utf-8')));
-  const packageNames = Object.keys(dependencies).join(' '); // npm unlink ${packageNames}
-  exec(`npm install`, (err, stdout, stderr) => {
-    console.log(`Unlinking local packages in ${path.basename(curDir)}`);
-    if (err) throw err;
-    else {
-      console.log(stderr, stdout);
-      for (const dep in dependencies) {
-        const depPath = dep === apiPackageName ? paths[dep] : path.join(paths[libScope], dep.split('/')[1]);
-        exec(`npm install`, {cwd: depPath}, (err, stdout, stderr) => {
-          console.log(`Unlinking local packages in ${path.basename(depPath)}`);
-          if (err) throw err;
-          else console.log(stderr, stdout);
-        });
-      }
-    }
-  });
-
-  return true;
-}
-
-function readDependencies(json: utils.Indexable): utils.Indexable {
+function readDependencies(packagePath: string): utils.Indexable {
+  if (packagePath in packageDependencies)
+    return packageDependencies[packagePath];
+  const fileContent = fs.readFileSync(path.join(packagePath, 'package.json'), 'utf-8');
+  const json = JSON.parse(fileContent);
   const libs: utils.Indexable = {};
   for (let dep in json.dependencies) {
     if (dep === apiPackageName || dep.startsWith(`${libScope}/`))
       libs[dep] = json.dependencies[dep];
   }
+  packageDependencies[packagePath] = libs;
   return libs;
+}
+
+function getPackagePath(packageName: string): string {
+  return packageName === apiPackageName ? paths[packageName] : path.join(paths[libScope], packageName.split('/')[1]);
+}
+
+/** Forms a hierarchy to understand in which order packages should be linked. */
+function getHierarchy(packageDir: string): string[] {
+  const hierarchy: string[] = [];
+  const dependencies = Object.keys(readDependencies(packageDir));
+  const cachedHierarchy: {[path: string]: string[]} = {};
+  for (const dep of dependencies) {
+    let idx = hierarchy.indexOf(dep);
+    if (idx === -1)
+      idx = hierarchy.push(dep) - 1;
+    const depPath = getPackagePath(dep);
+    const internalHierarchy = cachedHierarchy[depPath] ?? getHierarchy(depPath);
+    if (!(depPath in cachedHierarchy))
+      cachedHierarchy[depPath] = internalHierarchy;
+    for (const internalDep of internalHierarchy) {
+      const depIdx = hierarchy.indexOf(internalDep);
+      if (depIdx === -1) {
+        // Insert the internal dependency before the main package
+        hierarchy.splice(idx, 0, internalDep);
+        idx++;
+      } else if (depIdx > idx) {
+        // Remove the internal dependency from the list and place it before the main package
+        // (doesn't affect the order of same-level libraries, their order is arbitrary)
+        hierarchy.splice(depIdx, 1);
+        idx = hierarchy.indexOf(dep);
+        hierarchy.splice(idx, 0, internalDep);
+        idx++;
+      }
+    }
+  }
+  return hierarchy;
+}
+
+/** Executes a script for a package and its dependencies with messages on the current progress. */
+function runScript(packageDir: string, script: string, dependencies: utils.Indexable, options:
+  {dirMessage?: string, successMessage?: string, callback?: (dir: string) => void} = {}) {
+  exec(script, {cwd: packageDir}, (err, stdout, stderr) => {
+    if (options.dirMessage)
+      console.log(`${options.dirMessage}${path.basename(packageDir)}`);
+    if (options.callback)
+      options.callback(packageDir);
+    if (err) throw err;
+    else {
+      console.log(stderr, stdout);
+      for(const dep in dependencies) {
+        const depPath = getPackagePath(dep);
+        exec(script, {cwd: depPath}, (err, stdout, stderr) => {
+          if (options.dirMessage)
+            console.log(`${options.dirMessage}${path.basename(depPath)}`);
+          if (options.callback)
+            options.callback(depPath);
+          if (err) throw err;
+          else console.log(stderr, stdout);
+        });
+      }
+      // if (options.successMessage)
+      //   setTimeout(() => color.success(options.successMessage!), 5000);
+    }
+  });
+}
+
+/** Generates a package script to build all dependencies using the provided hierarchy. */
+function generateBuildScript(packagePath: string, hierarchy: string[]): string {
+  const dependencies = Object.keys(readDependencies(packagePath));
+  const packageNames = hierarchy.filter((p) => dependencies.includes(p));
+  const prefix = './../../'; // TODO: adjust programmatically (for js-api, libraries/, packages/)
+  let script = '';
+  for (const dep of dependencies) {
+    if (!packageNames.includes(dep))
+      color.error(`Hierarchy does not include package ${dep}`);
+    else
+      script += `npm --prefix ${prefix}${dep === apiPackageName ?
+        'js-api' : `libraries/${dep.split('/')[1]}`} run build`;
+  }
+  return `${script ? (script + ' && ') : ''}npm run build`;
+}
+
+/** Generates a package script to link all dependencies using the provided hierarchy. */
+function generateLinkScript(packagePath: string, hierarchy: string[]): string {
+  const dependencies = Object.keys(readDependencies(packagePath));
+  const packageNames = hierarchy.filter((p) => dependencies.includes(p));
+  for (const dep of dependencies) {
+    if (!packageNames.includes(dep))
+      color.error(`Hierarchy does not include package ${dep}`);
+  }
+  let script = `npm link${packageNames.length ? (' ' + packageNames.join(' ')) : ''}`;
+  return script;
 }
 
 interface LinkArgs {
   _: string[],
+  local?: boolean,
+  npm?: boolean,
 }
