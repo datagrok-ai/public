9ec5e0caf185370f6570c0caf6b03aaae1eea202
Dmytro Kovalyov <laykdimon278@gmail.com>
2023-04-10 14:50:30 +0300

    GROK-12548: Tutorials | Demo: Fixed routing problem

--- packages/Charts/src/demos/demo.ts
+++ packages/Charts/src/demos/demo.ts
@@ -20,16 +20,13 @@ const VIEWER_TABLES_PATH: {[key: string]: string} = {
 
 export async function viewerDemo(viewerName: string, options?: object | null) {
   const df = await (['GroupAnalysis', 'SurfacePlot', 'Timelines'].includes(viewerName) ?
-    grok.data.loadTable(`${_package.webRoot}${VIEWER_TABLES_PATH[viewerName]}`)
-    : grok.data.getDemoTable(VIEWER_TABLES_PATH[viewerName]));
+    grok.data.loadTable(`${_package.webRoot}${VIEWER_TABLES_PATH[viewerName]}`) :
+    grok.data.getDemoTable(VIEWER_TABLES_PATH[viewerName]));
 
-  const tableView = DG.TableView.create(df, false);
-  tableView.basePath = `/apps/Tutorials/Demo/Viewers/${viewerName}`;
-  grok.shell.addTable(df);
-  grok.shell.addView(tableView);
+  const tableView = grok.shell.addTableView(df);
 
   if (['Globe', 'GroupAnalysis'].includes(viewerName)) {
-    DG.debounce(df.onSemanticTypeDetected, 300).subscribe((_) => tableView.addViewer(viewerName, options));
+    DG.debounce(df.onSemanticTypeDetected, 800).subscribe((_) => tableView.addViewer(viewerName, options));
     return;
   }
 
