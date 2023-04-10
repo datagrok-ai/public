d571bc6b4c03ce092cd639b8928f432bf781df84
Alex Paramonov <alex.aprm@gmail.com>
2023-04-10 20:51:31 +0100

    UsageAnalysis: Overview tab and refactoring

--- packages/UsageAnalysis/queries/widget.sql
+++ packages/UsageAnalysis/queries/widget.sql
@@ -1,12 +1,23 @@
---name: WidgetData
+--name:UniqueUsersSummary
 --meta.cache: true
+--meta.invalidate: 0 * * * *
 --connection: System:Datagrok
-select COALESCE(u.name, 'System') as name,
-to_timestamp(floor((extract('epoch' from e.event_time) / 3600 )) * 3600)
-AT TIME ZONE 'UTC' as time, count(*)
-from events e
-inner join users_sessions s on e.session_id = s.id
-inner join users u on u.id = s.user_id
-where e.event_time::TIMESTAMP between 'now'::TIMESTAMP - interval '7 day' and 'now'::TIMESTAMP
-group by time, u.name
+select date(e.event_time) as date, count(distinct u.id)
+	from events e
+	inner join users_sessions s on e.session_id = s.id
+	inner join users u on u.id = s.user_id
+	WHERE e.event_time > (current_timestamp - '60 day'::interval)
+group by date(e.event_time)
+--end
+
+--name:UsersEventsSummary
+--meta.cache: true
+--meta.invalidate: 0 * * * *
+--connection: System:Datagrok
+select date(e.event_time) as date,  count(e.id)
+	from events e
+	inner join users_sessions s on e.session_id = s.id
+	inner join users u on u.id = s.user_id
+	WHERE e.event_time > (current_timestamp - '60 day'::interval)
+group by date(e.event_time)
 --end
--- packages/UsageAnalysis/src/viewers/abstract/ua-query-viewer.ts
+++ packages/UsageAnalysis/src/viewers/abstract/ua-query-viewer.ts
@@ -13,23 +13,22 @@ export abstract class UaQueryViewer extends UaViewer {
   viewerFunction: Function;
   staticFilter: Object = {};
   filter: Object = {};
-  viewer: DG.Viewer;
+  viewer: DG.Viewer | undefined;
 
   protected constructor(name: string, queryName: string, viewerFunction: Function,
-    setStyle?: Function | null, staticFilter?: Object | null, filter?: UaFilter | null, viewer?: DG.Viewer) {
+    setStyle?: Function | null, staticFilter?: Object | null, filter?: UaFilter | null) {
     super(name, setStyle);
     this.queryName = queryName;
     this.viewerFunction = viewerFunction;
     // if (staticFilter) this.staticFilter = staticFilter;
     if (filter) this.filter = filter;
-    this.viewer = viewer as DG.Viewer;
-    this.root.appendChild(this.viewer.root);
     this.root.appendChild(this.loader);
   }
 
   reloadViewer(staticFilter?: object) {
     this.loader.style.display = 'block';
-    this.viewer.root.style.display = 'none';
+    if (this.viewer !== undefined)
+      this.viewer.root.style.display = 'none';
     const filter = {...this.filter, ...staticFilter};
     grok.data.query('UsageAnalysis:' + this.queryName, filter).then((dataFrame) => {
       if (dataFrame.columns.byName('count') != null)
@@ -40,10 +39,10 @@ export abstract class UaQueryViewer extends UaViewer {
         userColumn.categories.forEach((u: string) => {users[u] = colorHash.hex(u);});
         userColumn.meta.colors.setCategorical(users);
       }
-      this.viewerFunction(dataFrame);
-      this.viewer.dataFrame = dataFrame;
-      this.viewer.setOptions({markerMinSize: 10, markerMaxSize: 30, color: 'user'});
-      this.viewer.root.style.display = 'flex';
+      this.viewer = this.viewerFunction(dataFrame);
+      this.root.appendChild(this.viewer!.root);
+      this.viewer!.dataFrame = dataFrame;
+      this.viewer!.root.style.display = 'flex';
       this.loader.style.display = 'none';
     });
   }
