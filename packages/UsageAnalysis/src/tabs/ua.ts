64fef17eb189eae27f9a5a0ec3ba69cf2cf76920
Anna Muzychyna <annamuz69@gmail.com>
2023-04-13 09:45:45 +0300

    GROK-12109 UsageAnalysis: Events tab

--- packages/UsageAnalysis/src/tabs/ua.ts
+++ packages/UsageAnalysis/src/tabs/ua.ts
@@ -27,18 +27,18 @@ export class UaView extends DG.ViewBase {
     this.box = true;
   }
 
-  tryToinitViewers() {
+  tryToinitViewers(): void {
     if (!this.initialized) {
       this.initialized = true;
       this.initViewers();
     }
   }
 
-  getScatterPlot() {
+  getScatterPlot(): UaQueryViewer {
     return this.viewers[0];
   }
 
-  getTime(date: Date, format: string = 'en-GB') {
+  getTime(date: Date, format: string = 'en-GB'): string {
     return date.toLocaleString(format, {hour12: false}).replace(',', '');
   }
 
