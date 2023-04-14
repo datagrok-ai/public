1f9cfe388350525ea09efd1df42fbe6ea17845e7
Dmitry Illarionov <illarionow@yahoo.com>
2023-04-14 15:26:02 +0200

    Charts: Added viewers to dockManager

--- packages/Charts/src/demos/demo.ts
+++ packages/Charts/src/demos/demo.ts
@@ -35,8 +35,11 @@ export async function viewerDemo(viewerName: string, options?: object | null) {
     return;
   }
 
-  const viewer = tableView.addViewer(viewerName, options);
-  tableView.dockManager.dock(viewer, 'up', null, viewerName, 0.75);
-  tableView.filters();
+  let rootNode = tableView.dockManager.rootNode;
+  let node1 = tableView.dockManager.dock(tableView.addViewer('scatterplot', options), 'right', rootNode, 'Scatter plot', 0.5);
+  let node2 = tableView.dockManager.dock(tableView.addViewer('histogram', options), 'right', node1, 'Histogram', 0.3);
+  let node3 = tableView.dockManager.dock(tableView.addViewer(viewerName, options), 'up', null, viewerName, 0.7);
+  tableView.dockManager.dock(tableView.filters(), 'left', node3, 'Filters', 0.3);
+
   //TODO: set grid instead of null, 'up' -> 'right' (for histogram and scatterplot)
 }
