234e4bb5b9175fd14363d967b80e0998555648ab
PavloPolovyi <pasha32662@gmail.com>
2023-04-17 19:38:17 +0300

    GROK-12870: Grok Connect: Created queries

---
+++ connectors/grok_connect/src/test/resources/scripts/teradata/init.sql
@@ -0,0 +1,117 @@
+CREATE TABLE TEST.MOCK_DATA (
+                           id NUMBER(18),
+                           first_name VARCHAR2(50),
+                           last_name VARCHAR2(50),
+                           email VARCHAR2(50),
+                           gender VARCHAR2(50),
+                           ip_address VARCHAR2(50),
+                           country VARCHAR2(50),
+                           dat DATE,
+                           some_number NUMBER(5,2)
+);
+
+INSERT INTO TEST.MOCK_DATA (id, first_name, last_name, email, gender, ip_address, country, dat, some_number) VALUES (1, 'Burk', 'Kemery', 'bkemery0@businesswire.com', 'Male', '249.64.22.121/32',  'China', '2017-09-20',  510.32);
+INSERT INTO TEST.MOCK_DATA (id, first_name, last_name, email, gender, ip_address, country, dat, some_number) VALUES (2, 'Nicholle', 'Karoly', 'nkaroly1@alexa.com', 'Female', '255.233.247.118/32',  'Poland', '2014-02-27',  864.09);
+INSERT INTO TEST.MOCK_DATA (id, first_name, last_name, email, gender, ip_address, country, dat, some_number) VALUES (3, 'Orlando', 'Westgate', 'owestgate2@dedecms.com', 'Polygender', '75.0.252.254/32',  'Netherlands', '2020-09-03',  822.7);
+INSERT INTO TEST.MOCK_DATA (id, first_name, last_name, email, gender, ip_address, country, dat, some_number) VALUES (4, 'Gothart', 'Cokayne', 'gcokayne3@plala.or.jp', 'Male', '196.83.12.163/32',  'Philippines', '2001-01-31',  251.05);
+INSERT INTO TEST.MOCK_DATA (id, first_name, last_name, email, gender, ip_address, country, dat, some_number) VALUES (5, 'Mitchell', 'Haglington', 'mhaglington4@indiegogo.com', 'Male', '209.93.181.190/32',  'Poland', '2020-10-09',  15.22);
+INSERT INTO TEST.MOCK_DATA (id, first_name, last_name, email, gender, ip_address, country, dat, some_number) VALUES (6, 'Jeromy', 'Twinn', 'jtwinn5@globo.com', 'Male', '25.13.2.132/32',  'Serbia', '2014-10-04',  378.4);
+INSERT INTO TEST.MOCK_DATA (id, first_name, last_name, email, gender, ip_address, country, dat, some_number) VALUES (7, 'Joela', 'Cornau', 'jcornau6@imgur.com', 'Female', '195.47.88.236/32',  'Indonesia', '2020-03-19',  349.11);
+INSERT INTO TEST.MOCK_DATA (id, first_name, last_name, email, gender, ip_address, country, dat, some_number) VALUES (8, 'Darren', 'Juares', 'djuares7@hexun.com', 'Male', '94.170.16.96/32',  'China', '2011-04-09',  631.89);
+INSERT INTO TEST.MOCK_DATA (id, first_name, last_name, email, gender, ip_address, country, dat, some_number) VALUES (9, 'Marlie', 'Mayze', 'mmayze8@google.com.au', 'Female', '68.41.25.65/32',  'France', '2011-11-10',  561.72);
+INSERT INTO TEST.MOCK_DATA (id, first_name, last_name, email, gender, ip_address, country, dat, some_number) VALUES (10, 'Scottie', 'Formilli', 'sformilli9@aol.com', 'Male', '101.241.191.228/32',  'Vietnam', '2003-01-04',  978.01);
+INSERT INTO TEST.MOCK_DATA (id, first_name, last_name, email, gender, ip_address, country, dat, some_number) VALUES (11, 'Lenci', 'Simecek', 'lsimeceka@cmu.edu', 'Agender', '252.190.171.190/32',  'Jamaica', '2001-09-23',  607.93);
+INSERT INTO TEST.MOCK_DATA (id, first_name, last_name, email, gender, ip_address, country, dat, some_number) VALUES (12, 'Faye', 'Elix', 'felixb@hatena.ne.jp', 'Female', '185.52.22.155/32',  'Peru', '2022-11-11',  972.79);
+INSERT INTO TEST.MOCK_DATA (id, first_name, last_name, email, gender, ip_address, country, dat, some_number) VALUES (13, 'Pail', 'Boxell', 'pboxellc@moonfruit.com', 'Genderqueer', '2.37.160.155/32',  'Indonesia', '2012-01-14',  73.47);
+INSERT INTO TEST.MOCK_DATA (id, first_name, last_name, email, gender, ip_address, country, dat, some_number) VALUES (14, 'Beverie', 'Jirousek', 'bjirousekd@arizona.edu', 'Female', '13.132.82.24/32',  'Indonesia', '2020-10-07',  950.04);
+INSERT INTO TEST.MOCK_DATA (id, first_name, last_name, email, gender, ip_address, country, dat, some_number) VALUES (15, 'Jecho', 'O''Garmen', 'jogarmene@woothemes.com', 'Male', '245.125.192.16/32',  'China', '2007-08-25',  257.19);
+INSERT INTO TEST.MOCK_DATA (id, first_name, last_name, email, gender, ip_address, country, dat, some_number) VALUES (16, 'Anni', 'Emmanueli', 'aemmanuelif@wikia.com', 'Female', '75.112.191.173/32',  'Saudi Arabia', '2015-08-11',  362.45);
+INSERT INTO TEST.MOCK_DATA (id, first_name, last_name, email, gender, ip_address, country, dat, some_number) VALUES (17, 'Evered', 'Marrow', 'emarrowg@tripadvisor.com', 'Male', '223.159.183.17/32',  'Niue', '2013-08-23',  418.18);
+INSERT INTO TEST.MOCK_DATA (id, first_name, last_name, email, gender, ip_address, country, dat, some_number) VALUES (18, 'Dell', 'Vondrak', 'dvondrakh@furl.net', 'Bigender', '83.89.160.155/32',  'Indonesia', '1999-03-06',  578.6);
+INSERT INTO TEST.MOCK_DATA (id, first_name, last_name, email, gender, ip_address, country, dat, some_number) VALUES (19, 'Renato', 'Swane', 'rswanei@scientificamerican.com', 'Genderfluid', '234.76.8.11/32',  'France', '2019-04-30',  80.81);
+INSERT INTO TEST.MOCK_DATA (id, first_name, last_name, email, gender, ip_address, country, dat, some_number) VALUES (20, 'Lucius', 'Edelmann', 'ledelmannj@bravesites.com', 'Male', '66.174.30.225/32',  'Brazil', '1999-06-22',  378.73);
+INSERT INTO TEST.MOCK_DATA (id, first_name, last_name, email, gender, ip_address, country, dat, some_number) VALUES (21, 'Gale', 'Norman', 'gnormank@skype.com', 'Female', '96.224.46.11/32',  'Russia', '2000-05-30',  152.93);
+INSERT INTO TEST.MOCK_DATA (id, first_name, last_name, email, gender, ip_address, country, dat, some_number) VALUES (22, 'Essie', 'McFfaden', 'emcffadenl@elpais.com', 'Female', '241.58.196.50/32',  'Greece', '2006-07-31',  75.77);
+INSERT INTO TEST.MOCK_DATA (id, first_name, last_name, email, gender, ip_address, country, dat, some_number) VALUES (23, 'Waly', 'Rogliero', 'wroglierom@berkeley.edu', 'Female', '122.90.196.231/32',  'Sweden', '2011-12-18',  147.69);
+INSERT INTO TEST.MOCK_DATA (id, first_name, last_name, email, gender, ip_address, country, dat, some_number) VALUES (24, 'Dillie', 'Iannazzi', 'diannazzin@biblegateway.com', 'Male', '112.79.17.198/32',  'Bangladesh', '2013-12-22',  699.62);
+INSERT INTO TEST.MOCK_DATA (id, first_name, last_name, email, gender, ip_address, country, dat, some_number) VALUES (25, 'Zolly', 'Wimmers', 'zwimmerso@hatena.ne.jp', 'Male', '123.12.225.114/32',  'Bosnia and Herzegovina', '2003-02-12',  217.18);
+INSERT INTO TEST.MOCK_DATA (id, first_name, last_name, email, gender, ip_address, country, dat, some_number) VALUES (26, 'Daryle', 'O''Shaughnessy', 'doshaughnessyp@com.com', 'Male', '204.107.16.207/32',  'Honduras', '2010-05-04',  983.03);
+INSERT INTO TEST.MOCK_DATA (id, first_name, last_name, email, gender, ip_address, country, dat, some_number) VALUES (27, 'Nick', 'Sings', 'nsingsq@boston.com', 'Male', '110.64.63.165/32',  'United States', '2011-03-17', 514.48);
+INSERT INTO TEST.MOCK_DATA (id, first_name, last_name, email, gender, ip_address, country, dat, some_number) VALUES (28, 'Ilsa', 'Huguenet', 'ihuguenetr@harvard.edu', 'Female', '147.1.198.181/32',  'China', '2014-05-11', 318.96);
+INSERT INTO TEST.MOCK_DATA (id, first_name, last_name, email, gender, ip_address, country, dat, some_number) VALUES (29, 'Grantham', 'Fayter', 'gfayters@desdev.cn', 'Male', '26.120.76.78/32',  'Sweden', '2009-10-02', 595.22);
+INSERT INTO TEST.MOCK_DATA (id, first_name, last_name, email, gender, ip_address, country, dat, some_number) VALUES (30, 'Bran', 'Longlands', 'blonglandst@tripod.com', 'Genderqueer', '14.92.3.30/32',  'France', '2016-07-10', 879.94);
+
+CREATE TYPE arr AS INTEGER ARRAY[3];
+CREATE TABLE TEST.ARRAY_TYPE (id BIGINT, data arr);
+INSERT INTO TEST.ARRAY_TYPE (1, NEW arr(121, 255, 1241244));
+INSERT INTO TEST.ARRAY_TYPE (2, NEW arr(0, -124412, 5555));
+
+CREATE TABLE TEST.character_type (
+                                ch CHAR(10),
+                                varch VARCHAR(10),
+                                clb CLOB(2K) CHARACTER SET UNICODE
+);
+
+INSERT INTO TEST.character_type(ch, varch, clb) VALUES ('Hello', 'World', 'Datagrok');
+
+
+CREATE TABLE TEST.dates_type (
+                            dat DATE,
+                            time_data TIME,
+                            stamp TIMESTAMP,
+                            time_zoned TIME WITH TIME ZONE,
+                            zoned_stamp TIMESTAMP WITH TIME ZONE
+);
+
+INSERT INTO TEST.dates_type (dat, time_data, stamp, time_zoned, zoned_stamp)
+VALUES ('2023-01-01', '12:55:33.333', '2023-01-01 12:55:33.333', '12:55:33.333+02:00', '2000-01-01 11:37:58.222');
+
+Set session calendar = iso;
+
+CREATE TABLE TEST.dates_patterns (
+    dat DATE
+);
+
+INSERT INTO TEST.dates_patterns(dat) VALUES (CURRENT_DATE);
+INSERT INTO TEST.dates_patterns(dat) VALUES (CURRENT_DATE - 1);
+INSERT INTO TEST.dates_patterns(dat) SELECT TRUNC(CURRENT_DATE + 6, 'DAY')
+    WHERE NOT EXISTS (SELECT * FROM TEST.dates_patterns WHERE dat = TRUNC(CURRENT_DATE + 6, 'DAY'));
+INSERT INTO TEST.dates_patterns(dat) VALUES (CURRENT_DATE - 150);
+INSERT INTO TEST.dates_patterns(dat) VALUES ('2021-04-09');
+
+CREATE TABLE TEST.JSON_DATA (
+    id BIGINT,
+    data JSON
+);
+
+INSERT INTO TEST.JSON_DATA(id, data)
+VALUES (1, '{"phones":[{"type": "mobile", "phone": "001001"}, {"type": "fix", "phone": "002002"}]}');
+INSERT INTO TEST.JSON_DATA(id, data) VALUES (2, '{"bar": "baz", "balance": 7.77, "active":false}');
+INSERT INTO TEST.JSON_DATA(id, data) VALUES (3, '{"reading": 1.230e-5}');
+
+CREATE TABLE TEST.SPATIAL_TYPES (id BIGINT, point ST_Geometry, polygon ST_Geometry, linestring ST_Geometry, multipolygon ST_Geometry);
+INSERT INTO TEST.SPATIAL_TYPES(id, point, polygon, linestring, multipolygon)
+VALUES (1, 'POINT(10 20)', 'POLYGON((1 1, 1 3, 6 3, 6 0, 1 1))', 'LINESTRING(1 1, 2 2, 3 3, 4 4)', 'MULTIPOLYGON(
+                                  ((1 1, 1 3, 6 3, 6 0, 1 1)),
+                                  ((10 5, 10 10, 20 10, 20 5, 10 5)))');
+
+CREATE TABLE TEST.XML_DATA (
+    id BIGINT,
+    data XML
+);
+
+INSERT INTO TEST.XML_DATA (id, data) VALUES (1, '<foo>Hello World!</foo>');
+INSERT INTO TEST.XML_DATA (id, data) VALUES (2, '<book><title>Manual</title><chapter>...</chapter></book>');
+
+CREATE TABLE TEST.INTEGER_TYPES (bigint_type BIGINT, byte_int BYTEINT, small_int SMALLINT, int_type INTEGER);
+INSERT INTO TEST.INTEGER_TYPES(bigint_type, byte_int, small_int, int_type)
+VALUES (9223372036854775807, -128, 32760, 2147483647);
+INSERT INTO TEST.INTEGER_TYPES(bigint_type, byte_int, small_int, int_type)
+VALUES (0, 127, -23444, -2147483647);
+
+CREATE TABLE TEST.FLOAT_TYPE(float_type FLOAT, real_type REAL, double_type DOUBLE PRECISION,
+decimal_type DECIMAL(38, 30), number_type NUMBER(5, 2));
+INSERT INTO TEST.FLOAT_TYPE(float_type, real_type, double_type, decimal_type, number_type)
+VALUES (3.4020234234, -3.4028423, -0.0043512, 235234.4646, 999.99);
+INSERT INTO TEST.FLOAT_TYPE(float_type, real_type, double_type, decimal_type, number_type)
+VALUES (0.9999, 1.9999, 213515.435545, 235234.4646, 0.99);
