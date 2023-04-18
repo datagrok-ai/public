b665aa205900d59b9f42377377c99ee2f788705f
onuf <56073962+onuf@users.noreply.github.com>
2023-04-18 16:02:46 +0200

    GROK-12831 Tutorials: Grid customization (WIP)

--- packages/Tutorials/src/tracks/eda/tutorials/grid.ts
+++ packages/Tutorials/src/tracks/eda/tutorials/grid.ts
@@ -45,8 +45,30 @@ export class GridTutorial extends Tutorial {
     await this.action('Jump to the first column', this.t!.onCurrentColChanged.pipe(filter(() =>
     this.t!.currentCol.name === columnNames[0])), null, 'Press <b>Home</b> to return to the first column.');
 
-    // Edit values
-    await this.action('Click the <b>+</b> icon', this.t!.onRowsAdded, $('.d4-grid-add-row-icon')[0], '');
+    this.title('Editing');
+
+    await this.action('Add a new row by clicking the "+" icon at the last row', this.t!.onRowsAdded,
+      $('.d4-grid-add-row-icon')[0], 'There are several ways to add a row to a spreadsheet: clicking the <b>+</b> ' +
+      'icon (requires "Allow Edit" property set to true), editing the last row (if "Add New Row On Last Row Edit" ' +
+      'is enabled), selecting <b>Edit > Add Rows...</b> in the top menu (this way you can add a specific number of ' +
+      'rows at a specific position in the grid).');
+
+    const lastRowIdx = this.t!.rowCount - 1;
+    await this.action(`Enter "X0273T51080200024" as USUBJID in the new row (#${lastRowIdx + 1})`, this.t!.onValuesChanged.pipe(filter(() =>
+      this.t!.cell(lastRowIdx, 'USUBJID').value === 'X0273T51080200024')), null, 'Once you finish editing, ' +
+      'one more row will appear (due to the above mentioned "Add New Row On Last Row Edit" grid property).');
+
+    await this.action(`Set AGE to "37" in the row you edited (#${lastRowIdx + 1})`, this.t!.onValuesChanged.pipe(
+      filter(() => this.t!.cell(lastRowIdx, 'AGE').value === 37)));
+
+    await this.action(`Set SEX to "F" in the row you edited (#${lastRowIdx + 1})`, this.t!.onValuesChanged.pipe(
+      filter(() => this.t!.cell(lastRowIdx, 'SEX').value === 'F')));
+
+    await this.action(`Copy the value from row #${lastRowIdx} to row #${lastRowIdx + 1} for the RACE column`,
+      this.t!.onValuesChanged.pipe(filter(() => this.t!.cell(lastRowIdx, 'RACE').value ===
+      this.t!.cell(lastRowIdx - 1, 'RACE').value)), null, 'Use <b>Ctrl+C</b> to copy the value of the ' +
+      'current cell and <b>Ctrl+V</b> to paste into the current cell.');
+
     this.title('Selection');
     // Selection of rows/columns
     // Add/remove rows/columns
