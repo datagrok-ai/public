9b10591b572c2e94d5a83497c21c1a1ed66aec54
Dmytro Kovalyov <laykdimon278@gmail.com>
2023-04-20 17:10:20 +0300

    ApiSamples: Changed console.log() to grok.shell.info()

--- packages/ApiSamples/scripts/ui/components/breadcrumbs.js
+++ packages/ApiSamples/scripts/ui/components/breadcrumbs.js
@@ -1,4 +1,4 @@
 const breadcrumbs = ui.breadcrumbs(['dev', 'datagrok', 'ai', 'apps', 'Curves']);
-breadcrumbs.onPathClick.subscribe((value) => console.log(value));
+breadcrumbs.onPathClick.subscribe((value) => grok.shell.info(value));
 
 grok.shell.newView('Breadcrumbs', [breadcrumbs.root]);
--- packages/ApiSamples/scripts/ui/elements/inputs/tags-input.js
+++ packages/ApiSamples/scripts/ui/elements/inputs/tags-input.js
@@ -3,8 +3,8 @@ const tagsInput = ui.tagsInput('Tags', ['Apps', 'Demo', 'Curves'], false);
 tagsInput.addTag('New');
 tagsInput.removeTag('Demo');
 
-tagsInput.onTagAdded.subscribe((value) => console.log(`${value} tag was added.`));
-tagsInput.onTagRemoved.subscribe((value) => console.log(`${value} tag was removed.`));
+tagsInput.onTagAdded.subscribe((value) => grok.shell.info(`${value} tag was added.`));
+tagsInput.onTagRemoved.subscribe((value) => grok.shell.info(`${value} tag was removed.`));
 
 const tagsInput2 = ui.tagsInput('Tags', ['Apps2', 'Demo2', 'Curves2'], true);
 tagsInput2.setTags(['Apps', 'Demo', 'Viewers', 'Tree']);
