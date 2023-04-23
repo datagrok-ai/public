661e5ea61fe4dd2c0bcf57da068a95cd3d41c7fd
skalkin <skalkin@gmail.com>
2023-04-23 12:18:07 -0400

    GROK-12908: JS API: Expose grok.shell.windows.xxxPanels - ability to show custom elements as help

---
+++ packages/ApiSamples/scripts/shell/windows/shell-windows.js
@@ -0,0 +1,6 @@
+// Basic manipulations with the standard panels
+
+grok.shell.windows.help.visible = true;
+grok.shell.windows.help.showHelp(ui.h1('Custom help element'));
+
+grok.shell.windows.context.visible = true;
