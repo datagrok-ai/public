b874981bb6904ffe066b280cd2b1dd6897289cb7
PavloPolovyi <pasha32662@gmail.com>
2023-04-24 22:12:41 +0300

    ApiTests: Created tests for cache

--- packages/ApiTests/src/connections/queries-test.ts
+++ packages/ApiTests/src/connections/queries-test.ts
@@ -1,10 +1,10 @@
-import {category, test} from '@datagrok-libraries/utils/src/test';
+import {category, expect, test} from '@datagrok-libraries/utils/src/test';
 import * as grok from 'datagrok-api/grok';
 import * as DG from 'datagrok-api/dg';
 import {_package} from '../package-test';
 
 category('Connections', () => {
-  
+
   test('queriesTest', async () => {
     const queries = await grok.dapi.queries
       .filter(`options.testExpectedRows != null and package.shortName = "${_package.name}"`).include('params').list();
@@ -38,19 +38,19 @@ category('Connections', () => {
   test('ScalarQueryTest', async () => {
     const query = await grok.dapi.queries.filter(`friendlyName = "Postgre Scalar Output"`).include('params').first();
     const call = query.prepare();
-    await call.call();  
+    await call.call();
     const t = call.getOutputParamValue() as number;
     console.log(t);
     if (t != 830)
       // eslint-disable-next-line no-throw-literal
       throw 'Rows number in' + query.name + 'table is not as expected';
   });
-  
+
   test('External Provider Chembl Perf', async () => {
     const query = await grok.dapi.queries.filter(`friendlyName = "ChemblPerfGenerated"`).include('params').first();
     const lim = 5000000;
     const call = query.prepare({'num': lim});
-    await call.call();  
+    await call.call();
     const t = call.getOutputParamValue() as DG.DataFrame;
     if (t.rowCount != lim)
       // eslint-disable-next-line no-throw-literal
@@ -68,4 +68,24 @@ category('Connections', () => {
         throw 'First rows await time exceeded';
     }, 2000);
   });
+
+  test('Cache test for Table_Wide', async () => {
+    const dataQueryName = 'PostgresqlTestCacheTableWide';
+    const firstExecutionTime = await getExecutionTime(dataQueryName);
+    const secondExecutionTime = await getExecutionTime(dataQueryName);
+    expect(firstExecutionTime > secondExecutionTime * 2, true);
+  });
+
+  test('Cache test for Table_Normal', async () => {
+    const dataQueryName = 'PostgresqlTestCacheTableNormal';
+    const firstExecutionTime = await getExecutionTime(dataQueryName);
+    const secondExecutionTime = await getExecutionTime(dataQueryName);
+    expect(firstExecutionTime > secondExecutionTime * 2, true);
+  });
 });
+
+async function getExecutionTime(dataQueryName: string): Promise<number> {
+  const startTime = Date.now();
+  await grok.functions.eval('ApiTests:' + dataQueryName);
+  return Date.now() - startTime;
+}
