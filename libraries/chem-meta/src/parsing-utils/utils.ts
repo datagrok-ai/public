693601aa673040cc4e580380070cab03a7bba880
Maria Dolotova <maria.dolotova@softwarecountry.com>
2023-04-25 11:23:46 +0300

    Chem meta: Bump version

--- libraries/chem-meta/src/parsing-utils/utils.ts
+++ libraries/chem-meta/src/parsing-utils/utils.ts
@@ -1,5 +1,4 @@
-export function isAlpha(sym: string): boolean {
-    const charCode = sym.charCodeAt(0);
+export function isAlpha(charCode: number): boolean {
     return (charCode > 64 && charCode < 91) ||
         (charCode > 96 && charCode < 123);
 }
