d484d4147e8d875677b04f6320cc2b6098fa6cc0
Anna Muzychyna <annamuz69@gmail.com>
2023-04-25 14:14:10 +0300

    GROK-12844 ApiSamples: Parameterized-query fix

--- packages/ApiSamples/scripts/data-access/parameterized-query.js
+++ packages/ApiSamples/scripts/data-access/parameterized-query.js
@@ -2,5 +2,5 @@
 //help-url: https://datagrok.ai/help/access/parameterized-queries
 // An example of using parameterized query
 
-grok.data.query('System:CoffeeCompany:StoresInState', {'state': 'NY'})
-  .then(t => grok.shell.info('Stores: ' + t.rowCount));
+grok.data.query('ApiTests:PostgresqlByDouble', {freight: 100.1})
+  .then(t => grok.shell.info('Number of orders: ' + t.rowCount));
