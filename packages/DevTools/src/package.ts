c6c4583ab34543b4abfc6696e6d19fc7ff9f45ca
Oleksandra Serhiienko <59337516+Aleksashka11@users.noreply.github.com>
2023-04-27 16:13:25 +0300

    DevTools: Minor fix

--- packages/DevTools/src/package.ts
+++ packages/DevTools/src/package.ts
@@ -1,7 +1,7 @@
 import * as grok from 'datagrok-api/grok';
 import * as ui from 'datagrok-api/ui';
 import * as DG from 'datagrok-api/dg';
-import {dataQueryEditor, scriptEditor} from './script-editor';
+import {scriptEditor} from './script-editor';
 import {IconTool} from './icon-tool';
 import {EntityType} from './constants';
 import '../css/styles.css';
