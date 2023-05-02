515b52b14cfc46bc48f8b7c93f4d5c6c7d71c75f
Volodymyr Dyma <vdyma@datagrok.ai>
2023-05-02 20:16:29 +0300

    Peptides: Added Table view selection and filtering tests

---
+++ packages/Peptides/src/tests/table-view.ts
@@ -0,0 +1,90 @@
+import * as DG from 'datagrok-api/dg';
+
+import {category, test, before, expect} from '@datagrok-libraries/utils/src/test';
+import {_package} from '../package-test';
+import { PeptidesModel } from '../model';
+import { startAnalysis } from '../widgets/peptides';
+import { ScalingMethods } from '../utils/types';
+import { scaleActivity } from '../utils/misc';
+import { NOTATION } from '@datagrok-libraries/bio/src/utils/macromolecule';
+
+category('Table view', () => {
+  let df: DG.DataFrame;
+  let model: PeptidesModel;
+  let activityCol: DG.Column<number>;
+  let sequenceCol: DG.Column<string>;
+  let clusterCol: DG.Column<any>;
+  let scaledActivityCol: DG.Column<number>;
+  let scaling: ScalingMethods = 'none';
+
+  const firstPair = {monomer: 'N', position: '4', count: 7};
+  const secondPair = {monomer: 'meI', position: '1', count: 10};
+
+  before(async () => {
+    df = DG.DataFrame.fromCsv(await _package.files.readAsText('tests/HELM_small.csv'));
+    activityCol = df.getCol('activity');
+    sequenceCol = df.getCol('sequence');
+    sequenceCol.semType = DG.SEMTYPE.MACROMOLECULE;
+    sequenceCol.setTag(DG.TAGS.UNITS, NOTATION.HELM);
+    scaledActivityCol = scaleActivity(activityCol, scaling);
+    clusterCol = df.getCol('cluster');
+    const tempModel = await startAnalysis(activityCol, sequenceCol, clusterCol, df, scaledActivityCol, scaling);
+    if (tempModel === null)
+      throw new Error('Model is null');
+    model = tempModel;
+  });
+
+  test('Tooltip', async () => {
+    
+  }, {skipReason: 'Not implemented yet'});
+
+  test('Visible columns', async () => {
+    
+  }, {skipReason: 'Not implemented yet'});
+
+  test('Selection', async () => {
+    const selection = model.df.selection;
+
+    // Select first monomer-position pair
+    model.modifyMonomerPositionSelection(firstPair.monomer, firstPair.position, false);
+    expect(model.mutationCliffsSelection[firstPair.position].includes(firstPair.monomer), true);
+    expect(selection.trueCount, firstPair.count);
+
+    // Select second monomer-position pair
+    model.modifyMonomerPositionSelection(secondPair.monomer, secondPair.position, false);
+    expect(model.mutationCliffsSelection[secondPair.position].includes(secondPair.monomer), true);
+    expect(selection.trueCount, secondPair.count);
+
+    // Deselect second monomer-position pair
+    model.modifyMonomerPositionSelection(secondPair.monomer, secondPair.position, false);
+    expect(model.mutationCliffsSelection[secondPair.position].includes(secondPair.monomer), false);
+    expect(selection.trueCount, firstPair.count);
+
+    // Clear selection
+    model.initMutationCliffsSelection(true);
+    expect(selection.trueCount, 0);
+  });
+  
+  test('Filtering', async () => {
+    const filter = model.df.filter;
+
+    // Select second monomer-position pair
+    model.modifyMonomerPositionSelection(secondPair.monomer, secondPair.position, true);
+    expect(model.invariantMapSelection[secondPair.position].includes(secondPair.monomer), true);
+    expect(filter.trueCount, secondPair.count);
+
+    // Select first monomer-position pair
+    model.modifyMonomerPositionSelection(firstPair.monomer, firstPair.position, true);
+    expect(model.invariantMapSelection[firstPair.position].includes(firstPair.monomer), true);
+    expect(filter.trueCount, firstPair.count);
+
+    // Deselect second monomer-position pair
+    model.modifyMonomerPositionSelection(secondPair.monomer, secondPair.position, true);
+    expect(model.invariantMapSelection[secondPair.position].includes(secondPair.monomer), false);
+    expect(filter.trueCount, firstPair.count);
+
+    // Clear selection
+    model.initInvariantMapSelection(true);
+    expect(filter.trueCount, df.rowCount);
+  });
+});
