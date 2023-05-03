883de74ea48e16858988e57b90d223d3045e2df4
vmakarichev <vmakarichev@datagrok.ai>
2023-05-03 16:51:52 +0200

    GROK-13011: EDA | TS package: PCA, PLS
    PCA and PLS methods are added.

--- packages/EDA/src/package.ts
+++ packages/EDA/src/package.ts
@@ -3,6 +3,9 @@ import * as grok from 'datagrok-api/grok';
 import * as ui from 'datagrok-api/ui';
 import * as DG from 'datagrok-api/dg';
 
+import {_initEDAAPI} from '../wasm/EDAAPI';
+import {computePCA, computePLS} from './EDAtools';
+
 export const _package = new DG.Package();
 
 //name: info
@@ -10,89 +13,27 @@ export function info() {
   grok.shell.info(_package.webRoot);
 }
 
-import {_initEDAAPI, _principalComponentAnalysis, _principalComponentAnalysisInWebWorker, _error, _errorInWebWorker, _partialLeastSquareRegression, _partialLeastSquareRegressionInWebWorker} from '../wasm/EDAAPI';
-
 //tags: init
 export async function init(): Promise<void> {
   await _initEDAAPI();
 }
 
-//name: principalComponentAnalysis
+//name: PCA
+//description: Principal component analysis (PCA).
 //input: dataframe table
-//input: column_list columns
-//input: int componentsCount
-//output: dataframe result 
-export function principalComponentAnalysis(table: DG.DataFrame, columns: DG.ColumnList, componentsCount: number): DG.DataFrame {
-  return _principalComponentAnalysis(table, columns, componentsCount);
+//input: column_list features
+//input: int components = 3
+//output: dataframe result {action:join(table)}
+export async function PCA(table: DG.DataFrame, features: DG.ColumnList, components: number): Promise<DG.DataFrame> {
+  return await computePCA(table, features, components);
 }
 
-//name: principalComponentAnalysisInWebWorker
-//input: dataframe table
-//input: column_list columns
-//input: int componentsCount
-//output: dataframe result 
-export async function principalComponentAnalysisInWebWorker(table: DG.DataFrame, columns: DG.ColumnList, componentsCount: number): Promise<DG.DataFrame> {
-  let _output: any;
-  let _promise = _principalComponentAnalysisInWebWorker(table, columns, componentsCount);
-
-  await _promise.then(
-    _result => {  _output = _result; },
-    _error => {  throw new Error (`Error: ${_error}`); }
-  );
-
-  return _output;
-}
-
-//name: error
-//input: dataframe df
-//input: column col1
-//input: column col2
-//output: double mad 
-export function error(df: DG.DataFrame, col1: DG.Column, col2: DG.Column): number {
-  return _error(df, col1, col2);
-}
-
-//name: errorInWebWorker
-//input: dataframe df
-//input: column col1
-//input: column col2
-//output: double mad 
-export async function errorInWebWorker(df: DG.DataFrame, col1: DG.Column, col2: DG.Column): Promise<number> {
-  let _output: any;
-  let _promise = _errorInWebWorker(df, col1, col2);
-
-  await _promise.then(
-    _result => {  _output = _result; },
-    _error => {  throw new Error (`Error: ${_error}`); }
-  );
-
-  return _output;
-}
-
-//name: partialLeastSquareRegression
+//name: PLS
+//description: Partial least square regression (PLS).
 //input: dataframe table
 //input: column_list features
 //input: column predict
-//input: int componentsCount
-export function partialLeastSquareRegression(table: DG.DataFrame, features: DG.ColumnList, predict: DG.Column, componentsCount: number): any {
-  return _partialLeastSquareRegression(table, features, predict, componentsCount);
+//input: int components = 3
+export async function PLS(table: DG.DataFrame, features: DG.ColumnList, predict: DG.Column, components: number): Promise<void> {
+  await computePLS(table, features, predict, components);
 }
-
-//name: partialLeastSquareRegressionInWebWorker
-//input: dataframe table
-//input: column_list features
-//input: column predict
-//input: int componentsCount
-export async function partialLeastSquareRegressionInWebWorker(table: DG.DataFrame, features: DG.ColumnList, predict: DG.Column, componentsCount: number): Promise<any> {
-  let _output: any;
-  let _promise = _partialLeastSquareRegressionInWebWorker(table, features, predict, componentsCount);
-
-  await _promise.then(
-    _result => {  _output = _result; },
-    _error => {  throw new Error (`Error: ${_error}`); }
-  );
-
-  return _output;
-}
-
-
---
+++ packages/EDA/src/utils.ts
@@ -0,0 +1,61 @@
+import * as grok from 'datagrok-api/grok';
+import * as ui from 'datagrok-api/ui';
+import * as DG from 'datagrok-api/dg';
+
+// Inputs correctness check tools
+
+//Limitation constants
+const COMP_MIN = 1;
+const SAMPLES_COUNT_MIN = 1;
+const FEATURES_COUNT_MIN = 1;
+const PERCENTAGE_MIN = 0;
+const PERCENTAGE_MAX = 100;
+
+// Error messages
+const COMP_POSITVE_MES = 'components must be positive.';
+const COMP_EXCESS = 'components must not be greater than feautures count.';
+const INCORERRECT_MIN_MAX_MES = 'min must be less than max.';
+const INCORERRECT_FEATURES_MES = 'features must be positive.';
+const INCORERRECT_SAMPLES_MES = 'samples must be positive.';
+const INCORERRECT_PERCENTAGE_MES = 'violators percentage must be from the range from 0 to 100.';
+
+// Check components count (PCA, PLS)
+export function checkComponenets(features: DG.ColumnList, components: number): void {
+  if (components < COMP_MIN)
+    throw new Error(COMP_POSITVE_MES);
+
+  if (components > features.length)
+    throw new Error(COMP_EXCESS);
+}
+
+/*
+// Check whether data processing requires much time (PCA, PLS)
+export function isProcExpensive(table, features, max) {
+  return ((table.rowCount > max) || (features.length > max));
+}
+
+// Check inputs of data for SVM testing generator
+export function checkGeneratorSVMinputs(samplesCount, featuresCount, min, max, violatorsPercentage) 
+{
+  if (min >= max)
+    throw new Error(INCORERRECT_MIN_MAX_MES);
+  
+  if (featuresCount < FEATURES_COUNT_MIN) 
+    throw new Error(INCORERRECT_FEATURES_MES);
+
+  if (samplesCount < SAMPLES_COUNT_MIN) 
+    throw new Error(INCORERRECT_SAMPLES_MES);
+
+  if ((violatorsPercentage < PERCENTAGE_MIN) || (violatorsPercentage > PERCENTAGE_MAX))
+    throw new Error(INCORERRECT_PERCENTAGE_MES);
+}
+
+// Check whether SVM computation requires much time
+export function isDataBigForSVM(featureCols, featuresMax, labelsCol, labelsMax) {
+  if (featureCols.length > featuresMax)
+    return true;
+
+  if (labelsCol.length > labelsMax)
+    return true;
+}
+*/
