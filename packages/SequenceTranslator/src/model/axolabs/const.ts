c4fa14f141c4e6c1e6999eeaf42c431bde7d4f50
Alexey <achopovsky@datagrok.ai>
2023-05-08 22:23:00 +0300

    GROK-13049: Bio | ST: start refactoring axolabs function of 700+ lines

---
+++ packages/SequenceTranslator/src/model/axolabs/const.ts
@@ -0,0 +1,5 @@
+export const defaultPto: boolean = true;
+export const defaultSequenceLength: number = 23;
+export const maximalValidSequenceLength: number = 35;
+export const userStorageKey: string = 'SequenceTranslator';
+export const exampleMinWidth: string = '400px';
