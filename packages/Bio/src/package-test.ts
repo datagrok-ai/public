2b5cb295489d184f1b4427a657fa90f8426224f7
drizhina <130361925+drizhina@users.noreply.github.com>
2023-05-14 21:46:57 +0400

    GROK-12901: Bio: Add distance tests (#1900)
    * GROK-12901: Bio: Add distance tests
    
    * GROK-12901: Bio: Bump version

--- packages/Bio/src/package-test.ts
+++ packages/Bio/src/package-test.ts
@@ -22,6 +22,7 @@ import './tests/substructure-filters-tests';
 import './tests/pepsea-tests';
 import './tests/viewers';
 import './tests/units-handler-tests';
+import './tests/mm-distance-tests';
 
 // Tests hanging github CI
 import './tests/activity-cliffs-tests';
