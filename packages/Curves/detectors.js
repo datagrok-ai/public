aea49ee24466121f2f50966a825de018326f5609
Dmytro Kovalyov <laykdimon278@gmail.com>
2023-05-18 18:56:06 +0300

    GROK-13061: Curves: Added tests for detectors

--- packages/Curves/detectors.js
+++ packages/Curves/detectors.js
@@ -6,13 +6,15 @@
  * Follow this naming convention to ensure that your detectors are properly loaded.
  */
 const FIT_SEM_TYPE = 'fit';
+const TAG_FIT_CHART_FORMAT = '.fitChartFormat';
+const TAG_FIT_CHART_FORMAT_3DX = '3dx';
 class CurvesPackageDetectors extends DG.Package {
   //tags: semTypeDetector
   //input: column col
   //output: string semType
   detectXMLCurveChart(col) {
     if (DG.Detector.sampleCategories(col, (s) => s.startsWith('<chart>') && s.endsWith('</chart>'), 1)) {
-      col.setTag('.fitChartFormat', '3dx');
+      col.setTag(TAG_FIT_CHART_FORMAT, TAG_FIT_CHART_FORMAT_3DX);
       col.semType = FIT_SEM_TYPE;
       return col.semType;
     }
