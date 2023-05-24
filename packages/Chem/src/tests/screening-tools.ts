2ad11fb257cd97167917731329e60b7c012e2d66
Maria Dolotova <maria.dolotova@softwarecountry.com>
2023-05-24 10:53:27 +0300

    Chem: Fixed tests

--- packages/Chem/src/tests/screening-tools.ts
+++ packages/Chem/src/tests/screening-tools.ts
@@ -42,7 +42,7 @@ category('screening tools', () => {
   test('elementalAnalysis.molV2000', async () => {
     const df = spgi100.clone();
     elementalAnalysis(df, df.getCol('Structure'), false, false);
-    expect(df.columns.length, 58);
+    expect(df.columns.length, 95);
   });
 
   test('elementalAnalysis.molV3000', async () => {
