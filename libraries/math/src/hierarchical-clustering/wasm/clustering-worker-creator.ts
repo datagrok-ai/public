6a386f4a9511ac3a560212cac2f1a950d320fdf1
drizhina <drizhinashvili@datagrok.ai>
2023-05-24 20:40:33 +0400

    GROK-13165: Math-Lib: Address comments

--- libraries/math/src/hierarchical-clustering/wasm/clustering-worker-creator.ts
+++ libraries/math/src/hierarchical-clustering/wasm/clustering-worker-creator.ts
@@ -1,10 +1,12 @@
 import {ClusterMatrix} from '../types';
+import {LinkageMethodCode} from '../consts';
 
 /** Calculates Cluster matrix based on distance matrix reduces array. i.e. the top right triangle of the matrix
  * turned into the array.
- * @param distMatArray - distance matrix reduces array.
+ * @param distMatArray - reduced distance matrix array.
  * @param n - dimension of the distance matrix, i.e. side length.
- * @param methodCode - method code for the clustering algorithm.
+ * @param methodCode - method code for the clustering algorithm. Method codes can be found in {@link LinkageMethodCode} object.
+ * @returns Cluster matrix.
  */
 export async function getClusterMatrixWorker(
   distMatArray: Float32Array, n: number, methodCode: number
