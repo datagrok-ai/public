251f24be470267f9da5fe5aaae68554794a72ed9
Oleksandra Serhiienko <59337516+Aleksashka11@users.noreply.github.com>
2023-05-26 11:24:17 +0300

    Chembl: Add default values for query inputs

--- packages/Chembl/queries/queries.sql
+++ packages/Chembl/queries/queries.sql
@@ -179,15 +179,15 @@ select count(from_id) from src10src11
 --name: FracClassification
 --friendlyName: Search | FRAC classification
 --connection: Chembl 
---input: string level1 {category: FRAC; choices: Query("SELECT DISTINCT level1_description FROM frac_classification")}
+--input: string level1 = 'MITOSIS AND CELL DIVISION' {category: FRAC; choices: Query("SELECT DISTINCT level1_description FROM frac_classification")}
 --input: string level2 {category: FRAC; nullable: true; choices: Query("SELECT DISTINCT level2_description FROM frac_classification where level1_description = @level1")}
 --input: string level3 {category: FRAC; nullable: true; choices: Query("SELECT DISTINCT level3_description FROM frac_classification where level2_description = @level2")}
 --input: string level4 {category: FRAC; nullable: true; choices: Query("SELECT DISTINCT level4_description FROM frac_classification where level3_description = @level3")}
-SELECT * 
+SELECT s.* 
 FROM compound_structures s
-INNER JOIN molecule_frac_classification m 
+JOIN molecule_frac_classification m 
 ON s.molregno = m.molregno
-INNER JOIN frac_classification f
+JOIN frac_classification f
 ON m.frac_class_id = f.frac_class_id
 WHERE f.level4_description = @level4
 --end
@@ -195,17 +195,17 @@ WHERE f.level4_description = @level4
 
 --name: QueryBySubstructure
 --friendlyName: Search | By substructure, country and action type
---connection: Chembl 
+--connection: ChemblSql 
 --meta.batchMode: true
---input: string substructure {semType: Molecule}
+--input: string substructure = 'c1ccccc1' {semType: Molecule}
 --input: string threshold = '0.1' 
---input: string actionType {choices: Query("SELECT DISTINCT action_type from drug_mechanism")}
---input: string mechanismOfAction {choices: Query("SELECT DISTINCT mechanism_of_action from drug_mechanism where action_type = @actionType")}
---input: string country {choices: Query("SELECT DISTINCT country from research_companies")}
---input: list company {choices: Query("SELECT DISTINCT company from research_companies where country = @country")}
+--input: string actionType = 'BLOCKER' {choices: Query("SELECT DISTINCT action_type from drug_mechanism")}
+--input: string mechanismOfAction = 'Amiloride-sensitive sodium channel, ENaC blocker' {choices: Query("SELECT DISTINCT mechanism_of_action from drug_mechanism where action_type = @actionType")}
+--input: string country = 'UK' {choices: Query("SELECT DISTINCT country from research_companies")}
+--input: list company = ['GlaxoSmithKline'] {choices: Query("SELECT DISTINCT company from research_companies where country = @country")}
 SELECT set_config('rdkit.tanimoto_threshold', @threshold, true);
 --batch
-SELECT *
+SELECT s.*
 FROM compound_structures s
 INNER JOIN drug_mechanism d
 ON s.molregno = d.molregno
@@ -226,7 +226,7 @@ AND r.company IN (
 --name: ByChemblIds
 --friendlyName: Search | byChemblIds
 --connection: Chembl 
---input: list chemblIds = ['CHEMBL1185'] {inputType: TextArea}
+--input: list chemblIds = ['CHEMBL1185', 'CHEMBL1186'] {inputType: TextArea}
 SELECT *
 FROM molecule_dictionary
 WHERE chembl_id IN (
