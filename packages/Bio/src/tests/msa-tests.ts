e72844f2ea980341b96a52f2abb17f7a057372a8
Alexey <achopovsky@datagrok.ai>
2023-05-29 17:28:49 +0300

    Bio: skip PepSea tests

--- packages/Bio/src/tests/msa-tests.ts
+++ packages/Bio/src/tests/msa-tests.ts
@@ -76,33 +76,33 @@ MWRSWYCKHPMWRSWYCKHPMWRSWYCKHPMWRSWYCKHPMWRSWYCKHPMWRSWYCKHPMWRSWYCKHPMWRSWYCKHP
 
   test('isCorrect', async () => {
     await _testMsaIsCorrect(fromCsv, toCsv);
-  });
+  }, {skipReason: 'GROK-13221'});
 
   test('isCorrectLong', async () => {
     await _testMsaIsCorrect(longFromCsv, longToCsv);
-  });
+  }, {skipReason: 'GROK-13221'});
 
   test('isCorrectHelm', async () => {
     await awaitContainerStart();
     await _testMSAOnColumn(helmFromCsv, helmToCsv, NOTATION.HELM, NOTATION.SEPARATOR, undefined, 'mafft');
-  });
+  }, {skipReason: 'GROK-13221'});
 
   test('isCorrectHelmLong', async () => {
     await awaitContainerStart();
     await _testMSAOnColumn(longHelmFromCsv, longHelmToCsv, NOTATION.HELM, NOTATION.SEPARATOR, undefined, 'mafft');
-  });
+  }, {skipReason: 'GROK-13221'});
 
   test('isCorrectSeparator', async () => {
     await _testMSAOnColumn(
       SeparatorFromCsv, SeparatorToCsv, NOTATION.SEPARATOR, NOTATION.FASTA, ALPHABET.PT
     );
-  });
+  }, {skipReason: 'GROK-13221'});
 
   test('isCorrectSeparatorLong', async () => {
     await _testMSAOnColumn(
       SeparatorLongFromCsv, SeparatorLongToCsv, NOTATION.SEPARATOR, NOTATION.FASTA, ALPHABET.PT
     );
-  });
+  }, {skipReason: 'GROK-13221'});
 });
 
 async function _testMsaIsCorrect(srcCsv: string, tgtCsv: string): Promise<void> {
