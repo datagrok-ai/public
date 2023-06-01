e2026d7507db88d3065ec4cc57736dffa4fbbef6
skalkin <skalkin@gmail.com>
2023-06-01 16:26:08 -0400

    Core: provided typing for FuncCall.

--- packages/ApiSamples/scripts/functions/func-call.js
+++ packages/ApiSamples/scripts/functions/func-call.js
@@ -11,5 +11,8 @@ f.apply({x: 0.5}).then((result) => grok.shell.info(result));
 // more complex: preparing a FuncCall that lets you track inputs and outputs, intercept execution, etc
 let fc = f.prepare({"x": 0.5});
 
+grok.shell.info('Input name and type: ' + fc.inputParams['x'].name + ' ' + fc.inputParams['x'].property.propertyType);
+grok.shell.info('Input value:' + fc.inputs['x']);
+
 grok.shell.info(fc.inputs['x']);
 fc.call().then((call) => grok.shell.info(call.outputs['result']));
