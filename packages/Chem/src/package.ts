2dfccb5f97758fcdf2c020687ceba7247c8e04e7
Oleksandra Serhiienko <59337516+Aleksashka11@users.noreply.github.com>
2023-06-06 11:09:56 +0300

    GROK-13254: Chem: Substructure Search select molecular columns

--- packages/Chem/src/package.ts
+++ packages/Chem/src/package.ts
@@ -424,7 +424,7 @@ export function searchSubstructureEditor(call: DG.FuncCall) {
   } else if (molColumns.length === 1)
     call.func.prepare({molecules: molColumns[0]}).call(true);
   else {
-    const colInput = ui.columnInput('Molecules', grok.shell.tv.dataFrame, molColumns[0]);
+    const colInput = ui.columnInput('Molecules', grok.shell.tv.dataFrame, molColumns[0], null, {'predicate': (col: DG.Column) => col.semType === DG.SEMTYPE.MOLECULE});
     ui.dialog({title: 'Substructure search'})
       .add(colInput)
       .onOK(async () => {
