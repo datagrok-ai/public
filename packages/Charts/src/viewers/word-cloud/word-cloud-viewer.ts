feb5041012a00a1dc12749a4c64af20ea6ce4942
onuf <56073962+onuf@users.noreply.github.com>
2023-06-07 15:08:59 +0200

    Charts: Fix typo in parameter name

--- packages/Charts/src/viewers/word-cloud/word-cloud-viewer.ts
+++ packages/Charts/src/viewers/word-cloud/word-cloud-viewer.ts
@@ -21,7 +21,7 @@ export class WordCloudViewer extends DG.JsViewer {
   maxTextSize: any;
   minRotationDegree: number;
   maxRotationDegree: number;
-  roationStep: number;
+  rotationStep: number;
   gridSize: number;
   drawOutOfBound: boolean;
   fontFamily: string;
@@ -44,7 +44,7 @@ export class WordCloudViewer extends DG.JsViewer {
 
     this.minRotationDegree = this.int('minRotationDegree', -30);
     this.maxRotationDegree = this.int('maxRotationDegree', 30);
-    this.roationStep = this.int('rotationStep', 5);
+    this.rotationStep = this.int('rotationStep', 5);
 
     this.gridSize = this.int('gridSize', 8);
 
@@ -142,7 +142,7 @@ export class WordCloudViewer extends DG.JsViewer {
         sizeRange: [this.minTextSize, this.maxTextSize],
         gridSize: this.gridSize,
         rotationRange: [this.minRotationDegree, this.maxRotationDegree],
-        rotationStep: this.roationStep,
+        rotationStep: this.rotationStep,
         drawOutOfBound: this.drawOutOfBound,
         textStyle: {
           fontFamily: this.fontFamily,
