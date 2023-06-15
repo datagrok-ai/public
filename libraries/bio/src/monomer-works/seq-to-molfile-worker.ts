37777e375a7d871f9dd7ba4664ef812fa748fc27
drizhina <drizhinashvili@datagrok.ai>
2023-06-15 14:29:57 +0400

    GROK-13139: Bio-lib: Parallelize calculation

--- libraries/bio/src/monomer-works/seq-to-molfile-worker.ts
+++ libraries/bio/src/monomer-works/seq-to-molfile-worker.ts
@@ -1,12 +1,12 @@
 import {monomerSeqToMolfile} from './to-atomic-level-utils';
 onmessage = (event) => {
-  const {monomerSequencesArray, monomersDict, alphabet, polymerType, srcColLength} = event.data;
-  const molfileList: string[] = new Array<string>(srcColLength);
+  const {monomerSequencesArray, monomersDict, alphabet, polymerType, start, end} = event.data;
+  const molfileList: string[] = new Array<string>(end - start);
   const molfileWarningList = new Array<string>(0);
-  for (let rowI = 0; rowI < srcColLength; ++rowI) {
+  for (let rowI = start; rowI < end; ++rowI) {
     try {
       const monomerSeq = monomerSequencesArray[rowI];
-      molfileList[rowI] = monomerSeqToMolfile(monomerSeq, monomersDict, alphabet, polymerType);
+      molfileList[rowI - start] = monomerSeqToMolfile(monomerSeq, monomersDict, alphabet, polymerType);
     } catch (err: any) {
       const errMsg: string = err instanceof Error ? err.message : err.toString();
       const msg: string = `Cannot get molfile of row #${rowI}: ${errMsg}.`;
--- libraries/bio/src/monomer-works/seq-to-molfile.ts
+++ libraries/bio/src/monomer-works/seq-to-molfile.ts
@@ -10,12 +10,34 @@ export type SeqToMolfileResult = {
 export async function seqToMolFileWorker(monomerSequencesArray: string[][], monomersDict: Map<string, MolGraph>,
   alphabet: ALPHABET, polymerType: HELM_POLYMER_TYPE, srcColLength: number
 ): Promise<SeqToMolfileResult> {
-  const worker = new Worker(new URL('./seq-to-molfile-worker', import.meta.url));
-  worker.postMessage({monomerSequencesArray, monomersDict, alphabet, polymerType, srcColLength});
-  return new Promise((resolve) => {
-    worker.onmessage = ({data: {molfileList, molfileWarningList}}) => {
-      resolve({molfileList, molfileWarningList});
-      setTimeout(() => worker.terminate(), 0);
-    };
+  const threadCount = Math.max(navigator.hardwareConcurrency - 2, 1);
+  const workers = new Array(threadCount).fill(null)
+    .map(() => new Worker(new URL('./seq-to-molfile-worker', import.meta.url)));
+  const chunkSize = srcColLength / threadCount;
+  let res: string[] = [];
+  let warnings: string[] = [];
+  const promises = new Array<Promise<SeqToMolfileResult>>(threadCount);
+  for (let i = 0; i < threadCount; i++) {
+    const start = Math.floor(i * chunkSize);
+    const end = (i === threadCount - 1) ? srcColLength : Math.floor((i + 1) * chunkSize);
+    workers[i].postMessage({monomerSequencesArray, monomersDict, alphabet, polymerType, start, end});
+    promises[i] = new Promise<SeqToMolfileResult>((resolveWorker) => {
+      workers[i].onmessage = ({data: {molfileList, molfileWarningList}}): void => {
+        resolveWorker({molfileList, molfileWarningList});
+      };
+    });
+  }
+  const resArray = await Promise.all(promises);
+
+  resArray.forEach((resItem) => {
+    res = res.concat(...resItem.molfileList);
+    warnings = warnings.concat(...resItem.molfileWarningList);
   });
+
+  setTimeout(() => {
+    workers.forEach((worker) => {
+      worker.terminate();
+    });
+  }, 0);
+  return {molfileList: res, molfileWarningList: warnings};
 }
