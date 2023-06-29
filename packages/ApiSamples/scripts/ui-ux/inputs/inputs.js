cedff6e2b2dbc4555067f8791e47847184815a1e
Dmitry Illarionov <illarionow@yahoo.com>
2023-06-29 13:20:11 +0200

    API Samples: Added form sample

--- packages/ApiSamples/scripts/ui-ux/inputs/inputs.js
+++ packages/ApiSamples/scripts/ui-ux/inputs/inputs.js
@@ -1,31 +1,22 @@
 // Different input types
 
-
-
 let v = grok.shell.newView('demo: inputs');
 
-let type = ui.choiceInput('Form type', 'Default', ['Default', 'Left', 'Wide', 'Condensed'], (x)=>{
-  if (x == 'Default')
-    form.className = 'ui-form';
-  if (x == 'Left')
-    form.className = 'ui-form ui-form-left';  
-  if (x == 'Wide')
-    form.className = 'ui-form ui-form-wide'; 
-  if (x == 'Condensed')
-    form.className = 'ui-form ui-form-condensed';   
-});
-
+let search = ui.searchInput('Search', '');
 let name = ui.stringInput('Name', 'Arthur Dent');
 let age = ui.intInput('Age', 30);
 let date = ui.dateInput('Birthday', dayjs('1970-5-10'));
 let alien = ui.boolInput('Alien', false);
 let friends = ui.multiChoiceInput('Friends', ['Ford', 'Fenchurch'], ['Ford', 'Fenchurch', 'Zaphod', 'Slartibartfast']);
+let bestFriend = ui.radioInput('Best friend', 'Ford', ['Ford', 'Fenchurch']);
 let active = ui.switchInput('Active', true);
 let color = ui.colorInput('Favorite color', '#ff0000');
-let data = ui.tableInput('Details', null);
+let country = ui.typeAhead('Country', {source: {local: ['USA', 'Ukraine', 'Antigua', 'United Kingdom', 'United Arab Emirates']}});
+let data = ui.tableInput('Data', null);
+let tags = ui.tagsInput('Skills', ['HTML', 'CSS', 'JS'], true);
 let memo = ui.textInput('Memo', '');
 
-let form = ui.form([type, name, age, date, alien, friends, active, color, data, memo]);
+let form = ui.form([search, data, name, age, date, alien, friends,bestFriend, active, color, country, tags, memo]);
 
-form.append(ui.buttonsInput([ui.bigButton('Submit', ()=>{grok.shell.info('Form type: '+form.className)})]));
+form.append(ui.buttonsInput([ui.bigButton('Submit', ()=>{grok.shell.info('Submit')})]));
 v.append(form);
