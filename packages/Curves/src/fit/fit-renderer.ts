f5bb5c459bf463c2e68f54944f2984f7eccda463
Dmytro Kovalyov <laykdimon278@gmail.com>
2023-06-30 14:09:27 +0300

    #2101: Curves: Changed margins for grid cell axes

--- packages/Curves/src/fit/fit-renderer.ts
+++ packages/Curves/src/fit/fit-renderer.ts
@@ -36,8 +36,22 @@ import {MultiCurveViewer} from './multi-curve-viewer';
 
 export const TAG_FIT_CHART_FORMAT = '.fitChartFormat';
 export const TAG_FIT_CHART_FORMAT_3DX = '3dx';
-export const CANDLESTICK_BORDER_PX_SIZE = 4;
-export const OUTLIER_HITBOX_RADIUS = 2;
+const MIN_CELL_RENDERER_PX_WIDTH = 20;
+const MIN_CELL_RENDERER_PX_HEIGHT = 10;
+const MIN_POINTS_AND_STATS_VISIBILITY_PX_WIDTH = 70;
+const MIN_POINTS_AND_STATS_VISIBILITY_PX_HEIGHT = 45;
+const OUTLIER_PX_SIZE = 12;
+const POINT_PX_SIZE = 4;
+const OUTLIER_HITBOX_RADIUS = 2;
+const MIN_AXES_CELL_PX_WIDTH = 70;
+const MIN_AXES_CELL_PX_HEIGHT = 55;
+const AXES_LEFT_PX_MARGIN = 30;
+const AXES_TOP_PX_MARGIN = 5;
+const AXES_BOTTOM_PX_MARGIN = 15;
+const CANDLESTICK_BORDER_PX_SIZE = 4;
+const CANDLESTICK_MEDIAN_PX_SIZE = 3.5;
+const CANDLESTICK_OUTLIER_PX_SIZE = 6;
+
 
 
 /** Merges properties of the two objects by iterating over the specified {@link properties}
@@ -76,12 +90,12 @@ export function getChartData(gridCell: DG.GridCell): IFitChartData {
 
 /** Performs a chart layout, returning [viewport, xAxis, yAxis] */
 export function layoutChart(rect: DG.Rect): [DG.Rect, DG.Rect?, DG.Rect?] {
-  if (rect.width < 70 || rect.height < 55)
+  if (rect.width < MIN_AXES_CELL_PX_WIDTH || rect.height < MIN_AXES_CELL_PX_HEIGHT)
     return [rect, undefined, undefined];
   return [
-    rect.cutLeft(30).cutBottom(30),
-    rect.getBottom(30).cutLeft(30),
-    rect.getLeft(30).cutBottom(30)
+    rect.cutLeft(AXES_LEFT_PX_MARGIN).cutBottom(AXES_BOTTOM_PX_MARGIN).cutTop(AXES_TOP_PX_MARGIN),
+    rect.getBottom(AXES_BOTTOM_PX_MARGIN).getTop(AXES_TOP_PX_MARGIN).cutLeft(AXES_LEFT_PX_MARGIN),
+    rect.getLeft(AXES_LEFT_PX_MARGIN).cutBottom(AXES_BOTTOM_PX_MARGIN).cutTop(AXES_TOP_PX_MARGIN)
   ];
 }
 
@@ -99,7 +113,7 @@ function drawCandlestick(g: CanvasRenderingContext2D, x: number, boxPlotStats: B
   g.lineTo(transform.xToScreen(x), transform.yToScreen(boxPlotStats.upperAdjacentValue));
   drawCandlestickBorder(g, x, boxPlotStats.upperAdjacentValue, transform);
   DG.Paint.marker(g, DG.MARKER_TYPE.CIRCLE, transform.xToScreen(x), transform.yToScreen(boxPlotStats.q2),
-    markerColor, 3.5 * ratio);
+    markerColor, CANDLESTICK_MEDIAN_PX_SIZE * ratio);
 }
 
 /** Performs points drawing */
@@ -112,7 +126,7 @@ function drawPoints(g: CanvasRenderingContext2D, series: IFitSeries,
       DG.Color.scatterPlotMarker;
     DG.Paint.marker(g,
       p.outlier ? DG.MARKER_TYPE.OUTLIER : (series.markerType as DG.MARKER_TYPE),
-      transform.xToScreen(p.x), transform.yToScreen(p.y), color, (p.outlier ? 12 : 4) * ratio);
+      transform.xToScreen(p.x), transform.yToScreen(p.y), color, (p.outlier ? OUTLIER_PX_SIZE : POINT_PX_SIZE) * ratio);
   }
 }
 
@@ -144,7 +158,7 @@ function drawCandles(g: CanvasRenderingContext2D, series: IFitSeries,
             DG.Paint.marker(g, DG.MARKER_TYPE.OUTLIER,
               transform.xToScreen(p.x), transform.yToScreen(values[ind]),
               series.pointColor ? DG.Color.fromHtml(series.pointColor) : DG.Color.scatterPlotMarker,
-              6 * ratio);
+              CANDLESTICK_OUTLIER_PX_SIZE * ratio);
           }
         }
       }
@@ -222,7 +236,7 @@ export class FitChartCellRenderer extends DG.GridCellRenderer {
         const p = data.series![i].points[j];
         const screenX = viewport.xToScreen(p.x);
         const screenY = viewport.yToScreen(p.y);
-        const pxPerMarkerType = ((p.outlier ? 6 : 4) / 2) + OUTLIER_HITBOX_RADIUS;
+        const pxPerMarkerType = ((p.outlier ? OUTLIER_PX_SIZE : POINT_PX_SIZE) / 2) + OUTLIER_HITBOX_RADIUS;
         if (e.offsetX >= screenX - pxPerMarkerType && e.offsetX <= screenX + pxPerMarkerType &&
           e.offsetY >= screenY - pxPerMarkerType && e.offsetY <= screenY + pxPerMarkerType) {
           p.outlier = !p.outlier;
@@ -269,7 +283,7 @@ export class FitChartCellRenderer extends DG.GridCellRenderer {
     viewport.drawCoordinateGrid(g, xAxisBox, yAxisBox);
 
     for (const series of data.series!) {
-      if (w < 70 || h < 45) {
+      if (w < MIN_POINTS_AND_STATS_VISIBILITY_PX_WIDTH || h < MIN_POINTS_AND_STATS_VISIBILITY_PX_HEIGHT) {
         series.showPoints = '';
         if (data.chartOptions)
           data.chartOptions.showStatistics = [];
@@ -342,7 +356,8 @@ export class FitChartCellRenderer extends DG.GridCellRenderer {
   render(g: CanvasRenderingContext2D,
          x: number, y: number, w: number, h: number,
          gridCell: DG.GridCell, cellStyle: DG.GridCellStyle): void {
-    if (w < 20 || h < 10) return;
+    if (w < MIN_CELL_RENDERER_PX_WIDTH || h < MIN_CELL_RENDERER_PX_HEIGHT)
+      return;
 
     const data = gridCell.cell.column.getTag(TAG_FIT_CHART_FORMAT) === TAG_FIT_CHART_FORMAT_3DX
       ? convertXMLToIFitChartData(gridCell.cell.value)
@@ -376,7 +391,7 @@ export class FitChartCellRenderer extends DG.GridCellRenderer {
         const p = data.series![i].points[j];
         const screenX = viewport.xToScreen(p.x);
         const screenY = viewport.yToScreen(p.y);
-        const pxPerMarkerType = ((p.outlier ? 6 : 4) / 2) + OUTLIER_HITBOX_RADIUS;
+        const pxPerMarkerType = ((p.outlier ? OUTLIER_PX_SIZE : POINT_PX_SIZE) / 2) + OUTLIER_HITBOX_RADIUS;
         if (e.offsetX >= screenX - pxPerMarkerType && e.offsetX <= screenX + pxPerMarkerType &&
           e.offsetY >= screenY - pxPerMarkerType && e.offsetY <= screenY + pxPerMarkerType) {
           document.body.style.cursor = 'pointer';
