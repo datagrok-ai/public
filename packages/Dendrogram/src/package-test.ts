627df5b92b223ebe0a3a30defe858577f237d738
Aleksandr Tanas <atanas@datagrok.ai>
2023-07-12 13:06:37 +0400

    GROK-13354: Bio | Tools: Rearrange top menu, Dendrogram (#2159)
    Bio top menu rearranged
    Fix tests

--- packages/Dendrogram/src/package-test.ts
+++ packages/Dendrogram/src/package-test.ts
@@ -12,6 +12,7 @@ import './tests/package-funcs-tests';
 import './tests/distance-matrix-tests';
 import './tests/calc-matrix-tests';
 import './tests/cluster-matrix-tests';
+import './tests/viewers';
 
 export const _package = new DG.Package();
 export {tests};
