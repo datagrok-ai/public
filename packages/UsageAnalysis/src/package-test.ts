394f54af73c49c2ac20754bcfe18e242746dcdb5
Anna Muzychyna <annamuz69@gmail.com>
2023-07-13 00:37:12 +0300

    GROK-12578 UsageAnalysis: tests

--- packages/UsageAnalysis/src/package-test.ts
+++ packages/UsageAnalysis/src/package-test.ts
@@ -1,7 +1,7 @@
 import * as DG from 'datagrok-api/dg';
 import {runTests, tests, TestContext} from '@datagrok-libraries/utils/src/test';
 
-import './tests/usage-analysis-tests';
+import './tests/app';
 
 export const _package = new DG.Package();
 export {tests};
