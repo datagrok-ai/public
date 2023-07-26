0c03751c6e93b958a60055094e1f29f7575d9491
Oleksandra Serhiienko <59337516+Aleksashka11@users.noreply.github.com>
2023-07-26 12:08:56 +0300

    #2098: Sunburst: If the text doesn't fit - remove it

--- packages/Charts/src/viewers/sunburst/sunburst-viewer.ts
+++ packages/Charts/src/viewers/sunburst/sunburst-viewer.ts
@@ -35,6 +35,10 @@ export class SunburstViewer extends EChartViewer {
           label: {
             rotate: 'radial',
           },
+          labelLayout: {
+            hideOverlap: 'true',
+            align: 'center',
+          }
         },
       ],
     };
