c0bf7606bbeeee2a42a90684a9fc1ef6006bad89
Oleksandra Serhiienko <59337516+Aleksashka11@users.noreply.github.com>
2023-08-01 17:09:47 +0400

    GROK-13260: Full Profile: Error loading Form

--- packages/ADMETox/src/package.ts
+++ packages/ADMETox/src/package.ts
@@ -3,6 +3,7 @@ import * as grok from 'datagrok-api/grok';
 import * as ui from 'datagrok-api/ui';
 import * as DG from 'datagrok-api/dg';
 import { getModelsSingle, addForm, addTooltip, addColorCoding, addPredictions } from './admet-analysis/admet-calculation';
+import { ColumnInputOptions } from '@datagrok-libraries/utils/src/type-declarations';
 
 export const _package = new DG.Package();
 
@@ -27,17 +28,35 @@ export async function admetoxCalculators() {
   addPredictions(table, col!);
 }
 
+//name: admeFormEditor
+//tags: editor
+//input: funccall call
+export async function admeFormEditor(call: DG.FuncCall) {
+  const molColumns = grok.shell.tv.dataFrame.columns.bySemTypeAll(DG.SEMTYPE.MOLECULE);
+  if (molColumns.length === 1) {
+    call.func.prepare({molecules: molColumns[0]}).call(true);
+  } else {
+    const colInput = ui.columnInput('Molecules', grok.shell.tv.dataFrame, molColumns[0], null, {filter: (col: DG.Column) => col.semType === DG.SEMTYPE.MOLECULE} as ColumnInputOptions);
+    ui.dialog({title: 'Full profile'})
+    .add(colInput)
+    .onOK(async () => {
+      call.func.prepare({molecules: colInput.value}).call(true);
+    })
+    .show();
+  }
+}
+
 //top-menu: Chem | ADME/Tox | Full Profile
 //description: Calculates all properties and visualizes on the form
 //name: Full Profile...
-export async function addFormViewer() {
+//input: column molecules {semType: Molecule}
+//editor: Admetox:admeFormEditor
+export async function addFormViewer(molecules: DG.Column) {
   const df = grok.shell.tv.dataFrame;
-  const col = df.columns.bySemType(DG.SEMTYPE.MOLECULE);
-  if (col)
-    await addForm(col, df);
+  await addForm(molecules, df);
   const layout = await _package.files.readAsText('form-layout.json');
   const tableName = df.name;
-  const modifiedLayout = layout.replaceAll("tableName", tableName).replaceAll("smilesColumn", col!.name);
+  const modifiedLayout = layout.replaceAll("tableName", tableName).replaceAll("smilesColumn", molecules.name);
   const view = grok.shell.tv;
   view.loadLayout(DG.ViewLayout.fromJson(modifiedLayout));
   addTooltip();
