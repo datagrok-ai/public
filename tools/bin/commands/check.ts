770d49557ee0dfb10d765d650d0170e25ed86007
Anna Muzychyna <annamuz69@gmail.com>
2023-08-11 11:51:21 +0300

    Tools: api version check fix

--- tools/bin/commands/check.ts
+++ tools/bin/commands/check.ts
@@ -307,9 +307,9 @@ export function checkPackageFile(packagePath: string, json: PackageFile, options
   if (api) {
     if (api === '../../js-api') {}
     else if (api === 'latest')
-      warnings.push('File "package.json": you should specify Datagrok API version constraint.');
-    else if (!(api.startsWith('^') || api.startsWith('>')))
-      warnings.push('File "package.json": Datagrok API version should contain "^" or ">" symbol, otherwise it is locked to the single Datagrok version.');
+      warnings.push('File "package.json": you should specify Datagrok API version constraint (for example ^1.16.0, >=1.16.0).');
+    else if (!/^(\^|>|<|~).+/.test(api))
+      warnings.push('File "package.json": Datagrok API version should starts with > | >= | ~ | ^ | < | <=');
   }
 
   const dt = json.devDependencies?.['datagrok-tools'] ?? json.dependencies?.['datagrok-tools'];
