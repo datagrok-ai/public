90df79f57098285c95ea7023c81a84d3c7ac64c9
Dmytro Kovalyov <laykdimon278@gmail.com>
2023-08-11 15:21:02 +0300

    Curves: Fixed transform tests

--- packages/Curves/src/tests/transform-tests.ts
+++ packages/Curves/src/tests/transform-tests.ts
@@ -1,21 +1,21 @@
 import * as DG from 'datagrok-api/dg';
 
 import {Viewport} from '@datagrok-libraries/utils/src/transform';
-import {layoutChart} from '../fit/fit-renderer';
+import {layoutChart, INFLATE_SIZE} from '../fit/fit-renderer';
 
 import {category, expect, test} from '@datagrok-libraries/utils/src/test';
 
 
 category('viewport', () => {
   test('viewportMethods', async () => {
-    const screenBounds = new DG.Rect(120, 20, 160, 100).inflate(-6, -6);
+    const screenBounds = new DG.Rect(120, 20, 160, 100).inflate(INFLATE_SIZE / 2, INFLATE_SIZE / 2);
     const [dataBox] = layoutChart(screenBounds);
     const dataBounds = new DG.Rect(0.1, 0.06, 7, 5.94);
     const viewport = new Viewport(dataBounds, dataBox, false, false);
 
-		expect(viewport.xToScreen(1.5), 179.6);
-		expect(viewport.yToScreen(2.33), 73.01346801346801);
-		expect(viewport.xToWorld(150), -0.2559322033898306);
-		expect(viewport.yToWorld(67), 2.855294117647059);
+		expect(viewport.xToScreen(1.5), 182.4);
+		expect(viewport.yToScreen(2.33), 58.803030303030305);
+		expect(viewport.xToWorld(150), -0.9652173913043479);
+		expect(viewport.yToWorld(67), 1.2479999999999998);
   });
 });
