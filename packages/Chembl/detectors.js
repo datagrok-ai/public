5bb2baec7ab8bc1475a0351abdea9b969c7156b4
Oleksandra Serhiienko <59337516+Aleksashka11@users.noreply.github.com>
2023-08-25 17:48:27 +0300

    Chembl: Info panel (#2318)
    * Chembl: Compound-related info panels
    
    * Chembl: Update queries and sample
    
    * Chembl: Update detectors

--- packages/Chembl/detectors.js
+++ packages/Chembl/detectors.js
@@ -5,7 +5,17 @@ class ChemblPackageDetectors extends DG.Package {
   //output: string semType
   detectMolRegNo(col) {
     if (col.name === 'molregno')
-      return 'chembl:molregno';
+      return 'molregno';
+    return null;
+  }
+
+  //tags: semTypeDetector
+  //input: column col
+  //output: string semType
+  detectChembl(col) {
+    const chembl = Array.from(col.values()).filter((val) => val.startsWith('CHEMBL'));
+    if (chembl.length == col.length)
+      return 'chembl';
     return null;
   }
 }
