1acd94e62cfcf3df67c639b8ef80e1fa012e377a
Alexey <achopovsky@datagrok.ai>
2023-08-28 17:29:39 +0300

    Bio | Enumerator: WIP

--- packages/Bio/src/package.ts
+++ packages/Bio/src/package.ts
@@ -65,6 +65,7 @@ import {PackageSettingsEditorWidget} from './widgets/package-settings-editor-wid
 import {getCompositionAnalysisWidget} from './widgets/composition-analysis-widget';
 import {MacromoleculeColumnWidget} from './utils/macromolecule-column-widget';
 import {addCopyMenuUI} from './utils/context-menu';
+import {getEnumeratorWidget, _runEnumerator} from './utils/enumerator-tools';
 
 export const _package = new BioPackage();
 
@@ -836,43 +837,7 @@ export async function demoBioHelmMsaSequenceSpace(): Promise<void> {
 
 //name: runEnumerator
 export async function runEnumerator(): Promise<void> {
-  const df: DG.DataFrame = await _package.files.readCsv('samples/enumerator.csv');
-  const separatorCol = df.col('Synthesis');
-  separatorCol?.setTag('quality', 'Macromolecule');
-  separatorCol?.setTag('units', 'separator');
-  separatorCol?.setTag('separator', '-');
-  separatorCol?.setTag('cell.renderer', 'sequence');
-  separatorCol?.setTag('aligned', 'SEQ');
-  separatorCol?.setTag('alphabet', 'PT');
-  const tableView = DG.TableView.create(df);
-  grok.shell.addView(tableView);
-}
-
-async function enumerator(molColumn: DG.Column): Promise<void> {
-  function getCyclicHelm(inputHelm: string): string {
-    const leftWrapper = 'PEPTIDE1{';
-    const rightWrapper = '}$$$$';
-    const seq = inputHelm.replace(leftWrapper, '').replace(rightWrapper, '');
-    const lastMonomerNumber = seq.split('.').length;
-    const result = inputHelm.replace(rightWrapper,
-      `}$PEPTIDE1,PEPTIDE1,${lastMonomerNumber}:R2-1:R1${'$'.repeat(6)}V2.0`);
-    console.log('result:', result);
-    return result;
-  }
-
-  const df = molColumn.dataFrame;
-  const nc = new NotationConverter(molColumn);
-  const sourceHelmCol = nc.convert(NOTATION.HELM);
-  df.columns.add(sourceHelmCol);
-  const targetList = sourceHelmCol.toList().map((helm) => getCyclicHelm(helm));
-  const targetHelmCol = DG.Column.fromList('string', 'Enumerator(helm)', targetList);
-  targetHelmCol.setTag('quality', 'Macromolecule');
-  targetHelmCol.setTag('units', 'helm');
-  targetHelmCol.setTag('cell.renderer', 'helm');
-  targetHelmCol.setTag('aligned', 'SEQ');
-  targetHelmCol.setTag('alphabet', 'PT');
-  df.columns.add(targetHelmCol);
-  await grok.data.detectSemanticTypes(df);
+  _runEnumerator();
 }
 
 //name: Enumerator
@@ -880,9 +845,5 @@ async function enumerator(molColumn: DG.Column): Promise<void> {
 //tags: panel, exclude-actions-panel
 //output: widget result
 export function molColumnPropertyPanel(molColumn: DG.Column): DG.Widget {
-  const btn = ui.bigButton('Run', async () => enumerator(molColumn));
-
-  const div = ui.div([btn]);
-
-  return new DG.Widget(div);
+  return getEnumeratorWidget(molColumn);
 }
