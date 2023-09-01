20a2414ef908d6e4b097e64e9a8358f35ecb8aea
Sofia Podolskaia <86597056+dnillovna@users.noreply.github.com>
2023-09-01 14:48:51 +0300

    GitHub Actions: IaC (#2326)
    * GitHub Actions: Iac
    
    * Help: Deploy: CFN load balancer type
    
    * Help: Deploy | CFN: Allow access to LB from VPC
    
    * Help: Deploy | CFN: Allow Lambda in VPC
    
    * Help: Deploy | CFN: Datagrok prefix for resources
    
    * Help: Deploy| CFN: Ignore YAMLIncompatibleTypes
    
    * GitHub Actions: IaC: Commit CFN files from github actions actor
    
    * Help: Deploy: CFN: Autoname for merged files
    
    * Help: Deploy: CFN: Fix versions description
    
    * GitHub Actions: IaC: Snyk changed files
    
    * GitHub Actions: Docusaurus: Trigger on specific files only
    
    * GitHub Actions: IaC: Trigger on deploy changes only
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- help/deploy/aws/cloudformation/templates/cf_template_merge.py
+++ help/deploy/aws/cloudformation/templates/cf_template_merge.py
@@ -3,6 +3,7 @@
 import argparse
 import yaml
 from deepmerge import always_merger
+import os
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
@@ -15,16 +16,22 @@ parser.add_argument(
     "-o",
     "--output",
     help="Path for the result file",
-    default='cloudformation.yaml'
+    default=''
 )
 args = parser.parse_args()
 
 result = {}
+output_names=[]
 for i in args.input:
     with open(i, 'r') as file:
         data = yaml.safe_load(file)
     result = always_merger.merge(result, data)
+    output_names += os.path.basename(i).replace('.template.yml', '')
 
-# Write the merged dictionary to a new file
+if args.output:
+    output = args.output
+else:
+    output = "-".join(output_names) + '.yml'
+
 with open(args.output, 'w') as file:
     yaml.dump(result, file, explicit_start=True)
