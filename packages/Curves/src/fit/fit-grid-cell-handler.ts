efba1d2dfce3830ca094f4a027a78c2df568fc82
Dmytro Kovalyov <laykdimon278@gmail.com>
2023-09-01 20:07:34 +0300

    #2103: Curves: Resturcutred property panel and made it work

--- packages/Curves/src/fit/fit-grid-cell-handler.ts
+++ packages/Curves/src/fit/fit-grid-cell-handler.ts
@@ -7,14 +7,17 @@ import {
   getSeriesStatistics,
   getSeriesFitFunction,
 } from '@datagrok-libraries/statistics/src/fit/fit-data';
-import {statisticsProperties, fitSeriesProperties, fitChartDataProperties, FIT_CELL_TYPE, TAG_FIT} from '@datagrok-libraries/statistics/src/fit/fit-curve';
-import {getChartData} from './fit-renderer';
+import {statisticsProperties, fitSeriesProperties, fitChartDataProperties, FIT_CELL_TYPE, TAG_FIT, IFitChartData, IFitSeries, IFitChartOptions} from '@datagrok-libraries/statistics/src/fit/fit-curve';
+import {TAG_FIT_CHART_FORMAT, TAG_FIT_CHART_FORMAT_3DX, getChartData, mergeProperties} from './fit-renderer';
 import {MultiCurveViewer} from './multi-curve-viewer';
+import {convertXMLToIFitChartData} from './fit-parser';
 
 
 const SOURCE_COLUMN_TAG = '.sourceColumn';
 const SERIES_NUMBER_TAG = '.seriesNumber';
 const STATISTICS_TAG = '.statistics';
+const CHART_OPTIONS = 'chartOptions';
+const SERIES_OPTIONS = 'seriesOptions';
 
 
 function addStatisticsColumn(chartColumn: DG.GridColumn, p: DG.Property, seriesNumber: number): void {
@@ -34,6 +37,31 @@ function addStatisticsColumn(chartColumn: DG.GridColumn, p: DG.Property, seriesN
   grid.dataFrame.columns.add(column);
 }
 
+function changeCurvesOptions(gridCell: DG.GridCell, columnChartOptions: IFitChartData,
+  inputBase: DG.InputBase, options: string): void {
+  gridCell.cell.column.tags[TAG_FIT] = JSON.stringify(columnChartOptions);
+  for (let i = 0; i < gridCell.cell.column.length; i++) {
+    const value = gridCell.cell.column.get(i);
+    if (value === '') continue;
+    const chartData: IFitChartData = gridCell.cell.column.getTag(TAG_FIT_CHART_FORMAT) === TAG_FIT_CHART_FORMAT_3DX ?
+      convertXMLToIFitChartData(value) : JSON.parse(value ?? '{}') ?? {};
+
+    if (options === CHART_OPTIONS) {
+      if (chartData.chartOptions === undefined) continue;
+      (chartData.chartOptions[inputBase.property.caption as keyof IFitChartOptions] as any) = inputBase.value;
+    }
+    else if (options === SERIES_OPTIONS) {
+      if (chartData.series === undefined) continue;
+      for (let j = 0; j < chartData.series.length; j++)
+        (chartData.series[j][inputBase.property.caption as keyof IFitSeries] as any) = inputBase.value;
+    }
+    gridCell.cell.column.set(i, JSON.stringify(chartData));
+  }
+  if (gridCell.cell.column.getTag(TAG_FIT_CHART_FORMAT) === TAG_FIT_CHART_FORMAT_3DX)
+    gridCell.cell.column.setTag(TAG_FIT_CHART_FORMAT, '');
+  gridCell.grid.invalidate();
+}
+
 
 export class FitGridCellHandler extends DG.ObjectHandler {
   get type(): string {
@@ -48,14 +76,21 @@ export class FitGridCellHandler extends DG.ObjectHandler {
     const acc = ui.accordion();
     const chartData = getChartData(gridCell);
     const columnChartOptions = getColumnChartOptions(gridCell.cell.column);
-    const refresh = {onValueChanged: (_: any) => {
-      gridCell.cell.column.tags[TAG_FIT] = JSON.stringify(columnChartOptions);
-      gridCell.grid.invalidate();
-    }};
+    const seriesOptionsRefresh = {onValueChanged: (inputBase: DG.InputBase) => 
+      changeCurvesOptions(gridCell, columnChartOptions, inputBase, SERIES_OPTIONS)};
+    const chartOptionsRefresh = {onValueChanged: (inputBase: DG.InputBase) =>
+      changeCurvesOptions(gridCell, columnChartOptions, inputBase, CHART_OPTIONS)};
+
+    mergeProperties(fitSeriesProperties, columnChartOptions.seriesOptions, chartData.seriesOptions ? chartData.seriesOptions :
+      chartData.series ? chartData.series[0] ?? {} : {});
+    mergeProperties(fitChartDataProperties, columnChartOptions.chartOptions,
+      chartData.chartOptions ? chartData.chartOptions : {});
 
     acc.addPane('Options', () => ui.divV([
-      ui.input.form(columnChartOptions.seriesOptions, fitSeriesProperties, refresh),
-      ui.input.form(columnChartOptions.chartOptions, fitChartDataProperties, refresh),
+      ui.h3('Series options'),
+      ui.input.form(chartData.seriesOptions ? chartData.seriesOptions : chartData.series![0], fitSeriesProperties, seriesOptionsRefresh),
+      ui.h3('Chart options'),
+      ui.input.form(chartData.chartOptions, fitChartDataProperties, chartOptionsRefresh),
     ]));
 
     acc.addPane('Fit', () => {
