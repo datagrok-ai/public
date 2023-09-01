dc85727e6b7deadbde18c55f560dc2ea5e5c5179
Sofia Podolsky <spodolskaya@datagrok.ai>
2023-09-01 14:23:13 +0300

    Help: Deploy: CFN: Autoname for merged files

--- help/deploy/aws/cloudformation/templates/cf_template_merge.py
+++ help/deploy/aws/cloudformation/templates/cf_template_merge.py
@@ -3,6 +3,7 @@
 import argparse
 import yaml
 from deepmerge import always_merger
+import os
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
@@ -15,15 +16,22 @@ parser.add_argument(
     "-o",
     "--output",
     help="Path for the result file",
-    default='cloudformation.yaml'
+    default=''
 )
 args = parser.parse_args()
 
 result = {}
+output_names=[]
 for i in args.input:
     with open(i, 'r') as file:
         data = yaml.safe_load(file)
     result = always_merger.merge(result, data)
+    output_names += os.path.basename(i).replace('.template.yml', '')
+
+if args.output:
+    output = args.output
+else:
+    output = "-".join(output_names)
 
 with open(args.output, 'w') as file:
     yaml.dump(result, file, explicit_start=True)
