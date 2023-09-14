6ee5a3e387ee5490b6ee39a6e78fb58e607885bf
Dmitry Illarionov <illarionow@yahoo.com>
2023-09-14 13:37:27 +0200

    ApiSamples: Update UI samples

--- packages/ApiSamples/scripts/ui/components/color-palettes.js
+++ packages/ApiSamples/scripts/ui/components/color-palettes.js
@@ -3,7 +3,7 @@
 let v = grok.shell.newView('palettes');
 
 function getBlock(c) {
-  let block = ui.divText(DG.Color.toRgb(c));
+  let block = ui.div([DG.Color.toRgb(c),'  hex:', DG.Color.toHtml(c)]);
   block.style.backgroundColor = DG.Color.toRgb(c);
   block.style.color = DG.Color.toRgb(DG.Color.getContrastColor(c));
   return block;
@@ -12,6 +12,6 @@ function getBlock(c) {
 v.appendAll([
   ui.h1('Categorical palette with contrast text color'),
   ui.div(DG.Color.categoricalPalette.map(getBlock)),
-  ui.h1('Category colors (looping over the palette)'),
-  ui.div(DG.utils.identity(30).map(DG.Color.getCategoricalColor).map(getBlock))
+  ui.h1('Get contrast color from white'),
+  ui.div(getBlock(DG.Color.getContrastColor(DG.Color.white)))
 ]);
