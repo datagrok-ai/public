e42f7bafd42f42b54f5a7fc43c555d73644b633e
tanas80@gmail.com <tanas80@gmail.com>
2023-09-14 19:15:02 +0400

    GROK-13472: Bio: VdRegionsViewer fit width, Bio
    Investigating test failed with and Unhandled exception (disabled 3/4)

--- packages/Bio/src/package-test.ts
+++ packages/Bio/src/package-test.ts
@@ -17,22 +17,22 @@ import './tests/converters-test';
 import './tests/fasta-handler-test';
 import './tests/fasta-export-tests';
 
-import './tests/bio-tests';
-import './tests/WebLogo-positions-test';
-import './tests/checkInputColumn-tests';
-import './tests/pepsea-tests';
-import './tests/viewers';
-import './tests/units-handler-tests';
-import './tests/units-handler-splitted-tests';
+// import './tests/bio-tests';
+// import './tests/WebLogo-positions-test';
+// import './tests/checkInputColumn-tests';
+// import './tests/pepsea-tests';
+// import './tests/viewers';
+// import './tests/units-handler-tests';
+// import './tests/units-handler-splitted-tests';
 
-// import './tests/units-handler-get-region';
-// import './tests/to-atomic-level-tests';
-// import './tests/mm-distance-tests';
-// import './tests/similarity-diversity-tests';
-// import './tests/substructure-filters-tests';
-// import './tests/activity-cliffs-tests';
-// import './tests/sequence-space-test';
-// import './tests/scoring';
+import './tests/units-handler-get-region';
+import './tests/to-atomic-level-tests';
+import './tests/mm-distance-tests';
+import './tests/similarity-diversity-tests';
+import './tests/substructure-filters-tests';
+import './tests/activity-cliffs-tests';
+import './tests/sequence-space-test';
+import './tests/scoring';
 
 
 export const _package = new DG.Package();
