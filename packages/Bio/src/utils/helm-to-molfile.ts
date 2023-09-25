c8714f2a63f66025493f7cbf9a0fe38791fd1af7
Alexey <achopovsky@datagrok.ai>
2023-09-25 13:14:41 +0300

    Bio: helm to molfile, WIP

--- packages/Bio/src/utils/helm-to-molfile.ts
+++ packages/Bio/src/utils/helm-to-molfile.ts
@@ -2,6 +2,12 @@
 import * as grok from 'datagrok-api/grok';
 import * as ui from 'datagrok-api/ui';
 import * as DG from 'datagrok-api/dg';
+import {MonomerLibHelper} from './monomer-lib';
+
+import {MolfileHandler} from '@datagrok-libraries/chem-meta/src/parsing-utils/molfile-handler';
+import {MolfileHandlerBase} from '@datagrok-libraries/chem-meta/src/parsing-utils/molfile-handler-base';
+import {HELM_POLYMER_TYPE} from '@datagrok-libraries/bio/src/utils/const';
+import {Monomer} from '@datagrok-libraries/bio/src/types';
 
 export class HelmToMolfileConverter {
   constructor(private helmColumn: DG.Column<string> ) {
@@ -9,12 +15,60 @@ export class HelmToMolfileConverter {
   }
 
   async convert(): Promise<DG.Column<string>> {
-    const polymerMolfileCol: DG.Column<string> = await this.getPolymerMolfiles();
-    return polymerMolfileCol;
+    const helmCoordinatesColumn: DG.Column<string> = await this.getHelmCoordinatesColumn();
+    helmCoordinatesColumn.toList().forEach(
+      (pseudoMolfile: string) => {
+        this.getPolymerMolfile(pseudoMolfile);
+      });
+    return helmCoordinatesColumn;
   }
 
-  async getPolymerMolfiles(): Promise<DG.Column<string>> {
+  async getHelmCoordinatesColumn(): Promise<DG.Column<string>> {
     const polymerMolfileCol: DG.Column<string> = await grok.functions.call('HELM:getMolfiles', {col: this.helmColumn});
     return polymerMolfileCol;
   }
+
+  private getPolymerMolfile(helmCoordinates: string): string {
+    const meta = new MonomerShiftMetadata(helmCoordinates);
+    console.log('symbols:', meta.monomerSymbols);
+    meta.monomerSymbols.forEach((symbol: string, idx: number) => {
+      const shift = meta.getMonomerShifts(idx);
+      console.log('shift:', shift);
+    });
+    return '';
+  }
+}
+
+class MonomerShiftMetadata {
+  constructor(helmCoordinates: string) {
+    this.molfileHandler = MolfileHandler.getInstance(helmCoordinates);
+  }
+
+  private molfileHandler: MolfileHandlerBase;
+
+  get monomerSymbols(): string[] {
+    return this.molfileHandler.atomTypes;
+  }
+
+  getMonomerShifts(monomerIdx: number): {x: number, y: number} {
+    const x = this.molfileHandler.x[monomerIdx];
+    const y = this.molfileHandler.y[monomerIdx];
+    return {x, y};
+  }
+}
+
+class MonomerWrapper {
+  constructor(monomerSymbol:string) {
+    const monomerLib = MonomerLibHelper.instance.getBioLib();
+    const monomer = monomerLib.getMonomer(HELM_POLYMER_TYPE.PEPTIDE, monomerSymbol);
+    if (!monomer)
+      throw new Error(`Monomer ${monomerSymbol} is not found in the library`);
+    this.monomer = monomer;
+  }
+
+  private monomer: Monomer;
+}
+
+class MolfileCompiler {
+  constructor() { }
 }
