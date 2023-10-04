5a59c03cacdaa30a19c76495d33378ebbded0de9
Anna Muzychyna <annamuz69@gmail.com>
2023-10-04 12:58:44 +0300

    GROK-13680 JS API Test: DataFrame

--- packages/ApiTests/src/dataframe/dataframe.ts
+++ packages/ApiTests/src/dataframe/dataframe.ts
@@ -1,9 +1,7 @@
 import * as DG from 'datagrok-api/dg';
-import * as grok from 'datagrok-api/grok';
+// import * as grok from 'datagrok-api/grok';
 
-import dayjs from 'dayjs';
-import {hashDataFrame} from '@datagrok-libraries/utils/src/dataframe-utils';
-import {category, expect, expectArray, test, expectTable} from '@datagrok-libraries/utils/src/test';
+import {category, expect, expectArray, test, expectTable, expectFloat} from '@datagrok-libraries/utils/src/test';
 
 const createDf = (): DG.DataFrame => {
   const df = DG.DataFrame.create(2);
@@ -19,14 +17,13 @@ const createDf2 = (): DG.DataFrame => {
   return df;
 };
 
-category('DataFrame: Methods', () => {
+const createCol = (): DG.Column => DG.Column.fromStrings('countries', ['USA', 'Canada', 'France', 'Mexico']);
 
-  test('create from typed arrays', async () => {
-    DG.DataFrame.fromColumns([
-      DG.Column.fromInt32Array('ints', new Int32Array(100)),
-      DG.Column.fromFloat32Array('floats', new Float32Array(100)),
-    ]);
-  });
+const createCol2 = (): DG.Column => DG.Column.fromInt32Array('population', Int32Array.from([1, 4, 2, 3]));
+
+// DataFrame
+
+category('DataFrame: Methods', () => {
 
   test('append', async () => {
     const df1 = createDf();
@@ -136,6 +133,9 @@ category('DataFrame: Methods', () => {
     const data = t.toCsv();
     const t2 = DG.DataFrame.fromCsv(data);
     expectTable(t2, t);
+    const df = DG.DataFrame.fromColumns([]);
+    const csv = df.toCsv();
+    expect(csv, '');
   });
 
   test('toJson | fromJson', async () => {
@@ -192,15 +192,206 @@ Canada,4`;
   });
 
   test('fromProperties', async () => {
-    
+    const prop1 = DG.Property.fromOptions({name: 'countries', type: 'string'});
+    const prop2 = DG.Property.fromOptions({name: 'population', type: 'int'});
+    const df = DG.DataFrame.fromProperties([prop1, prop2], 4);
+    expect(df.columns.length, 2);
+    expect(df.rowCount, 4);
   });
 });
 
+// Column
+
+category('DataFrame: Column', () => {
+  const COL1 = createCol();
+  const COL2 = createCol2();
+
+  test('aggregate', async () => {
+    expect(COL1.aggregate(DG.STR_AGG.CONCAT_ALL), 'USA,Canada,France,Mexico');
+    expect(COL2.aggregate(DG.AGG.SUM), 10);
+  });
+
+  test('applyFormula', async () => {
+    const df = createDf2();
+    const col = df.getCol('population');
+    await col.applyFormula('${population} + 5', 'int');
+    expectArray(df.getCol('population').toList(), [6, 9, 7, 8]);
+  });
+
+  // test('compact', async () => {
+  // });
+
+  test('convertTo', async () => {
+    const col = createCol2();
+    expectArray(col.convertTo('string').toList(), ['1', '4', '2', '3']);
+  });
+
+  test('get', async () => {
+    expect(COL2.get(0), 1);
+    expect(COL2.get(1), 4);
+  });
+
+  test('getCategory', async () => {
+    expect(COL1.getCategory(0), 'Canada');
+    expect(COL1.getCategory(1), 'France');
+  });
+
+  test('getCategoryOrder | setCategoryOrder', async () => {
+    const col = createCol();
+    col.setCategoryOrder(['Canada', 'France', 'USA', 'Mexico']);
+    expectArray(col.getCategoryOrder(), ['Canada', 'France', 'USA', 'Mexico']);
+  });
+
+  test('getRawData | setRawData', async () => {
+    const col = DG.Column.int('col', 3);
+    col.setRawData(Int32Array.from([1, 2, 3]));
+    expectArray(col.getRawData(), [1, 2, 3]);
+  }, {skipReason: 'skip'});
+
+  test('getSortedOrder', async () => {
+    expectArray(COL1.getSortedOrder(), [1, 2, 3, 0]);
+    expectArray(COL2.getSortedOrder(), [0, 2, 3, 1]);
+  });
+
+  test('getString | setString', async () => {
+    const col = createCol();
+    expect(col.getString(0), 'USA');
+    col.setString(1, '20');
+    expect(col.getString(1), '20');
+  });
+
+  test('getTag | setTag', async () => {
+    const col = createCol();
+    col.setTag('tag', 'value');
+    expect(col.getTag('tag'), 'value');
+  });
+
+  test('init', async () => {
+    const col = DG.Column.int('col', 5);
+    col.init((i) => i + 1);
+    expectArray(col.toList(), [1, 2, 3, 4, 5]);
+  });
+
+  test('isNone', async () => {
+    const col = DG.Column.int('col', 5);
+    expect(col.isNone(0), true);
+    expect(COL1.isNone(0), false);
+  });
+
+  test('matches', async () => {
+    expect(COL1.matches('string'), true);
+    expect(COL1.matches('int'), false);
+  });
+
+  test('scale', async () => {
+    expect(COL2.scale(0), 0);
+    expect(COL2.scale(1), 1);
+    expectFloat(COL2.scale(2), 0.33, 0.01);
+    expectFloat(COL2.scale(3), 0.66, 0.01);
+  });
+
+  test('set', async () => {
+    const col = createCol();
+    col.set(0, 'UK');
+    expect(col.get(0), 'UK');
+  });
+  
+  test('toList', async () => {
+    expectArray(createCol2().toList(), [1, 4, 2, 3]);
+  });
+  
+  test('toString', async () => {
+    expect(COL1.toString(), 'countries');
+  });
+  
+  test('values', async () => {
+    const gen = COL1.values()[Symbol.iterator]();
+    expect(gen.next().value, 'USA');
+    expect(gen.next().value, 'Canada');
+  });
+  
+  test('bool', async () => {
+    const col = DG.Column.bool('col', 3);
+    col.init((_) => true);
+    expect(col.type, 'bool');
+    expectArray(col.toList(), [true, true, true]);
+  });
+  
+  test('dataFrame', async () => {
+    const col = DG.Column.dataFrame('col', 3);
+    expect(col.type, 'dataframe');
+  });
+  
+  test('dateTime', async () => {
+    const col = DG.Column.dateTime('col', 3);
+    col.init((_) => Date.now());
+    expect(col.type, 'datetime');
+  });
+  
+  test('float', async () => {
+    const col = DG.Column.float('col', 3);
+    col.init((_) => 1.1);
+    expect(col.type, 'double');
+    expectFloat(col.get(0) ?? 0, 1.1);
+  });
+  
+  // test('fromBitSet', async () => {
+    
+  // });
+  
+  test('fromFloat32Array', async () => {
+    const col = DG.Column.fromFloat32Array('col', Float32Array.from([1.23, 4.45, 3.34]), 3);
+    expectFloat(col.get(0) ?? 0, 1.23);
+  });
+  
+  test('fromInt32Array', async () => {
+    const col = DG.Column.fromInt32Array('col', Int32Array.from([1, 2, 3]), 3);
+    expect(col.get(0) ?? 0, 1);
+  });
+
+  test('fromList', async () => {
+    const col = DG.Column.fromList('string', 'col', ['a', 'b', 'c']);
+    expect(col.get(0), 'a');
+  });
+
+  test('fromStrings', async () => {
+    const col = DG.Column.fromStrings('col', ['a', 'b', 'c']);
+    expect(col.get(0), 'a');
+  });
+
+  test('fromType', async () => {
+    const col = DG.Column.fromType('bigint', 'col', 5);
+    expect(col.type, 'bigint');
+  });
+
+  test('int', async () => {
+    const col = DG.Column.int('col', 3);
+    col.init((_) => 1);
+    expect(col.get(1), 1);
+  });
+
+  test('qnum', async () => {
+    const col = DG.Column.qnum('col', 3, [1, 2, 3]);
+    expectFloat(col.get(0) ?? 0, 1);
+  });
+
+  test('string', async () => {
+    const col = DG.Column.string('col', 3);
+    col.init((_) => 'val');
+    expect(col.get(0), 'val');
+  });
+});
+
+// ColumnList
+
 category('DataFrame: ColumnList', () => {
-  test('create from columns', async () => {
-    const t = DG.DataFrame.create(3);
-    t.columns.add(DG.Column.fromStrings('countries', ['USA', 'Canada', 'Mexico']));
-    t.columns.add(DG.Column.fromStrings('population', ['321', '35', '121']));
+
+  test('iterator', async () => {
+    const df = createDf();
+    const gen = df.columns[Symbol.iterator]();
+    const col: DG.Column = gen.next().value;
+    expect(col.get(0), 'USA');
+    expect(col.get(1), 'Canada');
   });
 
   test('add', async () => {
@@ -221,6 +412,14 @@ category('DataFrame: ColumnList', () => {
     df.columns.addNewBool('newColumnBool');
     expect(typeof(df.get('newColumnBool', 1)), 'boolean');
   });
+
+  test('addNewBytes', async () => {
+    const df = createDf();
+    df.columns.addNewBytes('newColumnBytes');
+    expect(typeof(df.get('newColumnBytes', 1)), 'object');
+  });
+
+  // addNewCalculated in separate file
 
   test('addNewDateTime', async () => {
     const df = createDf2();
@@ -252,6 +451,12 @@ category('DataFrame: ColumnList', () => {
     expect(typeof(df.get('newColumnString', 1)), 'string');
   });
 
+  test('addNewVirtual', async () => {
+    const df = createDf();
+    df.columns.addNewVirtual('newColumnVirtual', (i) => i * 2, DG.TYPE.INT);
+    expect(typeof(df.get('newColumnVirtual', 1)), 'number');
+  });
+
   test('byIndex', async () => {
     const df = createDf2();
     expect(df.columns.byIndex(1).getRawData().toString(), '1,4,2,3');
@@ -260,6 +465,13 @@ category('DataFrame: ColumnList', () => {
   test('byName', async () => {
     const df = createDf2();
     expect(df.columns.byName('population').getRawData().toString(), '1,4,2,3');
+  });
+
+  test('byNames', async () => {
+    const df = createDf();
+    const [col1, col2] = df.columns.byNames(['countries', 'population']);
+    expect(col1.toList().toString(), 'USA,Canada');
+    expect(col2.getRawData().toString(), '1,4');
   });
 
   test('bySemType', async () => {
@@ -278,9 +490,35 @@ category('DataFrame: ColumnList', () => {
     });
   });
 
+  test('bySemTypesExact', async () => {
+    const df1 = createDf();
+    df1.onSemanticTypeDetected.subscribe((_) => {
+      const res = df1.columns.bySemTypesExact(['country']);
+      expect(res?.toString(), 'countries');
+    });
+  });
+
+  test('byTags', async () => {
+    const df = createDf();
+    df.col('countries')?.setTag('tag1', 'value1');
+    df.col('population')?.setTag('tag2', 'value2');
+    const res1 = [...df.columns.byTags({tag1: 'value1'})];
+    const res2 = [...df.columns.byTags({tag2: undefined})];
+    expect(res1[0].name, 'countries');
+    expect(res2[0].name, 'population');
+  });
+
   test('contains', async () => {
     const df = createDf2();
     expect(df.columns.contains('population'), true);
+  });
+
+  test('getOrCreate', async () => {
+    const df = createDf();
+    df.columns.getOrCreate('countries', 'string', 2);
+    expect(df.columns.length, 2);
+    df.columns.getOrCreate('countries1', 'string', 2);
+    expect(df.columns.length, 3);
   });
 
   test('getUnusedName', async () => {
@@ -324,62 +562,115 @@ category('DataFrame: ColumnList', () => {
   });
 });
 
+// Row
+
+category('DataFrame: Row', () => {
+  const row = createDf().row(0);
+
+  test('get', async () => {
+    expect(row.get('countries'), 'USA');
+    expect(row.get('population'), 1);
+  });
+
+  test('toDart', async () => {
+    expect(typeof row.toDart(), 'object');
+  });
+});
+
+// RowList
+
 category('DataFrame: RowList', () => {
-  test('row list addNew', async () => {
+
+  test('iterator', async () => {
+    const df = createDf();
+    const gen = df.rows[Symbol.iterator]();
+    const row = gen.next().value as DG.Row;
+    expect(row.countries, 'USA');
+    expect(row.population, 1);
+  });
+
+  test('addFilterState', async () => {
+    const state = {
+      column: 'population',
+      type: 'string',
+    };
+    const df = createDf();
+    df.rows.addFilterState(state);
+  });
+
+  test('addNew', async () => {
     const df5 = createDf();
     df5.rows.addNew(['12', 23]);
     expect(df5.get('countries', df5.rowCount - 1).toString(), '12');
   });
 
-  test('row list filter', async () => {
-    const df1 = createDf();
-    return df1.rows.filter((row) => row.countries === 'USA');
+  test('filter', async () => {
+    const df = createDf2();
+    df.rows.filter((r) => r.get('population') < 3);
+    expect(df.filter.trueCount, 2);
   });
 
-  test('row list insertAt', async () => {
+  test('get', async () => {
+    const df = createDf();
+    expectArray([...df.rows.get(0).cells].map((i) => i.value), ['USA', 1]);
+  });
+
+  test('highlight', async () => {
+    const df = createDf2();
+    df.rows.highlight((i) => i > 1);
+  });
+
+  test('insertAt', async () => {
     const df6 = createDf();
     df6.rows.insertAt(2, 2);
     expect(df6.get('countries', 2), '');
   });
 
-  test('row list match', async () => {
+  test('match', async () => {
     const df5 = createDf();
     return df5.rows.match('countries = USA').highlight();
   });
 
-  test('row list removeWhere', async () => {
+  test('removeAt', async () => {
     const df = createDf2();
-    df.rows.removeWhere((r) => {
-      return r.get('population') < 3;
-    });
+    df.rows.removeAt(1, 2);
     expect(df.rowCount, 2);
   });
 
-  test('datetime column', async () => {
-    const t = grok.data.testData('demog');
-    const c = t.columns.byName('started');
-    c.set(1, dayjs.utc('2022-01-01'));
-    expect(c.get(1).valueOf(), 1640995200000);
-    c.set(1, null);
-    expect(c.get(1), null);
+  test('removeWhere', async () => {
+    const df = createDf2();
+    df.rows.removeWhere((r) => r.get('population') < 3);
+    expect(df.rowCount, 2);
   });
 
-  test('hash', async () => {
-    const df = grok.data.demo.demog(10);
-    expect(hashDataFrame(df).length, 32);
-
-    const df1 = DG.DataFrame.fromCsv(`a,b\n1,0\n2,0\n3,0`);
-    const df2 = DG.DataFrame.fromCsv(`a,b\n2,0\n1,0\n3,0`);
-    expectArray(hashDataFrame(df1), hashDataFrame(df2));
-
-    const df3 = DG.DataFrame.fromCsv(`a,b\n"abc",0\n"dce",0\n"xyz",0`);
-    const df4 = DG.DataFrame.fromCsv(`a,b\n"dce",0\n"abc",0\n"xyz",0`);
-    expectArray(hashDataFrame(df3), hashDataFrame(df4));
+  test('removeWhereIdx', async () => {
+    const df = createDf2();
+    df.rows.removeWhereIdx((i) => i > 1);
+    expect(df.rowCount, 2);
   });
 
-  test('emptyDataFrameToCsv', async () => {
-    const df: DG.DataFrame = DG.DataFrame.fromColumns([]);
-    const csv: string = df.toCsv();
-    expect(csv, '');
+  test('requestFilter', async () => {
+    const df = createDf2();
+    df.rows.filter((r) => r.get('population') < 3);
+    expect(df.filter.trueCount, 2);
+    df.rows.requestFilter();
+    expect(df.filter.trueCount, 4);
+  });
+
+  test('select', async () => {
+    const df = createDf2();
+    df.rows.select((r) => r.get('population') < 3);
+    expect(df.selection.trueCount, 2);
+  });
+
+  test('setValues', async () => {
+    const df = createDf();
+    df.rows.setValues(0, ['France', 2]);
+    expectArray([...df.rows.get(0).cells].map((i) => i.value), ['France', 2]);
+  });
+
+  test('toString', async () => {
+    const df = createDf();
+    expect(df.rows.toString(), `(Instance of 'dn', Instance of 'dn')`);
   });
 });
