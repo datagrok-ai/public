0bd88a49ef23f489db345c54e9517ee53f133ee1
PavloPolovyi <pasha32662@gmail.com>
2023-10-22 17:49:16 +0200

    ApiTests: Dockerfiles: Added tests

--- packages/ApiTests/src/packages/docker.ts
+++ packages/ApiTests/src/packages/docker.ts
@@ -3,32 +3,67 @@ import * as grok from 'datagrok-api/grok';
 
 import {DockerContainer} from 'datagrok-api/dg';
 import {delay, category, expect, test} from '@datagrok-libraries/utils/src/test';
-import {_package} from '../package-test';
 
-
-export async function awaitContainerStart(): Promise<DockerContainer> {
+export async function awaitContainerStart(containerName: string, tries: number = 3): Promise<DockerContainer> {
   let dockerContainer;
+  let count: number = 0;
   do {
-    dockerContainer = await grok.dapi.docker.dockerContainers.filter(_package.name).first();
+    dockerContainer = await grok.dapi.docker.dockerContainers.filter(containerName).first();
+    count++;
+    if (dockerContainer.status == 'started' || dockerContainer.status == 'checking')
+      return dockerContainer;
     await delay(500);
   }
-  while (!dockerContainer || (dockerContainer?.status !== 'started' && dockerContainer?.status !== 'checking'));
-  return dockerContainer;
+  while (count < tries);
+  throw Error('Container didn\'t start');
 }
 
 category('Packages: Docker', () => {
-  test('Get response', async () => {
-    const dockerContainer = await awaitContainerStart();
-    console.log(`Docker container ID: ${dockerContainer.id}`);
-    console.log(`Docker container status: ${dockerContainer.status}`);
-    expect(dockerContainer.status === 'started' || dockerContainer.status === 'checking',
-      true, 'Docker container has not started yet');
+  const containerOnDemandName: string = 'Apitests-docker-test1';
+  const containerName: string = 'Apitests-docker-test2';
 
-    const params: RequestInit = {
-      method: 'GET',
-    };
-    const path = '/square?number=4';
-    const response = await grok.dapi.docker.dockerContainers.request(dockerContainer.id, path, params);
-    expect(response?.trim(), '{"result":16}');
+  test('Get response', async () => {
+    const container = await awaitContainerStart(containerName);
+    await testResponse(container.id);
+  }, {timeout: 30000});
+
+  test('Get response: On demand', async () => {
+    const container = await grok.dapi.docker.dockerContainers.filter(containerOnDemandName).first();
+    if (container.status !== 'stopped') {
+      await grok.dapi.docker.dockerContainers.stop(container.id);
+      await delay(5000);
+    }
+    await testResponse(container.id);
+  }, {timeout: 60000});
+
+  test('Get build logs', async () => {
+    const image = await grok.dapi.docker.dockerImages.filter(containerName).first();
+    expect(image.status === 'ready');
+    expect(!image.logs || image.logs.length === 0, false);
   });
+
+  test('Get container logs', async () => {
+    const container = await awaitContainerStart(containerName);
+    const logs = await grok.dapi.docker.dockerContainers.getContainerLogs(container.id);
+    expect(!logs || logs.length === 0, false);
+  });
+
+  test('Container timeout', async () => {
+    let container = await grok.dapi.docker.dockerContainers.filter(containerOnDemandName).first();
+    await grok.dapi.docker.dockerContainers.run(container.id);
+    await delay(10000);
+    await awaitContainerStart(containerOnDemandName, 10);
+    await delay(70000);
+    container = await grok.dapi.docker.dockerContainers.filter(containerOnDemandName).first();
+    expect(container.status, 'stopped');
+  }, {timeout: 120000});
 });
+
+async function testResponse(containerId: string): Promise<void> {
+  const params: RequestInit = {
+    method: 'GET',
+  };
+  const path = '/square?number=4';
+  const response = await grok.dapi.docker.dockerContainers.request(containerId, path, params);
+  expect(response?.trim(), '{"result":16}');
+}
