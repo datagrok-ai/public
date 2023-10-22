f8c09949004f8b34b0d039767f11f2ed16da730d
Anna Muzychyna <annamuz69@gmail.com>
2023-10-23 01:04:52 +0300

    Tutorials: chem

--- packages/Tutorials/src/tracks/chem/index.ts
+++ packages/Tutorials/src/tracks/chem/index.ts
@@ -1,9 +1,15 @@
 import { Track } from '@datagrok-libraries/tutorials/src/track';
 import { VirtualScreeningTutorial } from './tutorials/virtual-screening';
+import {RGroupsAnalysisTutorial} from './tutorials/r-groups-analysis';
+import {ActivityCliffsTutorial} from './tutorials/activity-cliffs';
+import {SimilarityDiversitySearchTutorial} from './tutorials/similarity-diversity-search';
 
 
 export const tutorials = [
   VirtualScreeningTutorial,
+  RGroupsAnalysisTutorial,
+  ActivityCliffsTutorial,
+  SimilarityDiversitySearchTutorial
 ];
 
 export const chem = new Track(
