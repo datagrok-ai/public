a95415969ac33e83cdd6f8071c82dde448b82b18
Maria Dolotova <maria.dolotova@softwarecountry.com>
2023-10-23 13:24:39 +0300

    GROK-14060: Utils: REnder lines on sp - detect cursor over bezier curve, refactoring

--- libraries/utils/src/render-lines-on-sp.ts
+++ libraries/utils/src/render-lines-on-sp.ts
@@ -2,6 +2,11 @@ import * as DG from 'datagrok-api/dg';
 import { Subject } from 'rxjs';
 import BitArray from './bit-array';
 
+type MarkerSize = {
+    sizeFrom: number,
+    sizeTo: number
+}
+
 export type ILineSeries = {
     from: Uint32Array;
     to: Uint32Array;
@@ -11,7 +16,7 @@ export type ILineSeries = {
     widths?: Float32Array;      // line widths. If they are the same for the series, use [width] instead.
     opacity?: number;           // common opacity. Use [opacities] if you need individual opacities per line
     opacities?: Float32Array;   // line opacities. If they are the same for the series, use [opacity] instead
-    drawArrows?: Boolean;       // common parameter to draw arrows. Use [drawArrowsArr] if you need to draw arrows not for each line
+    drawArrows?: boolean;       // common parameter to draw arrows. Use [drawArrowsArr] if you need to draw arrows not for each line
     drawArrowsArr?: BitArray;   // individual parameter for each line. If they are the same for the series, use [drawArrows] instead
 
 }
@@ -19,17 +24,15 @@ export type ILineSeries = {
 export class ScatterPlotLinesRenderer {
     sp: DG.ScatterPlotViewer;
     xAxisCol: DG.Column;
-    yAxisCol: DG.Column;;
+    yAxisCol: DG.Column;
     currentLineIdx = -1;
     lines: ILineSeries;
     lineClicked = new Subject<number>();
     lineHover = new Subject<number>();
     canvas: HTMLCanvasElement;
     ctx: CanvasRenderingContext2D;
-    paths: Path2D[];
     mouseOverLineId: number | null = null;
-    multipleLinesIndices: Uint32Array;
-    changePointsOrder: BitArray;
+    multipleLinesCounts: Uint8Array;
 
     get currentLineId(): number {
         return this.currentLineIdx;
@@ -47,9 +50,7 @@ export class ScatterPlotLinesRenderer {
         this.lines = lines;
         this.canvas = this.sp.getInfo()['canvas'];
         this.ctx = this.canvas.getContext('2d') as CanvasRenderingContext2D;
-        this.paths = new Array<Path2D>(this.lines.from.length);
-        this.multipleLinesIndices = new Uint32Array(this.lines.from.length);
-        this.changePointsOrder = new BitArray(this.lines.from.length, false);
+        this.multipleLinesCounts = new Uint8Array(this.lines.from.length);
 
         this.createMultiLinesIndices();
 
@@ -72,40 +73,74 @@ export class ScatterPlotLinesRenderer {
 
 
     renderLines(): void {
+        const spLook = this.sp.getOptions().look;
+        const individualLineStyles = this.lines.colors || this.lines.width || this.lines.opacities || this.lines.drawArrowsArr;
+        if (!individualLineStyles) {
+            this.ctx.lineWidth = this.lines.width ?? 1;
+            this.ctx.strokeStyle = `rgba(${this.lines.color ?? '0,128,0'},${this.lines.opacity ?? 1})`;
+        }
+        const markerSizeCol = spLook['sizeColumnName'] ? this.sp.dataFrame.col(spLook['sizeColumnName']) : null;
         for (let i = 0; i < this.lines.from.length; i++) {
-            const pointsOrderChanged = this.changePointsOrder.getBit(i);
-            let pointFromIdx = pointsOrderChanged ? this.lines.to[i] : this.lines.from[i];
-            let pointToIdx = pointsOrderChanged ? this.lines.from[i] : this.lines.to[i];
-            const pointFrom = this.sp.worldToScreen(this.xAxisCol.get(pointFromIdx), this.yAxisCol.get(pointFromIdx));
-            const aX = pointFrom?.x;
-            const aY = pointFrom?.y;
-            const pointTo = this.sp.worldToScreen(this.xAxisCol.get(pointToIdx), this.yAxisCol.get(pointToIdx));
-            const bX = pointTo?.x;
-            const bY = pointTo?.y;
+            const { sizeFrom, sizeTo } = this.getMarkersSizes(spLook, markerSizeCol, i);
+            const pointFrom = this.sp.worldToScreen(this.xAxisCol.get(this.lines.from[i]), this.yAxisCol.get(this.lines.from[i]));
+            let aX = pointFrom?.x;
+            let aY = pointFrom?.y;
+            const pointTo = this.sp.worldToScreen(this.xAxisCol.get(this.lines.to[i]), this.yAxisCol.get(this.lines.to[i]));
+            let bX = pointTo?.x;
+            let bY = pointTo?.y;
+            this.ctx.beginPath();
             if (aX && aY && bX && bY) {
-                const line = new Path2D();
-                this.paths[i] = line;
-                line.moveTo(aX!, aY!);
-                const color = this.lines.color ?? this.lines.colors?.[i] ? this.lines.colors?.[i] : '0,128,0';
-                const opacity = this.lines.opacity ?? this.lines.opacities?.[i] ? this.lines.opacities?.[i] : 1;
-                this.ctx.strokeStyle = `rgba(${color},${opacity})`;
-                this.ctx.lineWidth = this.lines.width ? this.lines.width : this.lines.widths?.[i] ? i === this.currentLineIdx ? this.lines.widths?.[i] + 2 :
-                    this.lines.widths?.[i] : i === this.currentLineIdx ? 3 : 1;
-                let midPoint: DG.Point = this.midPoint(pointFrom, pointTo);
-                if (this.multipleLinesIndices[i]) {
-                    midPoint = this.findPerpendicularPointOnCurve(this.multipleLinesIndices[i],
-                        pointFrom.x, pointFrom.y, midPoint.x, midPoint.y)
+                if (individualLineStyles) {
+                    const color = this.lines.colors?.[i] ? this.lines.colors?.[i] : '0,128,0';
+                    const opacity = this.lines.opacities?.[i] ? this.lines.opacities?.[i] : 1;
+                    this.ctx.strokeStyle = `rgba(${color},${opacity})`;
+                    this.ctx.lineWidth = this.lines.widths?.[i] ? this.lines.widths?.[i] : 1;
                 }
-                line.quadraticCurveTo(midPoint.x, midPoint.y, bX, bY);
-                if (this.lines.drawArrows ?? this.lines.drawArrowsArr?.getBit(i))
-                    this.canvasArrow(line, pointsOrderChanged ? aX : bX, pointsOrderChanged ? aY : bY, midPoint.x, midPoint.y);
-                this.ctx.beginPath();
-                this.ctx.stroke(line);
+                const multiLines = this.multipleLinesCounts[i];
+                let controlPoint: DG.Point | null = null;
+                if (multiLines) {
+                    const startPointWithMarker = this.getPointOnDistance(aX, aY, bX, bY, sizeTo);
+                    const endtPointWithMarker = this.getPointOnDistance(bX, bY, aX, aY, sizeFrom);
+                    aX = startPointWithMarker.x;
+                    aY = startPointWithMarker.y;
+                    bX = endtPointWithMarker.x;
+                    bY = endtPointWithMarker.y;
+                    controlPoint = this.lines.from[i] > this.lines.to[i] ?
+                        this.findControlPoint(multiLines, aX, aY, bX, bY, i) :
+                        this.findControlPoint(multiLines, bX, bY, aX, aY, i);
+                    this.ctx.moveTo(aX!, aY!);
+                    this.ctx.quadraticCurveTo(controlPoint.x, controlPoint.y, bX, bY);
+                } else {
+                    this.ctx.moveTo(aX!, aY!);
+                    this.ctx.lineTo(bX, bY);
+                }
+                if (this.lines.drawArrows ?? this.lines.drawArrowsArr?.getBit(i)) {
+                    const arrowPoint = !multiLines ? this.getPointOnDistance(aX, aY, bX, bY, sizeTo) : null;
+                    const arrowCPX = multiLines ? controlPoint!.x : aX;
+                    const arrowCPY = multiLines ? controlPoint!.y : aY;
+                    this.canvasArrow(this.ctx, arrowPoint?.x ?? bX, arrowPoint?.y ?? bY, arrowCPX, arrowCPY);
+                }
+                this.ctx.stroke();
                 this.ctx.closePath();
             }
         }
         this.fillLeftBottomRect();
     }
+
+
+    getMarkersSizes(spLook: any, markerSizeCol: DG.Column | null, i: number): MarkerSize {
+        let sizeFrom = 3;
+        let sizeTo = 3;
+        if (markerSizeCol) {
+            sizeFrom = (spLook.markerMinSize + (spLook.markerMaxSize - spLook.markerMinSize) * markerSizeCol.scale(this.lines.from[i])) / 2;
+            sizeTo = (spLook.markerMinSize + (spLook.markerMaxSize - spLook.markerMinSize) * markerSizeCol.scale(this.lines.to[i])) / 2;
+        } else if (spLook.markerDefaultSize) {
+            sizeFrom = spLook.markerDefaultSize / 2;
+            sizeTo = spLook.markerDefaultSize / 2;
+        }
+        return { sizeFrom, sizeTo };
+    }
+
 
     fillLeftBottomRect() {
         const rect = new Path2D();
@@ -117,62 +152,151 @@ export class ScatterPlotLinesRenderer {
     }
 
     createMultiLinesIndices(): void {
-        const arrayIdxs = new Array<number>(this.lines.from.length);
-        for (let i = 0; i < arrayIdxs.length; i++)
-            arrayIdxs[i] = i;
-        while (arrayIdxs.length) {
-            let firstLineIdx = arrayIdxs[0];
+        const arrayIdxsBitArray = new BitArray(this.lines.from.length);
+        arrayIdxsBitArray.setAll(true);
+        for (let i = -1; (i = arrayIdxsBitArray.findNext(i)) !== -1;) {
+            let firstLineIdx = i;
             let p1 = this.lines.from[firstLineIdx];
             let p2 = this.lines.to[firstLineIdx];
-            if (p1 > p2)
-                this.changePointsOrder.setBit(firstLineIdx, true, false);
             let linesPerPair = 1;
-            for (let i = 1; i < arrayIdxs.length; i++) {
-                const pointToCompare1 = this.lines.from[arrayIdxs[i]];
-                const pointToCompare2 = this.lines.to[arrayIdxs[i]];
+            for (let j = i; (j = arrayIdxsBitArray.findNext(j)) !== -1;) {
+                const pointToCompare1 = this.lines.from[j];
+                const pointToCompare2 = this.lines.to[j];
                 if (pointToCompare1 === p1 && pointToCompare2 === p2 ||
                     pointToCompare2 === p1 && pointToCompare1 === p2) {
-                    if (pointToCompare1 > pointToCompare2)
-                        this.changePointsOrder.setBit(arrayIdxs[i], true);
-                    this.multipleLinesIndices[arrayIdxs[i]] = ++linesPerPair;
-                    arrayIdxs.splice(i, 1);
-                    i--;
+                    this.multipleLinesCounts[j] = ++linesPerPair;
+                    arrayIdxsBitArray.setBit(j, false, false);
                 }
             }
             if (linesPerPair > 1)
-                this.multipleLinesIndices[firstLineIdx] = 1;
-            arrayIdxs.splice(0, 1);
+                this.multipleLinesCounts[firstLineIdx] = 1;
+            arrayIdxsBitArray.setBit(i, false, false);
         }
     }
 
-
     checkCoordsOnLine(x: number, y: number): number | null {
-        for (let i = 0; i < this.paths.length; i++) {
-            this.ctx.lineWidth = 5;
-            const inStroke = this.ctx.isPointInStroke(this.paths[i], x * window.devicePixelRatio, y * window.devicePixelRatio);
-            if (inStroke)
-                return i;
+        let candidateIdx = null;
+        let minDist = null;
+        let dist = null;
+        const spLook = this.sp.getOptions().look;
+        const markerSizeCol = spLook['sizeColumnName'] ? this.sp.dataFrame.col(spLook['sizeColumnName']) : null;
+        for (let i = 0; i < this.lines.from.length; i++) {
+            const { sizeFrom, sizeTo } = this.getMarkersSizes(spLook, markerSizeCol, i);
+            const pFrom = this.sp.worldToScreen(this.xAxisCol.get(this.lines.from[i]), this.yAxisCol.get(this.lines.from[i]));
+            const pTo = this.sp.worldToScreen(this.xAxisCol.get(this.lines.to[i]), this.yAxisCol.get(this.lines.to[i]));
+            if (this.multipleLinesCounts[i]) {
+                const fromMarker = this.getPointOnDistance(pFrom.x, pFrom.y, pTo.x, pTo.y, sizeTo);
+                const toMarker = this.getPointOnDistance(pTo.x, pTo.y, pFrom?.x, pFrom?.y, sizeFrom);
+                const controlPoint = this.lines.from[i] > this.lines.to[i] ?
+                    this.findControlPoint(this.multipleLinesCounts[i], fromMarker.x, fromMarker.y, toMarker.x, toMarker.y, i) :
+                    this.findControlPoint(this.multipleLinesCounts[i], toMarker.x, toMarker.y, fromMarker.x, fromMarker.y, i);
+                dist = this.calculateDistToCurveLine(i, x, y, fromMarker, toMarker, controlPoint);
+            } else {
+                dist = this.calculateDistToStraightLine(x, y, pFrom, pTo);
+            }
+            if ((!minDist && dist !== null && dist < 5) || minDist && dist !== null && dist < minDist) {
+                minDist = dist;
+                candidateIdx = i;
+            }
+        }
+        return candidateIdx;
+    }
+
+    calculateDistToStraightLine(x: number, y: number, p1: DG.Point, p2: DG.Point): number | null {
+        /* calculating coordinates of a rect around a line. If cursor coords are outside this rect - assume that
+        point is not on line and do not calculate distance to line */
+        let xMin = Math.min(p1.x, p2.x);
+        let xMax = Math.max(p1.x, p2.x);
+        let yMin = Math.min(p1.y, p2.y);
+        let yMax = Math.max(p1.y, p2.y);
+
+        //adding a couple of pixels to increase the width/height of the rect
+        const threshold = 2;
+        return x >= xMin - threshold && x <= xMax + threshold && y >= yMin - threshold && y <= yMax + threshold ?
+            Math.abs(Math.hypot(p1.x - x, p1.y - y) + Math.hypot(p2.x - x, p2.y - y) - Math.hypot(p1.x - p2.x, p1.y - p2.y)) :
+            null;
+    }
+
+    calculateDistToCurveLine(i: number, x: number, y: number, p1: DG.Point, p2: DG.Point,
+        pc: DG.Point): number | null {
+        /* calculating coordinates of a rect around a line. If cursor coords are outside this shape - assume that
+        point is not on line and do not calculate distance to line */
+        let xMin = Math.min(p1.x, p2.x, pc.x);
+        let xMax = Math.max(p1.x, p2.x, pc.x);
+        let yMin = Math.min(p1.y, p2.y, pc.y);
+        let yMax = Math.max(p1.y, p2.y, pc.y);
+
+        //adding a couple of pixels to increase the width/height of the rect
+        const threshold = 2;
+        if (x >= xMin - threshold && x <= xMax + threshold && y >= yMin - threshold && y <= yMax + threshold) {
+            const w = xMax - xMin;
+            const h = yMax - yMin;
+            return this.calculateDistToCurveInRect(x, y, p1, pc, p2, w, h);
         }
         return null;
     }
 
-    midPoint(p1: DG.Point, p2: DG.Point): DG.Point {
-        return new DG.Point(p1.x + (p2.x - p1.x) / 2, p1.y + (p2.y - p1.y) / 2);
+    calculateDistToCurveInRect(x: number, y: number, p0: DG.Point, p1: DG.Point, p2: DG.Point,
+        w: number, h: number): number {
+        const stepLen = 3;
+        const stepsNum = Math.floor((w + h) / stepLen);
+        const deltaT = 1 / stepsNum;
+        const arrX = new Uint32Array(stepsNum);
+        const arrY = new Uint32Array(stepsNum);
+        let maxHW = new Uint32Array(stepsNum);
+        let minSumHW: number | null = null;
+        const candidateIdxs = new BitArray(stepsNum);
+        for (let i = 0; i < arrX.length; i++) {
+            const t = i * deltaT;
+            const xOnCurve = Math.pow((1 - t), 2) * p0.x + 2 * t * (1 - t) * p1.x + Math.pow(t, 2) * p2.x;
+            const yOnCurve = Math.pow((1 - t), 2) * p0.y + 2 * t * (1 - t) * p1.y + Math.pow(t, 2) * p2.y;
+            const rectW = Math.abs(x - xOnCurve);
+            const rectH = Math.abs(y - yOnCurve);
+            const sumHW = rectW + rectH;
+            if (!minSumHW || minSumHW > sumHW)
+                minSumHW = sumHW;
+            maxHW[i] = Math.max(rectW, rectH);
+            arrX[i] = xOnCurve;
+            arrY[i] = yOnCurve;
+        }
+        for (let i = 0; i < arrX.length; i++) {
+            if (maxHW[i] < minSumHW!)
+                candidateIdxs.setBit(i, true, false);
+        }
+        let minDist: number | null = null;
+        for (let j = -1; (j = candidateIdxs.findNext(j)) !== -1;) {
+            const dist = Math.hypot((arrX[j] - x), (arrY[j] - y));
+            if (!minDist || minDist > dist)
+                minDist = dist;
+        }
+        return minDist!;
     }
 
-    findPerpendicularPointOnCurve(idx: number, x1: number, y1: number, x2: number, y2: number): DG.Point {
-        let dx = x2 - x1;
-        let dy = y2 - y1;
+    getPointOnDistance(p1x: number, p1y: number, p2x: number, p2y: number, distance: number): DG.Point {
+        const p1p2d = Math.sqrt(Math.pow(p2x - p1x, 2) + Math.pow(p2y - p1y, 2));
+        const dx = (p2x - p1x) / p1p2d;
+        const dy = (p2y - p1y) / p1p2d;
+        const p3x = p2x - distance * dx;
+        const p3y = p2y - distance * dy;
+
+        return new DG.Point(p3x, p3y);
+    }
+
+    findControlPoint(idx: number, x1: number, y1: number, x2: number, y2: number, i?: number): DG.Point {
+        const midX = x1 + (x2 - x1) / 2;
+        const midY = y1 + (y2 - y1) / 2;
+        let dx = midX - x1;
+        let dy = midY - y1;
         const dist = Math.sqrt(dx * dx + dy * dy);
         dx /= dist;
         dy /= dist;
         const perpendicularLen = 50 * Math.ceil(idx / 2);
         return idx % 2 === 0 ?
-            new DG.Point(x2 + (perpendicularLen / 2) * dy, y2 - (perpendicularLen / 2) * dx) :
-            new DG.Point(x2 - (perpendicularLen / 2) * dy, y2 + (perpendicularLen / 2) * dx);
+            new DG.Point(midX + (perpendicularLen / 2) * dy, midY - (perpendicularLen / 2) * dx) :
+            new DG.Point(midX - (perpendicularLen / 2) * dy, midY + (perpendicularLen / 2) * dx);
     }
 
-    canvasArrow(path: Path2D, arrowEndX: number, arrowEndY: number, quadX: number, quadY: number): void {
+    canvasArrow(path: CanvasRenderingContext2D, arrowEndX: number, arrowEndY: number, quadX: number, quadY: number): void {
         const arrowAngle = Math.atan2(quadX - arrowEndX, quadY - arrowEndY) + Math.PI;
         const arrowWidth = 15;
         path.moveTo(arrowEndX - (arrowWidth * Math.sin(arrowAngle - Math.PI / 10)),
