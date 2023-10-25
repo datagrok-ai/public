a6d1c2e4427c41bd5eeb708d4f82f9e52df20fb8
Dmitry Illarionov <illarionow@yahoo.com>
2023-10-25 11:56:19 +0200

    JS-API: Updated hintIndiactor

--- js-api/ui.ts
+++ js-api/ui.ts
@@ -1537,12 +1537,20 @@ export namespace hints {
 
     el.classList.add('ui-hint-target');
     $('body').append(hintIndicator);
-
-    const indicatorNode = el.getBoundingClientRect();
+    
     hintIndicator.style.position = 'fixed';
     hintIndicator.style.zIndex = '4000';
-    hintIndicator.style.left = indicatorNode.left + 'px';
-    hintIndicator.style.top = indicatorNode.top + 'px';
+
+    let setPosition = setInterval(function () {
+      if ($('body').has(el).length != 0) {
+        const indicatorNode = el.getBoundingClientRect();
+        hintIndicator.style.left = indicatorNode.left + 'px';
+        hintIndicator.style.top = indicatorNode.top + 'px';
+      } else {
+        hintIndicator.remove();
+        clearInterval(setPosition);
+      } 
+    }, 10);
 
     if (clickToClose) {
       $(el).on('click', () => {
