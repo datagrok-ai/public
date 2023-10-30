7dfcfa0eb624da345446e00873a9ef198d9e7585
vmakarichev <vmakarichev@datagrok.ai>
2023-10-30 14:12:21 +0200

    GROK-14119: ODEs: Inverse matrix by wasm
    Wasm-computation of inverse matrix is implemented.

--- packages/ODEs/src/package.ts
+++ packages/ODEs/src/package.ts
@@ -3,9 +3,50 @@ import * as grok from 'datagrok-api/grok';
 import * as ui from 'datagrok-api/ui';
 import * as DG from 'datagrok-api/dg';
 
+import {initMatrOperApi, getInverseMatrix} from '../wasm/matrix-operations-api';
+
 export const _package = new DG.Package();
 
 //name: info
 export function info() {
   grok.shell.info(_package.webRoot);
+}
+
+//tags: init
+export async function init() {
+  await initMatrOperApi();
+}
+
+//name: check
+//input: int n
+export function check(n: number) {
+  const A = (n === 1) ? new Float64Array([10]) : 
+  ((n === 2) ? new Float64Array([1,2,3,4]) : new Float64Array([1,1,1,0,1,1,0,0,1]));
+  console.log('A:');
+  console.log(A);
+  console.log('inv A:');
+  console.log(getInverseMatrix(A, n));
+}
+
+//name: performance
+//input: int size = 15
+//input: int times = 10000
+export function performance(size: number, times: number) {
+  const A = new Float64Array(size * size);
+
+  for (let i = 0; i < size; ++i)
+    for (let j = i; j < size; ++j)
+      A[j + i * size] = Math.random();
+
+  let sum = 0;
+
+  for (let k = 0; k < times; ++k) {
+    const start = new Date().getTime();
+    getInverseMatrix(A, size);
+    const finish = new Date().getTime();
+    sum += finish - start;
+  }
+
+  console.log(`Total time: ${sum} ms.`);
+  console.log(`Average time: ${sum/times} ms.`);
 }
---
+++ packages/ODEs/wasm/matrix-operations-api.js
@@ -0,0 +1,38 @@
+// Matrix operations by wasm-computations
+
+/** Initialize wasm-module */
+export async function initMatrOperApi() {
+  await initMatrOper();
+}
+
+/** Returns inverse matrix */
+export function getInverseMatrix(source, rowCount) {
+  if (rowCount === 1)
+    return new Float64Array([1.0 / source[0]]);
+  
+  if (rowCount === 2) {
+    const det = source[0] * source[3] - source[1] * source[2];
+
+    return new Float64Array([
+      source[3] / det,
+      -source[1] / det,
+      -source[2] / det,
+      source[0] / det
+    ]);
+  }
+  
+  const size = source.length;  
+  const sourceBuf = matrOperLib._malloc(size * 8);
+  const invBuf = matrOperLib._malloc(size * 8);
+  matrOperLib.HEAPF64.set(source, sourceBuf >> 3);  
+  matrOperLib._invMatrixD(sourceBuf, rowCount, invBuf);  
+  const inv = new Float64Array(size);
+  
+  for (let i = 0; i < size; ++i)
+    inv[i] = matrOperLib.HEAPF64[invBuf / 8 + i];
+
+  matrOperLib._free(sourceBuf);
+  matrOperLib._free(invBuf);
+
+  return inv;
+}
