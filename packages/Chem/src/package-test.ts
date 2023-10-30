308f3c01df2503d17b59486426c312b6395fc8dd
drizhina <drizhinashvili@datagrok.ai>
2023-10-31 00:50:46 +0200

    GROK-0: Chem: Add tests

--- packages/Chem/src/package-test.ts
+++ packages/Chem/src/package-test.ts
@@ -5,9 +5,9 @@ import './tests/ui-tests-info-panel';
 import './tests/ui-tests-top-menu';
 import './tests/menu-tests-chem-space';
 import './tests/menu-tests-cliffs';
-// import './tests/menu-tests-similarity-diversity';
-// import './tests/menu-tests-script-based';
-// import './tests/menu-tests-rgroups';
+import './tests/menu-tests-similarity-diversity';
+import './tests/menu-tests-script-based';
+import './tests/menu-tests-rgroups';
 
 // import './tests/col-panel-tests';
 // import './tests/cell-panel-tests';
