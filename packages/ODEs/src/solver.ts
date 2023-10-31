777489ec992debda45ead82354a889cd6d379fa0
vmakarichev <vmakarichev@datagrok.ai>
2023-10-31 14:26:25 +0200

    GROK-14119: ODEs : Solving method
    Implicit method for solving ODEs systems is implemented.

--- packages/ODEs/src/solver.ts
+++ packages/ODEs/src/solver.ts
@@ -1,6 +1,50 @@
+/* Adaptive step solvers for the initial problems for ODEs:
+     - explicit Runge-Kutta Cash-Karp method - TODO;
+     - implcit modified Rosnbrock triple method.
+
+   The formulas applied are taken from
+
+     [1] Endre Suli and David F. Mayers. An Introduction to Numerical Analysis, 2003.
+
+     [2] Steve Chapra and Raymond P. Canale. Numerical Methods for Engineers, 2021.
+
+     [3] https://doi.org/10.1137/S1064827594276424
+
+     [4] https://doi.org/10.1016/S0898-1221(00)00175-9
+*/
+
 import * as grok from 'datagrok-api/grok';
 import * as ui from 'datagrok-api/ui';
 import * as DG from 'datagrok-api/dg';
+
+import {invMatrix} from '../wasm/matrix-operations-api';
+
+/** A very small number */
+const TINY = 1e-20;
+
+/** Routine constants of adaptive step method */
+const SAFETY = 0.9;
+const PSHRNK = -0.25;
+const PSGROW = -0.2;
+const REDUCE_COEF = 0.25;
+const GROW_COEF = 4.0;
+const ERR_CONTR = 1.89e-4;
+
+// Quantities used in Rosenbrock method (see [3], [4] for more details)
+const EPS = 1.0e-10;		
+const D = 1.0 - Math.sqrt(2.0) / 2.0;
+const E32 = 6.0 + Math.sqrt(2.0);
+
+/** Error messeges */
+enum ERROR_MSG {
+  ROSENBROCK_METHOD_FAILS = 'the modified Rosenbrock triple method fails',
+};
+
+/** The abs function */
+const abs = (x: number) => (x > 0) ? x : -x;
+
+/** The max function */
+const max = (x: number, y: number) => (x > y) ? x : y;
 
 /** Right-hand side of IVP */
 type Func = (t: number, y: Float64Array, p: Float64Array, output: Float64Array) => void;
@@ -17,51 +61,8 @@ type ODEs = {
   initial: Float64Array,
   params: Float64Array,
   func: Func,
-  solutionColNames: string[],
-};
-
-const ex1: Func = (t: number, _y: Float64Array, _p: Float64Array, output: Float64Array) => {
-    // constants
-    const const1 = 1.0;
-    const const2 = 3.0;
-  
-    // parameters
-    const param1 = _p[0];
-    const param2 = _p[1];
-  
-    // function values
-    const x = _y[0];
-    const y = _y[1];
-  
-    // expressions
-    const coef1 = const1 + param1;
-    const coef2 = const2 + param2 + 0.0;
-  
-    output[0] = coef1 * y; // d(x)/dt
-    output[1] = coef2 * x; // dy/d(t)
-} // ex1
-
-  
-const ex2: Func = (t: number, _y: Float64Array, _p: Float64Array, output: Float64Array) => {  
-    // parameters
-    const alpha = _p[0];
-    const beta = _p[1];
-  
-    // function values
-    const x = _y[0];
-    const y = _y[1];  
-  
-    output[0] = alpha * x; // d(x)/dt
-    output[1] = beta * y; // dy/d(t)
-} // ex2
-
-export const example2: ODEs = {
-  name: 'Example 2',
-  arg: {name: 't', start: 0, finish: 5, step: 0.01},
-  initial: new Float64Array([1, 1]),
-  params: new Float64Array([2, 3]),
-  func: ex2,
-  solutionColNames: ['x(t)', 'y(t)']
+  tolerance: number,
+  solutionColNames: string[],  
 };
 
 /** Returns derivative with respect to t. */
@@ -91,30 +92,251 @@ function Jacobian(t: number, y: Float64Array, p: Float64Array, f: Func, eps: num
 }
 
 /** Solve initial value problem. */
-export function solveODEs(problem: ODEs): DG.DataFrame {
-  const start = problem.arg.start;
-  const finish = problem.arg.finish;
-  const step = problem.arg.step;
-  const initial = problem.initial;
+export function solveODEs(odes: ODEs): DG.DataFrame {
+  /** right-hand side of the IVP solved */
+  const f = odes.func;
+
+  /** parameters of the IVP solved */
+  const p = odes.params;
+
+  // operating variables
+	const t0 = odes.arg.start;
+	const t1 = odes.arg.finish;
+	let h = odes.arg.step;
+	const hDataframe = h;
+	const tolerance = odes.tolerance;
 
   /** number of solution dataframe rows */  
-  const rowCount = Math.trunc((finish - start) / step) + 1;
+  const rowCount = Math.trunc((t1 - t0) / h) + 1;
 
   /** dimension of the problem */
-  const dim = initial.length;
+  const dim = odes.initial.length;
+  const dimSquared = dim * dim;
 
-  /**  */
+  /** independent variable values */
   const tArr = new Float32Array(rowCount);
 
-  /**  */
+  /** arrays of solution values */
   const yArrs = Array<Float32Array>(dim);
 
   for (let i = 0; i < dim; ++i)
     yArrs[i] = new Float32Array(rowCount);
 
-  const solutionDf = DG.DataFrame.fromColumns([DG.Column.fromFloat32Array(problem.arg.name, tArr)]);
-  yArrs.forEach((arr, idx) => solutionDf.columns.add(DG.Column.fromFloat32Array(problem.solutionColNames[idx], arr)));
-  solutionDf.name = problem.name;
+  // method routine  
+  let timeDataframe = t0 + hDataframe;
+  let t = t0;
+  let tPrev = t0;
+  let hNext = 0.0;
+  let flag = true;
+	let index = 1;
+  let errmax = 0;
+  let hTemp = 0;
+  let tNew = 0;
+
+  // 0 BUFFERS & TEMP STRUCTURES
+
+  /** identity matrix */
+  const I = new Float64Array(dim * dim);
+
+  // compute identity matrix
+  for (let i = 0; i < dim; ++i)
+    for (let j = 0; j < dim; ++j)
+      I[j + i * dim] = (i === j) ? 1 : 0;
+
+  const y = new Float64Array(odes.initial);
+	const yPrev = new Float64Array(odes.initial);
+	const dydt = new Float64Array(dim);
+	const yScale = new Float64Array(dim);  
+  const yTemp = new Float64Array(dim);
+	const yErr = new Float64Array(dim);
+
+  const W = new Float64Array(dim * dim);
+  const invW = new Float64Array(dim * dim);
+  const f0 = new Float64Array(dim);
+  const k1 = new Float64Array(dim);
+  const f1 = new Float64Array(dim);
+  const k2 = new Float64Array(dim);
+  const f2 = new Float64Array(dim);
+  const k3 = new Float64Array(dim);
+  const yDer = new Float64Array(dim);
+  const hdT = new Float64Array(dim);
+
+  const f0Buf = new Float64Array(dim);
+  const f1Buf = new Float64Array(dim);
+  let hd = 0;
+  let sum = 0;
+  let h_div_num = 0;
+
+  // 1. SOLUTION AT THE POINT t0
+  tArr[0] = t0;
+  for (let i = 0; i < dim; ++i)
+    yArrs[i][0] = y[i];
+
+  // 2. COMPUTE NUMERICAL SOLUTION FOR THE POINTS FROM THE INTERVAL (t0, t1)
+  while (flag) {
+    // compute derivative
+    f(t, y, p, dydt);
+
+		// compute scale vector    
+    for (let i = 0; i < dim; ++i)
+      yScale[i] = abs(y[i]) + h * abs(dydt[i]) + TINY;      
+
+    // check end point
+		if (t + h > t1) {
+		  h = t1 - t;
+			flag = false;
+		}
+
+    // call of adaptive step modified Rosenbrok triple method
+    // int resultCode = adaptiveMRT(f, T, J, y, dydt, t, h, yScale, hNext, tolerance);
+// ==================================================================================
+		// computation of solution (y), time (t) and next step (hNext)
+		while (true) {
+			// one stage of the modified Rosenbrok triple approach
+			//int resultCode = MRT(f, T, J, y, dydt, t, h, yTemp, yErr);
+// ----------------------------------------------------------------------------------
+      // MatType hdT = h * d * T(t, y, EPS);
+      tDerivative(t, y, p, f, EPS, f0Buf, f1Buf, hdT);
+      hd = h * D;
+      for (let i = 0; i < dim; ++i)
+        hdT[i] *= hd;
+
+      // The main computations
+
+      // f0 = f(t, y);
+      f(t, y, p, f0);
+      
+      // W = I - h * d * J(t, y, EPS);      
+      Jacobian(t, y, p, f, EPS, f0Buf, f1Buf, W);
+      for (let i = 0; i < dimSquared; ++i)
+        W[i] = I[i] - hd * W[i];
+
+      // invW = W.inverse();
+      invMatrix(W, dim, invW);
+
+      // k1 = invW * (f0 + hdT);
+      for (let i = 0; i < dim; ++i)
+        f0Buf[i] = f0[i] + hdT[i];
+
+      for (let i = 0; i < dim; ++i) {
+        sum = 0;
+
+        for (let j = 0; j < dim; ++j)
+          sum += invW[j + i * dim] * f0Buf[j];
+
+        k1[i] = sum;
+      }
+
+      h_div_num = 0.5 * h;
+
+      // yDer = y + 0.5 * h * k1;
+      for (let i = 0; i < dim; ++i)
+        yDer[i] = y[i] + h_div_num * k1[i];
+      
+      // f1 = f(t + 0.5 * h, yDer);
+      f(t + h_div_num, yDer, p, f1);
+
+      // k2 = invW * (f1 - k1) + k1;
+      for (let i = 0; i < dim; ++i)
+        f1Buf[i] = f1[i] - k1[i];
+
+      for (let i = 0; i < dim; ++i) {
+        sum = k1[i];
+
+        for (let j = 0; j < dim; ++j)
+          sum += invW[j + i * dim] * f1Buf[j];
+
+        k2[i] = sum;
+      }
+
+      // yOut = y + k2 * h; <--> yTemp
+      for (let i = 0; i < dim; ++i)
+        yTemp[i] = y[i] + h * k2[i];
+
+      // f2 = f(t + h, yOut);
+      f(t + h, yTemp, p, f2);
+
+      // k3 = invW * (f2 - e32 * (k2 - f1) - 2.0 * (k1 - f0) + hdT);
+      for (let i = 0; i < dim; ++i)
+        f1Buf[i] = f2[i] - E32 * (k2[i] - f1[i]) - 2.0 * (k1[i] - f0[i]) + hdT[i];
+
+      for (let i = 0; i < dim; ++i) {
+        sum = 0;
+
+        for (let j = 0; j < dim; ++j)
+          sum += invW[j + i * dim] * f1Buf[j];
+
+        k3[i] = sum;
+      }
+
+      // yErr = (k1 - 2.0 * k2 + k3) * h / 6;
+      h_div_num = h / 6;
+
+      for (let i = 0; i < dim; ++i)
+        yErr[i] = (k1[i] - 2.0 * k2[i] + k3[i]) * h_div_num;
+
+// ----------------------------------------------------------------------------------
+
+			// estimating error
+			errmax = 0;
+			for (let i = 0; i < dim; ++i)
+				errmax = max(errmax, abs(yErr[i] / yScale[i]));
+			errmax /= tolerance;
+			
+			// processing the error obtained
+			if (errmax > 1) {
+				hTemp = SAFETY * h * errmax**PSHRNK;
+				h = max(hTemp, REDUCE_COEF * h);
+				tNew = t + h;
+				if (tNew == t)
+          throw new Error(ERROR_MSG.ROSENBROCK_METHOD_FAILS);
+			}
+			else {
+				if (errmax > ERR_CONTR)
+					hNext = SAFETY * h * errmax**PSGROW;
+				else
+					hNext = GROW_COEF * h;
+				t = t + h;
+
+        for (let i = 0; i < dim; ++i)
+          y[i] = yTemp[i];
+
+				break;
+			}
+		} // while (true)
+// ==================================================================================
+
+    // compute lineraly interpolated results and store them in dataframe
+		while (timeDataframe < t) {
+			let cLeft = (t - timeDataframe) / (t - tPrev);
+			let cRight = 1.0 - cLeft;
+
+			tArr[index] = timeDataframe;
+
+			for (let j = 0; j < dim; ++j)
+				yArrs[j][index] = cRight * y[j] + cLeft * yPrev[j];
+
+			timeDataframe += hDataframe;
+			++index;
+		}
+
+    h = hNext;
+		tPrev = t;
+
+    for (let i = 0; i < dim; ++i)
+		  yPrev[i] = y[i];
+
+  } // while (flag)
+
+  // 3. solution at the point t1
+	tArr[rowCount - 1] = t1;
+
+	for (let i = 0; i < dim; ++i)
+    yArrs[i][rowCount - 1] = y[i];	  
+
+  const solutionDf = DG.DataFrame.fromColumns([DG.Column.fromFloat32Array(odes.arg.name, tArr)]);
+  yArrs.forEach((arr, idx) => solutionDf.columns.add(DG.Column.fromFloat32Array(odes.solutionColNames[idx], arr)));
+  solutionDf.name = odes.name;
 
   return solutionDf;
 }
--- packages/ODEs/wasm/matrix-operations-api.js
+++ packages/ODEs/wasm/matrix-operations-api.js
@@ -36,3 +36,30 @@ export function getInverseMatrix(source, rowCount) {
 
   return inv;
 }
+
+/** Returns inverse matrix */
+export function invMatrix(source, rowCount, output) {
+  if (rowCount === 1)
+    output[0] = 1.0 / source[0];
+  else if (rowCount === 2) {
+    const det = source[0] * source[3] - source[1] * source[2];
+
+    output[0] = source[3] / det;
+    output[1] = -source[1] / det;
+    output[2] = -source[2] / det;
+    output[3] = source[0] / det;
+  }
+  else {  
+    const size = source.length;  
+    const sourceBuf = matrOperLib._malloc(size * 8);
+    const invBuf = matrOperLib._malloc(size * 8);
+    matrOperLib.HEAPF64.set(source, sourceBuf >> 3);  
+    matrOperLib._invMatrixD(sourceBuf, rowCount, invBuf);   
+  
+    for (let i = 0; i < size; ++i)
+      output[i] = matrOperLib.HEAPF64[invBuf / 8 + i];
+
+    matrOperLib._free(sourceBuf);
+    matrOperLib._free(invBuf);
+  }
+}
