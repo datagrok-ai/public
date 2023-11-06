9f67c2488ed8f71db37a64176484497e3a629e8b
Aleksandr Tanas <atanas@datagrok.ai>
2023-11-06 17:36:00 +0400

    Bio: Fix activityCliffs tests

--- packages/Bio/src/tests/activity-cliffs-tests.ts
+++ packages/Bio/src/tests/activity-cliffs-tests.ts
@@ -7,21 +7,30 @@ import {readDataframe} from './utils';
 import {_testActivityCliffsOpen} from './activity-cliffs-utils';
 import {DimReductionMethods} from '@datagrok-libraries/ml/src/reduce-dimensionality';
 
-import {_package} from '../package-test';
 import {MmDistanceFunctionsNames} from '@datagrok-libraries/ml/src/macromolecule-distance-functions';
 import {BitArrayMetricsNames} from '@datagrok-libraries/ml/src/typed-metrics';
+import {getMonomerLibHelper, IMonomerLibHelper} from '@datagrok-libraries/bio/src/monomer-works/monomer-utils';
+import {LIB_DEFAULT, LIB_STORAGE_NAME} from '../utils/monomer-lib';
 
+import {_package} from '../package-test';
 
 category('activityCliffs', async () => {
-  let actCliffsTableView: DG.TableView;
-  let actCliffsDf: DG.DataFrame;
-  let actCliffsTableViewWithEmptyRows: DG.TableView;
-  let actCliffsDfWithEmptyRows: DG.DataFrame;
-
   let viewList: DG.ViewBase[] = [];
   let dfList: DG.DataFrame[] = [];
 
+  let monomerLibHelper: IMonomerLibHelper;
+  /** Backup actual user's monomer libraries settings */
+  let userLibrariesSettings: {};
+
+
   before(async () => {
+    monomerLibHelper = await getMonomerLibHelper();
+    userLibrariesSettings = await grok.dapi.userDataStorage.get(LIB_STORAGE_NAME, true);
+
+    // Test 'helm' requires default monomer library loaded
+    await grok.dapi.userDataStorage.post(LIB_STORAGE_NAME, LIB_DEFAULT, true);
+    await monomerLibHelper.loadLibraries(true); // load default libraries
+
     viewList = [];
     dfList = [];
   });
@@ -29,14 +38,18 @@ category('activityCliffs', async () => {
   after(async () => {
     // for (const df of dfList) grok.shell.closeTable(df);
     // for (const view of viewList) view.close();
+
+    // UserDataStorage.put() replaces existing data
+    await grok.dapi.userDataStorage.put(LIB_STORAGE_NAME, userLibrariesSettings, true);
+    await monomerLibHelper.loadLibraries(true); // load user settings libraries
   });
 
   test('activityCliffsOpens', async () => {
-    actCliffsDf = await readDataframe(
+    const actCliffsDf = await readDataframe(
       DG.Test.isInBenchmark ? 'test/peptides_motif-with-random_10000.csv' : 'tests/100_3_clustests.csv',
     );
     dfList.push(actCliffsDf);
-    actCliffsTableView = grok.shell.addTableView(actCliffsDf);
+    const actCliffsTableView = grok.shell.addTableView(actCliffsDf);
     viewList.push(actCliffsTableView);
     const cliffsNum = DG.Test.isInBenchmark ? 6 : 3;
 
@@ -45,9 +58,9 @@ category('activityCliffs', async () => {
   });
 
   test('activityCliffsWithEmptyRows', async () => {
-    actCliffsDfWithEmptyRows = await readDataframe('tests/100_3_clustests_empty_vals.csv');
+    const actCliffsDfWithEmptyRows = await readDataframe('tests/100_3_clustests_empty_vals.csv');
     dfList.push(actCliffsDfWithEmptyRows);
-    actCliffsTableViewWithEmptyRows = grok.shell.addTableView(actCliffsDfWithEmptyRows);
+    const actCliffsTableViewWithEmptyRows = grok.shell.addTableView(actCliffsDfWithEmptyRows);
     viewList.push(actCliffsTableViewWithEmptyRows);
 
     await _testActivityCliffsOpen(actCliffsDfWithEmptyRows, DimReductionMethods.UMAP,
@@ -55,10 +68,10 @@ category('activityCliffs', async () => {
   });
 
   test('Helm', async () => {
-    const df = await _package.files.readCsv('samples/sample_HELM.csv');
+    const df = await _package.files.readCsv('data/sample_HELM_50.csv');
     const view = grok.shell.addTableView(df);
 
     await _testActivityCliffsOpen(df, DimReductionMethods.UMAP,
-      'HELM', 'Activity', 90, 53, BitArrayMetricsNames.Tanimoto);
+      'HELM', 'Activity', 65, 4, BitArrayMetricsNames.Tanimoto);
   });
 });
