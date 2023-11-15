a3f98664c89c099b016c5c026960c7d3200d46bb
Maria Dolotova <maria.dolotova@softwarecountry.com>
2023-11-15 11:22:49 +0300

    Chem: Fixed rendering test

--- packages/Chem/src/tests/rendering-tests.ts
+++ packages/Chem/src/tests/rendering-tests.ts
@@ -1,4 +1,4 @@
-import {category, test, expectArray} from '@datagrok-libraries/utils/src/test';
+import {category, test, expectArray, before} from '@datagrok-libraries/utils/src/test';
 import * as grok from 'datagrok-api/grok';
 import * as ui from 'datagrok-api/ui';
 import {readDataframe} from './utils';
@@ -6,8 +6,17 @@ import {scrollTable} from '../utils/demo-utils';
 import * as DG from 'datagrok-api/dg';
 import {RDKitCellRenderer} from '../rendering/rdkit-cell-renderer';
 import { FILTER_SCAFFOLD_TAG, HIGHLIGHT_BY_SCAFFOLD_TAG, SCAFFOLD_TREE_HIGHLIGHT } from '../constants';
+import { convertMolNotation } from '../package';
+import { _convertMolNotation } from '../utils/convert-notation-utils';
 
 category('rendering', () => {
+  
+  let rdkitModule: any;
+  
+  before(async () => {
+    rdkitModule = await grok.functions.call('Chem:getRdKitModule');
+  });
+
   test('visual rendering', async () => {
     ui.dialog()
       .add(grok.chem.svgMol('O=C1CN=C(C2CCCCC2)C2:C:C:C:C:C:2N1'))
@@ -50,8 +59,10 @@ category('rendering', () => {
 
   test('rdkit grid cell renderer benchmark', async () => {
     const df = DG.Test.isInBenchmark ? await readDataframe('tests/smi10K.csv') : await readDataframe('mol1K.csv');
+    const grid = df.plot.grid();
     const rowCount = df.rowCount;
-    const col = df.col(DG.Test.isInBenchmark ? 'smiles' : 'molecule')!;
+    const colName = DG.Test.isInBenchmark ? 'smiles' : 'molecule'
+    const col = df.col(colName)!;
     const renderFunctions = DG.Func.find({meta: {chemRendererName: 'RDKit'}});
     const rendndererObj = await renderFunctions[0].apply();
     const moleculeHost = ui.canvas(200, 100);
@@ -59,19 +70,30 @@ category('rendering', () => {
     let start = performance.now();
     //rendering without highlights
     for (let i = 0; i < rowCount; i++) {
-      rendndererObj.render(moleculeHost.getContext('2d')!, 0, 0, 200, 100, DG.GridCell.fromValue(col.get(i)));
+      rendndererObj.render(moleculeHost.getContext('2d')!, 0, 0, 200, 100, grid.cell(colName, i));
     }
     console.log(`rendering of ${rowCount} molecules without highlight took ${performance.now() - start} milliseconds`);
    
-    col.temp[FILTER_SCAFFOLD_TAG] = 'c1ccccc1';
+    col.temp[FILTER_SCAFFOLD_TAG] = JSON.stringify([{
+      molecule: _convertMolNotation('c1ccccc1', DG.chem.Notation.Smiles, DG.chem.Notation.MolBlock, rdkitModule),
+      isSuperstructure: false
+    }]);
     col.setTag(HIGHLIGHT_BY_SCAFFOLD_TAG, '[{"color":"#00FF00","molecule":"C1CCCCC1"}]');
-    col.setTag(HIGHLIGHT_BY_SCAFFOLD_TAG, '[{"color":"#00FF00","molecule":"C1CCCCC1"}]');
-    col.setTag(SCAFFOLD_TREE_HIGHLIGHT, `[{"molecule":"\n  MJ201900                      \n\n  7  7  0  0  0  0  0  0  0  0999 V2000\n    0.1338    0.4232    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0\n   -0.5805    0.0107    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0\n   -0.5805   -0.8143    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0\n    0.1338   -1.2268    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0\n    0.8483   -0.8143    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0\n    0.8483    0.0107    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0\n    0.1338    1.2482    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0\n  1  2  2  0  0  0  0\n  2  3  1  0  0  0  0\n  3  4  2  0  0  0  0\n  4  5  1  0  0  0  0\n  5  6  2  0  0  0  0\n  6  1  1  0  0  0  0\n  1  7  1  0  0  0  0\nM  END\n","color":"#ffbb78"},{"molecule":"\n  MJ201900                      \n\n  8  8  0  0  0  0  0  0  0  0999 V2000\n    0.1337    0.4232    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0\n   -0.5806    0.0107    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0\n   -0.5806   -0.8144    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0\n    0.1337   -1.2270    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0\n    0.8484   -0.8144    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0\n    0.8484    0.0107    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0\n    0.1337    1.2484    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0\n   -1.2951    0.4231    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0\n  1  2  2  0  0  0  0\n  2  3  1  0  0  0  0\n  3  4  2  0  0  0  0\n  4  5  1  0  0  0  0\n  5  6  2  0  0  0  0\n  6  1  1  0  0  0  0\n  1  7  1  0  0  0  0\n  2  8  1  0  0  0  0\nM  END\n","color":"#2ca02c"}]`);
-
+    const scaffoldTag = JSON.stringify([
+      {
+        molecule: _convertMolNotation('Cc1ccccc1', DG.chem.Notation.Smiles, DG.chem.Notation.MolBlock, rdkitModule),
+        color: '#ffbb78'
+      },
+      {
+        molecule: _convertMolNotation('Cc1ccccc1C', DG.chem.Notation.Smiles, DG.chem.Notation.MolBlock, rdkitModule),
+        color: '#2ca02c'
+      },
+    ]);
+    col.setTag(SCAFFOLD_TREE_HIGHLIGHT, scaffoldTag);
     //rendering with highlights
     start = performance.now();
     for (let i = 0; i < rowCount; i++) {
-      rendndererObj.render(moleculeHost.getContext('2d')!, 0, 0, 200, 100, DG.GridCell.fromValue(col.get(i)));
+      rendndererObj.render(moleculeHost.getContext('2d')!, 0, 0, 200, 100, grid.cell(colName, i));
     }
     console.log(`rendering of ${rowCount} molecules with highlight took ${performance.now() - start} milliseconds`);
     
