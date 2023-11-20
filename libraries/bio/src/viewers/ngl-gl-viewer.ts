9bfc7b8a3e91ce72d7960b2cefa1fcf89ef32dc8
tanas80 <ATanas@its.jnj.com>
2023-11-20 14:25:27 +0400

    #2481: BsV: Import ligands from pdbqt format
    BsV: Fix MolstarViewer error, Could not create WebGL program

--- libraries/bio/src/viewers/ngl-gl-viewer.ts
+++ libraries/bio/src/viewers/ngl-gl-viewer.ts
@@ -51,37 +51,3 @@ declare module 'datagrok-api/dg' {
     fromType(viewerType: 'NGL', options: Partial<NglProps>): Promise<DG.Viewer<NglProps> & INglViewer>;
   }
 }
-
-
-export type NglGlTask = {
-  name: string,
-  backColor: number,
-  props: { [propName: string]: any },
-  onAfterRender: (canvas: HTMLCanvasElement) => void
-};
-
-export abstract class NglGlServiceBase {
-  /** Queues NglGl render task
-   * @param key  Specify to skip previously queued tasks with the same key
-   */
-  abstract render(args: NglGlTask, key?: keyof any): void;
-
-  /** Default implementation of rendering tree on grid cell
-   * @param gCtx    Context to draw on grid
-   * @param bd      Bound rect to clip drawing on task moment
-   * @param gCell   Grid cell to draw
-   * @param canvas  Image of the tree rendered
-   */
-  abstract renderOnGridCell(
-    gCtx: CanvasRenderingContext2D, bd: DG.Rect, gCell: DG.GridCell, canvas: CanvasImageSource): void;
-}
-
-
-export async function getNglGlService(): Promise<NglGlServiceBase> {
-  const funcList = DG.Func.find({package: 'BiostructureViewer', name: 'getNglGlService'});
-  if (funcList.length === 0)
-    throw new Error('Package "BiostructureViewer" must be installed for NglGl services.');
-
-  const svc: NglGlServiceBase = (await funcList[0].prepare().call()).getOutputParamValue() as NglGlServiceBase;
-  return svc;
-}
