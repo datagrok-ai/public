3358c823a06284ab35795b876dcea09b240607a7
PavloPolovyi <pasha32662@gmail.com>
2023-12-08 13:06:32 +0100

    CvmTests: Use fetchProxy, added more tests

--- packages/CVMTests/src/docker/docker.ts
+++ packages/CVMTests/src/docker/docker.ts
@@ -1,8 +1,10 @@
 import * as grok from 'datagrok-api/grok';
-import {delay, category, expect, test} from '@datagrok-libraries/utils/src/test';
+import {delay, category, expect, test, expectObject, expectExceptionAsync} from '@datagrok-libraries/utils/src/test';
+import {dapi} from "datagrok-api/grok";
 
 category('Packages: Docker', () => {
   const containerOnDemandName: string = 'Cvmtests-docker-test1';
+  const containerSimple: string = 'Cvmtests-docker-test2';
 
   test('Get response: On demand', async () => {
     const container = await grok.dapi.docker.dockerContainers.filter(containerOnDemandName).first();
@@ -11,20 +13,51 @@ category('Packages: Docker', () => {
     await testResponse(container.id);
   }, {timeout: 60000});
 
+  test('Get response: Incorrect', async () => {
+    const incorrectId = crypto.randomUUID();
+    let response = await grok.dapi.docker.dockerContainers.fetchProxy(incorrectId, '/square?number=4');
+    // container not found
+    expect(response.status, 404);
+    const container = await grok.dapi.docker.dockerContainers.filter(containerSimple).first();
+    response = await grok.dapi.docker.dockerContainers.fetchProxy(container.id, '/foo?number=4');
+    // response from docker with 404 - no such path mapping
+    expect(response.status, 404);
+    response = await grok.dapi.docker.dockerContainers.fetchProxy(container.id, '/square?number=-0.01244');
+    // response from docker
+    expect(response.status, 500);
+    await dapi.docker.dockerContainers.stop(container.id, true);
+    response = await grok.dapi.docker.dockerContainers.fetchProxy(container.id, '/square?number=4');
+    // response from server, container status is stopped
+    expect(response.status, 400);
+    await dapi.docker.dockerContainers.run(container.id);
+  }, {timeout: 60000});
+
   test('Container timeout', async () => {
     let container = await grok.dapi.docker.dockerContainers.filter(containerOnDemandName).first();
     await grok.dapi.docker.dockerContainers.run(container.id, true);
-    await delay(80000);
+    await delay(70000);
     container = await grok.dapi.docker.dockerContainers.filter(containerOnDemandName).first();
     expect(container.status, 'stopped');
   }, {timeout: 120000});
+
+  test('Get container logs: Incorrect', async () => {
+    const incorrectId = crypto.randomUUID();
+    await expectExceptionAsync(async () => {
+      await grok.dapi.docker.dockerContainers.getContainerLogs(incorrectId);
+    });
+    const container = await grok.dapi.docker.dockerContainers.filter(containerSimple).first();
+    if (container.status != 'stopped')
+      await grok.dapi.docker.dockerContainers.stop(container.id, true);
+    await expectExceptionAsync(async () => {
+      await grok.dapi.docker.dockerContainers.getContainerLogs(incorrectId);
+    });
+  });
 });
 
 async function testResponse(containerId: string): Promise<void> {
-  const params: RequestInit = {
-    method: 'GET',
-  };
   const path = '/square?number=4';
-  const response = await grok.dapi.docker.dockerContainers.request(containerId, path, params);
-  expect(response?.trim(), '{"result":16}');
+  const response = await grok.dapi.docker.dockerContainers.fetchProxy(containerId, path);
+  expect(response.status, 200, `Container response status was ${response.status}`);
+  const result = await response.json();
+  expectObject(result, {"result": 16});
 }
