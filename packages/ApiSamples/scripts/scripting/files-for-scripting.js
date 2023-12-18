0ab6c79e0f6d85d6dce60eda257ce00d97f42b9f
Konstantin Amelichev <kostya.amelichev@gmail.com>
2023-12-18 14:02:52 +0100

    Apply suggestions from code review

--- packages/ApiSamples/scripts/scripting/files-for-scripting.js
+++ packages/ApiSamples/scripts/scripting/files-for-scripting.js
@@ -12,11 +12,12 @@ async function logFileContentAsBytes(fileInfo) {
     grok.shell.info(data);
 }
 
+// these values are the same for utf-8
 var line = 'test how script uses files'
 var bytes = [0x74, 0x65, 0x73, 0x74, 0x20, 0x68, 0x6F, 0x77, 0x20, 0x73, 0x63, 0x72, 0x69, 0x70, 0x74, 0x20, 0x75, 0x73, 0x65, 0x73, 0x20, 0x66, 0x69, 0x6C, 0x65, 0x73]
 var fileInfo = DG.FileInfo.fromString(line);
 logFileContentAsString(fileInfo);
 logFileContentAsBytes(fileInfo);
-fileInfo = DG.FileInfo.fromBytes();
+fileInfo = DG.FileInfo.fromBytes(bytes);
 logFileContentAsString(fileInfo);
 logFileContentAsBytes(fileInfo);
