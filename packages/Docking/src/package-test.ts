bdf2b669af3e971b34e71083f5583d828a3a98ce
Anna Muzychyna <annamuz69@gmail.com>
2023-12-19 15:16:19 +0200

    Docking: fix test func

--- packages/Docking/src/package-test.ts
+++ packages/Docking/src/package-test.ts
@@ -2,7 +2,7 @@ import * as DG from 'datagrok-api/dg';
 import * as grok from 'datagrok-api/grok';
 
 import '@datagrok-libraries/bio/src/types/ngl'; // To enable import from the NGL module declared in bio lib
-import {runTests, tests} from '@datagrok-libraries/utils/src/test';
+import {runTests, tests, TestContext} from '@datagrok-libraries/utils/src/test';
 
 import './tests/autodock-tests';
 
@@ -10,8 +10,12 @@ export let _package = new DG.Package();
 export {tests};
 
 //name: test
+//input: string category {optional: true}
+//input: string test {optional: true}
+//input: object testContext {optional: true}
 //output: dataframe result
-export async function test(): Promise<DG.DataFrame> {
-  let data = await runTests();
+export async function test(category: string, test: string, testContext: TestContext): Promise<DG.DataFrame> {
+  const data = await runTests({category, test, testContext});
   return DG.DataFrame.fromObjects(data)!;
 }
+
