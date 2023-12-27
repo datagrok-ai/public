b9e7f68dba3e78e841b5c1c988fa4e852f8c6f74
tanas80 <ATanas@its.jnj.com>
2023-12-27 17:56:17 +0400

    #2492: Docking: AutoDock
    Docking: Docking/autoDockApp
    Docking: Add AutoDock function editor UI

---
+++ packages/Docking/CHANGELOG.md
@@ -0,0 +1,13 @@
+# Docking changelog
+
+## 1.0.0 (WIP)
+
+### Features
+
+* Split from BiostructureViewer package
+
+### Bug fixes
+
+* Fix AutoDock for pose_count param
+* Fix AutoDock convert, add tests
+* Fix AutoDock add clinfo test
--- packages/Docking/src/package-test.ts
+++ packages/Docking/src/package-test.ts
@@ -1,9 +1,13 @@
-import * as DG from "datagrok-api/dg";
-import * as grok from "datagrok-api/grok";
+import * as DG from 'datagrok-api/dg';
+import * as grok from 'datagrok-api/grok';
+
+import '@datagrok-libraries/bio/src/types/ngl'; // To enable import from the NGL module declared in bio lib
 import {runTests, tests, TestContext} from '@datagrok-libraries/utils/src/test';
 
+import './tests/autodock-tests';
+
 export let _package = new DG.Package();
-export {tests}
+export {tests};
 
 //name: test
 //input: string category {optional: true}
@@ -14,3 +18,4 @@ export async function test(category: string, test: string, testContext: TestCont
   const data = await runTests({category, test, testContext});
   return DG.DataFrame.fromObjects(data)!;
 }
+
