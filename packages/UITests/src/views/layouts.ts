7760b0befc5e8d3b622034eae8547ee3e93c715c
Anna Muzychyna <annamuz69@gmail.com>
2024-01-08 21:12:57 +0200

    GROK-14498 UITests: Layout tests

--- packages/UITests/src/views/layouts.ts
+++ packages/UITests/src/views/layouts.ts
@@ -1,6 +1,6 @@
 import * as grok from 'datagrok-api/grok';
 import * as DG from 'datagrok-api/dg';
-import {after, before, category, expect, test} from '@datagrok-libraries/utils/src/test';
+import {after, awaitCheck, before, category, delay, expect, test} from '@datagrok-libraries/utils/src/test';
 
 
 category('Layouts', () => {
@@ -40,3 +40,47 @@ category('Layouts', () => {
     grok.shell.closeAll();
   });
 }, {clear: false});
+
+category('Layouts: Apply', () => {
+  const df: DG.DataFrame = grok.data.demo.demog(100);
+
+  test('toolbox', async () => {
+    const tv = grok.shell.addTableView(df);
+    grok.shell.windows.showToolbox = true;
+    const tb = tv.toolbox;
+    const layouts = Array.from(tb.querySelectorAll('div.d4-accordion-pane-header'))
+      .find((el) => el.textContent === 'Layouts') as HTMLElement;
+    if (!layouts.classList.contains('expanded'))
+      layouts.click();
+    tv.addViewer(DG.VIEWER.SCATTER_PLOT);
+    tv.addViewer(DG.VIEWER.HISTOGRAM);
+    tv.addViewer(DG.VIEWER.LINE_CHART);
+    await delay(2000);
+    let list: HTMLElement | null;
+    await awaitCheck(() => {
+      list = tb.querySelector('#layouts');
+      return list !== null;
+    }, '', 3000);
+    const save = Array.from(tb.querySelectorAll('.ui-btn'))
+      .find((el) => el.textContent === 'Save') as HTMLElement;
+    const num = list!.children.length + 1;
+    save.click();
+    await awaitCheck(() => tb.querySelector('#layouts')?.children.length === num, 'Layout was not saved', 3000);
+    try {
+      tv.resetLayout();
+      await delay(100);
+      await awaitCheck(() => [...tv.viewers].length === 1, 'Cannot reset layout', 3000);
+      (list!.firstElementChild?.firstElementChild as HTMLElement).click();
+      await awaitCheck(() => [...tv.viewers].length === 4,
+        `Layout was not applied, expected 3 viewers, got ${[...tv.viewers].length - 1}`, 3000);
+      await delay(100);
+    } finally {
+      const l = await grok.dapi.layouts.getApplicable(df);
+      l.sort((a, b) => a.createdOn > b.createdOn ? -1 : 1);
+      await grok.dapi.layouts.delete(l[0]);
+    }
+  });
+
+  test('gallery', async () => {
+  });
+});
