9a5c907a4294604a2c983339f6bbe1e4edc995dc
Dmitry Illarionov <illarionow@yahoo.com>
2024-01-18 16:08:34 +0100

    Docusaurus: Add new home-page design

---
+++ docusaurus/src/components/new/features.jsx
@@ -0,0 +1,18 @@
+import Markdown from 'markdown-to-jsx';
+import React from 'react';
+
+export default function Features({children, color}){
+    return(
+        <section className='container-fluid py-5'>
+            <div className='section' style={{backgroundColor: color != undefined ? color : 'white'}} >
+                <div className="container py-5 animate__animated animate__fadeIn" >
+                    <div className="row">
+                        <div className="col-lg-12 col-sm-10 mx-auto text-center section-content">
+                            {children}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
