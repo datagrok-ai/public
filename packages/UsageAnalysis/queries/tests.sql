afff2d112331de3c5ca61605b660f468ed90b3db
Anna Muzychyna <annamuz69@gmail.com>
2024-02-02 17:05:59 +0200

    UA: GROK-14638 Tests tab improvements

--- packages/UsageAnalysis/queries/tests.sql
+++ packages/UsageAnalysis/queries/tests.sql
@@ -63,7 +63,8 @@ case when v4.value::bool then 'skipped' when v1.value::bool then 'passed' else '
 v2.value as result,
 v3.value::int as ms,
 e.event_time as date,
-v5.value as type
+v5.value as type,
+v9.value as version
 from events e
 inner join event_types t on t.id = e.event_type_id and t.source = 'usage' and t.friendly_name like 'test-%'
 left join event_parameter_values v1 inner join event_parameters p1 on p1.id = v1.parameter_id and p1.name = 'success' on v1.event_id = e.id
@@ -74,14 +75,15 @@ inner join event_parameter_values v5 inner join event_parameters p5 on p5.id = v
 left join event_parameter_values v6 inner join event_parameters p6 on p6.id = v6.parameter_id and p6.name = 'packageName' on v6.event_id = e.id
 left join event_parameter_values v7 inner join event_parameters p7 on p7.id = v7.parameter_id and p7.name = 'test' on v7.event_id = e.id
 left join event_parameter_values v8 inner join event_parameters p8 on p8.id = v8.parameter_id and p8.name = 'category' on v8.event_id = e.id
+left join event_parameter_values v9 inner join event_parameters p9 on p9.id = v9.parameter_id and p9.name = 'version' on v9.event_id = e.id
 where e.event_time::date = @date
 order by e.description, e.event_time desc
 --end
 
 --name: TestsMonth
 --connection: System:Datagrok
---meta.cache: all
---meta.invalidateOn: 0 0 0 * *
+--meta.cache1: all
+--meta.invalidateOn1: 0 0 0 * *
 with ress as (select
 e.description, e.event_time,
 e.event_time::date as date,
