023ed45e5cb9309453b4ea91409d7123ba77e42e
Anna Muzychyna <annamuz69@gmail.com>
2024-02-09 01:55:29 +0200

    GROK-14410 ApiSamples: Broken scripts

--- packages/ApiSamples/scripts/ui/viewers/filters/custom-filters.js
+++ packages/ApiSamples/scripts/ui/viewers/filters/custom-filters.js
@@ -1,9 +1,9 @@
-let tv = grok.shell.addTableView(grok.data.demo.demog());
+let tv = grok.shell.addTableView(grok.data.demo.molecules());
 
 // We need to load the external package before we use the
 // synchronous "tv.filters" method that uses the filter
-grok.functions.call('Widgets:radioButtonFilter').then((_) => {
+grok.functions.call('Chem:substructureFilter').then((_) => {
   tv.filters({filters: [
-      {type: 'Widgets:radioButtonFilter', columnName: 'race'},
+      {type: 'Chem:substructureFilter', columnName: 'smiles'},
     ]});
 });
