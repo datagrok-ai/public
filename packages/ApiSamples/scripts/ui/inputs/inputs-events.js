140b357e1a4fd127808ca8e233d863d03fa3d624
Dmytro Kovalyov <laykdimon278@gmail.com>
2024-03-29 15:58:13 +0200

    GROK-14293: Inputs: Fixed samples

--- packages/ApiSamples/scripts/ui/inputs/inputs-events.js
+++ packages/ApiSamples/scripts/ui/inputs/inputs-events.js
@@ -5,7 +5,7 @@ const msg = (v) => grok.shell.info(v);
 let name = ui.input.string('Name', {value: 'Arthur Dent', onValueChanged: v => msg(v)});
 let age = ui.input.int('Age', {value: 30, onValueChanged: v => msg(v)});
 let sex = ui.input.choice('Sex', {items: ['Male', 'Female'], value: 'Male', onValueChanged: v => msg(v)});
-let date = ui.dateInput('Birthday', dayjs('1970-5-10'), v => msg(v));
+let date = ui.input.date('Birthday', {value: dayjs('1970-5-10'), onValueChanged: v => msg(v)});
 let alien = ui.boolInput('Alien', false, v => msg(v));
 let friends = ui.input.multiChoice('Friends', {items: ['Ford', 'Fenchurch', 'Zaphod', 'Slartibartfast'],
   value: ['Ford', 'Fenchurch'], onValueChanged: v => msg(v)});
--- packages/ApiSamples/scripts/ui/inputs/inputs.js
+++ packages/ApiSamples/scripts/ui/inputs/inputs.js
@@ -5,7 +5,7 @@ let v = grok.shell.newView('demo: inputs');
 let search = ui.input.search('Search');
 let name = ui.input.string('Name', {value: 'Arthur Dent'});
 let age = ui.input.int('Age', {value: 30});
-let date = ui.dateInput('Birthday', dayjs('1970-5-10'));
+let date = ui.input.date('Birthday', {value: dayjs('1970-5-10')});
 let alien = ui.boolInput('Alien', false);
 let friends = ui.input.multiChoice('Friends', {items: ['Ford', 'Fenchurch', 'Zaphod', 'Slartibartfast'],
   value: ['Ford', 'Fenchurch']});
