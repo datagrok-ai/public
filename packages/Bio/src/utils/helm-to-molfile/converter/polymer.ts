641e4190e20d57f53f4068203f39f6276e3a7511
Alexey <achopovsky@datagrok.ai>
2024-04-15 11:08:37 +0300

    Bio | PolyTool : polymer as v3k, WIP

--- packages/Bio/src/utils/helm-to-molfile/converter/polymer.ts
+++ packages/Bio/src/utils/helm-to-molfile/converter/polymer.ts
@@ -1,5 +1,5 @@
 import {RDModule} from '@datagrok-libraries/chem-meta/src/rdkit-api';
-import {V2K_CONST} from './const';
+import {V2K_CONST, V3K_CONST} from './const';
 import {Helm} from './helm';
 import {MonomerWrapper} from './monomer-wrapper';
 
@@ -59,22 +59,40 @@ export class Polymer {
     });
 
     const atomCount = atomLines.length;
-    if (atomCount > V2K_CONST.MAX_ATOM_COUNT) {
-      throw new Error(
-        `Atom count in polymer ${this.helm.toString()} is ${atomCount} and exceeds ${V2K_CONST.MAX_ATOM_COUNT}`
-      );
-    }
-
     const bondCount = bondLines.length;
-    const countsLine = `${
-      atomCount.toString().padStart(3, ' ')
-    }${
-      bondCount.toString().padStart(3, ' ')
-    }  0  0  1  0              0 V2000`;
-    const molfileEnd = 'M  END\n';
-    const newLineChar = '\n';
-    const blockList = [molfileHeader, countsLine, atomLines.join(newLineChar), bondLines.join(newLineChar), molfileEnd];
-    const molfile = blockList.join(newLineChar);
+
+    const header = this.getV3KHeader(atomCount, bondCount);
+    const atomBlock = this.getV3KAtomBlock(atomLines);
+    const bondBlock = this.getV3KBondBlock(bondLines);
+    const molfileEnd = V3K_CONST.END_CTAB + '\n' + V3K_CONST.END;
+    const blockList = [header, atomBlock, bondBlock, molfileEnd];
+    const molfile = blockList.join('\n');
     return molfile;
+  }
+
+  private getV3KHeader(atomCount: number, bondCount: number): string {
+    const countsLine = `${V3K_CONST.COUNTS_LINE_START}${atomCount} ${bondCount}${V3K_CONST.COUNTS_LINE_END}`;
+    return `${V3K_CONST.HEADER}\n${V3K_CONST.BEGIN_CTAB}\n${countsLine}`;
+  }
+
+  private getV3KAtomBlock(atomLines: string[]): string {
+    const regex = /^(M  V30 )(\d+)( .*)$/;
+    const newAtomLines = atomLines.map((line, idx) => {
+      const atomIndex = idx + 1;
+      return line.replace(regex, (match, p1, p2, p3) => { return p1 + atomIndex + p3; });
+    });
+
+    const atomBlock = [V3K_CONST.BEGIN_ATOM_BLOCK, ...newAtomLines, V3K_CONST.END_ATOM_BLOCK];
+    return atomBlock.join('\n');
+  }
+
+  private getV3KBondBlock(bondLines: string[]): string {
+    const regex = /^(M  V30 )(\d+)( .*)$/;
+    const newBondLines = bondLines.map((line, idx) => {
+      const atomIndex = idx + 1;
+      return line.replace(regex, (match, p1, p2, p3) => { return p1 + atomIndex + p3; });
+    });
+    const bondBlock = [V3K_CONST.BEGIN_BOND_BLOCK, ...newBondLines, V3K_CONST.END_BOND_BLOCK];
+    return bondBlock.join('\n');
   }
 }
