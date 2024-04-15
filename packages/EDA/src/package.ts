82363edc0ace3bfb414b130d6037300ff88d5bce
vmakarichev <vmakarichev@datagrok.ai>
2024-04-15 14:07:11 +0300

    GROK-15371: EDA | ANOVA: Remove old ANOVA
    Previous version of ANOVA is removed

--- packages/EDA/src/package.ts
+++ packages/EDA/src/package.ts
@@ -418,19 +418,6 @@ export async function applySigmoidKernelSVM(df: DG.DataFrame, model: any): Promi
   return await getPrediction(df, model);
 }
 
-//top-menu: ML | Analyze | ANOVA old...
-//name: ANOVAold
-//description: One-way analysis of variances (ANOVA) determines whether the examined factor has a significant impact on the studied feature.
-//input: dataframe table
-//input: column factor {type: categorical}
-//input: column feature {type: numerical}
-//input: double significance = 0.05 [The significance level is a value from the interval (0, 1) specifying the criterion used for rejecting the null hypothesis.]
-//input: bool validate = false [Indicates whether the normality of distribution and an eqaulity of varainces should be checked.]
-export function anovaOld(table: DG.DataFrame, factor: DG.Column, feature: DG.Column, significance: number, validate: boolean) {
-  const res = oneWayAnova(factor, feature, significance, validate);
-  //addOneWayAnovaVizualization(table, factor, feature, res);
-}
-
 //top-menu: ML | Analyze | ANOVA...
 //name: ANOVA
 //description: One-way analysis of variances (ANOVA) determines whether the examined factor has a significant impact on the explored feature.
