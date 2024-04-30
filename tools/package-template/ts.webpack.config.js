ad2ebf274da7cf046ba9a7464fd8512b9dc7fe71
Sofia Podolsky <spodolskaya@datagrok.ai>
2024-04-30 12:14:19 +0300

    Tools: ts-loader option for ts file in node modules

--- tools/package-template/ts.webpack.config.js
+++ tools/package-template/ts.webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
   },
   module: {
     rules: [
-      {test: /\.tsx?$/, loader: 'ts-loader'},
+      {test: /\.tsx?$/, loader: 'ts-loader', options: {allowTsInNodeModules: true}},
     ],
   },
   plugins: [
