5731ab200c09a5eb23f18b839461215d56f75785
lbankurova <97047900+lbankurova@users.noreply.github.com>
2024-05-22 07:47:30 -0400

    Update MDXComponents.js
    Added "fa-regular" class for FontAweseome icons

--- docusaurus/src/theme/MDXComponents.js
+++ docusaurus/src/theme/MDXComponents.js
@@ -6,6 +6,7 @@ import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'; // Import the Fo
 import {library} from '@fortawesome/fontawesome-svg-core'; // Import the library component.
 import {fab} from '@fortawesome/free-brands-svg-icons'; // Import all brands icons.
 import {fas} from '@fortawesome/free-solid-svg-icons'; // Import all solid icons.
+import {fas} from '@fortawesome/free-regular-svg-icons'; // Import all regular icons.
 
 library.add(fab, fas); // Add all icons to the library so you can use them without importing them individually.
 
