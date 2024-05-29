eb31f1d0b266786968fe851db1fd9f86742cb107
Leonid Stolbov <57249456+StLeonidas@users.noreply.github.com>
2024-05-29 12:18:49 +0300

    PolyTool: rules from json

--- packages/SequenceTranslator/src/polytool/const.ts
+++ packages/SequenceTranslator/src/polytool/const.ts
@@ -1,17 +1,5 @@
 import {HELM_REQUIRED_FIELD} from '@datagrok-libraries/bio/src/utils/const';
 
-export const ALL_MONOMERS = '<All>';
-
-export const enum TRANSFORMATION_TYPE {
-  CYCLIZATION = 'Cyclization',
-}
-
-export const enum CYCLIZATION_TYPE {
-  NO = 'N-O',
-  NCys = 'N-Cys',
-  R3 = 'R3-R3',
-}
-
 export const helmFieldsToPolyToolInputFields = {
   [HELM_REQUIRED_FIELD.SYMBOL]: 'Short Name',
   [HELM_REQUIRED_FIELD.NAME]: 'Medium Name',
