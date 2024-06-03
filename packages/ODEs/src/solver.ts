91fd002f2a01d8d8f8b5f728892e491bf2b4aa32
vmakarichev <vmakarichev@datagrok.ai>
2024-06-03 15:47:38 +0300

    GROK-15756: Diff Studio: Analysis Tools
    Added features for analyzing numerical schemes

--- packages/ODEs/src/solver.ts
+++ packages/ODEs/src/solver.ts
@@ -8,9 +8,64 @@ import {ODEs, SolverOptions} from './solver-tools/solver-defs';
 import {mrt} from './solver-tools/mrt-method';
 import {ros3prw} from './solver-tools/ros3prw-method';
 import {ros34prw} from './solver-tools/ros34prw-method';
+import {error} from './utils';
 
 /** Default solver of initial value problem. */
-export const solveDefault = (odes: ODEs): DG.DataFrame => mrt(odes);
+export const solveDefault = (odes: ODEs): DG.DataFrame => ros34prw(odes);
 
 /** Customizable solver of initial value problem. */
 export const solveIVP = (odes: ODEs, options?: Partial<SolverOptions>): DG.DataFrame => ros34prw(odes, options);
+
+/** Solve initial value problem and check performance of the Rosenbrok methods: MRT, ROS3PRw & ROS34PRw.
+
+    REMARK. The methods MRT & ROS3PRw may require much time for stiff problems. */
+export function checkPerformance(odes: ODEs): DG.DataFrame {
+  let start = 0;
+  let sum = 0;
+  const runsCount = 10;
+
+  console.log(odes.name);
+
+  const dfMrt = mrt(odes);
+  const dfRos3prw = ros3prw(odes);
+  const dfRos34prw = ros34prw(odes);
+
+  console.log('Deviation:');
+  const d1 = error(dfMrt, dfRos3prw);
+  const d2 = error(dfMrt, dfRos34prw);
+  const d3 = error(dfRos3prw, dfRos34prw);
+
+  console.log(`  MRT vs ROS3PRw: ${d1}`);
+  console.log(`  MRT vs ROS34PRw: ${d2}`);
+  console.log(`  ROS3PRw vs ROS34PRw: ${d3}`);
+  console.log(`  MAX: ${Math.max(d1, d2, d3)}`);
+
+  console.log('TIME');
+
+  for (let i = 0; i < runsCount; ++i) {
+    start = performance.now();
+    mrt(odes);
+    sum += performance.now() - start;
+  }
+  console.log(`  MRT: ${sum / runsCount} ms.`);
+
+  sum = 0;
+  for (let i = 0; i < runsCount; ++i) {
+    start = performance.now();
+    ros3prw(odes);
+    sum += performance.now() - start;
+  }
+  console.log(`  ROS3PRw: ${sum / runsCount} ms.`);
+
+  sum = 0;
+  for (let i = 0; i < runsCount; ++i) {
+    start = performance.now();
+    ros34prw(odes);
+    sum += performance.now() - start;
+  }
+  console.log(`  ROS34PRw: ${sum / runsCount} ms.`);
+
+  console.log('==============================================================================');
+
+  return dfRos34prw;
+};
