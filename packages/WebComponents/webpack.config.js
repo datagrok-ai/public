54e20da51166f1e367bc50dd3f45a78d3ba8b6ac
andrey-sharapov-sc <129750144+andrey-sharapov-sc@users.noreply.github.com>
2024-06-07 11:50:00 +0300

    Webcomponents: Initial  (#2882)
    * Webcomponents: Initial
    
    * Webcomponents: Vue bindings intial, test, better viewer reactivity
    
    * Webcomponents: Linter fixes
    
    * Webcomponents: Viewer tests and fixes
    
    * Webcomponents: Test fixes
    
    * Webcomponents: Form fixes
    
    * Webcomponents: Make libs
    
    * Webcomponents: Loader package, Libs optimize
    
    * Webcomponents: Linter fixes
    
    * Webcomponents: Expiremental reflection bridge between Viewers and Webcomponents
    
    * Revert "Webcomponents: Expiremental reflection bridge between Viewers and Webcomponents"
    
    This reverts commit 0f5b1401e7300c8f530ddc720a7449f0b95f904e.
    
    * Webcomponents: Naming cleanups

--- packages/WebComponents/webpack.config.js
+++ packages/WebComponents/webpack.config.js
@@ -4,26 +4,15 @@ const packageName = path.parse(require('./package.json').name).name.toLowerCase(
 module.exports = {
   mode: 'production',
   entry: {
-    test: {
-      filename: 'package-test.js',
-      library: {type: 'var', name: `${packageName}_test`},
-      import: './src/package-test.ts',
-    },
+    test: {filename: 'package-test.js', library: {type: 'var', name: `${packageName}_test`}, import: './src/package-test.ts'},
     package: './src/package.ts',
   },
   resolve: {
-    extensions: ['.wasm', '.mjs', '.ts', '.tsx', '.js', '.json'],
+    extensions: ['.wasm', '.mjs', '.ts', '.tsx', '.json', '.js'],
   },
   module: {
     rules: [
-      {test: /\.tsx?$/, loader: 'babel-loader', options: {
-	'plugins': ['@vue/babel-plugin-jsx']
-      }},
-      {test: /\.tsx?$/, loader: 'ts-loader', options: {allowTsInNodeModules: true}},
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
+      {test: /\.tsx?$/, loader: 'ts-loader'},
       {
         test: /\.(js|mjs|jsx|ts|tsx)$/,
         enforce: 'pre',
@@ -34,13 +23,11 @@ module.exports = {
   devtool: 'source-map',
   externals: {
     'datagrok-api/dg': 'DG',
-    'DG': 'DG',
     'datagrok-api/grok': 'grok',
     'datagrok-api/ui': 'ui',
     'openchemlib/full.js': 'OCL',
     'rxjs': 'rxjs',
     'rxjs/operators': 'rxjs.operators',
-    'rxjs.operators': 'rxjs.operators',
     'cash-dom': '$',
     'dayjs': 'dayjs',
     'wu': 'wu',
@@ -52,7 +39,6 @@ module.exports = {
     library: packageName,
     libraryTarget: 'var',
     path: path.resolve(__dirname, 'dist'),
-    clean: true,
   },
   optimization: {
     usedExports: true,
