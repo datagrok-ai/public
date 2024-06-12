83ea1cf13dc08ef99623fb4a19726ea28fd1cf0e
PavloPolovyi <pasha32662@gmail.com>
2024-06-12 10:59:49 +0200

    UsageAnalysis: Fixed tests

--- packages/UsageAnalysis/src/tests/app.ts
+++ packages/UsageAnalysis/src/tests/app.ts
@@ -1,21 +1,16 @@
 import * as DG from 'datagrok-api/dg';
 import * as grok from 'datagrok-api/grok';
-
 import { before, after, category, test, expect, awaitCheck } from '@datagrok-libraries/utils/src/test';
-import { ViewHandler } from '../view-handler';
+
 
 category('App', () => {
   const tabs = ['Overview', 'Packages', 'Functions', 'Events', 'Log', 'Tests'];
   const num = [4, 4, 5, 4, 3, 5];
-  let handler = new ViewHandler();
+  let view: DG.MultiView;
 
   before(async () => {
-    handler = new ViewHandler();
-    if (grok.shell.sidebar.panes.every((p) => p.name != ViewHandler.UA_NAME)) {
-      await handler.init();
-      grok.shell.addView(handler.view);
-      grok.shell.sidebar.currentPane = grok.shell.sidebar.getPane(ViewHandler.UA_NAME);
-    }
+    view = await grok.functions.eval('UsageAnalysis:usageAnalysisApp()');
+    grok.shell.addView(view);
   });
 
   test('open', async () => {
@@ -24,16 +19,15 @@ category('App', () => {
 
   for (let i = 0; i < 6; i++) {
     test(tabs[i], async () => {
-      handler.changeTab(tabs[i]);
-      const view = grok.shell.v.root;
+      view.tabs.currentPane = view.tabs.getPane(tabs[i]);
       let err: any = undefined;
       let s = ['Log'].includes(tabs[i]) ? '.grok-wait + .d4-grid canvas' : 'canvas';
       try {
-        await awaitCheck(() => view.querySelectorAll(s).length === num[i], '', 10000);
+        await awaitCheck(() => view.root.querySelectorAll(s).length === num[i], '', 10000);
       } catch (e) {
         err = 'Tab failed to load in 10 seconds';
       }
-      await awaitCheck(() => view.querySelectorAll(s).length === num[i],
+      await awaitCheck(() => view.root.querySelectorAll(s).length === num[i],
         `expected ${num[i]}, got ${document.querySelectorAll(s).length}`, 45000);
       if (err)
         throw new Error(err);
