f3b35d1db7f4adb49a3e02a9bb6f86f504e89416
drizhina <drizhinashvili@datagrok.ai>
2024-06-17 21:23:25 +0300

    #2905: Box plot: API for color/size/marker selection

--- js-api/src/interfaces/d4.ts
+++ js-api/src/interfaces/d4.ts
@@ -904,6 +904,8 @@ export interface ILineChartSettings {
   /// Requires *Multi Axis* to be enabled
   addLinesToLegend: boolean;
 
+  autoAdjustMultiAxisLegendPosition: boolean;
+
   multiAxisLegendPosition: keyof typeof FlexExtendedPosition;
 
   innerChartMarginTop: number;
@@ -1291,15 +1293,32 @@ export interface IBoxPlotSettings {
   /// See also *Bin Color*.
   binColorAggrType: string;
 
+  showColorSelector: boolean;
+
   /// Column to color-code markers.
   markerColor: string;
   markerColorColumnName: string;
+
+  markers: string;
+  markersColumnName: string;
+
+  markerMinSize: number;
+
+  markerMaxSize: number;
+
+  showSizeSelector: boolean;
+
+  markerSizeColumnName: string;
 
   markerType: string;
 
   markerSize: number;
 
   markerOpacity: number;
+
+  legendVisibility: keyof typeof VisibilityMode;
+
+  legendPosition: keyof typeof FlexPosition;
 
   showMeanCross: boolean;
 
@@ -1323,6 +1342,8 @@ export interface IBoxPlotSettings {
   showPValue: boolean;
 
   showMouseOverPoint: boolean;
+
+  showMouseOverRowGroup: boolean;
 
   statistics: Array<string>;
 
