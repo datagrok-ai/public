f7a0173c02d45e5667d7b996ba8bf97203212f07
Stanislav Savenko <ss.savenko@gmail.com>
2024-06-18 13:17:07 +0300

    Docusarus: Compatibility tool: after code review fixes

--- help/deploy/releases/compatibility/layout.js
+++ help/deploy/releases/compatibility/layout.js
@@ -14,7 +14,10 @@ const ToolMarkup = () => {
         <label for="horns">Breaking changes</label>
         <br />
         <input type="checkbox" id="anyCausesBreakingChangesCheckbox" disabled style={{ margin: '0 2px' }} />
-        <label for="horns">Include cases where parameter type changes either to or from ‘any’</label>
+        <label for="horns" style={{}}>Type conversions </label>
+        <div id="tooltip" style={{ display: 'inline-block', width: '15px', position: 'relative', marginLeft: '10px' }}>
+            <svg id="svgIcon" viewBox="0 0 14 16" ><path fill-rule="evenodd" d="M6.3 5.69a.942.942 0 0 1-.28-.7c0-.28.09-.52.28-.7.19-.18.42-.28.7-.28.28 0 .52.09.7.28.18.19.28.42.28.7 0 .28-.09.52-.28.7a1 1 0 0 1-.7.3c-.28 0-.52-.11-.7-.3zM8 7.99c-.02-.25-.11-.48-.31-.69-.2-.19-.42-.3-.69-.31H6c-.27.02-.48.13-.69.31-.2.2-.3.44-.31.69h1v3c.02.27.11.5.31.69.2.2.42.31.69.31h1c.27 0 .48-.11.69-.31.2-.19.3-.42.31-.69H8V7.98v.01zM7 2.3c-3.14 0-5.7 2.54-5.7 5.68 0 3.14 2.56 5.7 5.7 5.7s5.7-2.55 5.7-5.7c0-3.15-2.56-5.69-5.7-5.69v.01zM7 .98c3.86 0 7 3.14 7 7s-3.14 7-7 7-7-3.12-7-7 3.14-7 7-7z"></path></svg>
+        </div>
         <br />
         <input type="checkbox" id="newFunctionalityCheckbox" style={{ margin: '0 2px' }} />
         <label for="horns">New functionality</label>
@@ -24,9 +27,50 @@ const ToolMarkup = () => {
         <div id="compareResultsBlock">
         </div>
         <script src="./output.js" type="text/javascript"></script>
-    </div>);
+    </div >);
 
     useEffect(() => {
+        var tooltipTrigger = document.getElementById("tooltip");
+        var svgIcon = document.getElementById('svgIcon');
+        var mouseOnSvg = false;
+        tooltipTrigger.addEventListener('mouseover', function () {
+            var tooltip = document.createElement('span');
+            tooltip.textContent = 'Include cases where parameter type changes either to or from ‘any’';
+
+            tooltip.style.visibility = 'visible';
+            tooltip.style.width = '520px';
+            tooltip.style.backgroundColor = '#555';
+            tooltip.style.color = '#fff';
+            tooltip.style.textAlign = 'center';
+            tooltip.style.borderRadius = '6px';
+            tooltip.style.padding = '5px 0';
+            tooltip.style.position = 'absolute';
+            tooltip.style.zIndex = '1';
+            tooltip.style.bottom = '-10%';
+            tooltip.style.left = '70px';
+            tooltip.style.marginLeft = '-60px';
+            tooltip.style.opacity = '0.9';
+            tooltip.style.padding = '5px';
+            tooltip.style.transition = 'opacity 0.3s';
+
+            tooltipTrigger.appendChild(tooltip);
+        });
+
+        tooltipTrigger.addEventListener('mouseout', function () {
+            var tooltip = tooltipTrigger.querySelector('span');
+            if (tooltip && !mouseOnSvg) {
+                tooltip.remove();
+            }
+        });
+        
+        svgIcon.addEventListener('mouseover', function (event) {
+            mouseOnSvg = true; 
+        });
+
+        svgIcon.addEventListener('mouseout', function () {
+            mouseOnSvg = false;
+        });
+        
         var breakingChangesCheckbox = document.getElementById("breakingChangesCheckbox");
 
 
@@ -199,7 +243,6 @@ const ToolMarkup = () => {
                     if (functionsData[className][functionName].hasOwnProperty(selectorToCompare1.value)) {
                         if (functionsData[className][functionName].hasOwnProperty(selectorToCompare2.value)) {
                             var preElement1 = document.createElement("pre");
-                            var preElement2 = document.createElement("pre");
                             var textElement1 = functionSignatureToString(functionName, functionsData[className][functionName][selectorToCompare1.value]);
                             var textElement2 = functionSignatureToString(functionName, functionsData[className][functionName][selectorToCompare2.value]);
 
@@ -211,14 +254,12 @@ const ToolMarkup = () => {
                                     if (isBreakingChanges(functionsData[className][functionName][selectorToCompare1.value],
                                         functionsData[className][functionName][selectorToCompare2.value])) {
                                         detailsBlock.appendChild(preElement1)
-                                        detailsBlock.appendChild(preElement2)
                                         detailsBlock.appendChild(document.createElement("br"))
                                         hasCompares = true;
                                     }
                                 }
                                 else if (showNewFunctionality == false) {
                                     detailsBlock.appendChild(preElement1)
-                                    detailsBlock.appendChild(preElement2)
                                     detailsBlock.appendChild(document.createElement("br"))
                                     hasCompares = true;
                                 }
@@ -244,10 +285,10 @@ const ToolMarkup = () => {
                             divSignatureElement = document.createElement("div");
                             divSignatureElement.style = "display: inline-block;";
                             divSignatureElement.textContent = textElement2;
-                            
-                            preElement2.textContent = '';
-                            preElement2.appendChild(divVersionElement);
-                            preElement2.appendChild(divSignatureElement);
+
+                            preElement1.appendChild(document.createElement("br"));
+                            preElement1.appendChild(divVersionElement);
+                            preElement1.appendChild(divSignatureElement);
                         }
                         else {
                             var preElement1 = document.createElement("pre");
@@ -288,7 +329,6 @@ const ToolMarkup = () => {
 
                     addedElements.forEach((preElement1) => {
                         detailsBlock.appendChild(preElement1)
-                        detailsBlock.appendChild(document.createElement("br"))
                     });
                 }
 
@@ -301,7 +341,6 @@ const ToolMarkup = () => {
                     detailsBlock.appendChild(label)
                     removedElements.forEach((preElement1) => {
                         detailsBlock.appendChild(preElement1)
-                        detailsBlock.appendChild(document.createElement("br"))
                     });
                 }
 
