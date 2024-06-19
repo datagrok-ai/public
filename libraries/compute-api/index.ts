cb7ac04e6b77e0c382aa2ce5a3c09b431844d7aa
Aufar Zakiev <zaufar@gmail.com>
2024-06-19 14:53:37 +0300

    ComputeApi: Exposed testPipeline method

--- libraries/compute-api/index.ts
+++ libraries/compute-api/index.ts
@@ -3,6 +3,9 @@ import * as grok from 'datagrok-api/grok';
 import * as ui from 'datagrok-api/ui';
 import * as DG from 'datagrok-api/dg';
 
+import {testPipeline} from './src/utils';
+export {testPipeline};
+
 import * as UiUtils from './src/ui-utils';
 export {UiUtils};
 
