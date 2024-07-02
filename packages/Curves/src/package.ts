0ba7caaa7403570a19b9550fdfb98fc5ebd4ece7
Dmytro Kovalyov <laykdimon278@gmail.com>
2024-07-02 13:18:37 +0300

    Packages: Curves: Unskip demo test

--- packages/Curves/src/package.ts
+++ packages/Curves/src/package.ts
@@ -40,7 +40,6 @@ export function _FitViewer(): MultiCurveViewer {
 //description: Curve fitting is the process of constructing a curve, or mathematical function, that has the best fit to a series of data points
 //meta.demoPath: Curves | Curve fitting
 //test: curveFitDemo() //wait: 2000
-//meta.demoSkip: skip
 export async function curveFitDemo(): Promise<void> {
   await curveDemo();
 }
