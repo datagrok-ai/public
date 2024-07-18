a1176c3cdba4197f5dde9b3e7ca4a254d3266fd7
drizhina <drizhinashvili@datagrok.ai>
2024-07-18 09:21:38 -0400

    HT: Fix build

--- packages/HitTriage/webpack.config.js
+++ packages/HitTriage/webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-
+const packageName = path.parse(require('./package.json').name).name.toLowerCase().replace(/-/g, '');
 module.exports = {
   cache: {
     type: 'filesystem',
