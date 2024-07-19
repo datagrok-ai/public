7f715bfe8db3e7b2124ef3c103ce85df0a0c0fcd
Aufar Zakiev <zaufar@gmail.com>
2024-07-19 11:46:26 +0300

    WebComponents: Moved SplitH to another file

--- libraries/webcomponents/src/elements.ts
+++ libraries/webcomponents/src/elements.ts
@@ -1,7 +1,6 @@
 import * as grok from 'datagrok-api/grok';
 import * as ui from 'datagrok-api/ui';
 import * as DG from 'datagrok-api/dg';
-import wu from 'wu';
 
 export class DGButton extends HTMLButtonElement {
   constructor() {
@@ -20,50 +19,3 @@ export class DGBigButton extends HTMLButtonElement {
 }
 
 export interface DGBigButtonT extends DGBigButton {};
-
-export class DGSplitH extends HTMLElement {
-  // DG API
-  private _resize = true;
-
-  private defaultSlot!: HTMLSlotElement;
-  private inner = null as HTMLElement | null;
-
-  private initialSlots = [] as HTMLElement[];
-  private inited = false;
-
-  constructor() {
-    super();
-
-    if (!this.shadowRoot)
-      this.attachShadow({mode: 'open'});
-
-    this.defaultSlot = this.shadowRoot!.appendChild(ui.element('slot') as HTMLSlotElement);
-  }
-
-  private render() {
-    if (!this.inited) {
-      this.initialSlots = this.defaultSlot.assignedElements() as HTMLElement[];
-      this.inited = true;
-    }
-
-    const newInner = this.appendChild(ui.splitH(
-      this.initialSlots,
-      {},
-      this._resize,
-    ));
-
-    if (this.inner) {
-      ui.empty(this.inner);
-      this.inner.remove();
-    };
-
-    this.inner = newInner;
-  }
-
-  set resize(val: boolean) {
-    this._resize = val;
-    this.render();
-  }
-}
-
-export interface DGSplitHT extends DGSplitH {};
