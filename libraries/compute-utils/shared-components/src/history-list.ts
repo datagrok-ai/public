70abf3492b3099d1bedd6bee576e69293d4bc7d5
Dmytro Kovalyov <61393026+LaykDimon@users.noreply.github.com>
2024-08-06 22:25:15 +0300

    GROK-16240: ColumnMetaHelper: Replaced tags usages with ColumnMetaHelper (#2929)

--- libraries/compute-utils/shared-components/src/history-list.ts
+++ libraries/compute-utils/shared-components/src/history-list.ts
@@ -335,7 +335,7 @@ export class HistoricalRunsList extends DG.Widget {
       this._historyFilters.dataFrame = newRunsGridDf;
       this._historyGrid.dataFrame = newRunsGridDf;
 
-      this.currentDf.getCol(TAGS_COLUMN_NAME).setTag(DG.TAGS.MULTI_VALUE_SEPARATOR, ',');
+      this.currentDf.getCol(TAGS_COLUMN_NAME).meta.multiValueSeparator = ',';
 
       this.styleHistoryGrid();
       this.styleHistoryFilters();
