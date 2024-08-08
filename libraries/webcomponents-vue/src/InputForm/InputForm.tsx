200eeb7a6f2c09c055ace4d737d8a5416827a52f
Aufar Zakiev <zaufar@gmail.com>
2024-08-08 16:27:09 +0300

    LibTests: Simplified InputForm validation

--- libraries/webcomponents-vue/src/InputForm/InputForm.tsx
+++ libraries/webcomponents-vue/src/InputForm/InputForm.tsx
@@ -2,7 +2,7 @@ import * as grok from 'datagrok-api/grok';
 import * as ui from 'datagrok-api/ui';
 import * as DG from 'datagrok-api/dg';
 
-import {computed, defineComponent, PropType, reactive, ref} from 'vue';
+import {computed, defineComponent, isReactive, KeepAlive, PropType, ref, watch, watchEffect} from 'vue';
 import type {InputFormT} from '@datagrok-libraries/webcomponents/src';
 import {getValidators,
   injectInputBaseValidation,
@@ -11,7 +11,8 @@ import {getValidators,
 } from '@datagrok-libraries/compute-utils/shared-utils/utils';
 import {SYNC_FIELD} from '@datagrok-libraries/compute-utils/shared-utils/consts';
 import {Validator} from '@datagrok-libraries/compute-utils/shared-utils/validation';
-import {FuncCallInputValidated, isFuncCallInputValidated} from '@datagrok-libraries/compute-utils/shared-utils/input-wrappers';
+import {isFuncCallInputValidated} from '@datagrok-libraries/compute-utils/shared-utils/input-wrappers';
+import {computedAsync} from '@vueuse/core';
 
 declare global {
   namespace JSX {
@@ -20,10 +21,6 @@ declare global {
     }
   }
 }
-
-const getFormSource = (form: DG.InputForm): DG.FuncCall => {
-  return DG.toJs(form.source);
-};
 
 export const InputForm = defineComponent({
   name: 'InputForm',
@@ -42,6 +39,8 @@ export const InputForm = defineComponent({
     const allParams = (funcCall: DG.FuncCall) =>
       [...funcCall.inputParams.values() ?? []].map((param) => param.name);
 
+    let loadedValidators = {};
+
     const formReplacedCb = async (event: {detail?: DG.InputForm}) => {
       emit('formReplaced', event.detail);
 
@@ -51,33 +50,32 @@ export const InputForm = defineComponent({
 
       if (!form) return;
 
-      [...getFormSource(form).inputParams.values()].map((param) => param.name).forEach((param) => {
-        const input = form.getInput(param);
-        if (isInputBase(input))
-          injectInputBaseValidation(input);
-      });
-
-      const loadedValidators = await getValidators(getFormSource(form), SYNC_FIELD.INPUTS);
-      inputValidators.value = loadedValidators;
+      [...props.funcCall.inputParams.values()]
+        .map((param) => param.name)
+        .forEach((param) => {
+          const input = form.getInput(param);
+          if (isInputBase(input))
+            injectInputBaseValidation(input);
+        });
 
       runValidation();
     };
 
-    const inputChangedCb = () => {
-      runValidation();
+    const inputChangedCb = (event: {detail: DG.EventData<DG.InputArgs>}) => {
+      runValidation([event.detail.args.input.property.name]);
     };
 
     const runValidation = async (paramNames?: string[]) => {
       if (!currentForm) return;
 
-      const paramsToValidate = paramNames ?? allParams(getFormSource(currentForm));
+      const paramsToValidate = paramNames ?? allParams(props.funcCall);
 
       const controller = new AbortController();
 
       const results = await validate({isRevalidation: false},
         paramsToValidate, controller.signal, SYNC_FIELD.INPUTS, {
-          funcCall: getFormSource(currentForm),
-        }, inputValidators.value);
+          funcCall: props.funcCall,
+        }, loadedValidators);
 
       Object.entries(results).forEach(([paramName, res]) => {
         const t = currentForm!.getInput(paramName);
@@ -86,26 +84,16 @@ export const InputForm = defineComponent({
       });
     };
 
-    const inputValidators = ref({} as Record<string, Validator>);
-
-    getValidators(props.funcCall, SYNC_FIELD.INPUTS).then((loadedValidators) => {
-      inputValidators.value = loadedValidators;
-
-      runValidation();
-    });
-
     return () => {
+      getValidators(props.funcCall, SYNC_FIELD.INPUTS).then((res) => loadedValidators = res);
+
       const form = <dg-input-form
         funcCall={props.funcCall}
         onFormReplaced={formReplacedCb}
         onInputChanged={inputChangedCb}>
       </dg-input-form>;
 
-      return (
-        <keep-alive>
-          { form }
-        </keep-alive>
-      );
+      return form;
     };
   },
 });
--- packages/LibTests/src/components/VueFormTestApp.tsx
+++ packages/LibTests/src/components/VueFormTestApp.tsx
@@ -3,16 +3,10 @@ import * as grok from 'datagrok-api/grok';
 import * as ui from 'datagrok-api/ui';
 import * as DG from 'datagrok-api/dg';
 import {InputForm} from '@datagrok-libraries/webcomponents-vue/src';
-import {defineComponent, shallowRef, onMounted, triggerRef} from 'vue';
+import {defineComponent, shallowRef, onMounted, triggerRef, ref} from 'vue';
 
 export const VueFormTestApp = defineComponent({
   name: 'VueFormTestApp',
-  mounted() {
-    console.log('VueFormTestApp mounted');
-  },
-  unmounted() {
-    console.log('VueFormTestApp unmounted');
-  },
   setup() {
     const fc = shallowRef<DG.FuncCall | undefined>(undefined);
 
