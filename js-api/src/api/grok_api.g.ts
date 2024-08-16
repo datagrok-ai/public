781464a1038999386c1491d4a54fae280f0f1f42
Oleksandra Serhiienko <aleksandrasergienko12@gmail.com>
2024-08-16 11:36:17 +0300

    Js-Api: TreeViewGroup: Ability to set text

--- js-api/src/api/grok_api.g.ts
+++ js-api/src/api/grok_api.g.ts
@@ -1136,7 +1136,8 @@ export interface IDartApi {
   grok_TreeViewNode_Set_Tag(node: any, tag: any): any;
   grok_TreeViewNode_Get_CurrentItem(node: any): any;
   grok_TreeViewNode_Set_CurrentItem(group: any, node: any): any;
-  grok_TreeViewNode_Text(node: any): any;
+  grok_TreeViewNode_Get_Text(node: any): any;
+  grok_TreeViewNode_Set_Text(node: any, value: String): any;
   grok_TreeViewNode_Group(node: any, text: any, value: any, expanded: Bool, index: Num): any;
   grok_TreeViewNode_GetOrCreateGroup(node: any, text: String, value: any, expanded: Bool): any;
   grok_TreeViewNode_Item(node: any, text: any, value: any): any;
