45596b6d538088343a351358945f2df75c630fa8
Maria Dolotova <maria.dolotova@softwarecountry.com>
2024-08-27 13:16:06 +0300

    Revert "Chem: Removed custom molecule labels drawing on sp"
    This reverts commit 9de542efa4553434859f401c08be3e7cb4fd4844.

--- packages/Chem/src/analysis/molecular-matched-pairs/mmp-viewer.ts
+++ packages/Chem/src/analysis/molecular-matched-pairs/mmp-viewer.ts
@@ -13,6 +13,8 @@ import {getMmpTrellisPlot} from './mmp-frag-vs-frag';
 import {getMmpScatterPlot, runMmpChemSpace} from './mmp-cliffs';
 import {getGenerations} from './mmp-generations';
 
+
+import {drawMoleculeLabels} from '../../rendering/molecule-label';
 import {ILineSeries, MouseOverLineEvent, ScatterPlotLinesRenderer}
   from '@datagrok-libraries/utils/src/render-lines-on-sp';
 import BitArray from '@datagrok-libraries/utils/src/bit-array';
@@ -480,6 +482,8 @@ export class MatchedMolecularPairsViewer extends DG.JsViewer {
     console.log(`created mmpa filters`);
 
     const sp = getMmpScatterPlot(mmpInput, embedColsNames);
+
+    drawMoleculeLabels(mmpInput.table, mmpInput.molecules, sp as DG.ScatterPlotViewer, 20, 7, 100, 110);
 
     //running internal chemspace
     const linesEditor = runMmpChemSpace(mmpInput, sp, lines, linesIdxs, linesActivityCorrespondance,
