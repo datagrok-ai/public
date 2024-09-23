44c73534fedd3f91999f8eeab3b51bfe3b95dc03
vmakarichev <vmakarichev@datagrok.ai>
2024-09-23 21:28:42 +0300

    GROK-16716: EDA: ANOVA tests
    Added tests for ANOVA

--- packages/EDA/src/package-test.ts
+++ packages/EDA/src/package-test.ts
@@ -1,9 +1,10 @@
 import * as DG from 'datagrok-api/dg';
-import {runTests, tests, TestContext, initAutoTests as initTests } from '@datagrok-libraries/utils/src/test';
+import {runTests, tests, TestContext, initAutoTests as initTests} from '@datagrok-libraries/utils/src/test';
 import './tests/dim-reduction-tests';
 import './tests/linear-methods-tests';
 import './tests/classifiers-tests';
 import './tests/mis-vals-imputation-tests';
+import './tests/anova-tests';
 export const _package = new DG.Package();
 export {tests};
 
