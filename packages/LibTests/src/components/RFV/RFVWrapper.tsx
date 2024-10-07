02e5ffd00b5a836db044f02cfb3cc9534a097424
Aufar Zakiev <zaufar@gmail.com>
2024-10-07 14:29:33 +0300

    LibTests: Added run saving to RFV2

--- packages/LibTests/src/components/RFV/RFVWrapper.tsx
+++ packages/LibTests/src/components/RFV/RFVWrapper.tsx
@@ -4,6 +4,9 @@ import * as DG from 'datagrok-api/dg';
 import * as Vue from 'vue';
 
 import {RichFunctionView} from './RichFunctionView';
+import {FuncCallStateInfo} from '@datagrok-libraries/compute-utils/reactive-tree-driver/src/runtime/StateTreeNodes';
+import {historyUtils} from '@datagrok-libraries/compute-utils';
+import {isIncomplete, createPartialCopy} from '@datagrok-libraries/compute-utils/shared-utils/utils';
 
 export const RFVWrapper = Vue.defineComponent({
   props: {
@@ -14,16 +17,29 @@ export const RFVWrapper = Vue.defineComponent({
   },
   setup(props) {
     const currentFuncCall = Vue.shallowRef(props.funcCall);
+    const currentCallState = Vue.ref(
+      {isRunning: false, isOutputOutdated: false, isRunnable: true, pendingDependencies: []},
+    );
+
+    const func = Vue.computed(() => currentFuncCall.value.func);
+    const isRunningOnInput = Vue.computed(() => func.value.options['runOnInput'] === 'true');
+
     const runFunc = async () => {
       if (!currentFuncCall.value) return;
 
+      currentCallState.value.isRunning = true;
       await currentFuncCall.value.call();
+      currentCallState.value.isRunning = false;
+
+      if (!isRunningOnInput.value)     
+        await historyUtils.saveRun(currentFuncCall.value);
     };
 
     return () => (
       <div style={{width: '100%', height: '100%'}}>
         <RichFunctionView 
           funcCall={currentFuncCall.value}
+          callState={currentCallState.value}
           onUpdate:funcCall={(chosenCall) => currentFuncCall.value = chosenCall}
           onRunClicked={runFunc}
         />
