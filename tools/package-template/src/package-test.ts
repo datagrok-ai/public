a2cafef834c63d0f80a63184a755541429df4ffd
Stanislav Savenko <ss.savenko@gmail.com>
2024-10-16 10:07:19 +0300

    Grok Tools: Package Template: Added global file

--- tools/package-template/src/package-test.ts
+++ tools/package-template/src/package-test.ts
@@ -1,4 +1,5 @@
 import { runTests, tests, TestContext , initAutoTests as initTests } from '@datagrok-libraries/utils/src/test';
+import { DataFrame } from 'datagrok-api/dg';
 
 export let _package = new DG.Package();
 export { tests };
@@ -8,7 +9,7 @@ export { tests };
 //input: string test {optional: true}
 //input: object testContext {optional: true}
 //output: dataframe result
-export async function test(category: string, test: string, testContext: TestContext): Promise<DG.DataFrame> {
+export async function test(category: string, test: string, testContext: TestContext): Promise<DataFrame> {
   const data = await runTests({ category, test, testContext });
   return DG.DataFrame.fromObjects(data)!;
 }
