e2672c79713709bb42d819a9fc0e8e17aef96ebf
PavloPolovyi <pasha32662@gmail.com>
2024-10-22 18:41:39 +0200

    ApiSamples: Fixed sample

--- packages/ApiSamples/scripts/ui/inputs/forms.js
+++ packages/ApiSamples/scripts/ui/inputs/forms.js
@@ -5,31 +5,30 @@ let v = grok.shell.newView('demo: form types');
 //Basic form
 //To set labels aligned to left add class .ui-form-left
 let form = ui.form([
-  ui.h1('Basic form'),
   ui.input.string('First name', {value: 'Arthur'}),
   ui.input.string('Last name', {value: 'Dent'}),
   ui.input.int('Age', {value: 30}),
 ]);
 
 //Add class .ui-form-wide to make the form wide
-let wideForm = ui.form([
-  ui.h1('Wide form'),
-  ui.input.string('First name', {value: 'Arthur'}),
-  ui.input.string('Last name', {value: 'Dent'}),
-  ui.input.int('Age', {value: 30}),
-], 'ui-form-wide');
-
-//Condensed form. You can also use .ui-form-condensed
-let narrowForm = ui.narrowForm([
-  ui.h1('Condensed form'),
+let wideForm = ui.wideForm([
   ui.input.string('First name', {value: 'Arthur'}),
   ui.input.string('Last name', {value: 'Dent'}),
   ui.input.int('Age', {value: 30}),
 ]);
-v.append(ui.panel([narrowForm]));
+
+//Condensed form. You can also use .ui-form-condensed
+let narrowForm = ui.narrowForm([
+  ui.input.string('First name', {value: 'Arthur'}),
+  ui.input.string('Last name', {value: 'Dent'}),
+  ui.input.int('Age', {value: 30}),
+]);
 
 v.append(ui.divV([
+  ui.h1('Basic form'),
   form,
+  ui.h1('Wide form'),
   wideForm,
+  ui.h1('Condensed form'),
   narrowForm,
 ]));
