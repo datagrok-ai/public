ce667b6044aadf436e8deb3f2b5894910e459c29
Savenko Stanislav <ss.savenko@gmail.com>
2024-10-23 16:49:31 +0300

    Api Samples : Samples: default View fix

--- packages/ApiSamples/scripts/ui/views/default-views.js
+++ packages/ApiSamples/scripts/ui/views/default-views.js
@@ -1,9 +1,10 @@
-//tags: View
-let host = ui.divV(null, {style: {width: '500px', height: '500px'}});
+  //tags: View
+  let host = ui.divV(null, {style: {width: '500px', height: '500px'}});
 
-let selector = ui.input.choice('View', {items: DG.View.ALL_VIEW_TYPES, value: DG.View.APPS, onValueChanged: (x) => {
-  $(host).empty();
-  host.appendChild(DG.View.createByType(x).root);
-}});
+  let selector = ui.input.choice('View', {items: DG.View.ALL_VIEW_TYPES, value: DG.View.APPS, onValueChanged: (x) => {
+    $(host).empty();
+    host.appendChild(DG.View.createByType(x).root);
+  }});
 
-ui.dialog().add(ui.divV([selector, host])).show();
+  ui.dialog().add(ui.divV([selector, host])).show();
+  host.appendChild(DG.View.createByType(DG.View.ALL_VIEW_TYPES[0]).root);я
