187e4d9c8a81066ef011aac63519aa67a8d48caf
Alex Palamarchuk <opalamarchuk@datagrok.ai>
2024-10-24 15:55:42 +0300

    GROK-16923: Updated benchmarks

--- packages/PowerGrid/src/tests/webgpu-tests.ts
+++ packages/PowerGrid/src/tests/webgpu-tests.ts
@@ -1,53 +1,64 @@
 import * as grok from 'datagrok-api/grok';
-import * as ui from 'datagrok-api/ui';
 import * as DG from 'datagrok-api/dg';
-
-import $ from 'cash-dom';
 
 import {awaitCheck, category, expect, expectArray, test, testEvent} from '@datagrok-libraries/utils/src/test';
 import { _scWebGPURender, _scWebGPUPointHitTest } from '../package';
+import { forEach } from 'wu';
 
+const sizes = [10000, 1000000, 10000000];
+const types = ['dot', 'circle'];
+const consecutiveLaunches = 3;
 
 category('webgpu', () => {
-  test('gpuRender', async () => {
-    const types = ['dot', 'circle'];
-    for (let size = 10000000; size <= 10000000; size *= 10) {
-      for (let type = 0; type < types.length; ++type) {
-        const df: DG.DataFrame = grok.data.demo.randomWalk(size, 2);
-        df.name = 'test';
-        const view = grok.shell.addTableView(df);
-        df.currentRowIdx = 0;
-    
-        const sp: DG.ScatterPlotViewer = view.scatterPlot();
-        sp.props.markerType = types[type];
-  
-        for (let i = 1; i <= 3; ++i) {
-          await DG.timeAsync(`${i} GPU Render ${size / 1000000.0}m ${types[type]}s`, async() => {
-            await _scWebGPURender(sp, true);
-          });
-        }
-      }
-    }
-  }, {benchmarkTimeout: 20000});
-  
-  test('gpuHitTest', async () => {
-    const types = ['dot', 'circle'];
-    for (let size = 10000000; size <= 10000000; size *= 10) {
-      for (let type = 0; type < types.length; ++type) {
-        const df: DG.DataFrame = grok.data.demo.randomWalk(size, 2);
-        df.name = 'test';
-        const view = grok.shell.addTableView(df);
-        df.currentRowIdx = 0;
-    
-        const sp: DG.ScatterPlotViewer = view.scatterPlot();
-        sp.props.markerType = types[type];
-  
-        for (let i = 1; i <= 3; ++i) {
-          await DG.timeAsync(`${i} GPU Hit Test ${size / 1000000.0}m ${types[type]}s`, async() => {
-            await _scWebGPUPointHitTest(sp, new DG.Point(0, 0));
-          });
-        }
-      }
-    }
-  }, {benchmarkTimeout: 20000});
+  const render = DG.Func.find({tags: ['scWebGPURender']})[0];
+  const hitTest = DG.Func.find({tags: ['scWebGPUPointHitTest']})[0];
+  types.forEach((t) => {
+    sizes.forEach((s) => {
+        test(`GPURender: ${s / 1000000.0}m ${t}s`, async () => {
+          const view = grok.shell.addTableView(grok.data.demo.randomWalk(s, 2));    
+          const sp: DG.ScatterPlotViewer = view.scatterPlot();
+          sp.props.markerType = t;
+          for (let i = 1; i <= consecutiveLaunches; ++i) {        
+            await DG.timeAsync(`Render Call #${i}`, async() => {
+              await render.apply([sp, true]);
+            });
+          }
+        }, {benchmarkTimeout: 10000});
+
+        test(`GPURender Color: ${s / 1000000.0}m ${t}s`, async () => {
+          const view = grok.shell.addTableView(grok.data.demo.randomWalk(s, 2));    
+          const sp: DG.ScatterPlotViewer = view.scatterPlot();
+          sp.props.markerType = t;
+          sp.props.sizeColumnName = '#1';
+          for (let i = 1; i <= consecutiveLaunches; ++i) {        
+            await DG.timeAsync(`Render Call #${i}`, async() => {
+              await render.apply([sp, true]);
+            });
+          }
+        }, {benchmarkTimeout: 10000});
+
+        test(`GPURender Size: ${s / 1000000.0}m ${t}s`, async () => {
+          const view = grok.shell.addTableView(grok.data.demo.randomWalk(s, 2));    
+          const sp: DG.ScatterPlotViewer = view.scatterPlot();
+          sp.props.markerType = t;
+          sp.props.colorColumnName = '#0';
+          for (let i = 1; i <= consecutiveLaunches; ++i) {        
+            await DG.timeAsync(`Render Call #${i}`, async() => {
+              await render.apply([sp, true]);
+            });
+          }
+        }, {benchmarkTimeout: 10000});
+
+        test(`GPUHitTest: ${s / 1000000.0}m ${t}s`, async () => {
+          const view = grok.shell.addTableView(grok.data.demo.randomWalk(s, 2));    
+          const sp: DG.ScatterPlotViewer = view.scatterPlot();
+          sp.props.markerType = t;
+          for (let i = 1; i <= consecutiveLaunches; ++i) {        
+            await DG.timeAsync(`GPU HitTest call #${i}`, async() => {
+              await hitTest.apply([sp, new DG.Point(0, 0)]);
+            });
+          }
+        }, {benchmarkTimeout: 10000});
+    });
+  });
 }, {benchmarks: true});
