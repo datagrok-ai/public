3dbe7d71d23f3b17630ae3b9cfff844af809af07
Stanislav Savenko <ss.savenko@gmail.com>
2024-10-31 18:22:45 +0200

    Grok Tools: Test: Added ability reload page on Execution Timeout

--- tools/bin/utils/test-utils.ts
+++ tools/bin/utils/test-utils.ts
@@ -4,12 +4,17 @@ import path from 'path';
 import yaml from 'js-yaml';
 import * as utils from '../utils/utils';
 import { PuppeteerNode } from 'puppeteer';
+import { PuppeteerScreenRecorder } from 'puppeteer-screen-recorder';
 import { spaceToCamelCase } from '../utils/utils';
+import puppeteer from 'puppeteer';
+import { Browser, Page } from 'puppeteer';
+import * as color from '../utils/color-utils';
 
 const fetch = require('node-fetch');
 
 const grokDir = path.join(os.homedir(), '.grok');
 const confPath = path.join(grokDir, 'config.yaml');
+const testCollectionTimeout = 100000;
 
 export const defaultLaunchParameters: utils.Indexable = {
   args: [
@@ -157,7 +162,7 @@ export async function loadPackages(packagesDir: string, packagesToLoad?: string,
   let hostString = host === undefined ? `` : `${host}`;
   if (packagesToLoad !== "all") {
     for (let pacakgeName of (packagesToLoad ?? "").split(' ')) {
-      if((pacakgeName ?? '').length !== 0)
+      if ((pacakgeName ?? '').length !== 0)
         packagesToRun.set(spaceToCamelCase(pacakgeName).toLocaleLowerCase(), false);
     }
   }
@@ -168,7 +173,7 @@ export async function loadPackages(packagesDir: string, packagesToLoad?: string,
 
       try {
         const packageJsonData = JSON.parse(fs.readFileSync(path.join(packageDir, 'package.json'), { encoding: 'utf-8' }));
-        const packageFriendlyName = packagesToRun.get(spaceToCamelCase(packageJsonData["friendlyName"] ?? packageJsonData["name"].split("/")[1] ?? packageJsonData["name"]?? '').toLocaleLowerCase() ?? "");
+        const packageFriendlyName = packagesToRun.get(spaceToCamelCase(packageJsonData["friendlyName"] ?? packageJsonData["name"].split("/")[1] ?? packageJsonData["name"] ?? '').toLocaleLowerCase() ?? "");
 
         if (utils.isPackageDir(packageDir) && (packageFriendlyName !== undefined || packagesToLoad === "all")) {
           try {
@@ -200,8 +205,228 @@ export async function loadPackages(packagesDir: string, packagesToLoad?: string,
     .map(([key]) => key);;
 }
 
+export async function loadTestsList(packages: string[], core: boolean = false): Promise<Object[]> {
+  var packageTestsData = await timeout(async () => {
+    const params = Object.assign({}, defaultLaunchParameters);
+    // params['headless'] = false;
+    const out = await getBrowserPage(puppeteer, params);
+    const browser: Browser = out.browser;
+    const page: Page = out.page;
+
+    const r = await page.evaluate((packages, coreTests): Promise<any> => {
+      return new Promise<any>((resolve, reject) => {
+        const promises: any[] = [];
+        try {
+          packages.map((packageName: string) => {
+            const p = (<any>window).DG.Func.find({ package: packageName, name: 'test' })[0]?.package;
+            if (p) {
+              try {
+                promises.push(p.getTests(coreTests).catch((e: any) => {
+                  console.error('something else went wrong with collecting package tests')
+                  console.error(e?.message)
+                })
+                  .then((ts: any) => ({ packageName: packageName, tests: ts })))
+              } catch (e: any) {
+                console.error('something went wrong while adding test collection promise');
+                console.error(e.message);
+              }
+            }
+          });
+
+        } catch (err) {
+          console.error("Error during evaluation in browser context:", err);
+          reject();
+        }
+        Promise.all(promises)
+          .then((results: any[]) => {
+            resolve(results);
+          }).catch((e: any) => {
+            const stack = ((<any>window).DG.Logger.translateStackTrace(e.stack)).then(() => {
+              resolve({
+                failReport: `${e.message}\n${stack}`
+              });
+            });
+          });
+      });
+    }, packages, core);
+
+    if (browser != null) {
+      await browser.close();
+    }
+
+    return r;
+  }, testCollectionTimeout);
+
+  let testsList: Object[] = [];
+
+  for (let testPackage of packageTestsData) {
+    for (const key in testPackage.tests) {
+      if (testPackage.tests.hasOwnProperty(key)) {
+        for (let testValue of testPackage.tests[key].tests) {
+          testValue.packageName = testPackage.packageName;
+          testsList.push(testValue);
+        }
+      }
+    }
+  }
+  return testsList;
+}
+
+export function addLogsToFile(filePath: string, stringToSave: any) {
+  fs.appendFileSync(filePath, `${stringToSave}`);
+}
+
+export function printWorkersResult(workerResult: ResultObject, verbose: boolean = false) {
+  if (verbose) {
+    if ((workerResult.passedAmount ?? 0) > 0 && (workerResult.verbosePassed ?? []).length > 0) {
+      console.log("Passed: ");
+      console.log(workerResult.verbosePassed);
+    }
+    if ((workerResult.skippedAmount ?? 0) > 0 && (workerResult.verboseSkipped ?? []).length > 0) {
+      console.log("Skipped: ");
+      console.log(workerResult.verboseSkipped);
+    }
+  }
+
+  if ((workerResult.failedAmount ?? 0) > 0 && (workerResult.verboseFailed ?? []).length > 0) {
+    console.log("Failed: ");
+    console.log(workerResult.verboseFailed);
+  }
+  console.log("Passed amount:  " + workerResult?.passedAmount);
+  console.log("Skipped amount: " + workerResult?.skippedAmount);
+  console.log("Failed amount:  " + workerResult?.failedAmount);
+
+  if (workerResult.failed) {
+    color.fail('Tests failed.');
+  } else {
+    color.success('Tests passed.');
+  }
+}
+
+export function saveCsvResults(stringToSave: string[], csvReportDir: string) {
+  const modifiedStrings = stringToSave.map((str, index) => {
+    if (index === 0) return str;
+    return str.split('\n').slice(1).join('\n');
+  });
+
+  fs.writeFileSync(csvReportDir, modifiedStrings.join('\n'), 'utf8');
+  color.info('Saved `test-report.csv`\n');
+}
+
+export async function runWorker(testExecutionData: any[], workerOptions: WorkerOptions, workersId: number, testInvocationTimeout: number = 3600000): Promise<ResultObject> {
+  return await timeout(async () => {
+    const params = Object.assign({}, defaultLaunchParameters);
+    if (workerOptions.gui)
+      params['headless'] = false;
+    const out = await getBrowserPage(puppeteer, params);
+    const browser: Browser = out.browser;
+    const page: Page = out.page;
+    const recorder = new PuppeteerScreenRecorder(page, recorderConfig);
+
+    const currentWorkerNum = workersId;
+    const logsDir = `./test-console-output-${currentWorkerNum}.log`;
+    const recordDir = `./test-record-${currentWorkerNum}.mp4`;
+
+    if (workerOptions.record) {
+      await recorder.start(recordDir);
+      await page.exposeFunction("addLogsToFile", addLogsToFile);
+
+      fs.writeFileSync(logsDir, ``);
+      page.on('console', (msg) => { addLogsToFile(logsDir, `CONSOLE LOG ENTRY: ${msg.text()}\n`); });
+      page.on('pageerror', (error) => { addLogsToFile(logsDir, `CONSOLE LOG ERROR: ${error.message}\n`); });
+      page.on('response', (response) => {
+        addLogsToFile(logsDir, `CONSOLE LOG REQUEST: ${response.status()}, ${response.url()}\n`);
+      });
+    }
+    let testingResults = await page.evaluate((testData, options): Promise<any> => {
+      if (options.benchmark)
+        (<any>window).DG.Test.isInBenchmark = true;
+
+      return new Promise<any>((resolve, reject) => {
+        (<any>window).DG.Utils.executeTests(testData, options.stopOnTimeout)
+          .then((results: any) => {
+            resolve(results);
+          })
+          .catch((e: any) => {
+            resolve({
+              failed: true,
+              verbosePassed: "",
+              verboseSkipped: "",
+              verboseFailed: "Tests execution failed",
+              passedAmount: 0,
+              skippedAmount: 0,
+              failedAmount: 1,
+              csv: "",
+              df: undefined
+            })
+          });
+      })
+    }, testExecutionData, workerOptions);
+
+    if (workerOptions.record) {
+      await recorder.stop();
+    }
+
+    if (browser != null) {
+      await browser.close();
+    }
+    return testingResults;
+  }, testInvocationTimeout);
+}
+
+export async function mergeWorkersResults(workersResults: ResultObject[]) : Promise<ResultObject>{
+
+  let mergedResult: ResultObject = {
+    failed: workersResults[0].failed,
+    verbosePassed: workersResults[0].verbosePassed,
+    verboseSkipped:  workersResults[0].verboseSkipped,
+    verboseFailed: workersResults[0].verboseFailed,
+    passedAmount: workersResults[0].passedAmount,
+    skippedAmount:  workersResults[0].skippedAmount,
+    failedAmount: workersResults[0].failedAmount,
+    csv: workersResults[0].csv
+  }
+
+  for (let workersResult of workersResults) {
+    if (mergedResult.csv === workersResult.csv)
+      continue;
+
+    mergedResult.failed = mergedResult.failed && workersResult.failed;
+    mergedResult.verbosePassed = `${mergedResult.verbosePassed.trim()}\n${workersResult.verbosePassed.trim()}`;
+    mergedResult.verboseFailed = `${mergedResult.verboseFailed.trim()}\n${workersResult.verboseFailed.trim()}`;
+    mergedResult.verboseSkipped = `${mergedResult.verboseSkipped.trim()}\n${workersResult.verboseSkipped.trim()}`;
+
+    mergedResult.passedAmount += workersResult.passedAmount;
+    mergedResult.failedAmount += workersResult.failedAmount;
+    mergedResult.skippedAmount += workersResult.skippedAmount;
+
+    const resultToMerdge1 = mergedResult.csv.trim().split('\n');
+    const resultToMerdge2 = workersResult.csv.trim().split('\n');
+
+    const header = resultToMerdge1[0];
+    mergedResult.csv = [
+      header,
+      ...resultToMerdge1.slice(1),
+      ...resultToMerdge2.slice(1)
+    ].join('\n');
+  }
+  return mergedResult;
+}
+
 export interface WorkerOptions {
   path?: string, catchUnhandled?: boolean, core?: boolean,
   report?: boolean, record?: boolean, verbose?: boolean, benchmark?: boolean, platform?: boolean, category?: string, test?: string,
-  stressTest?: boolean, gui?: boolean
+  stressTest?: boolean, gui?: boolean, stopOnTimeout?: boolean
 }
+
+export type ResultObject = {
+  failed: boolean,
+  verbosePassed: string,
+  verboseSkipped: string,
+  verboseFailed: string,
+  passedAmount: number,
+  skippedAmount: number,
+  failedAmount: number,
+  csv: string,
+  df?: any
+};
