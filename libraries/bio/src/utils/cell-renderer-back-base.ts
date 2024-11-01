a1ac6f8e1e710e41b457b8daf92c749808e9c255
Oleksandra Serhiienko <aleksandrasergienko12@gmail.com>
2024-11-01 13:48:50 +0200

    Bio: Renderer: Add check for table column

--- libraries/bio/src/utils/cell-renderer-back-base.ts
+++ libraries/bio/src/utils/cell-renderer-back-base.ts
@@ -24,7 +24,7 @@ export function getGridCellColTemp<TValue, TBack extends CellRendererBackBase<TV
   } catch { [gridCol, temp] = [null, null]; }
 
   const tableCol: DG.Column<TValue> = gridCell.cell.column;
-  temp = temp ?? tableCol.temp;
+  temp = temp ?? (tableCol ? tableCol.temp : null);
 
   if (!temp)
     throw new Error(`Grid cell renderer back store (GridColumn or Column) not found.`);
