65816c67e1541b5e203775744a0cb4a0b2bd6b05
Aufar Zakiev <zaufar@gmail.com>
2024-11-05 18:03:39 +0300

    Compute2: Moved ScalarsPanel into separate component

--- packages/Compute2/src/components/RFV/RichFunctionView.tsx
+++ packages/Compute2/src/components/RFV/RichFunctionView.tsx
@@ -18,6 +18,7 @@ import {useUrlSearchParams} from '@vueuse/core';
 import {FuncCallStateInfo} from '@datagrok-libraries/compute-utils/reactive-tree-driver/src/runtime/StateTreeNodes';
 import {FittingView} from '@datagrok-libraries/compute-utils/function-views/src/fitting-view';
 import {SensitivityAnalysisView} from '@datagrok-libraries/compute-utils';
+import { ScalarsPanel } from './ScalarsPanel';
 
 type PanelsState = {
   historyHidden: boolean,
@@ -79,98 +80,6 @@ const tabToProperties = (func: DG.Func) => {
 
   return map;
 };
-
-const DEFAULT_FLOAT_PRECISION = 2;
-
-export const ScalarsPanel = Vue.defineComponent({
-  name: 'ScalarsPanel',
-  props: {
-    funcCall: {
-      type: Object as Vue.PropType<DG.FuncCall>,
-      required: true,
-    },
-    categoryScalars: {
-      type: Array as Vue.PropType<DG.Property[]>,
-      required: true,
-    },
-  },
-  setup(props) {
-    const getContent = (prop: DG.Property) => {
-      const precision = prop.options.precision;
-
-      const scalarValue = props.funcCall.outputs[prop.name];
-      const formattedScalarValue = prop.propertyType === DG.TYPE.FLOAT && scalarValue ?
-        precision ? scalarValue.toPrecision(precision): scalarValue.toFixed(DEFAULT_FLOAT_PRECISION):
-        scalarValue;
-      const units = prop.options['units'] ? ` [${prop.options['units']}]`: ``;
-
-      return [formattedScalarValue, units];
-    };
-
-    const copyToClipboard = async (text: string) => {
-      await navigator.clipboard.writeText(text);
-      grok.shell.info('Value is copied to clipboard');
-    };
-
-    const hoveredIdx = Vue.ref(null as null | number);
-
-    return () =>
-      props.categoryScalars.length <= 3 ?
-        <div
-          class='flex flex-wrap justify-around'
-        >
-          { props.categoryScalars.map((prop, idx) => {
-            const [scalarValue, units] = getContent(prop);
-
-            return <div
-              class='flex flex-col p-2 items-center gap-4 flex-nowrap'
-              onMouseenter={() => hoveredIdx.value = idx}
-              onMouseleave={() => hoveredIdx.value = null}
-            >
-              <div class='text-center' style={{color: 'var(--grey-4)'}}> { prop.caption ?? prop.name } </div>
-              <span style={{fontSize: 'var(--font-size-large)'}}>
-                { scalarValue } { units }
-                <span style={{color: 'var(--grey-3)', paddingLeft: '3px'}} class='absolute'>
-                  { hoveredIdx.value === idx && <IconFA
-                    name='copy'
-                    tooltip="Copy caption & value"
-                    onClick={() => copyToClipboard(`${ prop.caption ?? prop.name } ${ scalarValue } ${ units } `)}
-                  /> }
-                </span>
-              </span>
-            </div>;
-          })}
-        </div> :
-        <div class='h-full overflow-scroll'>
-          <table class='d4-table d4-item-table d4-info-table rfv-scalar-table'>
-            <tbody>
-              {
-                props.categoryScalars.map((prop, idx) => {
-                  const [scalarValue, units] = getContent(prop);
-
-                  return <tr
-                    onMouseenter={() => hoveredIdx.value = idx}
-                    onMouseleave={() => hoveredIdx.value = null}
-                  >
-                    <td> <span> { prop.caption ?? prop.name } </span></td>
-                    <td> <span> { units } </span></td>
-                    <td> <span> { scalarValue } </span></td>
-                    <td>
-                      { hoveredIdx.value === idx && <IconFA
-                        style={{color: 'var(--grey-3)'}}
-                        name='copy'
-                        tooltip="Copy caption & value"
-                        onClick={() => copyToClipboard(`${ prop.caption ?? prop.name } ${ scalarValue } ${ units } `)}
-                      /> }
-                    </td>
-                  </tr>;
-                })
-              }
-            </tbody>
-          </table>
-        </div>;
-  },
-});
 
 export const RichFunctionView = Vue.defineComponent({
   name: 'RichFunctionView',
