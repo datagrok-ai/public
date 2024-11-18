08207e0a8a4d3e732c56f9f6d4b96e8d4f21e18a
Stanislav Savenko <ss.savenko@gmail.com>
2024-11-18 18:53:31 +0200

    Usage Analysis: Detectors: test fixes

--- packages/UsageAnalysis/src/test-grid-cell-handler.ts
+++ packages/UsageAnalysis/src/test-grid-cell-handler.ts
@@ -1,5 +1,6 @@
 import { category } from "@datagrok-libraries/utils/src/test";
 import { delay, DockManager, GridCell } from "datagrok-api/dg";
+import * as DG from 'datagrok-api/dg';
 
 export class TestGridCellHandler extends DG.ObjectHandler {
     get type(): string {
@@ -16,12 +17,33 @@ export class TestGridCellHandler extends DG.ObjectHandler {
 
         const buttonsData = ui.button('Run', async () => {
             let df = gridCell.cell.dataFrame;
-            await grok.functions.call(`${testData[0].trim()}:test`, { test: testData[2].trim(), category: testData[1].trim() })
+            let progressBar = DG.TaskBarProgressIndicator.create(`Running test ${testData.join(':')}`);
+            let category = testData[1];
+            for (let pathPart of testData)
+                if (testData[0] != pathPart && testData[1] != pathPart && testData[testData.length - 1] != pathPart)
+                    category = [category, pathPart.trim()].join(':');
+            let testResult: DG.DataFrame = await grok.functions.call(`${testData[0].trim()}:test`, { test: testData[testData.length - 1].trim(), category: category.trim() })
             let tv = grok.shell.addTableView(df);
+            let wasError = false;
+            let wasSkipped = false;
+            for (let a of testResult.rows) {
+                if (a.get('skipped'))
+                    wasSkipped = true;
+                else if (!a.get('success'))
+                    wasError = true;
+            }
+            grok.shell.closeAll();
+            if (wasSkipped)
+                grok.shell.warning(`${testData.join(':')} was skipped`);
+            else if (!wasError)
+                grok.shell.info(`${testData.join(':')} successfully run`);
+            else
+                grok.shell.error(`${testData.join(':')} failed`);
 
             await delay(100);
-            tv.dataFrame.currentRowIdx =  gridCell.cell.rowIndex ?? 0;
+            tv.dataFrame.currentRowIdx = gridCell.cell.rowIndex ?? 0;
             await delay(100);
+            progressBar.close();
             df.currentCell = df.cell(gridCell.cell.rowIndex ?? 0, gridCell.cell.column.name);
         });
         buttonsData.classList.add('ui-btn-raised');
