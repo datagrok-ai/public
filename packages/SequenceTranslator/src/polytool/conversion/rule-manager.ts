82d4dca4ea0c2e7842c3d02eb4692ef71f7ab43b
StLeonid <stolbovla@yandex.ru>
2024-11-19 15:14:43 +0300

    ST: PolyTool: ui for ule manager

--- packages/SequenceTranslator/src/polytool/conversion/rule-manager.ts
+++ packages/SequenceTranslator/src/polytool/conversion/rule-manager.ts
@@ -87,16 +87,33 @@ export class RulesManager {
       reactionRules: this.rules.reactionRules,
     };
 
-    const rrrr = JSON.stringify(saveable, undefined, 2);
-    _package.files.writeAsText(`polytool-rules/${this.fileName}`, rrrr);
+    const json = JSON.stringify(saveable, undefined, 2);
+    _package.files.writeAsText(`polytool-rules/${this.fileName}`, json);
     grok.shell.info(`Polytool rules at ${this.fileName} was updated`);
   }
 
-  private createGridDiv(name: string, grid: DG.Grid) {
+  private createGridDiv(name: string, grid: DG.Grid, buttons: HTMLButtonElement[] = []) {
     const header = ui.h1(name, 'polytool-grid-header');
-    grid.root.prepend(header);
+    header.style.marginTop = '10px';
+    header.style.marginRight = '10px';
     grid.root.style.height = '100%';
-    return grid.root;
+
+    buttons.forEach((b) => {
+      b.style.marginLeft = '5px';
+      b.style.marginRight = '10px';
+    });
+
+    const gridDiv = ui.splitV([
+      ui.box(
+        ui.divH([header, ...buttons]),
+        {style: {maxHeight: '60px'}},
+      ),
+      grid.root,
+    ]);
+
+    gridDiv.style.height = '100%';
+
+    return gridDiv;
   };
 
   async getLinkExamplesGrid() {
@@ -160,69 +177,48 @@ export class RulesManager {
   async getForm() {
     inputsTabControl: DG.TabControl;
 
+    const saveLinksButton = ui.bigButton('Save', () => { this.save(); });
+    const addLinkButton = ui.button('Add rule', () => { this.linkRuleDataFrame.rows.addNew(); });
+    const linksGridDiv = this.createGridDiv('Rules',
+      this.linkRuleDataFrame.plot.grid({showAddNewRowIcon: true}), [saveLinksButton, addLinkButton]);
+    const linkExamples = this.createGridDiv('Examples', await this.getLinkExamplesGrid());
+    linksGridDiv.style.width = '50%';
+    linkExamples.style.width = '50%';
+    const links = ui.divH([linksGridDiv, linkExamples]);
+
+    const saveReactionsButton = ui.bigButton('Save', () => { this.save(); });
+    const addReactionButton = ui.button('Add rule', () => { this.synthRuleDataFrame.rows.addNew(); });
+    const reactionsGridDiv = this.createGridDiv('Rules',
+      this.synthRuleDataFrame.plot.grid({showAddNewRowIcon: true}), [saveReactionsButton, addReactionButton]);
+    const reactionExamples = this.createGridDiv('Examples', await this.getReactionExamplesGrid());
+    reactionsGridDiv.style.width = '50%';
+    reactionExamples.style.width = '50%';
+    const reactions = ui.divH([reactionsGridDiv, reactionExamples]);
+
     const dimerInputsDiv = ui.divV([
       this.homoDimerInput,
       this.heteroDimerInput,
     ]);
 
-    const linkExamples = await this.getLinkExamplesGrid();
-    const reactionExamples = await this.getReactionExamplesGrid();
-
     const inputsTabControl = ui.tabControl({
-      'Links': linkExamples,
-      'Reactions': reactionExamples,
+      'Links': links,
+      'Reactions': reactions,
       'Dimers': dimerInputsDiv,
     }, false);
 
-    inputsTabControl.root.style.height = '90%';
+    inputsTabControl.root.style.height = '100%';
     inputsTabControl.root.style.width = '100%';
     inputsTabControl.root.classList.add('rules-manager-form-tab-control');
     inputsTabControl.header.style.marginBottom = '10px';
 
-    const linksGridDiv = this.createGridDiv('Link rules', this.linkRuleDataFrame.plot.grid({showAddNewRowIcon: true}));
-    const reactionsGridDiv =
-      this.createGridDiv('Reaction rules', this.synthRuleDataFrame.plot.grid({showAddNewRowIcon: true}));
-
-    linksGridDiv.style.width = '100%';
-    reactionsGridDiv.style.width = '100%';
-
-    const divs = [linksGridDiv, reactionsGridDiv, ui.div()];
-    divs[0].style.removeProperty('display');
-    divs[1].style.display = 'none';
-    divs[2].style.display = 'none';
-
-    inputsTabControl.onTabChanged.subscribe(() => {
-      this.currentTab = inputsTabControl.currentPane.name;
-
-      const idx = inputsTabControl.panes.findIndex((p) => p.name == this.currentTab);
-
-      for (let i = 0; i < divs.length; i++) {
-        if (i == idx)
-          divs[i].style.removeProperty('display');
-        else
-          divs[i].style.display = 'none';
-      }
-    });
-
-    const saveButton = ui.bigButton('Save changes', () => {
-      this.save();
-    });
-
-
     const panel = ui.divV([
       inputsTabControl.root,
-      saveButton
     ]);
 
     panel.style.height = '100%';
     panel.style.alignItems = 'center';
 
-    const form = ui.splitH([
-      panel,
-      ui.divV(divs, {style: {width: '100%'}})
-    ], {style: {width: '100%'}}, true);
-    form.style.height = '100%';
-    return form;
+    return inputsTabControl.root;
   }
 }
 
