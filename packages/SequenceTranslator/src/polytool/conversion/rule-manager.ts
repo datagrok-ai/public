99bef17765d5b4bbb84292abcd654d0076723fe4
StLeonid <stolbovla@yandex.ru>
2024-11-22 22:47:19 +0300

    ST: Polytool rule manager names

--- packages/SequenceTranslator/src/polytool/conversion/rule-manager.ts
+++ packages/SequenceTranslator/src/polytool/conversion/rule-manager.ts
@@ -6,6 +6,11 @@ import {getHelmHelper} from '@datagrok-libraries/bio/src/helm/helm-helper';
 import {doPolyToolConvert} from './pt-conversion';
 import {NOTATION} from '@datagrok-libraries/bio/src/utils/macromolecule/consts';
 
+
+const TAB_LINKS = 'Links';
+const TAB_REACTIONS = 'Reactions';
+const TAB_DIMERS = 'Dimers';
+
 export class RulesManager {
   rules: Rules;
   linkRuleDataFrame: DG.DataFrame;
@@ -15,8 +20,6 @@ export class RulesManager {
 
   homoDimerInput: DG.InputBase;
   heteroDimerInput: DG.InputBase;
-
-  currentTab = '';
 
   // every rule set will have its editor instance
   private static instances: Record<string, RulesManager> = {};
@@ -92,20 +95,15 @@ export class RulesManager {
     grok.shell.info(`Polytool rules at ${this.fileName} was updated`);
   }
 
-  private createGridDiv(name: string, grid: DG.Grid, buttons: HTMLButtonElement[] = []) {
+  private createGridDiv(name: string, grid: DG.Grid) {
     const header = ui.h1(name, 'polytool-grid-header');
     header.style.marginTop = '10px';
     header.style.marginRight = '10px';
     grid.root.style.height = '100%';
 
-    buttons.forEach((b) => {
-      b.style.marginLeft = '5px';
-      b.style.marginRight = '10px';
-    });
-
     const gridDiv = ui.splitV([
       ui.box(
-        ui.divH([header, ...buttons]),
+        header,
         {style: {maxHeight: '60px'}},
       ),
       grid.root,
@@ -131,8 +129,8 @@ export class RulesManager {
 
     const helms = doPolyToolConvert(seqs, this.rules, helmHelper);
 
-    const initCol = DG.Column.fromStrings('monomers', seqs);
-    const helmCol = DG.Column.fromStrings('helm', helms);
+    const initCol = DG.Column.fromStrings('Monomers', seqs);
+    const helmCol = DG.Column.fromStrings('Helm', helms);
 
     applyNotationProviderForCyclized(initCol, '-');
     initCol.semType = DG.SEMTYPE.MACROMOLECULE;
@@ -160,8 +158,8 @@ export class RulesManager {
 
     const helms = doPolyToolConvert(seqs, this.rules, helmHelper);
 
-    const initCol = DG.Column.fromStrings('monomers', seqs);
-    const helmCol = DG.Column.fromStrings('helm', helms);
+    const initCol = DG.Column.fromStrings('Monomers', seqs);
+    const helmCol = DG.Column.fromStrings('Helm', helms);
 
     initCol.semType = DG.SEMTYPE.MACROMOLECULE;
     applyNotationProviderForCyclized(initCol, '-');
@@ -177,19 +175,15 @@ export class RulesManager {
   async getForm() {
     inputsTabControl: DG.TabControl;
 
-    const saveLinksButton = ui.bigButton('Save', () => { this.save(); });
-    const addLinkButton = ui.button('Add rule', () => { this.linkRuleDataFrame.rows.addNew(); });
     const linksGridDiv = this.createGridDiv('Rules',
-      this.linkRuleDataFrame.plot.grid({showAddNewRowIcon: true}), [saveLinksButton, addLinkButton]);
+      this.linkRuleDataFrame.plot.grid({showAddNewRowIcon: true}));
     const linkExamples = this.createGridDiv('Examples', await this.getLinkExamplesGrid());
     linksGridDiv.style.width = '50%';
     linkExamples.style.width = '50%';
     const links = ui.divH([linksGridDiv, linkExamples]);
 
-    const saveReactionsButton = ui.bigButton('Save', () => { this.save(); });
-    const addReactionButton = ui.button('Add rule', () => { this.synthRuleDataFrame.rows.addNew(); });
     const reactionsGridDiv = this.createGridDiv('Rules',
-      this.synthRuleDataFrame.plot.grid({showAddNewRowIcon: true}), [saveReactionsButton, addReactionButton]);
+      this.synthRuleDataFrame.plot.grid({showAddNewRowIcon: true}));
     const reactionExamples = this.createGridDiv('Examples', await this.getReactionExamplesGrid());
     reactionsGridDiv.style.width = '50%';
     reactionExamples.style.width = '50%';
@@ -214,6 +208,17 @@ export class RulesManager {
     const panel = ui.divV([
       inputsTabControl.root,
     ]);
+
+    const saveButton = ui.bigButton('Save', () => { this.save(); });
+    const addButton = ui.button('Add rule', () => {
+      const currentTab = inputsTabControl.currentPane.name;
+      if (currentTab == TAB_LINKS)
+        this.linkRuleDataFrame.rows.addNew();
+      else if (currentTab == TAB_REACTIONS)
+        this.synthRuleDataFrame.rows.addNew();
+    });
+    const topPanel = [saveButton, addButton];
+    this.v!.setRibbonPanels([topPanel]);
 
     panel.style.height = '100%';
     panel.style.alignItems = 'center';
