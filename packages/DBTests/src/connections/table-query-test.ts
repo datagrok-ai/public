642ea7499d5d1157831c14ecbcc07417036a518d
PavloPolovyi <pasha32662@gmail.com>
2024-11-26 18:10:35 +0100

    DbTests: TableQueryBuilder: Use connection method

--- packages/DBTests/src/connections/table-query-test.ts
+++ packages/DBTests/src/connections/table-query-test.ts
@@ -11,52 +11,47 @@ category('TableQueryBuilder', () => {
     });
 
     test('no fields', async () => {
-        const tq = DG.TableQueryBuilder.from(testTableName).build();
-        tq.connection = northwind;
+        const tq = northwind.buildQuery(testTableName).build();
         const df: DG.DataFrame = await tq.executeTable();
         expect(df.rowCount, 77);
         expect(df.columns.length, 10);
     });
 
     test('fields limit', async () => {
-        const tq = DG.TableQueryBuilder.from(testTableName)
+        const tq = northwind.buildQuery(testTableName)
             .select(['productname', 'unitprice'])
             .limit(10)
             .build();
-        tq.connection = northwind;
         const df: DG.DataFrame = await tq.executeTable();
         expect(df.rowCount, 10);
         expect(df.columns.length, 2);
     });
 
     test('fields where double pattern limit', async () => {
-        const tq = DG.TableQueryBuilder.from(testTableName)
+        const tq = northwind.buildQuery(testTableName)
             .select(['productname', 'unitprice'])
             .where('unitprice', '<=13',  DG.TYPE.FLOAT)
             .limit(3)
             .build();
-        tq.connection = northwind;
         const df: DG.DataFrame = await tq.executeTable();
         expect(df.rowCount, 3);
         expect(df.columns.length, 2);
     });
 
     test('fields where datetime pattern', async () => {
-        const tq = DG.TableQueryBuilder.from('orders')
+        const tq = northwind.buildQuery('orders')
             .select(['shipcity', 'shipcountry', 'orderdate'])
             .where('orderdate', 'before 1996-08-26',  DG.TYPE.DATE_TIME)
             .build();
-        tq.connection = northwind;
         const df: DG.DataFrame = await tq.executeTable();
         expect(df.rowCount, 42);
         expect(df.columns.length, 3);
     });
 
     test('fields where string pattern', async () => {
-        const tq = DG.TableQueryBuilder.from('customers')
+        const tq = northwind.buildQuery('customers')
             .where('region', 'is not null')
             .build();
-        tq.connection = northwind;
         const df: DG.DataFrame = await tq.executeTable();
         expect(df.rowCount, 31);
         expect(df.columns.length, 11);
@@ -64,12 +59,11 @@ category('TableQueryBuilder', () => {
 
     test('aggregation max sort', async () => {
         // translates to select * from products
-        const tq = DG.TableQueryBuilder.from(testTableName)
+        const tq = northwind.buildQuery(testTableName)
             .max('unitprice', 'max price')
             .groupBy(['categoryid', 'unitprice'])
             .sortBy('unitprice', false)
             .build();
-        tq.connection = northwind;
         const df: DG.DataFrame = await tq.executeTable();
         expect(df.rowCount, 73);
         expect(df.columns.length, 3);
@@ -78,13 +72,12 @@ category('TableQueryBuilder', () => {
 
     test('aggregation max sort left join', async () => {
         // translates to select * from products
-        const tq = DG.TableQueryBuilder.from(testTableName)
+        const tq = northwind.buildQuery(testTableName)
             .leftJoin('categories', ['categoryid'], ['categoryid'])
             .max('unitprice', 'max price')
             .groupBy(['categoryname', 'unitprice'])
             .sortBy('unitprice', false)
             .build();
-        tq.connection = northwind;
         const df: DG.DataFrame = await tq.executeTable();
         expect(df.rowCount, 73);
         expect(df.columns.length, 3);
@@ -93,14 +86,13 @@ category('TableQueryBuilder', () => {
 
     test('aggregation max sort left join having', async () => {
         // translates to select * from products
-        const tq = DG.TableQueryBuilder.from(testTableName)
+        const tq = northwind.buildQuery(testTableName)
             .leftJoin('categories', ['categoryid'], ['categoryid'])
             .max('unitprice', 'max price')
             .groupBy(['categoryname', 'unitprice'])
             .having('unitprice', '>100', DG.TYPE.FLOAT)
             .sortBy('unitprice', false)
             .build();
-        tq.connection = northwind;
         const df: DG.DataFrame = await tq.executeTable();
         expect(df.rowCount, 2);
         expect(df.columns.length, 3);
@@ -109,14 +101,13 @@ category('TableQueryBuilder', () => {
 
     test('aggregation max left inner joins having', async () => {
         // translates to select * from products
-        const tq = DG.TableQueryBuilder.from(testTableName)
+        const tq = northwind.buildQuery(testTableName)
             .leftJoin('categories', ['categoryid'], ['categoryid'])
             .innerJoin('suppliers', ['supplierid'], ['supplierid'])
             .valueCount('categoryname', 'categories by supplier')
             .groupBy(['companyname', 'products.supplierid', 'country'])
             .having('country', 'in (USA, UK)')
             .build();
-        tq.connection = northwind;
         const df: DG.DataFrame = await tq.executeTable();
         expect(df.rowCount, 6);
         expect(df.columns.length, 4);
