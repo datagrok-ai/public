51ba74dc664e6ec38dd74a127c44c07b7681ce1d
PavloPolovyi <pasha32662@gmail.com>
2024-11-28 11:29:42 +0100

    ApiSamples: DbTableQueryBuilder: Adjusted samples

--- packages/ApiSamples/scripts/data-access/query_builder_simple.js
+++ packages/ApiSamples/scripts/data-access/query_builder_simple.js
@@ -1,11 +1,11 @@
 //tags: TableQueryBuilder, TableQuery
 
 grok.data.db.buildQuery('Samples:PostgresNorthwind', 'orders')
-    .then((builder) => builder
-        .select(['shipcity', 'shipcountry', 'orderdate']) // TableQueryBuilder
-        .where('orderdate', 'before 1996-08-26', DG.TYPE.DATE_TIME) // TableQueryBuilder
-        .sortBy('orderdate', false) // TableQueryBuilder
-        .limit(10) // TableQueryBuilder
-        .build() // TableQuery
-        .executeTable()) // DataFrame
-    .then((df) => grok.shell.addTable(df));
+        .select(['shipcity', 'shipcountry', 'orderdate'])
+        .where('orderdate', 'before 1996-08-26', DG.TYPE.DATE_TIME)
+        .sortBy('orderdate', false)
+        .limit(10)
+        .build()
+        .executeTable()
+    .then((df) => grok.shell.addTable(df))
+    .catch((e) => grok.shell.error(e));
