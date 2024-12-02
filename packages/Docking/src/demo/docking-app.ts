73c09fd277592d96e97c18a4be4ff2afe8f762e1
Jenkins Pipeline <jenkins@datagrok.ai>
2024-12-02 11:30:58 +0100

    Jenkins: Checkout from master: packages, libraries

--- packages/Docking/src/demo/docking-app.ts
+++ packages/Docking/src/demo/docking-app.ts
@@ -2,19 +2,17 @@ import * as grok from 'datagrok-api/grok';
 import * as DG from 'datagrok-api/dg';
 import {BaseViewApp} from '@datagrok-libraries/tutorials/src/demo-base-view';
 import { getAutoDockService, getAutodockSingle, prepareAutoDockData, runDocking } from '../package';
-import { CACHED_DOCKING } from '../utils/constants';
 import { prepareDockingData } from './demo-docking';
 import { IAutoDockService } from '@datagrok-libraries/bio/src/pdb/auto-dock-service';
-import { AutoDockService } from '../utils/auto-dock-service';
 
 export class DockingViewApp extends BaseViewApp {
   constructor(parentCall: DG.FuncCall) {
     super(parentCall);
 
     this.formGenerator = this.generateCustomForm;
-    this.filePath = 'System:AppData/Docking/demo_files/demo_dataset_small.csv';
     this.uploadCachedData = this.loadCachedAutodockData.bind(this);
     this.abort = this.terminateProcess;
+    this.sketcherValue = {'(5~{S})-5-(1~{H}-indol-2-yl)pyrrolidin-2-one': 'c1ccc2c(c1)cc([nH]2)[C@@H]3CCC(=O)N3'};
     this.addTabControl = false;
   }
 
@@ -22,7 +20,7 @@ export class DockingViewApp extends BaseViewApp {
     const smilesCell = this.tableView!.grid.cell('smiles', 0);
     const semValue = DG.SemanticValue.fromGridCell(smilesCell);
 
-    const molstarWidget = await runDocking(semValue, 'BACE1', 10);
+    const molstarWidget = await runDocking(semValue, 'kras', 10);
     this.applyFullSizeStyles(molstarWidget?.root?.firstElementChild as HTMLElement);
 
     if (molstarWidget)
@@ -44,7 +42,7 @@ export class DockingViewApp extends BaseViewApp {
     this.tableView!.grid.invalidate();
 
     const resultsPath = 'System:AppData/Docking/demo_files/demo_app_results.csv';
-    await prepareDockingData(sampleData, resultsPath, 'BACE1', 'smiles', 10);
+    await prepareDockingData(sampleData, resultsPath, 'kras', 'pdb', 10);
 
     const dockingWidget = await this.createDockingWidget(sampleData.cell(0, 'pose'), sampleData);
     return dockingWidget!;
