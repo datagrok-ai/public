95f15a7b239931c1989366c0f3a68e53d181b1ce
Konstantin Amelichev <kostya.amelichev@gmail.com>
2024-12-02 15:29:21 +0100

    UsageAnalysis: Filter out stress tests from the test dashboard

--- packages/UsageAnalysis/queries/tests_dashboards.sql
+++ packages/UsageAnalysis/queries/tests_dashboards.sql
@@ -2,8 +2,11 @@
 --name: Tests
 --friendlyName: UA | Tests | Tests
 --connection: System:Datagrok
+--input: bool showNotRun = false
 WITH last_builds AS (
-    select name  from builds order by build_date desc limit 5
+    select name  from builds b
+    where EXISTS((SELECT 1 from test_runs r where r.build_name = b.name and not r.stress_test))
+    order by b.build_date desc limit 5
 )
 select
   b.name as build,
@@ -13,14 +16,12 @@ select
   case when r.passed is null then 'did not run' when r.skipped then 'skipped' when r.passed then 'passed' when not r.passed then 'failed' else 'unknown' end as status,
   r.result,
   r.duration,
-  r.benchmark,
   t.owner
 from tests t full join last_builds b on 1=1
 left join test_runs r on r.test_name = t.name and r.build_name = b.name and (t.first_run <= r.date_time or t.first_run is null) and
 r.date_time = (select max(_r.date_time) from test_runs _r where _r.test_name = r.test_name and _r.build_name = r.build_name and not _r.stress_test) and not r.stress_test
-where   t.name not like '%Unhandled exceptions: Exception' and (not t.name = ': : ') and t.name not like 'Unknown:%'
+where   t.name not like '%Unhandled exceptions: Exception' and (not t.name = ': : ') and t.name not like 'Unknown:%' and (@showNotRun or not r.passed is null) and r.benchmark = false
 order by b.name, t.name
-
 --end
 
 --name: Benchmarks
