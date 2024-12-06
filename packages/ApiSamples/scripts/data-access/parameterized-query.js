b68be682f2eda8cf556783f1888390ee684e26dd
Stanislav Savenko <ss.savenko@gmail.com>
2024-12-06 18:35:54 +0200

    ApiSamples: Samples: Minor fixes

--- packages/ApiSamples/scripts/data-access/parameterized-query.js
+++ packages/ApiSamples/scripts/data-access/parameterized-query.js
@@ -1,7 +1,8 @@
+
 //tags: DataQuery
 //help-url: https://datagrok.ai/help/access/databases#parameterized-queries
 // An example of using parameterized query
 
 let t = await grok.data
-  .query("DBTests:PostgresqlByDouble", { freight: 100.1 })
+  .query("Samples:MSSQLByInt", { orderid: 10 })
 grok.shell.info("Number of orders: " + t.rowCount);
