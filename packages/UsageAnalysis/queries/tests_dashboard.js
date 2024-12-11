528b7880fbc48a7981845b5215570dba0766ef75
Konstantin Amelichev <kostya.amelichev@gmail.com>
2024-12-11 21:59:40 +0100

    UsageAnalysis: Improve tests dashboard
    * Layout with relevant filters and basic aggregation through barchart
    * Build numbers have consistent order and names from run to run
    * Added test owners

--- packages/UsageAnalysis/queries/tests_dashboard.js
+++ packages/UsageAnalysis/queries/tests_dashboard.js
@@ -2,11 +2,12 @@
 //input: dataframe result
 //output: dataframe out  
 
-let builds = result.col('build').categories;
+let builds = result.col('build_index').categories;
 let pivot = result
-  .groupBy(['test'])
-  .pivot('build')
+  .groupBy(['test', 'owner'])
+  .pivot('build_index')
   .avg('duration')
+  .avg('build_date')
   .add(DG.STR_AGG.CONCAT_UNIQUE, 'status')
   .add(DG.STR_AGG.CONCAT_UNIQUE, 'result')
   .aggregate(); 
@@ -14,7 +15,7 @@ let pivot = result
 function generateCommonValueFormula(operation, value) {
   var result = '';
   if (builds.length == 1)
-    return builds[0] + value;
+    return '${' + builds[0] + ' concat unique(status)}' + value;
   for (var i = 0; i + 2 < builds.length; i++)
     result += operation + '(${' + builds[i] + ' concat unique(status)}' + value + ',';
   result += operation + '(${' + builds[builds.length - 2] + ' concat unique(status)}' + value + ',${' +  builds[builds.length - 2] + ' concat unique(status)}' + value + ')';
