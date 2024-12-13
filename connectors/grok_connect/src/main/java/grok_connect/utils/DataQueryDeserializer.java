6ee78625c8a551eb69031d1f915df2b3b180de94
PavloPolovyi <pasha32662@gmail.com>
2024-12-13 10:21:47 +0100

    GrokConnect: Fixes deserialization issue

--- connectors/grok_connect/src/main/java/grok_connect/utils/DataQueryDeserializer.java
+++ connectors/grok_connect/src/main/java/grok_connect/utils/DataQueryDeserializer.java
@@ -1,20 +1,19 @@
 package grok_connect.utils;
 
-import com.google.gson.JsonDeserializationContext;
-import com.google.gson.JsonDeserializer;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonParseException;
+import com.google.gson.*;
 import grok_connect.connectors_info.DataQuery;
 import grok_connect.table_query.TableQuery;
 import java.lang.reflect.Type;
 
 public class DataQueryDeserializer implements JsonDeserializer<DataQuery> {
+    private static final Gson gson = new Gson();
+
     @Override
     public DataQuery deserialize(JsonElement jsonElement, Type type, JsonDeserializationContext jsonDeserializationContext) throws JsonParseException {
         String myType = jsonElement.getAsJsonObject().get("#type").getAsString();
         switch (myType) {
             case "TableQuery": return jsonDeserializationContext.deserialize(jsonElement, TableQuery.class);
-            case "DataQuery": return jsonDeserializationContext.deserialize(jsonElement, DataQuery.class);
+            case "DataQuery": return gson.fromJson(jsonElement, DataQuery.class);
             default: return null;
         }
     }
