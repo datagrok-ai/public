7f961f76a4b19d69c984373717e1cb254c222acd
Aufar Zakiev <zaufar@gmail.com>
2024-12-18 16:07:09 +0300

    Compute2: Redesigned PipelineView cards

--- packages/Compute2/src/components/PipelineView/PipelineView.tsx
+++ packages/Compute2/src/components/PipelineView/PipelineView.tsx
@@ -2,11 +2,12 @@ import * as grok from 'datagrok-api/grok';
 import * as ui from 'datagrok-api/ui';
 import * as DG from 'datagrok-api/dg';
 import * as Vue from 'vue';
-import {DockManager, FunctionCard, IconFA, MarkDown, RibbonMenu, tooltip} from '@datagrok-libraries/webcomponents-vue';
+import {DockManager, FunctionCard, Icon, IconFA, MarkDown, RibbonMenu, tooltip} from '@datagrok-libraries/webcomponents-vue';
 import * as Utils from '@datagrok-libraries/compute-utils/shared-utils/utils';
 import {History} from '../History/History';
 import {hasAddControls, PipelineWithAdd} from '../../utils';
 import {isParallelPipelineState, isSequentialPipelineState, PipelineState, ViewAction} from '@datagrok-libraries/compute-utils/reactive-tree-driver/src/config/PipelineInstance';
+import { computedAsync } from '@vueuse/core';
 
 
 export const PipelineView = Vue.defineComponent({
@@ -52,16 +53,13 @@ export const PipelineView = Vue.defineComponent({
     const buttonActions = Vue.computed(() => props.buttonActions);
     const menuIconStyle = {width: '15px', display: 'inline-block', textAlign: 'center'};
 
-    const helpText = Vue.ref(null as null | string);
-    Vue.watch(() => props.funcCall, async (funcCall) => {
-      if (!funcCall) {
-        helpText.value = null;
-        return;
-      }
-      const loadedHelp = await Utils.getContextHelp(funcCall.func);
+    const hoveredFunc = Vue.shallowRef(null as DG.Func | null)
 
-      helpText.value = loadedHelp ?? null;
-    }, {immediate: true});
+    const helpText = computedAsync(async () => {
+      if (!hoveredFunc.value) return null;
+
+      return Utils.getContextHelp(hoveredFunc.value);
+    }, null)
 
     const handlePanelClose = async (el: HTMLElement) => {
       if (el === historyRef.value?.$el) historyHidden.value = true;
@@ -94,13 +92,6 @@ export const PipelineView = Vue.defineComponent({
               ref={historyRef}
               class='overflow-scroll h-full'
             /> }
-          { (!helpHidden.value && helpText.value) &&
-            <MarkDown
-              markdown={helpText.value}
-              dock-spawn-title='Help'
-              dock-spawn-dock-type='fill'
-              ref={helpRef}
-            /> }
           { hasAddControls(state.value) && !functionsHidden.value && <div 
             class={'grok-gallery-grid'} 
             dock-spawn-title='Steps to add'
@@ -108,36 +99,54 @@ export const PipelineView = Vue.defineComponent({
             ref={functionsRef}
           >
             { state.value.stepTypes
-              .map((stepType, idx) => stepType.nqName ? <FunctionCard 
-                func={DG.Func.byName(stepType.nqName)}
-                onClick={() => {
-                  const data = state.value as PipelineWithAdd;
-                  emit('addNode', {
-                    itemId: data.stepTypes[idx].configId,
-                    position: data.steps.length,
-                  });
-                }}
-              /> : <div
-              onClick={() => {
-                const data = state.value as PipelineWithAdd;
-                emit('addNode', {
-                  itemId: data.stepTypes[idx].configId,
-                  position: data.steps.length,
-                });
-              }}
-              class={'grok-gallery-grid-item-wrapper'}
-              style={{cursor: 'pointer'}}
-            >
-              <div class={'grok-gallery-grid-item grok-scripting-script d4-flex-col d4-gallery-card entity-script'}>
-                <div class={'d4-flex-col'}>
-                  <span class={'d4-link-label'}>
-                    <label class={'grok-gallery-grid-item-title'}>
-                      {stepType.friendlyName ?? stepType.configId}
-                    </label>
-                  </span>
-                </div>
-              </div>
-            </div>)
+              .map((stepType, idx) => {
+                const func = stepType.nqName ? DG.Func.byName(stepType.nqName): null;
+                const language = func instanceof DG.Script ? func.language: 'javascript';
+                const iconBackground = `background-image: url("/images/entities/${language}.png"); padding-right: 3px;`;
+                
+                return <div 
+                  class='p-2 border-solid border-1 border-[#dbdcdf] m-4' 
+                  style={{width: '208px'}}
+                  onMouseover={() => hoveredFunc.value = func}
+                  onMouseleave={() => hoveredFunc.value = null}
+                >
+                  <div class='flex flex-col'>
+                    <div class='flex flex-row justify-between'>
+                      <div class='flex flex-row items-center'>
+                        { func ? 
+                          <i style={iconBackground} class={'grok-icon image-icon'}></i>:
+                          <IconFA style={{'padding-right': '3px'}} name='folder-tree' />
+                        }
+                        <div> 
+                          <span class='text-[#2083d5]'> {stepType.friendlyName ?? stepType.configId} </span>
+                        </div>
+                      </div>
+                      <div class='flex flex-row'>
+                        { func?.options.help && <IconFA 
+                          class='d4-ribbon-item'
+                          name='question-circle' 
+                          tooltip={helpText.value}
+                          style={{'padding-left': '5px'}}
+                          /> 
+                        }
+                        <IconFA 
+                          class='d4-ribbon-item'
+                          name='plus' 
+                          tooltip='Add step to the tree'
+                          onClick={() => {
+                            const data = state.value as PipelineWithAdd;
+                            emit('addNode', {
+                              itemId: data.stepTypes[idx].configId,
+                              position: data.steps.length,
+                            });
+                          }}
+                          style={{'padding-left': '5px'}}
+                        />
+                      </div>
+                    </div>
+                    { func && <label class='description'> {func.description} </label> }
+                  </div>
+                </div>})
             }
             </div>
           }
@@ -175,14 +184,6 @@ export const PipelineView = Vue.defineComponent({
                   >
                     <IconFA name='history' class={'d4-picture'} />
                     <div> Load completed run </div>
-                  </div> }
-                { helpText.value &&
-                  <div
-                    class={cardsClasses}
-                    onClick={() => helpHidden.value = false}
-                  >
-                    <IconFA name='info' class={'d4-picture'} />
-                    <div> Review the docs </div>
                   </div> }
                 { buttonActions.value?.map((action) => Vue.withDirectives(
                   <div
