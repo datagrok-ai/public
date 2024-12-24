aef1e86419388b873bbac1ec58764e0025bb54f5
Andrey Sharapov <andrey.sharapov@softwarecountry.com>
2024-12-24 15:21:03 +0300

    WebComponents: New version

--- packages/WebComponents/src/package.ts
+++ packages/WebComponents/src/package.ts
@@ -3,10 +3,9 @@ import * as grok from 'datagrok-api/grok';
 import * as ui from 'datagrok-api/ui';
 import * as DG from 'datagrok-api/dg';
 import {
-  InputForm, Viewer, DGBigButton, 
-  DGButton, DGIconFA, 
+  InputForm, Viewer, DGBigButton,
+  DGButton, DGIconFA,
   DGToggleInput, DGComboPopup, DGMarkdown,
-  DGIcon
 } from '@datagrok-libraries/webcomponents';
 import {DockSpawnTsWebcomponent} from '@datagrok-libraries/dock-spawn-dg/lib'
 
@@ -23,7 +22,6 @@ export function init() {
   customElements.define('dg-button', DGButton, {extends: 'button'});
   customElements.define('dg-big-button', DGBigButton, {extends: 'button'});
   customElements.define('dg-icon-fa', DGIconFA);
-  customElements.define('dg-icon', DGIcon);
   customElements.define('dg-toggle-input', DGToggleInput);
   customElements.define('dg-combo-popup', DGComboPopup);
   customElements.define('dock-spawn-ts', DockSpawnTsWebcomponent);
