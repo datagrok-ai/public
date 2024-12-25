0faa5c148d04bb24d378d5574b19a8d052d41b6a
Stanislav Savenko <ss.savenko@gmail.com>
2024-12-25 11:50:27 +0200

    GrokTools: load: minor fixes

--- tools/bin/commands/load.ts
+++ tools/bin/commands/load.ts
@@ -1,9 +1,19 @@
-import { runWorker } from "../utils/test-utils";
-import * as utils from '../utils/utils';
+
 import fs from 'fs';
-import path from 'path';
 import os from 'os';
+import path from 'path'; 
 import yaml from 'js-yaml';
+import * as utils from '../utils/utils';
+import * as color from '../utils/color-utils';
+import * as Papa from 'papaparse';
+import * as testUtils from '../utils/test-utils';
+import { getBrowserPage, OrganizedTests as OrganizedTest, timeout, defaultLaunchParameters } from '../utils/test-utils';
+import { setAlphabeticalOrder } from '../utils/order-functions'; 
+import { PuppeteerNode } from 'puppeteer';
+import { PuppeteerScreenRecorder } from 'puppeteer-screen-recorder';
+import { spaceToCamelCase } from '../utils/utils';
+import puppeteer from 'puppeteer';
+import { Browser, Page } from 'puppeteer';
 
 const grokDir = path.join(os.homedir(), '.grok');
 const confPath = path.join(grokDir, 'config.yaml');
@@ -11,17 +21,21 @@ const confPath = path.join(grokDir, 'config.yaml');
 export  async function loadNPM(args: LoadNPMArgs) {
     const config = yaml.load(fs.readFileSync(confPath, { encoding: 'utf-8' })) as utils.Config;
     utils.setHost(args.host, config);
-    await runWorker([{
-        package: 'Loading Npm Packages',
-        params: {
-            test: '',
-            category: '',
-            options: {
-                catchUnhandled: undefined,
-                report: undefined
-            }
-        }
-    }], {}, 0);
+    const params = Object.assign({}, defaultLaunchParameters);
+    const out = await getBrowserPage(puppeteer, params);
+    const browser: Browser = out.browser;
+    const page: Page = out.page; 
+
+    let testingResults = await page.evaluate((): Promise<void> => { 
+      return new Promise<any>((resolve, reject) => {
+        (<any>window).grok.functions.call("triggerNpmInstall:test()", {}).then((results: any) => {
+            resolve(results);
+          })
+          .catch((e:any)=>{
+            resolve(e);
+          })
+      })
+    });
     process.exit(0);
 }
 
