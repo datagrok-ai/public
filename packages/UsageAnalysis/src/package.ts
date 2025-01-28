53d40fe07571b3d345062e834bc08dff5773097f
Konstantin Amelichev <kostya.amelichev@gmail.com>
2025-01-28 23:29:52 +0100

    GROK-17370: UsageAnalysis: Tests Dashboard: Remove temporary stacktrace handler
    * We now reuse one from the UserReports in core

--- packages/UsageAnalysis/src/package.ts
+++ packages/UsageAnalysis/src/package.ts
@@ -15,7 +15,6 @@ import { getDate } from './utils';
 import dayjs from "dayjs";
 import {ServiceLogsApp} from "./service_logs/service_logs";
 import { TestGridCellHandler } from './handlers/test-grid-cell-handler';
-import { StackTraceHandler } from './handlers/stack-trace-handler';
 import { initTestStickyMeta } from './test-analysis/sticky-meta-initialization';
 import { TestDashboardWidget } from './viewers/ua-test-dashboard-viewer';
 
@@ -27,7 +26,6 @@ export let _properties: any;
 export function _initUA(): void {
   _properties = _package.settings;
   DG.ObjectHandler.register(new TestGridCellHandler());
-  DG.ObjectHandler.register(new StackTraceHandler());
   initTestStickyMeta();
 }
 
