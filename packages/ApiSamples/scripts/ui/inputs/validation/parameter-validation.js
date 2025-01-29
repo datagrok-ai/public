e8549e3334675de7c28699c8ddf17c627c117ecf
Stanislav Savenko <ss.savenko@gmail.com>
2025-01-29 18:45:55 +0200

    ApiSamples: Tests: Updated samples test mechanism

--- packages/ApiSamples/scripts/ui/inputs/validation/parameter-validation.js
+++ packages/ApiSamples/scripts/ui/inputs/validation/parameter-validation.js
@@ -1,6 +1,6 @@
 //name: parameter-validation
 //language: javascript
 //input: int foo = 5 { validator: bar > 3 }
-//input: double bar = 2 { min: 0; max: 10 }
+//input: double bar = 4 { min: 0; max: 10 }
 
 grok.shell.info('foo: ' + foo ?? '' + '\n' + 'bar: ' + bar ?? '');
