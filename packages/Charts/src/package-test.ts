ce54d493b4b68b0578b2ce0d3fb6bfa3673fed44
Oleksandra Serhiienko <aleksandrasergienko12@gmail.com>
2025-02-04 10:47:56 +0200

    Charts: Run later

--- packages/Charts/src/package-test.ts
+++ packages/Charts/src/package-test.ts
@@ -2,10 +2,10 @@ import * as DG from 'datagrok-api/dg';
 
 import {runTests, tests, TestContext, initAutoTests as initTests } from '@datagrok-libraries/utils/src/test';
 
+import './tests/viewers';
 import './tests/timelines-viewer-test';
 import './tests/radar-viewer-test';
 import './tests/tree-viewer-test';
-import './tests/viewers';
 
 export const _package = new DG.Package();
 export {tests};
