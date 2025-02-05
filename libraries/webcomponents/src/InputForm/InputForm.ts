d610724d83bed16582fedad691446f60eafdc268
Andrey Sharapov <andrey.sharapov@softwarecountry.com>
2025-02-05 11:39:15 +0300

    Webcomponents: InputForm changes

--- libraries/webcomponents/src/InputForm/InputForm.ts
+++ libraries/webcomponents/src/InputForm/InputForm.ts
@@ -21,46 +21,17 @@ export class InputForm extends HTMLElement {
   }
 
   set funcCall(fc: DG.FuncCall | undefined) {
-    if (!fc) {
-      ui.empty(this);
-      return;
-    }
-
-    if (!this.funcCall) {
-      this.replaceFunc(fc);
-      return;
-    }
-
-    // if (this.funcCall.func.id === fc.func.id) {
-    //   if (this.funcCall.id !== fc.id) this.formInst!.source = this.funcCall;
-    // } else
-      this.replaceFunc(fc);
-  }
-
-  private async replaceFunc(funcCall: DG.FuncCall) {
-    const fcInputs = [...funcCall.inputs.entries()] as [string, any][];
-    this.formInst = await DG.InputForm.forFuncCall(funcCall, {twoWayBinding: false});
-    this.attach();
-
-    setTimeout(() => {
-      fcInputs.forEach(([paramName, paramValue]) => {
-        const input = this.formInst!.getInput(paramName);
-        input.notify = false;
-        try {
-          input.value = paramValue;
-        } finally {
-          input.notify = true;
-        }
-      });
-      this.formInst!.onInputChanged
-        .subscribe((event) => this.dispatchEvent(new CustomEvent('input-changed', {detail: event})));
-    }, 0);
-  }
-
-  private attach() {
     ui.empty(this);
-    if (this.formInst)
+    this.replaceFunc(fc);
+  }
+
+  private async replaceFunc(funcCall?: DG.FuncCall) {
+    if (!funcCall) {
+      this.formInst = undefined;
+    } else {
+      this.formInst = await DG.InputForm.forFuncCall(funcCall, {twoWayBinding: true, skipDefaultInit: true} as any);
       this.appendChild(this.formInst.root);
+    }
     this.dispatchEvent(new CustomEvent('form-replaced', {detail: this.formInst}));
   }
 }
