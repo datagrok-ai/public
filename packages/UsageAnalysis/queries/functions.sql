b6b567e9de17b3cbad0a462121d209a77e18e5d2
PavloPolovyi <pasha32662@gmail.com>
2025-02-10 17:15:37 +0100

    UsageAnalysis: Functions: Fixed query

--- packages/UsageAnalysis/queries/functions.sql
+++ packages/UsageAnalysis/queries/functions.sql
@@ -7,18 +7,19 @@
 --connection: System:Datagrok
 --test: FunctionsUsage(date='today', ['1ab8b38d-9c4e-4b1e-81c3-ae2bde3e12c5'], ['all'])
 with recursive selected_groups as (
-  select id from groups
-  where id::varchar = any(@groups)
-  union
-  select gr.child_id as id from selected_groups sg
-  join groups_relations gr on sg.id = gr.parent_id
-),
+    select id from groups
+    where id::varchar = any(@groups)
+union
+select gr.child_id as id from selected_groups sg
+                                  join groups_relations gr on sg.id = gr.parent_id
+    ),
 res AS (
 select DISTINCT e.id as id_, et.name as function, coalesce(pp.name, p1.name, 'Core') as package,
 u.friendly_name as user, e.event_time as time_old, u.id as uid,
 u.group_id as ugid, coalesce(pp.package_id, p1.id, '00000000-0000-0000-0000-000000000000') as pid
 from events e
-inner join event_types et on e.event_type_id = et.id
+inner join event_parameter_values evv on evv.event_id = e.id
+inner join funcs et on evv.value_uuid = et.id
 inner join entities en on et.id = en.id
 left join published_packages pp on en.package_id = pp.id
 left join project_relations pr ON pr.entity_id = en.id
@@ -51,9 +52,9 @@ AT TIME ZONE 'UTC' + trunc * interval '1 sec' as time_end,
 res.uid, res.ugid, coalesce(res.pid, '00000000-0000-0000-0000-000000000000') as pid
 from res, t2, selected_groups sg
 where res.ugid = sg.id
-and (res.package = any(@packages) or @packages = ARRAY['all']::varchar[])
+  and (res.package = any(@packages) or @packages = ARRAY['all']::varchar[])
 GROUP BY res.function, res.package, res.user, time_start, time_end,
-res.uid, res.ugid, res.pid
+    res.uid, res.ugid, res.pid
 --end
 
 
@@ -68,25 +69,25 @@ res.uid, res.ugid, res.pid
 --connection: System:Datagrok
 --test: FunctionsContextPane(1681084800, 1681516800, ['878c42b0-9a50-11e6-c537-6bf8e9ab02ee'], ['00000000-0000-0000-0000-000000000000'], ['OpenServerFile'])
 with res AS (
-select DISTINCT e.id as id_, coalesce(pp.name, p1.name, 'Core') as package,
-e.friendly_name as run, et.name as function, e.event_time as time, e.id as rid,
-coalesce(pp.package_id, p1.id, '00000000-0000-0000-0000-000000000000') as pid
-from events e
-inner join event_types et on e.event_type_id = et.id
-inner join entities en on et.id = en.id
-left join published_packages pp on en.package_id = pp.id
-left join project_relations pr ON pr.entity_id = en.id
-left join projects proj ON proj.id = pr.project_id
-and proj.is_root = true
-and proj.is_package = true
-left join packages p1 on proj.name = p1.name or proj.name = p1.friendly_name
-inner join users_sessions s on e.session_id = s.id
-inner join users u on u.id = s.user_id
-where e.event_time between to_timestamp(@time_start)
-and to_timestamp(@time_end)
-and u.id::varchar = any(@users)
-and et.name = any(@functions)
-)
+    select DISTINCT e.id as id_, coalesce(pp.name, p1.name, 'Core') as package,
+    e.friendly_name as run, et.name as function, e.event_time as time, e.id as rid,
+    coalesce(pp.package_id, p1.id, '00000000-0000-0000-0000-000000000000') as pid
+    from events e
+    inner join event_types et on e.event_type_id = et.id
+    inner join entities en on et.id = en.id
+    left join published_packages pp on en.package_id = pp.id
+    left join project_relations pr ON pr.entity_id = en.id
+    left join projects proj ON proj.id = pr.project_id
+    and proj.is_root = true
+    and proj.is_package = true
+    left join packages p1 on proj.name = p1.name or proj.name = p1.friendly_name
+    inner join users_sessions s on e.session_id = s.id
+    inner join users u on u.id = s.user_id
+    where e.event_time between to_timestamp(@time_start)
+    and to_timestamp(@time_end)
+    and u.id::varchar = any(@users)
+    and et.name = any(@functions)
+    )
 select res.package, res.run, res.function, res.time, res.rid, res.pid
 from res
 where res.pid::varchar = any(@packages)
@@ -97,13 +98,13 @@ where res.pid::varchar = any(@packages)
 --input: string function
 --connection: System:Datagrok
 with res as (
-select e.id, EXTRACT(EPOCH FROM (e.event_time_finished - e.event_time)) AS time, ep.name as input,
-ep.type as type, COALESCE(epv.value, epv.value_array::text, epv.data_frame_value_name, epv.value_string, epv.value_uuid::text) as value
-from events e
-inner join event_types et on e.event_type_id = et.id
-left join event_parameter_values epv inner join event_parameters ep on epv.parameter_id = ep.id
-and ep.is_input = true on epv.event_id = e.id
-where et.name = @function)
+    select e.id, EXTRACT(EPOCH FROM (e.event_time_finished - e.event_time)) AS time, ep.name as input,
+    ep.type as type, COALESCE(epv.value, epv.value_array::text, epv.data_frame_value_name, epv.value_string, epv.value_uuid::text) as value
+    from events e
+    inner join event_types et on e.event_type_id = et.id
+    left join event_parameter_values epv inner join event_parameters ep on epv.parameter_id = ep.id
+    and ep.is_input = true on epv.event_id = e.id
+    where et.name = @function)
 select res.time, json_object_agg(res.input, res.value)::text as input
 from res
 GROUP BY res.time
