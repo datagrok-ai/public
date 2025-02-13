ba1db1446a6a3e85e5297729f4952b7bebf333f3
Andrey Sharapov <andrey.sharapov@softwarecountry.com>
2025-02-13 15:17:45 +0300

    DockDpawnDg: Related build tweaks

--- packages/WebComponents/webpack.config.js
+++ packages/WebComponents/webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     package: './src/package.ts',
   },
   resolve: {
-    extensions: ['.wasm', '.mjs', '.ts', '.tsx', '.json', '.js'],
+    extensions: ['.wasm', '.mjs', '.js', '.jsx', '.ts', '.tsx', '.json'],
   },
   module: {
     rules: [
@@ -18,7 +18,7 @@ module.exports = {
         use: ['style-loader', 'css-loader'],
       },
       {
-        test: /\.(js|mjs|jsx|ts|tsx)$/,
+        test: /\.(mjs|js|jsx|ts|tsx)$/,
         enforce: 'pre',
         use: ['source-map-loader'],
       },
