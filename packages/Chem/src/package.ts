23c658c013a7b89f80cefc4130ead5af83e36bf9
drizhina <drizhinashvili@datagrok.ai>
2025-02-20 03:35:38 +0400

    Chem: Bring back context actions

--- packages/Chem/src/package.ts
+++ packages/Chem/src/package.ts
@@ -1312,6 +1312,16 @@ export async function convertNotation(data: DG.DataFrame, molecules: DG.Column<s
   }
 }
 
+//name: Convert Notation...
+//meta.action: Convert Notation...
+//input: column col {semType: Molecule}
+export function convertMolNotationAction(col: DG.Column) {
+  const func = DG.Func.find({name: 'convertNotation', package: 'Chem'})[0];
+  if (!func || !col?.dataFrame)
+    return;
+  func.prepare({data: col.dataFrame, molecules: col}).edit();
+} 
+
 //tags: cellEditor
 //description: Molecule
 //input: grid_cell cell
@@ -1435,7 +1445,6 @@ export async function oclCellRenderer(): Promise<OCLCellRenderer> {
 
 //name: Sort by similarity
 //description: Sorts a molecular column by similarity
-//tags: exclude-actions-panel
 //meta.action: Sort by similarity
 //input: semantic_value value { semType: Molecule }
 export async function sortBySimilarity(value: DG.SemanticValue): Promise<void> {
@@ -1444,7 +1453,9 @@ export async function sortBySimilarity(value: DG.SemanticValue): Promise<void> {
   const dframe = molCol.dataFrame;
   const smiles = molCol.get(tableRowIdx);
 
-  const grid = value.viewer as DG.Grid;
+  const grid = value.viewer as DG.Grid ?? grok.shell.getTableView(dframe.name)?.grid;
+  if (!grid)
+    throw new Error('Cannnot access value grid');
   ui.setUpdateIndicator(grid.root, true);
   const progressBar = DG.TaskBarProgressIndicator.create('Sorting Structures...');
   progressBar.update(0, 'Installing ScaffoldGraph..: 0% completed');
@@ -1466,7 +1477,6 @@ export async function sortBySimilarity(value: DG.SemanticValue): Promise<void> {
 
 //name: Use as filter
 //description: Adds this structure as a substructure filter
-//tags: exclude-actions-panel
 //meta.action: Use as filter
 //input: semantic_value value { semType: Molecule }
 export function useAsSubstructureFilter(value: DG.SemanticValue): void {
@@ -1495,6 +1505,25 @@ export function useAsSubstructureFilter(value: DG.SemanticValue): void {
   }, false);
 }
 
+//name: Copy as...
+//description: Copies structure in different formats
+//meta.action: Copy as...
+//tags: exclude-current-value-menu
+//input: semantic_value value { semType: Molecule }
+export function copyAsAction(value: DG.SemanticValue) {
+  const formats = ['Smiles', 'MolfileV2000', 'MolfileV3000', 'Smarts'];
+  const menu = DG.Menu.popup();
+
+  formats.forEach((format) => {
+    const func = DG.Func.find({package: 'Chem', name: `copyAs${format}`})[0];
+    if (func)
+      menu.item(format, () => {
+        func.apply({value: value});
+      });
+  });
+  menu.show();
+}
+
 //name: Copy as SMILES
 //description: Copies structure as smiles
 //tags: exclude-actions-panel
@@ -1517,6 +1546,30 @@ export function copyAsMolfileV2000(value: DG.SemanticValue): void {
     _convertMolNotation(value.value, DG.chem.Notation.Unknown, DG.chem.Notation.MolBlock, getRdKitModule());
   navigator.clipboard.writeText(molfileV2000);
   grok.shell.info('Molfile V2000 copied to clipboard');
+}
+
+//name: Copy as MOLFILE V3000
+//description: Copies structure as molfile V3000
+//tags: exclude-actions-panel
+//meta.action: Copy as MOLFILE V3000
+//input: semantic_value value { semType: Molecule }
+export function copyAsMolfileV3000(value: DG.SemanticValue): void {
+  const molfileV3000 = DG.chem.isMolBlock(value.value) && value.value.includes('V3000') ? value.value :
+    _convertMolNotation(value.value, DG.chem.Notation.Unknown, DG.chem.Notation.V3KMolBlock, getRdKitModule());
+  navigator.clipboard.writeText(molfileV3000);
+  grok.shell.info('Molfile V3000 copied to clipboard');
+}
+
+//name: Copy as SMARTS
+//description: Copies structure as smarts
+//tags: exclude-actions-panel
+//meta.action: Copy as SMARTS
+//input: semantic_value value { semType: Molecule }
+export function copyAsSmarts(value: DG.SemanticValue): void {
+  const smarts = !DG.chem.isMolBlock(value.value) && _isSmarts(value.value) ? value.value :
+    _convertMolNotation(value.value, DG.chem.Notation.Unknown, DG.chem.Notation.Smarts, getRdKitModule());
+  navigator.clipboard.writeText(smarts);
+  grok.shell.info('Smarts copied to clipboard');
 }
 
 //name: Copy as IMAGE
@@ -1543,31 +1596,6 @@ export function copyAsImage(value: DG.SemanticValue): void {
       grok.shell.info('Image copied to clipboard');
     });
   });
-}
-
-
-//name: Copy as MOLFILE V3000
-//description: Copies structure as molfile V3000
-//tags: exclude-actions-panel
-//meta.action: Copy as MOLFILE V3000
-//input: semantic_value value { semType: Molecule }
-export function copyAsMolfileV3000(value: DG.SemanticValue): void {
-  const molfileV3000 = DG.chem.isMolBlock(value.value) && value.value.includes('V3000') ? value.value :
-    _convertMolNotation(value.value, DG.chem.Notation.Unknown, DG.chem.Notation.V3KMolBlock, getRdKitModule());
-  navigator.clipboard.writeText(molfileV3000);
-  grok.shell.info('Molfile V3000 copied to clipboard');
-}
-
-//name: Copy as SMARTS
-//description: Copies structure as smarts
-//tags: exclude-actions-panel
-//meta.action: Copy as SMARTS
-//input: semantic_value value { semType: Molecule }
-export function copyAsSmarts(value: DG.SemanticValue): void {
-  const smarts = !DG.chem.isMolBlock(value.value) && _isSmarts(value.value) ? value.value :
-    _convertMolNotation(value.value, DG.chem.Notation.Unknown, DG.chem.Notation.Smarts, getRdKitModule());
-  navigator.clipboard.writeText(smarts);
-  grok.shell.info('Smarts copied to clipboard');
 }
 
 //name: isSmiles
