08226e79b354497315db11662459ec46b11a7d08
Dmytro Kovalyov <laykdimon278@gmail.com>
2025-02-20 16:17:42 +0200

    GROK-17599: Sparklines: Fixed margins for sparklines

--- packages/PowerGrid/src/sparklines/shared.ts
+++ packages/PowerGrid/src/sparklines/shared.ts
@@ -81,7 +81,7 @@ export function getSparklinesContextPanel(gridCell: DG.GridCell, colNames: strin
   const df = gridCell.grid.dataFrame;
   const row = gridCell.cell.row.idx;
   const cols = df.columns.byNames(colNames).filter((c) => c !== null);
-  const columnName = ui.div(gridCell.gridColumn.name, {style: {textAlign: 'center'}});
+  const columnName = ui.div(gridCell.gridColumn.name, {style: {textAlign: 'center', marginBottom: '10px'}});
   const values = ui.divV(cols.map((col) => ui.divText(col.name + ': ' + col.getString(row))), { style: { marginTop: '20px' } });
   return ui.div([columnName, DG.GridCellWidget.fromGridCell(gridCell).root, values]);
 }
