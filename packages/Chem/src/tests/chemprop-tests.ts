f73e67f9017972cdf40711651874e474c8df5f69
Oleksandra Serhiienko <aleksandrasergienko12@gmail.com>
2025-03-03 11:51:45 +0200

    Chem: Chemprop: Update test dataset and increase timeout

--- packages/Chem/src/tests/chemprop-tests.ts
+++ packages/Chem/src/tests/chemprop-tests.ts
@@ -2,7 +2,7 @@ import * as grok from 'datagrok-api/grok';
 import * as ui from 'datagrok-api/ui';
 import * as DG from 'datagrok-api/dg';
 import {category, test, before, expect} from '@datagrok-libraries/utils/src/test';
-import {_package, getContainer, applyModelChemprop, trainModelChemprop} from '../package';
+import {_package, applyModelChemprop, trainModelChemprop} from '../package';
 import {CONTAINER_TIMEOUT, ensureContainerRunning, readDataframe} from './utils';
 import JSZip from 'jszip';
 import { fetchWrapper } from '@datagrok-libraries/utils/src/fetch-utils';
@@ -27,16 +27,16 @@ category('chemprop', () => {
     binBlob = await file?.async('uint8array')!;
         
     expect(file !== null, true);
-  }, {timeout: 60000 + CONTAINER_TIMEOUT});
+  }, {timeout: 90000 + CONTAINER_TIMEOUT});
 
   test('applyModel', async () => {
     await ensureContainerRunning('chemprop');
     const smilesColumn = table.columns.byName('canonical_smiles');
     const column = await fetchWrapper(() => applyModelChemprop(binBlob, DG.DataFrame.fromColumns([smilesColumn]).toCsv()));
         
-    expect(column.length, 30);
+    expect(column.length, 20);
   });
-}, {timeout: CONTAINER_TIMEOUT});
+}, {timeout: 60000 + CONTAINER_TIMEOUT});
 
 function getParameterValues() {
   return {
