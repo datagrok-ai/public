0ddabf6bc12b3f1b0cb998a1607100ec7e80e332
Maria Dolotova <maria.dolotova@softwarecountry.com>
2025-03-04 16:11:16 +0300

    AiZynthFinder: First implementation in progress

---
+++ packages/AiZynthFinder/src/package.ts
@@ -0,0 +1,22 @@
+/* eslint-disable max-len */
+import * as grok from 'datagrok-api/grok';
+import * as ui from 'datagrok-api/ui';
+import * as DG from 'datagrok-api/dg';
+import '../css/aizynthfinder.css';
+
+export const _package = new DG.Package();
+
+
+//top-menu: Chem | RetroSynthesis
+//name: RetroSynthesis
+//input: string molecule { semType: molecule }
+export async function retroSynthesis(molecule: string): Promise<void> {
+  const container = await grok.dapi.docker.dockerContainers.filter('aizynthfinder').first();
+  const response = await grok.dapi.docker.dockerContainers.fetchProxy(container.id, '/aizynthfind', {
+    method: 'POST',
+    body: JSON.stringify({smiles: molecule}),
+    headers: {'Content-Type': 'application/json'},
+  });
+
+  console.log((await response.json()));
+}
