7e755774c323507608f3c1b275b3ccee1c7e5cc9
Alexander Paramonov <alex.aprm@Gmail.com>
2025-03-08 14:44:12 +0000

    Added Google Tag

--- docusaurus/src/pages/index.tsx
+++ docusaurus/src/pages/index.tsx
@@ -6,6 +6,7 @@ import Layout from '@theme/Layout';
 import styles from './index.module.css';
 import Hero from '../components/home/hero';
 import Body from '../components/home/body';
+import Head from '@docusaurus/Head';
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
@@ -25,6 +26,16 @@ export default function Home(): JSX.Element {
   return (
     <Layout
       title={`${siteConfig.title}`}>
+        <Head>
+            <script async src="https://www.googletagmanager.com/gtag/js?id=G-KGZGSFPGHS"></script>
+            <script>
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){window.dataLayer.push(arguments);}
+                gtag('js', new Date());
+
+                gtag('config', 'G-KGZGSFPGHS');
+            </script>
+        </Head>
       <HomepageHeader />
     </Layout>
   );
