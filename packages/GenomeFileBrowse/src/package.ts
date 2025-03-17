81f2f6bd4d9843661be9937fbd804a4183ae71f2
Stanislav Savenko <ss.savenko@gmail.com>
2025-03-17 15:55:35 +0200

    Packages: GenomeFileBrowse: base implementation

--- packages/GenomeFileBrowse/src/package.ts
+++ packages/GenomeFileBrowse/src/package.ts
@@ -2,7 +2,7 @@
 import * as grok from 'datagrok-api/grok';
 import * as ui from 'datagrok-api/ui';
 import * as DG from 'datagrok-api/dg';
-import {getGenomeViewer} from './genome-file-browse-viewer-wrapper'
+import {getGenomeFileBrowserComponent} from './genome-file-browse-viewer-wrapper'
 export const _package = new DG.Package();
 
 //name: info
@@ -10,61 +10,54 @@ export function info() {
   grok.shell.info(_package.webRoot);
 }
 
-async function readGenomeConfig(fileContent: string, fileName: string): Promise<DG.View> {
-  const root = ui.div([], { classes: 'd4-spectrum' });
-  const spectrumComponent = getGenomeViewer(root, fileContent);
-  const spectrumHtmlView = DG.View.fromRoot(spectrumComponent.host);
-  spectrumHtmlView.name = fileName;
-  spectrumComponent.loadData(fileContent);
-
-  spectrumHtmlView.subs.push(grok.events.onCurrentViewChanged.subscribe(() => {
-    const cv = grok.shell.v;
-    if ('id' in cv && cv.id && spectrumHtmlView.id && cv.id === spectrumHtmlView.id) {
-      spectrumComponent.loadData(fileContent);
-      spectrumHtmlView.root.appendChild(spectrumComponent.host);
-    }
-  }))
-  return spectrumHtmlView;
-}
-
-//tags: fileViewer
+//tags: genomeFileBrowse
 //input: file file
 //output: view v
-export async function previewNMRFromDX(fileData: DG.FileInfo): Promise<DG.View> {
+export async function genomeFileBrowse(fileData: DG.FileInfo): Promise<DG.View> {
   let viewerHost  = ui.div();
-  getGenomeViewer(viewerHost, await fileData.readAsString());
+  getGenomeFileBrowserComponent(viewerHost, await fileData.readAsString());
   let view = DG.View.fromRoot(viewerHost);
+  view.name = fileData.name;
   grok.shell.addView(view);
   return view;
 }
 
 
-//name: spectraDataCheck
-//input: string fileData
-//output: bool res
-export async function spectraDataCheck(fileData: string) {
-  return !fileData.includes('NMR SPECTRUM');
-}
-
-//name: previewSpectraData
 //tags: fileViewer
 //meta.fileViewer: json
-//meta.fileViewerCheck: SpectraViewer:spectraDataCheck
+//meta.fileViewerCheck: GenomeFileBrowse:checkGenomeConfig
 //input: file file
 //output: view v
-export async function previewSpectraData(fileData: DG.FileInfo): Promise<DG.View> {
-  return await readGenomeConfig(await fileData.readAsString(), fileData.name);
+export async function previewGenomeFileBrowse(fileData: DG.FileInfo): Promise<DG.View>{
+  let viewerHost  = ui.div();
+  getGenomeFileBrowserComponent(viewerHost, await fileData.readAsString());
+  let view = DG.View.fromRoot(viewerHost);
+  view.name = fileData.name;
+  grok.shell.addView(view);
+  return view;
+}
+  
+
+//name: genomeFileBrowseHadnler
+//input: string bytes
+//output: list tables
+//tags: file-handler
+//meta.fileViewerCheck: GenomeFileBrowse:checkGenomeConfig
+//meta.ext: json
+export async function genomeFileBrowseHadnler(bytes: string) {
+  let viewerHost  = ui.div();
+  getGenomeFileBrowserComponent(viewerHost, bytes);
+  let view = DG.View.fromRoot(viewerHost);
+  view.name = 'GenomeFileBrowse';
+  grok.shell.addView(view);
+  return [];
 }
 
-//name: importSpectraData
-//tags: file-handler
-//meta.ext: json
-//meta.fileViewerCheck: SpectraViewer:spectraDataCheck
-//input: string fileString
-//output: list v
-export async function importSpectraData(fileString: string) {
-  const res = await readGenomeConfig(fileString, 'Genome Browse');
-  const newView = grok.shell.addView(res);
-  newView != res && res.subs.forEach((s) => newView.subs.push(s));
-  return [];
+
+//name: checkGenomeConfig
+//input: string content
+//output: bool result
+export function checkGenomeConfig(content: string) {
+  let jsonObj  = JSON.parse(content);
+  return content.length < 1000000 && jsonObj.tracks && (jsonObj.assemblies || jsonObj.assembly);//megabyte
 }
