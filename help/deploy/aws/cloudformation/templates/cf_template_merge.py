e73181c14060f213d52f3a36a5e3f0a2074f985c
Vladyslav Hlushchenko <vhlushchenko@datagrok.ai>
2025-03-24 12:19:08 +0200

    release build docker images

--- help/deploy/aws/cloudformation/templates/cf_template_merge.py
+++ help/deploy/aws/cloudformation/templates/cf_template_merge.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+# This script merges multiple YAML CloudFormation template files into one,
+# optionally saving the result to a user-defined file or auto-generating an output name.
 
 import argparse
 import yaml
@@ -6,12 +8,14 @@ from deepmerge import always_merger
 import os
 
 parser = argparse.ArgumentParser()
+# Accepts one or more input YAML template files
 parser.add_argument(
     "-i",
     "--input",
     help="Path to the template file",
     default=[], action='append'
 )
+# Optional output file path
 parser.add_argument(
     "-o",
     "--output",
@@ -22,6 +26,7 @@ args = parser.parse_args()
 
 result = {}
 output_names=[]
+# Iterate through each provided input file
 for i in args.input:
     with open(i, 'r') as file:
         data = yaml.safe_load(file)
@@ -33,5 +38,6 @@ if args.output:
 else:
     output = "-".join(output_names) + '.yml'
 
+# Write the merged YAML to the output file
 with open(args.output, 'w') as file:
     yaml.dump(result, file, explicit_start=True)
