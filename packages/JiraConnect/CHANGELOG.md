3f8b5c3174eec63da86cc0d8171c970f065a4eb5
Stanislav Savenko <ss.savenko@gmail.com>
2025-03-26 02:29:00 +0200

    JiraConnect: Credentials: Fixed credentials

--- packages/JiraConnect/CHANGELOG.md
+++ packages/JiraConnect/CHANGELOG.md
@@ -1,5 +1,9 @@
 # JiraConnect changelog
 
+## 1.0.1 (2025-03-26)
+
+Added credential checks 
+
 ## 1.0.0 (2025-03-06)
 
 Package release to NPM
--- packages/JiraConnect/src/app/credentials.ts
+++ packages/JiraConnect/src/app/credentials.ts
@@ -8,11 +8,13 @@ interface JiraCredsData {
     host: string;
 }
 
-export async function getJiraCreds(): Promise<JiraCredsData> {
+export async function getJiraCreds(): Promise<JiraCredsData|null> {
     
     let credentialsParams = (await _package.getCredentials()).parameters;
     let authKey = credentialsParams[constants.AUTH_KEY];
     let userName = credentialsParams[constants.USERNAME];
     let host = credentialsParams[constants.HOST_NAME];
+    if(!(authKey && userName && host))
+        return null;
     return { authKey: authKey, userName: userName, host: host };
 }
