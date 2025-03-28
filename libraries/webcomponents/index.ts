d3ca45c7a3c5b95589ac33ab32441ce28b23e1ca
Andrey Sharapov <andrey.sharapov@softwarecountry.com>
2025-03-28 18:37:16 +0300

    Webcomponents: ValidationIcon refactor into separate component

--- libraries/webcomponents/index.ts
+++ libraries/webcomponents/index.ts
@@ -5,5 +5,7 @@ export {InputForm} from './src/InputForm/InputForm';
 export type {InputFormT} from './src/InputForm/InputForm';
 export {DGMarkdown} from './src/Markdown/Markdown';
 export type {DGMarkdown as DGMarkdownT} from './src/Markdown/Markdown';
+export {ValidationIcon} from './src/ValidationIcon/ValidationIcon';
+export type {ValidationIconT} from './src/ValidationIcon/ValidationIcon';
 export * from './src/elements';
 export type * from './src/elements';
