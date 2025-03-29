1e7c1d2ac4a8cce276bb4379e973aa6b49bdcdc0
Alex Paramonov <alex.aprm@Gmail.com>
2025-03-29 14:21:39 +0000

    Packages: Bump versions to 1.25.0 release

--- packages/Python/src/package-test.ts
+++ packages/Python/src/package-test.ts
@@ -1,5 +1,5 @@
 import { runTests, tests, TestContext , initAutoTests as initTests } from '@datagrok-libraries/utils/src/test';
-import { DataFrame } from 'datagrok-api/dg';
+import { DG } from 'datagrok-api/dg';
 
 export let _package = new DG.Package();
 export { tests };
@@ -9,7 +9,7 @@ export { tests };
 //input: string test {optional: true}
 //input: object testContext {optional: true}
 //output: dataframe result
-export async function test(category: string, test: string, testContext: TestContext): Promise<DataFrame> {
+export async function test(category: string, test: string, testContext: TestContext): Promise<DG.DataFrame> {
   const data = await runTests({ category, test, testContext });
   return DG.DataFrame.fromObjects(data)!;
 }
