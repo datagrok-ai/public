20b701e396b7e65879beabaa7251d2f095532ab6
drizhina <drizhinashvili@datagrok.ai>
2025-03-31 13:44:17 -0400

    Docosaurus: Id fixes for new home

--- docusaurus/src/components/new/ui-parts.jsx
+++ docusaurus/src/components/new/ui-parts.jsx
@@ -49,7 +49,7 @@ export function Tab({name, title, description, link, slides, indicators}){
                             <li className='nav-item' key={index}>
                                 <a
                                     className={index === activeIndex ? 'nav-link active' : 'nav-link'}
-                                    data-target={`#accesscarousel-${String(name).toLocaleLowerCase()}`}
+                                    data-target={`#accesscarousel-${getId(String(name)).toLocaleLowerCase()}`}
                                     data-slide-to={index}
                                     onClick={() => setActiveIndex(index)}
                                 >
@@ -80,7 +80,7 @@ export function Tab({name, title, description, link, slides, indicators}){
             </div>
             <div className="col-lg-6 col-xs-10">
                 <div
-                    id={`accesscarousel-${String(name).toLocaleLowerCase()}`}
+                    id={`accesscarousel-${getId(String(name)).toLocaleLowerCase()}`}
                     className="accesscarousel carousel slide carousel-fade"
                     data-ride="carousel"
                     data-interval="false"
@@ -88,7 +88,7 @@ export function Tab({name, title, description, link, slides, indicators}){
                     <ol className={indicators !== undefined ? "carousel-indicators" : "d-none"}>
                         {list.map((item, index) => (
                             <li
-                                data-target={`#accesscarousel-${String(name).toLocaleLowerCase()}`}
+                                data-target={`#accesscarousel-${getId(String(name)).toLocaleLowerCase()}`}
                                 data-slide-to={index}
                                 className={index === 0 ? "active" : ""}
                             ></li>
@@ -97,7 +97,7 @@ export function Tab({name, title, description, link, slides, indicators}){
                     <div className="carousel-inner">
                         {list.map((item, index) => (
                             <div className={index === 0 ? "carousel-item active" : "carousel-item"} key={index}>
-                                <Image src={item.image} />
+                                {item.details ?? <Image src={item.image} />}
                             </div>
                         ))}
                     </div>
@@ -107,31 +107,36 @@ export function Tab({name, title, description, link, slides, indicators}){
     );
 }
 
+export function getId(val) {
+    return val.replaceAll(' ', '-')
+}
+
 export function TabGroup({children}){
     const tabs = children.length ? Array.from(children) : [children];
+    const id = Math.random();
     return (
         <>
-            <ul className="nav nav-tabs nav-pills my-3 mt-5 " id="features-nav-tab" role="tablist">
+            <ul className="nav nav-tabs nav-pills my-3 mt-5 features-nav-tab" id={`features-nav-tab-${id}`} role="tablist">
                 {tabs.map((item, index) => (
                     <li className='nav-item' key={index}>
                         <a
                             className={index === 0 ? 'brand-btn btn-outline btn nav-link active' : 'brand-btn btn-outline btn nav-link'}
-                            id={item.props.name}
+                            id={getId(item.props.name)}
                             data-toggle="tab"
                             role="tab"
-                            aria-controls={`${item.props.name}-content`}
-                            href={`#${item.props.name}-content`}
+                            aria-controls={`${getId(item.props.name)}-content`}
+                            href={`#${getId(item.props.name)}-content`}
                         >
                             {item.props.name}
                         </a>
                     </li>
                 ))}
             </ul>
-            <div className="tab-content text-left container py-4" id="features-nav-tab-content">
+            <div className="tab-content text-left container py-4" id={`features-nav-tab-content-${id}`}>
                 {tabs.map((item, index) => (
                     <div
                         className={index === 0 ? "tab-pane fade show active" : "tab-pane fade"}
-                        id={`${item.props.name}-content`}
+                        id={`${getId(item.props.name)}-content`}
                         role="tabpanel"
                         key={index}
                         aria-labelledby={item.props.name}
