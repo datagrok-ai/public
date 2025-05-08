5e6e2f25286c30e33d31c0b7001ac9708c2920ac
PavloPolovyi <pasha32662@gmail.com>
2025-05-08 17:38:29 +0200

    Docusaurus: Fixed path

--- docusaurus/generate_docs_py.sh
+++ docusaurus/generate_docs_py.sh
@@ -10,13 +10,14 @@ cd ../docusaurus
 # Set the path to the Python script and the argument
 SCRIPT="../python-api/generate_docs.py"
 SRC_DIR="../python-api/datagrok_api"
-OUT_DIR="/api/py"
+OUT_DIR="api/py"
 
 # Run the Python script with the argument
 python "$SCRIPT" "$SRC_DIR" "$OUT_DIR"
 
-# Copy the python-api.md file to /api/py as index.md
-cp ../help/develop/python-api.md /api/py/index.md
+# Copy the python-api.md file to api/py as index.md
+cp ../help/develop/python-api.md api/py/index.md
 
-# Copy the api-overview.md file to /api as index.md
-cp ../help/develop/api-overview.md /api/index.md
+# Copy the api-overview.md file to api as index.md
+cp ../help/develop/api-overview.md api/index.md
+
