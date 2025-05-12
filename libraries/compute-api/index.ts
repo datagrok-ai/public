97f97cd92b36dd1fbd76c455913af06d4f3ccc6a
Andrey Sharapov <andrey.sharapov@softwarecountry.com>
2025-05-12 10:31:42 +0300

    ComputeApi: Expose start workflow with typings

--- libraries/compute-api/index.ts
+++ libraries/compute-api/index.ts
@@ -2,6 +2,7 @@
 import * as grok from 'datagrok-api/grok';
 import * as ui from 'datagrok-api/ui';
 import * as DG from 'datagrok-api/dg';
+import {PipelineInstanceConfig} from '@datagrok-libraries/compute-utils/reactive-tree-driver/index';
 
 type ConstructorTypeOf<T> = new (...args:any[]) => T;
 
@@ -57,4 +58,9 @@ export async function initComputeApi() {
   const initFunc = DG.Func.find({package: 'Compute', name: 'init'})[0];
   if (initFunc)
     await initFunc.prepare().call();
+}
+
+export async function startWorkflow(nqName: string, version: string, instanceConfig: PipelineInstanceConfig) {
+  const startFunc = DG.Func.find({package: 'Compute2', name: 'StartWorkflow'})[0];
+  await startFunc.prepare({nqName, version, instanceConfig}).call();
 }
