a39dab54ce9cc750fa0a0f1f945c6666966b5936
PavloPolovyi <pasha32662@gmail.com>
2025-06-20 17:07:16 +0200

    Admetica: Docker: Use celery and queue

--- packages/Admetica/src/package-api.ts
+++ packages/Admetica/src/package-api.ts
@@ -5,43 +5,39 @@ import * as DG from 'datagrok-api/dg';
 
 export namespace funcs {
   export async function info(): Promise<any> {
-    return await grok.functions.call('@datagrok/admetica:Info', {});
-  }
-
-  export async function runAdmetica(csvString: string, queryParams: string, raiseException: boolean): Promise<any> {
-    return await grok.functions.call('@datagrok/admetica:RunAdmetica', { csvString, queryParams, raiseException });
+    return await grok.functions.call('Admetica:Info', {});
   }
 
   export async function admeticaWidget(smiles: any): Promise<any> {
-    return await grok.functions.call('@datagrok/admetica:AdmeticaWidget', { smiles });
+    return await grok.functions.call('Admetica:AdmeticaWidget', { smiles });
   }
 
   export async function getModels(property: string): Promise<any> {
-    return await grok.functions.call('@datagrok/admetica:GetModels', { property });
+    return await grok.functions.call('Admetica:GetModels', { property });
   }
 
   export async function admeticaHT(table: DG.DataFrame, molecules: DG.Column): Promise<any> {
-    return await grok.functions.call('@datagrok/admetica:AdmeticaHT', { table, molecules });
+    return await grok.functions.call('Admetica:AdmeticaHT', { table, molecules });
   }
 
   export async function admeticaEditor(call: any): Promise<any> {
-    return await grok.functions.call('@datagrok/admetica:AdmeticaEditor', { call });
+    return await grok.functions.call('Admetica:AdmeticaEditor', { call });
   }
 
   export async function admeticaMenu(table: DG.DataFrame, molecules: DG.Column, template: string, addPiechart: boolean, addForm: boolean): Promise<any> {
-    return await grok.functions.call('@datagrok/admetica:AdmeticaMenu', { table, molecules, template, addPiechart, addForm });
+    return await grok.functions.call('Admetica:AdmeticaMenu', { table, molecules, template, addPiechart, addForm });
   }
 
   export async function admeProperty(molecule: string, prop: string): Promise<any> {
-    return await grok.functions.call('@datagrok/admetica:AdmeProperty', { molecule, prop });
+    return await grok.functions.call('Admetica:AdmeProperty', { molecule, prop });
   }
 
   export async function admeticaApp(): Promise<any> {
-    return await grok.functions.call('@datagrok/admetica:AdmeticaApp', {});
+    return await grok.functions.call('Admetica:AdmeticaApp', {});
   }
 
   //Evaluating ADMET properties
   export async function admeticaDemo(): Promise<any> {
-    return await grok.functions.call('@datagrok/admetica:AdmeticaDemo', {});
+    return await grok.functions.call('Admetica:AdmeticaDemo', {});
   }
 }
