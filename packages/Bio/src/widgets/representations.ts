a00db757e4d05464951c8a55427e1637dadce6bd
drizhina <drizhinashvili@datagrok.ai>
2025-06-26 21:52:22 +0300

    Bio: Revive Macromol 3d structure context panel

--- packages/Bio/src/widgets/representations.ts
+++ packages/Bio/src/widgets/representations.ts
@@ -6,8 +6,6 @@ import * as DG from 'datagrok-api/dg';
 import {TAGS as mmcrTAGS} from '@datagrok-libraries/bio/src/utils/cell-renderer';
 
 import {MmcrTemps, rendererSettingsChangedState} from '@datagrok-libraries/bio/src/utils/cell-renderer-consts';
-import {ISeqHelper} from '@datagrok-libraries/bio/src/utils/seq-helper';
-import {getMolfilesFromSingleSeq} from '@datagrok-libraries/bio/src/monomer-works/monomer-utils';
 
 import {_package} from '../package';
 import {NOTATION} from '@datagrok-libraries/bio/src/utils/macromolecule';
@@ -145,57 +143,4 @@ export function getMacromoleculeColumnPropertyPanel(col: DG.Column): DG.Widget {
 
   const sequenceConfigInputs = ui.inputs(inputsArray);
   return new DG.Widget(sequenceConfigInputs);
-}
-
-/**
- * 3D representation widget of macromolecule.
- *
- * @export
- * @param {DG.Cell} macroMolecule macromolecule cell.
- * @param {any[]} monomersLibObject
- * @param {ISeqHelper} seqHelper
- * @return {Promise<DG.Widget>} Widget.
- */
-export async function representationsWidget(
-  macroMolecule: DG.Cell, monomersLibObject: any[], seqHelper: ISeqHelper
-): Promise<DG.Widget> {
-  const pi = DG.TaskBarProgressIndicator.create('Creating 3D view');
-
-  let widgetHost;
-  let molBlock3D = '';
-  try {
-    try {
-      const _atomicCodes = getMolfilesFromSingleSeq(macroMolecule, monomersLibObject, seqHelper);
-      const result = '';//await getMacroMol(atomicCodes!);
-      const molBlock2D = result[0];
-      molBlock3D = (await grok.functions.call('Bio:Embed', {molBlock2D})) as unknown as string;
-    } catch (e) {
-      console.warn(e);
-    }
-
-    try {
-      molBlock3D = molBlock3D.replaceAll('\\n', '\n');
-      const stringBlob = new Blob([molBlock3D], {type: 'text/plain'});
-      const nglHost = ui.div([], {classes: 'd4-ngl-viewer', id: 'ngl-3d-host'});
-
-      //@ts-ignore
-      const stage = new NGL.Stage(nglHost, {backgroundColor: 'white'});
-      //@ts-ignore
-      stage.loadFile(stringBlob, {ext: 'sdf'}).then(function(comp: NGL.StructureComponent) {
-        stage.setSize(300, 300);
-        comp.addRepresentation('ball+stick');
-        comp.autoView();
-      });
-      const sketch = grok.chem.svgMol(molBlock3D);
-      const panel = ui.divH([sketch]);
-
-      widgetHost = ui.div([panel, nglHost]);
-    } catch (e) {
-      widgetHost = ui.divText('Couldn\'t get peptide structure');
-    }
-  } catch (e) {
-    widgetHost = ui.divText('Couldn\'t get peptide structure');
-  }
-  pi.close();
-  return new DG.Widget(widgetHost);
 }
