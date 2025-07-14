d19f550f9ce34bb80c47d9664cce91021dc4c66c
vmakarichev <vmakarichev@datagrok.ai>
2025-07-14 14:41:21 +0300

    GROK-18428: FileEditors: Added empty base
    Added the empty base file to prevent errors in console

--- packages/FileEditors/src/latex.ts
+++ packages/FileEditors/src/latex.ts
@@ -1,6 +1,4 @@
-//import * as grok from 'datagrok-api/grok';
 import * as ui from 'datagrok-api/ui';
-//import * as DG from 'datagrok-api/dg';
 
 // @ts-ignore
 import {parse, HtmlGenerator} from 'latex.js';
