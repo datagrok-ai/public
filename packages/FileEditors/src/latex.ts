715776bc10e4c5e310e74c454276e5b84f5bf9e5
vmakarichev <vmakarichev@datagrok.ai>
2025-07-14 18:36:20 +0300

    GROK-18428: FileEditors: Updated styles
    Updated transform

--- packages/FileEditors/src/latex.ts
+++ packages/FileEditors/src/latex.ts
@@ -9,16 +9,18 @@ export function getElementWithLatexContent(latexText: string): HTMLIFrameElement
   const htmlGenerator = parse(latexText, {generator: generator});
   const doc = htmlGenerator.htmlDocument(`${_package.webRoot}css/latex/`) as Document;
 
-  // const div = ui.div([]);
-  // div.style.width = '100%';
-  // div.style.height = '100%';
   const iframe = ui.iframe({width: '100%', height: '100%'});
-  //div.append(iframe);
-
   const rightMargin = doc.body.children[1];
 
   if ((rightMargin !== null) && (rightMargin !== null))
     (rightMargin as HTMLElement).style.display = 'none';
+
+  const contentRegion = doc.body.children[0];
+
+  if ((contentRegion !== null) && (contentRegion !== null)) {
+    (contentRegion as HTMLElement).style.transform = 'scale(1.5)';
+    (contentRegion as HTMLElement).style.transformOrigin = 'top';
+  }
 
   iframe.srcdoc = doc.documentElement.outerHTML;
   iframe.style.border = 'transparent';
