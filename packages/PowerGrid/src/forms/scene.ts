68715b33fd1a181ced669930d206f5df6cb5cabb
drizhina <drizhinashvili@datagrok.ai>
2025-07-28 20:45:52 +0300

    PowerGrid: Smartform improvements

--- packages/PowerGrid/src/forms/scene.ts
+++ packages/PowerGrid/src/forms/scene.ts
@@ -1,4 +1,4 @@
-import * as DG from "datagrok-api/dg";
+import * as DG from 'datagrok-api/dg';
 import * as ui from 'datagrok-api/ui';
 
 export interface IStyle {
@@ -29,27 +29,52 @@ export abstract class Element {
 
 
 export class LabelElement extends Element {
+  private shortenedText: string;
   constructor(bounds: DG.Rect,
+              private readonly averageCharWidth: number,
               public text: string,
               public style?: IStyle) {
     super(bounds);
+    this.shortenedText = shortenToCanvas(this.averageCharWidth, this.text, this.bounds.width);
+    if (this.shortenedText !== this.text) {
+      this.style ??= {};
+      this.style.tooltip = this.text;
+    }
   }
 
   render(g: CanvasRenderingContext2D) {
-    g.fillStyle = this.style?.color ?? 'grey'
+    g.fillStyle = this.style?.color ?? 'grey';
     g.textAlign = this.style?.horzAlign ?? 'left';
     g.textBaseline = 'top';
     //g.fillText(this.text, this.bounds.x, this.bounds.y, this.bounds.width);
     drawClipped(g, this.bounds, () => {
-      const x
-        = g.textAlign == 'left' ? this.bounds.x
-        : g.textAlign == 'right' ? this.bounds.right
-        : g.textAlign == 'center' ? this.bounds.midX : 0;
+      const x =
+        g.textAlign == 'left' ? this.bounds.x :
+          g.textAlign == 'right' ? this.bounds.right :
+            g.textAlign == 'center' ? this.bounds.midX : 0;
       if (this.style?.font != null)
         g.font = this.style.font;
-      g.fillText(this.text, x, this.bounds.top + 1, this.bounds.width);
+      g.fillText(this.shortenedText, x, this.bounds.top + 1, this.bounds.width);
     });
   }
+}
+
+export function shortenToCanvas(averageCharWidth: number, text: string, width: number): string {
+  if (averageCharWidth * (text ?? '').length <= width)
+    return text;
+  let result = text;
+  let p1 = 0;
+  let p2 = text.length - 1;
+  while (p1 < p2) {
+    const mid = Math.floor((p1 + p2) / 2);
+    result = text.substring(0, mid) + '...';
+    if (averageCharWidth * (result.length - 2) <= width)
+      p1 = mid + 1;
+    else
+      p2 = mid - 1;
+  }
+
+  return result;
 }
 
 
@@ -71,7 +96,7 @@ export class GridCellElement extends Element {
   constructor(bounds: DG.Rect,
               public gridCell: DG.GridCell) {
     super(bounds);
-    this.style = { tooltip: gridCell.cell.valueString }
+    this.style = {tooltip: gridCell.cell.valueString};
   }
 
   render(g: CanvasRenderingContext2D) {
@@ -99,14 +124,13 @@ export class Scene extends Element {
   }
 
   render(g: CanvasRenderingContext2D) {
-    for (const e of this.elements) {
+    for (const e of this.elements)
       e.render(g);
-    }
   }
 
   toCanvas(): HTMLCanvasElement {
     const canvas = ui.canvas(this.bounds.width, this.bounds.height);
-    this.render(canvas.getContext("2d")!);
+    this.render(canvas.getContext('2d')!);
     return canvas;
   }
 
@@ -126,8 +150,7 @@ function drawClipped(g: CanvasRenderingContext2D, bounds: DG.Rect, draw: () => v
     //g.rect(bounds.x, bounds.y, bounds.width, bounds.height);
     //g.clip();
     draw();
-  }
-  finally {
+  } finally {
     //g.restore();
   }
 }
