e92d12a63b68e45ff3af20edd03afcaeb29e85a0
Viktor Makarichev <112491505+vmakarichev@users.noreply.github.com>
2025-07-30 14:54:53 +0300

    GROK-18428: File editors: LaTeX (#3424)
    * GROK-18428: FileEditors: LaTex
    
    Added LaTex support
    
    * GROK-18428: FileEditors: Dependencies
    
    Updated dependencies
    
    * GROK-18428: FileEditors: LaTeX styles
    
    Added styles and fonts
    
    * GROK-18428: FileEditors: KaTeX
    
    Added KaTeX features
    
    * GROK-18428: FileEditors: LaTeX viewer
    
    Added latex via iframe
    
    * GROK-18428: FileEditors: LaTeX viewer
    
    Added debugging code
    
    * GROK-18428: FileEditors: LaTeX viewer
    
    Removed debugging code
    
    * GROK-18428: FileEditors: Added empty base
    
    Added the empty base file to prevent errors in console
    
    * GROK-18428: FileEditors: Fixed names
    
    Renamed css files to fix space in name issues
    
    * GROK-18428: FileEditors: Updated styles
    
    Updated transform
    
    * GROK-18428: FileEditors: Removed katex
    
    Removed the redundant code
    
    * GROK-18428: File Editors: Version upd
    
    Updated version
    
    * GROK-18428: File Editors: Removed scaling
    
    Removed scaling
    
    * GROK-18428: File Editors: Latex viewer
    
    Created latex viewer
    
    * GROK-18428: File Editors: Edit toggle
    
    Added the edit toggle
    
    * GROK-18428: File Editors: Tex editor
    
    Implemented the tex code editor
    
    * GROK-18428: File Editors: Download tex
    
    Implemented the tex code download feature
    
    * GROK-18428: File Editors: Download tex
    
    Implemented the tex code download feature
    
    * GROK-18428: File Editors: Refresh tex
    
    Updated the refresh feature
    
    * GROK-18428: File Editors: Updated the saving feature
    
    Updated the saving feature
    
    * GROK-18428: File Editors: Preserve scroll
    
    Implemented the preserve scroll feature
    
    * GROK-18428: File Editors: Tex editor upd
    
    Updated tex editor
    
    * GROK-18428: File Editors: Refactored code
    
    Refactoring
    
    * GROK-18428: File Editors: Copy paste
    
    Updated the copy paste features
    
    * GROK-18428: File Editors: Save feature upd
    
    Updated the save feature
    
    * GROK-18428: File Editors: Debounce upd
    
    Updated debouncing

---
+++ packages/FileEditors/src/latex/utils.ts
@@ -0,0 +1,28 @@
+import {ACTION_KEYS, CUT_PASTE, PRINTABLE_LEN} from './constants';
+
+export function debounce<T extends(...args: any[]) => void>(
+  func: T,
+  delay: number,
+): (...args: Parameters<T>) => void {
+  let timeoutId: ReturnType<typeof setTimeout>;
+
+  return function(...args: Parameters<T>) {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => {
+      func(...args);
+    }, delay);
+  };
+}
+
+export function isActionKey(key: string): boolean {
+  return (key.length === PRINTABLE_LEN) || ACTION_KEYS.has(key);
+}
+
+export function isCutPaste(event: KeyboardEvent): boolean {
+  const key = event.key.toLowerCase();
+
+  if (event.ctrlKey || event.metaKey)
+    return CUT_PASTE.has(key) && (key !== 'c');
+
+  return (event.shiftKey && key === 'insert');
+}
