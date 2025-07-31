646dac53405863176596166f45928845b9400a43
Oleksandra Serhiienko <aleksandrasergienko12@gmail.com>
2025-07-31 13:03:09 +0300

    Charts: Update package-api

--- packages/Charts/src/package-api.ts
+++ packages/Charts/src/package-api.ts
@@ -8,6 +8,66 @@ import * as DG from 'datagrok-api/dg';
 
 
 export namespace funcs {
+  export async function flagCellRenderer(): Promise<any> {
+    return await grok.functions.call(':FlagCellRenderer', {});
+  }
+
+  /**
+  Chord viewer visualizes weighted relationships between several entities
+  */
+  export async function chordViewerDemo(): Promise<any> {
+    return await grok.functions.call(':ChordViewerDemo', {});
+  }
+
+  /**
+  Radar viewer is used on multivariate data to plot groups of values over several common variables
+  */
+  export async function radarViewerDemo(): Promise<any> {
+    return await grok.functions.call(':RadarViewerDemo', {});
+  }
+
+  /**
+  Sankey viewer depicts a flow from one set of values to another
+  */
+  export async function sankeyViewerDemo(): Promise<any> {
+    return await grok.functions.call(':SankeyViewerDemo', {});
+  }
+
+  /**
+  Sunburst viewer displays hierarchical data
+  */
+  export async function sunburstViewerDemo(): Promise<any> {
+    return await grok.functions.call(':SunburstViewerDemo', {});
+  }
+
+  /**
+  Surface plot viewer displays a set of three-dimensional data as a mesh surface
+  */
+  export async function surfacePlotDemo(): Promise<any> {
+    return await grok.functions.call(':SurfacePlotDemo', {});
+  }
+
+  /**
+  Timelines viewer displays the flow of events over time
+  */
+  export async function timelinesViewerDemo(): Promise<any> {
+    return await grok.functions.call(':TimelinesViewerDemo', {});
+  }
+
+  /**
+  Tree viewer visualizes hierarchical data by categories
+  */
+  export async function treeViewerDemo(): Promise<any> {
+    return await grok.functions.call(':TreeViewerDemo', {});
+  }
+
+  /**
+  Word Cloud viewer visualizes unstructured text data
+  */
+  export async function wordCloudViewerDemo(): Promise<any> {
+    return await grok.functions.call(':WordCloudViewerDemo', {});
+  }
+
   /**
   Creates a chord viewer
   */
