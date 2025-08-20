59e48ce1a048f7e545bfbab10261fa3c5e62a1d5
Oleksandra Serhiienko <aleksandrasergienko12@gmail.com>
2025-08-20 12:12:56 +0300

    MolTrack: Get rid of the obsolete container

--- packages/MolTrack/dockerfiles/docker-entrypoint.sh
+++ packages/MolTrack/dockerfiles/docker-entrypoint.sh
@@ -4,11 +4,11 @@ set -e
 echo "Parsing DB environment variable..."
 
 if [ -n "$db" ]; then
-  export DB_HOST=$(echo "$db" | jq -r '.parameters.server')
+  export DB_HOST="host.docker.internal"
   export DB_USER=$(echo "$db" | jq -r '.credentials.parameters.login')
   export DB_PASSWORD=$(echo "$db" | jq -r '.credentials.parameters.password')
   export DB_NAME=$(echo "$db" | jq -r '.parameters.db')
-  export DB_SCHEMA=moltrack
+  export APP_PORT=18000
 else
   echo "Environment variable 'db' not set."
   exit 1
--- packages/MolTrack/queries/setup.sql
+++ packages/MolTrack/queries/setup.sql
@@ -54,3 +54,25 @@ values ('Compound Matching Rule',
         'ALL_LAYERS',
         'Defines the rule for matching compounds. Possible values: ALL_LAYERS (default), STEREO_INSENSITIVE_LAYERS, TAUTOMER_INSENSITIVE_LAYERS');
 --end
+
+--name: insertStandartization
+--connection: moltrack
+insert into moltrack.settings (name, value, description)
+values (
+  'Molecule standardization rules',
+  'operations:
+  - type: "Cleanup"
+    description: "Basic cleanup of the molecule, including removal of explicit hydrogens"
+    enable: true
+
+  - type: "FragmentParent"
+    description: "Retains the largest parent fragment of the molecule."
+    enable: true
+
+  - type: "Uncharger"
+    description: "Neutralizes charges on the molecule."
+    enable: true
+  ',
+  'Defines the molecule standardization pipeline'
+);
+--end
