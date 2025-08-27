37ae1d8b4c5706a038aa1b36474e807467cbba0f
Stanislav Savenko <ss.savenko@gmail.com>
2025-08-27 18:37:34 +0300

    JiraConnect: Added ability to detect few tickets in one cell

--- packages/JiraConnect/src/package.g.ts
+++ packages/JiraConnect/src/package.g.ts
@@ -1,49 +1,42 @@
 import {PackageFunctions} from './package';
 import * as DG from 'datagrok-api/dg';
 
-//name: info
-export function info() {
-  return PackageFunctions.info();
+export function info() : void {
+  PackageFunctions.info();
 }
 
-//name: _init
 //tags: autostart
-export async function _init() {
-  return PackageFunctions._init();
+export async function _init() : Promise<void> {
+  await PackageFunctions._init();
 }
 
-//name: projectsList
 //output: object projects
-export async function projectsList() {
-  return PackageFunctions.projectsList();
+export async function projectsList() : Promise<any> {
+  return await PackageFunctions.projectsList();
 }
 
-//name: projectData
 //input: string projectKey 
 //output: object projects
-export async function projectData(projectKey: string) {
-  return PackageFunctions.projectData(projectKey);
+export async function projectData(projectKey: string) : Promise<any> {
+  return await PackageFunctions.projectData(projectKey);
 }
 
-//name: issueData
 //input: string issueKey 
 //output: object issue
-export async function issueData(issueKey: string) {
-  return PackageFunctions.issueData(issueKey);
+export async function issueData(issueKey: string) : Promise<any> {
+  return await PackageFunctions.issueData(issueKey);
 }
 
-//name: getJiraField
 //input: column<string> ticketColumn 
 //input: string field 
 //output: column result
 //meta.vectorFunc: true
-export async function getJiraField(ticketColumn: DG.Column, field: string) {
-  return PackageFunctions.getJiraField(ticketColumn, field);
+export async function getJiraField(ticketColumn: DG.Column, field: string) : Promise<any> {
+  return await PackageFunctions.getJiraField(ticketColumn, field);
 }
 
-//name: getJiraTicketsByFilter
 //input: object filter 
 //output: object result
-export async function getJiraTicketsByFilter(filter?: any) {
-  return PackageFunctions.getJiraTicketsByFilter(filter);
+export async function getJiraTicketsByFilter(filter?: any) : Promise<any> {
+  return await PackageFunctions.getJiraTicketsByFilter(filter);
 }
