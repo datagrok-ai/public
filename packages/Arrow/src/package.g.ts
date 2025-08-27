4f0834527261edec61ac13723dce9ca948bac592
PavloPolovyi <pasha32662@gmail.com>
2025-08-27 12:14:34 +0200

    Arrow: Categorical column creation fix, datetime col fix, version bump

--- packages/Arrow/src/package.g.ts
+++ packages/Arrow/src/package.g.ts
@@ -1,80 +1,70 @@
 import {PackageFunctions} from './package';
 import * as DG from 'datagrok-api/dg';
 
-//name: parquetInit
 //tags: autostart
-export function parquetInit() {
-  return PackageFunctions.parquetInit();
+export function parquetInit() : void {
+  PackageFunctions.parquetInit();
 }
 
-//name: initPackage
 //tags: init
-export async function initPackage() {
-  return PackageFunctions.initPackage();
+export async function initPackage() : Promise<void> {
+  await PackageFunctions.initPackage();
 }
 
-//name: toFeather
 //description: Converts DG.DataFrame to arrow
 //input: dataframe table 
 //input: bool asStream { default: true }
 //output: blob result
-export function toFeather(table: DG.DataFrame, asStream: boolean) {
+export function toFeather(table: DG.DataFrame, asStream: boolean) : any {
   return PackageFunctions.toFeather(table, asStream);
 }
 
-//name: fromFeather
 //description: Converts arrow ipc stream to DG.DataFrame
 //input: blob bytes 
 //output: dataframe result
-export function fromFeather(bytes: Uint8Array) {
+export function fromFeather(bytes: Uint8Array) : any {
   return PackageFunctions.fromFeather(bytes);
 }
 
-//name: toParquet
 //description: Converts DG.DataFrame to parquet
 //input: dataframe table 
 //input: int compression { nullable: true }
 //output: blob result
-export function toParquet(table: DG.DataFrame, compression?: number) {
+export function toParquet(table: DG.DataFrame, compression?: number) : any {
   return PackageFunctions.toParquet(table, compression);
 }
 
-//name: fromParquet
 //description: Converts binary data in parquet format to DG.DataFrame
 //input: blob bytes 
 //output: dataframe result
-export function fromParquet(bytes: Uint8Array) {
+export function fromParquet(bytes: Uint8Array) : any {
   return PackageFunctions.fromParquet(bytes);
 }
 
-//name: parquetFileHandler
 //tags: file-handler
 //input: list bytes 
 //output: list<dataframe> result
 //meta.ext: parquet
-export function parquetFileHandler(bytes: any) {
+export function parquetFileHandler(bytes: any) : any {
   return PackageFunctions.parquetFileHandler(bytes);
 }
 
-//name: featherFileHandler
 //tags: file-handler
 //input: list bytes 
 //output: list<dataframe> result
 //meta.ext: feather
-export function featherFileHandler(bytes: any) {
+export function featherFileHandler(bytes: any) : any {
   return PackageFunctions.featherFileHandler(bytes);
 }
 
-//name: saveAsParquet
 //description: Save as Parquet
 //tags: fileExporter
-export function saveAsParquet() {
-  return PackageFunctions.saveAsParquet();
+export function saveAsParquet() : void {
+  PackageFunctions.saveAsParquet();
 }
 
-//name: saveAsFeather
 //description: Save as Feather
 //tags: fileExporter
-export function saveAsFeather() {
-  return PackageFunctions.saveAsFeather();
+export function saveAsFeather() : void {
+  PackageFunctions.saveAsFeather();
 }
