82d3d8b4976f3addf9059edcae2f0ba2b7bd8754
Oleksandra Serhiienko <aleksandrasergienko12@gmail.com>
2025-09-04 15:08:37 +0200

    MolTrack: Update db_host to work with the rdkit cartridge on dev

--- packages/MolTrack/dockerfiles/docker-entrypoint.sh
+++ packages/MolTrack/dockerfiles/docker-entrypoint.sh
@@ -3,8 +3,11 @@ set -e
 
 echo "Parsing DB environment variable..."
 
+echo $db
+
 if [ -n "$db" ]; then
-  export DB_HOST="host.docker.internal"
+  # export DB_HOST=$(echo "$db" | jq -r '.parameters.server')
+  export DB_HOST="10.152.183.177"
   export DB_USER=$(echo "$db" | jq -r '.credentials.parameters.login')
   export DB_PASSWORD=$(echo "$db" | jq -r '.credentials.parameters.password')
   export DB_NAME=$(echo "$db" | jq -r '.parameters.db')
