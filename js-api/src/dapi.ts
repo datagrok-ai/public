f171c5b92ed558c9622d6e935b065348b71c2039
PavloPolovyi <pasha32662@gmail.com>
2025-09-05 16:45:28 +0200

    GROK-18560: Add batch request to get all events for favorites

--- js-api/src/dapi.ts
+++ js-api/src/dapi.ts
@@ -1164,8 +1164,8 @@ export class LogDataSource extends HttpDataSource<LogEvent> {
     return new ActivityDataSource(api.grok_Dapi_Activity());
   }
 
-  where(options?: {entityId?: string, start?: dayjs.Dayjs, end?: dayjs.Dayjs}): LogDataSource {
-    return new LogDataSource(api.grok_Dapi_Log_Where(this.dart, options?.entityId ?? '', toDart(options?.start), toDart(options?.end)));
+  where(options?: {entityId?: string, start?: dayjs.Dayjs, end?: dayjs.Dayjs, favoritesOnly: boolean}): LogDataSource {
+    return new LogDataSource(api.grok_Dapi_Log_Where(this.dart, options?.entityId ?? '', toDart(options?.start), toDart(options?.end), options?.favoritesOnly ?? false));
   }
 }
 
