84851345949d8193ded95d09af9b5b9377386c87
Dmytro Kovalyov <laykdimon278@gmail.com>
2025-09-09 14:15:41 +0300

    GROK-18733: Recent projects: Made sure `ui.wait()` located in the center

--- packages/PowerPack/src/widgets/recent-projects-widget.ts
+++ packages/PowerPack/src/widgets/recent-projects-widget.ts
@@ -1,8 +1,8 @@
 import * as grok from 'datagrok-api/grok';
-import * as DG from "datagrok-api/dg";
-import * as ui from "datagrok-api/ui";
-import {div} from "datagrok-api/ui";
-import {IDartApi} from "datagrok-api/src/api/grok_api.g";
+import * as DG from 'datagrok-api/dg';
+import * as ui from 'datagrok-api/ui';
+import {div} from 'datagrok-api/ui';
+import {IDartApi} from 'datagrok-api/src/api/grok_api.g';
 
 const api: IDartApi = <any>window;
 
@@ -10,53 +10,54 @@ export class RecentProjectsWidget extends DG.Widget {
   caption: string;
 
   constructor() {
-    super(ui.panel());
+    super(ui.panel([], {style: {display: 'flex'}}));
     this.caption = super.addProperty('caption', DG.TYPE.STRING, 'Recent projects');
     this.getProjects();
   }
 
-  async getProjects(){
-    const projectList = ui.wait(async () => {
-      const listDiv = ui.divV([]);
+  async getProjects() {
+    const projectList = ui.divV([], {style: {flex: '1'}});
+    projectList.appendChild(ui.wait(async () => {
+      const contentDiv = ui.div([], {style: {flex: '1'}});
       const projects = await grok.dapi.projects.recent.list({pageSize: 5});
       if (projects.length > 0)
-        listDiv.appendChild(ui.divV(projects.map((p) => getProjectCard(p) ), {style:{overflowX:'scroll'}}));
+        contentDiv.appendChild(ui.divV(projects.map((p) => getProjectCard(p) ), {style: {overflowX: 'scroll'}}));
       else {
         this.caption = 'Demo projects';
         grok.dapi.projects
           .filter('#demo')
           .list({pageSize: 5})
-          .then((projects) => listDiv.appendChild(ui.divV(projects.map((p) => getDemoProjectCard(p)), {style: {overflowX: 'scroll'}})));
+          .then((projects) => contentDiv.appendChild(ui.divV(projects.map((p) => getDemoProjectCard(p)), {style: {overflowX: 'scroll'}})));
       }
       if (projects.length < 20) {
         const dropZone = div([
           ui.link('Open a file', () => grok.shell.openFileOpenDialog()),
-          ui.span([', or drop it here'])
+          ui.span([', or drop it here']),
         ], {classes: 'pp-drop-zone'});
-        listDiv.appendChild(dropZone);
+        contentDiv.appendChild(dropZone);
       }
-      return listDiv;
-    });
+      return contentDiv;
+    }));
     this.root.appendChild(projectList);
   }
 }
 
 function getProjectCard(p: DG.Project): HTMLElement {
-  let card = ui.cards.summary(
+  const card = ui.cards.summary(
     ui.image(p.pictureUrl, 70, 50, {target: () => {}}),
     [
-      ui.h2(ui.link(p.friendlyName, (_: any) => {}, undefined, null))
+      ui.h2(ui.link(p.friendlyName, (_: any) => {}, undefined, null)),
     ]);
   ui.bind(p, card, {contextMenu: true});
   return card;
 }
 
 function getDemoProjectCard(p: DG.Project): HTMLElement {
-  let card = ui.cards.summary(
+  const card = ui.cards.summary(
     ui.image(p.pictureUrl, 70, 50, {target: () => {}}),
     [
       ui.h2(ui.link(p.friendlyName, p, undefined, null)),
-      ui.div([p.createdOn], {style:{color:'var(--grey-4)'}})
+      ui.div([p.createdOn], {style: {color: 'var(--grey-4)'}}),
     ]);
   ui.bind(p, card);
   return card;
