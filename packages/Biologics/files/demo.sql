4f96fee36342af8293e281fd56f1cdabe3ade62c
drizhina <drizhinashvili@datagrok.ai>
2025-09-18 19:04:27 +0300

    Polytool: Rule summary tooltips

--- packages/Biologics/files/demo.sql
+++ packages/Biologics/files/demo.sql
@@ -2,6 +2,7 @@
 --name: assaysByOrganism
 --connection: Biologics:biologics
 --input: string organism {choices: query("SELECT distinct name FROM biologics.target_organisms")}
+--meta.searchPattern: "biologics assays for ${organism}"
 SELECT org.name AS organism, org.identifier as organism_identifier, at.name AS assay_type,
     ar.result_value, ar.units, seq.sequence AS antibody_sequence, seq.identifier as sequence_identifier,
     seq.name AS antibody_name, adc.name as adc_name, adc.identifier as adc_identifier, adc.glyph AS glyph,
@@ -15,3 +16,21 @@ LEFT JOIN biologics.sequences seq ON adc.antibody_id = seq.id
 LEFT JOIN biologics.drugs d ON adc.drug_id = d.id
 LEFT JOIN biologics.linkers l ON adc.linker_id = l.id
 WHERE org.name = @organism
+
+-- end
+
+--name: adcsLinkedToDrug
+--connection: Biologics:biologics
+--input: string drugID {choices: query("SELECT distinct identifier FROM biologics.drugs")}
+--meta.searchPattern: "adcs linked to ${drugID}"
+SELECT adc.identifier AS adc_identifier, adc.name AS adc_name, adc.glyph AS glyph,
+    seq.identifier as sequence_identifier, seq.name AS antibody_name, seq.sequence AS antibody_sequence,
+    l.identifier as linker_identifier, l.linker_type, l.linker_molecule_smiles, l.linker_sequence,
+    d.identifier as drug_identifier, d.name AS drug_name, d.smiles AS drug_smiles
+FROM biologics.adc adc
+JOIN biologics.sequences seq ON adc.antibody_id = seq.id
+JOIN biologics.drugs d ON adc.drug_id = d.id
+JOIN biologics.linkers l ON adc.linker_id = l.id
+WHERE d.identifier = @drugID;
+
+-- end
