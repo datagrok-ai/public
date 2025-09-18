4e14d5170bfa951bf5c435af2cad67df9f2dcc98
Dmytro Kovalyov <laykdimon278@gmail.com>
2025-09-18 18:48:07 +0300

    GROK-17845: Tutorials widget: Fixed each click opens additional app menu

--- packages/Tutorials/src/widget.ts
+++ packages/Tutorials/src/widget.ts
@@ -44,6 +44,12 @@ export class TutorialWidget extends DG.Widget {
           const dockRoot = ui.div([root,
             ui.panel([], {id: 'tutorial-child-node', style: {paddingTop: '10px'}}),
           ], 'tutorials-root');
+          const existingTutorials = document.querySelector('.tutorials-root') as HTMLElement;
+          if (existingTutorials) {
+            const existingDock = grok.shell.dockManager.findNode(existingTutorials);
+            if (existingDock)
+              grok.shell.dockManager.close(existingDock);
+          }
           grok.shell.dockManager.dock(dockRoot, DG.DOCK_TYPE.LEFT, null, 'Tutorials', 0.27);
         });
 
