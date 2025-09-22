d00f61082d35f0fdd83cae2664142a6fa1a61760
Maria Dolotova <maria.dolotova@softwarecountry.com>
2025-09-22 17:46:07 +0300

    RevvitySignals: Added more tests

--- packages/RevvitySignalsLink/src/tests/revvity-tests.ts
+++ packages/RevvitySignalsLink/src/tests/revvity-tests.ts
@@ -1,19 +1,19 @@
 import * as DG from 'datagrok-api/dg';
 import * as grok from 'datagrok-api/grok';
 
-import {awaitCheck, before, category, expect, test} from '@datagrok-libraries/utils/src/test';
+import { awaitCheck, before, category, expect, test } from '@datagrok-libraries/utils/src/test';
 //import { funcs } from '../package-api';
 import { openRevvityNode } from '../view-utils';
 import { Operators } from '@datagrok-libraries/utils/src/query-builder/query-builder';
 import { funcs } from '../package-api';
+import { MOL_COL_NAME } from '../constants';
 
-
-category('revvity signals', () => {
+category('revvity signals app', () => {
 
   before(async () => {
   });
-  
-  test('app tests', async () => {
+
+  test('app initial statistics', async () => {
     const view = await funcs.revvitySignalsLinkApp();
     //check that statistics view has been created
     await awaitCheck(() => view.root.getElementsByTagName('table') != null, 'Initial statistics hasn\'t been loaded', 30000);
@@ -28,17 +28,18 @@ category('revvity signals', () => {
     grok.shell.closeAll();
   });
 
-    test('open compounds|assets with pre-defined query', async () => {
-        const query = {
-            "logicalOperator": Operators.Logical.and,
-            "conditions": [
-                {
-                    "field": "createdAt",
-                    "operator": "before",
-                    "value": "2025-09-06T21:00:00.000Z"
-                }
-            ]
+  test('open compounds|assets with pre-defined query', async () => {
+
+    const query = {
+      "logicalOperator": Operators.Logical.and,
+      "conditions": [
+        {
+          "field": "createdAt",
+          "operator": "before",
+          "value": "2025-09-06T21:00:00.000Z"
         }
+      ]
+    }
     const node = grok.shell.browsePanel.mainTree.getOrCreateGroup('Apps').getOrCreateGroup('Chem').getOrCreateGroup('Revvity Signals');
     node.expanded = true;
     openRevvityNode(node, ['Compounds'], 'asset', 'Compounds', 'asset', query);
@@ -48,3 +49,63 @@ category('revvity signals', () => {
 });
 
 
+category('revvity signals functions', () => {
+
+  before(async () => {
+  });
+
+  test('searchEntities', async () => {
+    const query = {
+      "query": {
+        "$match": {
+          "field": "type",
+          "value": "batch",
+          "mode": "keyword"
+        }
+      }
+    };
+    const df = await funcs.searchEntities(JSON.stringify(query), '{}');
+    expect(df.rowCount > 0, true, 'Returned empty dataframe');
+  });
+
+  test('searchEntitiesWithStructures', async () => {
+    const query = {
+      "query": {
+        "$match": {
+          "field": "type",
+          "value": "asset",
+          "mode": "keyword"
+        }
+      }
+    };
+    const df = await funcs.searchEntitiesWithStructures(JSON.stringify(query), '{}');
+    expect(df.rowCount > 0, true, 'Returned empty dataframe');
+    await awaitCheck(() => !df.col(MOL_COL_NAME)!.isEmpty, 'Molecules column is not filling with values', 30000);
+  });
+
+  test('getLibraries', async () => {
+    const libs = JSON.parse(await funcs.getLibraries());
+    expect(libs.length > 0, true, 'Returned empty libraries list');
+  });
+
+  test('getUsers', async () => {
+    const users = JSON.parse(await funcs.getUsers());
+    expect(Object.keys(users).length > 0, true, 'Returned empty users list');
+  });
+
+  test('getTags', async () => {
+    const tags = JSON.parse(await funcs.getTags('batch', 'assetType:686ecf60e3c7095c954bd94f'));
+    expect(Object.keys(tags).length > 0, true, 'Returned empty tags list');
+  });
+
+  test('getTerms', async () => {
+    const terms = JSON.parse(await funcs.getTerms('materials.Batch Chemical Name', 'batch', 'assetType:686ecf60e3c7095c954bd94f', true));
+    expect(terms.length > 0, true, 'Returned empty terms list');
+  });
+
+  test('getTerms', async () => {
+    const widget = JSON.parse(await funcs.entityTreeWidget(DG.SemanticValue.fromValueType('batch:687a867714c8e27853238590', 'RevvitySignalsId')));
+    await awaitCheck(() => (widget as HTMLElement).querySelector('table') != null, 'Widget hasn\'t been created', 30000);
+  });
+
+});
