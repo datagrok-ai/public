1f857f23d5d7a755c0c9469fe55f90ce921c4292
Oleksandra Serhiienko <aleksandrasergienko12@gmail.com>
2025-10-01 13:26:49 +0300

    MolTrack: Add logic to set database host when running on localhost

--- packages/MolTrack/dockerfiles/docker-entrypoint.sh
+++ packages/MolTrack/dockerfiles/docker-entrypoint.sh
@@ -2,16 +2,25 @@
 set -e
 
 echo "Parsing DB environment variable..."
-
-echo $db
+echo "$db"
 
 if [ -n "$db" ]; then
-  # export DB_HOST=$(echo "$db" | jq -r '.parameters.server')
-  export DB_HOST="10.152.183.177"
-  export DB_USER=$(echo "$db" | jq -r '.credentials.parameters.login')
-  export DB_PASSWORD=$(echo "$db" | jq -r '.credentials.parameters.password')
-  export DB_NAME=$(echo "$db" | jq -r '.parameters.db')
-  export APP_PORT=18000
+  DB_USER=$(echo "$db" | jq -r '.credentials.parameters.login')
+  DB_PASSWORD=$(echo "$db" | jq -r '.credentials.parameters.password')
+  DB_NAME=$(echo "$db" | jq -r '.parameters.db')
+  APP_PORT=18000
+
+  DB_HOST_RAW=$(echo "$db" | jq -r '.parameters.server')
+  echo $DB_HOST_RAW
+
+  if [[ "$DB_HOST_RAW" == "localhost" ]]; then
+    DB_HOST='host.docker.internal'
+  else
+    DB_HOST="10.152.183.177"
+  fi
+
+  export DB_HOST DB_USER DB_PASSWORD DB_NAME APP_PORT
+
 else
   echo "Environment variable 'db' not set."
   exit 1
