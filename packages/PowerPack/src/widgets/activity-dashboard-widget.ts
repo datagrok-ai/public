047841309427def9f6cdd785177c9f5e36291912
Dmytro Kovalyov <laykdimon278@gmail.com>
2025-10-03 04:06:29 +0300

    GROK-18139: Activity dashboard: Made subwidget functions return `DG.Widget` instead of `HTMLElement[]`

--- packages/PowerPack/src/widgets/activity-dashboard-widget.ts
+++ packages/PowerPack/src/widgets/activity-dashboard-widget.ts
@@ -378,14 +378,13 @@ export class ActivityDashboardWidget extends DG.Widget {
     if (root.children.length === 0)
       root = await this.getNewUserInfoColumns();
 
-    const additionalFuncs = DG.Func.find({meta: {'isActivityWidget': 'true'}});
+    const additionalFuncs = DG.Func.find({meta: {'activityWidgetHeader': null}});
     for (const func of additionalFuncs) {
-      const elements: HTMLElement[] = await func.apply();
-      const list = ui.list(elements);
-      list.classList.add('power-pack-activity-widget-subwidget-list-content');
-      const listRoot = ui.divV([ui.h3(ui.span([ui.span([func.options['activityWidgetHeader'] ?? ''])]), 'power-pack-activity-widget-spotlight-column-header'),
-        list], 'power-pack-activity-widget-spotlight-column');
-      root.appendChild(listRoot);
+      const subWidget: DG.Widget = await func.apply();
+      subWidget.root.classList.add('power-pack-activity-widget-subwidget-list-content');
+      const rootToAppend = ui.divV([ui.h3(ui.span([ui.span([func.options['activityWidgetHeader'] ?? ''])]), 'power-pack-activity-widget-spotlight-column-header'),
+        subWidget.root], 'power-pack-activity-widget-spotlight-column');
+      root.appendChild(rootToAppend);
     }
 
     setTimeout(() => this.cleanLists(), 500);
