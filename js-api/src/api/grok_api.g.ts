41cdb42273a00ceab3ba76bef05ac591fecd9a9e
Maria Dolotova <maria.dolotova@softwarecountry.com>
2025-10-21 13:11:32 +0300

    JS-API: Set icon method expose

--- js-api/src/api/grok_api.g.ts
+++ js-api/src/api/grok_api.g.ts
@@ -102,6 +102,7 @@ export interface IDartApi {
   grok_View_ResetLayout(v: any): any;
   grok_View_CreateJsViewHost(v: any): any;
   grok_View_Get_Viewers(v: any): any;
+  grok_View_SetIcon(v: any, icon: any): any;
   grok_TableView(d: any, addToWorkspace: Bool): any;
   grok_TableView_Get_SyncCurrentObject(tv: any): any;
   grok_TableView_Set_SyncCurrentObject(tv: any, x: Bool): any;
