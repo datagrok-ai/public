39d9b079dab48c7d436ffb48cdd8f0230f0e4bb9
Viktor Makarichev <112491505+vmakarichev@users.noreply.github.com>
2025-10-30 13:00:16 +0300

    GROK-18969: Compute: Fitting (#3493)
    * GROK-18969: Compute: Fitting: Seeded generator
    
    Implemented seeded pseudorandom generator
    
    * GROK-18969: Compute: Fitting: Seed UI
    
    Implemented UI for seeded pseudorandom generator
    
    * GROK-18969: Compute: Fitting: Refactoring
    
    Refactored the code
    
    * GROK-18969: Compute: Fitting: Filtering inputs upd
    
    Updated the filtering inputs
    
    * GROK-18969: Compute: Fitting: Stop at first
    
    Implemented the stop at first feature
    
    * GROK-18969: Compute: Fitting: Stop at first
    
    Refactored the code
    
    * GROK-18969: Compute: Fitting: Bumped version
    
    Bumped the library version
    
    * GROK-18969: Compute: Fitting: Tooltips upd
    
    Updated tooltips of the fitting view
    
    * GROK-18969: Compute: Fitting: Float64
    
    Implemented the use of Float64Array instead of Float32Array
    
    * GROK-18995: Fitting: UI upd
    
    Updated the samples inputs
    
    * GROK-18995: Fitting: Stop after
    
    Implemented the stop after feature
    
    * GROK-18990: Fitting: Fixed icons behavior
    
    Fixed the behavior of table input icons
    
    * GROK-18968: Fitting: Diff Studio: Early stopping
    
    Implemented the early stopping feature for Diff Studio models
    
    * GROK-18968: Fitting: Diff Studio: Stop after
    
    Implemented the stop after feature for Diff Studio models
    
    * Compute: Fitting initial points restrictions with formulas (#3499)
    
    Formula parser
    Fitting UI validation formulas handling
    Optimizer throttling improved
    Optimizer package level api and datastructures
    Separate loss functions
    Conversion from ui to optimizer arguments datastructures
    Default values overrides improved
    New sampler with formulas for values and bounds
    Fixed early stopping to gather only relevant extremums
    
    * Fitting: Tweak formula input naming
    
    * Fitting: Fix fitting api
    
    * Fitting: Fix choice and optional inputs
    
    * Fitting: cache cost funccall, dont apply early stopping to an individual result
    
    * Fitting: Use textarea for formulas, show var names in tooltips
    
    * Fitting: Proper constraint optimization
    
    * Fitting: Fine granularity throttling
    
    * Fitting: Tweak name
    
    * Fitting: Allow canceling of fitting
    
    * Fitting: Fix min and max from annotations
    
    * Tutorials: Update for new fitting ui
    
    * Fitting: Fix BoundsChecker
    
    * Fitting: Use original inputs order for the view
    
    * Fitting: Update validator
    
    * Compute: Optimize FuncCalls
    
    * Compute2: FuncCall started fix
    
    * GROK: 18969: Compute: Fitting: Eslint
    
    Fixed eslint
    
    * GROK: 18969: Fitting: Header removal
    
    Removed the redundant header
    
    * Fitting: Fix styles
    
    * Fitting: Restore showing all settings
    
    * Fitting: Fix form alignment
    
    * Tutorials: Update fitting
    
    * Compute: Typo fix
    
    ---------
    
    Co-authored-by: andrey-sharapov-sc <129750144+andrey-sharapov-sc@users.noreply.github.com>
    Co-authored-by: Andrey Sharapov <andrey.sharapov@softwarecountry.com>

---
+++ libraries/compute-utils/function-views/src/fitting/cost-functions.ts
@@ -0,0 +1,90 @@
+import * as grok from 'datagrok-api/grok';
+import * as ui from 'datagrok-api/ui';
+import * as DG from 'datagrok-api/dg';
+import {LOSS, TIMEOUT} from './constants';
+import {getErrors, getInputsData, makeGetCalledFuncCall} from './fitting-utils';
+import {OutputTargetItem, throttle, ValueBoundsData} from './optimizer-misc';
+import {makeBoundsChecker} from './optimizer-sampler';
+
+
+export function makeConstFunction(
+  type: LOSS,
+  func: DG.Func,
+  bounds: Record<string, ValueBoundsData>,
+  outputTargets: OutputTargetItem[],
+) {
+  const {variedInputNames, fixedInputs} = getInputsData(bounds);
+  if (type === LOSS.MAD)
+    return makeMadCostFunc(func, bounds, fixedInputs, variedInputNames, outputTargets);
+  if (type === LOSS.RMSE)
+    return makeRmseCostFunc(func, bounds, fixedInputs, variedInputNames, outputTargets);
+  throw new Error(`Unknown type ${type}`);
+}
+
+function makeMadCostFunc(func: DG.Func, bounds: Record<string, ValueBoundsData>,
+  inputs: Record<string, any>, variedInputNames: string[], outputTargets: OutputTargetItem[]) {
+  /** Maximum absolute deviation (MAD) cost function */
+  const getCalledFuncCall = makeGetCalledFuncCall(func, inputs, variedInputNames);
+  const boundsChecker = makeBoundsChecker(bounds, variedInputNames);
+  let lastWorkStartTs: number | undefined = undefined;
+
+  const madCostFunc = async (x: Float64Array): Promise<number | undefined> => {
+    if (!boundsChecker(x))
+      return;
+
+    const calledFuncCall = await getCalledFuncCall(x);
+    lastWorkStartTs = await throttle(TIMEOUT.MS_TO_SLEEP * 19, TIMEOUT.MS_TO_SLEEP, lastWorkStartTs);
+
+    let mad = 0;
+
+    outputTargets.forEach((output) => {
+      if (output.type !== DG.TYPE.DATA_FRAME)
+        mad = Math.max(mad, Math.abs(output.target as number - calledFuncCall.getParamValue(output.propName)));
+      else {
+        const df = output.target as DG.DataFrame;
+        getErrors(df.col(output.argName), output.cols, calledFuncCall.getParamValue(output.propName), false)
+          .forEach((err) => mad = Math.max(mad, Math.abs(err)));
+      }
+    });
+
+    return mad;
+  };
+  return madCostFunc;
+}
+
+function makeRmseCostFunc(func: DG.Func, bounds: Record<string, ValueBoundsData>,
+  inputs: Record<string, any>, variedInputNames: string[], outputTargets: OutputTargetItem[]) {
+  /** Root mean sqaure error (RMSE) cost function */
+  const getCalledFuncCall = makeGetCalledFuncCall(func, inputs, variedInputNames);
+  const boundsChecker = makeBoundsChecker(bounds, variedInputNames);
+  let lastWorkStartTs: number | undefined = undefined;
+  const rmseCostFunc = async (x: Float64Array): Promise<number| undefined> => {
+    if (!boundsChecker(x))
+      return;
+
+    const calledFuncCall = await getCalledFuncCall(x);
+    lastWorkStartTs = await throttle(TIMEOUT.MS_TO_SLEEP * 19, TIMEOUT.MS_TO_SLEEP, lastWorkStartTs);
+
+    let sumOfSquaredErrors = 0;
+    let outputsCount = 0;
+    let cur = 0;
+
+    outputTargets.forEach((output) => {
+      if (output.type !== DG.TYPE.DATA_FRAME) {
+        cur = output.target as number;
+        sumOfSquaredErrors += ((cur - calledFuncCall.getParamValue(output.propName)) / (cur !== 0 ? cur : 1)) ** 2;
+        ++outputsCount;
+      } else {
+        const df = output.target;
+        getErrors(df.col(output.argName), output.cols, calledFuncCall.getParamValue(output.propName), true)
+          .forEach((err) => {
+            sumOfSquaredErrors += err ** 2;
+            ++outputsCount;
+          });
+      }
+    });
+
+    return Math.sqrt(sumOfSquaredErrors / outputsCount);
+  };
+  return rmseCostFunc;
+}
--- libraries/compute-utils/function-views/src/fitting/optimizer-sampler.ts
+++ libraries/compute-utils/function-views/src/fitting/optimizer-sampler.ts
@@ -1,29 +1,136 @@
-function sampleUniform(samplesCount: number, top: number, bottom: number): number[] {
+import {runFormula} from './formulas-resolver';
+import {BoundFormula, ChangingValue, ConstValue, ValueBoundsData} from './optimizer-misc';
+
+function sampleUniform(samplesCount: number, top: number, bottom: number,
+  rand: () => number = Math.random): number[] {
   const scale = top - bottom;
   const sample = new Array<number>(samplesCount);
 
   for (let i = 0; i < samplesCount; i ++) {
-    const r = Math.random();
+    const r =rand();
     sample[i] = bottom + r*scale;
   }
 
   return sample;
 }
 
-export function sampleParams(samplesCount: number, top: Float32Array, bottom: Float32Array): Float32Array[] {
+export function sampleParams(samplesCount: number, top: Float64Array, bottom: Float64Array,
+  rand: () => number = Math.random): Float64Array[] {
   const dim = top.length;
-  const params = new Array<Float32Array>(samplesCount);
+  const params = new Array<Float64Array>(samplesCount);
   for (let i = 0; i < samplesCount; i ++)
-    params[i] = new Float32Array(dim);
+    params[i] = new Float64Array(dim);
 
   for (let i = 0; i < dim; i ++) {
     if (top[i] === bottom[i]) {
       for (let j = 0; j < samplesCount; j ++)
         params[j][i] = top[i];
     } else {
-      const paramVariations = sampleUniform(samplesCount, top[i], bottom[i]);
+      const paramVariations = sampleUniform(samplesCount, top[i], bottom[i], rand);
       for (let j = 0; j < samplesCount; j ++)
         params[j][i] = paramVariations[j];
+    }
+  }
+
+  return params;
+}
+
+type AccData = {
+  constValues: [string, ConstValue][],
+  nonFormulaBounds: [string, number, ChangingValue][],
+  formulaBounds: [string, number, ChangingValue][],
+  boundsIdx: number,
+};
+
+export type SamplerInputsConfig = {
+  inputs: Record<string, ValueBoundsData>,
+}
+
+function getAccData(inputs: Record<string, ValueBoundsData>) {
+  // partition inputs based on the type: fixed, numeric or formula bounds,
+  // keeping original order index of varried inputs
+  const data = Object.entries(inputs).reduce((acc, [name, val]) => {
+    if (val.type === 'const')
+      acc.constValues.push([name, val]);
+    else if (val.top.type === 'value' && val.bottom.type === 'value') {
+      acc.nonFormulaBounds.push([name, acc.boundsIdx, val]);
+      acc.boundsIdx++;
+    } else {
+      acc.formulaBounds.push([name, acc.boundsIdx, val]);
+      acc.boundsIdx++;
+    }
+    return acc;
+  }, {constValues: [], nonFormulaBounds: [], formulaBounds: [], boundsIdx: 0} as AccData);
+  return data;
+}
+
+function getFixedContext(constValues: [string, ConstValue][]) {
+  const contextFixed: Record<string, any> = {};
+  // add fixes inputs (can be any type) to formula context
+  for (const [name, data] of constValues)
+    contextFixed[name] = data.value;
+
+  return contextFixed;
+}
+
+function evalBoundFormula(bound: BoundFormula, context: Record<string, any>) {
+  return runFormula(bound.formula, context);
+}
+
+export function makeBoundsChecker(inputs: Record<string, ValueBoundsData>, variedInputNames: string[]) {
+  // indexing by name in point
+  const variedNameToPosition = new Map(variedInputNames.map((name, pos) => [name, pos]));
+  const {constValues, nonFormulaBounds, formulaBounds} = getAccData(inputs);
+  const contextFixed = getFixedContext(constValues);
+
+  return function isInsideBounds(point: Float64Array) {
+    const context = {...contextFixed};
+    for (const [name, , bound] of [...nonFormulaBounds, ...formulaBounds]) {
+      const pos = variedNameToPosition.get(name)!;
+      const val = point[pos];
+      const min = bound.bottom.type === 'value' ? bound.bottom.value : evalBoundFormula(bound.bottom, context);
+      const max = bound.top.type === 'value' ? bound.top.value : evalBoundFormula(bound.top, context);
+      if (max == null || min == null || val == null || min > max || val < min || val > max)
+        return false;
+      context[name] = val;
+    }
+    return true;
+  };
+}
+
+export function sampleParamsWithFormulaBounds(
+  samplesCount: number,
+  inputs: Record<string, ValueBoundsData>,
+  rand: () => number = Math.random,
+): Float64Array[] {
+  const {constValues, nonFormulaBounds, formulaBounds} = getAccData(inputs);
+
+  const variedInputsCount = nonFormulaBounds.length + formulaBounds.length;
+
+  const params = new Array<Float64Array>(samplesCount).fill(null as any).map(() => new Float64Array(variedInputsCount));
+
+  const contextFixed = getFixedContext(constValues);
+
+  // iteration over samples
+  for (let nsample = 0; nsample < samplesCount; nsample++) {
+    // reset context for each sample to fixed inputs only
+    const context = {...contextFixed};
+    // iteration over a single sample point varied dimensions
+    // non-formula inputs first, so formulas can refer to already picked values
+    for (const [name, boundsIdx, bound] of [...nonFormulaBounds, ...formulaBounds]) {
+      const min = bound.bottom.type === 'value' ? bound.bottom.value : evalBoundFormula(bound.bottom, context);
+      const max = bound.top.type === 'value' ? bound.top.value : evalBoundFormula(bound.top, context);
+      if (min == null || max == null || min > max) {
+        const msg = `Sampling staring point failed for input ${name}, min: ${min}, max: ${max}`;
+        console.error(msg);
+        throw new Error(msg);
+      }
+      // pick value and add to context
+      const r = rand();
+      const scale = max - min;
+      const val = min + r*scale;
+      params[nsample][boundsIdx] = val;
+      context[name] = val;
     }
   }
 
