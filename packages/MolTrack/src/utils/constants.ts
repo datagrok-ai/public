94e7bdf7c7c63d1113d268aadcd476737c287125
Oleksandra Serhiienko <aleksandrasergienko12@gmail.com>
2025-11-04 11:12:35 +0200

    MolTrack: Fix nullable type

--- packages/MolTrack/src/utils/constants.ts
+++ packages/MolTrack/src/utils/constants.ts
@@ -46,7 +46,7 @@ export const scopeToUrl: { [key: string]: string } = {
 };
 
 export type MolTrackProp = {
-  nullable: any;
+  nullable: boolean;
   name: string;
   value_type: string;
   entity_type?: string;
