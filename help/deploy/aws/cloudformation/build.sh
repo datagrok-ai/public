ea7e55be427df4175d1da51b72ddb38306c46796
Anatol <anatol1988@gmail.com>
2025-11-27 18:03:13 +0100

    Add formatting to build script

--- help/deploy/aws/cloudformation/build.sh
+++ help/deploy/aws/cloudformation/build.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+rain fmt --write ./templates/*.yml
+
 ./templates/cf_template_merge.py -i templates/create_network.template.yml -i templates/fargate.template.yml -i templates/r53.template.yml -i templates/marketplace.template.yml -i templates/versions.template.yml -o network-fargate-r53-marketplace.yml
 ./templates/cf_template_merge.py -i templates/create_network.template.yml -i templates/fargate.template.yml -i templates/dns.template.yml -i templates/marketplace.template.yml -i templates/versions.template.yml -o network-fargate-dns-marketplace.yml
 ./templates/cf_template_merge.py -i templates/choose_vpc.template.yml -i templates/fargate.template.yml -i templates/dns.template.yml -i templates/marketplace.template.yml -i templates/versions.template.yml -o vpc-fargate-dns-marketplace.yml
@@ -8,3 +10,5 @@
 ./templates/cf_template_merge.py -i templates/choose_vpc.template.yml -i templates/fargate.template.yml -i templates/r53.template.yml -i templates/basic.template.yml -i templates/versions.template.yml -o vpc-fargate-r53-basic.yml
 ./templates/cf_template_merge.py -i templates/create_network.template.yml -i templates/fargate.template.yml -i templates/dns.template.yml -i templates/basic.template.yml -i templates/versions.template.yml -o network-fargate-dns-basic.yml
 ./templates/cf_template_merge.py -i templates/choose_vpc.template.yml -i templates/fargate.template.yml -i templates/dns.template.yml -i templates/basic.template.yml -i templates/versions.template.yml -o vpc-fargate-dns-basic.yml
+
+rain fmt --write *.yml
