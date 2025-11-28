0307dad3aa2b383cda9322e109b9e7b01770a339
Andrew Golovko <agolovko@datagrok.ai>
2025-11-28 10:41:59 +0200

    Grok api: Added viewport getter/seetter for any canvas mixin instance, added it to box plot

--- js-api/src/api/grok_api.g.ts
+++ js-api/src/api/grok_api.g.ts
@@ -325,8 +325,6 @@ export interface IDartApi {
   grok_ScatterPlotViewer_Get_ViewBox(s: any): any;
   grok_ScatterPlotViewer_Get_XAxisBox(s: any): any;
   grok_ScatterPlotViewer_Get_YAxisBox(s: any): any;
-  grok_ScatterPlotViewer_Get_Viewport(s: any): any;
-  grok_ScatterPlotViewer_Set_Viewport(s: any, x: Num, y: Num, width: Num, height: Num): any;
   grok_ScatterPlotViewer_PointToScreen(s: any, index: Num): any;
   grok_ScatterPlotViewer_WorldToScreen(s: any, x: Num, y: Num): any;
   grok_ScatterPlotViewer_ScreenToWorld(s: any, x: Num, y: Num): any;
@@ -368,6 +366,8 @@ export interface IDartApi {
   grok_FormulaLineHelper_GetMeta(formulaItem: any): any;
   grok_FormulaLineHelper_GetMetaByFormula(formula: String, type: String): any;
   grok_CanvasViewer_RunPostponedComputations(v: any): any;
+  grok_CanvasViewer_Get_Viewport(s: any): any;
+  grok_CanvasViewer_Set_Viewport(s: any, x: Num, y: Num, width: Num, height: Num): any;
   grok_Widget_RegisterCleanup(element: any, cleanup: any): any;
   grok_Widget_Get_Root(widget: any): any;
   grok_Widget_Get_Type(widget: any): any;
@@ -1720,10 +1720,10 @@ export interface IDartApi {
   grok_FuncOptions_Create(): any;
   grok_FuncParamOptions_Create(): any;
 
-  // Generated from ../d4\lib\src\common\common.api.g.dart
+  // Generated from ../d4/lib/src/common/common.api.g.dart
   grok_UsageType_Create(): any;
 
-  // Generated from ../d4\lib\src\grid\grid.api.g.dart
+  // Generated from ../d4/lib/src/grid/grid.api.g.dart
   grok_GridCellStyle_Create(): any;
   grok_GridCellStyle_Get_defaultStyle(): any;
   grok_GridCellStyle_Set_defaultStyle(v: any): any;
@@ -1773,7 +1773,7 @@ export interface IDartApi {
   grok_GridCellStyle_Set_choices(x: any, v: any): any;
   grok_renderMultipleHistograms(g: any, bounds: any, histograms: any, categoryColumn: any, colors: any, tension: Num, normalize: Bool, markerSize: Num, fill: Bool, minBin: Num, maxBin: Num, localMaximum: Bool, highlightedHistogram: Num): any;
 
-  // Generated from ../d4\lib\src\viewer_base\viewer_base.api.g.dart
+  // Generated from ../d4/lib/src/viewer_base/viewer_base.api.g.dart
   grok_ViewerEvent_Create(): any;
   grok_ViewerEvent_Get_viewer(x: any): any;
   grok_ViewerEvent_Set_viewer(x: any, v: any): any;
@@ -1789,6 +1789,6 @@ export interface IDartApi {
   grok_ViewerEvent_Set_mouseEvent(x: any, v: any): any;
   grok_ViewerEvent_Get_bitset(x: any): any;
 
-  // Generated from ../d4\lib\src\widgets\widgets.api.g.dart
+  // Generated from ../d4/lib/src/widgets/widgets.api.g.dart
   grok_InputType_Create(): any;
 }
