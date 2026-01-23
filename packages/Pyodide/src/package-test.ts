abf6fd50a5ae617739844880cc956b1225dd2bc6
PavloPolovyi <pasha32662@gmail.com>
2026-01-23 18:21:25 +0100

    Pyodide: Migrated tests to new library

--- packages/Pyodide/src/package-test.ts
+++ packages/Pyodide/src/package-test.ts
@@ -1,10 +1,9 @@
 import * as DG from "datagrok-api/dg";
 import * as grok from 'datagrok-api/grok';
-import {runTests, tests, TestContext, initAutoTests as initTests } from '@datagrok-libraries/utils/src/test';
+import {runTests, tests, TestContext, initAutoTests as initTests, expect, expectTable as _expectTable } from '@datagrok-libraries/test/src/test';
 
 import './tests/funccall';
 
-import {expect, expectTable as _expectTable} from '@datagrok-libraries/utils/src/test';
 export let _package = new DG.Package();
 export { tests };
 import dayjs from "dayjs";
