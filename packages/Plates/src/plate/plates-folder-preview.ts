8e35f4c04d2c78d5d1243e1b89d0e6174eb67679
drizhina <drizhinashvili@datagrok.ai>
2026-02-11 15:08:24 +0200

    Plates: Fix Preview and basic functionality

--- packages/Plates/src/plate/plates-folder-preview.ts
+++ packages/Plates/src/plate/plates-folder-preview.ts
@@ -10,7 +10,7 @@ import {_package} from '../package';
 import {DrcAnalysis} from './analyses/drc/drc-analysis';
 import {AnalysisManager} from './analyses/analysis-manager';
 import {PlateWidget} from './plate-widget/plate-widget';
-// import {PlateWidget} from './plate-widget';
+import {autoDetectDrcMappings} from './analyses/drc/utils';
 
 
 export async function getPlatesFolderPreview(files: DG.FileInfo[]): Promise<DG.Widget | DG.ViewBase | undefined> {
@@ -23,13 +23,16 @@ export async function getPlatesFolderPreview(files: DG.FileInfo[]): Promise<DG.W
     const drcAnalysis = await AnalysisManager.instance.getAnalysis('DRC') as DrcAnalysis;
     if (drcAnalysis) {
       const plateWidget = PlateWidget.fromPlate(plate);
-      const mappings = new Map<string, string>([
-
-      ]);
-      const analysisView = drcAnalysis.createView(plate, plateWidget, mappings, () => {}, () => {});
-      plateWidget.addAnalysisTab('DRC', analysisView);
+      const mappings = autoDetectDrcMappings(plate);
+      const analysisView = drcAnalysis.createView(
+        plate, plateWidget, mappings,
+        (target, source) => mappings.set(target, source),
+        (target) => mappings.delete(target)
+      );
+      plateWidget.addAnalysisTab('DRC', analysisView, false);
       csvView = plateWidget;
       plate.data.name = `${csvFiles.map((file) => file.name.slice(0, file.name.length - 4)).join('_')}.csv`;
+      csvView.root.classList.add('assay-plates-folder-preview');
       if (csvFiles.length === files.length)
         return csvView;
     }
@@ -42,33 +45,46 @@ export async function getPlatesFolderPreview(files: DG.FileInfo[]): Promise<DG.W
     return undefined;
   else if (xlsxFiles.length == 0 && csvView)
     return csvView;
-  for (const xlsxFile of xlsxFiles) {
-    try {
-      const plate = await Plate.fromExcelFile(xlsxFile);
-      const drcAnalysis = await AnalysisManager.instance.getAnalysis('DRC') as DrcAnalysis;
-      if (drcAnalysis) {
-        const plateWidget = PlateWidget.fromPlate(plate);
-        const mappings = new Map<string, string>([]);
-        const analysisView = drcAnalysis.createView(plate, plateWidget, mappings, () => {}, () => {});
-        plateWidget.addAnalysisTab('DRC', analysisView);
+  let first = true;
+  setTimeout(async () => {
+    for (const xlsxFile of xlsxFiles) {
+      try {
+        const plate = await Plate.fromExcelFile(xlsxFile);
+        const drcAnalysis = await AnalysisManager.instance.getAnalysis('DRC') as DrcAnalysis;
+        if (drcAnalysis) {
+          const paneName = xlsxFile.name.substring(0, xlsxFile.name.length - 5);
 
-        const v = DG.View.fromRoot(plateWidget.root);
-        v.name = xlsxFile.name.substring(0, xlsxFile.name.length - 5);
-        multiView.addView(v.name, () => v, true);
+          multiView.addView(paneName, () => {
+            const plateWidget = PlateWidget.fromPlate(plate);
+            const mappings = autoDetectDrcMappings(plate);
+            const analysisView = drcAnalysis.createView(
+              plate, plateWidget, mappings,
+              (target, source) => mappings.set(target, source),
+              (target) => mappings.delete(target)
+            );
+            plateWidget.addAnalysisTab('DRC', analysisView, false);
+
+            const v = DG.View.fromRoot(plateWidget.root);
+            v.name = paneName;
+            return v;
+          }, first);
+          first = false;
+        }
+      } catch (e) {
+        _package.logger.error(e);
       }
-    } catch (e) {
-      _package.logger.error(e);
     }
-  }
-  setTimeout(() => {
-    const header: HTMLElement | null = multiView.root.querySelector('.d4-tab-header-stripe');
-    if (!header)
-      return;
-    header.style.overflow = 'scroll';
+    setTimeout(() => {
+      const header: HTMLElement | null = multiView.root.querySelector('.d4-tab-header-stripe');
+      if (!header)
+        return;
+      header.style.overflow = 'scroll';
 
-    const headerItems = header.querySelectorAll('.d4-tab-header');
-    headerItems?.forEach((el) => (el as HTMLElement).style.whiteSpace = 'nowrap');
-  }, 300);
+      const headerItems = header.querySelectorAll('.d4-tab-header');
+      headerItems?.forEach((el) => (el as HTMLElement).style.whiteSpace = 'nowrap');
+    }, 300);
+  }, 200);
+  multiView.root.classList.add('assay-plates-folder-preview');
 
   return multiView;
 }
