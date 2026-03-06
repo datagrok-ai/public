1817c78093a0bc25dfae75a5dd70856ed3a30d9d
drizhina <drizhinashvili@datagrok.ai>
2026-03-06 18:18:09 +0200

    Bio: Correct Sequence input creation

--- libraries/bio/src/utils/sequence-column-input.ts
+++ libraries/bio/src/utils/sequence-column-input.ts
@@ -8,9 +8,20 @@ export interface ISequenceColumnInput {
     get inputBase(): DG.InputBase<DG.Column | null> | undefined | null;
 }
 
-export async function createSequenceColumnInput(name: string, options: ui.input.IColumnInputInitOptions<DG.Column>): Promise<ISequenceColumnInput> {
+class SequenceColumnInputBase implements ISequenceColumnInput {
+    private readonly _inputBase: DG.InputBase<DG.Column | null>;
+    constructor(inp: DG.InputBase<DG.Column | null>) {
+        this._inputBase = inp;
+    }
+    get root(): HTMLElement { return this._inputBase.root; }
+    get value(): DG.Column | null { return this._inputBase.value; }
+    set value(col: DG.Column | null) { this._inputBase.value = col; }
+    get inputBase(): DG.InputBase<DG.Column | null> { return this._inputBase; }
+}
+
+export async function createSequenceColumnInput(name: string, options?: ui.input.IColumnInputInitOptions<DG.Column>): Promise<ISequenceColumnInput> {
     const func = DG.Func.find({package: 'Bio', name: 'sequenceColumnInput'})[0];
     if (!func)
-        return ui.input.column(name, options) as unknown as ISequenceColumnInput;
+        return new SequenceColumnInputBase(ui.input.column(name, options));
     return await func.apply({name: name, options: options}) as ISequenceColumnInput;
 }
