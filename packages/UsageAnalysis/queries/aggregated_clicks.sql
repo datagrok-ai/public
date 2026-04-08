f36e654466974684588357e0dde4098273bfe6ee
Dmytro Kovalyov <laykdimon278@gmail.com>
2026-04-08 23:54:49 +0300

    Click tracking: Click analysis: Improved UI, added count filter, integrated with UA toolbox, added readme

--- packages/UsageAnalysis/queries/aggregated_clicks.sql
+++ packages/UsageAnalysis/queries/aggregated_clicks.sql
@@ -1,15 +1,16 @@
 --name: GetAggregatedClicks
 --friendlyName: Aggregated Clicks
 --connection: System:Datagrok
---meta.cache: true
---meta.cache.invalidateOn: 0 0 * * *
+--input: string date { pattern: datetime }
 
 SELECT
     et.friendly_name as event_type,
     e.description,
-    COUNT(*) as count
+    COUNT(*)::int as count
 FROM events e
 JOIN event_types et ON e.event_type_id = et.id
-WHERE et.source = 'usage' AND et.friendly_name IN ('click', 'menu click', 'dialog show', 'dialog close', 'dialog ok', 'input', 'command', 'navigate')
+WHERE et.source = 'usage'
+  AND et.friendly_name IN ('click', 'menu click', 'dialog show', 'dialog close', 'dialog ok', 'input', 'command', 'navigate')
+  AND @date(e.event_time)
 GROUP BY et.friendly_name, e.description
 ORDER BY count DESC
