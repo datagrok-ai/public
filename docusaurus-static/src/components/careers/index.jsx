878ab03b355086b4378565d11f0714b1b859a220
skalkin <skalkin@gmail.com>
2026-04-09 11:46:19 -0400

    Careers: Standalone position pages, updated header text, removed title

--- docusaurus-static/src/components/careers/index.jsx
+++ docusaurus-static/src/components/careers/index.jsx
@@ -4,11 +4,16 @@ import React from 'react';
 export function LayoutHeader ({meta}){
     return(
         <div className='row'>
-                <div className="col-lg-12 mb-12 px-5 pb-3">
-                    <div className='display-5 text-secondary'>{meta.title}</div>
-                    <h4 className='mt-2 pb-2'>{meta.description}</h4>
+            <div className="col-lg-12 mb-12 px-5 pb-3">
+                <h4 className='mt-2 pb-2'>{meta.description}</h4>
+            </div>
+            {meta.footer && (
+                <div className="col-lg-12 px-5 pb-3">
+                    <p className="text-secondary">
+                        {meta.footer} <a href={'mailto:' + meta.footerEmail}>Apply {meta.footerEmail}</a>
+                    </p>
                 </div>
+            )}
         </div>
-        
     );
 }
