ee625e5795f80db10039c6035af2ca4f618afbfa
skalkin <skalkin@gmail.com>
2026-04-09 11:15:31 -0400

    Team: Added pharma stats section, removed banner, updated numbers

--- docusaurus-static/src/components/team/index.jsx
+++ docusaurus-static/src/components/team/index.jsx
@@ -2,20 +2,7 @@ import styles from './styles.module.css';
 import React from 'react';
 
 export function LayoutHeader ({children, meta}){
-    return(
-        <section className="container-fluid bg-light py-5">
-                <div className="container">
-                    <div className="row">
-                        <div className="row py-5 my-3 align-items-center">
-                            <div className="col-lg-12 mb-12 px-5 text-center">
-                                <p className="display-5 text-secondary">{meta.description}</p>
-                                <a href="https://datagrok.ai/company/careers" className="mt-3 btn btn-primary px-">JOIN OUR TEAM</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-        </section>
-    );
+    return null;
 }
 export function LayoutBody ({children, meta}){
     return(
