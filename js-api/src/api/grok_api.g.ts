7c85afa1fdadc4528c031599466ba38290ffdacc
Jenkins <jenkins@datagrok.ai>
2026-08-13 12:39:47 +0200

    CI: nightly codegen sync - regenerate grok_api.g.ts

--- js-api/src/api/grok_api.g.ts
+++ js-api/src/api/grok_api.g.ts
@@ -2068,6 +2068,8 @@ export interface IDartApi {
   grok_DockerImage_Get_updatedBy(x: any): any;
   grok_DockerImage_Get_logs(x: any): any;
   grok_DockerImage_Set_logs(x: any, v: String): any;
+  grok_DockerImage_Get_imageRef(x: any): any;
+  grok_DockerImage_Set_imageRef(x: any, v: String): any;
   grok_DockerImage_Get_completed(x: any): any;
   grok_DockerImage_Get_iconStatus(x: any): any;
   grok_DockerImage_Get_dockerFullName(x: any): any;
@@ -2093,8 +2095,55 @@ export interface IDartApi {
   grok_ViewerEvent_Set_mouseEvent(x: any, v: any): any;
   grok_ViewerEvent_Get_bitset(x: any): any;
 
-  // Generated from ../d4/lib/src/common/common.api.g.dart
-  grok_UsageType_Create(): any;
+  // Generated from ../d4/lib/src/grid/grid.api.g.dart
+  grok_GridCellStyle_Create(): any;
+  grok_GridCellStyle_Get_defaultStyle(): any;
+  grok_GridCellStyle_Set_defaultStyle(v: any): any;
+  grok_GridCellStyle_Get_textStyle(): any;
+  grok_GridCellStyle_Set_textStyle(v: any): any;
+  grok_GridCellStyle_Get_numberStyle(): any;
+  grok_GridCellStyle_Set_numberStyle(v: any): any;
+  grok_GridCellStyle_Get_styles(): any;
+  grok_GridCellStyle_Set_styles(v: any): any;
+  grok_GridCellStyle_Get_font(x: any): any;
+  grok_GridCellStyle_Set_font(x: any, v: String): any;
+  grok_GridCellStyle_Get_horzAlign(x: any): any;
+  grok_GridCellStyle_Set_horzAlign(x: any, v: String): any;
+  grok_GridCellStyle_Get_vertAlign(x: any): any;
+  grok_GridCellStyle_Set_vertAlign(x: any, v: String): any;
+  grok_GridCellStyle_Get_tooltip(x: any): any;
+  grok_GridCellStyle_Set_tooltip(x: any, v: String): any;
+  grok_GridCellStyle_Get_cursor(x: any): any;
+  grok_GridCellStyle_Set_cursor(x: any, v: String): any;
+  grok_GridCellStyle_Get_textWrap(x: any): any;
+  grok_GridCellStyle_Set_textWrap(x: any, v: String): any;
+  grok_GridCellStyle_Get_marker(x: any): any;
+  grok_GridCellStyle_Set_marker(x: any, v: String): any;
+  grok_GridCellStyle_Get_textColor(x: any): any;
+  grok_GridCellStyle_Set_textColor(x: any, v: Num): any;
+  grok_GridCellStyle_Get_backColor(x: any): any;
+  grok_GridCellStyle_Set_backColor(x: any, v: Num): any;
+  grok_GridCellStyle_Get_marginLeft(x: any): any;
+  grok_GridCellStyle_Set_marginLeft(x: any, v: Num): any;
+  grok_GridCellStyle_Get_marginRight(x: any): any;
+  grok_GridCellStyle_Set_marginRight(x: any, v: Num): any;
+  grok_GridCellStyle_Get_marginTop(x: any): any;
+  grok_GridCellStyle_Set_marginTop(x: any, v: Num): any;
+  grok_GridCellStyle_Get_marginBottom(x: any): any;
+  grok_GridCellStyle_Set_marginBottom(x: any, v: Num): any;
+  grok_GridCellStyle_Get_textVertical(x: any): any;
+  grok_GridCellStyle_Set_textVertical(x: any, v: Bool): any;
+  grok_GridCellStyle_Get_imageScale(x: any): any;
+  grok_GridCellStyle_Set_imageScale(x: any, v: Num): any;
+  grok_GridCellStyle_Get_opacity(x: any): any;
+  grok_GridCellStyle_Set_opacity(x: any, v: Num): any;
+  grok_GridCellStyle_Get_clip(x: any): any;
+  grok_GridCellStyle_Set_clip(x: any, v: Bool): any;
+  grok_GridCellStyle_Get_element(x: any): any;
+  grok_GridCellStyle_Set_element(x: any, v: any): any;
+  grok_GridCellStyle_Get_choices(x: any): any;
+  grok_GridCellStyle_Set_choices(x: any, v: any): any;
+  grok_renderMultipleHistograms(g: any, bounds: any, histograms: any, categoryColumn: any, colors: any, tension: Num, normalize: Bool, markerSize: Num, fill: Bool, minBin: Num, maxBin: Num, localMaximum: Bool, highlightedHistogram: Num): any;
 
   // Generated from ../d4/lib/src/viewers/grid/grid.api.g.dart
   grok_GridCellStyle_Create(): any;
@@ -2149,53 +2198,6 @@ export interface IDartApi {
   // Generated from ../d4/lib/src/widgets/widgets.api.g.dart
   grok_InputType_Create(): any;
 
-  // Generated from ../d4/lib/src/grid/grid.api.g.dart
-  grok_GridCellStyle_Create(): any;
-  grok_GridCellStyle_Get_defaultStyle(): any;
-  grok_GridCellStyle_Set_defaultStyle(v: any): any;
-  grok_GridCellStyle_Get_textStyle(): any;
-  grok_GridCellStyle_Set_textStyle(v: any): any;
-  grok_GridCellStyle_Get_numberStyle(): any;
-  grok_GridCellStyle_Set_numberStyle(v: any): any;
-  grok_GridCellStyle_Get_styles(): any;
-  grok_GridCellStyle_Set_styles(v: any): any;
-  grok_GridCellStyle_Get_font(x: any): any;
-  grok_GridCellStyle_Set_font(x: any, v: String): any;
-  grok_GridCellStyle_Get_horzAlign(x: any): any;
-  grok_GridCellStyle_Set_horzAlign(x: any, v: String): any;
-  grok_GridCellStyle_Get_vertAlign(x: any): any;
-  grok_GridCellStyle_Set_vertAlign(x: any, v: String): any;
-  grok_GridCellStyle_Get_tooltip(x: any): any;
-  grok_GridCellStyle_Set_tooltip(x: any, v: String): any;
-  grok_GridCellStyle_Get_cursor(x: any): any;
-  grok_GridCellStyle_Set_cursor(x: any, v: String): any;
-  grok_GridCellStyle_Get_textWrap(x: any): any;
-  grok_GridCellStyle_Set_textWrap(x: any, v: String): any;
-  grok_GridCellStyle_Get_marker(x: any): any;
-  grok_GridCellStyle_Set_marker(x: any, v: String): any;
-  grok_GridCellStyle_Get_textColor(x: any): any;
-  grok_GridCellStyle_Set_textColor(x: any, v: Num): any;
-  grok_GridCellStyle_Get_backColor(x: any): any;
-  grok_GridCellStyle_Set_backColor(x: any, v: Num): any;
-  grok_GridCellStyle_Get_marginLeft(x: any): any;
-  grok_GridCellStyle_Set_marginLeft(x: any, v: Num): any;
-  grok_GridCellStyle_Get_marginRight(x: any): any;
-  grok_GridCellStyle_Set_marginRight(x: any, v: Num): any;
-  grok_GridCellStyle_Get_marginTop(x: any): any;
-  grok_GridCellStyle_Set_marginTop(x: any, v: Num): any;
-  grok_GridCellStyle_Get_marginBottom(x: any): any;
-  grok_GridCellStyle_Set_marginBottom(x: any, v: Num): any;
-  grok_GridCellStyle_Get_textVertical(x: any): any;
-  grok_GridCellStyle_Set_textVertical(x: any, v: Bool): any;
-  grok_GridCellStyle_Get_imageScale(x: any): any;
-  grok_GridCellStyle_Set_imageScale(x: any, v: Num): any;
-  grok_GridCellStyle_Get_opacity(x: any): any;
-  grok_GridCellStyle_Set_opacity(x: any, v: Num): any;
-  grok_GridCellStyle_Get_clip(x: any): any;
-  grok_GridCellStyle_Set_clip(x: any, v: Bool): any;
-  grok_GridCellStyle_Get_element(x: any): any;
-  grok_GridCellStyle_Set_element(x: any, v: any): any;
-  grok_GridCellStyle_Get_choices(x: any): any;
-  grok_GridCellStyle_Set_choices(x: any, v: any): any;
-  grok_renderMultipleHistograms(g: any, bounds: any, histograms: any, categoryColumn: any, colors: any, tension: Num, normalize: Bool, markerSize: Num, fill: Bool, minBin: Num, maxBin: Num, localMaximum: Bool, highlightedHistogram: Num): any;
+  // Generated from ../d4/lib/src/common/common.api.g.dart
+  grok_UsageType_Create(): any;
 }
