b9fd86cb2717770c9dc59c8084195c4f84e9c33f
drizhina <drizhinashvili@datagrok.ai>
2026-09-11 17:27:03 +0300

    Bio: Correct package settings assignment

--- packages/Bio/src/package.ts
+++ packages/Bio/src/package.ts
@@ -1899,7 +1899,7 @@ async function initBioInt() {
   // first make sure chem and rdkit module are loaded
   const rdKitModule = await getRdKitModule();
   // then load package settings
-  const bioPkgProps = new BioPackageProperties(Object.entries(_package.settings ?? {}));
+  const bioPkgProps = new BioPackageProperties(_package.settings ?? {});
   _package.properties = bioPkgProps;
   // then load monomer lib
   const libHelper = await MonomerLibManager.getInstance();
