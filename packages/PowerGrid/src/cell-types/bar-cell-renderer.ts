c26b3e398d676c543b955d2075e967d5109e605f
Dmytro Kovalyov <laykdimon278@gmail.com>
2026-09-15 21:42:54 +0300

    GROK-20888: Summary columns: Fixed adding Tags renderer just renders the whole grid as blue color

--- packages/PowerGrid/src/cell-types/bar-cell-renderer.ts
+++ packages/PowerGrid/src/cell-types/bar-cell-renderer.ts
@@ -26,6 +26,7 @@ export class BarCellRenderer extends DG.GridCellRenderer {
 
     const ratio = gridCell.cell.value / gridCell.cell.column.max;
     g.fillStyle = '#006400';
+    g.beginPath();
     g.roundRect(x + 2, y + 4, (w - 4) * ratio, h - 8, 4).fill();
   }
 }
